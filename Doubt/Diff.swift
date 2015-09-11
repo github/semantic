367e3b0ebc6b0b793b0d87e2a93a768d6257dd00
Rob Rix <rob.rix@github.com>
2015-09-11 11:38:42 -0400

    Diffing is defined over Fix.

--- Doubt/Diff.swift
+++ Doubt/Diff.swift
@@ -1,5 +1,5 @@
 public enum Diff: CustomDocConvertible, Equatable {
-	case Patch(Syntax<Fix>, Syntax<Fix>)
+	case Patch(Fix, Fix)
 	case Copy(() -> Syntax<Diff>)
 
 	public var doc: Doc<Pretty> {
@@ -23,16 +23,16 @@ public enum Diff: CustomDocConvertible, Equatable {
 		}
 	}
 
-	public init(_ a: Syntax<Fix>, _ b: Syntax<Fix>) {
-		switch (a, b) {
+	public init(_ a: Fix, _ b: Fix) {
+		switch (a.out, b.out) {
 		case let (.Apply(a, aa), .Apply(b, bb)):
-			self = .Copy({ .Apply(Diff(a.out, b.out), Array(zip(aa, bb).lazy.map { ($0.out, $1.out) }.map(Diff.init))) })
+			self = .Copy({ .Apply(Diff(a, b), Array(zip(aa, bb).lazy.map(Diff.init))) })
 
 		case let (.Abstract(p1, b1), .Abstract(p2, b2)):
-			self = .Copy({ .Abstract(Array(zip(p1, p2).lazy.map { ($0.out, $1.out) }.map(Diff.init)), Diff(b1.out, b2.out)) })
+			self = .Copy({ .Abstract(Array(zip(p1, p2).lazy.map(Diff.init)), Diff(b1, b2)) })
 
 		case let (.Assign(n1, v1), .Assign(n2, v2)) where n1 == n2:
-			self = .Copy({ .Assign(n2, Diff(v1.out, v2.out)) })
+			self = .Copy({ .Assign(n2, Diff(v1, v2)) })
 
 		case let (.Variable(n1), .Variable(n2)) where n1 == n2:
 			self = .Copy({ .Variable(n2) })
@@ -41,7 +41,7 @@ public enum Diff: CustomDocConvertible, Equatable {
 			self = .Copy({ .Literal(v2) })
 
 		case let (.Group(n1, v1), .Group(n2, v2)):
-			self = .Copy({ .Group(Diff(n1.out, n2.out), Array(zip(v1, v2).lazy.map { ($0.out, $1.out) }.map(Diff.init))) })
+			self = .Copy({ .Group(Diff(n1, n2), Array(zip(v1, v2).lazy.map(Diff.init))) })
 
 		default:
 			self = .Patch(a, b)
