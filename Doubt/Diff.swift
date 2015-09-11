e829798d591b63a716e1b4048c4a1b9fff008b2c
Rob Rix <rob.rix@github.com>
2015-09-11 11:35:51 -0400

    Rewrite the diff function as an initializer.

--- Doubt/Diff.swift
+++ Doubt/Diff.swift
@@ -22,30 +22,30 @@ public enum Diff: CustomDocConvertible, Equatable {
 			return ".Copy(\(a()))"
 		}
 	}
-}
 
+	public init(_ a: Syntax<Fix>, _ b: Syntax<Fix>) {
+		switch (a, b) {
+		case let (.Apply(a, aa), .Apply(b, bb)):
+			self = .Copy({ .Apply(Diff(a.out, b.out), Array(zip(aa, bb).lazy.map { ($0.out, $1.out) }.map(Diff.init))) })
 
-public func diff(a: Syntax<Fix>, _ b: Syntax<Fix>) -> Diff {
-	switch (a, b) {
-	case let (.Apply(a, aa), .Apply(b, bb)):
-		return .Copy({ .Apply(diff(a.out, b.out), Array(zip(aa, bb).lazy.map { ($0.out, $1.out) }.map(diff))) })
+		case let (.Abstract(p1, b1), .Abstract(p2, b2)):
+			self = .Copy({ .Abstract(Array(zip(p1, p2).lazy.map { ($0.out, $1.out) }.map(Diff.init)), Diff(b1.out, b2.out)) })
 
-	case let (.Abstract(p1, b1), .Abstract(p2, b2)):
-		return .Copy({ .Abstract(Array(zip(p1, p2).lazy.map { ($0.out, $1.out) }.map(diff)), diff(b1.out, b2.out)) })
+		case let (.Assign(n1, v1), .Assign(n2, v2)) where n1 == n2:
+			self = .Copy({ .Assign(n2, Diff(v1.out, v2.out)) })
 
-	case let (.Assign(n1, v1), .Assign(n2, v2)) where n1 == n2:
-		return .Copy({ .Assign(n2, diff(v1.out, v2.out)) })
+		case let (.Variable(n1), .Variable(n2)) where n1 == n2:
+			self = .Copy({ .Variable(n2) })
 
-	case let (.Variable(n1), .Variable(n2)) where n1 == n2:
-		return .Copy({ .Variable(n2) })
+		case let (.Literal(v1), .Literal(v2)) where v1 == v2:
+			self = .Copy({ .Literal(v2) })
 
-	case let (.Literal(v1), .Literal(v2)) where v1 == v2:
-		return .Copy({ .Literal(v2) })
+		case let (.Group(n1, v1), .Group(n2, v2)):
+			self = .Copy({ .Group(Diff(n1.out, n2.out), Array(zip(v1, v2).lazy.map { ($0.out, $1.out) }.map(Diff.init))) })
 
-	case let (.Group(n1, v1), .Group(n2, v2)):
-		return .Copy({ .Group(diff(n1.out, n2.out), Array(zip(v1, v2).lazy.map { ($0.out, $1.out) }.map(diff))) })
-
-	default:
-		return .Patch(a, b)
+		default:
+			self = .Patch(a, b)
+		}
 	}
 }
+
