8bf34647645f76d77a429ddaa5a4f4b5a7954fde
Rob Rix <rob.rix@github.com>
2015-09-15 14:29:54 -0400

    Test the production of copying diffs.

--- prototype/DoubtTests/DoubtTests.swift
+++ prototype/DoubtTests/DoubtTests.swift
@@ -1,4 +1,10 @@
-final class DoubtTests: XCTestCase {}
+final class DoubtTests: XCTestCase {
+	func testEqualSyntaxResultsInRecursivelyCopyingDiff() {
+		if let s = sexpr("\t(\n( a) \n)\t")?.value, t = sexpr("((a))")?.value {
+			XCTAssertEqual(Diff(s, t), Diff.Copy(.Apply(.Copy(.Apply(.Copy(.Variable("a")), [])), [])))
+		}
+	}
+}
 
 let atom = Syntax<Fix>.Variable <^> ^("abcdefghijklmnopqrstuvwxyz".characters.map { String($0) })
 let ws = ^(" \t\n".characters.map { String($0) })
