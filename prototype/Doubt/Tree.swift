d23d646f66de6eaa1badd9599574dff5ad2c4c3f
Rob Rix <rob.rix@github.com>
2015-09-15 12:52:07 -0400

    Tree is CustomStringConvertible.

--- prototype/Doubt/Tree.swift
+++ prototype/Doubt/Tree.swift
@@ -1,6 +1,15 @@
 //  Copyright © 2015 GitHub. All rights reserved.
 
-public enum Tree<A> {
+public enum Tree<A>: CustomStringConvertible {
 	case Leaf(A)
 	case Branch([Tree])
+
+	public var description: String {
+		switch self {
+		case let .Leaf(value):
+			return String(value)
+		case let .Branch(children):
+			return "(" + children.lazy.map { String($0) }.joinWithSeparator(" ") + ")"
+		}
+	}
 }
