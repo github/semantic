02de7f075e46fe65c518aa8662676246c9a640b2
Rob Rix <rob.rix@github.com>
2015-09-15 14:28:32 -0400

    Add the sexpr grammar to the tests.

--- prototype/DoubtTests/DoubtTests.swift
+++ prototype/DoubtTests/DoubtTests.swift
@@ -1,3 +1,18 @@
 final class DoubtTests: XCTestCase {}
 
+let atom = Syntax<Fix>.Variable <^> ^("abcdefghijklmnopqrstuvwxyz".characters.map { String($0) })
+let ws = ^(" \t\n".characters.map { String($0) })
+
+let sexpr: String -> State<Fix>? = fix { sexpr in
+	let list = Syntax<Fix>.Apply <^> (ws* *> ^"(" *> ws* *> sexpr <*> sexpr* <* ^")")
+	return Fix.init <^> (atom <|> list) <* ws*
+}
+
+
+func fix<T, U>(f: (T -> U) -> T -> U) -> T -> U {
+	return { f(fix(f))($0) }
+}
+
+
+import Doubt
 import XCTest
