8a2ff37cb9dc49868e75e53ce73a18eabc93ddb8
Rob Rix <rob.rix@github.com>
2015-09-16 10:02:20 -0400

    (Failing) test of empty/non-empty terms.

--- prototype/DoubtTests/DiffTests.swift
+++ prototype/DoubtTests/DiffTests.swift
@@ -2,7 +2,14 @@ final class DiffTests: XCTestCase {
 	func testSESOverEmptyCollectionsIsEmpty() {
 		XCTAssertEqual(Diff.diff([], []), [])
 	}
+
+	func testSESOverEmptyAndNonEmptyCollectionsIsInsertions() {
+		XCTAssertEqual(Diff.diff([], [ a, b ]), [ Diff.Patch(.Empty, a), Diff.Patch(.Empty, b) ])
+	}
 }
+
+private let a = Fix(.Literal("a"))
+private let b = Fix(.Literal("b"))
 
 import Doubt
 import XCTest
