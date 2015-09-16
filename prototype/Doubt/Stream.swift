fc7fa9db3b5460c223a67c5e92aefbdd576ac94a
Rob Rix <rob.rix@github.com>
2015-09-16 11:54:19 -0400

    Construct Streams with sequences, generators, and sources.

--- prototype/Doubt/Stream.swift
+++ prototype/Doubt/Stream.swift
@@ -2,6 +2,18 @@ public enum Stream<A> {
 	case Nil
 	case Cons(A, () -> Stream)
 
+	public init<S: SequenceType where S.Generator.Element == A>(sequence: S) {
+		self = Stream(generator: sequence.generate())
+	}
+
+	public init<G: GeneratorType where G.Element == A>(var generator: G) {
+		self = Stream { generator.next() }
+	}
+
+	public init(_ f: () -> A?) {
+		self = f().map { Stream.Cons($0, { Stream(f) }) } ?? Stream.Nil
+	}
+
 	public var uncons: (first: A, rest: Stream)? {
 		switch self {
 		case let .Cons(first, rest):
