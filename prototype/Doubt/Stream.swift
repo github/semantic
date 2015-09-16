bec4764a39441a402b48f9f2a1dc9a3bd79abce0
Rob Rix <rob.rix@github.com>
2015-09-16 11:44:56 -0400

    Stream unconsing.

--- prototype/Doubt/Stream.swift
+++ prototype/Doubt/Stream.swift
@@ -1,4 +1,21 @@
 public enum Stream<A> {
 	case Nil
 	case Cons(A, () -> Stream)
+
+	public var uncons: (first: A, rest: Stream)? {
+		switch self {
+		case let .Cons(first, rest):
+			return (first, rest())
+		default:
+			return nil
+		}
+	}
+
+	public var first: A? {
+		return uncons?.first
+	}
+
+	public var rest: Stream {
+		return uncons?.rest ?? .Nil
+	}
 }
