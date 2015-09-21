aa82ed1a2ba29997cd0ede9e782c2d9dc58f9cb4
Rob Rix <rob.rix@github.com>
2015-09-21 17:35:38 -0400

    Remove Swift AST parsing.

--- prototype/Doubt/Swift.swift
+++ prototype/Doubt/Swift.swift
@@ -3,28 +3,4 @@ enum SwiftAST: Equatable {
 	case Symbol(String, [String])
 	case KeyValue(String, String)
 	case Branch(String, [SwiftAST])
-
-	struct Parsers {
-		static let alphabetic = ^"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".characters
-		static let ws = ^" \t\n".characters
-
-		static let word = concat <^> (alphabetic <|> ^"_")+
-		static let atom = concat <^> not(ws <|> ^")")+
-		static let quoted = join(^"'", join((concat <^> not(^"'")*), ^"'"))
-
-		static let symbol = SwiftAST.Symbol <^> (join(^"\"", join((concat <^> not(^"\"")*), ^"\"")) <*> (^"<" *> interpolate(concat <^> alphabetic+, ^"," <* ws*) <* ^">"))
-		static let keyValue = KeyValue <^> (word <* ^"=" <*> (quoted <|> atom))
-		static let branch: String -> State<SwiftAST>? = Branch <^> (^"(" *> ws* *> word <* ws* <*> sexpr* <* ws* <* ^")")
-		static let sexpr = delay { (branch <|> symbol <|> keyValue <|> (SwiftAST.Atom <^> atom)) <* ws* }
-
-		static let root = ws* *> sexpr*
-	}
-}
-
-private func join(a: String -> State<String>?, _ b: String -> State<String>?) -> String -> State<String>? {
-	return (+) <^> (a <*> b)
-}
-
-private func concat(strings: [String]) -> String {
-	return strings.joinWithSeparator("")
 }
--- prototype/DoubtTests/SwiftTests.swift
+++ prototype/DoubtTests/SwiftTests.swift
@@ -1,24 +1,4 @@
 final class SwiftTests: XCTestCase {
-	func testValuesCanBeAlphabetic() {
-		XCTAssertEqual(full(SwiftAST.Parsers.keyValue)("key=value"), .KeyValue("key", "value"))
-	}
-
-	func testKeyValueCanBeQuoted() {
-		XCTAssertEqual(full(SwiftAST.Parsers.keyValue)("key='value'"), .KeyValue("key", "'value'"))
-	}
-
-	func testQuotedMatchesQuotedStrings() {
-		XCTAssertEqual(full(SwiftAST.Parsers.quoted)("'value'"), "'value'")
-	}
-
-	func testBranchesStartWithAnIdentifier() {
-		XCTAssertEqual(full(SwiftAST.Parsers.branch)("(a b=c)"), .Branch("a", [ .KeyValue("b", "c") ]))
-	}
-
-	func testBranchesDoNotRequireChildren() {
-		XCTAssertEqual(full(SwiftAST.Parsers.branch)("(return_stmt)"), .Branch("return_stmt", []))
-	}
-
 	func testParsingAFile() {
 		guard let file = File(path: __FILE__) else {
 			XCTFail("Could not make a File from \(__FILE__)")
@@ -33,7 +13,7 @@ final class SwiftTests: XCTestCase {
 		}
 		let focus = prism.forward(toAnyObject(structure.dictionary))
 		XCTAssertEqual(focus?[0].0, "SwiftTests")
-		XCTAssertEqual(focus?[0].1 ?? [], ["testValuesCanBeAlphabetic()", "testKeyValueCanBeQuoted()", "testQuotedMatchesQuotedStrings()", "testBranchesStartWithAnIdentifier()", "testBranchesDoNotRequireChildren()", "testParsingAFile()"])
+		XCTAssertEqual(focus?[0].1 ?? [], ["testParsingAFile()"])
 	}
 }
 
