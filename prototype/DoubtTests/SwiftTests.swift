e54bc56d13a0161249f81568c88a5a1cf2edb3b8
Rob Rix <rob.rix@github.com>
2015-09-21 17:32:41 -0400

    Extract document structure with SourceKitten.

--- prototype/DoubtTests/SwiftTests.swift
+++ prototype/DoubtTests/SwiftTests.swift
@@ -18,8 +18,26 @@ final class SwiftTests: XCTestCase {
 	func testBranchesDoNotRequireChildren() {
 		XCTAssertEqual(full(SwiftAST.Parsers.branch)("(return_stmt)"), .Branch("return_stmt", []))
 	}
+
+	func testParsingAFile() {
+		guard let file = File(path: __FILE__) else {
+			XCTFail("Could not make a File from \(__FILE__)")
+			return
+		}
+
+		let structure = Structure(file: file)
+		print(toAnyObject(structure.dictionary))
+
+		let prism: Prism<AnyObject, [(String, [String])]> = JSON.JSON.dictionary["key.substructure"].array.map {
+			$0.dictionary["key.name"].string &&& $0.dictionary["key.substructure"].array.map { $0.dictionary["key.name"].string }
+		}
+		let focus = prism.forward(toAnyObject(structure.dictionary))
+		XCTAssertEqual(focus?[0].0, "SwiftTests")
+		XCTAssertEqual(focus?[0].1 ?? [], ["testValuesCanBeAlphabetic()", "testKeyValueCanBeQuoted()", "testQuotedMatchesQuotedStrings()", "testBranchesStartWithAnIdentifier()", "testBranchesDoNotRequireChildren()", "testParsingAFile()"])
+	}
 }
 
 
 @testable import Doubt
+import SourceKittenFramework
 import XCTest
