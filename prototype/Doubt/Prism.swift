13f1d70071103825523b5c3967b25e9f8c0d0dd1
Rob Rix <rob.rix@github.com>
2015-09-21 16:45:52 -0400

    We really don’t need this old thing.

--- prototype/Doubt/Prism.swift
+++ prototype/Doubt/Prism.swift
@@ -8,12 +8,6 @@ public struct Prism<From, To> {
 	public let backward: To -> From
 }
 
-extension Dictionary {
-	static func prism(key: Key) -> Prism<[Key:Value], Value> {
-		return Prism(forward: { $0[key] }, backward: { [key: $0] })
-	}
-}
-
 public func >>> <From, Part, To> (left: Prism<From, Part>, right: Prism<Part, To>) -> Prism<From, To> {
 	return Prism(forward: { left.forward($0).flatMap(right.forward) }, backward: right.backward >>> left.backward)
 }
