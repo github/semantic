3ff518ac46255390f67fe0a0fdf77a2a56b83461
Rob Rix <rob.rix@github.com>
2015-09-21 15:19:42 -0400

    Add a dictionary constructor over key/value pairs.

---
+++ prototype/Doubt/Dictionary.swift
@@ -0,0 +1,8 @@
+extension Dictionary {
+	init<C: CollectionType where C.Generator.Element == (Key, Value)>(elements: C) {
+		self.init(minimumCapacity: Int(elements.count.toIntMax()))
+		for (key, value) in elements {
+			self[key] = value
+		}
+	}
+}
