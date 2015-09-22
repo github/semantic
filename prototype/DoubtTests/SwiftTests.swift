b7647a46edb5d4c84d0e07ec76b8470b5610afd4
Rob Rix <rob.rix@github.com>
2015-09-22 17:17:58 -0400

    Use the temporary.

--- prototype/DoubtTests/SwiftTests.swift
+++ prototype/DoubtTests/SwiftTests.swift
@@ -13,7 +13,7 @@ final class SwiftTests: XCTestCase {
 		let prism: Prism<AnyObject, [(String, [String])]> = JSON.JSON.dictionary["key.substructure"].array.map {
 			$0.dictionary["key.name"].string &&& $0.dictionary["key.substructure"].array.map { $0.dictionary["key.name"].string }
 		}
-		let focus = prism.forward(toAnyObject(structure.dictionary))
+		let focus = prism.forward(dictionary)
 		XCTAssertEqual(focus?[0].0, "SwiftTests")
 		XCTAssertEqual(focus?[0].1 ?? [], ["testParsingAFile()"])
 	}
