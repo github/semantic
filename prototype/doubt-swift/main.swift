53270aacfa259de8d809e3326278b535eb1e40f6
Rob Rix <rob.rix@github.com>
2015-09-23 16:37:22 -0400

    Try to construct terms for the argument files.

--- prototype/doubt-swift/main.swift
+++ prototype/doubt-swift/main.swift
@@ -1,2 +1,20 @@
+import Doubt
+import SourceKittenFramework
+
 let arguments = BoundsCheckedArray(array: Process.arguments)
 
+extension Term {
+	init?(path: String) {
+		guard let term = File(path: path)
+			.map(Structure.init)
+			.map({ $0.dictionary })
+			.map(toAnyObject)
+			.flatMap({ JSON(object: $0).map { Term(path: path, JSON: $0) } }) else { return nil }
+		self = term
+	}
+}
+
+if let a = arguments[1].map(Term.init), b = arguments[2].map(Term.init) {
+	print(a)
+	print(b)
+}
