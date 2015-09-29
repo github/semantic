278bba4f829bb174b51fb38586c862101c9ddef6
Rob Rix <rob.rix@github.com>
2015-09-29 17:00:26 -0400

    Add an algebraic Hash type.

---
+++ prototype/Doubt/Hash.swift
@@ -0,0 +1,41 @@
+public enum Hash: Hashable {
+	case Sequence([Hash])
+	case String(Swift.String)
+	case Int(Swift.Int)
+
+
+	public var hashValue: Swift.Int {
+		switch self {
+		case let .Sequence(s):
+			// Bob Jenkins’ one-at-a-time hash: https://en.wikipedia.org/wiki/Jenkins_hash_function
+			var hash = 0
+			for each in s {
+				hash += each.hashValue
+				hash += hash << 10
+				hash ^= hash >> 6
+			}
+			hash += hash << 3
+			hash ^= hash >> 11
+			hash += hash << 15
+			return hash
+		case let .String(s):
+			return s.hashValue
+		case let .Int(i):
+			return i.hashValue
+		}
+	}
+}
+
+public func == (left: Hash, right: Hash) -> Bool {
+	switch (left, right) {
+	case let (.Sequence(a), .Sequence(b)):
+		return a == b
+	case let (.String(a), .String(b)):
+		return a == b
+	case let (.Int(a), .Int(b)):
+		return a == b
+	default:
+		return false
+	}
+}
+
