146f59bc23c2c4d9e6489e0de6a8c9b59c2b772c
Rob Rix <rob.rix@github.com>
2015-10-01 15:16:25 -0400

    Move Vertex equality to its file.

--- prototype/Doubt/Equatable.swift
+++ prototype/Doubt/Equatable.swift
@@ -36,13 +36,3 @@ public func == (left: Doc, right: Doc) -> Bool {
 	}
 }
 
-public func == <A: Equatable> (left: Vertex<A>, right: Vertex<A>) -> Bool {
-	switch (left, right) {
-	case (.End, .End):
-		return true
-	case let (.XY(a, x1, y1), .XY(b, x2, y2)):
-		return a == b && x1.value == x2.value && y1.value == y2.value
-	default:
-		return false
-	}
-}
