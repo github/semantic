a2a55531c9d278bd80e1e09011ade129f33cf50a
Rob Rix <rob.rix@github.com>
2015-10-01 09:59:26 -0400

    AnyHashable.

---
+++ prototype/Doubt/AnyHashable.swift
@@ -0,0 +1,17 @@
+public enum AnyHashable<A>: Hashable {
+	case External(A, (A, A) -> Bool, A -> Int)
+
+	public var hashValue: Int {
+		switch self {
+		case let .External(a, _, hash):
+			return hash(a)
+		}
+	}
+}
+
+public func == <A> (left: AnyHashable<A>, right: AnyHashable<A>) -> Bool {
+	switch (left, right) {
+	case let (.External(a, eq, _), .External(b, _, _)):
+		return eq(a, b)
+	}
+}
