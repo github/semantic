3ea42437f91c06b640bc46b79343e5d633876af3
Rob Rix <rob.rix@github.com>
2015-10-01 10:31:18 -0400

    Abstract AnyHashable over AnyEquatable.

--- prototype/Doubt/AnyHashable.swift
+++ prototype/Doubt/AnyHashable.swift
@@ -1,24 +1,24 @@
 public enum AnyHashable<A>: Hashable {
-	case External(A, (A, A) -> Bool, A -> Int)
+	case External(AnyEquatable<A>, A -> Int)
 
 	public var value: A {
 		switch self {
-		case let .External(a, _, _):
-			return a
+		case let .External(a, _):
+			return a.value
 		}
 	}
 
 	public var hashValue: Int {
 		switch self {
-		case let .External(a, _, hash):
-			return hash(a)
+		case let .External(a, hash):
+			return hash(a.value)
 		}
 	}
 }
 
 public func == <A> (left: AnyHashable<A>, right: AnyHashable<A>) -> Bool {
 	switch (left, right) {
-	case let (.External(a, eq, _), .External(b, _, _)):
-		return eq(a, b)
+	case let (.External(a, _), .External(b, _)):
+		return a == b
 	}
 }
