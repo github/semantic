39bbe4df427f6871434d5d6ccdf4c7a08f296b72
Rob Rix <rob.rix@github.com>
2015-10-01 10:29:48 -0400

    AnyEquatable can unpack its value.

--- prototype/Doubt/AnyEquatable.swift
+++ prototype/Doubt/AnyEquatable.swift
@@ -1,5 +1,12 @@
 public enum AnyEquatable<A>: Equatable {
 	case External(A, (A, A) -> Bool)
+
+	public var value: A {
+		switch self {
+		case let .External(a, _):
+			return a
+		}
+	}
 }
 
 public func == <A> (left: AnyEquatable<A>, right: AnyEquatable<A>) -> Bool {
