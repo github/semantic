48c2112124cb56439139e13b51c2781a318ac664
Rob Rix <rob.rix@github.com>
2015-10-02 16:38:02 -0400

    Add a property destructuring Patches.

--- prototype/Doubt/Patch.swift
+++ prototype/Doubt/Patch.swift
@@ -1,4 +1,11 @@
 /// A patch to some part of a `Syntax` tree.
 public enum Patch<A> {
 	case Replace(Fix<A>?, Fix<A>?)
+
+	public var replace: (before: Fix<A>?, after: Fix<A>?) {
+		switch self {
+		case let .Replace(a, b):
+			return (a, b)
+		}
+	}
 }
