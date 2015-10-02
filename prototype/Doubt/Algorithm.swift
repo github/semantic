1b13580eb30563503a18184dbc3aa548d8f57a3e
Rob Rix <rob.rix@github.com>
2015-10-02 17:29:13 -0400

    Document the `Recursive` case.

--- prototype/Doubt/Algorithm.swift
+++ prototype/Doubt/Algorithm.swift
@@ -6,5 +6,8 @@ enum Algorithm<Recur, A> {
 	/// The type of `Diff`s which `Algorithm`s produce.
 	typealias Diff = Free<A, Patch<A>>
 
+	/// Indicates that diffing should recur into the enclosed `Term`.
+	///
+	/// When run, the enclosed function will be applied to the resulting `Diff`.
 	case Recursive(Term, Diff -> Recur)
 }
