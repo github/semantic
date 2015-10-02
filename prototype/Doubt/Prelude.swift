9a19d477df11f8368955ade8aee55e0a7ff67524
Rob Rix <rob.rix@github.com>
2015-10-02 16:48:44 -0400

    Parameterized Optional equality.

--- prototype/Doubt/Prelude.swift
+++ prototype/Doubt/Prelude.swift
@@ -12,3 +12,16 @@ func >>> <T, U, V> (f: T -> U, g: U -> V) -> T -> V {
 	return { g(f($0)) }
 }
 
+
+extension Optional {
+	static func equals(param: (Wrapped, Wrapped) -> Bool)(_ left: Wrapped?, _ right: Wrapped?) -> Bool {
+		switch (left, right) {
+		case let (.Some(a), .Some(b)):
+			return param(a, b)
+		case (.None, .None):
+			return true
+		default:
+			return false
+		}
+	}
+}
