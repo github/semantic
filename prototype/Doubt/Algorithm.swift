b30aa915021b124b070c81d5d15e956053798078
Rob Rix <rob.rix@github.com>
2015-10-02 17:30:09 -0400

    Algorithm is public.

--- prototype/Doubt/Algorithm.swift
+++ prototype/Doubt/Algorithm.swift
@@ -1,10 +1,10 @@
 /// A language of diffing algorithms.
-enum Algorithm<Recur, A> {
+public enum Algorithm<Recur, A> {
 	/// The type of `Term`s over which `Algorithm`s operate.
-	typealias Term = Fix<A>
+	public typealias Term = Fix<A>
 
 	/// The type of `Diff`s which `Algorithm`s produce.
-	typealias Diff = Free<A, Patch<A>>
+	public typealias Diff = Free<A, Patch<A>>
 
 	/// Indicates that diffing should recur into the enclosed `Term`.
 	///
