321622d553ffec5836268af110a4788b767fde95
Rob Rix <rob.rix@github.com>
2015-10-06 21:38:44 -0400

    Extract the discarding of null terms into a private helper function.

--- prototype/Doubt/Free.swift
+++ prototype/Doubt/Free.swift
@@ -91,6 +91,17 @@ public enum Free<A, B>: CustomDebugStringConvertible {
 extension Free where B: PatchConvertible, B.Info == A {
 	public typealias Term = Fix<A>
 
+	private func discardNullTerms(syntax: Syntax<Term?, A>) -> Term? {
+		switch syntax {
+		case let .Leaf(a):
+			return .In(.Leaf(a))
+		case let .Indexed(a):
+			return .In(.Indexed(a.flatMap(id)))
+		case let .Keyed(a):
+			return .In(.Keyed(Dictionary(elements: a.flatMap { k, v in v.map { (k, $0) } })))
+		}
+	}
+
 	public var before: Term? {
 		return map { $0.patch.state.before }.iterate {
 			switch $0 {
