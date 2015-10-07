eb62a4e08fd77f9beb1eeed0fcf484f60c054a2d
Rob Rix <rob.rix@github.com>
2015-10-07 19:36:53 -0400

    Dictionary.init(elements:) is public.

--- prototype/Doubt/Dictionary.swift
+++ prototype/Doubt/Dictionary.swift
@@ -1,5 +1,5 @@
 extension Dictionary {
-	init<C: CollectionType where C.Generator.Element == (Key, Value)>(elements: C) {
+	public init<C: CollectionType where C.Generator.Element == (Key, Value)>(elements: C) {
 		self.init(minimumCapacity: Int(elements.count.toIntMax()))
 		for (key, value) in elements {
 			self[key] = value
