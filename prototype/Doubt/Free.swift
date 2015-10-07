ee969a75faf8e5ffd18a901044fff64136636ec9
Rob Rix <rob.rix@github.com>
2015-10-06 21:35:47 -0400

    Compute the after state of diffs.

--- prototype/Doubt/Free.swift
+++ prototype/Doubt/Free.swift
@@ -103,6 +103,19 @@ extension Free where B: PatchConvertible, B.Info == A {
 			}
 		}
 	}
+
+	public var after: Term? {
+		return map { $0.patch.state.after }.iterate { syntax -> Term? in
+			switch syntax {
+			case let .Leaf(a):
+				return .In(.Leaf(a))
+			case let .Indexed(a):
+				return .In(.Indexed(a.flatMap(id)))
+			case let .Keyed(a):
+				return .In(.Keyed(Dictionary(elements: a.flatMap { k, v in v.map { (k, $0) } })))
+			}
+		}
+	}
 }
 
 
