6efa8249cd1711190f49f9dde3765b7cdcc17d21
Rob Rix <rob.rix@github.com>
2015-10-07 19:32:02 -0400

    Extension to construct `JSON` with a path.

--- prototype/doubt-json/main.swift
+++ prototype/doubt-json/main.swift
@@ -1,8 +1,17 @@
 import Doubt
+import Cocoa
 
 enum JSONLeaf {
 	case Number(Double)
 	case Boolean(Bool)
 	case String(Swift.String)
 	case Null
+}
+
+extension JSON {
+	init?(path: Swift.String) {
+		guard let data = try? NSData(contentsOfFile: path, options: []) else { return nil }
+		guard let object = try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) else { return nil }
+		self.init(object: object)
+	}
 }
