05e1b97c900fcc5baee262a3f8889de6885a1d63
Rob Rix <rob.rix@github.com>
2015-10-09 09:02:02 -0500

    I’m very confused as to how this has remained broken.

--- prototype/doubt-swift/main.swift
+++ prototype/doubt-swift/main.swift
@@ -104,5 +104,5 @@ extension Fix where A: StringConvertible {
 }
 
 if let a = arguments[1].flatMap({ Fix<Info>(path: $0) }), b = arguments[2].flatMap({ Fix<Info>(path: $0) }) {
-	print(FreeAlgorithm<Info, Free<Info, Patch<Info>>>(a, b).evaluate())
+	print(Algorithm<Info, Free<Info, Patch<Info>>>(a, b).evaluate())
 }
