ff559845d8fb5125869d316503db8fbd2447d85d
Rob Rix <rob.rix@github.com>
2015-10-14 16:07:07 -0400

    Add a couple of fixtures.

--- prototype/DoubtTests/AlgorithmTests.swift
+++ prototype/DoubtTests/AlgorithmTests.swift
@@ -3,6 +3,10 @@ final class AlgorithmTests: XCTestCase {
 }
 
 
+private let a = Cofree(0, [ Cofree(1, .Leaf("a")), Cofree(2, .Leaf("b")), Cofree(3, .Leaf("c")) ])
+private let b = Cofree(0, [ Cofree(1, .Leaf("c")), Cofree(2, .Leaf("b")), Cofree(3, .Leaf("a")) ])
+
+
 import Assertions
 @testable import Doubt
 import XCTest
