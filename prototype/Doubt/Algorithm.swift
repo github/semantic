7c099bac6ff42d08c138a9faee5bb875d384790e
Rob Rix <rob.rix@github.com>
2015-10-14 18:51:18 -0400

    Parameterize SES by the cost function.

--- prototype/Doubt/Algorithm.swift
+++ prototype/Doubt/Algorithm.swift
@@ -94,7 +94,7 @@ public enum Algorithm<Term: TermType, B> {
 			return f(Dictionary(elements: deleted + inserted + patched)).evaluate(equals, recur: recur)
 
 		case let .Roll(.ByIndex(a, b, f)):
-			return f(SES(a, b, recur: recur)).evaluate(equals, recur: recur)
+			return f(SES(a, b, cost: cost, recur: recur)).evaluate(equals, recur: recur)
 		}
 	}
 }
