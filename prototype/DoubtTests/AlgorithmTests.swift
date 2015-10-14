ed10ec12576f5138668ece876b47868ba0b32441
Rob Rix <rob.rix@github.com>
2015-10-14 16:10:23 -0400

    Write the fixtures using the typealias.

--- prototype/DoubtTests/AlgorithmTests.swift
+++ prototype/DoubtTests/AlgorithmTests.swift
@@ -6,8 +6,8 @@ final class AlgorithmTests: XCTestCase {
 private typealias Term = Cofree<String, Int>
 private typealias Diff = Free<String, Patch<Term>>
 
-private let a = Cofree(0, [ Cofree(1, .Leaf("a")), Cofree(2, .Leaf("b")), Cofree(3, .Leaf("c")) ])
-private let b = Cofree(0, [ Cofree(1, .Leaf("c")), Cofree(2, .Leaf("b")), Cofree(3, .Leaf("a")) ])
+private let a = Term(0, [ Term(1, .Leaf("a")), Term(2, .Leaf("b")), Term(3, .Leaf("c")) ])
+private let b = Term(0, [ Term(1, .Leaf("c")), Term(2, .Leaf("b")), Term(3, .Leaf("a")) ])
 
 
 import Assertions
