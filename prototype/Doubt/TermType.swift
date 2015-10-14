b4a95647474238705679433b4e72395e643d7ba1
Rob Rix <rob.rix@github.com>
2015-10-14 18:47:20 -0400

    TermType has a size property.

--- prototype/Doubt/TermType.swift
+++ prototype/Doubt/TermType.swift
@@ -10,6 +10,21 @@ extension TermType {
 	public func cata<Result>(transform: Syntax<Result, LeafType> -> Result) -> Result {
 		return self |> ({ $0.out } >>> { $0.map { $0.cata(transform) } } >>> transform)
 	}
+
+
+	/// The size of the receiver.
+	public var size: Int {
+		return cata {
+			switch $0 {
+			case .Leaf:
+				return 1
+			case let .Indexed(i):
+				return i.reduce(1, combine: +)
+			case let .Keyed(k):
+				return k.values.reduce(1, combine: +)
+			}
+		}
+	}
 }
 
 
