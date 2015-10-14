35617fee705376f32c6371f78132513c7bea8d06
Rob Rix <rob.rix@github.com>
2015-10-14 16:09:58 -0400

    Add typealiases for Diff and Term.

--- prototype/DoubtTests/AlgorithmTests.swift
+++ prototype/DoubtTests/AlgorithmTests.swift
@@ -3,6 +3,9 @@ final class AlgorithmTests: XCTestCase {
 }
 
 
+private typealias Term = Cofree<String, Int>
+private typealias Diff = Free<String, Patch<Term>>
+
 private let a = Cofree(0, [ Cofree(1, .Leaf("a")), Cofree(2, .Leaf("b")), Cofree(3, .Leaf("c")) ])
 private let b = Cofree(0, [ Cofree(1, .Leaf("c")), Cofree(2, .Leaf("b")), Cofree(3, .Leaf("a")) ])
 
