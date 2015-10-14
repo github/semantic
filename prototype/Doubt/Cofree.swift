ab3d297d9509678d977432853be57140f8e08b29
Rob Rix <rob.rix@github.com>
2015-10-14 10:19:02 -0400

    Document Cofree.

--- prototype/Doubt/Cofree.swift
+++ prototype/Doubt/Cofree.swift
@@ -1,5 +1,10 @@
 //  Copyright © 2015 GitHub. All rights reserved.
 
+/// The cofree comonad over `Syntax`.
+///
+/// This is “free” in the sense of “unconstrained” rather than “zero-cost”; it’s the comonad obtained by taking a functor (in this case `Syntax`) and adding the minimum necessary details (the `B` paired with it) to satisfy the comonad laws.
+///
+/// This type is dual to `Free`. Where `Free` is inhabited by syntax trees where some terms are replaced with `B`s, `Cofree` is inhabited by syntax trees where all terms are annotated with `B`s. In Doubt, this allows us to e.g. annotate terms with source range information, categorization, etc.
 public enum Cofree<A, B> {
 	indirect case Unroll(B, Syntax<Cofree, A>)
 
