79969bb69d2282fa9eb9ca2c2d6bd3df32ddaafc
Rob Rix <rob.rix@github.com>
2015-10-14 11:24:23 -0400

    Move FreeConvertible into Free.swift.

--- prototype/Doubt/Algorithm.swift
+++ prototype/Doubt/Algorithm.swift
@@ -128,20 +128,6 @@ extension Algorithm where A: Equatable {
 	}
 }
 
-/// A hack to work around the unavailability of same-type requirements.
-public protocol FreeConvertible {
-	typealias RollType
-	typealias PureType
-
-	init(free: Free<RollType, PureType>)
-	var free: Free<RollType, PureType> { get }
-}
-
-extension Free: FreeConvertible {
-	public init(free: Free<A, B>) { self = free }
-	public var free: Free { return self }
-}
-
 extension Algorithm where B: FreeConvertible, B.RollType == A, B.PureType == Patch<A> {
 	/// `Algorithm<A, Diff>`s can be constructed from a pair of `Term`s using `ByKey` when `Keyed`, `ByIndex` when `Indexed`, and `Recursive` otherwise.
 	public init(_ a: Term, _ b: Term) {
