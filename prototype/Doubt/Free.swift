6ee73cca0649a238b411e14857ab1527bd115a11
Rob Rix <rob.rix@github.com>
2015-10-15 11:18:06 -0400

    We don’t need Cofree terms to construct patches.

--- prototype/Doubt/Free.swift
+++ prototype/Doubt/Free.swift
@@ -121,8 +121,10 @@ extension Free where B: PatchConvertible, B.Element == Cofree<A, ()> {
 	public var inverse: Free {
 		return map { B(patch: $0.patch.inverse) }
 	}
+}
 
 
+extension Free where B: PatchConvertible {
 	public static func Replace(before: B.Element, _ after: B.Element) -> Free {
 		return .Pure(B(patch: .Replace(before, after)))
 	}
