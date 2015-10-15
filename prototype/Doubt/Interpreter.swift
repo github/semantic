0cedf87540e8458c57918ba152a02218a837c808
Rob Rix <rob.rix@github.com>
2015-10-15 11:01:35 -0400

    Interpreter has a Diff typealias.

--- prototype/Doubt/Interpreter.swift
+++ prototype/Doubt/Interpreter.swift
@@ -1,5 +1,7 @@
 /// An interpreter of `Algorithm`s.
 public struct Interpreter<Term: TermType> {
+	public typealias Diff = Free<Term.LeafType, Patch<Term>>
+
 	public let equals: (Term, Term) -> Bool
 
 	public func run() {}
