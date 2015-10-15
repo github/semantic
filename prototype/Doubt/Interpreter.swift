787681eafcc9b4bcd6c87998ca2e787f2e755397
Rob Rix <rob.rix@github.com>
2015-10-15 11:07:35 -0400

    Construct an algorithm.

--- prototype/Doubt/Interpreter.swift
+++ prototype/Doubt/Interpreter.swift
@@ -6,6 +6,18 @@ public struct Interpreter<Term: TermType> {
 
 	public func run(a: Term, _ b: Term) -> Diff {
 		if equals(a, b) { return Diff(b) }
+		let algorithm: Algorithm<Term, Diff>
+		switch (a.unwrap, b.unwrap) {
+		case let (.Keyed(a), .Keyed(b)):
+			algorithm = .Roll(.ByKey(a, b, Syntax.Keyed >>> Diff.Roll >>> Algorithm.Pure))
+		case let (.Indexed(a), .Indexed(b)):
+			algorithm = .Roll(.ByIndex(a, b, Syntax.Indexed >>> Diff.Roll >>> Algorithm.Pure))
+		default:
+			algorithm = .Roll(.Recursive(a, b, Algorithm.Pure))
+		}
 		return Diff.Pure(.Replace(a, b))
 	}
 }
+
+
+import Prelude
