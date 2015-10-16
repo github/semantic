570d4efa5d24e3a5b85b34b3687428c703069d16
Rob Rix <rob.rix@github.com>
2015-10-16 13:23:38 -0400

    Generate arbitrary leaves.

--- prototype/DoubtTests/RangedTerm.swift
+++ prototype/DoubtTests/RangedTerm.swift
@@ -1,11 +1,22 @@
 struct RangedTerm {
-	let term: Cofree<String, Range<String.Index>>
+	typealias Term = Cofree<String, (String, Range<Int>)>
+	let term: Term
 }
 
 
 extension RangedTerm: Arbitrary {
 	static var arbitrary: Gen<RangedTerm> {
-		return Gen.oneOf([])
+		func arbitrary(from: Int) -> Gen<Term> {
+			let leaf: Gen<Term> = String.arbitrary.fmap {
+				Cofree(($0, from..<Int($0.characters.count)), .Leaf($0))
+			}
+			return Gen.oneOf([
+				leaf,
+			])
+		}
+		return arbitrary(0).fmap {
+			RangedTerm(term: $0)
+		}
 	}
 }
 
