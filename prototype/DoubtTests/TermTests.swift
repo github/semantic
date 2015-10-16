541ea6b45c0f2cd4da6ba39272ae424a2ab6102e
Rob Rix <rob.rix@github.com>
2015-10-16 17:03:18 -0400

    Test that equality is reflexive.

--- prototype/DoubtTests/TermTests.swift
+++ prototype/DoubtTests/TermTests.swift
@@ -2,6 +2,12 @@ final class TermTests: XCTestCase {
 	override static func setUp() {
 		sranddev()
 	}
+
+	func testEqualityIsReflexive() {
+		property("equality is reflexive") <- forAll { (term: RangedTerm) in
+			Cofree.equals(annotation: ==, leaf: ==)(term.term, term.term)
+		}
+	}
 }
 
 
