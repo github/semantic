ce20112304f49171a90c972cd87ffb5170b32344
Rob Rix <rob.rix@github.com>
2015-10-16 17:14:08 -0400

    Test that equal terms produce identity diffs.

--- prototype/DoubtTests/DiffTests.swift
+++ prototype/DoubtTests/DiffTests.swift
@@ -7,6 +7,12 @@ final class DiffTests: XCTestCase {
 	typealias Diff = Free<String, Patch<Term>>
 
 	let interpreter = Interpreter<Term>(equal: ==, comparable: const(true), cost: Diff.sum(const(1)))
+
+	func testEqualTermsProduceIdentityDiffs() {
+		property("equal terms produce identity diffs") <- forAll { (term: RangedTerm) in
+			Diff.sum(const(1))(self.interpreter.run(term.term, term.term)) == 0
+		}
+	}
 }
 
 
