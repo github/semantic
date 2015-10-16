541897aa4392ea4367ab176bf0fa796e982ae85e
Rob Rix <rob.rix@github.com>
2015-10-16 15:05:01 -0400

    Arrange unannotated terms.

--- prototype/DoubtTests/RangedTerm.swift
+++ prototype/DoubtTests/RangedTerm.swift
@@ -1,23 +1,75 @@
 struct RangedTerm {
-	typealias Term = Cofree<String, (String, Range<Int>)>
+	typealias Term = Cofree<String, Range<String.Index>>
 	let term: Term
+	let source: String
 }
 
 struct UnannotatedTerm {
 	typealias Term = Cofree<String, ()>
 	let term: Term
 
+	static let indexed = (prefix: "[\n\t", separator: ",\n\t", suffix: "\n]")
+	static let keyed = (prefix: "[\n\t", separator: ",\n\t", suffix: "\n}")
+
 	var source: String {
+		let indexed = UnannotatedTerm.indexed
+		let keyed = UnannotatedTerm.keyed
 		return term.cata {
 			switch $0 {
 			case let .Leaf(s):
 				return s
 			case let .Indexed(s):
-				return "[\n\t" + s.joinWithSeparator(",\n\t") + "\n]"
+				return indexed.prefix + s.joinWithSeparator(indexed.separator) + indexed.suffix
 			case let .Keyed(s):
-				return "{\n\t" + s.map { "\"\($0)\": \($1)" }.joinWithSeparator(",\n\t") + "\n}"
+				return keyed.prefix + s.map { "\"\($0)\": \($1)" }.joinWithSeparator(keyed.separator) + keyed.suffix
 			}
 		}
+	}
+
+	var ranged: Cofree<String, Range<Int>> {
+		let indexed = UnannotatedTerm.indexed
+		let keyed = UnannotatedTerm.keyed
+		return term.cata {
+			switch $0 {
+			case let .Leaf(s):
+				return Cofree(0..<s.characters.count, .Leaf(s))
+			case let .Indexed(i):
+				var length = indexed.prefix.characters.count
+				var results: [Cofree<String, Range<Int>>] = []
+				for value in i {
+					if results.count > 0 {
+						length += indexed.separator.characters.count
+					}
+					results.append(value.map {
+						($0.startIndex + length)..<($0.endIndex + length)
+					})
+					length += value.extract.count
+				}
+				return Cofree(0..<(length + indexed.suffix.characters.count), .Indexed(results))
+			case let .Keyed(k):
+				var length = keyed.prefix.characters.count
+				var results: [(String, Cofree<String, Range<Int>>)] = []
+				for (key, value) in k {
+					if results.count > 0 {
+						length += keyed.separator.characters.count
+					}
+					results.append((key, value.map {
+						($0.startIndex + length)..<($0.endIndex + length)
+					}))
+					length += value.extract.count + 4 // for the characters around the key
+				}
+				return Cofree(0..<(length + keyed.suffix.characters.count), .Keyed(Dictionary(elements: results)))
+			}
+		}
+	}
+
+	var arranged: RangedTerm {
+		let source = self.source
+		return RangedTerm(
+			term: ranged.map {
+				source.startIndex.advancedBy($0.startIndex)..<source.startIndex.advancedBy($0.endIndex)
+			},
+			source: source)
 	}
 }
 
