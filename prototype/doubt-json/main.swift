f1e1541d8b7f5b6f404186a6323af773ea3a4ddc
Rob Rix <rob.rix@github.com>
2015-10-19 15:15:38 -0400

    Diff and serialize JSON strings.

--- prototype/doubt-json/main.swift
+++ prototype/doubt-json/main.swift
@@ -20,9 +20,31 @@ print(parse(json, input: dictWithMembers))
 let dictWithArray = "{\"hello\": [\"world\"],\"sup\": [\"cat\", \"dog\", \"keith\"] }"
 print(parse(json, input: dictWithArray))
 
-if let a = arguments[1].flatMap(JSON.init), b = arguments[2].flatMap(JSON.init) {
-	let diff = Interpreter(comparable: const(true), cost: Diff.sum(Patch.difference)).run(a.term, b.term)
-	if let JSON = NSString(data: diff.JSON(ifPure: { $0.JSON { $0.JSON } }, ifLeaf: { $0.JSON }).serialize(), encoding: NSUTF8StringEncoding) {
-		print(JSON)
-	}
+
+func data(path: String) -> NSData? {
+	return try? NSData(contentsOfFile: path, options: [])
+}
+
+func string(data: NSData) -> String? {
+	return NSString(data: data, encoding: NSUTF8StringEncoding) as String?
+}
+
+func parseJSON(string: String) -> CofreeJSON? {
+	return parse(json, input: string).right
+}
+
+func diffAndSerialize(a aString: String, b bString: String) -> String? {
+	guard let a = parseJSON(aString), b = parseJSON(bString) else { return nil }
+
+	let diff = Interpreter<CofreeJSON>(equal: CofreeJSON.equals(annotation: const(true), leaf: ==), comparable: const(true), cost: Free.sum(Patch.difference)).run(a, b)
+
+	guard let JSON = NSString(data: diff.JSON(ifPure: { $0.JSON(a: aString, b: bString) }, ifLeaf: { $0.JSON }).serialize(), encoding: NSUTF8StringEncoding) else { return nil }
+
+	return JSON as String
+}
+
+_ = diffAndSerialize(a: "{\"hello\":\"world\",\"sup\":\"cat\"}", b: "{\"hello\":\"world\",\"sup\":\"dog\"}").map { print($0) }
+
+if let aString = arguments[1].flatMap(data).flatMap(string), bString = arguments[2].flatMap(data).flatMap(string), JSON = diffAndSerialize(a: aString, b: bString) {
+	print(JSON)
 }
