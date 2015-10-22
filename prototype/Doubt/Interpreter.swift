f8739e90b006b97d64f7b9c6b8499358ede50f74
Rob Rix <rob.rix@github.com>
2015-10-22 18:42:04 -0400

    zip the terms.

--- prototype/Doubt/Interpreter.swift
+++ prototype/Doubt/Interpreter.swift
@@ -42,7 +42,7 @@ public struct Interpreter<Term: CofreeType> {
 
 	/// Diff `a` against `b`, if comparable.
 	private func recur(a: Term, _ b: Term) -> Diff? {
-		if equal(a, b) { return hylo(Diff.Roll, Term.unwrap)(b) }
+		if equal(a, b) { return hylo(Diff.Roll, { $0.unwrap })(Term.zip(a, b)!) }
 		guard comparable(a, b) else { return nil }
 
 		let algorithm: Algorithm<Term, Diff>
