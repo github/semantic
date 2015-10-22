695b69b086edf3242fa03f4cc832bf0cca0f162c
Rob Rix <rob.rix@github.com>
2015-10-22 10:26:45 -0400

    Algorithm’s parameter must be a CofreeType.

--- prototype/Doubt/Algorithm.swift
+++ prototype/Doubt/Algorithm.swift
@@ -3,7 +3,7 @@
 /// As with `Free`, this is “free” in the sense of “unconstrained,” i.e. “the monad induced by `Operation` without extra assumptions.”
 ///
 /// Where `Operation` models a single diffing strategy, `Algorithm` models the recursive selection of diffing strategies at each node. Thus, a value in `Algorithm` models an algorithm for constructing a value in the type `Result` from the resulting diffs. By this means, diffing can be adapted not just to the specific grammar, but to specific trees produced by that grammar, and even the values of type `A` encapsulated at each node.
-public enum Algorithm<Term: TermType, Result> {
+public enum Algorithm<Term: CofreeType, Result> {
 	/// The type of `Patch`es produced by `Algorithm`s.
 	public typealias Patch = Doubt.Patch<Term>
 
