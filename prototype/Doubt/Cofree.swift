810c6081b97b058e5cc6b82558231578ac9d63f3
Rob Rix <rob.rix@github.com>
2015-10-22 10:22:21 -0400

    Rename the B parameter to Annotation.

--- prototype/Doubt/Cofree.swift
+++ prototype/Doubt/Cofree.swift
@@ -1,10 +1,10 @@
 /// The cofree comonad over `Syntax`.
 ///
-/// This is “free” in the sense of “unconstrained” rather than “zero-cost”; it’s the comonad obtained by taking a functor (in this case `Syntax`) and adding the minimum necessary details (the `B` paired with it) to satisfy the comonad laws.
+/// This is “free” in the sense of “unconstrained” rather than “zero-cost”; it’s the comonad obtained by taking a functor (in this case `Syntax`) and adding the minimum necessary details (the `Annotation` paired with it) to satisfy the comonad laws.
 ///
-/// This type is dual to `Free`. Where `Free` is inhabited by syntax trees where some terms are replaced with `B`s, `Cofree` is inhabited by syntax trees where all terms are annotated with `B`s. In Doubt, this allows us to e.g. annotate terms with source range information, categorization, etc.
-public enum Cofree<A, B> {
-	indirect case Unroll(B, Syntax<Cofree, A>)
+/// This type is dual to `Free`. Where `Free` is inhabited by syntax trees where some terms are replaced with `Annotation`s, `Cofree` is inhabited by syntax trees where all terms are annotated with `Annotation`s. In Doubt, this allows us to e.g. annotate terms with source range information, categorization, etc.
+public enum Cofree<A, Annotation> {
+	indirect case Unroll(Annotation, Syntax<Cofree, A>)
 
 	public var unwrap: Syntax<Cofree, A> {
 		switch self {
@@ -14,7 +14,7 @@ public enum Cofree<A, B> {
 	}
 
 
-	public init(_ annotation: B, _ syntax: Syntax<Cofree, A>) {
+	public init(_ annotation: Annotation, _ syntax: Syntax<Cofree, A>) {
 		self = .Unroll(annotation, syntax)
 	}
 
@@ -24,7 +24,7 @@ public enum Cofree<A, B> {
 	/// The initial seed is used as the annotation of the returned value. The continuation of the structure is unpacked by applying `annotate` to the seed and mapping the resulting syntax’s values recursively. In this manner, the structure is unfolded bottom-up, starting with `seed` and ending at the leaves.
 	///
 	/// As this is the dual of `Free.iterate`, it’s unsurprising that we have a similar guarantee: coiteration is linear in the size of the constructed tree.
-	public static func coiterate(annotate: B -> Syntax<B, A>)(_ seed: B) -> Cofree {
+	public static func coiterate(annotate: Annotation -> Syntax<Annotation, A>)(_ seed: Annotation) -> Cofree {
 		return .Unroll(seed, annotate(seed).map(coiterate(annotate)))
 	}
 }
@@ -42,7 +42,7 @@ extension Cofree: CustomDebugStringConvertible {
 // MARK: - Functor
 
 extension Cofree {
-	public func map<Other>(transform: B -> Other) -> Cofree<A, Other> {
+	public func map<Other>(transform: Annotation -> Other) -> Cofree<A, Other> {
 		return .Unroll(transform(extract), unwrap.map { $0.map(transform) })
 	}
 }
@@ -52,7 +52,7 @@ extension Cofree {
 
 extension Cofree {
 	/// Returns the value annotating the syntax tree at this node.
-	public var extract: B {
+	public var extract: Annotation {
 		switch self {
 		case let .Unroll(b, _):
 			return b
@@ -65,7 +65,7 @@ extension Cofree {
 	}
 
 	/// Returns a new `Cofree` constructed by recursively annotating each subtree with itself.
-	public var duplicate: Cofree<A, Cofree<A, B>> {
+	public var duplicate: Cofree<A, Cofree<A, Annotation>> {
 		return extend(id)
 	}
 }
@@ -74,13 +74,13 @@ extension Cofree {
 // MARK: - Equality
 
 extension Cofree {
-	public static func equals(annotation annotation: (B, B) -> Bool, leaf: (A, A) -> Bool)(_ left: Cofree, _ right: Cofree) -> Bool {
+	public static func equals(annotation annotation: (Annotation, Annotation) -> Bool, leaf: (A, A) -> Bool)(_ left: Cofree, _ right: Cofree) -> Bool {
 		return annotation(left.extract, right.extract)
 			&& Syntax.equals(ifLeaf: leaf, ifRecur: Cofree.equals(annotation: annotation, leaf: leaf))(left.unwrap, right.unwrap)
 	}
 }
 
-public func == <A: Equatable, B: Equatable> (left: Cofree<A, B>, right: Cofree<A, B>) -> Bool {
+public func == <A: Equatable, Annotation: Equatable> (left: Cofree<A, Annotation>, right: Cofree<A, Annotation>) -> Bool {
 	return Cofree.equals(annotation: ==, leaf: ==)(left, right)
 }
 
@@ -88,7 +88,7 @@ public func == <A: Equatable, B: Equatable> (left: Cofree<A, B>, right: Cofree<A
 // MARK: - JSON
 
 extension Cofree {
-	public func JSON(annotation annotation: B -> Doubt.JSON, leaf: A -> Doubt.JSON) -> Doubt.JSON {
+	public func JSON(annotation annotation: Annotation -> Doubt.JSON, leaf: A -> Doubt.JSON) -> Doubt.JSON {
 		return [
 			"extract": annotation(extract),
 			"unwrap": unwrap.JSON(ifLeaf: leaf, ifRecur: { $0.JSON(annotation: annotation, leaf: leaf) })
@@ -96,7 +96,7 @@ extension Cofree {
 	}
 }
 
-extension Cofree where A: CustomJSONConvertible, B: CustomJSONConvertible {
+extension Cofree where A: CustomJSONConvertible, Annotation: CustomJSONConvertible {
 	public var JSON: Doubt.JSON {
 		return JSON(annotation: { $0.JSON }, leaf: { $0.JSON })
 	}
@@ -105,8 +105,8 @@ extension Cofree where A: CustomJSONConvertible, B: CustomJSONConvertible {
 
 // MARK: - Categorizable
 
-extension Cofree where B: Categorizable {
-	var categories: Set<B.Category> {
+extension Cofree where Annotation: Categorizable {
+	var categories: Set<Annotation.Category> {
 		return extract.categories
 	}
 }
