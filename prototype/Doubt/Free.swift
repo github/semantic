e7a525910fecc81fcc309c7e5fe23063aecc4d65
Rob Rix <rob.rix@github.com>
2015-10-22 12:49:41 -0400

    Implement anamorphism over Free.

--- prototype/Doubt/Free.swift
+++ prototype/Doubt/Free.swift
@@ -115,6 +115,15 @@ public enum Free<A, B>: CustomDebugStringConvertible, SyntaxConvertible {
 }
 
 
+// MARK: - Anamorphism
+
+extension Free {
+	public func ana<Seed>(f: Seed -> Syntax<Seed, A>)(_ seed: Seed) -> Free {
+		return (Roll <<< { $0.map(self.ana(f)) } <<< f)(seed)
+	}
+}
+
+
 extension Free where B: PatchType, B.Element == Cofree<A, ()> {
 	public typealias Term = B.Element
 
