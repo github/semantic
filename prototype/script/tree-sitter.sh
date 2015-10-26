0e7639f0d6214e4ec513f5ea6d3b763b2770ede3
Rob Rix <rob.rix@github.com>
2015-10-26 15:45:51 -0400

    Build and clean.

--- prototype/script/tree-sitter.sh
+++ prototype/script/tree-sitter.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 cd `dirname $0`/../External/tree-sitter
-script/configure.sh
-make compiler
-make runtime
+
+case "$1" in
+build)
+	script/configure.sh
+	make "$PRODUCT_NAME"
+	;;
+clean)
+	script/clean.sh
+	;;
+*)
+	echo "I don't know how to '$1'"
+	exit 1
+	;;
+esac
