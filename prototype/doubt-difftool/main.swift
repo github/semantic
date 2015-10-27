ce79e07cf8e2eb9310336a024cdbe248ecb76549
Rob Rix <rob.rix@github.com>
2015-10-27 11:04:00 -0400

    Attempt to open and read a file with `TSInput`.

--- prototype/doubt-difftool/main.swift
+++ prototype/doubt-difftool/main.swift
@@ -1,5 +1,21 @@
 import Cocoa
+import Darwin
 import Doubt
+extension TSInput {
+	init(path: String) {
+		let file = fopen(path, "r")
+		self.init(
+			payload: file,
+			read_fn: { (payload: UnsafeMutablePointer<Void>, bytesRead: UnsafeMutablePointer<Int>) -> UnsafePointer<Int8> in
+				let result = UnsafePointer<Int8>(fgets(nil, 100, UnsafeMutablePointer<FILE>(payload)))
+				bytesRead.memory = Int(strlen(result))
+				return result
+			},
+			seek_fn: { (payload: UnsafeMutablePointer<Void>, position: TSLength) -> Int32 in
+				fseek(UnsafeMutablePointer<FILE>(payload), position.bytes, SEEK_CUR)
+			})
+	}
+}
 
 
 let document = ts_document_make()
