3270ec91c1618bf3d07bf927ea61370181fff3ef
Rob Rix <rob.rix@github.com>
2015-10-27 11:08:28 -0400

    Term is in its own file.

---
+++ prototype/UI/term.js
@@ -0,0 +1,23 @@
+function termFromJSON(json) {
+	return new Term({
+		extract: json.extract,
+		unwrap: new Syntax(json.unwrap, function(x) {
+			return termFromJSON(x);
+		})
+	});
+}
+
+function Term(object) {
+	this.range = object.extract;
+	this.unwrap = object.unwrap;
+	return this;
+}
+
+/// Term -> String -> DOM
+function termToDOM(term, which, source) {
+	return rangeAndSyntaxToDOM(term.range, term.unwrap, source, function(term) {
+		return term.range;
+	}, function(term) {
+		return termToDOM(term, which, source);
+	});
+}
