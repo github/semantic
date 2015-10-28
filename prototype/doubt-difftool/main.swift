1ed4b683787581aef9be7ce5e1875be823f3b4a2
Rob Rix <rob.rix@github.com>
2015-10-28 18:45:50 -0400

    Restrict comparisons by category.

--- prototype/doubt-difftool/main.swift
+++ prototype/doubt-difftool/main.swift
@@ -80,7 +80,7 @@ func termWithInput(string: String) -> Term? {
 let arguments = BoundsCheckedArray(array: Process.arguments)
 if let aString = arguments[1].flatMap(readFile), bString = arguments[2].flatMap(readFile), c = arguments[3], ui = arguments[4] {
 	if let a = termWithInput(aString), b = termWithInput(bString) {
-		let diff = Interpreter<Term>(equal: Term.equals(annotation: const(true), leaf: ==), comparable: const(true), cost: Free.sum(Patch.difference)).run(a, b)
+		let diff = Interpreter<Term>(equal: Term.equals(annotation: const(true), leaf: ==), comparable: Interpreter<Term>.comparable { $0.extract.categories }, cost: Free.sum(Patch.difference)).run(a, b)
 		let range: Range<Int> -> Doubt.JSON = {
 			[
 				$0.startIndex.JSON,
