2e76f541eb12468a6b4d2c13ee673068b62f1416
Rob Rix <rob.rix@github.com>
2015-10-29 15:12:16 -0400

    Match against the predicate.

--- prototype/doubt-difftool/main.swift
+++ prototype/doubt-difftool/main.swift
@@ -46,7 +46,7 @@ func termWithInput(string: String) -> Term? {
 				let count = node.namedChildren.count
 				guard count > 0 else { return try Syntax.Leaf(node.substring(string)) }
 				switch category {
-				case "pair", "rel_op", "math_op", "bool_op", "bitwise_op", "type_op", "math_assignment", "assignment", "subscript_access", "member_access", "new_expression", "function_call", "function", "ternary":
+				case fixedProductions.contains:
 					return try .Fixed(node.namedChildren.map {
 						($0, try $0.category(document))
 					})
