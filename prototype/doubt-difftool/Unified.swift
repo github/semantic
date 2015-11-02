3d5b0115b208505f8dbf0ff8e9f4e97226c34307
Rob Rix <rob.rix@github.com>
2015-11-02 14:21:34 -0500

    Extract Patch stringification into a separate function.

--- prototype/doubt-difftool/Unified.swift
+++ prototype/doubt-difftool/Unified.swift
@@ -1,8 +1,12 @@
+func unified(patch: Patch<Term>, source: String) -> String {
+	return (patch.state.before.map { "{-\($0)-}" } ?? "")
+		+ (patch.state.after.map { "{+\($0)+}" } ?? "")
+}
+
 func unified(diff: Diff, before: String, after: String) -> String {
 	switch diff {
 	case let .Pure(patch):
-		return (patch.state.before.map { "{-\($0)-}" } ?? "")
-			+ (patch.state.after.map { "{+\($0)+}" } ?? "")
+		return unified(patch, source: after)
 
 	case .Roll:
 		return ""
