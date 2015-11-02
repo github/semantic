ba42033f0903e5587e2dcccba80bca979d8c4ebc
Rob Rix <rob.rix@github.com>
2015-11-02 11:37:54 -0500

    Retrieve the array of files.

--- prototype/doubt-difftool/Argument.swift
+++ prototype/doubt-difftool/Argument.swift
@@ -1,6 +1,15 @@
 enum Argument {
 	indirect case File(Source, Argument)
 	case End
+
+	var files: [Source] {
+		switch self {
+		case let .File(a, rest):
+			return [a] + rest.files
+		case .End:
+			return []
+		}
+	}
 }
 
 let argumentsParser: Madness.Parser<[String], Argument>.Function = none()
