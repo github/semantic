6f2176ca578b95799e4f726389419b486b0fd949
Rob Rix <rob.rix@github.com>
2015-11-02 16:02:58 -0500

    Compute the unified diff as a catamorphism.

--- prototype/doubt-difftool/Unified.swift
+++ prototype/doubt-difftool/Unified.swift
@@ -15,6 +15,25 @@ func unified(diff: Diff, before: String, after: String) -> String {
 	case .Roll:
 		return ""
 	}
+	return diff.map { (unified($0, source: after), range($0)) }.cata { info, syntax in
+		switch syntax {
+		case .Leaf:
+			return (String(after.utf16[info.1.range]), info.1.range)
+		case let .Indexed(i):
+			var previous = info.1.range.startIndex
+			var out: String = ""
+			for (string, range) in i {
+				out += String(after.utf16[previous..<range.startIndex])
+				out += string
+				previous = range.endIndex
+			}
+			return (out + String(after.utf16[previous..<info.1.range.endIndex]), info.1.range)
+		case let .Fixed(f):
+			return (f.map { $0.0 }.joinWithSeparator(""), info.1.range)
+		case let .Keyed(k):
+			return (k.values.map { $0.0 }.joinWithSeparator(""), info.1.range)
+		}
+	}.0
 }
 
 
