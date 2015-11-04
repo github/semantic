2cb3521cb039b93029dbcb135f4855ae1dadda4d
joshvera <vera@github.com>
2015-11-04 15:11:01 -0500

    Pass Line and Column to Info

--- prototype/doubt-difftool/Info.swift
+++ prototype/doubt-difftool/Info.swift
@@ -1,16 +1,24 @@
 struct Info: Categorizable, CustomJSONConvertible, Equatable {
-	init(range: Range<Int>, categories: Set<String>) {
+	init(range: Range<Int>, line: Line, column: Column, categories: Set<String>) {
 		self.range = range
+		self.line = line
+		self.column = column
 		self.categories = categories
 	}
 
-	init(range: Range<String.CharacterView.Index>, categories: Set<String>) {
+	init(range: Range<String.CharacterView.Index>, line: Line, column: Column, categories: Set<String>) {
 		// FIXME: this is terrible. see also https://github.com/github/semantic-diff/issues/136
 		self.range = Int(String(range.startIndex))!..<Int(String(range.endIndex))!
+		self.line = line
+		self.column = column
 		self.categories = categories
 	}
 
 	let range: Range<Int>
+
+	let line: Line
+	
+	let column: Column
 
 
 	// MARK: Categorizable
@@ -23,6 +31,8 @@ struct Info: Categorizable, CustomJSONConvertible, Equatable {
 	var JSON: Doubt.JSON {
 		return [
 			"range": range.JSON,
+			"line": line.JSON,
+			"column": column.JSON,
 			"categories": Array(categories).JSON
 		]
 	}
@@ -32,5 +42,5 @@ func == (left: Info, right: Info) -> Bool {
 	return left.range == right.range && left.categories == right.categories
 }
 
-
+import Madness
 import Doubt
