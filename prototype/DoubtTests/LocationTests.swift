f13d2068ab61eae8c5ef731ca2a6c4830d29ad05
Rob Rix <rob.rix@github.com>
2015-11-05 10:56:36 -0500

    Add some test fixtures.

--- prototype/DoubtTests/LocationTests.swift
+++ prototype/DoubtTests/LocationTests.swift
@@ -1,4 +1,17 @@
 final class LocationTests: XCTestCase {
 }
+
+
+private let leaf = Cofree(1, .Leaf("a string"))
+private let term: Cofree<String, Int> = Cofree(0, .Indexed([
+	leaf,
+	Cofree(2, .Leaf("b string")),
+	Cofree(3, .Keyed([
+		"a": Cofree(4, .Leaf("a nested string")),
+		"b": Cofree(5, .Leaf("b nested string")),
+	])),
+]))
+
+
 @testable import Doubt
 import XCTest
