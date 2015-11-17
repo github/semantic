a700b7f7ff2ec00fc1e9bbaa777f4ca4e3eac6d3
Rob Rix <rob.rix@github.com>
2015-11-17 15:39:51 -0800

    Derive functor.

--- src/Operation.hs
+++ src/Operation.hs
@@ -6,3 +6,4 @@ data Operation a f
   = Recur (Term a Info) (Term a Info) (Diff a -> f)
   | ByKey [(String, Term a Info)] [(String, Term a Info)] ([(String, Diff a)] -> f)
   | ByIndex [Term a Info] [Term a Info] ([Diff a] -> f)
+  deriving Functor
