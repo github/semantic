af280ae63dbce09006e0e091ee2b758b388b60d6
Rob Rix <rob.rix@github.com>
2015-11-17 15:40:03 -0800

    Rename the Recursive constructor.

--- src/Operation.hs
+++ src/Operation.hs
@@ -3,7 +3,7 @@ module Operation where
 import Diff
 
 data Operation a f
-  = Recur (Term a Info) (Term a Info) (Diff a -> f)
+  = Recursive (Term a Info) (Term a Info) (Diff a -> f)
   | ByKey [(String, Term a Info)] [(String, Term a Info)] ([(String, Diff a)] -> f)
   | ByIndex [Term a Info] [Term a Info] ([Diff a] -> f)
   deriving Functor
