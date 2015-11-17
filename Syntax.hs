1a9c5346f3d9994d1cf8ef2dd32113f76a032a10
Rob Rix <rob.rix@github.com>
2015-11-17 13:38:05 -0800

    Spaces.

--- Syntax.hs
+++ Syntax.hs
@@ -5,8 +5,8 @@ module Syntax where
 import Data.Map
 
 data Syntax f a =
-	Leaf a
-	| Indexed [f]
-	| Fixed [f]
-	| Keyed (Map String f)
-	deriving (Functor, Show, Eq)
+    Leaf a
+    | Indexed [f]
+    | Fixed [f]
+    | Keyed (Map String f)
+    deriving (Functor, Show, Eq)
