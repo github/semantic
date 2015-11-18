7c09fd1503a5ba81d4e490bdbfe90d9ccb5c2174
Rob Rix <rob.rix@github.com>
2015-11-17 17:05:16 -0800

    Stub in interpretation.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,1 +1,16 @@
 module Interpreter where
+
+import Algorithm
+import Control.Monad.Free
+import Control.Comonad.Cofree
+import Operation
+import Diff
+
+recur :: Term a Info -> Term a Info -> Maybe (Diff a)
+recur a b =
+  run algorithm where
+    algorithm = Free $ Recursive a b Pure
+
+
+run :: Algorithm a (Diff a) -> Maybe (Diff a)
+run (Pure diff) = Just diff
