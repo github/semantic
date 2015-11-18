9bafea4749f85a9b95123c8058b751f0259d4dc8
Rob Rix <rob.rix@github.com>
2015-11-18 07:25:42 -0800

    SES returns both terms.

--- src/SES.hs
+++ src/SES.hs
@@ -3,6 +3,6 @@ module SES where
 import Patch
 import Diff
 
-ses :: Eq a => [a] -> [a] -> [Either (Patch a) a]
+ses :: Eq a => [a] -> [a] -> [Either (Patch a) (a, a)]
 ses a b | Prelude.null a = (Left . Insert) <$> b
 ses a b | Prelude.null b = (Left . Delete) <$> a
