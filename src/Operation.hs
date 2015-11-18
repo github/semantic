e01e4bb923eb047f5b0c17c4ce937c932d7b0fb5
Rob Rix <rob.rix@github.com>
2015-11-17 18:56:28 -0800

    ByKey’s continuation takes a map.

--- src/Operation.hs
+++ src/Operation.hs
@@ -1,9 +1,10 @@
 module Operation where
 
 import Diff
+import Data.Map
 
 data Operation a f
   = Recursive (Term a Info) (Term a Info) (Diff a -> f)
-  | ByKey [(String, Term a Info)] [(String, Term a Info)] ([(String, Diff a)] -> f)
+  | ByKey [(String, Term a Info)] [(String, Term a Info)] ([Map String (Diff a)] -> f)
   | ByIndex [Term a Info] [Term a Info] ([Diff a] -> f)
   deriving Functor
