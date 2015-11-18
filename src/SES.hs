e742cf6859ca04bcc0ad0da57f19d789f737e825
Rob Rix <rob.rix@github.com>
2015-11-18 07:13:55 -0800

    Annotate the type of SES.
    I’m not at all sure this is correct.

--- src/SES.hs
+++ src/SES.hs
@@ -1,7 +1,9 @@
 module SES where
 
 import Patch
+import Diff
 import Control.Monad.Free
 
+ses :: Functor f => Eq a => [a] -> [a] -> [Free f (Patch a)]
 ses a b | Prelude.null a = (Pure . Insert) <$> b
 ses a b | Prelude.null b = (Pure . Delete) <$> a
