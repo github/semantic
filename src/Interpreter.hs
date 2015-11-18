e620eb76ff9d1e9c627496a1221c319e93e65edf
Rob Rix <rob.rix@github.com>
2015-11-17 17:29:18 -0800

    Stub in recurring through indexed terms.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -5,6 +5,7 @@ import Control.Monad.Free
 import Control.Comonad.Cofree
 import Operation
 import Diff
+import Syntax
 
 constructAndRun :: Term a Info -> Term a Info -> Maybe (Diff a)
 constructAndRun a b =
@@ -14,6 +15,8 @@ constructAndRun a b =
 run :: Algorithm a (Diff a) -> Maybe (Diff a)
 run (Pure diff) = Just diff
 run (Free (Recursive a b f)) = recur a b where
+  recur (_ :< Indexed a') (_ :< Indexed b') | length a' == length b' =
+    run $ f $ Pure Patch { old = Just a, new = Just b }
   recur _ _ = run $ f $ Pure Patch { old = Just a, new = Just b }
 
 interpret :: Term a Info -> Term a Info -> Diff a
