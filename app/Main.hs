5129bc455b3a42b05215f2edf8bda160f4d97b6f
Rob Rix <rob.rix@github.com>
2015-11-18 11:45:23 -0800

    Move the diff fixture thing to the executable.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import Diff
+import Patch
+import Term
+import Syntax
+import Control.Comonad.Cofree
+import Control.Monad.Free
+import Data.Map
+
+a :: Term String Info
+a = Info :< (Keyed $ fromList [
+  ("hello", Info :< Indexed [ Info :< Leaf "hi" ]),
+  ("goodbye", Info :< Leaf "goodbye") ])
+
+b :: Term String Info
+b = Info :< (Keyed $ fromList [
+  ("hello", Info :< Indexed []),
+  ("goodbye", Info :< Indexed []) ])
+
+d :: Diff String
+d = Free $ Keyed $ fromList [
+  ("hello", Free $ Indexed [ Pure . Delete $ Info :< Leaf "hi" ]),
+  ("goodbye", Pure $ Replace (Info :< Leaf "goodbye") (Info :< Indexed [])) ]
+
 main :: IO ()
 main = putStrLn "hello world"
--- src/Diff.hs
+++ src/Diff.hs
@@ -16,21 +16,6 @@ data Info = Info -- Range [String]
 
 type Diff a = Free (Syntax a) (Patch (Term a Info))
 
-a :: Term String Info
-a = Info :< (Keyed $ fromList [
-  ("hello", Info :< Indexed [ Info :< Leaf "hi" ]),
-  ("goodbye", Info :< Leaf "goodbye") ])
-
-b :: Term String Info
-b = Info :< (Keyed $ fromList [
-  ("hello", Info :< Indexed []),
-  ("goodbye", Info :< Indexed []) ])
-
-d :: Diff String
-d = Free $ Keyed $ fromList [
-  ("hello", Free $ Indexed [ Pure . Delete $ Info :< Leaf "hi" ]),
-  ("goodbye", Pure $ Replace (Info :< Leaf "goodbye") (Info :< Indexed [])) ]
-
 cost :: Diff a -> Integer
 cost f = iter c $ fmap g f where
   c (Leaf _) = 0
