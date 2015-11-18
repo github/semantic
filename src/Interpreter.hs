9504ebe586397a3e4f42dc4d53d86e4ddf4c4dab
Rob Rix <rob.rix@github.com>
2015-11-17 17:50:15 -0800

    Recursive interpretation of Keyed terms.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -6,6 +6,7 @@ import Control.Comonad.Cofree
 import Operation
 import Diff
 import Syntax
+import Data.Map
 
 constructAndRun :: Term a Info -> Term a Info -> Maybe (Diff a)
 constructAndRun a b =
@@ -19,6 +20,12 @@ run (Free (Recursive a b f)) = recur a b where
     run $ f $ Free $ Indexed $ zipWith interpret a' b'
   recur (_ :< Fixed a') (_ :< Fixed b') | length a' == length b' =
     run $ f $ Free $ Fixed $ zipWith interpret a' b'
+  recur (_ :< Keyed a') (_ :< Keyed b') | keys a' == keys b' =
+    run $ f $ Free $ Keyed $ fromList $ fmap (\ x -> (x, interpretInBoth x a' b')) $ keys b' where
+      interpretInBoth :: String -> Map String (Term a Info) -> Map String (Term a Info) -> Diff a
+      interpretInBoth key a' b' = maybeInterpret (Data.Map.lookup key a') (Data.Map.lookup key b')
+      maybeInterpret :: Maybe (Term a Info) -> Maybe (Term a Info) -> Diff a
+      maybeInterpret (Just a) (Just b) = interpret a b
   recur _ _ = run $ f $ Pure Patch { old = Just a, new = Just b }
 
 interpret :: Term a Info -> Term a Info -> Diff a
