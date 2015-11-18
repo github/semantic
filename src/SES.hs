08f3d217c02e8d678d101a0d5d71e93a51c10f7f
Rob Rix <rob.rix@github.com>
2015-11-18 07:40:54 -0800

    Implement SES in terms of cost and recur functions.

--- src/SES.hs
+++ src/SES.hs
@@ -9,3 +9,12 @@ ses [] b = (Pure . Insert) <$> b
 ses a [] = (Pure . Delete) <$> a
 ses (a : as) (b : bs) = case recur a b of
   Just f -> f : ses as bs
+  Nothing -> if SES.cost delete < SES.cost insert then delete else insert where
+    delete = (Pure . Delete $ a) : ses as (b : bs)
+    insert = (Pure . Insert $ b) : ses (a : as) bs
+
+cost :: Functor f => [Free f (Patch a)] -> Integer
+cost = _
+
+recur :: Functor f => Eq a => a -> a -> Maybe (Free f (Patch a))
+recur a b = _
