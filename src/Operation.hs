213d8fbd551f5f785c4992cabfcc440fcaa2178e
Rob Rix <rob.rix@github.com>
2015-11-17 18:58:51 -0800

    Maps all the way down.

--- src/Operation.hs
+++ src/Operation.hs
@@ -5,6 +5,6 @@ import Data.Map
 
 data Operation a f
   = Recursive (Term a Info) (Term a Info) (Diff a -> f)
-  | ByKey [(String, Term a Info)] [(String, Term a Info)] ([Map String (Diff a)] -> f)
+  | ByKey (Map String (Term a Info)) (Map String (Term a Info)) (Map String (Diff a) -> f)
   | ByIndex [Term a Info] [Term a Info] ([Diff a] -> f)
   deriving Functor
