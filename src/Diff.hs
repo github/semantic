e74340d4cf7a15927bea0f8df504fd1aaa55c5c2
Rob Rix <rob.rix@github.com>
2015-11-18 11:50:30 -0800

    :fire: the RangedWhatever things.

--- src/Diff.hs
+++ src/Diff.hs
@@ -23,9 +23,3 @@ cost f = iter c $ fmap g f where
   c (Indexed xs) = sum xs
   c (Fixed xs) = sum xs
   g _ = 1
-
-type RangedTerm a = Cofree (Syntax a) Int
--- data Difff a f = Difff (Either (Patch (Term a)) (Syntax a f))
--- type RangedDiff a = Cofree (Difff a) Range
-data AnnotatedSyntax a f = AnnotatedSyntax (Range, Syntax a f)
-type RangedDiff a = Free (AnnotatedSyntax a) (Patch (Term a Info))
