13499cc5205f38cb0a2da2fdc6928f5a4dc7589f
Rob Rix <rob.rix@github.com>
2015-11-18 07:26:24 -0800

    Implement the equal head case of SES.

--- src/SES.hs
+++ src/SES.hs
@@ -6,3 +6,4 @@ import Diff
 ses :: Eq a => [a] -> [a] -> [Either (Patch a) (a, a)]
 ses a b | Prelude.null a = (Left . Insert) <$> b
 ses a b | Prelude.null b = (Left . Delete) <$> a
+ses (a : as) (b : bs) | a == b = Right (a, b) : ses as bs
