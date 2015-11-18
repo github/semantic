359c1818d08f229292e414b7cd9990e9ac856ae4
Rob Rix <rob.rix@github.com>
2015-11-18 12:09:08 -0800

    Use `const`.

--- src/Diff.hs
+++ src/Diff.hs
@@ -16,9 +16,8 @@ data Info = Info -- Range [String]
 type Diff a = Free (Syntax a) (Patch (Term a Info))
 
 cost :: Diff a -> Integer
-cost f = iter c $ fmap g f where
+cost f = iter c $ fmap (const 1) f where
   c (Leaf _) = 0
   c (Keyed xs) = sum $ snd <$> toList xs
   c (Indexed xs) = sum xs
   c (Fixed xs) = sum xs
-  g _ = 1
