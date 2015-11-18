0799dfa51d2febf1c016aff9c7d4dd8c162cbbe7
Rob Rix <rob.rix@github.com>
2015-11-18 10:37:42 -0800

    Capture insert/delete costs.

--- src/SES.hs
+++ src/SES.hs
@@ -11,14 +11,16 @@ ses :: Compare a -> [Term a Info] -> [Term a Info] -> [Diff a]
 ses _ [] b = (Pure . Insert) <$> b
 ses _ a [] = (Pure . Delete) <$> a
 ses recur (a : as) (b : bs) = case recur a b of
-  Just f | SES.cost delete < SES.cost insert && SES.cost delete < SES.cost copy -> delete
-         | SES.cost insert < SES.cost copy -> insert
+  Just f | deleteCost < insertCost && deleteCost < SES.cost copy -> delete
+         | insertCost < SES.cost copy -> insert
          | otherwise -> copy
     where
       copy = f : ses recur as bs
-  Nothing | SES.cost delete < SES.cost insert -> delete
+  Nothing | deleteCost < insertCost -> delete
           | otherwise -> insert
   where
+    deleteCost = SES.cost delete
+    insertCost = SES.cost insert
     delete = (Pure . Delete $ a) : ses recur as (b : bs)
     insert = (Pure . Insert $ b) : ses recur (a : as) bs
 
