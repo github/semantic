16e4bf891de51d2018e9e29d8496d17a42e00a01
Rob Rix <rob.rix@github.com>
2015-11-17 17:13:48 -0800

    Interpretation.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -11,6 +11,10 @@ recur a b =
   run algorithm where
     algorithm = Free $ Recursive a b Pure
 
-
 run :: Algorithm a (Diff a) -> Maybe (Diff a)
 run (Pure diff) = Just diff
+
+interpret :: Term a Info -> Term a Info -> Diff a
+interpret a b = maybeReplace $ recur a b where
+  maybeReplace (Just a) = a
+  maybeReplace Nothing = Just a </> Just b
