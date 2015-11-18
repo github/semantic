485e947da6fbdaac1e4c98b9df226974d3731afb
Rob Rix <rob.rix@github.com>
2015-11-17 17:17:45 -0800

    Rename to constructAndRun.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -6,8 +6,8 @@ import Control.Comonad.Cofree
 import Operation
 import Diff
 
-recur :: Term a Info -> Term a Info -> Maybe (Diff a)
-recur a b =
+constructAndRun :: Term a Info -> Term a Info -> Maybe (Diff a)
+constructAndRun a b =
   run algorithm where
     algorithm = Free $ Recursive a b Pure
 
@@ -15,6 +15,6 @@ run :: Algorithm a (Diff a) -> Maybe (Diff a)
 run (Pure diff) = Just diff
 
 interpret :: Term a Info -> Term a Info -> Diff a
-interpret a b = maybeReplace $ recur a b where
+interpret a b = maybeReplace $ constructAndRun a b where
   maybeReplace (Just a) = a
   maybeReplace Nothing = Just a </> Just b
