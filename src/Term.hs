1e3ae866c2dcd1bc7bf1d50605d982bf8fa6d969
Rob Rix <rob.rix@github.com>
2015-11-18 11:39:08 -0800

    Terms with Categorizable annotations are themselves Categorizable.

--- src/Term.hs
+++ src/Term.hs
@@ -2,5 +2,9 @@ module Term where
 
 import Control.Comonad.Cofree
 import Syntax
+import Categorizable
 
 type Term a annotation = Cofree (Syntax a) annotation
+
+instance Categorizable annotation => Categorizable (Term a annotation) where
+  categories (annotation :< _) = categories annotation
