e91bdbd981cbf7c8644d2a51afe4d3e73d964b4c
Rob Rix <rob.rix@github.com>
2015-11-18 17:33:09 -0800

    Factor out the operator section.

--- src/Term.hs
+++ src/Term.hs
@@ -1,5 +1,6 @@
 module Term where
 
+import Data.Map
 import Data.Maybe
 import Control.Comonad.Cofree
 import Syntax
@@ -11,10 +12,10 @@ instance Categorizable annotation => Categorizable (Term a annotation) where
   categories (annotation :< _) = categories annotation
 
 zipTerms :: Term a annotation -> Term a annotation -> Maybe (Cofree (Syntax a) (annotation, annotation))
-zipTerms (annotation1 :< a) (annotation2 :< b) = zipUnwrap a b
+zipTerms (annotation1 :< a) (annotation2 :< b) = annotate $ zipUnwrap a b
   where
-    annotations = (annotation1, annotation2)
-    zipUnwrap (Leaf a) (Leaf b) = Just $ annotations :< Leaf b
-    zipUnwrap (Indexed a) (Indexed b) = Just $ annotations :< (Indexed . catMaybes $ zipWith zipTerms a b)
-    zipUnwrap (Fixed a) (Fixed b) = Just $ annotations :< (Fixed . catMaybes $ zipWith zipTerms a b)
+    annotate = fmap ((annotation1, annotation2) :<)
+    zipUnwrap (Leaf a) (Leaf b) = Just $ Leaf b
+    zipUnwrap (Indexed a) (Indexed b) = Just $ Indexed . catMaybes $ zipWith zipTerms a b
+    zipUnwrap (Fixed a) (Fixed b) = Just $ Fixed . catMaybes $ zipWith zipTerms a b
     zipUnwrap a b = Nothing
