0cd3b8c16ca21ab724bd119e62c042d96f1c4eac
Rob Rix <rob.rix@github.com>
2015-11-19 14:37:07 -0800

    Stub in the implementation of `unified` over `Diff`.

--- src/Unified.hs
+++ src/Unified.hs
@@ -1,5 +1,18 @@
 module Unified where
 
 import Diff
+import Patch
+import Syntax
+import Term
+import Control.Monad.Free
 
 unified :: Diff a annotation -> String -> String -> String
+unified diff before after =
+  iter f mapped where
+    mapped = fmap unifiedPatch diff
+    f (Annotated annotations (Leaf _)) = ""
+    f (Annotated annotations (Indexed i)) = ""
+    f (Annotated annotations (Fixed f)) = ""
+    f (Annotated annotations (Keyed k)) = ""
+    unifiedPatch :: Patch (Term a annotation) -> String
+    unifiedPatch _ = ""
