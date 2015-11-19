e8274ed379e39735c51bc24fa40b1078915a6862
Rob Rix <rob.rix@github.com>
2015-11-18 16:58:37 -0800

    Zip terms.

--- src/Term.hs
+++ src/Term.hs
@@ -8,3 +8,9 @@ type Term a annotation = Cofree (Syntax a) annotation
 
 instance Categorizable annotation => Categorizable (Term a annotation) where
   categories (annotation :< _) = categories annotation
+
+zipTerms :: Term a annotation -> Term a annotation -> Maybe (Cofree (Syntax a) (annotation, annotation))
+zipTerms (annotation1 :< a) (annotation2 :< b) = zipUnwrap a b
+  where
+    annotations = (annotation1, annotation2)
+    zipUnwrap a b = Nothing
