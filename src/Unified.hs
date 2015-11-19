0702ad277d0fe3ce17fd62598cc8547bd6a2044c
Rob Rix <rob.rix@github.com>
2015-11-19 15:59:24 -0800

    Import Data.Map.

--- src/Unified.hs
+++ src/Unified.hs
@@ -7,6 +7,7 @@ import Term
 import Control.Arrow
 import Control.Monad.Free
 import Control.Comonad.Cofree
+import Data.Map
 
 unified :: Diff a Info -> String -> String -> String
 unified diff before after =
@@ -22,7 +23,7 @@ unified diff before after =
 
     unifiedRange :: Range -> [(String, Maybe Range)] -> String -> String
     unifiedRange range children source = out ++ substring Range { start = previous, end = end range } after where
-      (out, previous) = foldl accumulateContext ("", start range) children
+      (out, previous) = Prelude.foldl accumulateContext ("", start range) children
       accumulateContext (out, previous) (child, Just range) = (out ++ substring Range { start = previous, end = start range } source ++ child, end range)
       accumulateContext (out, previous) (child, _) = (out ++ child, previous)
 
