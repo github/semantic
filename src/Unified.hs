6fa7204728014692a4df8f1e808b93d3065225ed
Rob Rix <rob.rix@github.com>
2015-11-19 15:56:58 -0800

    Accumulate context between children.

--- src/Unified.hs
+++ src/Unified.hs
@@ -23,9 +23,8 @@ unified diff before after =
     unifiedRange :: Range -> [(String, Maybe Range)] -> String -> String
     unifiedRange range children source = out ++ substring Range { start = previous, end = end range } after where
       (out, previous) = foldl accumulateContext ("", start range) children
-      accumulateContext (out, previous) (source, Just range) = (out ++ source, end range)
-      accumulateContext (out, previous) (source, _) = (out, previous)
-
+      accumulateContext (out, previous) (child, Just range) = (out ++ substring Range { start = previous, end = start range } source ++ child, end range)
+      accumulateContext (out, previous) (child, _) = (out ++ child, previous)
 
 substring :: Range -> String -> String
 substring range = take (end range) . drop (start range)
