28f93515bba59c05477d20c1209b6f2c98a792a6
Rob Rix <rob.rix@github.com>
2015-11-20 08:12:00 -0800

    Remove the fixtures.

--- app/Main.hs
+++ app/Main.hs
@@ -8,20 +8,7 @@ import Control.Comonad.Cofree
 import Control.Monad.Free
 import Data.Map
 
-a :: Term String ()
-a = () :< (Keyed $ fromList [
-  ("hello", () :< Indexed [ () :< Leaf "hi" ]),
-  ("goodbye", () :< Leaf "goodbye") ])
 
-b :: Term String ()
-b = () :< (Keyed $ fromList [
-  ("hello", () :< Indexed []),
-  ("goodbye", () :< Indexed []) ])
-
-d :: Diff String ()
-d = Free . Annotated ((), ()) . Keyed $ fromList [
-  ("hello", Free . Annotated ((), ()) $ Indexed [ Pure . Delete $ () :< Leaf "hi" ]),
-  ("goodbye", Pure $ Replace (() :< Leaf "goodbye") (() :< Indexed [])) ]
 
 main :: IO ()
 main = putStrLn "hello world"
