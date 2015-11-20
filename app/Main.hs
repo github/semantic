58cffa4e4c9bded80fcb93051d3c787847ece7f4
Rob Rix <rob.rix@github.com>
2015-11-20 08:36:54 -0800

    Read some files in.

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,24 @@ import Syntax
 import Control.Comonad.Cofree
 import Control.Monad.Free
 import Data.Map
-
+import Language.Haskell.Parser
+import Language.Haskell.Syntax
+import System.Environment
 
 
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+  args <- getArgs
+  let (a, b) = files args in do
+    a' <- parseModuleFile a
+    b' <- parseModuleFile b
+    return (a', b')
+  return ()
+
+parseModuleFile :: FilePath -> IO (ParseResult HsModule)
+parseModuleFile file = do
+  contents <- readFile file
+  return $ parseModule contents
+
+files (a : as) = (a, file as) where
+  file (a : as) = a
