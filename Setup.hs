12aadf6547622311b97e1de45241767266d3c812
Rob Rix <rob.rix@github.com>
2015-11-24 12:50:19 -0500

    Custom setup.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,25 @@
+import Data.Maybe
+import qualified Distribution.PackageDescription as PD
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.LocalBuildInfo
+import System.Directory
+
+main = defaultMainWithHooks simpleUserHooks {
+  confHook = semanticDiffConfHook
+}
+
+semanticDiffConfHook (description, buildInfo) flags = do
+  localBuildInfo <- confHook simpleUserHooks (description, buildInfo) flags
+  let packageDescription = localPkgDescr localBuildInfo
+      library = fromJust $ PD.library packageDescription
+      libraryBuildInfo = PD.libBuildInfo library
+  dir <- getCurrentDirectory
+  return localBuildInfo {
+      localPkgDescr = packageDescription {
+          PD.library = Just $ library {
+              PD.libBuildInfo = libraryBuildInfo {
+                  PD.extraLibDirs = (dir ++ "/prototype/External/tree-sitter/out/Release"):(dir ++ "/prototype/External/tree-sitter-c"):PD.extraLibDirs libraryBuildInfo
+              }
+          }
+      }
+  }
