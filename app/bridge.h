f82ff23e8d36e8846c0e529d847d4aecf33ed280
Rob Rix <rob.rix@github.com>
2015-11-25 18:38:38 -0500

    Declare ts_language_c().

--- app/bridge.h
+++ app/bridge.h
@@ -1,5 +1,7 @@
 #include "tree_sitter/runtime.h"
 
+extern TSLanguage *ts_language_c();
+
 void ts_document_root_node_p(TSDocument *document, TSNode *outNode);
 
 const char *ts_node_p_name(const TSNode *node, const TSDocument *document);
