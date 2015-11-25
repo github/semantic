854225666bec795b3e7127ba3a19acdedc70a1e0
Rob Rix <rob.rix@github.com>
2015-11-25 15:10:49 -0500

    Produce Fixed nodes.

--- app/Main.hs
+++ app/Main.hs
@@ -90,7 +90,10 @@ toTerm document contents (node, category) = do
   annotation <- return . Info range $ Data.Set.fromList [ category ]
   case children of
     [] -> return (annotation, Leaf $ substring range contents)
-    _ -> do
+    _ | member name fixedProductions -> do
+      children <- mapM nodeAndCategory children
+      return (annotation, Fixed children)
+    _ | otherwise -> do
       children <- mapM nodeAndCategory children
       return (annotation, Indexed children)
   where
