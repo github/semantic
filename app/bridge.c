0b735a697d836c66fda6dd8ae990dd50395a5e90
Rob Rix <rob.rix@github.com>
2015-11-25 15:01:27 -0500

    None of these may be NULL.

--- app/bridge.c
+++ app/bridge.c
@@ -1,28 +1,38 @@
 #include "bridge.h"
+#include <assert.h>
 
 void ts_document_root_node_p(TSDocument *document, TSNode *outNode) {
+	assert(document != NULL);
+	assert(outNode != NULL);
 	*outNode = ts_document_root_node(document);
 }
 
 
 const char *ts_node_p_name(const TSNode *node, const TSDocument *document) {
+	assert(node != NULL);
+	assert(document != NULL);
 	return ts_node_name(*node, document);
 }
 
 
 size_t ts_node_p_named_child_count(const TSNode *node) {
+	assert(node != NULL);
 	return ts_node_named_child_count(*node);
 }
 
 void ts_node_p_named_child(const TSNode *node, size_t index, TSNode *outNode) {
+	assert(node != NULL);
+	assert(outNode != NULL);
 	*outNode = ts_node_named_child(*node, index);
 }
 
 
 size_t ts_node_p_pos_chars(const TSNode *node) {
+	assert(node != NULL);
 	return ts_node_pos(*node).chars;
 }
 
 size_t ts_node_p_size_chars(const TSNode *node) {
+	assert(node != NULL);
 	return ts_node_size(*node).chars;
 }
