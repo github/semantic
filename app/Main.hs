1df3b71e2cf88173805a5b977ddf3f4bca63edaa
Rob Rix <rob.rix@github.com>
2015-11-25 18:42:21 -0500

    Rename the variable.

--- app/Main.hs
+++ app/Main.hs
@@ -96,9 +96,9 @@ toTerm document contents node = do
     fixedProductions = Data.Set.fromList [ "pair", "rel_op", "math_op", "bool_op", "bitwise_op", "type_op", "math_assignment", "assignment", "subscript_access", "member_access", "new_expression", "function_call", "function", "ternary" ]
 
 withAlloc :: Storable a => (Ptr a -> IO b) -> IO b
-withAlloc f = do
+withAlloc writeTo = do
   bytes <- malloc
-  f bytes
+  writeTo bytes
 
 namedChildren :: Ptr TSNode -> IO [Ptr TSNode]
 namedChildren node = do
