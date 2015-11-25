8030d173a2786330020eb58fa9f9cc5796f2869e
Rob Rix <rob.rix@github.com>
2015-11-25 10:08:16 -0500

    Bridge a function for getting a node’s name.

--- app/bridge.c
+++ app/bridge.c
@@ -3,3 +3,7 @@
 void ts_document_root_node_p(TSDocument *document, TSNode *outNode) {
 	*outNode = ts_document_root_node(document);
 }
+
+const char *ts_node_p_name(const TSNode *node, const TSDocument *document) {
+	return ts_node_name(*node, document);
+}
--- app/bridge.h
+++ app/bridge.h
@@ -1,3 +1,5 @@
 #include "tree_sitter/runtime.h"
 
 void ts_document_root_node_p(TSDocument *document, TSNode *outNode);
+
+const char *ts_node_p_name(const TSNode *node, const TSDocument *document);
