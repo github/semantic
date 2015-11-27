4467b4444e01b2f1bfe9c6abdc032187460dabd9
Rob Rix <rob.rix@github.com>
2015-11-27 12:02:28 -0500

    Don’t shadow.

--- src/Term.hs
+++ src/Term.hs
@@ -11,12 +11,12 @@ zipTerms :: Term a annotation -> Term a annotation -> Maybe (Term a (annotation,
 zipTerms (annotation1 :< a) (annotation2 :< b) = annotate $ zipUnwrap a b
   where
     annotate = fmap ((annotation1, annotation2) :<)
-    zipUnwrap (Leaf _) (Leaf b) = Just $ Leaf b
-    zipUnwrap (Indexed a) (Indexed b) = Just . Indexed . catMaybes $ zipWith zipTerms a b
-    zipUnwrap (Fixed a) (Fixed b) = Just . Fixed . catMaybes $ zipWith zipTerms a b
-    zipUnwrap (Keyed a) (Keyed b) | keys a == keys b = Just . Keyed . fromList . catMaybes $ zipUnwrapMaps a b <$> keys a
-    zipUnwrap a b = Nothing
-    zipUnwrapMaps a b key = (,) key <$> zipTerms (a ! key) (b ! key)
+    zipUnwrap (Leaf _) (Leaf b') = Just $ Leaf b'
+    zipUnwrap (Indexed a') (Indexed b') = Just . Indexed . catMaybes $ zipWith zipTerms a' b'
+    zipUnwrap (Fixed a') (Fixed b') = Just . Fixed . catMaybes $ zipWith zipTerms a' b'
+    zipUnwrap (Keyed a') (Keyed b') | keys a' == keys b' = Just . Keyed . fromList . catMaybes $ zipUnwrapMaps a' b' <$> keys a'
+    zipUnwrap _ _ = Nothing
+    zipUnwrapMaps a' b' key = (,) key <$> zipTerms (a' ! key) (b' ! key)
 
 cata :: (annotation -> Syntax a b -> b) -> Term a annotation -> b
 cata f (annotation :< syntax) = f annotation $ cata f <$> syntax
