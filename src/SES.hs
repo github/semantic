3429f52e3f95444c340c0dbd34397ebf3acbb8cc
Rob Rix <rob.rix@github.com>
2015-11-30 16:45:54 -0500

    Move the `return` inside the handling of the `Just` case.

--- src/SES.hs
+++ src/SES.hs
@@ -30,6 +30,6 @@ diffAt (i, j) [] bs = return $ (Pure . Insert) <$> bs
 diffAt (i, j) as [] = return $ (Pure . Delete) <$> as
 diffAt (i, j) as bs = do
   state <- get
-  return . maybe $ (compute state) (fmap fst) $ fetch state where
+  maybe $ (compute state) (return . fmap fst) $ fetch state where
     fetch state = Map.lookup (i, j) state
     compute state = _
