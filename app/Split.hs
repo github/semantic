f68c0521a8d9c1640fd3127b1b4b33431a0908d2
Rob Rix <rob.rix@github.com>
2015-12-01 15:31:02 -0500

    Add a stupid HTML type.

--- app/Split.hs
+++ app/Split.hs
@@ -6,10 +6,18 @@ import Syntax
 import Term
 import Unified
 import Control.Comonad.Cofree
+import qualified Data.Map as Map
 import Rainbow
 
 type ClassName = String
 type Element a = Cofree (Syntax a) (Maybe ClassName, String)
+
+data HTML =
+  Text String
+  | Span ClassName String
+  | Ul ClassName [HTML]
+  | Dl ClassName (Map.Map String HTML)
+  deriving (Eq, Show)
 
 split :: Diff a Info -> String -> String -> IO ByteString
 split _ _ _ = return mempty
