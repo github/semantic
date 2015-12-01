f2ca75a276cece43751ddf91b40211994f2e9ea7
Rob Rix <rob.rix@github.com>
2015-12-01 14:35:41 -0500

    Map patches into pairs of optional elements.

--- app/Split.hs
+++ app/Split.hs
@@ -17,8 +17,8 @@ split _ _ _ = return mempty
 splitDiff :: Diff a Info -> String -> String -> [(String, String)]
 splitDiff _ _ _ = []
 
-splitPatch :: Patch (Term a Info) -> String -> String -> [(String, String)]
-splitPatch _ _ _ = []
+splitPatch :: String -> String -> Patch (Term a Info) -> (Maybe (Element a), Maybe (Element a))
+splitPatch before after patch = (fmap (splitTerm before) $ Patch.before patch, fmap (splitTerm after) $ Patch.after patch)
 
 splitTerm :: String -> Term a Info -> Element a
 splitTerm source term = toElement term where
