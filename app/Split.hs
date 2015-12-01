9b1b1b864395a3dac9a5f6f02485dee2922d7df1
Rob Rix <rob.rix@github.com>
2015-12-01 16:07:45 -0500

    Extract a makeList function for lists.

--- app/Split.hs
+++ app/Split.hs
@@ -32,9 +32,15 @@ splitPatch before after patch = (fmap (splitTerm before) $ Patch.before patch, f
 splitTerm :: String -> Term a Info -> HTML
 splitTerm source term = fst $ cata toElement term where
   toElement (Info range lineRange categories) (Leaf _) = (Span (classify categories) $ substring range source, range)
-  toElement (Info range lineRange categories) (Indexed i) = (Ul (classify categories) $ children ++ [ subtext previous $ end range ], range) where
-    (children, previous) = foldl accumulate ([], start range) i
+  toElement (Info range lineRange categories) (Indexed i) = makeList i range categories
+
   accumulate (children, previous) (child, range) = (children ++ [ subtext previous $ start range, child ], end range)
+
+  makeList i range categories = (Ul (classify categories) items, range)
+    where
+      (children, previous) = foldl accumulate ([], start range) i
+      items = children ++ [ subtext previous $ end range ]
+
   subtext :: Int -> Int -> HTML
   subtext start end = Text $ substring (Range start end) source
 
