aa1db29cc7b8eacd451875d1bfa0cf8c8c66e242
Rob Rix <rob.rix@github.com>
2015-12-03 11:44:42 -0500

    Stub in a test test of adjoinLines.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,10 @@
 module Main where
 
+import Split
 import Test.Hspec
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "split" $ do
+    it "two single line elements should concatenate into a single line" $
+      adjoinLines ([], []) ([], []) `shouldBe` ([], [])
