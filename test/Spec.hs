ea58415617602d6a1e7ca42ab76a34a2bf975c94
Rob Rix <rob.rix@github.com>
2015-12-03 12:07:03 -0500

    Test the multiple line cases.

--- test/Spec.hs
+++ test/Spec.hs
@@ -14,3 +14,9 @@ main = hspec $ do
 
     it "two single line elements should concatenate into a single line" $
       adjoinLines ([ Line "a" ], [ Line "b" ]) ([ Line "a" ], [ Line "b" ]) `shouldBe` ([ Line "aa" ], [ Line "bb" ])
+
+    it "single line elements on the left concatenate onto the first of multiple lines on the right" $
+      adjoinLines ([ Line "a1" ], [ Line "b1" ]) ([ Line "a2", Line "a3" ], [ Line "b2", Line "b3" ]) `shouldBe` ([ Line "a1a2", Line "a3" ], [ Line "b1b2", Line "b3" ])
+
+    it "the last of multiple line elements on the left concatenate onto the first of multiple lines on the right" $
+      adjoinLines ([ Line "a1", Line "a2" ], [ Line "b1", Line "b2" ]) ([ Line "a3", Line "a4" ], [ Line "b3", Line "b4" ]) `shouldBe` ([ Line "a1", Line "a2a3", Line "a4" ], [ Line "b1", Line "b2b3", Line "b4" ])
