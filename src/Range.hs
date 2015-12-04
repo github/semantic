19ff0415d680a464352737c1bfb5cafaba531381
Rob Rix <rob.rix@github.com>
2015-12-04 09:20:23 -0500

    Add a convenience to construct the complete range of a list.

--- src/Range.hs
+++ src/Range.hs
@@ -6,5 +6,8 @@ data Range = Range { start :: Int, end :: Int }
 substring :: Range -> String -> String
 substring range = take (end range - start range) . drop (start range)
 
+totalRange :: [a] -> Range
+totalRange list = Range 0 $ length list
+
 instance Ord Range where
   a <= b = start a <= start b
