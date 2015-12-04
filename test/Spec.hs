a37e199f11e5489653ee7a474a3c1826b7920229
joshvera <vera@github.com>
2015-12-04 14:18:41 -0500

    format tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -24,7 +24,13 @@ main = hspec $ do
       adjoinRows [ Row [ Text "a1" ] [ Text "b1" ] ] [ Row [ Text "a2" ] [ Text "b2" ], Row [ Text "a3" ] [ Text "b3" ] ] `shouldBe` [ Row [ Text "a1", Text "a2" ] [ Text "b1", Text "b2" ], Row [ Text "a3" ] [ Text "b3" ] ]
 
     it "the last of multiple line elements on the left concatenate onto the first of multiple lines on the right" $
-      adjoinRows [ Row [ Text "a1" ] [ Text "b1" ], Row [ Text "a2" ] [ Text "b2" ] ] [ Row [ Text "a3" ] [ Text "b3" ], Row [ Text "a4" ] [ Text "b4" ] ] `shouldBe` [ Row [ Text "a1" ] [ Text "b1" ], Row [ Text "a2", Text "a3" ] [ Text "b2", Text "b3" ], Row [ Text "a4" ] [ Text "b4" ] ]
+      adjoinRows [ Row [ Text "a1" ] [ Text "b1" ], Row [ Text "a2" ] [ Text "b2" ] ]
+                 [ Row [ Text "a3" ] [ Text "b3" ], Row [ Text "a4" ] [ Text "b4" ] ]
+      `shouldBe`
+      [ Row [ Text "a1" ] [ Text "b1" ],
+        Row [ Text "a2", Text "a3" ] [ Text "b2", Text "b3" ],
+        Row [ Text "a4" ] [ Text "b4" ] ]
+
 
   describe "annotatedToRows" $ do
     it "outputs one row for single-line unchanged leaves" $
