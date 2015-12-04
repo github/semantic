d3c5db105d689d20cae6b56017385cfb77ba9472
Rob Rix <rob.rix@github.com>
2015-12-04 10:19:19 -0500

    Test the rows computed for non-empty unchanged branches.

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import Diff
 import Range
 import Split
 import Syntax
+import Control.Monad.Free
 import qualified Data.Set as Set
 import Test.Hspec
 
@@ -31,6 +32,12 @@ main = hspec $ do
 
     it "outputs one row for single-line empty unchanged indexed nodes" $
       annotatedToRows (unchanged "[]" "branch" (Indexed [])) "[]" "[]" `shouldBe` ([ Row [ Ul (Just "branch") [ Text "[]" ] ] [ Ul (Just "branch") [ Text "[]" ] ] ], Range 0 2, Range 0 2)
+
+    it "outputs one row for single-line non-empty unchanged indexed nodes" $
+      annotatedToRows (unchanged "[ a, b ]" "branch" (Indexed [
+        Free . offsetAnnotated 2 $ unchanged "a" "leaf" (Leaf ""),
+        Free . offsetAnnotated 5 $ unchanged "b" "leaf" (Leaf "")
+      ])) "[ a, b ]" "[ a, b ]" `shouldBe` ([ Row [ Ul (Just "branch") [ Text "[ ", span "a", Text ", ", span "b", Text " ]" ] ] [ Ul (Just "branch") [ Text "[ ", span "a", Text ", ", span "b", Text " ]" ] ] ], Range 0 2, Range 0 2)
     where
       info source category = Info (totalRange source) (Range 0 0) (Set.fromList [ category ])
       unchanged source category = Annotated (info source category, info source category)
