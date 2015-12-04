c0c94a3be95d9e343f10423f9f8df773e319158e
joshvera <vera@github.com>
2015-12-04 14:17:14 -0500

    Remove splitDiff/splitPatch/termToHTML

--- src/Split.hs
+++ src/Split.hs
@@ -83,41 +83,6 @@ zipWithMaybe f la lb = take len $ zipWith f la' lb'
     la' = (Just <$> la) ++ (repeat Nothing)
     lb' = (Just <$> lb) ++ (repeat Nothing)
 
-splitDiff :: Diff a Info -> String -> String -> Patch (HTML, Range)
-splitDiff diff before after = iter toElements $ splitPatch before after <$> diff
-  where
-    toElements (Annotated (left, right) (Leaf _)) = Replace (leafToElement before left) (leafToElement after right)
-
-    leafToElement source (Info range _ categories) = (Span (classify categories) $ substring range source, range)
-
-splitPatch :: String -> String -> Patch (Term a Info) -> Patch (HTML, Range)
-splitPatch before after (Replace a b) = Replace (termToHTML before a) (termToHTML after b)
-splitPatch _ after (Insert b) = Insert $ termToHTML after b
-splitPatch before _ (Delete a) = Delete $ termToHTML before a
-
-termToHTML :: String -> Term a Info -> (HTML, Range)
-termToHTML source = cata toElement where
-  toElement (Info range _ categories) (Leaf _) = (Span (classify categories) $ substring range source, range)
-  toElement (Info range _ categories) (Indexed i) = makeList i range categories
-  toElement (Info range _ categories) (Fixed i) = makeList i range categories
-  toElement (Info range _ categories) (Keyed k) = makeMap (Map.toList k) range categories
-
-  accumulate (children, previous) (child, range) = (children ++ [ subtext previous $ start range, child ], end range)
-  accumulateFromMap (children, previous) (key, (child, range)) = (children ++ [ subtext previous $ start range, Dt key, child ], end range)
-
-  makeList i range categories = (Ul (classify categories) items, range)
-    where
-      (children, previous) = foldl accumulate ([], start range) i
-      items = children ++ [ subtext previous $ end range ]
-
-  makeMap k range categories = (Dl (classify categories) items, range)
-    where
-      (children, previous) = foldl accumulateFromMap ([], start range) k
-      items = children ++ [ subtext previous $ end range ]
-
-  subtext :: Int -> Int -> HTML
-  subtext start end = Text $ substring (Range start end) source
-
 classify :: Set.Set Category -> Maybe ClassName
 classify = foldr (const . Just) Nothing
 
