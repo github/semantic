b59623bf3d8b597b412117790c28cb6a0fc4f7ad
Rob Rix <rob.rix@github.com>
2015-12-09 10:32:22 -0500

    Construct a parser for the lines in some input file.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,6 +1,22 @@
 module Parser where
 
 import Diff
+import Range
+import Syntax
 import Term
+import Control.Comonad.Cofree
+import qualified Data.Set as Set
 
 type Parser = String -> IO (Term String Info)
+
+lineByLineParser :: Parser
+lineByLineParser input = return . root . Indexed $ case foldl annotateLeaves ([], 0, 0) lines of
+  (leaves, _, _) -> leaves
+  where
+    lines = Prelude.lines input
+    root syntax = Info (Range 0 $ length input) (Range 0 $ length lines) Set.empty :< syntax
+    leaf charIndex lineIndex line = Info (Range charIndex $ charIndex + length line) (Range lineIndex $ lineIndex + 1) Set.empty :< Leaf line
+    annotateLeaves (accum, charIndex, lineIndex) line =
+      (accum ++ [ leaf charIndex lineIndex line ]
+      , charIndex + length line + 1
+      , lineIndex + 1)
