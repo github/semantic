d9f56ee632211ad06b77cea100d394dbbecaebce
Rob Rix <rob.rix@github.com>
2015-12-11 17:16:28 -0500

    Implement !.

--- src/OrderedMap.hs
+++ src/OrderedMap.hs
@@ -3,6 +3,7 @@ module OrderedMap (
   , fromList
   , toList
   , keys
+  , (!)
   ) where
 
 data OrderedMap key value = OrderedMap { toList :: [(key, value)] }
@@ -13,3 +14,10 @@ fromList list = OrderedMap list
 
 keys :: OrderedMap key value -> [key]
 keys (OrderedMap pairs) = fst <$> pairs
+
+infixl 9 !
+
+(!) :: Eq key => OrderedMap key value -> key -> value
+OrderedMap pairs ! key = case lookup key pairs of
+  Just value -> value
+  Nothing -> error "no value found for key"
