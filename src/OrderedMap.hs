3b0dc863e0dd85b41979e8f33406decbce75ed9f
Rob Rix <rob.rix@github.com>
2015-12-11 17:05:21 -0500

    Implement fromList.

--- src/OrderedMap.hs
+++ src/OrderedMap.hs
@@ -1,3 +1,6 @@
-module OrderedMap (OrderedMap) where
+module OrderedMap (OrderedMap, fromList) where
 
 data OrderedMap key value = OrderedMap [(key, value)]
+
+fromList :: [(key, value)] -> OrderedMap key value
+fromList list = OrderedMap list
