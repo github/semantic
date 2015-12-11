d4941c954ca4d830d06ce4f6e8d4350e5c925195
Rob Rix <rob.rix@github.com>
2015-12-11 17:10:31 -0500

    Derive: Show, Eq, Functor, Foldable, and Traversable.

--- src/OrderedMap.hs
+++ src/OrderedMap.hs
@@ -1,6 +1,7 @@
 module OrderedMap (OrderedMap, fromList, toList) where
 
 data OrderedMap key value = OrderedMap { toList :: [(key, value)] }
+  deriving (Show, Eq, Functor, Foldable, Traversable)
 
 fromList :: [(key, value)] -> OrderedMap key value
 fromList list = OrderedMap list
