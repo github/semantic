8d0272bdacaa4a9e8d9f1c403f490206fb1e5e07
Rob Rix <rob.rix@github.com>
2015-12-11 11:14:33 -0500

    Implement shrinking over ArbitraryTerm.

--- test/Spec.hs
+++ test/Spec.hs
@@ -24,6 +24,12 @@ unTerm arbitraryTerm = unfold unpack arbitraryTerm
 
 instance (Arbitrary a, Arbitrary annotation) => Arbitrary (ArbitraryTerm a annotation) where
   arbitrary = arbitraryBounded 4
+  shrink (ArbitraryTerm (annotation, syntax)) = ArbitraryTerm <$> ((,) <$> shrink annotation <*> shrinkSyntax syntax)
+    where shrinkSyntax (Leaf a) = Leaf <$> shrink a
+          shrinkSyntax (Indexed i) = (getSyntax <$> i) ++ (Indexed <$> shrink i)
+          shrinkSyntax (Syntax.Fixed f) = (getSyntax <$> f) ++ (Syntax.Fixed <$> shrink f)
+          shrinkSyntax (Keyed k) = (getSyntax . snd <$> (Map.toList k)) ++ (Keyed . Map.fromList <$> shrink (Map.toList k))
+          getSyntax (ArbitraryTerm (_, syntax)) = syntax
 
 arbitraryBounded :: (Arbitrary a, Arbitrary annotation) => Int -> Gen (ArbitraryTerm a annotation)
 arbitraryBounded k = ArbitraryTerm <$> ((,) <$> arbitrary <*> oneof [
