dfadb116aa3bfa807d0f7f52076aa9bc1006c582
Rob Rix <rob.rix@github.com>
2015-12-11 17:11:44 -0500

    Add `keys`.

--- src/OrderedMap.hs
+++ src/OrderedMap.hs
@@ -1,7 +1,10 @@
-module OrderedMap (OrderedMap, fromList, toList) where
+module OrderedMap (OrderedMap, fromList, toList, keys) where
 
 data OrderedMap key value = OrderedMap { toList :: [(key, value)] }
   deriving (Show, Eq, Functor, Foldable, Traversable)
 
 fromList :: [(key, value)] -> OrderedMap key value
 fromList list = OrderedMap list
+
+keys :: OrderedMap key value -> [key]
+keys (OrderedMap pairs) = fst <$> pairs
