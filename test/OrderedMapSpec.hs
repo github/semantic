7f488bb351564230452517c9eb2d5e07405e8706
Rob Rix <rob.rix@github.com>
2015-12-14 17:47:42 -0500

    Knock the nesting out.

--- test/OrderedMapSpec.hs
+++ test/OrderedMapSpec.hs
@@ -5,7 +5,6 @@ import Test.Hspec
 
 orderedMapSpec :: IO ()
 orderedMapSpec = hspec $ do
-  describe "OrderedMap" $ do
-    describe "difference" $ do
-      it "should return those elements of a not in b" $
-        Map.difference (Map.fromList [ ("a", 1), ("b", 2), ("c", 3) ]) (Map.fromList [ ("b", 2), ("c", 3), ("d", 4) ]) `shouldBe` (Map.fromList [ ("a", 1) ])
+  describe "difference" $ do
+    it "should return those elements of a not in b" $
+      Map.difference (Map.fromList [ ("a", 1), ("b", 2), ("c", 3) ]) (Map.fromList [ ("b", 2), ("c", 3), ("d", 4) ]) `shouldBe` (Map.fromList [ ("a", 1) ])
