6cef2cb17f0d023cd30c9a23196af8a8aa2b8013
Rob Rix <rob.rix@github.com>
2015-12-14 12:20:51 -0500

    Add a function to offset ranges.

--- src/Range.hs
+++ src/Range.hs
@@ -9,5 +9,8 @@ substring range = take (end range - start range) . drop (start range)
 totalRange :: [a] -> Range
 totalRange list = Range 0 $ length list
 
+offsetRange :: Int -> Range -> Range
+offsetRange i (Range start end) = Range (i + start) (i + end)
+
 instance Ord Range where
   a <= b = start a <= start b
