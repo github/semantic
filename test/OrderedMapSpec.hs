0401bcb83d50df8b9ee8577f75c29bca57521163
Rob Rix <rob.rix@github.com>
2015-12-14 17:48:59 -0500

    Rename the spec function.

--- test/OrderedMapSpec.hs
+++ test/OrderedMapSpec.hs
@@ -3,8 +3,8 @@ module OrderedMapSpec where
 import Data.Map as Map
 import Test.Hspec
 
-orderedMapSpec :: IO ()
-orderedMapSpec = hspec $ do
+spec :: Spec
+spec = do
   describe "difference" $ do
     it "should return those elements of a not in b" $
       Map.difference (Map.fromList [ ("a", 1), ("b", 2), ("c", 3) ]) (Map.fromList [ ("b", 2), ("c", 3), ("d", 4) ]) `shouldBe` (Map.fromList [ ("a", 1) ])
