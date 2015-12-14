ab6658edc847263bc8114ccccd14df558b312bbe
Rob Rix <rob.rix@github.com>
2015-12-14 17:51:15 -0500

    Extract the fixtures.

--- test/OrderedMapSpec.hs
+++ test/OrderedMapSpec.hs
@@ -7,4 +7,7 @@ spec :: Spec
 spec = do
   describe "difference" $ do
     it "should return those elements of a not in b" $
-      Map.difference (Map.fromList [ ("a", 1), ("b", 2), ("c", 3) ]) (Map.fromList [ ("b", 2), ("c", 3), ("d", 4) ]) `shouldBe` (Map.fromList [ ("a", 1) ])
+      Map.difference a b `shouldBe` (Map.fromList [ ("a", 1) ])
+
+  where a = Map.fromList [ ("a", 1), ("b", 2), ("c", 3) ]
+        b = Map.fromList [ ("b", 2), ("c", 3), ("d", 4) ]
