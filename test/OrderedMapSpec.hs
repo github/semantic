aeb99b54a07c998e8bba957ef60ea0334ab62ecf
Rob Rix <rob.rix@github.com>
2015-12-14 17:53:05 -0500

    Test the union.

--- test/OrderedMapSpec.hs
+++ test/OrderedMapSpec.hs
@@ -9,5 +9,9 @@ spec = do
     it "should return those elements of a not in b" $
       Map.difference a b `shouldBe` (Map.fromList [ ("a", 1) ])
 
+  describe "union" $ do
+    it "should return those elements in either a or b" $
+      Map.union a b `shouldBe` (Map.fromList $ ("a", 1) : Map.toList b)
+
   where a = Map.fromList [ ("a", 1), ("b", 2), ("c", 3) ]
         b = Map.fromList [ ("b", 2), ("c", 3), ("d", 4) ]
