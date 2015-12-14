4083829e124f7787e90e787594222809c5efdf3f
Rob Rix <rob.rix@github.com>
2015-12-14 13:04:21 -0500

    Bind a different variable for the subrange.

--- app/Main.hs
+++ app/Main.hs
@@ -63,10 +63,10 @@ parserForType mediaType = maybe P.lineByLineParser parseTreeSitterFile $ case me
 replaceLeavesWithWordBranches :: String -> Term String Info -> Term String Info
 replaceLeavesWithWordBranches source term = replaceIn source 0 term
   where
-    replaceIn source startIndex (info@(Info range lineRange categories) :< syntax) | substring <- substring range source, range <- offsetRange (negate startIndex) range = info :< case syntax of
+    replaceIn source startIndex (info@(Info range lineRange categories) :< syntax) | substring <- substring range source, subrange <- offsetRange (negate startIndex) range = info :< case syntax of
       Leaf _ | ranges <- rangesOfWordsFrom (start range) substring, length ranges > 1 -> Indexed $ makeLeaf substring startIndex lineRange categories <$> ranges
-      Indexed i -> Indexed $ replaceIn substring (start range) <$> i
-      Fixed f -> Fixed $ replaceIn substring (start range) <$> f
-      Keyed k -> Keyed $ replaceIn substring (start range) <$> k
+      Indexed i -> Indexed $ replaceIn substring (start subrange) <$> i
+      Fixed f -> Fixed $ replaceIn substring (start subrange) <$> f
+      Keyed k -> Keyed $ replaceIn substring (start subrange) <$> k
       _ -> syntax
     makeLeaf source startIndex lineRange categories range = Info range lineRange categories :< Leaf (substring (offsetRange (negate startIndex) range) source)
