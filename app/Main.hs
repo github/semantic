1d9e945aad5edb0e54d609b4494f76224fad849a
Rob Rix <rob.rix@github.com>
2015-12-14 14:40:30 -0500

    Pass absolute start indices to children.

--- app/Main.hs
+++ app/Main.hs
@@ -61,8 +61,8 @@ replaceLeavesWithWordBranches source term = replaceIn source 0 term
   where
     replaceIn source startIndex (info@(Info range lineRange categories) :< syntax) | subrange <- offsetRange (negate startIndex) range, substring <- substring subrange source = info :< case syntax of
       Leaf _ | ranges <- rangesOfWordsFrom (start range) substring, length ranges > 1 -> Indexed $ makeLeaf substring startIndex lineRange categories <$> ranges
-      Indexed i -> Indexed $ replaceIn substring (start subrange) <$> i
-      Fixed f -> Fixed $ replaceIn substring (start subrange) <$> f
-      Keyed k -> Keyed $ replaceIn substring (start subrange) <$> k
+      Indexed i -> Indexed $ replaceIn substring (start range) <$> i
+      Fixed f -> Fixed $ replaceIn substring (start range) <$> f
+      Keyed k -> Keyed $ replaceIn substring (start range) <$> k
       _ -> syntax
     makeLeaf source startIndex lineRange categories range = Info range lineRange categories :< Leaf (substring (offsetRange (negate startIndex) range) source)
