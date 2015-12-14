3c8b79f0b072abad75166d59fef9279e100548e9
Rob Rix <rob.rix@github.com>
2015-12-14 17:22:53 -0500

    Correct some merge confusion.

--- app/Main.hs
+++ app/Main.hs
@@ -59,10 +59,10 @@ parserForType mediaType = maybe P.lineByLineParser parseTreeSitterFile $ case me
 replaceLeavesWithWordBranches :: String -> Term String Info -> Term String Info
 replaceLeavesWithWordBranches source term = replaceIn source 0 term
   where
-    replaceIn source startIndex (info@(Info range lineRange categories) :< syntax) | substring <- substring (offsetRange (negate startIndex) range) source = info :< case syntax of
-      Leaf _ | ranges <- rangesAndWordsFrom (start range) substring, length ranges > 1 -> Indexed $ makeLeaf substring startIndex lineRange categories <$> ranges
+    replaceIn source startIndex (info@(Info range categories) :< syntax) | substring <- substring (offsetRange (negate startIndex) range) source = info :< case syntax of
+      Leaf _ | ranges <- rangesAndWordsFrom (start range) substring, length ranges > 1 -> Indexed $ makeLeaf substring startIndex categories <$> ranges
       Indexed i -> Indexed $ replaceIn substring (start range) <$> i
       Fixed f -> Fixed $ replaceIn substring (start range) <$> f
       Keyed k -> Keyed $ replaceIn substring (start range) <$> k
       _ -> syntax
-    makeLeaf source startIndex lineRange categories (range, substring) = Info range lineRange categories :< Leaf substring
+    makeLeaf source startIndex categories (range, substring) = Info range categories :< Leaf substring
