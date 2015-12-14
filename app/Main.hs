4dd9907130f55015c7e6d689725ae02fee2af250
Rob Rix <rob.rix@github.com>
2015-12-14 12:34:18 -0500

    Try negating the offset.

--- app/Main.hs
+++ app/Main.hs
@@ -64,7 +64,7 @@ parserForType mediaType = maybe P.lineByLineParser parseTreeSitterFile $ case me
 replaceLeavesWithWordBranches :: String -> Term String Info -> Term String Info
 replaceLeavesWithWordBranches source term@(Info range _ _ :< _) = replaceIn source range term
   where
-    replaceIn source parentRange (info@(Info range lineRange categories) :< syntax) | range <- offsetRange (start parentRange) range = info :< case syntax of
+    replaceIn source parentRange (info@(Info range lineRange categories) :< syntax) | range <- offsetRange (negate $ start parentRange) range = info :< case syntax of
       Leaf _ | ranges <- rangesOfWordsFrom (start range) (substring range source), length ranges > 1 -> Indexed $ makeLeaf lineRange categories <$> ranges
       Indexed i -> Indexed $ replaceIn (substring range source) range <$> i
       Fixed f -> Fixed $ replaceIn (substring range source) range <$> f
