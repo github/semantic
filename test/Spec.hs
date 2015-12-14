2e5d85748b5dada323b4fe88a40bdae381fb10e1
Rob Rix <rob.rix@github.com>
2015-12-14 15:08:32 -0500

    Capture the construction of strictly smaller terms in the where clause.

--- test/Spec.hs
+++ test/Spec.hs
@@ -32,9 +32,10 @@ instance (Eq a, Eq annotation, Arbitrary a, Arbitrary annotation) => Arbitrary (
           boundedSyntax _ n | n <= 0 = liftM Leaf arbitrary
           boundedSyntax m n = frequency
             [ (12, liftM Leaf arbitrary),
-              (1, liftM Indexed $ take n <$> listOf (boundedTerm (div m 2) (div n 3))),
-              (1, liftM Fixed $ take n <$> listOf (boundedTerm (div m 2) (div n 3))),
-              (1, liftM (Keyed . Map.fromList) $ take n <$> listOf (arbitrary >>= (\x -> ((,) x) <$> boundedTerm (div m 2) (div n 3)))) ]
+              (1, liftM Indexed $ take n <$> listOf (smallerTerm m n)),
+              (1, liftM Fixed $ take n <$> listOf (smallerTerm m n)),
+              (1, liftM (Keyed . Map.fromList) $ take n <$> listOf (arbitrary >>= (\x -> ((,) x) <$> smallerTerm m n))) ]
+          smallerTerm m n = boundedTerm (div m 2) (div n 3)
   shrink term@(ArbitraryTerm (annotation, syntax)) = (++) (subterms term) $ filter (/= term) $
     ArbitraryTerm <$> ((,) <$> shrink annotation <*> case syntax of
       Leaf a -> Leaf <$> shrink a
