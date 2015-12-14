170fc65f5c6409f0e006a4fbea1f20243c2a7148
Rob Rix <rob.rix@github.com>
2015-12-14 15:03:24 -0500

    Test for word characters specifically.

--- src/Range.hs
+++ src/Range.hs
@@ -15,13 +15,13 @@ offsetRange :: Int -> Range -> Range
 offsetRange i (Range start end) = Range (i + start) (i + end)
 
 rangesOfWordsFrom :: Int -> String -> [Range]
-rangesOfWordsFrom startIndex string = case break Char.isSpace string of
+rangesOfWordsFrom startIndex string = case break (not . isWord) string of
   ([], []) -> []
   ([], rest) -> rangesOfWordsAfterWhitespace startIndex rest
   (word, []) -> [ Range startIndex $ startIndex + length word ]
   (word, rest) -> (Range startIndex $ startIndex + length word) : rangesOfWordsAfterWhitespace (startIndex + length word) rest
   where
-    rangesOfWordsAfterWhitespace startIndex string | (whitespace, rest) <- break (not . Char.isSpace) string = rangesOfWordsFrom (startIndex + length whitespace) rest
+    rangesOfWordsAfterWhitespace startIndex string | (whitespace, rest) <- break isWord string = rangesOfWordsFrom (startIndex + length whitespace) rest
     -- | Is this a word character?
     -- | Word characters are defined as in [Ruby’s `\p{Word}` syntax](http://ruby-doc.org/core-2.1.1/Regexp.html#class-Regexp-label-Character+Properties), i.e.:
     -- | > A member of one of the following Unicode general category _Letter_, _Mark_, _Number_, _Connector_Punctuation_
