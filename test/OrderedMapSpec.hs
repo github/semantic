79d5e38b26f6f104e1b25f740a565703718eb2e2
Rob Rix <rob.rix@github.com>
2015-12-14 18:15:14 -0500

    Use distinct elements in `a` and `b`.

--- test/OrderedMapSpec.hs
+++ test/OrderedMapSpec.hs
@@ -14,7 +14,7 @@ spec = do
 
   describe "union" $ do
     it "should return those elements in either a or b" $
-      Map.union a b `shouldBe` (Map.fromList $ ("a", 1) : Map.toList b)
+      Map.union a b `shouldBe` (Map.fromList $ Map.toList a ++ [ ("d", -4) ])
 
   describe "unions" $ do
     it "is the union of multiple maps" $
@@ -22,11 +22,11 @@ spec = do
 
   describe "intersectionWith" $ do
     it "should return those elements in both a and b, combined with a function" $
-      Map.intersectionWith (+) a b `shouldBe` (Map.fromList [ ("b", 4), ("c", 6) ])
+      Map.intersectionWith (-) a b `shouldBe` (Map.fromList [ ("b", 4), ("c", 6) ])
 
   describe "keys" $ do
     it "should return all the keys in a map" $
       Map.keys a `shouldBe` [ "a", "b", "c" ]
 
   where a = Map.fromList [ ("a", 1), ("b", 2), ("c", 3) ]
-        b = Map.fromList [ ("b", 2), ("c", 3), ("d", 4) ]
+        b = Map.fromList [ ("b", -2), ("c", -3), ("d", -4) ]
