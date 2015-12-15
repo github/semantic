9c2b12a615a08b12674ff64c8606768cb541e5a9
Rob Rix <rob.rix@github.com>
2015-12-15 15:08:49 -0500

    Stub in a `runHaskellParser` function.

--- app/Haskell.hs
+++ app/Haskell.hs
@@ -1,7 +1,17 @@
 module Haskell where
 
+import Diff
+import Range
+import Syntax
+import Term
+import Control.Comonad.Cofree
 import Text.Parsec
 import Text.Parsec.String
 
-haskellParser :: Parser String
-haskellParser = many anyChar
+haskellParser :: Parser (Term String Info)
+haskellParser = toTerm <$> many anyChar
+  where toTerm a = Info (Range 0 0) mempty :< Leaf a
+
+runHaskellParser :: String -> IO (Term String Info)
+runHaskellParser input = return . either errorToTerm id $ parse haskellParser "" input
+  where errorToTerm _ = Info (Range 0 0) mempty :< Leaf "_"
