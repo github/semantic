5add0089a25a1b030a05e0b2e797f7f60907977f
Rob Rix <rob.rix@github.com>
2015-12-15 14:35:37 -0500

    Lean on the Monoid instance.

--- app/Parser.hs
+++ app/Parser.hs
@@ -6,7 +6,6 @@ import Syntax
 import Term
 import TreeSitter
 import Control.Comonad.Cofree
-import qualified Data.Set as Set
 
 type Parser = String -> IO (Term String Info)
 
@@ -15,8 +14,8 @@ lineByLineParser input = return . root . Indexed $ case foldl annotateLeaves ([]
   (leaves, _) -> leaves
   where
     lines = Prelude.lines input
-    root syntax = Info (Range 0 $ length input) Set.empty :< syntax
-    leaf charIndex line = Info (Range charIndex $ charIndex + length line) Set.empty :< Leaf line
+    root syntax = Info (Range 0 $ length input) mempty :< syntax
+    leaf charIndex line = Info (Range charIndex $ charIndex + length line) mempty :< Leaf line
     annotateLeaves (accum, charIndex) line =
       (accum ++ [ leaf charIndex line ]
       , charIndex + length line + 1)
