71d55d177e2508f366119e1a383ee6618adf7c92
Rob Rix <rob.rix@github.com>
2015-12-17 16:34:28 -0500

    Revert "Figure out our extra lib dirs correctly."
    This reverts commit fe4dcf2ae2f5f4118e3df6ae28d63b304482683c.

--- Setup.hs
+++ Setup.hs
@@ -1,25 +1,2 @@
-import Data.Maybe
-import qualified Distribution.PackageDescription as P
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Setup
-import System.Directory
-
-main = defaultMainWithHooks simpleUserHooks { confHook = conf }
-
-conf :: (P.GenericPackageDescription, P.HookedBuildInfo) -> ConfigFlags -> IO LocalBuildInfo
-conf x flags = do
-  localBuildInfo <- confHook simpleUserHooks x flags
-  let packageDescription = localPkgDescr localBuildInfo
-      library = fromJust $ P.library packageDescription
-      libraryBuildInfo = P.libBuildInfo library in do
-      dir <- getCurrentDirectory
-      return localBuildInfo {
-        localPkgDescr = packageDescription {
-          P.library = Just $ library {
-            P.libBuildInfo = libraryBuildInfo {
-              P.extraLibDirs = (dir ++ "/prototype/External/tree-sitter/out/Release") : P.extraLibDirs libraryBuildInfo
-            }
-          }
-        }
-      }
+main = defaultMain
