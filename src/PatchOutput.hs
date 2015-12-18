e22f951038c689166c61be734ad0f8e18eb35246
Rob Rix <rob.rix@github.com>
2015-12-18 08:12:11 -0500

    Count the inserted & deleted lines.

--- src/PatchOutput.hs
+++ src/PatchOutput.hs
@@ -8,7 +8,12 @@ patch diff sourceA sourceB = mconcat $ hunks diff sourceA sourceB
 data Hunk = Hunk Int Int [Line]
 
 header :: Hunk -> String
-header (Hunk offsetA offsetB _) = "@@ -" ++ show offsetA ++ " +" ++ show offsetB ++ " @@\n"
+header (Hunk offsetA offsetB lines) = "@@ -" ++ show offsetA ++ " +" ++ show offsetB ++ " @@\n"
+  where (countDeleted, countInserted) = foldl countLine (0, 0) lines
+        countLine (countDeleted, countInserted) line = case line of
+          Insert _ -> (countDeleted, countInserted + 1)
+          Delete _ -> (countDeleted + 1, countInserted)
+          Context _ -> (countDeleted + 1, countInserted + 1)
 
 data Line = Insert String | Delete String | Context String
 
