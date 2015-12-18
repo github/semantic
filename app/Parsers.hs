e2817db88583ca156c9622a00785a60c8f79bb35
joshvera <vera@github.com>
2015-12-18 14:48:44 -0500

    Convert lineByLineParser to return text

--- app/Parsers.hs
+++ app/Parsers.hs
@@ -7,17 +7,18 @@ import Syntax
 import Term
 import TreeSitter
 import Control.Comonad.Cofree
+import qualified Data.Text as T
 
-parserForType :: String -> Parser
+parserForType :: T.Text -> Parser
 parserForType mediaType = maybe lineByLineParser parseTreeSitterFile $ languageForType mediaType
 
 lineByLineParser :: Parser
 lineByLineParser input = return . root . Indexed $ case foldl annotateLeaves ([], 0) lines of
   (leaves, _) -> leaves
   where
-    lines = Prelude.lines input
-    root syntax = Info (Range 0 $ length input) mempty :< syntax
-    leaf charIndex line = Info (Range charIndex $ charIndex + length line) mempty :< Leaf line
+    lines = T.lines input
+    root syntax = Info (Range 0 $ T.length input) mempty :< syntax
+    leaf charIndex line = Info (Range charIndex $ charIndex + T.length line) mempty :< Leaf line
     annotateLeaves (accum, charIndex) line =
       (accum ++ [ leaf charIndex line ]
-      , charIndex + length line + 1)
+      , charIndex + T.length line + 1)
