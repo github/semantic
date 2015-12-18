44d437ccdb688d2f530da2bc552efbd3abcefd16
Rob Rix <rob.rix@github.com>
2015-12-18 14:20:46 -0500

    :fire: the prototype.

--- prototype/Doubt/StringLiteralConvertible.swift
+++
@@ -1,9 +0,0 @@
-extension StringLiteralConvertible {
-	public init(unicodeScalarLiteral: Self.StringLiteralType) {
-		self.init(stringLiteral: unicodeScalarLiteral)
-	}
-
-	public init(extendedGraphemeClusterLiteral: Self.StringLiteralType) {
-		self.init(stringLiteral: extendedGraphemeClusterLiteral)
-	}
-}
