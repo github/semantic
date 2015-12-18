6711d29ea4881009ef52c55ac95880a35b3bd6a8
Rob Rix <rob.rix@github.com>
2015-12-18 08:07:43 -0500

    Stub in hunk headers.

--- src/PatchOutput.hs
+++ src/PatchOutput.hs
@@ -6,6 +6,10 @@ patch :: Diff a Info -> String -> String -> String
 patch diff sourceA sourceB = mconcat $ hunks diff sourceA sourceB
 
 data Hunk = Hunk Int Int [Line]
+
+header :: Hunk -> String
+header (Hunk offsetA offsetB _) = "@@ -" ++ show offsetA ++ " +" ++ show offsetB ++ " @@\n"
+
 data Line = Insert String | Delete String | Context String
 
 hunks :: Diff a Info -> String -> String -> [String]
