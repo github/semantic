c3a292b8331e368e4389c4d7bb4204997ccd9bf9
Rob Rix <rob.rix@github.com>
2015-12-18 08:00:01 -0500

    A patch is the concatenation of a list of hunks.

--- src/PatchOutput.hs
+++ src/PatchOutput.hs
@@ -3,4 +3,7 @@ module PatchOutput where
 import Diff
 
 patch :: Diff a Info -> String -> String -> String
-patch _ _ _ = ""
+patch diff sourceA sourceB = mconcat $ hunks diff sourceA sourceB
+
+hunks :: Diff a Info -> String -> String -> [String]
+hunks diff sourceA sourceB = []
