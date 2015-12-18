dd924c68dda8d39505700782e66e6e8d137885fc
Rob Rix <rob.rix@github.com>
2015-12-18 09:49:33 -0500

    Test that empty diffs have no hunks.

--- test/PatchOutputSpec.hs
+++ test/PatchOutputSpec.hs
@@ -1,7 +1,14 @@
 module PatchOutputSpec where
 
+import Diff
+import PatchOutput
+import Range
+import Syntax
+import Control.Monad.Free
 import Test.Hspec
 
 spec :: Spec
 spec = do
-  return ()
+  describe "hunks" $ do
+    it "empty diffs have no hunks" $
+      hunks (Free . Annotated (Info (Range 0 0) mempty, Info (Range 0 0) mempty) $ Leaf "") "" "" `shouldBe` []
