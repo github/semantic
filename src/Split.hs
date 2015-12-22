1bed6a771032254fdefced91dddfe713435f1f2f
Rob Rix <rob.rix@github.com>
2015-12-22 10:54:54 -0500

    Define toMarkup over Renderable fixed terms.

--- src/Split.hs
+++ src/Split.hs
@@ -138,6 +138,7 @@ instance ToMarkup (Renderable (Term a Info)) where
   toMarkup (Renderable (source, Info range categories :< syntax)) = classifyMarkup (maybeLast categories) $ case syntax of
     Leaf _ -> span . string $ substring range source
     Indexed children -> ul . mconcat $ contentElements children
+    Fixed children -> ul . mconcat $ contentElements children
     where markupForSeparatorAndChild :: ([Markup], Int) -> Term a Info -> ([Markup], Int)
           markupForSeparatorAndChild (rows, previous) child = (rows ++ [ string (substring (Range previous $ start $ getRange child) source), toMarkup (Renderable (source, child)) ], end $ getRange child)
 
