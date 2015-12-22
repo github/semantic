b9ed84b7b6e062f9be33824d7164c4caf3fb443c
Rob Rix <rob.rix@github.com>
2015-12-21 22:34:08 -0500

    Pass categories to contextLines explicitly.

--- src/Split.hs
+++ src/Split.hs
@@ -151,16 +151,16 @@ diffToRows (Pure (Replace a b)) _ before after = (replacedRows, (leftRange, righ
 -- | Takes a term and a source and returns a list of lines and their range within source.
 splitTermByLines :: Term a Info -> String -> ([Line (Term a Info)], Range)
 splitTermByLines (Info range categories :< syntax) source = flip (,) range $ case syntax of
-  Leaf a -> contextLines (Leaf a) range source
+  Leaf a -> contextLines (Leaf a) range categories source
   Indexed children -> adjoinChildLines Indexed children
   Fixed children -> adjoinChildLines Fixed children
   Keyed children -> adjoinChildLines Keyed children
   where adjoin = reverse . foldl (adjoinLinesBy $ openTerm source) []
         adjoinChildLines constructor children = let (lines, previous) = foldl (childLines $ constructor mempty) ([], start range) children in
-          adjoin $ lines ++ contextLines (constructor mempty) (Range previous $ end range) source
-        contextLines constructor range source = Line True . (:[]) . (:< constructor) . (`Info` categories) <$> actualLineRanges range source
+          adjoin $ lines ++ contextLines (constructor mempty) (Range previous $ end range) categories source
+        contextLines constructor range categories source = Line True . (:[]) . (:< constructor) . (`Info` categories) <$> actualLineRanges range source
         childLines constructor (lines, previous) child = let (childLines, childRange) = splitTermByLines child source in
-          (adjoin $ lines ++ contextLines constructor (Range previous $ start childRange) source ++ childLines, end childRange)
+          (adjoin $ lines ++ contextLines constructor (Range previous $ start childRange) categories source ++ childLines, end childRange)
 
 -- | Takes a term and a source and returns a list of lines and their range within source.
 termToLines :: Term a Info -> String -> ([Line HTML], Range)
