cff017ccddac5e06c5b5c4c33f5c118fc576e8e7
Rob Rix <rob.rix@github.com>
2015-12-21 22:45:21 -0500

    Define splitAnnotatedByLines over Keyed diffs.

--- src/Split.hs
+++ src/Split.hs
@@ -191,6 +191,7 @@ splitAnnotatedByLines sources ranges categories syntax = case syntax of
   Leaf a -> contextRows (Leaf a) ranges categories sources
   Indexed children -> adjoinChildRows Indexed children
   Fixed children -> adjoinChildRows Fixed children
+  Keyed children -> adjoinChildRows Keyed children
   where contextRows constructor ranges categories sources = zipWithDefaults Row EmptyLine EmptyLine (contextLines constructor (fst ranges) (fst categories) (fst sources)) (contextLines constructor (snd ranges) (snd categories) (snd sources))
 
         adjoin = reverse . foldl (adjoinRowsBy (openTerm $ fst sources) (openTerm $ snd sources)) []
