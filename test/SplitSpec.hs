b121ee54e336958abb353b605ae1f327355456ea
Rob Rix <rob.rix@github.com>
2015-12-22 17:06:55 -0500

    Remove openString.

--- test/SplitSpec.hs
+++ test/SplitSpec.hs
@@ -121,7 +121,6 @@ spec = do
 
       leafWithRangesInSources sourceA sourceB rangeA rangeB = Free $ Annotated (Info rangeA mempty, Info rangeB mempty) (Leaf $ substring rangeA sourceA ++ substring rangeB sourceB)
 
-      openString string = const string <$> openRange string (totalRange string)
       openMaybe :: Maybe String -> Maybe (Maybe String)
       openMaybe (Just a) = Just (Just a)
       openMaybe Nothing =  Nothing
