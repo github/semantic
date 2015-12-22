cea5193999deeb34a63d74545fd60d69f05b0694
Rob Rix <rob.rix@github.com>
2015-12-21 22:58:12 -0500

    Define splitDiffByLines over Insert patches.

--- src/Split.hs
+++ src/Split.hs
@@ -130,8 +130,10 @@ instance Monoid (Line a) where
   mappend (Line c1 xs) (Line c2 ys) = Line (c1 || c2) (xs <> ys)
 
 splitDiffByLines :: Diff a Info -> (Int, Int) -> (String, String) -> ([Row (Term a Info)], (Range, Range))
-splitDiffByLines diff _ sources = case diff of
+splitDiffByLines diff (prevLeft, prevRight) sources = case diff of
   Free (Annotated annotation syntax) -> (splitAnnotatedByLines sources (ranges annotation) (categories annotation) syntax, ranges annotation)
+  Pure (Insert term) -> let (lines, range) = splitTermByLines term (snd sources) in
+    (Row EmptyLine <$> lines, (Range prevLeft prevLeft, range))
   where categories (Info _ left, Info _ right) = (left, right)
         ranges (Info left _, Info right _) = (left, right)
 
