ed0b0b39e93e2c39fbc42551605ca18af2893275
Rob Rix <rob.rix@github.com>
2015-12-28 12:31:23 -0500

    wrapRowContents takes separate functions for each side.

--- src/Row.hs
+++ src/Row.hs
@@ -5,8 +5,8 @@ import Line
 data Row a = Row { unLeft :: Line a, unRight :: Line a }
   deriving (Eq, Functor)
 
-wrapRowContents :: ([a] -> b) -> Row a -> Row b
-wrapRowContents transform (Row left right) = Row (wrapLineContents transform left) (wrapLineContents transform right)
+wrapRowContents :: ([a] -> b) -> ([a] -> b) -> Row a -> Row b
+wrapRowContents transformLeft transformRight (Row left right) = Row (wrapLineContents transformLeft left) (wrapLineContents transformRight right)
 
 adjoinRowsBy :: (a -> Maybe a) -> (a -> Maybe a) -> [Row a] -> Row a -> [Row a]
 adjoinRowsBy _ _ [] row = [row]
