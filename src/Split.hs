1fa2450c6edfe1583373ec5c856819379fe6a09c
Rob Rix <rob.rix@github.com>
2015-12-29 09:51:00 -0500

    String/Term pairs are term containers.

--- src/Split.hs
+++ src/Split.hs
@@ -109,6 +109,10 @@ instance TermContainer (Term String Info) where
   toTerm = id
   setTerm _ = id
 
+instance TermContainer (String, Term String Info) where
+  toTerm = snd
+  setTerm (key, _) t = (key, t)
+
 -- | Takes a term and a source and returns a list of lines and their range within source.
 splitTermByLines :: Eq a => Term a Info -> Source Char -> ([Line (Term a Info)], Range)
 splitTermByLines (Info range categories :< syntax) source = flip (,) range $ case syntax of
