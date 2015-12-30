8b4d158f4c25f2b118f3027d80b576e6438679e2
Rob Rix <rob.rix@github.com>
2015-12-30 15:31:27 -0500

    Pass sources as a pair.

--- test/PatchOutputSpec.hs
+++ test/PatchOutputSpec.hs
@@ -12,4 +12,4 @@ spec :: Spec
 spec = do
   describe "hunks" $ do
     it "empty diffs have no hunks" $
-      hunks (Free . Annotated (Info (Range 0 0) mempty, Info (Range 0 0) mempty) $ Leaf "") (fromList "") (fromList "") `shouldBe` []
+      hunks (Free . Annotated (Info (Range 0 0) mempty, Info (Range 0 0) mempty) $ Leaf "") (fromList "", fromList "") `shouldBe` []
