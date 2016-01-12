a62d1defe2520c11709ba72cd66f5cc816c14077
Matt Diephouse <matt@diephouse.com>
2016-01-12 10:59:13 -0500

    Extract range and word extraction

--- app/Main.hs
+++ app/Main.hs
@@ -72,8 +72,9 @@ printDiff arguments (aSource, bSource) (aTerm, bTerm) = case renderer arguments 
 replaceLeavesWithWordBranches :: Source Char -> Term T.Text Info -> Term T.Text Info
 replaceLeavesWithWordBranches source = cata replaceIn
   where
-    replaceIn info@(Info range categories) (Leaf _) | ranges <- rangesAndWordsFrom (start range) (toList $ slice range source), length ranges > 1 = info :< (Indexed $ makeLeaf categories <$> ranges)
+    replaceIn info@(Info range categories) (Leaf _) | ranges <- rangesAndWordsInSource range, length ranges > 1 = info :< (Indexed $ makeLeaf categories <$> ranges)
     replaceIn info syntax = info :< syntax
+    rangesAndWordsInSource range = rangesAndWordsFrom (start range) (toList $ slice range source)
     makeLeaf categories (range, substring) = Info range categories :< Leaf (T.pack substring)
 
 readAndTranscodeFile :: FilePath -> IO (Source Char)
