b5b00505cc33851b0c1c55c7f917198b82a6591a
Matt Diephouse <matt@diephouse.com>
2016-01-13 12:39:52 -0500

    Make take and skip take predicates

--- src/Range.hs
+++ src/Range.hs
@@ -28,22 +28,21 @@ offsetRange i (Range start end) = Range (i + start) (i + end)
 
 rangesAndWordsFrom :: Int -> String -> [(Range, String)]
 rangesAndWordsFrom _ "" = []
-rangesAndWordsFrom startIndex string = fromMaybe [] $ take <$> (word <|> punctuation) <|> skip <$> space
+rangesAndWordsFrom startIndex string = fromMaybe [] $ take isWord <|> take isPunctuation <|> skip Char.isSpace
   where
-    word = parse isWord string
-    punctuation = parse isPunctuation string
-    space = parse Char.isSpace string
-    take (parsed, rest) = (Range startIndex $ endFor parsed, parsed) : rangesAndWordsFrom (endFor parsed) rest
-    skip (parsed, rest) = rangesAndWordsFrom (endFor parsed) rest
+    take predicate = recurse <$> parse predicate string
+      where recurse (parsed, rest) = (Range startIndex $ endFor parsed, parsed) : rangesAndWordsFrom (endFor parsed) rest
+    skip predicate = recurse <$> parse predicate string
+      where recurse (parsed, rest) = rangesAndWordsFrom (endFor parsed) rest
     endFor parsed = startIndex + length parsed
     parse predicate string = case span predicate string of
       ([], _) -> Nothing
       (parsed, rest) -> Just (parsed, rest)
-    isPunctuation c = not (Char.isSpace c || isWord c)
     -- | Is this a word character?
     -- | Word characters are defined as in [Ruby’s `\p{Word}` syntax](http://ruby-doc.org/core-2.1.1/Regexp.html#class-Regexp-label-Character+Properties), i.e.:
     -- | > A member of one of the following Unicode general category _Letter_, _Mark_, _Number_, _Connector_Punctuation_
     isWord c = Char.isLetter c || Char.isNumber c || Char.isMark c || Char.generalCategory c == Char.ConnectorPunctuation
+    isPunctuation c = not (Char.isSpace c || isWord c)
 
 -- | Return Just the last index from a non-empty range, or if the range is empty, Nothing.
 maybeLastIndex :: Range -> Maybe Int
