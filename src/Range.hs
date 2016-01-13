cc5a1b7736969cc1f1f39e84bd1c6b826c21c218
Matt Diephouse <matt@diephouse.com>
2016-01-13 13:03:11 -0500

    Let the transform take 2 arguments

--- src/Range.hs
+++ src/Range.hs
@@ -30,12 +30,12 @@ rangesAndWordsFrom :: Int -> String -> [(Range, String)]
 rangesAndWordsFrom _ "" = []
 rangesAndWordsFrom startIndex string = fromMaybe [] $ take isWord <|> take isPunctuation <|> skip Char.isSpace
   where
-    take predicate = parse predicate $ \(parsed, rest) -> (Range startIndex $ endFor parsed, parsed) : rangesAndWordsFrom (endFor parsed) rest
-    skip predicate = parse predicate $ \(parsed, rest) -> rangesAndWordsFrom (endFor parsed) rest
+    take predicate = parse predicate $ \ parsed rest -> (Range startIndex $ endFor parsed, parsed) : rangesAndWordsFrom (endFor parsed) rest
+    skip predicate = parse predicate $ \ parsed rest -> rangesAndWordsFrom (endFor parsed) rest
     endFor parsed = startIndex + length parsed
     parse predicate transform = case span predicate string of
       ([], _) -> Nothing
-      (parsed, rest) -> Just $ transform (parsed, rest)
+      (parsed, rest) -> Just $ transform parsed rest
     -- | Is this a word character?
     -- | Word characters are defined as in [Ruby’s `\p{Word}` syntax](http://ruby-doc.org/core-2.1.1/Regexp.html#class-Regexp-label-Character+Properties), i.e.:
     -- | > A member of one of the following Unicode general category _Letter_, _Mark_, _Number_, _Connector_Punctuation_
