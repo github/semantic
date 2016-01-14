9c272a40d7c9ab7f5e9ab351555cf97e5ee70c31
Matt Diephouse <matt@diephouse.com>
2016-01-14 11:18:57 -0500

    Document declarations in Patch.hs

--- src/Patch.hs
+++ src/Patch.hs
@@ -1,20 +1,24 @@
 module Patch where
 
+-- | An operation to replace, insert, or delete an item.
 data Patch a =
   Replace a a
   | Insert a
   | Delete a
   deriving (Functor, Show, Eq)
 
+-- | Return the item from the after side of the patch.
 after :: Patch a -> Maybe a
 after (Replace _ a) = Just a
 after (Insert a) = Just a
 after _ = Nothing
 
+-- | Return the item from the before side of the patch.
 before :: Patch a -> Maybe a
 before (Replace a _) = Just a
 before (Delete a) = Just a
 before _ = Nothing
 
+-- | Calculate the cost of the patch given a function to compute the cost of a item.
 patchSum :: (a -> Integer) -> Patch a -> Integer
 patchSum termCost patch = (maybe 0 termCost $ before patch) + (maybe 0 termCost $ after patch)
