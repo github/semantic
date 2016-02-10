219d47bc95f1fd0d9c989f193124dd92d4a07ed0
Matt Diephouse <matt@diephouse.com>
2016-02-10 10:03:15 -0800

    Don't pass preassembled Infos to Constructors

--- src/TreeSitter.hs
+++ src/TreeSitter.hs
@@ -1,7 +1,6 @@
 module TreeSitter where
 
 import Category
-import Diff
 import Range
 import Parser
 import Source
@@ -47,9 +46,9 @@ data Language = Language { getTsLanguage :: Ptr TSLanguage, getConstructor :: Co
 -- | Returns a Language based on the file extension (including the ".").
 languageForType :: T.Text -> Maybe Language
 languageForType mediaType = case mediaType of
-    ".h" -> Just . Language ts_language_c $ termConstructor
-    ".c" -> Just . Language ts_language_c $ termConstructor
-    ".js" -> Just . Language ts_language_javascript $ termConstructor
+    ".h" -> Just . Language ts_language_c $ termConstructor categoryForNodeName
+    ".c" -> Just . Language ts_language_c $ termConstructor categoryForNodeName
+    ".js" -> Just . Language ts_language_javascript $ termConstructor categoryForNodeName
     _ -> Nothing
 
 -- | Returns a parser for the given language.
@@ -65,10 +64,10 @@ parseTreeSitterFile (Language language constructor) contents = do
     return term)
 
 -- | Given a node name from TreeSitter, return the correct category.
-categoryForNodeName :: String -> Category
+categoryForNodeName :: String -> Set.Set Category
 categoryForNodeName name = case name of
-  "function_call" -> FunctionCall
-  _ -> Other name
+  "function_call" -> Set.singleton FunctionCall
+  _ -> Set.singleton (Other name)
 
 -- | Given a constructor and a tree sitter document, return a parser.
 documentToTerm :: Constructor -> Ptr TSDocument -> Parser
@@ -84,7 +83,7 @@ documentToTerm constructor document contents = alloca $ \ root -> do
           -- Note: The strict application here is semantically important. Without it, we may not evaluate the range until after we’ve exited the scope that `node` was allocated within, meaning `alloca` will free it & other stack data may overwrite it.
           range <- return $! Range { start = fromIntegral $ ts_node_p_start_char node, end = fromIntegral $ ts_node_p_end_char node }
 
-          return (name, constructor contents (Info range (Set.singleton $ categoryForNodeName name)) children)
+          return (name, constructor contents range name children)
         getChild node n out = do
           _ <- ts_node_p_named_child node n out
           toTerm out
