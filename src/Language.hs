b7355bf7e648e9cf985d1f62a4945b10af5d12b1
Matt Diephouse <matt@diephouse.com>
2016-02-10 14:35:08 -0800

    Add a bunch more languages
    Even though they don’t do anything

--- src/Language.hs
+++ src/Language.hs
@@ -5,7 +5,21 @@ import Data.Text
 -- | A programming language.
 data Language =
     C
+    | CoffeeScript
+    | CPlusPlus
+    | CSharp
+    | CSS
+    | Haskell
+    | HTML
+    | Java
     | JavaScript
+    | ObjectiveC
+    | Perl
+    | PHP
+    | Python
+    | R
+    | Ruby
+    | Swift
 
 -- | Returns a Language based on the file extension (including the ".").
 languageForType :: Text -> Maybe Language
