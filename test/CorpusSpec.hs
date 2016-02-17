bf12a2a13e6f3a09afdaafcc5d5a1e57545447a5
Matt Diephouse <matt@diephouse.com>
2016-02-17 15:54:37 -0500

    Pass a renderer into testDiff

--- test/CorpusSpec.hs
+++ test/CorpusSpec.hs
@@ -1,6 +1,7 @@
 module CorpusSpec where
 
 import Diffing
+import Renderer
 import Unified
 
 import Data.Bifunctor.Join
@@ -8,6 +9,7 @@ import qualified Data.ByteString.Char8 as B1
 import Data.List as List
 import Data.Map as Map
 import Data.Set as Set
+import qualified Data.Text as T
 import Rainbow
 import System.FilePath
 import System.FilePath.Glob
@@ -25,7 +27,10 @@ spec = do
   where
     runTestsIn directory = do
       tests <- runIO $ examples directory
-      mapM_ (\ (a, b, diff) -> it (normalizeName a) $ testDiff a b diff `shouldReturn` True) tests
+      mapM_ (\ (a, b, diff) -> it (normalizeName a) $ testDiff testUnified a b diff `shouldReturn` True) tests
+    testUnified :: Renderer a String
+    testUnified diff sources = B1.unpack $ mconcat $ chunksToByteStrings toByteStringsColors0 $ unified diff sources
+
 
 -- | Return all the examples from the given directory. Examples are expected to
 -- | have the form "foo.A.js", "foo.B.js", "foo.diff.js". Diffs are not
@@ -50,14 +55,13 @@ normalizeName path = addExtension (dropExtension $ dropExtension path) (takeExte
 -- | Given file paths for A, B, and, optionally, a diff, return whether diffing
 -- | the files will produce the diff. If no diff is provided, then the result
 -- | is true, but the diff will still be calculated.
-testDiff :: FilePath -> FilePath -> Maybe FilePath -> IO Bool
-testDiff a b diff = do
+testDiff :: Renderer T.Text String -> FilePath -> FilePath -> Maybe FilePath -> IO Bool
+testDiff renderer a b diff = do
   let parser = parserForFilepath a
   sources <- sequence $ readAndTranscodeFile <$> Join (a, b)
-  chunks <- diffFiles parser unified (runJoin sources)
-  let actual = mconcat $ chunksToByteStrings toByteStringsColors0 chunks
+  actual <- diffFiles parser renderer (runJoin sources)
   case diff of
     Nothing -> return $ actual /= "<should not be a thing>"
     Just file -> do
-      expected <- B1.readFile file
+      expected <- readFile file
       return $ expected == actual
