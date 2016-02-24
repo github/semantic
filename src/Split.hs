fdc2956ab3638d515e8ce626326ae98f9e05b872
Rob Rix <rob.rix@github.com>
2016-02-24 09:26:57 -0700

    Derive an Eq instance for SplitPatch.

--- src/Split.hs
+++ src/Split.hs
@@ -90,7 +90,7 @@ split diff (beforeBlob, afterBlob) = renderHtml
 
 -- | A patch to only one side of a diff.
 data SplitPatch a = SplitInsert a | SplitDelete a | SplitReplace a
-  deriving Show
+  deriving (Show, Eq)
 
 -- | A diff with only one side’s annotations.
 type SplitDiff leaf annotation = Free (Annotated leaf annotation) (SplitPatch (Term leaf annotation))
