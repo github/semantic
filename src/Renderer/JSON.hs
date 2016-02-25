1a45d0b1d642d606a6a3004a686f3bb7bda45c2b
Rob Rix <rob.rix@github.com>
2016-02-25 14:15:22 -0700

    Stub in a JSON renderer.

--- src/Renderer/JSON.hs
+++ src/Renderer/JSON.hs
@@ -1,11 +1,15 @@
 module Renderer.JSON (
-  JSON (JSON, rows)
+  json
 ) where
 
 import Diff
 import Row
 import Source
+import Renderer
 import Renderer.Split
 
 -- | JSON representing an aligned diff.
 data JSON = JSON { rows :: [Row (SplitDiff (Source Char) Info)] }
+
+json :: Renderer a String
+json diff sources = ""
