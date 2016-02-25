8fcb8c3e7668de776cd96f741fe33de2a22cade1
Rob Rix <rob.rix@github.com>
2016-02-25 16:49:18 -0700

    iter/cata over diffs & patches.

--- src/Renderer/JSON.hs
+++ src/Renderer/JSON.hs
@@ -11,7 +11,8 @@ import Renderer
 import Renderer.Split
 import Source hiding ((++), toList)
 import Syntax
-import Control.Comonad.Cofree
+import Term
+import Control.Arrow
 import Control.Monad.Free
 import Data.Foldable
 
@@ -27,12 +28,10 @@ instance Show (JSON a) where
     where showRow (Row left right) = "{'left':" ++ showLine left ++ ",'right':" ++ showLine right ++ "}"
           showLine EmptyLine = "null"
           showLine (Line diffs) = mconcat (showDiff <$> toList diffs)
-          showDiff (Pure patch) = showPatch patch
-          showDiff (Free (Annotated info syntax)) = showInfoSyntax info syntax
-          showPatch (SplitInsert term) = "{'insert':" ++ showTerm term ++ "}"
-          showPatch (SplitDelete term) = "{'delete':" ++ showTerm term ++ "}"
-          showPatch (SplitReplace term) = "{'replace':" ++ showTerm term ++ "}"
-          showTerm (info :< syntax) = showInfoSyntax info syntax
+          showDiff diff = iter (uncurry showInfoSyntax . (getAnnotation &&& getSyntax)) (showPatch <$> diff)
+          showPatch (SplitInsert term) = "{'insert':" ++ cata showInfoSyntax term ++ "}"
+          showPatch (SplitDelete term) = "{'delete':" ++ cata showInfoSyntax term ++ "}"
+          showPatch (SplitReplace term) = "{'replace':" ++ cata showInfoSyntax term ++ "}"
           showInfoSyntax (Info range categories) syntax = "{'range':" ++ showRange range ++ ",'categories':" ++ showCategories categories ++ "," ++ showSyntax syntax ++ "}"
           showRange (Range start end) = "{'start':" ++ show start ++ ",'end':" ++ show end ++ "}"
           showCategories categories = "{" ++ mconcat (show <$> toList categories) ++ "}"
