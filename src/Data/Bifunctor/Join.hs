509ddedf7b7acb9bd6f2afedf9b701686273320d
Rob Rix <rob.rix@github.com>
2016-02-26 18:56:44 -0500

    Add an Applicative instance.

--- src/Data/Bifunctor/Join.hs
+++ src/Data/Bifunctor/Join.hs
@@ -2,3 +2,7 @@ module Data.Bifunctor.Join where
 
 newtype Join a = Join { runJoin :: (a, a) }
   deriving (Eq, Show, Functor, Foldable, Traversable)
+
+instance Applicative Join where
+  pure a = Join (a, a)
+  Join (f, g) <*> Join (a, b) = Join (f a, g b)
