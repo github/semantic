5525da7afd657c285fc96144efd9b4e0eda9cec9
Rob Rix <rob.rix@github.com>
2016-02-26 12:18:19 -0700

    Write Eq instances.

--- src/Control/Monad/Free.hs
+++ src/Control/Monad/Free.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Monad.Free where
 
 data Free functor pure = Free (functor (Free functor pure)) | Pure pure
   deriving (Functor, Foldable, Traversable)
+
+instance (Eq pure, Eq (functor (Free functor pure))) => Eq (Free functor pure) where
+  Pure a == Pure b = a == b
+  Free f == Free g = f == g
+  _ == _ = False
