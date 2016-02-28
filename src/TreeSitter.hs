e0b73da735ddfadf767ff58f9d50f163afa42f71
Rob Rix <rob.rix@github.com>
2016-02-27 19:34:10 -0500

    Delegate the FFI to tree-sitter-parsers.

--- src/TreeSitter.hs
+++ src/TreeSitter.hs
@@ -7,39 +7,12 @@ import Range
 import Source
 import qualified Data.Set as Set
 import Foreign
-import Foreign.C
-import Foreign.C.Types
-import Foreign.CStorable
-import qualified GHC.Generics as Generics
-
-data TSLanguage = TsLanguage deriving (Show, Eq)
-
-data TSDocument = TsDocument deriving (Show, Eq)
-foreign import ccall "prototype/External/tree-sitter/include/tree_sitter/runtime.h ts_document_make" ts_document_make :: IO (Ptr TSDocument)
-foreign import ccall "prototype/External/tree-sitter/include/tree_sitter/runtime.h ts_document_set_language" ts_document_set_language :: Ptr TSDocument -> Ptr TSLanguage -> IO ()
-foreign import ccall "prototype/External/tree-sitter/include/tree_sitter/runtime.h ts_document_set_input_string" ts_document_set_input_string :: Ptr TSDocument -> CString -> IO ()
-foreign import ccall "prototype/External/tree-sitter/include/tree_sitter/runtime.h ts_document_parse" ts_document_parse :: Ptr TSDocument -> IO ()
-foreign import ccall "prototype/External/tree-sitter/include/tree_sitter/runtime.h ts_document_free" ts_document_free :: Ptr TSDocument -> IO ()
-
-data TSNode = TsNode { _data :: Ptr (), offset0 :: CSize, offset1 :: CSize, offset2 :: CSize }
-  deriving (Show, Eq, Generics.Generic)
-
-instance CStorable TSNode
-instance Storable TSNode where
-  alignment = cAlignment
-  sizeOf = cSizeOf
-  peek = cPeek
-  poke = cPoke
-
-foreign import ccall "app/bridge.h ts_document_root_node_p" ts_document_root_node_p :: Ptr TSDocument -> Ptr TSNode -> IO ()
-foreign import ccall "app/bridge.h ts_node_p_name" ts_node_p_name :: Ptr TSNode -> Ptr TSDocument -> IO CString
-foreign import ccall "app/bridge.h ts_node_p_named_child_count" ts_node_p_named_child_count :: Ptr TSNode -> IO CSize
-foreign import ccall "app/bridge.h ts_node_p_named_child" ts_node_p_named_child :: Ptr TSNode -> CSize -> Ptr TSNode -> IO CSize
-foreign import ccall "app/bridge.h ts_node_p_start_char" ts_node_p_start_char :: Ptr TSNode -> CSize
-foreign import ccall "app/bridge.h ts_node_p_end_char" ts_node_p_end_char :: Ptr TSNode -> CSize
+import Foreign.C.String
+import Text.Parser.TreeSitter hiding (Language(..))
+import qualified Text.Parser.TreeSitter as TS
 
 -- | Returns a TreeSitter parser for the given language and TreeSitter grammar.
-treeSitterParser :: Language -> Ptr TSLanguage -> Parser
+treeSitterParser :: Language -> Ptr TS.Language -> Parser
 treeSitterParser language grammar contents = do
   document <- ts_document_make
   ts_document_set_language document grammar
@@ -70,7 +43,7 @@ defaultCategoryForNodeName name = case name of
   _ -> Set.singleton (Other name)
 
 -- | Given a constructor and a tree sitter document, return a parser.
-documentToTerm :: Constructor -> Ptr TSDocument -> Parser
+documentToTerm :: Constructor -> Ptr Document -> Parser
 documentToTerm constructor document contents = alloca $ \ root -> do
   ts_document_root_node_p document root
   toTerm root
