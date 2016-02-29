cff25113057bc7a17216c8cd2a796c9ad3964820
Rob Rix <rob.rix@github.com>
2016-02-29 01:54:35 -0500

    Implement zipWith over Both.

--- src/Data/Functor/Both.hs
+++ src/Data/Functor/Both.hs
@@ -1,5 +1,7 @@
 module Data.Functor.Both where
 
+import Prelude hiding (zipWith)
+
 newtype Both a = Both { runBoth :: (a, a) }
   deriving (Eq, Show, Functor, Foldable, Traversable)
 
@@ -8,9 +10,12 @@ both :: a -> a -> Both a
 both = curry Both
 
 zip :: Both [a] -> [Both a]
-zip (Both ([], _)) = []
-zip (Both (_, [])) = []
-zip (Both (a : as, b : bs)) = both a b : Data.Functor.Both.zip (both as bs)
+zip = zipWith both
+
+zipWith :: (a -> a -> b) -> Both [a] -> [b]
+zipWith f (Both ([], _)) = []
+zipWith f (Both (_, [])) = []
+zipWith f (Both (a : as, b : bs)) = f a b : zipWith f (both as bs)
 
 unzip :: [Both a] -> Both [a]
 unzip = foldr pair (pure [])
