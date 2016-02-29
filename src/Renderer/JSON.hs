25214616c80deebe88420526bf801e464174e642
Rob Rix <rob.rix@github.com>
2016-02-29 17:10:47 -0500

    Remove the old JSON type.

--- src/Renderer/JSON.hs
+++ src/Renderer/JSON.hs
@@ -19,30 +19,9 @@ import Syntax
 import Term
 import Text.JSON
 
--- | JSON representing an aligned diff.
-newtype JSON a = JSON { rows :: [Row (SplitDiff a Info)] }
-
 -- | Render a diff to a string representing its JSON.
 json :: Renderer a String
-json diff (a, b) = show . JSON . fst $ splitDiffByLines diff (0, 0) (source a, source b)
-
-instance Show (JSON a) where
-  show (JSON rows) = "{'rows':[" ++ intercalate "," (showRow <$> rows) ++ "]}"
-    where showRow (Row left right) = "{'left':" ++ showLine left ++ ",'right':" ++ showLine right ++ "}"
-          showLine EmptyLine = "null"
-          showLine (Line diffs) = "[" ++ intercalate "," (showDiff <$> toList diffs) ++ "]"
-          showDiff diff = iter (uncurry showInfoSyntax . (getAnnotation &&& getSyntax)) (showPatch <$> diff)
-          showPatch (SplitInsert term) = "{'insert':" ++ cata showInfoSyntax term ++ "}"
-          showPatch (SplitDelete term) = "{'delete':" ++ cata showInfoSyntax term ++ "}"
-          showPatch (SplitReplace term) = "{'replace':" ++ cata showInfoSyntax term ++ "}"
-          showInfoSyntax (Info range categories) syntax = "{'range':" ++ showRange range ++ ",'categories':" ++ showCategories categories ++ "," ++ showSyntax syntax ++ "}"
-          showRange (Range start end) = "{'start':" ++ show start ++ ",'end':" ++ show end ++ "}"
-          showCategories categories = "[" ++ intercalate "," (show <$> toList categories) ++ "]"
-          showSyntax (Leaf _) = "'type':'leaf'"
-          showSyntax (Indexed children) = "'type':'indexed','children':[" ++ intercalate "," children ++ "]"
-          showSyntax (Fixed children) = "'type':'fixed','children':[" ++ intercalate "," children ++ "]"
-          showSyntax (Keyed children) = "'type':'keyed','children':{" ++ intercalate "," (uncurry showKeyValue <$> Map.toList children) ++ "}"
-          showKeyValue key value = "'" ++ show key ++ "': " ++ value
+json diff (a, b) = show . showJSON . fst $ splitDiffByLines diff (0, 0) (source a, source b)
 
 newtype JSONWrapper a = JSONWrapper { unWrap :: a }
 
