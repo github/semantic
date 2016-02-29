e33014ef272bd85c85e8b5b88d3f0747783265d1
Rob Rix <rob.rix@github.com>
2016-02-28 21:18:03 -0500

    Add a Monoid instance for Join.

--- src/Data/Bifunctor/Join.hs
+++ src/Data/Bifunctor/Join.hs
@@ -6,3 +6,7 @@ newtype Join a = Join { runJoin :: (a, a) }
 instance Applicative Join where
   pure a = Join (a, a)
   Join (f, g) <*> Join (a, b) = Join (f a, g b)
+
+instance Monoid a => Monoid (Join a) where
+  mempty = pure mempty
+  mappend a b = pure mappend <*> a <*> b
