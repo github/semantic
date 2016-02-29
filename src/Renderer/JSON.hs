8f85ad7fb3d16011676c4275ce8c2023583e3345
Rob Rix <rob.rix@github.com>
2016-02-29 16:18:24 -0500

    Intercalate lines and categories.

--- src/Renderer/JSON.hs
+++ src/Renderer/JSON.hs
@@ -29,14 +29,14 @@ instance Show (JSON a) where
   show (JSON rows) = "{'rows':[" ++ mconcat (showRow <$> rows) ++ "]}"
     where showRow (Row left right) = "{'left':" ++ showLine left ++ ",'right':" ++ showLine right ++ "}"
           showLine EmptyLine = "null"
-          showLine (Line diffs) = mconcat (showDiff <$> toList diffs)
+          showLine (Line diffs) = "[" ++ intercalate "," (showDiff <$> toList diffs) ++ "]"
           showDiff diff = iter (uncurry showInfoSyntax . (getAnnotation &&& getSyntax)) (showPatch <$> diff)
           showPatch (SplitInsert term) = "{'insert':" ++ cata showInfoSyntax term ++ "}"
           showPatch (SplitDelete term) = "{'delete':" ++ cata showInfoSyntax term ++ "}"
           showPatch (SplitReplace term) = "{'replace':" ++ cata showInfoSyntax term ++ "}"
           showInfoSyntax (Info range categories) syntax = "{'range':" ++ showRange range ++ ",'categories':" ++ showCategories categories ++ "," ++ showSyntax syntax ++ "}"
           showRange (Range start end) = "{'start':" ++ show start ++ ",'end':" ++ show end ++ "}"
-          showCategories categories = "{" ++ mconcat (show <$> toList categories) ++ "}"
+          showCategories categories = "{" ++ intercalate "," (show <$> toList categories) ++ "}"
           showSyntax (Leaf _) = "'type':'leaf'"
           showSyntax (Indexed children) = "'type':'indexed','children':[" ++ intercalate "," children ++ "]"
           showSyntax (Fixed children) = "'type':'fixed','children':[" ++ intercalate "," children ++ "]"
