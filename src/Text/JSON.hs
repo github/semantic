84f140977f065942de58e76cb1d5af5938725400
Rob Rix <rob.rix@github.com>
2016-02-29 16:42:06 -0500

    Define JSON types.
    These are intentionally source-compatible with the `json` package.

--- src/Text/JSON.hs
+++ src/Text/JSON.hs
@@ -1,1 +1,20 @@
 module Text.JSON where
+
+data JSValue
+  = JSNull
+  | JSBool !Bool
+  | JSRational Bool !Rational
+  | JSString JSString
+  | JSArray [JSValue]
+  | JSObject (JSObject JSValue)
+
+newtype JSString = JSONString { fromJSString :: String }
+
+toJSString :: String -> JSString
+toJSString = JSONString
+
+newtype JSObject value = JSONObject { fromJSObject :: [(String, value)] }
+
+toJSObject :: [(String, value)] -> JSObject value
+toJSObject = JSONObject
+
