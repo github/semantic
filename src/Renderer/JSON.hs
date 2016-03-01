2b3116fcbbb6338bb931ba71ed24db57989a760a
Rob Rix <rob.rix@github.com>
2016-03-01 13:40:24 -0500

    Encode patches directly into the node.

--- src/Renderer/JSON.hs
+++ src/Renderer/JSON.hs
@@ -41,7 +41,11 @@ instance ToJSON (SplitDiff leaf Info) where
   toJSON (Free (Annotated (Info range categories) syntax)) = object [ "range" .= toJSON range, "categories" .= toJSON categories, "syntax" .= toJSON syntax ]
   toJSON (Pure patch) = toJSON patch
   toEncoding (Free (Annotated info syntax)) = pairs (termSeries info syntax)
-  toEncoding (Pure patch) = toEncoding patch
+  toEncoding (Pure patch) = pairs . series $ case patch of
+    SplitInsert a -> ("insert", a)
+    SplitDelete a -> ("delete", a)
+    SplitReplace a -> ("replace", a)
+    where series (kind, (info :< syntax)) = termSeries info syntax <> "patch" .= T.pack kind
 instance ToJSON a => ToJSON (SplitPatch a) where
   toJSON (SplitInsert a) = object [ "insert" .= toJSON a ]
   toJSON (SplitDelete a) = object [ "delete" .= toJSON a ]
