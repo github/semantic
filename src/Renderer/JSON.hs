e01b0c7df9c616fa946260cff0b159346abb7c6d
Rob Rix <rob.rix@github.com>
2016-02-29 21:21:24 -0500

    Customize the Category instance.

--- src/Renderer/JSON.hs
+++ src/Renderer/JSON.hs
@@ -9,6 +9,7 @@ import Category
 import Data.Aeson hiding (json)
 import Data.ByteString.Lazy
 import Data.OrderedMap
+import qualified Data.Text as T
 import Diff
 import Line
 import Range
@@ -24,7 +25,9 @@ json :: ToJSON a => Renderer a ByteString
 json diff (a, b) = encode $ splitDiffByLines diff (0, 0) (source a, source b)
 
 instance (ToJSON leaf, ToJSON annotation, ToJSON recur) => ToJSON (Annotated leaf annotation recur)
-instance ToJSON Category
+instance ToJSON Category where
+  toJSON (Other s) = String $ T.pack s
+  toJSON s = String . T.pack $ show s
 instance ToJSON Info
 instance ToJSON a => ToJSON (Line a)
 instance (ToJSON key, ToJSON value) => ToJSON (OrderedMap key value)
