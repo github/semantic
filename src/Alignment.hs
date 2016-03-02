595b249391503e1f75fd9b769c2c4225c1310b10
Rob Rix <rob.rix@github.com>
2016-03-02 18:18:13 -0500

    Transpose Both terms inline.

--- src/Alignment.hs
+++ src/Alignment.hs
@@ -50,9 +50,7 @@ splitPatchByLines patch previous sources = case patch of
   Delete term -> let (lines, range) = splitTermByLines term (fst sources) in
     (flip makeRow EmptyLine . fmap (Pure . SplitDelete) <$> lines, Both (range, rangeAt $ snd previous))
   Replace leftTerm rightTerm -> (zipWithDefaults makeRow (pure mempty) $ fmap (fmap (Pure . SplitReplace)) <$> lines, ranges)
-    where (lines, ranges) = unpackLinesAndRanges $ splitTermByLines <$> both leftTerm rightTerm <*> sources
-          unpackLinesAndRanges :: Both ([Line (Term leaf Info)], Range) -> (Both [Line (Term leaf Info)], Both Range)
-          unpackLinesAndRanges = runBothWith (uncurry bimap . bimap both both)
+    where (lines, ranges) = runBothWith (uncurry bimap . bimap both both) $ splitTermByLines <$> both leftTerm rightTerm <*> sources
 
 -- | Takes a term and a source and returns a list of lines and their range within source.
 splitTermByLines :: Term leaf Info -> Source Char -> ([Line (Term leaf Info)], Range)
