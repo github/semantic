49eb9c1e4f71f10826b9a9ccd90a480d4c7b858b
Rob Rix <rob.rix@github.com>
2016-03-02 23:20:54 -0500

    Add a Copointed instance for Identity.

--- src/Data/Copointed.hs
+++ src/Data/Copointed.hs
@@ -1,7 +1,12 @@
 module Data.Copointed where
 
+import Data.Functor.Identity
+
 class Copointed c where
   copoint :: c a -> a
 
 instance Copointed ((,) a) where
   copoint = snd
+
+instance Copointed Identity where
+  copoint = runIdentity
