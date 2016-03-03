30c63a34a58c9d53c0c494634a0b94889fd12f64
Rob Rix <rob.rix@github.com>
2016-03-03 10:42:06 -0500

    Revert "Add a type annotation for safeRange."
    This reverts commit b456a9bfd53efec7cb61c204762773a2fdd5995e.

--- src/Alignment.hs
+++ src/Alignment.hs
@@ -110,7 +110,6 @@ splitAnnotatedByLines sources ranges categories syntax = case syntax of
         makeRanges a b = runBothWith safeRange <$> sequenceA (both a b)
 
         -- | Constructs a Range such that its end is clamped to its start.
-        safeRange :: Int -> Int -> Range
         safeRange start end = Range start (max start end)
 
 -- | Produces the starting indices of a diff.
