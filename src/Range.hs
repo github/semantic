cf8673b14f19c8b3040a8d6af4a21eb0bba6111e
Rob Rix <rob.rix@github.com>
2016-03-03 15:58:48 -0500

    Correct the docs of unionRanges.

--- src/Range.hs
+++ src/Range.hs
@@ -64,7 +64,7 @@ maybeLastIndex (Range _ end) = Just $ end - 1
 unionRange :: Range -> Range -> Range
 unionRange (Range start1 end1) (Range start2 end2) = Range (min start1 start2) (max end1 end2)
 
--- | Return a range that contains all the ranges in f.
+-- | Return a range that contains all the ranges in a Foldable, or Range 0 0 if it’s empty.
 unionRanges :: (Functor f, Foldable f) => f Range -> Range
 unionRanges ranges = option (Range 0 0) id . foldl mappend mempty $ Option . Just <$> ranges
 
