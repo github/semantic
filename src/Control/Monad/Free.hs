e86e1299be8da8c1dc37e532fae2939b986671c9
Rob Rix <rob.rix@github.com>
2016-03-04 15:37:23 -0500

    Revert "Document `unfold`."
    This reverts commit a9266026fc9705c8e793590337a7619672641f06.

--- src/Control/Monad/Free.hs
+++ src/Control/Monad/Free.hs
@@ -17,8 +17,5 @@ iter :: Functor functor => (functor pure -> pure) -> Free functor pure -> pure
 iter _ (Pure a) = a
 iter f (Free g) = f (iter f <$> g)
 
--- | Construct a `Free` by recursive applications of an unrolling function to seed values.
--- |
--- | Each time the function is applied to a seed, it can return either a `pure` value (which terminates this branch of the unfolding) or a value of `functor` parameterized by the seeds of its choice. In this way, the unrolling function can lazily construct infinite values in `Free`.
 unfold :: Functor functor => (seed -> Either pure (functor seed)) -> seed -> Free functor pure
 unfold unroll seed = either Pure (Free . fmap (unfold unroll)) (unroll seed)
