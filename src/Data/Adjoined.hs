9ecfdf3bfbe9b0babc1c11ff8389884f310a5d97
Rob Rix <rob.rix@github.com>
2016-03-04 22:08:56 -0500

    Replace Adjoining with PartialSemigroup.

--- src/Data/Adjoined.hs
+++ src/Data/Adjoined.hs
@@ -3,7 +3,5 @@ module Data.Adjoined where
 newtype Adjoined a = Adjoined { unAdjoined :: [a] }
   deriving (Eq, Foldable, Functor, Show, Traversable)
 
-class Adjoining a where
-  adjacent :: a -> a -> Bool
-  separate :: a -> a -> Bool
-  separate a = not . adjacent a
+class PartialSemigroup a where
+  coalesce :: a -> a -> Maybe a
