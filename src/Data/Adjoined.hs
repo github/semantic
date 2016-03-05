b251f1c67da9aab628819b7dc86e6a86a34b9da5
Rob Rix <rob.rix@github.com>
2016-03-04 23:43:33 -0500

    Define `Adjoined` as an ad hoc list-ish tree thing.

--- src/Data/Adjoined.hs
+++ src/Data/Adjoined.hs
@@ -2,7 +2,7 @@ module Data.Adjoined where
 
 import Control.Monad
 
-newtype Adjoined a = Adjoined { unAdjoined :: Maybe a }
+data Adjoined a = Separated a (Adjoined a) | Adjoined a | Empty
   deriving (Eq, Foldable, Functor, Show, Traversable)
 
 -- | A partial semigroup consists of a set and a binary operation which is associative but not necessarily closed.
@@ -12,16 +12,23 @@ class PartialSemigroup a where
   coalesce :: a -> a -> Maybe a
 
 instance Applicative Adjoined where
-  pure = Adjoined . pure
-  Adjoined f <*> Adjoined a = Adjoined $ f <*> a
+  pure = return
+  (<*>) = ap
 
 instance Monad Adjoined where
-  return = pure
-  Adjoined a >>= f = Adjoined $ a >>= unAdjoined . f
+  return = Adjoined
+  Empty >>= _ = Empty
+  Adjoined a >>= f = f a
+  Separated a b >>= f = f a >>= (`Separated` (b >>= f))
 
 instance PartialSemigroup a => Monoid (Adjoined a) where
-  mempty = Adjoined Nothing
-  mappend (Adjoined a) (Adjoined b) = Adjoined . join $ coalesce <$> a <*> b
+  mempty = Empty
+  mappend a b = case (a, b) of
+    (_, Empty) -> a
+    (Empty, _) -> b
+    (Adjoined a, Adjoined b) -> maybe (Separated a (Adjoined b)) Adjoined $ coalesce a b
+    (Adjoined a, Separated b c) -> maybe (Separated a (Separated b c)) (`Separated` c) $ coalesce a b
+    (Separated a b, c) -> Separated a (b `mappend` c)
 
 instance PartialSemigroup Bool where
   coalesce True = Just
