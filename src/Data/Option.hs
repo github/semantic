d6ec8d0d8c580c841f194c77ebf896f35f54250a
Rob Rix <rob.rix@github.com>
2016-03-04 20:06:32 -0500

    Generalize maybeUnionRanges to maybeConcat.

--- src/Data/Option.hs
+++ src/Data/Option.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Data.Option where
 
 newtype Option a = Option { getOption :: Maybe a }
 
 option :: b -> (a -> b) -> Option a -> b
 option b f = maybe b f . getOption
+
+maybeConcat :: (Foldable f, Monoid (Option a)) => f a -> Maybe a
+maybeConcat = getOption . foldMap (Option. Just)
