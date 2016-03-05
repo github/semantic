4b2c548d323453403f54bda15774ee5898fc54eb
Rob Rix <rob.rix@github.com>
2016-03-05 00:09:18 -0500

    Defined Adjoined as a binary tree-ish thing.

--- src/Data/Adjoined.hs
+++ src/Data/Adjoined.hs
@@ -2,7 +2,7 @@ module Data.Adjoined where
 
 import Control.Monad
 
-data Adjoined a = Separated a (Adjoined a) | Adjoined a | Empty
+data Adjoined a = Separated (Adjoined a) (Adjoined a) | Adjoined a | Empty
   deriving (Eq, Foldable, Functor, Show, Traversable)
 
 -- | A partial semigroup consists of a set and a binary operation which is associative but not necessarily closed.
@@ -19,15 +19,15 @@ instance Monad Adjoined where
   return = Adjoined
   Empty >>= _ = Empty
   Adjoined a >>= f = f a
-  Separated a b >>= f = f a >>= (`Separated` (b >>= f))
+  Separated a b >>= f = Separated (a >>= f) (b >>= f)
 
 instance PartialSemigroup a => Monoid (Adjoined a) where
   mempty = Empty
   mappend a b = case (a, b) of
     (_, Empty) -> a
     (Empty, _) -> b
-    (Adjoined a, Adjoined b) -> maybe (Separated a (Adjoined b)) Adjoined $ coalesce a b
-    (Adjoined a, Separated b c) -> maybe (Separated a (Separated b c)) (`Separated` c) $ coalesce a b
+    (Adjoined a, Adjoined b) -> maybe (Separated (Adjoined a) (Adjoined b)) Adjoined $ coalesce a b
+    (a, Separated b c) -> Separated (a `mappend` b) c
     (Separated a b, c) -> Separated a (b `mappend` c)
 
 instance PartialSemigroup Bool where
