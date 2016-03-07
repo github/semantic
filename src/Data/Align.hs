63782505afcd708d35bef3f9920cfd978180aaf3
Rob Rix <rob.rix@github.com>
2016-03-07 09:42:53 -0500

    Add an Align instance for lists.

--- src/Data/Align.hs
+++ src/Data/Align.hs
@@ -6,3 +6,9 @@ class Functor f => Align f where
   align a b = alignWith id a b
   alignWith :: (These a b -> c) -> f a -> f b -> f c
   alignWith f a b = f <$> align a b
+
+instance Align [] where
+  nil = []
+  alignWith f as [] = f . This <$> as
+  alignWith f [] bs = f . That <$> bs
+  alignWith f (a : as) (b : bs) = f (These a b) : alignWith f as bs
