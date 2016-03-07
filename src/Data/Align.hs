1dfcf8e783e198a6dd4d5028ac2ae10316218c26
Rob Rix <rob.rix@github.com>
2016-03-07 12:57:00 -0500

    Revert "Add an Align typeclass."
    This reverts commit b5e07c617dded12b6cca0e31664c0b12c679da11.

--- src/Data/Align.hs
+++ src/Data/Align.hs
@@ -1,8 +1,1 @@
 module Data.Align where
-
-class Functor f => Align f where
-  nil :: f a
-  align :: f a -> f b -> f (These a b)
-  align a b = alignWith id a b
-  alignWith :: (These a b -> c) -> f a -> f b -> f c
-  alignWith f a b = f <$> align a b
