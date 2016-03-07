25e7dbdbe4143169e581c38c6354237cd2570de7
Rob Rix <rob.rix@github.com>
2016-03-07 09:31:01 -0500

    These is an instance of Bifunctor.

--- src/Data/Bifunctor/These.hs
+++ src/Data/Bifunctor/These.hs
@@ -1,4 +1,14 @@
 module Data.Bifunctor.These where
 
+import Data.Bifunctor
+
 data These a b = This a | That b | These a b
   deriving (Eq, Show)
+
+
+-- Instances
+
+instance Bifunctor These where
+  bimap f _ (This a) = This (f a)
+  bimap _ g (That b) = That (g b)
+  bimap f g (These a b) = These (f a) (g b)
