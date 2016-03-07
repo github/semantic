cd0a65236aa2844f287463be8b5fc622c134c5a3
Rob Rix <rob.rix@github.com>
2016-03-07 12:56:44 -0500

    Revert "Eta-reduce `align`."
    This reverts commit be2bea951b9e94567537dc4d5c0d9aa7e8e08d4b.

--- src/Data/Align.hs
+++ src/Data/Align.hs
@@ -5,7 +5,7 @@ import Data.Bifunctor.These
 class Functor f => Align f where
   nil :: f a
   align :: f a -> f b -> f (These a b)
-  align = alignWith id
+  align a b = alignWith id a b
   alignWith :: (These a b -> c) -> f a -> f b -> f c
   alignWith f a b = f <$> align a b
 
