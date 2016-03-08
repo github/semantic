d14b04290bd24956ac1286df6fb268be32ed98cb
Rob Rix <rob.rix@github.com>
2016-03-07 19:34:34 -0500

    Remove adjoinRowsBy.

--- src/Row.hs
+++ src/Row.hs
@@ -19,28 +19,6 @@ unLeft = fst . unRow
 unRight :: Row a -> Line a
 unRight = snd . unRow
 
--- | Given functions that determine whether an item is open, add a row to a
--- | first open, non-empty item in a list of rows, or add it as a new row.
-adjoinRowsBy :: Both (MaybeOpen a) -> [Row a] -> Row a -> [Row a]
-adjoinRowsBy _ [] row = [row]
-
-adjoinRowsBy f rows (Row bothLines) | Both (Just _, Just _) <- openLineBy <$> f <*> Both.unzip (unRow <$> rows) = Both.zipWith makeRow $ both <*> bothLines
-  where both = adjoinLinesBy <$> f <*> Both.unzip (unRow <$> rows)
-
-adjoinRowsBy (Both (f, _)) rows (Row (Both (left', right'))) | Just _ <- openLineBy f $ unLeft <$> rows = case right' of
-  EmptyLine -> rest
-  _ -> makeRow EmptyLine right' : rest
-  where rest = Prelude.zipWith makeRow (lefts left') rights
-        (lefts, rights) = first (adjoinLinesBy f) . runBoth $ Both.unzip $ unRow <$> rows
-
-adjoinRowsBy (Both (_, g)) rows (Row (Both (left', right'))) | Just _ <- openLineBy g $ unRight <$> rows = case left' of
-  EmptyLine -> rest
-  _ -> makeRow left' EmptyLine : rest
-  where rest = Prelude.zipWith makeRow lefts (rights right')
-        (lefts, rights) = second (adjoinLinesBy g) . runBoth $ Both.unzip $ unRow <$> rows
-
-adjoinRowsBy _ rows row = row : rows
-
 isOpenRowBy :: Both (MaybeOpen a) -> Row a -> Bool
 isOpenRowBy f = runBothWith (&&) . (isOpenLineBy <$> f <*>) . unRow
 
