7a0b9483380119fdc98c304f0dcf4b6af4237e07
Rob Rix <rob.rix@github.com>
2016-03-10 10:00:29 -0500

    `coalesce` distributes through `Identity`.

--- src/Data/Coalescent.hs
+++ src/Data/Coalescent.hs
@@ -1,6 +1,11 @@
 module Data.Coalescent where
 
+import Data.Functor.Identity
+
 -- | The class of types which can optionally be coalesced together.
 class Coalescent a where
   -- | Returns either Just the combined value of its inputs, or Nothing if they cannot be combined.
   coalesce :: a -> a -> Maybe a
+
+instance Coalescent a => Coalescent (Identity a) where
+  a `coalesce` b = Identity <$> runIdentity a `coalesce` runIdentity b
