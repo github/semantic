d792ddcd57d0770a665dfc4d853b81073891e982
Rob Rix <rob.rix@github.com>
2016-03-10 13:14:29 -0500

    Define `these` for case analysis over `These`.

--- src/Data/Bifunctor/These.hs
+++ src/Data/Bifunctor/These.hs
@@ -5,6 +5,12 @@ import Data.Bifunctor
 data These a b = This a | That b | These a b
   deriving (Eq, Show)
 
+-- | Eliminate These by case analysis.
+these :: (a -> c) -> (b -> c) -> (a -> b -> c) -> These a b -> c
+these f _ _ (This this) = f this
+these _ f _ (That that) = f that
+these _ _ f (These this that) = f this that
+
 
 -- Instances
 
