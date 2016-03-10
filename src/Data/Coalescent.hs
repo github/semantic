7fd23f6f290c4d25c780b229c5e51203a8817f75
Rob Rix <rob.rix@github.com>
2016-03-10 09:44:43 -0500

    Document `coalesce`.

--- src/Data/Coalescent.hs
+++ src/Data/Coalescent.hs
@@ -2,4 +2,5 @@ module Data.Coalescent where
 
 -- | The class of types which can optionally be coalesced together.
 class Coalescent a where
+  -- | Returns either Just the combined value of its inputs, or Nothing if they cannot be combined.
   coalesce :: a -> a -> Maybe a
