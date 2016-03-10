bf74a6a6f621ef16acd884a919869a96244fe3e4
Rob Rix <rob.rix@github.com>
2016-03-10 09:51:29 -0500

    Add a Monoid instance for Adjoined over Coalescent types.

--- src/Data/Adjoined.hs
+++ src/Data/Adjoined.hs
@@ -1,6 +1,7 @@
 module Data.Adjoined where
 
 import Control.Monad
+import Data.Coalescent
 import Data.Sequence as Seq
 
 newtype Adjoined a = Adjoined { unAdjoined :: Seq a }
@@ -23,6 +24,11 @@ instance Monad Adjoined where
     (a :< as) -> Adjoined $ unAdjoined (f a) >< unAdjoined (Adjoined as >>= f)
 
 type Coalesce a = a -> a -> [a]
+
+instance Coalescent a => Monoid (Adjoined a) where
+  mempty = Adjoined mempty
+  Adjoined a `mappend` Adjoined b | as :> a' <- viewr a, b' :< bs <- viewl b, Just coalesced <- coalesce a' b' = Adjoined (as >< (coalesced <| bs))
+                                  | otherwise = Adjoined (a >< b)
 
 mappendBy :: Coalesce a -> Adjoined a -> Adjoined a -> Adjoined a
 mappendBy coalesce (Adjoined a) (Adjoined b) = case (viewr a, viewl b) of
