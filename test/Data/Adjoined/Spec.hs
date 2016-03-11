1815a74b6893a6eca4ff160673a76f77afeb455d
Rob Rix <rob.rix@github.com>
2016-03-11 09:48:55 -0500

    Add a test of the reflexivity of equality.

--- test/Data/Adjoined/Spec.hs
+++ test/Data/Adjoined/Spec.hs
@@ -2,8 +2,14 @@ module Data.Adjoined.Spec where
 
 import Data.Adjoined
 import Data.Coalescent
+import Test.Hspec
+import Test.Hspec.QuickCheck
 import Test.QuickCheck
 
+spec :: Spec
+spec = do
+  prop "equality is reflexive" $
+    \ a -> a `shouldBe` (a :: Adjoined (Separated Char))
 
 instance Arbitrary a => Arbitrary (Adjoined a) where
   arbitrary = fromList <$> arbitrary
