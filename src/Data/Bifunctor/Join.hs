601004d936030e9a35cc140e91a59b6f47017975
Rob Rix <rob.rix@github.com>
2016-03-14 20:44:04 -0400

    Revert "Add an Applicative instance for Join over These."
    This reverts commit abd121876c8d7cc02f29e249eb0290805f326c81.

--- src/Data/Bifunctor/Join.hs
+++ src/Data/Bifunctor/Join.hs
@@ -9,14 +9,5 @@ newtype Join p a = Join { runJoin :: p a a }
 instance Bifunctor p => Functor (Join p) where
   fmap f = Join . bimap f f . runJoin
 
-instance Applicative (Join These) where
-  pure a = Join $ These a a
-  Join a <*> Join b | Just first <- first, Just second <- second = Join $ These first second
-                    | Just first <- first = Join $ This first
-                    | Just second <- second = Join $ That second
-                    | otherwise = Join a <*> Join (swap b)
-    where first = maybeFirst a <*> maybeFirst b
-          second = maybeSecond a <*> maybeSecond b
-
 instance Foldable (Join These) where
   foldMap f = these id id mappend . bimap f f . runJoin
