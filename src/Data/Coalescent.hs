210d0e9492d8660b8a46686b1897944d09bb7cf3
Rob Rix <rob.rix@github.com>
2016-03-15 08:16:28 -0400

    Fix the docs for coalesce.

--- src/Data/Coalescent.hs
+++ src/Data/Coalescent.hs
@@ -5,7 +5,7 @@ import Data.Functor.Identity
 
 -- | The class of types which can optionally be coalesced together.
 class Coalescent a where
-  -- | Returns either Just the combined value of its inputs, or Nothing if they cannot be combined.
+  -- | Returns the result of coalescing the operands together in an Alternative context. If they cannot be coalesced, they should each be produced individually.
   coalesce :: Alternative f => a -> a -> f a
 
 instance Coalescent a => Coalescent (Identity a) where
