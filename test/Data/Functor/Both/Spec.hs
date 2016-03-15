2ea0fa9109b22338c7f67b1f8e687c3a832fa825
Rob Rix <rob.rix@github.com>
2016-03-15 10:54:28 -0400

    Add a (failing) test that Both coalesces asymmetrically at left.

--- test/Data/Functor/Both/Spec.hs
+++ test/Data/Functor/Both/Spec.hs
@@ -13,3 +13,6 @@ spec = do
 
     it "should not coalesce when neither side coalesces" $
       (pure (Closed [True]) `coalesce` pure (Line [True]) :: [Both (Line Bool)]) `shouldBe` [pure (Closed [True]), pure (Line [True])]
+
+    it "should coalesce asymmetrically at left" $
+      (both (Line [True]) (Closed [True]) `coalesce` pure (Line [True]) :: [Both (Line Bool)]) `shouldBe` [both (Line [True, True]) (Closed [True]), both (Line []) (Line [True])]
