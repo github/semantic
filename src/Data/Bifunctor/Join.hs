5ed6dac35528e55a4217ce370eac0d078332c57b
Rob Rix <rob.rix@github.com>
2016-03-14 20:44:20 -0400

    Revert "Forgot the constructor name."
    This reverts commit 8d508be2b62d3729791630800c02221ad8636c34.

--- src/Data/Bifunctor/Join.hs
+++ src/Data/Bifunctor/Join.hs
@@ -1,6 +1,6 @@
 module Data.Bifunctor.Join where
 
-newtype Join p a = Join { runJoin :: p a a }
+newtype Join p a = { runJoin :: p a a }
 
 instance Bifunctor p => Functor (Join p) where
   fmap f = Join . bimap f f . runJoin
