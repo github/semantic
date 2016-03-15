3a36d67245b4e573a898ec22a0f8aa42d06b3175
Rob Rix <rob.rix@github.com>
2016-03-14 20:10:28 -0400

    Join is Foldable over These.

--- src/Data/Bifunctor/Join.hs
+++ src/Data/Bifunctor/Join.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Data.Bifunctor.Join where
 
 import Data.Bifunctor
+import Data.Bifunctor.These
 
 newtype Join p a = Join { runJoin :: p a a }
 
 instance Bifunctor p => Functor (Join p) where
   fmap f = Join . bimap f f . runJoin
+
+instance Foldable (Join These) where
+  foldMap f = these id id mappend . bimap f f . runJoin
