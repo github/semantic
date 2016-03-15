01330d9d2ee3ff5b9a83de7609009a72132ebff3
Rob Rix <rob.rix@github.com>
2016-03-15 10:52:43 -0400

    Add a (failing) test that it doesn’t coalesce when neither side coalesces.

--- test/Data/Functor/Both/Spec.hs
+++ test/Data/Functor/Both/Spec.hs
@@ -10,3 +10,6 @@ spec = do
   describe "Coalescent" $ do
     it "should coalesce when both sides coalesce" $
       (pure (Line [True]) `coalesce` pure (Line [True]) :: [Both (Line Bool)]) `shouldBe` [pure (Line [True, True])]
+
+    it "should not coalesce when neither side coalesces" $
+      (pure (Closed [True]) `coalesce` pure (Line [True]) :: [Both (Line Bool)]) `shouldBe` [pure (Closed [True]), pure (Line [True])]
