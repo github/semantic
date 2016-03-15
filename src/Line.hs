970a3b24d34bdce23476a5403c601513d320d4e4
Rob Rix <rob.rix@github.com>
2016-03-15 15:08:26 -0400

    Revert "Explicitly preserve Closedness on both sides under mappend."
    This reverts commit bd4094153350b67d383edffd1f22fbd97de81774.

--- src/Line.hs
+++ src/Line.hs
@@ -54,8 +54,7 @@ instance Applicative Line where
 
 instance Monoid (Line a) where
   mempty = Line []
-  mappend xs ys | isOpen xs && isOpen ys = Line (unLine xs `mappend` unLine ys)
-                | otherwise = Closed (unLine xs `mappend` unLine ys)
+  mappend xs ys = lineMap (mappend (unLine xs)) ys
 
 instance Coalescent (Line a) where
   coalesce a b | isOpen a = pure (a `mappend` b)
