61df559751a0e5ba793b3ae06fbba8d06aa39dc0
Rob Rix <rob.rix@github.com>
2016-03-18 10:50:00 -0400

    Add an Aligned type.

--- src/Data/Aligned.hs
+++ src/Data/Aligned.hs
@@ -1,1 +1,9 @@
-module Data.Aligned where
+module Data.Aligned
+( Aligned (..)
+) where
+
+import Data.Bifunctor.Join
+import Data.Bifunctor.These
+
+-- | A functor over either or both sides of a list of computations.
+newtype Aligned f recur = Aligned { unAligned :: Join These [f recur] }
