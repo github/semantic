99292baa6ad46099defe489ee8fe7311486f1788
Rob Rix <rob.rix@github.com>
2016-03-18 11:22:17 -0400

    Add convenience constructors for Aligned.

--- src/Data/Aligned.hs
+++ src/Data/Aligned.hs
@@ -1,5 +1,8 @@
 module Data.Aligned
 ( Aligned (..)
+, alignThis
+, alignThat
+, alignThese
 ) where
 
 import Data.Bifunctor.Join
@@ -7,3 +10,12 @@ import Data.Bifunctor.These
 
 -- | A functor over either or both sides of a list of computations.
 newtype Aligned f recur = Aligned { unAligned :: Join These [f recur] }
+
+alignThis :: [f recur] -> Aligned f recur
+alignThis = Aligned . Join . This
+
+alignThat :: [f recur] -> Aligned f recur
+alignThat = Aligned . Join . That
+
+alignThese :: [f recur] -> [f recur] -> Aligned f recur
+alignThese = ((Aligned . Join) .) . These
