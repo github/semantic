b05b1694a7a9f1cfce6776a160ae26cd33b2b0c2
Rob Rix <rob.rix@github.com>
2016-03-24 11:45:52 -0400

    Get the first ranges for a child.

--- src/Alignment.hs
+++ src/Alignment.hs
@@ -136,6 +136,7 @@ groupChildrenByLine ranges children = go (fromThese [] [] $ runJoin ranges) chil
                            , (child:rest) <- children
                            = go ranges rest
                            | otherwise = uncurry (alignWith (fmap (flip (,) []) . Join)) ranges
+        firstRangesForChild child = getRange <$> head child
         getRange (Free (Annotated (Info range _) _)) = range
         getRange (Pure patch) | Info range _ :< _ <- getSplitTerm patch = range
 
