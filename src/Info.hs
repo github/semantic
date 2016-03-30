e8fbe8c78971a86ec83675c8abd0ea8d30f6eb44
Rob Rix <rob.rix@github.com>
2016-03-30 17:26:52 -0400

    Move Info into its own module.

--- src/Info.hs
+++ src/Info.hs
@@ -1,1 +1,13 @@
 module Info where
+
+import Category
+import Data.Set
+import Range
+
+-- | An annotation for a source file, including the source range and semantic
+-- | categories.
+data Info = Info { characterRange :: !Range, categories :: !(Set Category) }
+  deriving (Eq, Show)
+
+instance Categorizable Info where
+  categories = Info.categories
