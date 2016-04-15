3662b021df3f181f0c5b2f328f65c0156f7da024
Rob Rix <rob.rix@github.com>
2016-04-14 20:31:03 -0400

    Correct the test of insertion patch alignment.

--- test/AlignmentSpec.hs
+++ test/AlignmentSpec.hs
@@ -89,7 +89,7 @@ spec = parallel $ do
       alignDiff (both (Source.fromList "a") (Source.fromList "a\nb")) (both (info 0 1) (info 0 3) `branch` [ pure (info 0 1) `leaf` "a", Pure (Insert (info 2 3 :< Leaf "b")) ]) `shouldBe`
         [ Join (These (info 0 1 `branch` [ info 0 1 `leaf` "a" ])
                       (info 0 2 `branch` [ info 0 1 `leaf` "a" ]))
-        , Join (That (Pure (SplitInsert (info 2 3 :< Leaf "b"))))
+        , Join (That (info 2 3 `branch` [ Pure (SplitInsert (info 2 3 :< Leaf "b")) ]))
         ]
 
     where
