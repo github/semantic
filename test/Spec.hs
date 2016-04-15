1972e20233d4b533e0e63d72c05f7f95d8b7f526
Rob Rix <rob.rix@github.com>
2016-04-14 21:54:11 -0400

    :fire: Data.Functor.Both.Spec.

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,6 @@ module Main where
 import qualified AlignmentSpec
 import qualified CorpusSpec
 import qualified Data.Adjoined.Spec
-import qualified Data.Functor.Both.Spec
 import qualified InterpreterSpec
 import qualified OrderedMapSpec
 import qualified PatchOutputSpec
@@ -15,7 +14,6 @@ main = hspec $ parallel $ do
   describe "Alignment" AlignmentSpec.spec
   describe "Corpus" CorpusSpec.spec
   describe "Data.Adjoined" Data.Adjoined.Spec.spec
-  describe "Data.Functor.Both" Data.Functor.Both.Spec.spec
   describe "Interpreter" InterpreterSpec.spec
   describe "OrderedMap" OrderedMapSpec.spec
   describe "PatchOutput" PatchOutputSpec.spec
