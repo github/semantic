7c8354f8627d7b8ee5fba0a8fa754f2982fe6116
Rob Rix <rob.rix@github.com>
2016-04-15 11:57:26 -0400

    Revert "Hide Alt."
    This reverts commit a43afb89898b918c06834e92ade43113e74fd35f.

--- src/Alignment.hs
+++ src/Alignment.hs
@@ -19,7 +19,7 @@ import Data.Foldable
 import Data.Functor.Both as Both
 import Data.Functor.Identity
 import Data.Maybe
-import Data.Monoid hiding (Alt)
+import Data.Monoid
 import Data.These
 import Diff
 import Info
