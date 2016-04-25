33270bd65aaa9b2a42d700a5aa74cff089c0ec75
joshvera <josh@joshvera.com>
2016-04-25 11:46:10 -0400

    stub diffSummary

---
+++ src/DiffSummary.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module DiffSummary where
+
+import Diff
+import Info
+import Patch
+import Term
+import Syntax
+import Data.Monoid
+
+newtype DiffSummary = DiffSummary { diffChanges :: [DiffChange] }
+  deriving (Monoid)
+
+emptyDiffSummary :: DiffSummary
+emptyDiffSummary = DiffSummary { diffChanges = [] }
+
+data DiffChange = Insertion { category :: String, termName :: String }
+
+
+patchSummary :: (Term a Info -> DiffSummary) -> Patch (Term a Info) -> DiffSummary
+patchSummary termSummary patch = maybe emptyDiffSummary termSummary beforeTerm <>
+  maybe emptyDiffSummary termSummary afterTerm
+  where
+    beforeTerm = before patch
+    afterTerm = after patch
+
+diffSummary :: Diff (Patch a) Info -> DiffSummary
+diffSummary = foldMap (patchSummary termSummary)
+-- $ fmap (patchSummary termSummary) diff-- diffSum $ patchSummary diff
+-- (a -> b -> b) -> b -> t a -> b
+
+termSummary :: Term (Patch a) Info -> DiffSummary
+termSummary = cata summary where
+  summary _ (Leaf (Replace _ _)) = emptyDiffSummary
+  summary _ (Leaf (Insert _)) = emptyDiffSummary
+  summary _ (Leaf (Delete _)) = emptyDiffSummary
+  summary _ (Indexed _) = emptyDiffSummary
+  summary _ (Fixed _) = emptyDiffSummary
+  summary _ (Keyed _) = emptyDiffSummary
