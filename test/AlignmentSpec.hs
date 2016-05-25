3663cc612b07f19c86d697da77f206bcc4cb710e
Rob Rix <rob.rix@github.com>
2016-05-25 10:43:20 -0400

    Revert "Add an Arbitrary instance over BranchAlignment."
    This reverts commit 17e215b1aec6fdb80212133fe362a9c5120e0b8f.

--- test/AlignmentSpec.hs
+++ test/AlignmentSpec.hs
@@ -258,9 +258,6 @@ instance Arbitrary BranchElement where
           (prefix, suffix) = ('(' : key, ")" :: String)
   shrink (Margin contents) = Margin <$> traverse (shrinkList (const [])) contents
 
-instance Arbitrary BranchAlignment where
-  arbitrary = BranchAlignment <$> listOf1 arbitrary
-
 instance Arbitrary (PrettyDiff [(String, Range)]) where
   arbitrary = toPrettyDiff <$> listOf1 arbitrary
 
