f3804160f9c542520f7ac844eae9f9b4dd938f6c
Rob Rix <rob.rix@github.com>
2016-05-26 16:02:45 -0400

    Add traceShowId to Prologue.

--- src/Prologue.hs
+++ src/Prologue.hs
@@ -1,9 +1,17 @@
 module Prologue
 ( module X
 , lookup
+, traceShowId
 , FilePath
 ) where
 
 import Protolude as X
 import Data.List (lookup)
 import System.IO (FilePath)
+
+import qualified GHC.Show as P
+import qualified Debug.Trace as T
+
+{-# WARNING traceShowId "'traceShowId' remains in code" #-}
+traceShowId :: P.Show a => a -> a
+traceShowId a = T.trace (P.show a) a
