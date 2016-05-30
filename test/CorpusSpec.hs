93b36a852f1d3ea9f445657ecb81a96047e0cc04
Rob Rix <rob.rix@github.com>
2016-05-30 12:37:05 -0400

    Add a Show instance for Verbatim which emits the unquoted string.

--- test/CorpusSpec.hs
+++ test/CorpusSpec.hs
@@ -81,3 +81,6 @@ testDiff renderer paths diff matcher = do
         sourceBlobs sources = pure S.SourceBlob <*> sources <*> pure mempty <*> paths <*> pure (Just S.defaultPlainBlob)
 
 newtype Verbatim = Verbatim String
+
+instance Show Verbatim where
+  showsPrec _ (Verbatim string) = (string++)
