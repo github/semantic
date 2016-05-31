c93e0c65c03d9a2ef334f0ba00bce45cff7b928d
Rob Rix <rob.rix@github.com>
2016-05-31 10:40:25 -0400

    Apply the benchmark to its input.

--- bench/Main.hs
+++ bench/Main.hs
@@ -9,11 +9,11 @@ main :: IO ()
 main = defaultMain
   []
 
-generativeBenchmark :: (Arbitrary a, Show m) => String -> (a -> m) -> Benchmarkable -> IO Benchmark
+generativeBenchmark :: (Arbitrary a, Show m) => String -> (a -> m) -> (a -> Benchmarkable) -> IO Benchmark
 generativeBenchmark name metric benchmark = do
   benchmarks <- traverse measure [0..100]
   pure $! bgroup name benchmarks
   where measure n = do
           input <- generate (resize n arbitrary)
           let measurement = metric input
-          pure $! bench (show measurement) benchmark
+          pure $! bench (show measurement) (benchmark input)
