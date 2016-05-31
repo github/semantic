7a3cfd3425df339931ad63a17f6a80c6b7a21fd1
Rob Rix <rob.rix@github.com>
2016-05-31 10:37:34 -0400

    Add a function to produce generative benchmarks.

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,8 +1,19 @@
 module Main where
 
 import Criterion.Main
+import Data.String
 import Prologue
+import Test.QuickCheck
 
 main :: IO ()
 main = defaultMain
   []
+
+arbitraryBenchmark :: (Arbitrary a, Show m) => String -> (a -> m) -> Benchmarkable -> IO Benchmark
+arbitraryBenchmark name metric benchmark = do
+  benchmarks <- traverse measure [0..100]
+  pure $! bgroup name benchmarks
+  where measure n = do
+          input <- generate (resize n arbitrary)
+          let measurement = metric input
+          pure $! bench (show measurement) benchmark
