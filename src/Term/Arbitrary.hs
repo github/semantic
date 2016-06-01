6206f8ae93cbd9471a968893115cc570068e8334
Rob Rix <rob.rix@github.com>
2016-06-01 09:14:06 -0400

    Define ArbitraryTerm in Term.Arbitrary.

--- src/Term/Arbitrary.hs
+++ src/Term/Arbitrary.hs
@@ -1,1 +1,7 @@
 module Term.Arbitrary where
+
+import Prologue
+import Term
+
+newtype ArbitraryTerm leaf annotation = ArbitraryTerm { unArbitraryTerm :: TermF leaf annotation (ArbitraryTerm leaf annotation) }
+  deriving (Show, Eq, Generic)
