d9a13717b66cde329a160fc64275c05c62038219
Rob Rix <rob.rix@github.com>
2016-06-01 09:14:29 -0400

    Define toTerm in Term.Arbitrary.

--- src/Term/Arbitrary.hs
+++ src/Term/Arbitrary.hs
@@ -1,7 +1,11 @@
 module Term.Arbitrary where
 
+import Data.Functor.Foldable (unfold)
 import Prologue
 import Term
 
 newtype ArbitraryTerm leaf annotation = ArbitraryTerm { unArbitraryTerm :: TermF leaf annotation (ArbitraryTerm leaf annotation) }
   deriving (Show, Eq, Generic)
+
+toTerm :: ArbitraryTerm leaf annotation -> Term leaf annotation
+toTerm = unfold unArbitraryTerm
