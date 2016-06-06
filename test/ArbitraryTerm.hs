1109fdf32cfe4f7502b599a105f1a96da5600579
Rob Rix <rob.rix@github.com>
2016-06-06 11:51:30 -0400

    :fire: redundant imports.

--- test/ArbitraryTerm.hs
+++ test/ArbitraryTerm.hs
@@ -1,14 +1,10 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module ArbitraryTerm where
 
-import Category
 import Data.Text.Arbitrary ()
 import Data.These.Arbitrary ()
-import Info
 import Prologue hiding (fst, snd)
-import Range
 import Source hiding ((++))
-import Syntax
 import Test.QuickCheck hiding (Fixed)
 
 data CategorySet = A | B | C | D deriving (Eq, Show)
