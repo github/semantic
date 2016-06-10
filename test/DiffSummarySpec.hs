c229d439343c245dcaf627653da5ee06de1a44d0
joshvera <josh@joshvera.com>
2016-06-10 16:42:11 -0400

    Fix DiffSummary tests
    now that parentAnnotations are Categories

--- test/DiffSummarySpec.hs
+++ test/DiffSummarySpec.hs
@@ -1,7 +1,6 @@
 module DiffSummarySpec where
 
 import Prologue
-import Data.String
 import Test.Hspec
 import Diff
 import Info
@@ -17,22 +16,22 @@ arrayInfo = Info (rangeAt 0) ArrayLiteral 2 0
 literalInfo :: Info
 literalInfo = Info (rangeAt 1) StringLiteral 1 0
 
-testDiff :: Diff String Info
+testDiff :: Diff Text Info
 testDiff = free $ Free (pure arrayInfo :< Indexed [ free $ Pure (Insert (cofree $ literalInfo :< Leaf "a")) ])
 
 testSummary :: DiffSummary DiffInfo
-testSummary = DiffSummary { patch = Insert (DiffInfo "string" (Just "a")), parentAnnotations = [] }
+testSummary = DiffSummary { patch = Insert (DiffInfo "string" "a"), parentAnnotations = [] }
 
 replacementSummary :: DiffSummary DiffInfo
-replacementSummary = DiffSummary { patch = Replace (DiffInfo "string" (Just "a")) (DiffInfo "symbol" (Just "b")), parentAnnotations = [ (DiffInfo "array" (Just "switch {}")) ] }
+replacementSummary = DiffSummary { patch = Replace (DiffInfo "string" "a") (DiffInfo "symbol" "b"), parentAnnotations = [ ArrayLiteral ] }
 
 spec :: Spec
 spec = parallel $ do
   describe "diffSummary" $ do
     it "outputs a diff summary" $ do
-      diffSummary testDiff `shouldBe` [ DiffSummary { patch = Insert (DiffInfo "string" (Just "a")), parentAnnotations = [ DiffInfo "array" Nothing ] } ]
+      diffSummary testDiff `shouldBe` [ DiffSummary { patch = Insert (DiffInfo "string" "a"), parentAnnotations = [ ArrayLiteral ] } ]
   describe "show" $ do
     it "should print adds" $
-      show testSummary `shouldBe` ("Added the 'a' string" :: String)
+      show testSummary `shouldBe` ("Added the 'a' string" :: Text)
     it "prints a replacement" $ do
-      show replacementSummary `shouldBe` ("Replaced the 'a' string with the 'b' symbol in the array context" :: String)
+      show replacementSummary `shouldBe` ("Replaced the 'a' string with the 'b' symbol in the array context" :: Text)
