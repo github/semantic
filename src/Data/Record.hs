3b8c1546a5d9e2efd352a56764fe64384ac63b5d
Rob Rix <rob.rix@github.com>
2016-06-16 11:52:14 -0400

    Add HasField instances for hlists.

--- src/Data/Record.hs
+++ src/Data/Record.hs
@@ -23,3 +23,12 @@ type a :=> b = Tagged a b
 -- | This has type a :=> b. When you require a to be some concrete type (and you usually will), it should be provided by context, whether using ascription, a type signature for the binding, `asTypeOf`, or some other way to allow the specific type to be inferred.
 field :: b -> a :=> b
 field = Tagged
+
+
+-- Instances
+
+instance {-# OVERLAPPABLE #-} HasField fields field => HasField (notIt ': fields) field where
+  getField (RCons _ t) = getField t
+
+instance {-# OVERLAPPABLE #-} HasField (field ': fields) field where
+  getField (RCons h _) = h
