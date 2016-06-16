d4b63443ab0591cbfd739f062c36e119c743a880
Rob Rix <rob.rix@github.com>
2016-06-16 11:38:29 -0400

    Add a simple phantom type tagger.

--- src/Data/Record.hs
+++ src/Data/Record.hs
@@ -1,1 +1,12 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, TypeOperators #-}
 module Data.Record where
+
+import Prologue
+
+infix 9 :=>
+
+newtype a :=> b = (:=>) b
+  deriving (Eq, Show)
+
+field :: b -> a :=> b
+field = (:=>)
