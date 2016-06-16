db035a80094f2494e60a06ef1126f8802c1737e1
Rob Rix <rob.rix@github.com>
2016-06-16 11:45:53 -0400

    :memo:

--- src/Data/Record.hs
+++ src/Data/Record.hs
@@ -6,8 +6,12 @@ import Prologue
 
 infix 9 :=>
 
+-- | A phantom type tag constructor.
 type a :=> b = Tagged a b
 
+-- | Smart constructor for type-tagged data fields.
+-- |
+-- | This has type a :=> b. When you require a to be some concrete type (and you usually will), it should be provided by context, whether using ascription, a type signature for the binding, `asTypeOf`, or some other way to allow the specific type to be inferred.
 field :: b -> a :=> b
 field = Tagged
 
