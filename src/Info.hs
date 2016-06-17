9ccd3d2d772b5a7d05106c7502f80542e38edd4b
Rob Rix <rob.rix@github.com>
2016-06-17 10:26:20 -0400

    Implement Data.Record-based annotations.

--- src/Info.hs
+++ src/Info.hs
@@ -1,8 +1,31 @@
+{-# LANGUAGE DataKinds, FlexibleContexts, TypeOperators #-}
 module Info where
 
+import Data.Record
 import Prologue
 import Category
 import Range
+
+newtype RangeA = RangeA { unRangeA :: Range }
+newtype CategoryA = CategoryA { unCategoryA :: Category }
+newtype SizeA = SizeA { unSizeA :: Integer }
+newtype CostA = CostA { unCostA :: Integer }
+
+type InfoFields = '[ RangeA, CategoryA, SizeA, CostA ]
+
+type Info' = Record InfoFields
+
+characterRange' :: HasField fields RangeA => Record fields -> Range
+characterRange' = unRangeA . getField
+
+category' :: HasField fields CategoryA => Record fields -> Category
+category' = unCategoryA . getField
+
+size' :: HasField fields SizeA => Record fields -> Integer
+size' = unSizeA . getField
+
+cost' :: HasField fields CostA => Record fields -> Integer
+cost' = unCostA . getField
 
 -- | An annotation for a source file, including the source range and semantic
 -- | categories.
