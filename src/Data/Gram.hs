0e887866660c4b0a8bc53bc8e8c31a8e3b8695f6
Rob Rix <rob.rix@github.com>
2016-06-21 16:58:46 -0400

    Add a Monoid instance for Bags.

--- src/Data/Gram.hs
+++ src/Data/Gram.hs
@@ -8,3 +8,7 @@ serialize :: Gram label -> [label]
 serialize gram = stem gram <> base gram
 
 newtype Bag a = Bag { unBag :: [a] -> [a] }
+
+instance Monoid (Bag a) where
+  mempty = Bag (const [])
+  mappend = (Bag .) . ((.) `on` unBag)
