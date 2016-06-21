87b9c1c8e2b8f0588898e1d3e58dae05e0c6409c
Rob Rix <rob.rix@github.com>
2016-06-21 17:29:09 -0400

    Bags are Hashable.

--- src/Data/Gram.hs
+++ src/Data/Gram.hs
@@ -1,5 +1,6 @@
 module Data.Gram where
 
+import Data.Hashable
 import Prologue
 
 data Gram label = Gram { stem :: [label], base :: [label] }
@@ -8,6 +9,10 @@ serialize :: Gram label -> [label]
 serialize gram = stem gram <> base gram
 
 newtype Bag a = Bag { unBag :: [a] -> [a] }
+
+instance Hashable a => Hashable (Bag a) where
+  hashWithSalt _ = hash
+  hash = hash . ($ mempty) . unBag
 
 instance Monoid (Bag a) where
   mempty = Bag (const [])
