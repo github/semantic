53b53a7c8fe71bfd835181b417db4b73fb4a893d
Rob Rix <rob.rix@github.com>
2016-06-21 16:58:33 -0400

    Serialize Grams to a flat list of labels.

--- src/Data/Gram.hs
+++ src/Data/Gram.hs
@@ -1,3 +1,8 @@
 module Data.Gram where
 
+import Prologue
+
 data Gram label = Gram { stem :: [label], base :: [label] }
+
+serialize :: Gram label -> [label]
+serialize gram = stem gram <> base gram
