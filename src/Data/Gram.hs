226be486c80d71b5dac30bb24c42781f397bb803
Rob Rix <rob.rix@github.com>
2016-06-21 18:48:44 -0400

    Add a Random instance over Ratios.

--- src/Data/Gram.hs
+++ src/Data/Gram.hs
@@ -1,6 +1,7 @@
 module Data.Gram where
 
 import Data.DList
+import Control.Monad.Random
 import Data.Hashable
 import Prologue
 
@@ -15,3 +16,6 @@ type Bag = DList
 instance Hashable label => Hashable (Gram label) where
   hashWithSalt _ = hash
   hash = hash . serialize
+
+instance (Random a, Integral a) => Random (Ratio a) where
+  random = first (% 1) . random
