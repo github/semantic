0bef12d7222fa27bc38e1e79f93eba7d985d58e2
Rob Rix <rob.rix@github.com>
2016-06-22 11:04:27 -0400

    Test that featureVector produces d-vectors.

--- test/Data/Gram/Spec.hs
+++ test/Data/Gram/Spec.hs
@@ -1,9 +1,18 @@
 module Data.Gram.Spec where
 
+import Data.DList as DList
+import Data.Gram
+import Data.Gram.Arbitrary ()
+import Data.String
 import Prologue
 import Test.Hspec
+import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec = parallel $ do
   describe "pqGrams" $
     it "exists" pending
+
+  describe "featureVector" $ do
+    prop "produces a vector of the specified dimension" $
+      \ grams d -> length (featureVector (fromList (grams :: [Gram String])) d) `shouldBe` d
