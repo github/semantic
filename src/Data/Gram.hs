dac5431c1b197d142c7862371a43ee6b3294f418
Rob Rix <rob.rix@github.com>
2016-06-22 15:17:39 -0400

    Fold instead of unpacking children.

--- src/Data/Gram.hs
+++ src/Data/Gram.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes, ScopedTypeVariables #-}
 module Data.Gram where
 
 import Control.Monad.Random
@@ -16,13 +16,13 @@ data Gram label = Gram { stem :: [Maybe label], base :: [Maybe label] }
 serialize :: Gram label -> [Maybe label]
 serialize gram = stem gram <> base gram
 
-pqGrams :: forall label tree. Foldable.Foldable tree => Int -> Int -> (forall a. Base tree a -> (label, [a])) -> tree -> Bag (Gram label)
+pqGrams :: forall label tree. (Foldable.Foldable tree, Prologue.Foldable (Base tree)) => Int -> Int -> (forall a. Base tree a -> (label, [a])) -> tree -> Bag (Gram label)
 pqGrams p q unpack = cata merge . foldr (\ p rest -> assignParent Nothing p . rest) identity [0..p] . hylo go project
   where go :: Base tree (Cofree (Base tree) (Bag (Gram label))) -> Cofree (Base tree) (Bag (Gram label))
         go functor = let (label, children) = unpack functor in
           cofree (DList.singleton (Gram [] [ Just label ]) :< (assignParent (Just label) p <$> functor))
         merge :: CofreeF (Base tree) (Bag (Gram label)) (Bag (Gram label)) -> Bag (Gram label)
-        merge (head :< tail) = let (label, children) = unpack tail in head <> foldr (<>) mempty children
+        merge (head :< tail) = let (label, children) = unpack tail in head <> Prologue.fold tail
           -- DList.singleton (Gram [] [ Just label ]) : (children >>= assignParent (Just label) p)
         assignParent :: Maybe label -> Int -> Cofree (Base tree) (Bag (Gram label)) -> Cofree (Base tree) (Bag (Gram label))
         assignParent parentLabel n tree
