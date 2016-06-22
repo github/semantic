ad380bf08d5ec09370641720181d754d94d55588
Rob Rix <rob.rix@github.com>
2016-06-22 15:02:10 -0400

    Limit the p,q-gram test to p > 0 & q > 0.

--- test/Data/Gram/Spec.hs
+++ test/Data/Gram/Spec.hs
@@ -21,8 +21,8 @@ spec = parallel $ do
           Indexed c -> c
           Fixed c -> c
           Keyed c -> toList c in
-    prop "produces grams with stems of the specified length" $ forAll (arbitrary `suchThat` ((> 0) . fst . snd)) $
-      \ (term, (p, q)) -> pqGrams p q (headF &&& getChildren) (toTerm term :: Term String String) `shouldSatisfy` all ((== p) . length . stem)
+    prop "produces grams with stems of the specified length" $ forAll (arbitrary `suchThat` (\ (_, p, q) -> p > 0 && q > 0)) $
+      \ (term, p, q) -> pqGrams p q (headF &&& getChildren) (toTerm term :: Term String String) `shouldSatisfy` all ((== p) . length . stem)
 
   describe "featureVector" $ do
     prop "produces a vector of the specified dimension" $ forAll (arbitrary `suchThat` ((> 0) . snd)) $
