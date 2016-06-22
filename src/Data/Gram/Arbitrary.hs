b85e2f27ce06f2496232b6adf5c0b8b1cd695828
Rob Rix <rob.rix@github.com>
2016-06-22 10:53:50 -0400

    Add an Arbitrary instance over Grams.

--- src/Data/Gram/Arbitrary.hs
+++ src/Data/Gram/Arbitrary.hs
@@ -1,2 +1,9 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Data.Gram.Arbitrary where
 
+import Data.Gram
+import Prologue
+import Test.QuickCheck
+
+instance Arbitrary label => Arbitrary (Gram label) where
+  arbitrary = Gram <$> arbitrary <*> arbitrary
