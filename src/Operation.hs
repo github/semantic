108decbcaa8f6339e366875a6ab4b4be92aedf1f
Rob Rix <rob.rix@github.com>
2016-06-22 17:48:56 -0400

    Reformatting.

--- src/Operation.hs
+++ src/Operation.hs
@@ -10,9 +10,8 @@ data Operation
   a -- ^ The type of leaves in the syntax tree, typically String, but possibly some datatype representing different leaves more precisely.
   annotation -- ^ The type of annotations.
   f -- ^ The type representing another level of the diffing algorithm. Often Algorithm.
-  =
   -- | Recursively diff two terms and pass the result to the continuation.
-  Recursive (Term a annotation) (Term a annotation) (Diff a annotation -> f)
+  = Recursive (Term a annotation) (Term a annotation) (Diff a annotation -> f)
   -- | Diff two dictionaries and pass the result to the continuation.
   | ByKey (OrderedMap Text (Term a annotation)) (OrderedMap Text (Term a annotation)) (OrderedMap Text (Diff a annotation) -> f)
   -- | Diff two arrays and pass the result to the continuation.
