656d8318ad7060407570531f668e6037c6c273da
Rob Rix <rob.rix@github.com>
2016-06-23 09:45:41 -0400

    Add an Arbitrary instance over Data.Record.

--- src/Data/Record/Arbitrary.hs
+++ src/Data/Record/Arbitrary.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Data.Record.Arbitrary where
+
+import Data.Record
+import Prologue
+import Test.QuickCheck
+
+instance Arbitrary fields => Arbitrary (Record '[fields]) where
+  arbitrary = RCons <$> arbitrary <*> pure RNil
