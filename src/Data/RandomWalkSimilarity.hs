2faf708435bb0867f8b51bd6869570923c229dbb
Rob Rix <rob.rix@github.com>
2016-06-23 09:37:43 -0400

    Extract the root base assignment into a setRootBase function.

--- src/Data/RandomWalkSimilarity.hs
+++ src/Data/RandomWalkSimilarity.hs
@@ -29,7 +29,7 @@ serialize :: Gram label -> [Maybe label]
 serialize gram = stem gram <> base gram
 
 pqGrams :: Int -> Int -> Cofree (Syntax leaf) label -> Bag (Gram label)
-pqGrams p q = cata merge . (\ t -> let (a :< f) = runCofree t in cofree (setBase a (base a) :< f)) . foldr (\ p rest -> assignParent Nothing p . rest) identity [0..p] . hylo go project
+pqGrams p q = cata merge . setRootBase . foldr (\ p rest -> assignParent Nothing p . rest) identity [0..p] . hylo go project
   where go (label :< functor) = cofree (Gram [] [ Just label ] :< (assignParent (Just label) p <$> functor))
         merge (head :< tail) = DList.singleton head <> Prologue.fold tail
         assignParent parentLabel n tree
@@ -44,6 +44,7 @@ pqGrams p q = cata merge . (\ t -> let (a :< f) = runCofree t in cofree (setBase
         setBases child siblings rest = let (gram :< further) = (runCofree child) in cofree (setBase gram (siblings >>= base . extract) :< further) : rest
         setBasesKV (key, child) siblings rest = let (gram :< further) = (runCofree child) in (key, cofree (setBase gram (siblings >>= base . extract . snd) :< further)) : rest
         setBase gram newBase = gram { base = take q (newBase <> repeat Nothing) }
+        setRootBase term = let (a :< f) = runCofree term in cofree (setBase a (base a) :< f)
 
 windowed :: Int -> (a -> [a] -> b -> b) -> b -> [a] -> b
 windowed n f seed = para alg
