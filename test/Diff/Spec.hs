1dd10050f6fc4d3ae6a0e2728fe288ac6911fb81
Rob Rix <rob.rix@github.com>
2016-06-27 13:18:20 -0400

    Move some tests around.

--- test/Diff/Spec.hs
+++ test/Diff/Spec.hs
@@ -16,18 +16,17 @@ import Test.QuickCheck
 
 spec :: Spec
 spec = parallel $ do
+  prop "equality is reflexive" $
+    \ a b -> let diff = diffTerms (free . Free) (==) diffCost (toTerm a) (toTerm (b :: ArbitraryTerm Text (Record '[Category]))) in
+      diff == diff
+
+  prop "equal terms produce identity diffs" $
+    \ a -> let term = toTerm (a :: ArbitraryTerm Text (Record '[Category])) in
+      diffCost (diffTerms (free . Free) (==) diffCost term term) == 0
+
   describe "mergeMaybe" $ do
     it "is symmetrical" $ pending
 
   describe "ArbitraryDiff" $ do
     prop "generates diffs of a specific size" . forAll ((arbitrary >>= \ n -> (,) n <$> diffOfSize n) `suchThat` ((> 0) . fst)) $
       \ (n, diff) -> arbitraryDiffSize (diff :: ArbitraryDiff Text ()) `shouldBe` n
-
-  describe "Diff" $ do
-    prop "equality is reflexive" $
-      \ a b -> let diff = diffTerms (free . Free) (==) diffCost (toTerm a) (toTerm (b :: ArbitraryTerm Text (Record '[Category]))) in
-        diff == diff
-
-    prop "equal terms produce identity diffs" $
-      \ a -> let term = toTerm (a :: ArbitraryTerm Text (Record '[Category])) in
-        diffCost (diffTerms (free . Free) (==) diffCost term term) == 0
