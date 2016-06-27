346bec0d0b9e554db03d980f137b0447eca40ed9
Rob Rix <rob.rix@github.com>
2016-06-27 13:16:56 -0400

    Clean up redundant imports and use Text, not String.

--- test/TermSpec.hs
+++ test/TermSpec.hs
@@ -1,14 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 module TermSpec where
 
-import Category
-import Data.String
 import Data.Text.Arbitrary ()
-import Data.Record
-import Data.Record.Arbitrary ()
-import Diff
-import Diff.Arbitrary
-import Interpreter
 import Prologue
 import Term.Arbitrary
 import Test.Hspec
@@ -19,8 +12,8 @@ spec :: Spec
 spec = parallel $ do
   describe "Term" $ do
     prop "equality is reflexive" $
-      \ a -> toTerm a == toTerm (a :: ArbitraryTerm String ())
+      \ a -> toTerm a == toTerm (a :: ArbitraryTerm Text ())
 
   describe "ArbitraryTerm" $ do
-    prop "generates terms of a specific size" $ forAll ((arbitrary >>= \ n -> (,) n <$> termOfSize n) `suchThat` ((> 0) . fst)) $
-      \ (n, term) -> arbitraryTermSize (term :: ArbitraryTerm String ()) `shouldBe` n
+    prop "generates terms of a specific size" . forAll ((arbitrary >>= \ n -> (,) n <$> termOfSize n) `suchThat` ((> 0) . fst)) $
+      \ (n, term) -> arbitraryTermSize (term :: ArbitraryTerm Text ()) `shouldBe` n
