8fe92c025cd9e12ae418420fb076086d8c1479dc
Rob Rix <rob.rix@github.com>
2016-06-27 13:08:12 -0400

    Pare the unicode replacement test down further.

--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -3,19 +3,16 @@ module InterpreterSpec where
 import Prologue
 import Diff
 import Data.Record
-import qualified Interpreter as I
-import Range
+import Interpreter
 import Syntax
 import Patch
-import Info
 import Category
 import Test.Hspec
 
 spec :: Spec
-spec = parallel $
+spec = parallel $ do
   describe "interpret" $ do
     it "returns a replacement when comparing two unicode equivalent terms" $
-      let infoA = Range 0 2 .: StringLiteral .: 0 .: 0 .: RNil
-          infoB = Range 0 1 .: StringLiteral .: 0 .: 0 .: RNil in
-          I.diffTerms (free . Free) ((==) `on` extract) diffCost (cofree (infoA :< Leaf "t\776")) (cofree (infoB :< Leaf "\7831")) `shouldBe`
-          free (Pure (Replace (cofree (infoA :< Leaf "t\776")) (cofree (infoB :< Leaf "\7831"))))
+      let termA = cofree $ (StringLiteral .: RNil) :< Leaf "t\776"
+          termB = cofree $ (StringLiteral .: RNil) :< Leaf "\7831" in
+          diffTerms (free . Free) ((==) `on` extract) diffCost termA termB `shouldBe` free (Pure (Replace termA termB))
