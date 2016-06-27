833802eb8a03b4adc3d86d5cae4f91419a77ad83
Rob Rix <rob.rix@github.com>
2016-06-27 15:32:09 -0400

    Text, not String.

--- test/Data/RandomWalkSimilarity/Spec.hs
+++ test/Data/RandomWalkSimilarity/Spec.hs
@@ -4,7 +4,6 @@ module Data.RandomWalkSimilarity.Spec where
 import Data.DList as DList hiding (toList)
 import Data.RandomWalkSimilarity
 import Data.RandomWalkSimilarity.Arbitrary ()
-import Data.String
 import Prologue
 import Term
 import Term.Arbitrary
@@ -16,11 +15,11 @@ spec :: Spec
 spec = parallel $ do
   describe "pqGrams" $ do
     prop "produces grams with stems of the specified length" . forAll (arbitrary `suchThat` (\ (_, p, q) -> p > 0 && q > 0)) $
-      \ (term, p, q) -> pqGrams p q identity (toTerm term :: Term String String) `shouldSatisfy` all ((== p) . length . stem)
+      \ (term, p, q) -> pqGrams p q identity (toTerm term :: Term Text Text) `shouldSatisfy` all ((== p) . length . stem)
 
     prop "produces grams with bases of the specified length" . forAll (arbitrary `suchThat` (\ (_, p, q) -> p > 0 && q > 0)) $
-      \ (term, p, q) -> pqGrams p q identity (toTerm term :: Term String String) `shouldSatisfy` all ((== q) . length . base)
+      \ (term, p, q) -> pqGrams p q identity (toTerm term :: Term Text Text) `shouldSatisfy` all ((== q) . length . base)
 
   describe "featureVector" $ do
     prop "produces a vector of the specified dimension" . forAll (arbitrary `suchThat` ((> 0) . snd)) $
-      \ (grams, d) -> length (featureVector d (fromList (grams :: [Gram String]))) `shouldBe` d
+      \ (grams, d) -> length (featureVector d (fromList (grams :: [Gram Text]))) `shouldBe` d
