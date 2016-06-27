85f39fd5a826ec9bc3bb28e6a35229cc021844b5
Rob Rix <rob.rix@github.com>
2016-06-27 13:15:03 -0400

    Move some tests out of TermSpec into Diff.Spec.

--- test/Diff/Spec.hs
+++ test/Diff/Spec.hs
@@ -1,10 +1,33 @@
+{-# LANGUAGE DataKinds #-}
 module Diff.Spec where
 
+import Category
+import Data.Record
+import Data.Record.Arbitrary ()
+import Data.String
 import Diff
+import Diff.Arbitrary
+import Interpreter
 import Prologue
+import Term.Arbitrary
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 spec :: Spec
 spec = parallel $ do
   describe "mergeMaybe" $ do
     it "is symmetrical" $ pending
+
+  describe "ArbitraryDiff" $ do
+    prop "generates diffs of a specific size" . forAll ((arbitrary >>= \ n -> (,) n <$> diffOfSize n) `suchThat` ((> 0) . fst)) $
+      \ (n, diff) -> arbitraryDiffSize (diff :: ArbitraryDiff String ()) `shouldBe` n
+
+  describe "Diff" $ do
+    prop "equality is reflexive" $
+      \ a b -> let diff = diffTerms (free . Free) (==) diffCost (toTerm a) (toTerm (b :: ArbitraryTerm String (Record '[Category]))) in
+        diff == diff
+
+    prop "equal terms produce identity diffs" $
+      \ a -> let term = toTerm (a :: ArbitraryTerm String (Record '[Category])) in
+        diffCost (diffTerms (free . Free) (==) diffCost term term) == 0
--- test/TermSpec.hs
+++ test/TermSpec.hs
@@ -24,16 +24,3 @@ spec = parallel $ do
   describe "ArbitraryTerm" $ do
     prop "generates terms of a specific size" $ forAll ((arbitrary >>= \ n -> (,) n <$> termOfSize n) `suchThat` ((> 0) . fst)) $
       \ (n, term) -> arbitraryTermSize (term :: ArbitraryTerm String ()) `shouldBe` n
-
-  describe "ArbitraryDiff" $ do
-    prop "generates diffs of a specific size" $ forAll ((arbitrary >>= \ n -> (,) n <$> diffOfSize n) `suchThat` ((> 0) . fst)) $
-      \ (n, diff) -> arbitraryDiffSize (diff :: ArbitraryDiff String ()) `shouldBe` n
-
-  describe "Diff" $ do
-    prop "equality is reflexive" $
-      \ a b -> let diff = diffTerms (free . Free) (==) diffCost (toTerm a) (toTerm (b :: ArbitraryTerm String (Record '[Category]))) in
-        diff == diff
-
-    prop "equal terms produce identity diffs" $
-      \ a -> let term = toTerm (a :: ArbitraryTerm String (Record '[Category])) in
-        diffCost (diffTerms (free . Free) (==) diffCost term term) == 0
