7bdbb0a963a117d4e3febb9737cff23fb73ef65d
Rob Rix <rob.rix@github.com>
2016-06-29 11:23:11 -0400

    Derive an Ord instance for Syntax.

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -17,4 +17,4 @@ data Syntax
   | Fixed [f]
   -- | A branch of child nodes indexed by some String identity. This is useful for identifying e.g. methods & properties in a class scope by their names. Note that comments can generally occur in these scopes as well; one strategy for dealing with this is to identify comments by their text in the source.
   | Keyed (OrderedMap T.Text f)
-  deriving (Functor, Show, Eq, Foldable, Traversable)
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
