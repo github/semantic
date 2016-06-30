e3a43b77a9c1c49fe7b0dd1da0e17b1a714700ec
Rob Rix <rob.rix@github.com>
2016-06-30 15:02:28 -0400

    Fix overloaded string warning.

--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -16,7 +16,7 @@ spec :: Spec
 spec = parallel $ do
   describe "interpret" $ do
     it "returns a replacement when comparing two unicode equivalent terms" $
-      let termA = cofree $ (StringLiteral .: RNil) :< Leaf "t\776"
+      let termA = cofree $ (StringLiteral .: RNil) :< Leaf ("t\776" :: Text)
           termB = cofree $ (StringLiteral .: RNil) :< Leaf "\7831" in
           diffTerms (free . Free) ((==) `on` extract) diffCost termA termB `shouldBe` free (Pure (Replace termA termB))
 
