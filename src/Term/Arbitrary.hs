72a909ac526f9d0d2bee5b2d9116b01b24f7c570
Rob Rix <rob.rix@github.com>
2016-06-30 14:01:54 -0400

    ArbitraryTerm does not wrap CofreeF.

--- src/Term/Arbitrary.hs
+++ src/Term/Arbitrary.hs
@@ -10,14 +10,17 @@ import Syntax
 import Term
 import Test.QuickCheck hiding (Fixed)
 
-newtype ArbitraryTerm leaf annotation = ArbitraryTerm { unArbitraryTerm :: TermF leaf annotation (ArbitraryTerm leaf annotation) }
+data ArbitraryTerm leaf annotation = ArbitraryTerm annotation (Syntax leaf (ArbitraryTerm leaf annotation))
   deriving (Show, Eq, Generic)
+
+unArbitraryTerm :: ArbitraryTerm leaf annotation -> TermF leaf annotation (ArbitraryTerm leaf annotation)
+unArbitraryTerm (ArbitraryTerm a s) = a :< s
 
 toTerm :: ArbitraryTerm leaf annotation -> Term leaf annotation
 toTerm = unfold unArbitraryTerm
 
 termOfSize :: (Arbitrary leaf, Arbitrary annotation) => Int -> Gen (ArbitraryTerm leaf annotation)
-termOfSize n = (ArbitraryTerm .) . (:<) <$> arbitrary <*> syntaxOfSize n
+termOfSize n = ArbitraryTerm <$> arbitrary <*> syntaxOfSize n
   where syntaxOfSize n | n <= 1 = oneof $ (Leaf <$> arbitrary) : branchGeneratorsOfSize n
                        | otherwise = oneof $ branchGeneratorsOfSize n
         branchGeneratorsOfSize n =
@@ -38,16 +41,16 @@ arbitraryTermSize = cata (succ . sum) . toTerm
 
 -- Instances
 
-type instance Base (ArbitraryTerm leaf annotation) = CofreeF (Syntax leaf) annotation
-instance Unfoldable (ArbitraryTerm leaf annotation) where embed = ArbitraryTerm
+type instance Base (ArbitraryTerm leaf annotation) = TermF leaf annotation
+instance Unfoldable (ArbitraryTerm leaf annotation) where embed (a :< s) = ArbitraryTerm a s
 
 instance (Eq leaf, Eq annotation, Arbitrary leaf, Arbitrary annotation) => Arbitrary (ArbitraryTerm leaf annotation) where
   arbitrary = sized $ \ n -> do
     m <- choose (0, n)
     termOfSize m
 
-  shrink term@(ArbitraryTerm (annotation :< syntax)) = (subterms term ++) $ filter (/= term) $
-    (ArbitraryTerm .) . (:<) <$> shrink annotation <*> case syntax of
+  shrink term@(ArbitraryTerm annotation syntax) = (subterms term ++) . filter (/= term) $
+    ArbitraryTerm <$> shrink annotation <*> case syntax of
       Leaf a -> Leaf <$> shrink a
       Indexed i -> Indexed <$> (List.subsequences i >>= recursivelyShrink)
       Fixed f -> Fixed <$> (List.subsequences f >>= recursivelyShrink)
