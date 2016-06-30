3cd2a4b82c877d69e5030e789ccdb0592c3b6d07
Rob Rix <rob.rix@github.com>
2016-06-30 14:24:59 -0400

    Extract a function to select the category.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -76,4 +76,5 @@ run construct comparable cost algorithm = case runFree algorithm of
 
   Free (ByIndex a b f) -> run construct comparable cost . f $ ses (constructAndRun construct comparable cost) cost a b
 
-  Free (ByRandomWalkSimilarity a b f) -> run construct comparable cost . f $ rws (constructAndRun construct comparable cost) category a b
+  Free (ByRandomWalkSimilarity a b f) -> run construct comparable cost . f $ rws (constructAndRun construct comparable cost) getLabel a b
+    where getLabel = category
