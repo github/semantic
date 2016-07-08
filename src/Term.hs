847465c1b522a6962cbb54f19ea181fab5597f3b
joshvera <josh@joshvera.com>
2016-07-08 15:48:12 -0400

    Add more remaining symmetric cases to zipUnwrap

--- src/Term.hs
+++ src/Term.hs
@@ -26,25 +26,28 @@ zipTerms t1 t2 = annotate (zipUnwrap a b)
     annotate = fmap (cofree . (both annotation1 annotation2 :<))
     zipUnwrap (Leaf _) (Leaf b') = Just $ Leaf b'
     zipUnwrap (Comment _) (Comment b) = Just $ Comment b
-    zipUnwrap (Indexed a') (Indexed b') = Just . Indexed . catMaybes $ zipWith zipTerms a' b'
-    zipUnwrap (FunctionCall idA' a') (FunctionCall idB' b') = case (zipTerms idA' idB') of
-      (Just id') ->  Just $ FunctionCall id' (catMaybes $ zipWith zipTerms a' b')
-      (_) -> Nothing
+    zipUnwrap (Indexed a') (Indexed b') = Indexed <$> zipWithM zipTerms a' b'
+    zipUnwrap (Fixed a') (Fixed b') = Fixed <$> zipWithM zipTerms a' b'
+    zipUnwrap (Keyed a') (Keyed b') | keys a' == keys b' = Just . Keyed . fromList . catMaybes $ zipUnwrapMaps a' b' <$> keys a'
+    zipUnwrap (FunctionCall idA' a') (FunctionCall idB' b') = FunctionCall <$> zipTerms idA' idB' <*> zipWithM zipTerms a' b'
     zipUnwrap (Function idA' paramsA' exprsA') (Function idB' paramsB' exprsB') = case (zipTerms exprsA' exprsB') of
       Just exprs' ->  Just (Function (join $ liftA2 zipTerms idA' idB') (join $ liftA2 zipTerms paramsA' paramsB') exprs')
       _ -> Nothing
-    zipUnwrap (Case eA' bodyA') (Case eB' bodyB') = case (zipTerms eA' eB', zipTerms bodyA' bodyB') of
-      (Just id', Just body') -> Just $ Case id' body'
-      _ -> Nothing
-    zipUnwrap (Switch a' as') (Switch b' bs') = case (zipTerms a' b') of
-      (Just expr') -> Just $ Switch expr' (catMaybes $ zipWith zipTerms as' bs')
-      _ -> Nothing
-    zipUnwrap (Object as') (Object bs') | as' == bs' = Just . Object . catMaybes $ zipWith zipTerms as' bs'
+    zipUnwrap (Case eA' bodyA') (Case eB' bodyB') = Case <$> zipTerms eA' eB' <*> zipTerms bodyA' bodyB'
+    zipUnwrap (Switch a' as') (Switch b' bs') = Switch <$> (zipTerms a' b') <*> zipWithM zipTerms as' bs'
+    zipUnwrap (MethodCall iA mA paramsA) (MethodCall iB mB paramsB) = MethodCall <$> zipTerms iA iB <*> zipTerms mA mB <*> zipTerms paramsA paramsB
+    zipUnwrap (Args a1) (Args b1) = Args <$> (zipWithM zipTerms a1 b1)
+    zipUnwrap (MemberAccess a1 a2) (MemberAccess b1 b2) = MemberAccess <$> zipTerms a1 b1 <*> zipTerms a2 b2
+    zipUnwrap (MathAssignment a1 a2) (MathAssignment b1 b2) = MathAssignment <$> zipTerms a1 b1 <*> zipTerms a2 b2
+    zipUnwrap (Assignment a1 a2) (Assignment b1 b2) = Assignment <$> zipTerms a1 b1 <*> zipTerms a2 b2
+    zipUnwrap (Ternary a1 a2) (Ternary b1 b2) = Ternary <$> zipTerms a1 b1 <*> (zipWithM zipTerms a2 b2)
+    zipUnwrap (Object as') (Object bs') | as' == bs' = Object <$> zipWithM zipTerms as' bs'
+    zipUnwrap (Operator as) (Operator bs) = Operator <$> zipWithM zipTerms as bs
+    zipUnwrap (VarDecl a) (VarDecl b) = VarDecl <$> zipTerms a b
+    zipUnwrap (VarAssignment a1 a2) (VarAssignment b1 b2) = VarAssignment <$> zipTerms a1 b1 <*> zipTerms a2 b2
+    zipUnwrap (SubscriptAccess a1 a2) (SubscriptAccess b1 b2) = SubscriptAccess <$> zipTerms a1 b1 <*> zipTerms a2 b2
     zipUnwrap (Pair a1' a2') (Pair b1' b2') = Pair <$> zipTerms a1' b1' <*> zipTerms a2' b2'
-    zipUnwrap (Commented cs1 a) (Commented cs2 b) = Commented (catMaybes $ zipWith zipTerms cs1 cs2) <$> (zipTerms <$> a <*> b)
-    zipUnwrap (Fixed a') (Fixed b') = Just . Fixed . catMaybes $ zipWith zipTerms a' b'
-    zipUnwrap (Keyed a') (Keyed b') | keys a' == keys b' = Just . Keyed . fromList . catMaybes $ zipUnwrapMaps a' b' <$> keys a'
-    zipUnwrap _ _ = Nothing
+    zipUnwrap (Commented cs1 a) (Commented cs2 b) = Commented <$> zipWithM zipTerms cs1 cs2 <*> (zipTerms <$> a <*> b)
     zipUnwrapMaps a' b' key = (,) key <$> zipTerms (a' ! key) (b' ! key)
 
 -- | Return the node count of a term.
