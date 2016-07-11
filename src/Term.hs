b1b9806e6193fab02917db48f3c12a64b5ab8875
Rob Rix <rob.rix@github.com>
2016-07-11 14:41:59 -0400

    :memo: alignCofreeWith’s parameters.

--- src/Term.hs
+++ src/Term.hs
@@ -37,7 +37,12 @@ termSize = cata size where
   size (_ :< syntax) = 1 + sum syntax
 
 -- | Aligns (zips, retaining non-overlapping portions of the structure) a pair of terms.
-alignCofreeWith :: Functor f => (forall a b. f a -> f b -> Maybe (f (These a b))) -> (These (Cofree f a) (Cofree f b) -> contrasted) -> (a -> b -> combined) -> These (Cofree f a) (Cofree f b) -> Free (CofreeF f combined) contrasted
+alignCofreeWith :: Functor f
+  => (forall a b. f a -> f b -> Maybe (f (These a b))) -- ^ A function comparing a pair of structures, returning `Just` the combined structure if they are comparable (e.g. if they have the same constructor), and `Nothing` otherwise. The 'Data.Align.Generic.galign' function is usually what you want here.
+  -> (These (Cofree f a) (Cofree f b) -> contrasted) -- ^ A function mapping a 'These' of incomparable terms into 'Pure' values in the resulting tree.
+  -> (a -> b -> combined) -- ^ A function mapping the input terms’ annotations into annotations in the 'Free' values in the resulting tree.
+  -> These (Cofree f a) (Cofree f b) -- ^ The input terms.
+  -> Free (CofreeF f combined) contrasted
 alignCofreeWith compare contrast combine = go
   where go terms = fromMaybe (pure (contrast terms)) $ case terms of
           These t1 t2 -> wrap . (combine (extract t1) (extract t2) :<) . fmap go <$> compare (unwrap t1) (unwrap t2)
