ce500a822fdaa0687f83bb657764aaf178530e89
Rob Rix <rob.rix@github.com>
2016-07-11 15:13:16 -0400

    Define zipTerms using galign.

--- src/Term.hs
+++ src/Term.hs
@@ -3,9 +3,9 @@
 module Term where
 
 import Prologue
+import Data.Align.Generic
 import Data.Functor.Foldable as Foldable
 import Data.Functor.Both
-import Data.OrderedMap hiding (size)
 import Data.These
 import Syntax
 
@@ -20,16 +20,8 @@ instance Functor f => Foldable.Unfoldable (Cofree f a) where embed = cofree
 -- | Zip two terms by combining their annotations into a pair of annotations.
 -- | If the structure of the two terms don't match, then Nothing will be returned.
 zipTerms :: Term a annotation -> Term a annotation -> Maybe (Term a (Both annotation))
-zipTerms t1 t2 = annotate (zipUnwrap a b)
-  where
-    (annotation1 :< a, annotation2 :< b) = (runCofree t1, runCofree t2)
-    annotate = fmap (cofree . (both annotation1 annotation2 :<))
-    zipUnwrap (Leaf _) (Leaf b') = Just $ Leaf b'
-    zipUnwrap (Indexed a') (Indexed b') = Just . Indexed . catMaybes $ zipWith zipTerms a' b'
-    zipUnwrap (Fixed a') (Fixed b') = Just . Fixed . catMaybes $ zipWith zipTerms a' b'
-    zipUnwrap (Keyed a') (Keyed b') | keys a' == keys b' = Just . Keyed . fromList . catMaybes $ zipUnwrapMaps a' b' <$> keys a'
-    zipUnwrap _ _ = Nothing
-    zipUnwrapMaps a' b' key = (,) key <$> zipTerms (a' ! key) (b' ! key)
+zipTerms t1 t2 = iter go (alignCofreeWith galign (const Nothing) both (These t1 t2))
+  where go (a :< s) = cofree . (a :<) <$> sequenceA s
 
 -- | Return the node count of a term.
 termSize :: Term a annotation -> Integer
