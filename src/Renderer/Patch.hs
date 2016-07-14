4d6ab97691cd338be7f43110a05138b031962548
Rob Rix <rob.rix@github.com>
2016-07-14 15:53:52 -0400

    Placate hlint.

--- src/Renderer/Patch.hs
+++ src/Renderer/Patch.hs
@@ -17,19 +17,19 @@ import Info
 import Patch
 import Prologue hiding (fst, snd)
 import Renderer
-import Source hiding ((++), break)
+import Source hiding ((<>), break)
 import SplitDiff
 
 -- | Render a timed out file as a truncated diff.
 truncatePatch :: DiffArguments -> Both SourceBlob -> Text
-truncatePatch _ blobs = pack $ header blobs ++ "#timed_out\nTruncating diff: timeout reached.\n"
+truncatePatch _ blobs = pack $ header blobs <> "#timed_out\nTruncating diff: timeout reached.\n"
 
 -- | Render a diff in the traditional patch format.
 patch :: Renderer
 patch diff blobs = pack $ case getLast (foldMap (Last . Just) string) of
-  Just c | c /= '\n' -> string ++ "\n\\ No newline at end of file\n"
+  Just c | c /= '\n' -> string <> "\n\\ No newline at end of file\n"
   _ -> string
-  where string = header blobs ++ mconcat (showHunk blobs <$> hunks diff blobs)
+  where string = header blobs <> mconcat (showHunk blobs <$> hunks diff blobs)
 
 -- | A hunk in a patch, including the offset, changes, and context.
 data Hunk a = Hunk { offset :: Both (Sum Int), changes :: [Change a], trailingContext :: [Join These a] }
@@ -53,20 +53,20 @@ rowIncrement = Join . fromThese (Sum 0) (Sum 0) . runJoin . (Sum 1 <$)
 
 -- | Given the before and after sources, render a hunk to a string.
 showHunk :: Both SourceBlob -> Hunk (SplitDiff a Info) -> String
-showHunk blobs hunk = maybeOffsetHeader ++
-  concat (showChange sources <$> changes hunk) ++
+showHunk blobs hunk = maybeOffsetHeader <>
+  concat (showChange sources <$> changes hunk) <>
   showLines (snd sources) ' ' (maybeSnd . runJoin <$> trailingContext hunk)
   where sources = source <$> blobs
         maybeOffsetHeader = if lengthA > 0 && lengthB > 0
                             then offsetHeader
                             else mempty
-        offsetHeader = "@@ -" ++ offsetA ++ "," ++ show lengthA ++ " +" ++ offsetB ++ "," ++ show lengthB ++ " @@" ++ "\n"
+        offsetHeader = "@@ -" <> offsetA <> "," <> show lengthA <> " +" <> offsetB <> "," <> show lengthB <> " @@" <> "\n"
         (lengthA, lengthB) = runJoin . fmap getSum $ hunkLength hunk
         (offsetA, offsetB) = runJoin . fmap (show . getSum) $ offset hunk
 
 -- | Given the before and after sources, render a change to a string.
 showChange :: Both (Source Char) -> Change (SplitDiff a Info) -> String
-showChange sources change = showLines (snd sources) ' ' (maybeSnd . runJoin <$> context change) ++ deleted ++ inserted
+showChange sources change = showLines (snd sources) ' ' (maybeSnd . runJoin <$> context change) <> deleted <> inserted
   where (deleted, inserted) = runJoin $ pure showLines <*> sources <*> both '-' '+' <*> Join (unzip (fromThese Nothing Nothing . runJoin . fmap Just <$> contents change))
 
 -- | Given a source, render a set of lines to a string with a prefix.
@@ -82,25 +82,25 @@ showLine source line | Just line <- line = Just . toString . (`slice` source) $ 
 
 -- | Returns the header given two source blobs and a hunk.
 header :: Both SourceBlob -> String
-header blobs = intercalate "\n" [filepathHeader, fileModeHeader, beforeFilepath, afterFilepath] ++ "\n"
-  where filepathHeader = "diff --git a/" ++ pathA ++ " b/" ++ pathB
+header blobs = intercalate "\n" [filepathHeader, fileModeHeader, beforeFilepath, afterFilepath] <> "\n"
+  where filepathHeader = "diff --git a/" <> pathA <> " b/" <> pathB
         fileModeHeader = case (modeA, modeB) of
-          (Nothing, Just mode) -> intercalate "\n" [ "new file mode " ++ modeToDigits mode, blobOidHeader ]
-          (Just mode, Nothing) -> intercalate "\n" [ "deleted file mode " ++ modeToDigits mode, blobOidHeader ]
-          (Just mode, Just other) | mode == other -> "index " ++ oidA ++ ".." ++ oidB ++ " " ++ modeToDigits mode
+          (Nothing, Just mode) -> intercalate "\n" [ "new file mode " <> modeToDigits mode, blobOidHeader ]
+          (Just mode, Nothing) -> intercalate "\n" [ "deleted file mode " <> modeToDigits mode, blobOidHeader ]
+          (Just mode, Just other) | mode == other -> "index " <> oidA <> ".." <> oidB <> " " <> modeToDigits mode
           (Just mode1, Just mode2) -> intercalate "\n" [
-            "old mode " ++ modeToDigits mode1,
-            "new mode " ++ modeToDigits mode2,
+            "old mode " <> modeToDigits mode1,
+            "new mode " <> modeToDigits mode2,
             blobOidHeader
             ]
           (Nothing, Nothing) -> ""
-        blobOidHeader = "index " ++ oidA ++ ".." ++ oidB
+        blobOidHeader = "index " <> oidA <> ".." <> oidB
         modeHeader :: String -> Maybe SourceKind -> String -> String
         modeHeader ty maybeMode path = case maybeMode of
-           Just _ -> ty ++ "/" ++ path
+           Just _ -> ty <> "/" <> path
            Nothing -> "/dev/null"
-        beforeFilepath = "--- " ++ modeHeader "a" modeA pathA
-        afterFilepath = "+++ " ++ modeHeader "b" modeB pathB
+        beforeFilepath = "--- " <> modeHeader "a" modeA pathA
+        afterFilepath = "+++ " <> modeHeader "b" modeB pathB
         (pathA, pathB) = runJoin $ path <$> blobs
         (oidA, oidB) = runJoin $ oid <$> blobs
         (modeA, modeB) = runJoin $ blobKind <$> blobs
