81279c910e37ecd2f57a2dc32910a73726379b92
Rob Rix <rob.rix@github.com>
2016-07-15 11:05:39 -0400

    Split out the attributes into separate functions.

--- src/Renderer/Split.hs
+++ src/Renderer/Split.hs
@@ -120,7 +120,9 @@ instance (HasField fields Category, HasField fields Cost, HasField fields Range)
 instance (HasField fields Category, HasField fields Cost, HasField fields Range) => ToMarkup (Renderable (SplitDiff leaf (Record fields))) where
   toMarkup (Renderable source diff) = Prologue.fst . iter (\ t -> (toMarkup $ Renderable source t, characterRange (headF t))) $ toMarkupAndRange <$> diff
     where toMarkupAndRange patch = let term@(info :< _) = runCofree $ getSplitTerm patch in
-            ((div ! A.class_ (splitPatchToClassName patch) ! A.data_ (stringValue (show (unCost (cost info))))) . toMarkup $ Renderable source (cofree term), characterRange info)
+            ((div ! patchAttribute patch ! costAttribute info) . toMarkup $ Renderable source (cofree term), characterRange info)
+          patchAttribute patch = A.class_ (splitPatchToClassName patch)
+          costAttribute info = A.data_ (stringValue (show (unCost (cost info))))
 
 instance ToMarkup a => ToMarkup (Cell a) where
   toMarkup (Cell hasChanges num line) =
