a71f9f05cc676c5767a51ff6f0eb1076c941898e
Rob Rix <rob.rix@github.com>
2016-07-21 17:21:13 -0400

    Simplify the PatchOutputSpec annotations.
    Also applies some HLint advice.

--- test/PatchOutputSpec.hs
+++ test/PatchOutputSpec.hs
@@ -7,11 +7,10 @@ import Range
 import Renderer.Patch
 import Source
 import Syntax
-import Category
 import Test.Hspec
 
 spec :: Spec
-spec = parallel $
-  describe "hunks" $
+spec = parallel $ do
+  describe "hunks" $ do
     it "empty diffs have empty hunks" $
-        hunks (free . Free $ pure (Range 0 0 .: StringLiteral .: 1 .: 0 .: RNil) :< Leaf "") (both (SourceBlob (fromList "") "abcde" "path2.txt" (Just defaultPlainBlob)) (SourceBlob (fromList "") "xyz" "path2.txt" (Just defaultPlainBlob))) `shouldBe` [Hunk {offset = pure 0, changes = [], trailingContext = []}]
+        hunks (wrap $ pure (Range 0 0 .: RNil) :< Leaf ("" :: Text)) (both (SourceBlob (fromList "") "abcde" "path2.txt" (Just defaultPlainBlob)) (SourceBlob (fromList "") "xyz" "path2.txt" (Just defaultPlainBlob))) `shouldBe` [Hunk {offset = pure 0, changes = [], trailingContext = []}]
