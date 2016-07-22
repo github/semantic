7203942df202f170c2100dbb6c58aae82e16396a
Rob Rix <rob.rix@github.com>
2016-07-22 15:36:17 -0400

    Implement GSequenceable over unit constructors.

--- src/Data/Sequenceable/Generic.hs
+++ src/Data/Sequenceable/Generic.hs
@@ -1,6 +1,15 @@
 module Data.Sequenceable.Generic where
 
+import GHC.Generics
 import Prologue
+
+-- Classes
 
 class GSequenceable t where
   gsequenceAlt :: Alternative f => t (f a) -> f (t a)
+
+
+-- Instances
+
+instance GSequenceable U1 where
+  gsequenceAlt _ = pure U1
