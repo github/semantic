afd67dbbd9030e5fedd50f587da7018f1701ba3d
Rob Rix <rob.rix@github.com>
2016-07-22 17:51:57 -0400

    Define a merge method in Mergeable which generalizes sequenceAlt.

--- src/Data/Mergeable.hs
+++ src/Data/Mergeable.hs
@@ -8,6 +8,10 @@ import Prologue
 -- Classes
 
 class Functor t => Mergeable t where
+  merge :: Alternative f => (a -> f b) -> t a -> f (t b)
+  default merge :: (Generic1 t, GMergeable (Rep1 t), Alternative f) => (a -> f b) -> t a -> f (t b)
+  merge = genericMerge
+
   sequenceAlt :: Alternative f => t (f a) -> f (t a)
   default sequenceAlt :: (Generic1 t, GMergeable (Rep1 t), Alternative f) => t (f a) -> f (t a)
   sequenceAlt = genericSequenceAlt
