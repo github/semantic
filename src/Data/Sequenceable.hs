a9829e67cd3b7b2a46cba861809c4c146e171cef
Rob Rix <rob.rix@github.com>
2016-07-22 15:29:12 -0400

    Add a Sequenceable instance over [].

--- src/Data/Sequenceable.hs
+++ src/Data/Sequenceable.hs
@@ -2,5 +2,14 @@ module Data.Sequenceable where
 
 import Prologue
 
+-- Classes
+
 class Sequenceable t where
   sequenceAlt :: Alternative f => t (f a) -> f (t a)
+
+
+-- Instances
+
+instance Sequenceable [] where
+  sequenceAlt (x:xs) = ((:) <$> x <|> pure identity) <*> sequenceAlt xs
+  sequenceAlt [] = pure []
