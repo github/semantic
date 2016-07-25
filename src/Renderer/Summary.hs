de1088f4903a4959422c6a6297e8c8f6681cf6ad
joshvera <josh@joshvera.com>
2016-07-25 14:56:43 -0400

    Remove redundant import

--- src/Renderer/Summary.hs
+++ src/Renderer/Summary.hs
@@ -7,7 +7,6 @@ import Data.Aeson
 import Data.Record
 import Range
 import DiffSummary
-import Text.PrettyPrint.Leijen.Text (pretty)
 
 summary :: (HasField fields Category, HasField fields Range) => Renderer (Record fields)
 summary diff _ = toS . encode $ annotatedTexts
