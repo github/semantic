fa3b7d126ecfb30de4c4188d8fbb830051d4ddf9
Rob Rix <rob.rix@github.com>
2016-07-26 04:04:24 -0400

    Factor the sequenceAlt law properties into a separate function.

--- test/Data/Mergeable/Spec.hs
+++ test/Data/Mergeable/Spec.hs
@@ -8,6 +8,10 @@ import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec = do
+  sequenceAltLaws
+
+sequenceAltLaws :: Spec
+sequenceAltLaws = do
   describe "sequenceAlt" $ do
     prop "identity" $
       \ a -> sequenceAlt (fmap Just a) `shouldBe` Just (a :: [Char])
