93c53ed90ab1fa78beda34867225d01e80decba5
Rob Rix <rob.rix@github.com>
2016-07-26 04:02:52 -0400

    Stub in a property test for the proposed identity law.

--- test/Data/Mergeable/Spec.hs
+++ test/Data/Mergeable/Spec.hs
@@ -1,7 +1,13 @@
 module Data.Mergeable.Spec where
 
+import Data.Functor.Identity
+import Data.Mergeable
 import Prologue
 import Test.Hspec
+import Test.Hspec.QuickCheck
 
 spec :: Spec
-spec = pure ()
+spec = do
+  describe "sequenceAlt" $ do
+    prop "identity" $
+      \ a -> sequenceAlt (fmap Just a) `shouldBe` Just (a :: [Char])
