be973b5baeb6228b9e7cc720eeeccd9593f89848
Rob Rix <rob.rix@github.com>
2016-07-26 13:11:52 -0400

    Generalize out a helper function to validate laws against alternative instances.

--- test/Data/Mergeable/Spec.hs
+++ test/Data/Mergeable/Spec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
 module Data.Mergeable.Spec where
 
 import Data.Functor.Identity
@@ -12,23 +12,23 @@ import Test.QuickCheck
 spec :: Spec
 spec = parallel $ do
   describe "[]" $ do
-    sequenceAltLaws (arbitrary :: Gen [Char])
-    mergeLaws (arbitrary :: Gen [Char]) (arbitrary :: Gen (Blind (Char -> Maybe Char)))
-  describe "Maybe" $ sequenceAltLaws (arbitrary :: Gen (Maybe Char))
-  describe "Identity" $ sequenceAltLaws (Identity <$> arbitrary :: Gen (Identity Char))
-  describe "Syntax" $ sequenceAltLaws (sized (syntaxOfSize (const arbitrary)) :: Gen (Syntax Char Char))
+    withAlternativeInstances sequenceAltLaws (arbitrary :: Gen [Char])
+    withAlternativeInstances mergeLaws (arbitrary :: Gen [Char])
+  describe "Maybe" $ withAlternativeInstances sequenceAltLaws (arbitrary :: Gen (Maybe Char))
+  describe "Identity" $ withAlternativeInstances sequenceAltLaws (Identity <$> arbitrary :: Gen (Identity Char))
+  describe "Syntax" $ withAlternativeInstances sequenceAltLaws (sized (syntaxOfSize (const arbitrary)) :: Gen (Syntax Char Char))
 
-mergeLaws :: (Mergeable f, Alternative g, Eq (g (f a)), Show (f a), Show (g (f a))) => Gen (f a) -> Gen (Blind (a -> g a)) -> Spec
+mergeLaws :: forall f g a. (Mergeable f, Alternative g, Eq (g (f a)), Show (f a), Show (g (f a))) => Gen (f a) -> Gen (Blind (a -> g a)) -> Spec
 mergeLaws value function = describe "merge" $ do
   prop "relationship with sequenceAlt" . forAll ((,) <$> value <*> function) $
     \ (a, f) -> merge (getBlind f) a `shouldBe` sequenceAlt (fmap (getBlind f) a)
 
-sequenceAltLaws :: forall f a. (Arbitrary a, CoArbitrary a, Mergeable f, Eq (f a), Show (f a)) => Gen (f a) -> Spec
-sequenceAltLaws gen = do
-  describe "Maybe" $ sequenceAltLaws' gen (arbitrary :: Gen (Blind (a -> Maybe a)))
+withAlternativeInstances :: forall f a. (Arbitrary a, CoArbitrary a, Mergeable f, Eq (f a), Show (f a)) => (forall g. (Alternative g, Eq (g (f a)), Show (g (f a))) => Gen (f a) -> Gen (Blind (a -> g a)) -> Spec) -> Gen (f a) -> Spec
+withAlternativeInstances laws gen = do
+  describe "Maybe" $ laws gen (arbitrary :: Gen (Blind (a -> Maybe a)))
 
-sequenceAltLaws' :: forall f g a. (Arbitrary a, CoArbitrary a, Mergeable f, Alternative g, Eq (f a), Eq (g (f a)), Show (f a), Show (g (f a))) => Gen (f a) -> Gen (Blind (a -> g a)) -> Spec
-sequenceAltLaws' value function = do
+sequenceAltLaws :: forall f g a. (Arbitrary a, CoArbitrary a, Mergeable f, Alternative g, Eq (f a), Eq (g (f a)), Show (f a), Show (g (f a))) => Gen (f a) -> Gen (Blind (a -> g a)) -> Spec
+sequenceAltLaws value function = do
   describe "sequenceAlt" $ do
     prop "identity" . forAll value $
       \ a -> sequenceAlt (pure <$> a) `shouldBe` (pure a :: g (f a))
