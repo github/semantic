7f4c97a9a0fdd07e74f8a57f6b8844dfb71ba58e
Rob Rix <rob.rix@github.com>
2016-07-26 23:32:48 -0400

    Add an example to the docs for `Mergeable`.

--- src/Data/Mergeable.hs
+++ src/Data/Mergeable.hs
@@ -11,6 +11,12 @@ import Prologue
 -- | A 'Mergeable' functor is one which supports pushing itself through an 'Alternative' functor. Note the similarities with 'Traversable' & 'Crosswalk'.
 --
 -- This is a kind of distributive law which produces (at least) the union of the two functors’ shapes; i.e. unlike 'Traversable', an 'empty' value in the inner functor does not produce an 'empty' result, and unlike 'Crosswalk', an 'empty' value in the outer functor does not produce an 'empty' result.
+--
+-- For example, we can use 'merge' to select one side or the other of a diff node in 'Syntax', while correctly handling the fact that some patches don’t have any content for that side:
+--
+-- @
+-- let before = iter (\ (a :< s) -> sequenceAlt (cofree . (fst a :<)) syntax) . fmap (maybeFst . unPatch)
+-- @
 class Functor t => Mergeable t where
   -- | Merge a functor by mapping its elements into an 'Alternative' functor, combining them, and pushing the 'Mergeable' functor inside.
   merge :: Alternative f => (a -> f b) -> t a -> f (t b)
