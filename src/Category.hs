ef78210447f649eb674fd2db874219d7838adac9
joshvera <josh@joshvera.com>
2016-07-29 16:28:27 -0400

    Add a Class category

--- src/Category.hs
+++ src/Category.hs
@@ -77,6 +77,8 @@ data Category
   | Object
   -- | A return statement.
   | Return
+  -- | A class declaration.
+  | Class
   -- | A non-standard category, which can be used for comparability.
   | Other Text
   deriving (Eq, Generic, Ord, Show)
