c8b5fb8c08b2ace5426dca333c0805b8894076e7
joshvera <josh@joshvera.com>
2016-07-30 14:00:47 -0400

    Add Method to toTermName

--- src/DiffSummary.hs
+++ src/DiffSummary.hs
@@ -69,6 +69,7 @@ toTermName source term = case unwrap term of
   S.While expr _ -> toTermName' expr
   S.DoWhile _ expr -> toTermName' expr
   S.Class identifier _ _ -> toTermName' identifier
+  S.Method identifier _ _ -> toTermName' identifier
   Comment a -> toCategoryName a
   where toTermName' = toTermName source
 
