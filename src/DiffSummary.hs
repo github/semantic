001407df2b1cff73e827a80528bf822ee2037834
joshvera <josh@joshvera.com>
2016-07-30 14:26:13 -0400

    Add Constructor to HasCategory Category instance

--- src/DiffSummary.hs
+++ src/DiffSummary.hs
@@ -115,6 +115,7 @@ instance HasCategory Category where
     C.DoWhile -> "do/while statement"
     C.Object -> "object"
     C.Return -> "return statement"
+    C.Constructor -> "constructor"
 
 instance (HasCategory leaf, HasField fields Category) => HasCategory (Term leaf (Record fields)) where
   toCategoryName = toCategoryName . category . extract
