fab4a3c829a550a84705cdf0541cd4754244c8a4
Rob Rix <rob.rix@github.com>
2016-08-02 13:48:15 -0400

    Build the label for the current node.

--- src/Diffing.hs
+++ src/Diffing.hs
@@ -129,7 +129,7 @@ termCostDecorator :: (Prologue.Foldable f, Functor f) => TermDecorator f a Cost
 termCostDecorator c = 1 + sum (cost <$> tailF c)
 
 pqGramDecorator :: (Prologue.Foldable f, Functor f) => (forall b. CofreeF f (Record a) b -> label) -> Int -> Int -> TermDecorator f a (Gram label, DList.DList (Gram label))
-pqGramDecorator getLabel p q (a :< s) = (Gram [] [], foldMap (Prologue.snd . childGrams) s)
+pqGramDecorator getLabel p q c@(a :< s) = (Gram [] [ Just (getLabel c) ], foldMap (Prologue.snd . childGrams) s)
   where childGrams :: HasField fields (Gram label, DList.DList (Gram label)) => Record fields -> (Gram label, DList.DList (Gram label))
         childGrams = getField
 
