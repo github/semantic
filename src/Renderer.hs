11863b8748edc2a7f0266d21841b636f76043b73
joshvera <josh@joshvera.com>
2016-08-09 18:51:21 -0400

    Add Output and return Output from Renderer in Renderer.hs

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -4,12 +4,15 @@ import Prologue
 import Data.Functor.Both
 import Diff
 import Source
+import Data.Aeson.Types (Series)
 
 -- | A function that will render a diff, given the two source blobs.
-type Renderer annotation = Both SourceBlob -> Diff Text annotation -> Text
+type Renderer annotation = Both SourceBlob -> Diff Text annotation -> Output
 
 data DiffArguments = DiffArguments { format :: Format, output :: Maybe FilePath, outputPath :: FilePath }
  deriving (Show)
+
+data Output = SplitOutput Text | PatchOutput Text | JSONOutput Series  | SummaryOutput Series
 
 -- | The available types of diff rendering.
 data Format = Split | Patch | JSON | Summary
