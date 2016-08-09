c286b39146b3924bfb7f3254632252945652a3b8
joshvera <josh@joshvera.com>
2016-08-09 16:01:25 -0400

    Fix oids

--- test/diffs/no-newline-at-eof.patch.js
+++ test/diffs/no-newline-at-eof.patch.js
@@ -1,5 +1,5 @@
 diff --git a/test/diffs/no-newline-at-eof.A.js b/test/diffs/no-newline-at-eof.B.js
-index .. 100644
+index 0000000000000000000000000000000000000000..0000000000000000000000000000000000000000 100644
 --- a/test/diffs/no-newline-at-eof.A.js
 +++ b/test/diffs/no-newline-at-eof.B.js
 @@ -1,1 +1,3 @@
