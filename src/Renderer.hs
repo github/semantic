220ade047340f49a09503f641731d540181c5c18
joshvera <josh@joshvera.com>
2016-08-10 12:17:23 -0400

    Move concatOutputs to Renderer

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -1,10 +1,12 @@
-module Renderer where
+module Renderer (Renderer, DiffArguments(..), Output(..), concatOutputs, Format(..)) where
 
 import Prologue
 import Data.Functor.Both
 import Diff
-import Source
-import Data.Aeson.Types (Series)
+import Source (SourceBlob)
+import Data.Aeson.Types (Series, pairs)
+import Data.Text as T (intercalate)
+import Data.Aeson.Encoding (encodingToLazyByteString)
 
 -- | A function that will render a diff, given the two source blobs.
 type Renderer annotation = Both SourceBlob -> Diff Text annotation -> Output
@@ -13,6 +15,20 @@ data DiffArguments = DiffArguments { format :: Format, output :: Maybe FilePath,
  deriving (Show)
 
 data Output = SplitOutput Text | PatchOutput Text | JSONOutput Series  | SummaryOutput Series
+
+concatOutputs :: [Output] -> Text
+concatOutputs l@(JSONOutput _ : _) = toS . encodingToLazyByteString . pairs . mconcat $ toSeries <$> l
+concatOutputs l@(SummaryOutput _ : _) = toS . encodingToLazyByteString . pairs . mconcat $  toSeries <$> l
+concatOutputs l = T.intercalate "\n" (toText <$> l)
+
+toSeries :: Output -> Series
+toSeries (JSONOutput series) = series
+toSeries (SummaryOutput series) = series
+
+toText :: Output -> Text
+toText (SplitOutput text) = text
+toText (PatchOutput text) = text
+
 
 -- | The available types of diff rendering.
 data Format = Split | Patch | JSON | Summary
