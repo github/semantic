e29651ce073c86e619552dff0b33ac4b882e408c
Rick Winfrey <rewinfrey@github.com>
2016-08-16 13:09:43 -0500

    Create Comment Category instances from TreeSitter productions

--- src/TreeSitter.hs
+++ src/TreeSitter.hs
@@ -93,6 +93,7 @@ defaultCategoryForNodeName name = case name of
   "throw_statement" -> Throw
   "try_statement" -> Try
   "method_definition" -> Method
+  "comment" -> Comment
   _ -> Other name
 
 -- | Return a parser for a tree sitter language & document.
