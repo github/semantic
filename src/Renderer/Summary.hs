130724d05445a5f2951e0f7579131d6b4718d4db
joshvera <josh@joshvera.com>
2016-08-20 00:18:33 -0400

    Partition changes and errors into { changes:, errors: }

--- src/Renderer/Summary.hs
+++ src/Renderer/Summary.hs
@@ -1,16 +1,23 @@
 module Renderer.Summary where
 
 import Category
-import Prologue
+import Prologue hiding (isPrefixOf)
+import Data.Text (isPrefixOf)
 import Renderer
 import Data.Record
 import Range
 import DiffSummary
-import Source
 import Data.Aeson
+import Data.List (partition)
 import Data.Functor.Both (runBothWith)
+import Source
 
 summary :: (HasField fields Category, HasField fields Range) => Renderer (Record fields)
-summary blobs diff = SummaryOutput $ (runBothWith toSummaryKey (path <$> blobs)) .= (summaries >>= annotatedSummaries)
-  where summaries = diffSummaries (source <$> blobs) diff
-        toSummaryKey before after = toS $ if before == after then after else before <> " -> " <> after
+summary blobs diff = SummaryOutput $ "changes" .= changes <> "errors" .= errors
+  where
+    changes = ((toSummaryKey (path <$> blobs) <> ": ") <>) <$> changes'
+    (errors, changes') = partition ("Error" `isPrefixOf`) summaries
+    toSummaryKey = runBothWith $ \before after ->
+      toS $ if before == after then after else before <> " -> " <> after
+
+    summaries = diffSummaries blobs diff
