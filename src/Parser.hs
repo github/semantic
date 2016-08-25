74a5b3475092569a2859188a7696d2a8788e692a
Rob Rix <rob.rix@github.com>
2016-08-25 15:38:52 -0400

    :fire: withDefaultInfo.

--- src/Parser.hs
+++ src/Parser.hs
@@ -30,71 +30,70 @@ termConstructor :: forall fields. (Show (Record fields), HasField fields Categor
   -> Record fields -- ^ The annotation for the term.
   -> [Term Text (Record fields)] -- ^ The child nodes of the term.
   -> IO (Term Text (Record fields)) -- ^ The resulting term, in IO.
-termConstructor source sourceSpan info = fmap cofree . construct
+termConstructor source sourceSpan info = fmap (cofree . (info :<)) . construct
   where
-    withDefaultInfo syntax = pure (info :< syntax)
     errorWith children = do
       sourceSpan' <- sourceSpan
-      withDefaultInfo (S.Error sourceSpan' children)
-    construct :: (Show (Record fields), HasField fields Category, HasField fields Range) => [Term Text (Record fields)] -> IO (CofreeF (S.Syntax Text) (Record fields) (Term Text (Record fields)))
-    construct [] = case category info of
-      Return -> withDefaultInfo $ S.Return Nothing -- Map empty return statements to Return Nothing
-      _ -> withDefaultInfo . S.Leaf . pack . toString $ slice (characterRange info) source
+      pure (S.Error sourceSpan' children)
+    construct :: (Show (Record fields), HasField fields Category, HasField fields Range) => [Term Text (Record fields)] -> IO (S.Syntax Text (Term Text (Record fields)))
+    construct [] = pure $ case category info of
+      Return -> S.Return Nothing -- Map empty return statements to Return Nothing
+      _ -> S.Leaf . pack . toString $ slice (characterRange info) source
     construct children | Return == category info =
-      withDefaultInfo $ S.Return (listToMaybe children)
+      pure $ S.Return (listToMaybe children)
     construct children | Assignment == category info = case children of
-      (identifier:value:[]) -> withDefaultInfo $ S.Assignment identifier value
+      (identifier:value:[]) -> pure $ S.Assignment identifier value
       children -> errorWith children
     construct children | MathAssignment == category info = case children of
-      (identifier:value:[]) -> withDefaultInfo $ S.MathAssignment identifier value
+      (identifier:value:[]) -> pure $ S.MathAssignment identifier value
       children -> errorWith children
     construct children | MemberAccess == category info = case children of
-      (base:property:[]) -> withDefaultInfo $ S.MemberAccess base property
+      (base:property:[]) -> pure $ S.MemberAccess base property
       children -> errorWith children
     construct children | SubscriptAccess == category info = case children of
-      (base:element:[]) -> withDefaultInfo $ S.SubscriptAccess base element
+      (base:element:[]) -> pure $ S.SubscriptAccess base element
       _ -> errorWith children
-    construct children | isOperator (category info) = withDefaultInfo $ S.Operator children
-    construct children | CommaOperator == category info = withDefaultInfo $ case children of
+    construct children | isOperator (category info) = pure $ S.Operator children
+    construct children | CommaOperator == category info = pure $ case children of
       [child, rest] | S.Indexed cs <- unwrap rest -> S.Indexed $ child : toList cs
       _ -> S.Indexed children
     construct children | Function == category info = case children of
-      (body:[]) -> withDefaultInfo $ S.AnonymousFunction Nothing body
+      (body:[]) -> pure $ S.AnonymousFunction Nothing body
       (params:body:[]) | (info :< _) <- runCofree params, Params == category info ->
-        withDefaultInfo $ S.AnonymousFunction (Just params) body
+        pure $ S.AnonymousFunction (Just params) body
       (id:body:[]) | (info :< _) <- runCofree id, Identifier == category info ->
-        withDefaultInfo $ S.Function id Nothing body
+        pure $ S.Function id Nothing body
       (id:params:body:[]) | (info :< _) <- runCofree id, Identifier == category info ->
-        withDefaultInfo $ S.Function id (Just params) body
+        pure $ S.Function id (Just params) body
       _ -> errorWith children
 
     construct children | FunctionCall == category info = case runCofree <$> children of
       [ (_ :< S.MemberAccess{..}), (_ :< S.Args args) ] ->
-        pure $! setCategory info MethodCall :< S.MethodCall memberId property args
+        pure $ S.MethodCall memberId property args
       [ (_ :< S.MemberAccess{..}) ] ->
-        pure $! setCategory info MethodCall :< S.MethodCall memberId property []
+        pure $ S.MethodCall memberId property []
       (x:xs) ->
-        withDefaultInfo $ S.FunctionCall (cofree x) (cofree <$> xs)
+        pure $ S.FunctionCall (cofree x) (cofree <$> xs)
       _ -> errorWith children
 
     construct children | Ternary == category info = case children of
-      (condition:cases) -> withDefaultInfo $ S.Ternary condition cases
+      (condition:cases) -> pure $ S.Ternary condition cases
       _ -> errorWith children
-    construct children | Args == category info = withDefaultInfo $ S.Args children
+    construct children | Args == category info = pure $ S.Args children
     construct children | VarAssignment == category info
-                         , [x, y] <- children = withDefaultInfo $ S.VarAssignment x y
-    construct children | VarDecl == category info = withDefaultInfo . S.Indexed $ toVarDecl <$> children
+                         , [x, y] <- children = pure $ S.VarAssignment x y
+    construct children | VarDecl == category info = pure . S.Indexed $ toVarDecl <$> children
       where
         toVarDecl :: (HasField fields Category) => Term Text (Record fields) -> Term Text (Record fields)
         toVarDecl child = cofree $ (setCategory (extract child) VarDecl :< S.VarDecl child)
 
     construct children | Switch == category info, (expr:_) <- children =
-      withDefaultInfo $ S.Switch expr children
+      pure $ S.Switch expr children
 
     construct children | Case == category info, [expr, body] <- children =
-      withDefaultInfo $ S.Case expr body
+      pure $ S.Case expr body
 
-    construct children | Object == category info = withDefaultInfo . S.Object $ foldMap toTuple children
+    construct children | Object == category info = pure . S.Object $ foldMap toTuple children
       where
         toTuple :: Term Text (Record fields) -> [Term Text (Record fields)]
         toTuple child | S.Indexed [key,value] <- unwrap child = [cofree (extract child :< S.Pair key value)]
@@ -102,47 +101,47 @@ termConstructor source sourceSpan info = fmap cofree . construct
         toTuple child | S.Leaf c <- unwrap child = [cofree (extract child :< S.Comment c)]
         toTuple child = pure child
 
-    construct children | Pair == (category info) = withDefaultInfo $ S.Fixed children
+    construct children | Pair == (category info) = pure $ S.Fixed children
     construct children | C.Error == category info =
       errorWith children
     construct children | If == category info, Just (expr, clauses) <- uncons children =
       case clauses of
-        [clause1, clause2] -> withDefaultInfo $ S.If expr clause1 (Just clause2)
-        [clause] -> withDefaultInfo $ S.If expr clause Nothing
+        [clause1, clause2] -> pure $ S.If expr clause1 (Just clause2)
+        [clause] -> pure $ S.If expr clause Nothing
         _ -> errorWith children
     construct children | For == category info, Just (exprs, body) <- unsnoc children =
-      withDefaultInfo $ S.For exprs body
+      pure $ S.For exprs body
     construct children | While == category info, [expr, body] <- children =
-      withDefaultInfo $ S.While expr body
+      pure $ S.While expr body
     construct children | DoWhile == category info, [expr, body] <- children =
-      withDefaultInfo $ S.DoWhile expr body
+      pure $ S.DoWhile expr body
     construct children | Throw == category info, [expr] <- children =
-      withDefaultInfo $ S.Throw expr
+      pure $ S.Throw expr
     construct children | Constructor == category info, [expr] <- children =
-      withDefaultInfo $ S.Constructor expr
+      pure $ S.Constructor expr
     construct children | Try == category info = case children of
-      [body] -> withDefaultInfo $ S.Try body Nothing Nothing
-      [body, catch] | Catch <- category (extract catch) -> withDefaultInfo $ S.Try body (Just catch) Nothing
-      [body, finally] | Finally <- category (extract finally) -> withDefaultInfo $ S.Try body Nothing (Just finally)
+      [body] -> pure $ S.Try body Nothing Nothing
+      [body, catch] | Catch <- category (extract catch) -> pure $ S.Try body (Just catch) Nothing
+      [body, finally] | Finally <- category (extract finally) -> pure $ S.Try body Nothing (Just finally)
       [body, catch, finally] | Catch <- category (extract catch),
                                Finally <- category (extract finally) ->
-        withDefaultInfo $ S.Try body (Just catch) (Just finally)
+        pure $ S.Try body (Just catch) (Just finally)
       _ -> errorWith children
     construct children | ArrayLiteral == category info =
-      withDefaultInfo $ S.Array children
+      pure $ S.Array children
     construct children | Method == category info = case children of
       [identifier, params, exprs] |
         Params == category (extract params),
         S.Indexed params' <- unwrap params ->
-          withDefaultInfo $ S.Method identifier params' (toList (unwrap exprs))
+          pure $ S.Method identifier params' (toList (unwrap exprs))
       [identifier, exprs] ->
-        withDefaultInfo $ S.Method identifier mempty (toList (unwrap exprs))
+        pure $ S.Method identifier mempty (toList (unwrap exprs))
       _ -> errorWith children
     construct children | Class == category info = case children of
       [identifier, superclass, definitions] ->
-        withDefaultInfo $ S.Class identifier (Just superclass) (toList (unwrap definitions))
+        pure $ S.Class identifier (Just superclass) (toList (unwrap definitions))
       [identifier, definitions] ->
-        withDefaultInfo $ S.Class identifier Nothing (toList (unwrap definitions))
+        pure $ S.Class identifier Nothing (toList (unwrap definitions))
       _ -> errorWith children
     construct children =
-      withDefaultInfo $ S.Indexed children
+      pure $ S.Indexed children
