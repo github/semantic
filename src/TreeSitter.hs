b5534682a840bc36ea86f4e743897d23b0563745
Rob Rix <rob.rix@github.com>
2016-08-25 18:20:40 -0400

    Construct in javascriptTermConstructor.

--- src/TreeSitter.hs
+++ src/TreeSitter.hs
@@ -119,7 +119,6 @@ documentToTerm language document blob = alloca $ \ root -> do
             , spanEnd = SourcePos (fromIntegral $! ts_node_p_end_point_row node) (fromIntegral $! ts_node_p_end_point_column node) }
 
           -- Note: The strict application here is semantically important. Without it, we may not evaluate the range until after we’ve exited the scope that `node` was allocated within, meaning `alloca` will free it & other stack data may overwrite it.
-          let info = range `seq` range .: categoriesForLanguage language (toS name) .: RNil
-          termConstructor (source blob) (sourceSpan `seq` pure sourceSpan) info (filter (\child -> category (extract child) /= Empty) children)
+          range `seq` javascriptTermConstructor (source blob) (sourceSpan `seq` pure sourceSpan) (toS name) range (filter (\child -> category (extract child) /= Empty) children)
         getChild node n out = ts_node_p_named_child node n out >> toTerm out
         {-# INLINE getChild #-}
