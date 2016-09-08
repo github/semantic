e4a37940c071b2a072fa42a5201b2e37cfdafd84
Rob Rix <rob.rix@github.com>
2016-09-08 09:30:53 -0400

    Indent all the things.

--- src/Language/JavaScript.hs
+++ src/Language/JavaScript.hs
@@ -19,57 +19,57 @@ termConstructor
 termConstructor source sourceSpan name range children
   | name == "ERROR" = sourceSpan >>= withDefaultInfo . (`S.Error` children)
   | otherwise = withDefaultInfo $ case (name, children) of
-  ("return_statement", _) -> S.Return (listToMaybe children)
-  ("assignment", [ identifier, value ]) -> S.Assignment identifier value
-  ("math_assignment", [ identifier, value ]) -> S.MathAssignment identifier value
-  ("member_access", [ base, property ]) -> S.MemberAccess base property
-  ("subscript_access", [ base, element ]) -> S.SubscriptAccess base element
-  ("comma_op", [ a, b ]) -> case unwrap b of
-    S.Indexed rest -> S.Indexed $ a : rest
-    _ -> S.Indexed children
-  _ | name `elem` [ "op", "bool_op", "math_op", "delete_op", "type_op", "void_op", "rel_op", "bitwise_op" ]
-    -> S.Operator children
-  _ | name `elem` [ "arrow_function", "generator_function", "function" ] -> case children of
-    [ body ] -> S.AnonymousFunction Nothing body
-    [ params, body ] -> S.AnonymousFunction (Just params) body
-    [ id, params, body ] -> S.Function id (Just params) body
-    _ -> S.Indexed children
-  ("function_call", _) -> case runCofree <$> children of
-    [ (_ :< S.MemberAccess{..}), (_ :< S.Args args) ] -> S.MethodCall memberId property args
-    [ (_ :< S.MemberAccess{..}) ] -> S.MethodCall memberId property []
-    (x:xs) -> S.FunctionCall (cofree x) (cofree <$> xs)
-    _ -> S.Indexed children
-  ("ternary", (condition:cases)) -> S.Ternary condition cases
-  ("arguments", _) -> S.Args children
-  ("var_assignment", [ x, y ]) -> S.VarAssignment x y
-  ("var_declaration", _) -> S.Indexed $ toVarDecl <$> children
-  ("switch_statement", (expr:rest)) -> S.Switch expr rest
-  ("case", [ expr, body ]) -> S.Case expr body
-  ("object", _) -> S.Object $ foldMap toTuple children
-  ("pair", _) -> S.Fixed children
-  ("if_statement", [ expr, clause1, clause2 ]) -> S.If expr clause1 (Just clause2)
-  ("if_statement", [ expr, clause ]) -> S.If expr clause Nothing
-  _ | name `elem` [ "for_statement", "for_of_statement", "for_in_statement" ]
-    , Just (exprs, body) <- unsnoc children
-    -> S.For exprs body
-  ("while_statement", [ expr, body ]) -> S.While expr body
-  ("do_statement", [ expr, body ]) -> S.DoWhile expr body
-  ("throw_statement", [ expr ]) -> S.Throw expr
-  ("new_expression", [ expr ]) -> S.Constructor expr
-  ("try_statement", [ body ]) -> S.Try body Nothing Nothing
-  ("try_statement", [ body, catch ]) | Catch <- category (extract catch) -> S.Try body (Just catch) Nothing
-  ("try_statement", [ body, finally ]) | Finally <- category (extract finally) -> S.Try body Nothing (Just finally)
-  ("try_statement", [ body, catch, finally ])
-    | Catch <- category (extract catch)
-    , Finally <- category (extract finally) -> S.Try body (Just catch) (Just finally)
-  ("array", _) -> S.Array children
-  ("method_definition", [ identifier, params, exprs ]) -> S.Method identifier (toList (unwrap params)) (toList (unwrap exprs))
-  ("method_definition", [ identifier, exprs ]) -> S.Method identifier [] (toList (unwrap exprs))
-  ("class", [ identifier, superclass, definitions ]) -> S.Class identifier (Just superclass) (toList (unwrap definitions))
-  ("class", [ identifier, definitions ]) -> S.Class identifier Nothing (toList (unwrap definitions))
+    ("return_statement", _) -> S.Return (listToMaybe children)
+    ("assignment", [ identifier, value ]) -> S.Assignment identifier value
+    ("math_assignment", [ identifier, value ]) -> S.MathAssignment identifier value
+    ("member_access", [ base, property ]) -> S.MemberAccess base property
+    ("subscript_access", [ base, element ]) -> S.SubscriptAccess base element
+    ("comma_op", [ a, b ]) -> case unwrap b of
+      S.Indexed rest -> S.Indexed $ a : rest
+      _ -> S.Indexed children
+    _ | name `elem` [ "op", "bool_op", "math_op", "delete_op", "type_op", "void_op", "rel_op", "bitwise_op" ]
+      -> S.Operator children
+    _ | name `elem` [ "arrow_function", "generator_function", "function" ] -> case children of
+      [ body ] -> S.AnonymousFunction Nothing body
+      [ params, body ] -> S.AnonymousFunction (Just params) body
+      [ id, params, body ] -> S.Function id (Just params) body
+      _ -> S.Indexed children
+    ("function_call", _) -> case runCofree <$> children of
+      [ (_ :< S.MemberAccess{..}), (_ :< S.Args args) ] -> S.MethodCall memberId property args
+      [ (_ :< S.MemberAccess{..}) ] -> S.MethodCall memberId property []
+      (x:xs) -> S.FunctionCall (cofree x) (cofree <$> xs)
+      _ -> S.Indexed children
+    ("ternary", (condition:cases)) -> S.Ternary condition cases
+    ("arguments", _) -> S.Args children
+    ("var_assignment", [ x, y ]) -> S.VarAssignment x y
+    ("var_declaration", _) -> S.Indexed $ toVarDecl <$> children
+    ("switch_statement", (expr:rest)) -> S.Switch expr rest
+    ("case", [ expr, body ]) -> S.Case expr body
+    ("object", _) -> S.Object $ foldMap toTuple children
+    ("pair", _) -> S.Fixed children
+    ("if_statement", [ expr, clause1, clause2 ]) -> S.If expr clause1 (Just clause2)
+    ("if_statement", [ expr, clause ]) -> S.If expr clause Nothing
+    _ | name `elem` [ "for_statement", "for_of_statement", "for_in_statement" ]
+      , Just (exprs, body) <- unsnoc children
+      -> S.For exprs body
+    ("while_statement", [ expr, body ]) -> S.While expr body
+    ("do_statement", [ expr, body ]) -> S.DoWhile expr body
+    ("throw_statement", [ expr ]) -> S.Throw expr
+    ("new_expression", [ expr ]) -> S.Constructor expr
+    ("try_statement", [ body ]) -> S.Try body Nothing Nothing
+    ("try_statement", [ body, catch ]) | Catch <- category (extract catch) -> S.Try body (Just catch) Nothing
+    ("try_statement", [ body, finally ]) | Finally <- category (extract finally) -> S.Try body Nothing (Just finally)
+    ("try_statement", [ body, catch, finally ])
+      | Catch <- category (extract catch)
+      , Finally <- category (extract finally) -> S.Try body (Just catch) (Just finally)
+    ("array", _) -> S.Array children
+    ("method_definition", [ identifier, params, exprs ]) -> S.Method identifier (toList (unwrap params)) (toList (unwrap exprs))
+    ("method_definition", [ identifier, exprs ]) -> S.Method identifier [] (toList (unwrap exprs))
+    ("class", [ identifier, superclass, definitions ]) -> S.Class identifier (Just superclass) (toList (unwrap definitions))
+    ("class", [ identifier, definitions ]) -> S.Class identifier Nothing (toList (unwrap definitions))
 
-  (_, []) -> S.Leaf . toText $ slice range source
-  _ -> S.Indexed children
+    (_, []) -> S.Leaf . toText $ slice range source
+    _ -> S.Indexed children
   where withDefaultInfo syntax@(S.MethodCall _ _ _) = pure $! cofree ((range .:  MethodCall .: RNil) :< syntax)
         withDefaultInfo syntax = pure $! cofree ((range .: categoryForJavaScriptProductionName name .: RNil) :< syntax)
 
