04519db7a956ab54a4609ec8d5d67461b57c60a7
Rob Rix <rob.rix@github.com>
2016-09-13 11:39:27 -0400

    Verify that actualLineRanges produces the intended # of lines.

--- test/Source/Spec.hs
+++ test/Source/Spec.hs
@@ -1,9 +1,13 @@
 module Source.Spec where
 
 import Prologue
+import Range
 import Source
 import Test.Hspec
+import Test.Hspec.QuickCheck
 
 spec :: Spec
-spec = do
-  pure ()
+spec = parallel $ do
+  describe "actualLineRanges" $ do
+    prop "produces 1 more range than there are newlines" $
+      \ s -> length (actualLineRanges (totalRange s) (fromList s)) `shouldBe` succ (length (filter (== '\n') s))
