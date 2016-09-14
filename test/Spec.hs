1a25bf9a11be09d4acdeaf6a1752718c868de2d5
Rick Winfrey <rewinfrey@github.com>
2016-09-14 16:45:34 -0500

    Add RangeSpec to specs

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,6 +9,7 @@ import qualified Diff.Spec
 import qualified DiffSummarySpec
 import qualified InterpreterSpec
 import qualified PatchOutputSpec
+import qualified RangeSpec
 import qualified TermSpec
 import Test.Hspec
 
@@ -22,4 +23,5 @@ main = hspec . parallel $ do
   describe "DiffSummary" DiffSummarySpec.spec
   describe "Interpreter" InterpreterSpec.spec
   describe "PatchOutput" PatchOutputSpec.spec
+  describe "RangeSpec" RangeSpec.spec
   describe "Term" TermSpec.spec
