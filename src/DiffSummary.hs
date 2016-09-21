ba3342585b376d22b216270bf9109e2c2a72c3d9
Timothy Clem <timothy.clem@gmail.com>
2016-09-21 14:12:10 -0700

    Improve phrasing for anonymous functions

--- src/DiffSummary.hs
+++ src/DiffSummary.hs
@@ -102,11 +102,13 @@ prefixWithPatch patch = prefixWithThe (patchToPrefix patch)
 -- Optional determiner (e.g. "the") to tie together summary statements.
 determiner :: DiffInfo -> Doc
 determiner (LeafInfo "number" _) = ""
+determiner (LeafInfo "anonymous function" _) = "an"
 determiner (BranchInfo bs _ _) = determiner (last bs)
 determiner _ = "the"
 
 toLeafInfos :: DiffInfo -> [Doc]
 toLeafInfos (LeafInfo "number" termName) = pure (squotes (toDoc termName))
+toLeafInfos (LeafInfo "anonymous function" termName) = pure (toDoc termName)
 toLeafInfos LeafInfo{..} = pure (squotes (toDoc termName) <+> toDoc categoryName)
 toLeafInfos BranchInfo{..} = toLeafInfos =<< branches
 toLeafInfos err@ErrorInfo{} = pure (pretty err)
@@ -114,8 +116,9 @@ toLeafInfos err@ErrorInfo{} = pure (pretty err)
 -- Returns a text representing a specific term given a source and a term.
 toTermName :: forall leaf fields. (HasCategory leaf, HasField fields Category, HasField fields Range) => Source Char -> SyntaxTerm leaf fields -> Text
 toTermName source term = case unwrap term of
-  S.AnonymousFunction (Just ps) _ -> "anonymous (" <> termNameFromSource ps <> ")"
-  S.AnonymousFunction Nothing _ -> "anonymous"
+  S.AnonymousFunction maybeParams _ -> "anonymous function with " <> case maybeParams of
+    Just params -> "parameters (" <> termNameFromSource params <> ")"
+    Nothing -> "no parameters"
   S.Fixed children -> fromMaybe "branch" $ (toCategoryName . category) . extract <$> head children
   S.Indexed children -> fromMaybe "branch" $ (toCategoryName . category) . extract <$> head children
   Leaf leaf -> toCategoryName leaf
@@ -191,6 +194,7 @@ termToDiffInfo blob term = case unwrap term of
   S.Indexed children -> BranchInfo (termToDiffInfo' <$> children) (toCategoryName term) BIndexed
   S.Fixed children -> BranchInfo (termToDiffInfo' <$> children) (toCategoryName term) BFixed
   S.Ternary ternaryCondition _ -> LeafInfo (toCategoryName term) (toTermName' ternaryCondition)
+  S.AnonymousFunction _ _ -> LeafInfo "anonymous function" (toTermName' term)
   S.Function identifier _ _ -> LeafInfo (toCategoryName term) (toTermName' identifier)
   S.Assignment identifier _ -> LeafInfo (toCategoryName term) (toTermName' identifier)
   S.MathAssignment identifier _ -> LeafInfo (toCategoryName term) (toTermName' identifier)
