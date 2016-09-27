bbc6f06185ed820152bb3adcc1eeffb1982df2dd
Timothy Clem <timothy.clem@gmail.com>
2016-09-27 12:59:01 -0700

    Cleanup use of qualified Prelude

--- src/Arguments.hs
+++ src/Arguments.hs
@@ -1,14 +1,13 @@
 module Arguments (Arguments(..), args) where
 
 import Data.Functor.Both
-import qualified Prelude as P
 import Prelude
 import qualified Renderer as R
 
 -- | The command line arguments to the application.
 data Arguments = Arguments {
   format :: R.Format,
-  maybeShas :: Both (Maybe P.String),
+  maybeShas :: Both (Maybe String),
   maybeTimeout :: Maybe Float,
   output :: Maybe FilePath,
   filePaths :: [FilePath] }
