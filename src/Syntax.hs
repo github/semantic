63ca53b82809b9be31f7034f183a22cb25f7576d
Rick Winfrey <rewinfrey@github.com>
2016-10-06 15:19:11 -0500

    Add Import Syntax

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -74,6 +74,7 @@ data Syntax a f
   | If f f (Maybe f)
   -- | A module with an identifier, and a list of syntaxes.
   | Module { moduleId:: f, moduleBody :: [f] }
+  | Import f f
   deriving (Eq, Foldable, Functor, Generic, Generic1, Mergeable, Ord, Show, Traversable)
 
 
