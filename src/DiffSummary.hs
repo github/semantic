a4dfe48ff4e710fbe6a2a638ea63af19c431405e
Rick Winfrey <rewinfrey@github.com>
2016-10-14 13:39:57 -0500

    Output an import statement's expressions

--- src/DiffSummary.hs
+++ src/DiffSummary.hs
@@ -218,7 +218,8 @@ toTermName source term = case unwrap term of
   S.Comment a -> toCategoryName a
   S.Commented _ _ -> termNameFromChildren term (toList $ unwrap term)
   S.Module identifier _ -> toTermName' identifier
-  S.Import identifier _ -> toTermName' identifier
+  S.Import identifier [] -> toTermName' identifier
+  S.Import identifier expr -> intercalate ", " (termNameFromSource <$> expr) <> " from " <> toTermName' identifier
   S.Export Nothing expr -> "{ " <> intercalate ", " (termNameFromSource <$> expr) <> " }"
   S.Export (Just identifier) [] -> "{ " <> toTermName' identifier <> " }"
   S.Export (Just identifier) expr -> "{ " <> intercalate ", " (termNameFromSource <$> expr) <> " }" <> " from " <> toTermName' identifier
