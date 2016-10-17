2f0419e6521558c9dbb064e6361576aa13d7f83f
joshvera <josh@joshvera.com>
2016-10-17 15:45:08 -0400

    Add categoryForGoName

--- src/Language/Go.hs
+++ src/Language/Go.hs
@@ -1,5 +1,16 @@
 {-# LANGUAGE DataKinds #-}
 module Language.Go where
 
+import Prologue
+import Info
 
+categoryForGoName :: Text -> Category
+categoryForGoName = \case
+  "identifier" -> Identifier
+  "int_literal" -> NumberLiteral
+  "comment" -> Comment
+  "return_statement" -> Return
+  "interpreted_string_literal" -> StringLiteral
+  "raw_string_literal" -> StringLiteral
+  s -> Other (toS s)
 
