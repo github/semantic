4b42939bf35240a2e8880c8c6f953b3ac9b94969
Timothy Clem <timothy.clem@gmail.com>
2016-10-19 08:22:10 -0700

    Basics of Ruby termConstructor

---
+++ src/Language/Ruby.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds #-}
+module Language.Ruby where
+
+import Data.Record
+import Info
+import Prologue
+import Source
+import qualified Syntax as S
+import Term
+
+termConstructor
+  :: Source Char -- ^ The source that the term occurs within.
+  -> IO SourceSpan -- ^ The span that the term occupies. This is passed in 'IO' to guarantee some access constraints & encourage its use only when needed (improving performance).
+  -> Text -- ^ The name of the production for this node.
+  -> Range -- ^ The character range that the term occupies.
+  -> [Term (S.Syntax Text) (Record '[Range, Category, SourceSpan])] -- ^ The child nodes of the term.
+  -> IO (Term (S.Syntax Text) (Record '[Range, Category, SourceSpan])) -- ^ The resulting term, in IO.
+termConstructor source sourceSpan name range children = withDefaultInfo =<< do
+  putStrLn name
+  pure $ case (name, children) of
+    (_, []) -> S.Leaf . toText $ slice range source
+    _  -> S.Indexed children
+  where
+    withDefaultInfo syntax = do
+      sourceSpan' <- sourceSpan
+      pure $! cofree ((range .: categoryForRubyName name .: sourceSpan' .: RNil) :< syntax)
+
+categoryForRubyName :: Text -> Category
+categoryForRubyName = \case
+  s -> Other (toS s)
