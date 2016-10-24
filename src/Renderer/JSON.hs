52535ece24ce9d0b82d426aff082541a81a54dbb
Rick Winfrey <rewinfrey@github.com>
2016-10-24 12:24:03 -0500

    Let the instances breathe

--- src/Renderer/JSON.hs
+++ src/Renderer/JSON.hs
@@ -35,17 +35,22 @@ newtype NumberedLine a = NumberedLine (Int, a)
 instance (HasField fields Category, HasField fields Range) => ToJSON (NumberedLine (SplitSyntaxDiff leaf fields)) where
   toJSON (NumberedLine (n, a)) = object (lineFields n a (getRange a))
   toEncoding (NumberedLine (n, a)) = pairs $ mconcat (lineFields n a (getRange a))
+
 instance ToJSON Category where
   toJSON (Other s) = String s
   toJSON s = String . T.pack $ show s
+
 instance ToJSON Range where
   toJSON (Range start end) = A.Array . Vector.fromList $ toJSON <$> [ start, end ]
   toEncoding (Range start end) = foldable [ start, end ]
+
 instance ToJSON a => ToJSON (Join These a) where
   toJSON (Join vs) = A.Array . Vector.fromList $ toJSON <$> these pure pure (\ a b -> [ a, b ]) vs
   toEncoding = foldable
+
 instance ToJSON a => ToJSON (Join (,) a) where
   toJSON (Join (a, b)) = A.Array . Vector.fromList $ toJSON <$> [ a, b ]
+
 instance (HasField fields Category, HasField fields Range) => ToJSON (SplitSyntaxDiff leaf fields) where
   toJSON splitDiff = case runFree splitDiff of
     (Free (info :< syntax)) -> object (termFields info syntax)
@@ -53,6 +58,7 @@ instance (HasField fields Category, HasField fields Range) => ToJSON (SplitSynta
   toEncoding splitDiff = case runFree splitDiff of
     (Free (info :< syntax)) -> pairs $ mconcat (termFields info syntax)
     (Pure patch)            -> pairs $ mconcat (patchFields patch)
+
 instance (HasField fields Category, HasField fields Range) => ToJSON (SyntaxTerm leaf fields) where
   toJSON term     | (info :< syntax) <- runCofree term = object (termFields info syntax)
   toEncoding term | (info :< syntax) <- runCofree term = pairs $ mconcat (termFields info syntax)
