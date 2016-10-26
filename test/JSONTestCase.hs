a31a883b3063087e4a3216c4eec780379494ef4b
Rick Winfrey <rewinfrey@github.com>
2016-10-26 17:01:15 -0500

    Auto formatting

--- test/JSONTestCase.hs
+++ test/JSONTestCase.hs
@@ -1,30 +1,30 @@
 {-# LANGUAGE DeriveAnyClass #-}
 module JSONTestCase where
 
+import Data.Aeson
 import Data.Map.Strict as Map
 import Prelude
 import Prologue
-import Data.Aeson
 
 data JSONMetaRepo = JSONMetaRepo { repoPath :: !String
-                                 , repoUrl :: !String
+                                 , repoUrl  :: !String
                                  , language :: !String
                                  , syntaxes :: ![JSONMetaSyntax]
                                  } deriving (Show, Generic, FromJSON)
 
-data JSONMetaSyntax = JSONMetaSyntax { syntax :: !String
-                                     , repoFilePath :: !String
+data JSONMetaSyntax = JSONMetaSyntax { syntax           :: !String
+                                     , repoFilePath     :: !String
                                      , testCaseFilePath :: !String
-                                     , insert :: !String
-                                     , replacement :: !String
+                                     , insert           :: !String
+                                     , replacement      :: !String
                                      } deriving (Show, Generic, FromJSON)
 
-data JSONTestCase = JSONTestCase { gitDir :: !String
+data JSONTestCase = JSONTestCase { gitDir              :: !String
                                  , testCaseDescription :: !String
-                                 , filePaths :: ![String]
-                                 , sha1 :: !String
-                                 , sha2 :: !String
-                                 , expectedResult :: !(Map Text (Map Text [Value]))
+                                 , filePaths           :: ![String]
+                                 , sha1                :: !String
+                                 , sha2                :: !String
+                                 , expectedResult      :: !ExpectedResult
                                  } deriving (Show, Generic, FromJSON)
 
 instance ToJSON JSONTestCase where
