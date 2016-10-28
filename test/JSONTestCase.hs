df231ca64c92fb122dcce5316425265f907c80c2
Rick Winfrey <rewinfrey@github.com>
2016-10-27 20:40:20 -0500

    Add ToJSON and FromJSON instances for JSONTestCase and ExpectedResult

--- test/JSONTestCase.hs
+++ test/JSONTestCase.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveAnyClass, OverloadedStrings #-}
 module JSONTestCase where
 
 import Data.Aeson
+import Data.Aeson.Types
 import Data.Map.Strict as Map
 import Data.HashMap.Strict as HM
 import Prelude
@@ -33,8 +34,38 @@ data ExpectedResult = SummaryResult (Map Text (Map Text [Value]))
                     | EmptyResult
                     deriving (Show, Generic, FromJSON)
 
-instance ToJSON ExpectedResult where
-  toEncoding = genericToEncoding defaultOptions
+-- | These replace the defaultOptions normally used by genericToEncoding.
+-- | All options are default except for `sumEncoding`, which uses the `UntaggedValue`
+-- | option to prevent the sum type `ExpectedResult` from encoding with a `tag` and `contents`
+-- | fields when a JSONTestCase is encoded.
+jsonTestCaseOptions :: Options
+jsonTestCaseOptions = Options { fieldLabelModifier = id
+                              , constructorTagModifier = id
+                              , allNullaryToStringTag = False
+                              , omitNothingFields = True
+                              , sumEncoding = UntaggedValue
+                              , unwrapUnaryRecords = False
+                              }
 
 instance ToJSON JSONTestCase where
-  toEncoding = genericToEncoding defaultOptions
+  toJSON = genericToJSON jsonTestCaseOptions
+  toEncoding = genericToEncoding jsonTestCaseOptions
+
+instance ToJSON ExpectedResult where
+  toJSON = genericToJSON jsonTestCaseOptions
+  toEncoding = genericToEncoding jsonTestCaseOptions
+
+-- | We have to parse the specific formats of the ExpectedResults based on their keys.
+-- | This is how we determine which ExpectedResult constructor to use.
+instance FromJSON ExpectedResult where
+  parseJSON = Data.Aeson.withObject "ExpectedResult" $ \o ->
+    SummaryResult <$> summaryResultValues o <|>
+    JSONResult <$> jsonResultValues o <|>
+    EmptyResult <$> o .: ""
+    where
+      jsonResultValues :: Object -> Parser (Map Text Value)
+      jsonResultValues o = Map.fromList <$> (fromKey "oids" <> fromKey "rows" <> fromKey "paths")
+        where fromKey k = (\a -> [(k, a)]) <$> o .: k
+      summaryResultValues :: Object -> Parser (Map Text (Map Text [Value]))
+      summaryResultValues o = Map.fromList <$> (fromKey "changes" <> fromKey "errors")
+        where fromKey k = (\a -> [(k :: Text, Map.fromList . HM.toList $ a )] ) <$> o .: k
