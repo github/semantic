766a7236b845c757a9b03d2db02019f57292eaea
Rick Winfrey <rewinfrey@github.com>
2016-11-17 16:17:45 -0600

    Add catamorphism example

--- src/FDoc/RecursionSchemes.hs
+++ src/FDoc/RecursionSchemes.hs
@@ -31,3 +31,20 @@ indexedTermAna :: [leaf] -> Term (Syntax leaf) (Record '[NewField, Range, Catego
 indexedTermAna childrenLeaves = ana coalgebra (indexedTerm childrenLeaves)
   where
     coalgebra term = (NewField .: (extract term)) :< (unwrap term)
+
+{-
+Catamorphism
+
+cata :: (Base t a -> a) -- ^ a (Base t)-algebra
+       -> t               -- ^ fixed point
+       -> a               -- ^ result
+
+Catamorphism as a recursion scheme "tears down" a recursive structure.
+Catamorphisms work by using an algebra, which maps a shape in our fixed point structure to a new shape.
+
+The example below adds a new field to the `Record` fields.
+-}
+indexedTermCata :: [leaf] -> Term (Syntax leaf) (Record '[NewField, Range, Category])
+indexedTermCata childrenLeaves = cata algebra (indexedTerm childrenLeaves)
+  where
+    algebra term = cofree $ (NewField .: (headF term)) :< (tailF term)
