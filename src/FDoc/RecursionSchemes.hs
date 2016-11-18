8b7966b6cefed7a4750cc11717616ae2281a5063
Rick Winfrey <rewinfrey@github.com>
2016-11-18 11:33:27 -0600

    Comment formatting

--- src/FDoc/RecursionSchemes.hs
+++ src/FDoc/RecursionSchemes.hs
@@ -16,9 +16,9 @@ data NewField = NewField deriving (Show)
 {-
 Anamorphism -- add a new field to each term's Record fields
 
-ana :: (a -> Base t a) -- ^ a (Base t)-coalgebra
-    -> a               -- ^ seed
-    -> t               -- ^ resulting fixed point
+ana :: (a -> Base t a) -- a (Base t)-coalgebra
+    -> a               -- seed
+    -> t               -- resulting fixed point
 
 Anamorphism as a recursion scheme "builds up" a recursive structure.
 Anamorphisms work by using a coalgebra, which maps a seed value to a fixed point structure.
@@ -33,9 +33,9 @@ indexedTermAna childrenLeaves = ana coalgebra (indexedTerm childrenLeaves)
 {-
 Catamorphism example -- add a new field to each term's Record fields
 
-cata :: (Base t a -> a) -- ^ a (Base t)-algebra
-       -> t               -- ^ fixed point
-       -> a               -- ^ result
+cata :: (Base t a -> a) -- a (Base t)-algebra
+       -> t             -- fixed point
+       -> a             -- result
 
 Catamorphism as a recursion scheme "tears down" a recursive structure.
 Catamorphisms work by using an algebra, which maps a shape in our fixed point structure to a new shape.
