578d38426c67985ca918a8d150301248e24c8c18
Rick Winfrey <rewinfrey@github.com>
2016-11-21 17:48:22 -0600

    Better define hidden functor

--- src/FDoc/Term.hs
+++ src/FDoc/Term.hs
@@ -15,7 +15,7 @@ Constructs a Syntax.Leaf using the polymorphic type variable `leaf`.
 This is in the TermF shape: CofreeF f a b where
   f is the functor (Syntax.Leaf `leaf`)
   a is the annotation (Record '[Range, Category])
-  b is the hidden functor
+  b is the same type of functor defined by f
 
 Two common convenience operations when working with CofreeF (for docs, see Control.Comonad.Trans.Cofree.Types.CofreeF) are `headF` and `tailF`. `headF` return the annotation portion of the CofreeF structure, and `tailF` returns the functor portion (Syntax).
 
