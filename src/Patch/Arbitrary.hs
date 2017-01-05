8c566860ee51d9b8197b29e6464329182368ad1f
Rob Rix <rob.rix@github.com>
2017-01-05 14:45:56 -0500

    Define a Listable1 instance over Patch.

--- src/Patch/Arbitrary.hs
+++ src/Patch/Arbitrary.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Patch.Arbitrary where
 
+import Data.Functor.Listable
 import Patch
 import Prologue
 import Test.QuickCheck
@@ -16,3 +17,6 @@ instance Arbitrary a => Arbitrary (Patch a) where
   arbitrary = patchOf arbitrary arbitrary
 
   shrink patch = traverse shrink patch
+
+instance Listable1 Patch where
+  liftTiers t = liftCons1 t Insert \/ liftCons1 t Delete \/ liftCons2 t t Replace
