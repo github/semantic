da422724b3f25f7fc0c741c662d867285a9391dd
Rob Rix <rob.rix@github.com>
2017-01-05 14:36:16 -0500

    Re-export Listable & the tier operators.

--- src/Data/Functor/Listable.hs
+++ src/Data/Functor/Listable.hs
@@ -1,1 +1,13 @@
-module Data.Functor.Listable where
+module Data.Functor.Listable
+( Listable(..)
+, cons0
+, cons1
+, cons2
+, cons3
+, cons4
+, cons5
+, cons6
+, (\/)
+) where
+
+import Test.LeanCheck
