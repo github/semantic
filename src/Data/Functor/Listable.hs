f17fd281abd5fc8246f9671e5ffe8fbe76a34ebd
Rob Rix <rob.rix@github.com>
2017-01-05 14:37:39 -0500

    Lift Listable through Listable1.

--- src/Data/Functor/Listable.hs
+++ src/Data/Functor/Listable.hs
@@ -9,9 +9,13 @@ module Data.Functor.Listable
 , cons6
 , (\/)
 , Listable1(..)
+, tiers1
 ) where
 
 import Test.LeanCheck
 
 class Listable1 l where
   liftTiers :: [[a]] -> [[l a]]
+
+tiers1 :: (Listable a, Listable1 l) => [[l a]]
+tiers1 = liftTiers tiers
