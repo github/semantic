c326fd447cd75a0ba7f414ec150040dd0a732844
Rob Rix <rob.rix@github.com>
2017-01-09 14:45:09 -0500

    Move the Listable instance for Patch into its module.

--- src/Patch/Arbitrary.hs
+++ src/Patch/Arbitrary.hs
@@ -1,7 +1,6 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Patch.Arbitrary where
 
-import Data.Functor.Listable
 import Patch
 import Prologue
 import Test.QuickCheck
@@ -17,9 +16,3 @@ instance Arbitrary a => Arbitrary (Patch a) where
   arbitrary = patchOf arbitrary arbitrary
 
   shrink patch = traverse shrink patch
-
-instance Listable1 Patch where
-  liftTiers t = liftCons1 t Insert \/ liftCons1 t Delete \/ liftCons2 t t Replace
-
-instance Listable a => Listable (Patch a) where
-  tiers = tiers1
