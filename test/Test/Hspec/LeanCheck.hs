9a0845c72ab13bbd51bcd4badda122c8a72c93d4
Rob Rix <rob.rix@github.com>
2017-01-10 16:03:22 -0500

    :memo: the LeanCheck/Hspec integration API.

--- test/Test/Hspec/LeanCheck.hs
+++ test/Test/Hspec/LeanCheck.hs
@@ -16,12 +16,24 @@ import Test.LeanCheck.Core
 data Property where
   Property :: IOTestable prop => prop -> Property
 
+-- | Perform an enumerative test of a property using LeanCheck.
+--
+--   'prop' will typically be a function of one or more 'Listable' arguments, returning either 'Bool' or 'IO ()' (in the latter case, typically via 'shouldBe' and friends). For example:
+--
+-- > describe "+" $ do
+-- >   prop "associativity" $
+-- >     \ a b c -> a + (b + c) `shouldBe` (a + b :: Int) + c
 prop :: (HasCallStack, IOTestable prop) => String -> prop -> Spec
 prop s = it s . Property
 
 data ForAll a where
   ForAll :: IOTestable prop => [[a]] -> (a -> prop) -> ForAll a
 
+-- | Test a property given by an explicit list of tiers rather than a 'Listable' instance. This can be used to e.g. filter input values for which the property does not hold.
+--
+-- > describe "mean" $ do
+-- >   prop "≥ the minimum" . forAll (not . null `filterT` tiers) $
+-- >     \ list -> (mean list :: Int) `shouldSatisfy` (>= min list)
 forAll :: IOTestable prop => [[a]] -> (a -> prop) -> ForAll a
 forAll = ForAll
 
@@ -34,7 +46,7 @@ instance Example Property where
       Nothing -> pure Success
 
 class IOTestable t where
-  -- Like 'resultiers', but in 'IO'.
+  -- 'resultiers', lifted into 'IO'.
   ioresultiers :: t -> [[IO ([String], Bool)]]
 
 instance IOTestable (IO ()) where
@@ -51,11 +63,14 @@ instance IOTestable (ForAll a) where
   ioresultiers (ForAll tiers property) = concatMapT (ioresultiers . property) tiers
 
 
+-- | 'concatMapT', lifted into 'IO'.
 ioconcatMapT :: (a -> [[IO b]]) -> [[a]] -> [[IO b]]
 ioconcatMapT f = (>>= (>>= f))
 
+-- | 'counterExamples', lifted into 'IO'.
 iocounterExamples :: IOTestable a => Int -> a -> IO [[String]]
 iocounterExamples n = fmap (fmap fst . filter (not . snd)) . sequenceA . take n . concat . ioresultiers
 
+-- | 'counterExample', lifted into 'IO'.
 iocounterExample :: IOTestable a => Int -> a -> IO (Maybe [String])
 iocounterExample n = fmap listToMaybe . iocounterExamples n
