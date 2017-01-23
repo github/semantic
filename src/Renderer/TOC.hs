c35d7e8089661e46395b320317dd684d67c5c336
joshvera <josh@joshvera.com>
2017-01-23 17:26:43 -0500

    Summarize functions and methods to ExpressionInfo

--- src/Renderer/TOC.hs
+++ src/Renderer/TOC.hs
@@ -8,7 +8,6 @@ import qualified Data.Functor.Both as Both
 import Data.Record
 import Diff
 import Info
-import Patch
 import Prologue
 import qualified Data.List as List
 import qualified Data.Map as Map hiding (null)
@@ -16,7 +15,7 @@ import Renderer
 import Source
 import Syntax as S
 import Term
-
+import Patch
 
 {-
 
@@ -67,7 +66,7 @@ isErrorSummary :: JSONSummary -> Bool
 isErrorSummary ErrorSummary{} = True
 isErrorSummary _ = False
 
-data DiffInfo = LeafInfo { leafCategory :: Category, termName :: Text, sourceSpan :: SourceSpan }
+data DiffInfo = LeafInfo { leafCategory :: Category, termName :: Text, leafSourceSpan :: SourceSpan }
  | BranchInfo { branches :: [ DiffInfo ], branchCategory :: Category }
  | ErrorInfo { infoSpan :: SourceSpan, termName :: Text }
  | HideInfo -- Hide/Strip from summary output entirely.
@@ -115,9 +114,21 @@ diffTOC blobs diff = tocToJSONSummaries =<< diffToTOCSummaries (source <$> blobs
         -- Skip comments and leaves since they don't have any changes
         -- (Free (_ :< syntax)) -> annotateWithCategory (toList syntax >>= snd)
         (Free (_ :< syntax)) -> annotateWithCategory (toList syntax >>= snd)
-        (Pure patch) -> [ TOCSummary (mapPatch (termToDiffInfo beforeSource) (termToDiffInfo afterSource) patch) None ]
+        (Pure patch) ->
+          let
+            patch' = (mapPatch (termToDiffInfo beforeSource) (termToDiffInfo afterSource) patch)
+          in
+          case (maybeFst $ unPatch patch', maybeSnd $ unPatch patch') of
+            -- TODO: This is partial
+            (Just diffInfo, _) -> toTOCSummary patch' diffInfo
+            (_ , Just diffInfo) -> toTOCSummary patch' diffInfo
+            (Nothing, Nothing) -> panic "No diff"
       where
         (beforeSource, afterSource) = runJoin sources
+        toTOCSummary patch diffInfo = pure . TOCSummary patch $ case leafCategory diffInfo of
+              C.Function -> ExpressionInfo (leafCategory diffInfo) (termName diffInfo) (leafSourceSpan diffInfo)
+              C.Method -> ExpressionInfo (leafCategory diffInfo) (termName diffInfo) (leafSourceSpan diffInfo)
+              _ -> None
 
 
 
@@ -140,8 +151,8 @@ toLeafInfos _ _ ErrorInfo{..} = pure $ ErrorSummary termName infoSpan
 toLeafInfos parentInfo patchType BranchInfo{..} = branches >>= toLeafInfos parentInfo patchType
 toLeafInfos _ _ HideInfo = []
 toLeafInfos parentInfo patchType LeafInfo{..} = case leafCategory of
-  C.Function -> pure $ JSONSummary (ParentInfo leafCategory termName sourceSpan) patchType
-  C.Method -> pure $ JSONSummary (ParentInfo leafCategory termName sourceSpan) patchType
+  C.Function -> pure $ JSONSummary (ParentInfo leafCategory termName leafSourceSpan) patchType
+  C.Method -> pure $ JSONSummary (ParentInfo leafCategory termName leafSourceSpan) patchType
   _ -> case parentInfo of
     None -> []
     info -> pure $ JSONSummary info patchType
@@ -170,17 +181,17 @@ toTermName source = termNameFromSource
 appendSummary :: DefaultFields fields => Source Char -> SyntaxTerm leaf fields -> TOCSummary DiffInfo -> TOCSummary DiffInfo
 appendSummary source term summary =
   case (parentInfo summary, parent term) of
-    (None, Parent _) ->
+    (None, ParentSummarizable _) ->
       summary { parentInfo = ParentInfo (category (extract term)) (toTermName source term) (Info.sourceSpan (extract term)) }
-    (_, Parent _) -> summary
-    (_, NotParent _) -> summary
+    (_, ParentSummarizable _) -> summary
+    (_, NotSummarizable _) -> summary
     -- TODO: Add IsExpression?
 
-data IsParent a = Parent a | NotParent a
+data IsParent a = ParentSummarizable a | NotSummarizable a
 
 parent :: ComonadCofree (Syntax t) w => w a -> IsParent (w a)
 parent term = isParent (unwrap term) term
   where isParent = \case
-          S.Method{} -> Parent
-          S.Function{} -> Parent
-          _ -> NotParent
+          S.Method{} -> ParentSummarizable
+          S.Function{} -> ParentSummarizable
+          _ -> NotSummarizable
