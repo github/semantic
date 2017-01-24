9ec2262b38bc728a6b1c5059b3fd63df264e6325
Timothy Clem <timothy.clem@gmail.com>
2017-01-24 11:16:22 -0800

    Refactor names and handling of what's summarizable

--- src/Renderer/TOC.hs
+++ src/Renderer/TOC.hs
@@ -55,15 +55,15 @@ Example: https://github.com/github/github/pull/50259/files
 
 -}
 
-data JSONSummary = JSONSummary { info :: ParentInfo, changeType :: Text }
+data JSONSummary = JSONSummary { info :: Summarizable }
                  | ErrorSummary { error :: Text, errorSpan :: SourceSpan }
                  deriving (Generic, Eq, Show)
 
 instance ToJSON JSONSummary where
-  toJSON JSONSummary{..} = object . ([ "changeType" .= changeType ] <>) $ case info of
-    ParentInfo{..} -> [ "category" .= (show parentCategory :: Text), "term" .= parentTermName, "sourceSpan" .= parentSourceSpan ]
-    ExpressionInfo{..} -> [ "category" .= (show exprCategory :: Text), "term" .= exprTermName, "sourceSpan" .= exprSourceSpan ]
-    None -> panic "None ParentInfos should have been pruned"
+  toJSON JSONSummary{..} = object $ case info of
+    InSummarizable{..} -> [ "changeType" .= ("modified" :: Text), "category" .= (show parentCategory :: Text), "term" .= parentTermName, "sourceSpan" .= parentSourceSpan ]
+    Summarizable{..} -> [ "changeType" .= summarizableChangeType, "category" .= (show summarizableCategory :: Text), "term" .= summarizableTermName, "sourceSpan" .= summarizableSourceSpan ]
+    NotSummarizable -> panic "NotSummarizable should have been pruned"
   toJSON ErrorSummary{..} = object [ "error" .= error, "span" .= errorSpan ]
 
 isErrorSummary :: JSONSummary -> Bool
@@ -77,14 +77,16 @@ data DiffInfo = LeafInfo { leafCategory :: Category, termName :: Text, leafSourc
  deriving (Eq, Show)
 
 data TOCSummary a = TOCSummary {
-  patch :: Patch a,
-  parentInfo :: ParentInfo
-} deriving (Eq, Functor, Show, Generic)
+                      patch :: Patch a,
+                      parentInfo :: Summarizable
+                    } deriving (Eq, Functor, Show, Generic)
 
-data ParentInfo = ParentInfo { parentCategory :: Category, parentTermName :: Text, parentSourceSpan :: SourceSpan }
-  | ExpressionInfo { exprCategory :: Category, exprTermName :: Text, exprSourceSpan :: SourceSpan }
-  | None
+data Summarizable = Summarizable { summarizableCategory :: Category, summarizableTermName :: Text, summarizableSourceSpan :: SourceSpan, summarizableChangeType :: Text }
+  | InSummarizable { parentCategory :: Category, parentTermName :: Text, parentSourceSpan :: SourceSpan }
+  | NotSummarizable
   deriving (Eq, Show)
+
+data SummarizableTerm a = SummarizableTerm a | NotSummarizableTerm a
 
 toc :: (DefaultFields fields) => Renderer (Record fields)
 toc blobs diff = TOCOutput $ Map.fromList [
@@ -99,72 +101,74 @@ toc blobs diff = TOCOutput $ Map.fromList [
     summaries = diffTOC blobs diff
 
 diffTOC :: (StringConv leaf Text, DefaultFields fields) => Both SourceBlob -> SyntaxDiff leaf fields -> [JSONSummary]
-diffTOC blobs diff = tocToJSONSummaries =<< removeDupes (diffToTOCSummaries (source <$> blobs) diff)
+diffTOC blobs diff = toJSONSummaries =<< removeDupes (diffToTOCSummaries (source <$> blobs) diff)
   where
-    tocToJSONSummaries :: TOCSummary DiffInfo -> [JSONSummary]
-    tocToJSONSummaries TOCSummary{..} = summaries parentInfo patch
-
     removeDupes :: [TOCSummary DiffInfo] -> [TOCSummary DiffInfo]
+    removeDupes [] = []
     removeDupes xs = (fmap unsafeHead . List.groupBy (\a b -> parentInfo a == parentInfo b)) xs
 
     diffToTOCSummaries :: (StringConv leaf Text, DefaultFields fields) => Both (Source Char) -> SyntaxDiff leaf fields -> [TOCSummary DiffInfo]
     diffToTOCSummaries sources = para $ \diff ->
       let
-        -- TODO Turn this on to add annotateWithCategory back
         diff' = free (Prologue.fst <$> diff)
-        annotateWithCategory :: [TOCSummary DiffInfo] -> [TOCSummary DiffInfo]
-        annotateWithCategory children = case (beforeTerm diff', afterTerm diff') of
-          (_, Just diff'') -> appendSummary (Both.snd sources) diff'' <$> children
-          (Just diff'', _) -> appendSummary (Both.fst sources) diff'' <$> children
-          (Nothing, Nothing) -> []
-      in case diff of
-        -- Skip comments and leaves since they don't have any changes
-        -- (Free (_ :< syntax)) -> annotateWithCategory (toList syntax >>= snd)
-        (Free (_ :< syntax)) -> annotateWithCategory (toList syntax >>= snd)
-        (Pure patch) ->
-          let
-            patch' = (mapPatch (termToDiffInfo beforeSource) (termToDiffInfo afterSource) patch)
-          in
-          case (maybeFst $ unPatch patch', maybeSnd $ unPatch patch') of
-            -- TODO: This is partial
-            (Just diffInfo, _) -> toTOCSummary patch' diffInfo
-            (_ , Just diffInfo) -> toTOCSummary patch' diffInfo
-            (Nothing, Nothing) -> panic "No diff"
-      where
+        patch' = mapPatch (termToDiffInfo beforeSource) (termToDiffInfo afterSource)
         (beforeSource, afterSource) = runJoin sources
-        toTOCSummary patch diffInfo@LeafInfo{} = pure . TOCSummary patch $ case leafCategory diffInfo of
-              C.Function -> ExpressionInfo (leafCategory diffInfo) (termName diffInfo) (leafSourceSpan diffInfo)
-              C.Method -> ExpressionInfo (leafCategory diffInfo) (termName diffInfo) (leafSourceSpan diffInfo)
-              _ -> None
-        toTOCSummary patch BranchInfo{..} = toTOCSummary patch =<< branches
+      in case diff of
+        (Free (_ :< syntax)) -> mapToInSummarizable sources diff' (toList syntax >>= snd)
+        (Pure patch) -> toTOCSummaries (patch' patch)
 
+-- Mark which leaves are summarizable.
+toTOCSummaries :: Patch DiffInfo -> [TOCSummary DiffInfo]
+toTOCSummaries patch = case (maybeFst $ unPatch patch, maybeSnd $ unPatch patch) of
+  (_ , Just diffInfo) -> toTOCSummaries' diffInfo
+  (Just diffInfo, _) -> toTOCSummaries' diffInfo
+  (Nothing, Nothing) -> panic "No diff"
+  where
+    toTOCSummaries' = \case
+      ErrorInfo{..} -> pure $ TOCSummary patch NotSummarizable
+      BranchInfo{..} -> branches >>= toTOCSummaries'
+      HideInfo{} -> []
+      LeafInfo{..} -> pure . TOCSummary patch $ case leafCategory of
+        C.Function -> Summarizable leafCategory termName leafSourceSpan patchType
+        C.Method -> Summarizable leafCategory termName leafSourceSpan patchType
+        _ -> NotSummarizable
+        where
+          patchType = case patch of
+            Replace{} -> "replaced"
+            Insert{} -> "added"
+            Delete{} -> "deleted"
 
+mapToInSummarizable :: DefaultFields fields => Both (Source Char) -> SyntaxDiff leaf fields -> [TOCSummary DiffInfo] -> [TOCSummary DiffInfo]
+mapToInSummarizable sources diff children = case (beforeTerm diff, afterTerm diff) of
+  (_, Just diff') -> mapToInSummarizable' (Both.snd sources) diff' <$> children
+  (Just diff', _) -> mapToInSummarizable' (Both.fst sources) diff' <$> children
+  (Nothing, Nothing) -> []
+  where
+    mapToInSummarizable' :: DefaultFields fields => Source Char -> SyntaxTerm leaf fields -> TOCSummary DiffInfo -> TOCSummary DiffInfo
+    mapToInSummarizable' source term summary =
+      case (parentInfo summary, summarizable term) of
+        (NotSummarizable, SummarizableTerm _) ->
+          summary { parentInfo = InSummarizable (category (extract term)) (toTermName source term) (Info.sourceSpan (extract term)) }
+        (_, _) -> summary
 
--- If the term is a method/function grab the term type, name, and span here, otherwise grab them up top.
-summaries :: ParentInfo -> Patch DiffInfo -> [JSONSummary]
-summaries parentInfo = \case
-  (Replace i1 i2) -> zipWith (\a b ->
-    let jsonSummary = a
-        parentInfo = info a in
-      case parentInfo of
-        ParentInfo{} -> jsonSummary { info = parentInfo { parentTermName = (parentTermName $ info a) <> " -> " <> (parentTermName $ info b) } }
-        ExpressionInfo{} -> jsonSummary { info = parentInfo { exprTermName = (exprTermName $ info a) <> " -> " <> (exprTermName $ info b) } }
-        _ -> panic "None ParentInfos should be filtered out"
-    ) (toLeafInfos parentInfo "replace" i1) (toLeafInfos parentInfo "replace" i2)
-  (Insert info) -> toLeafInfos parentInfo "insert" info
-  (Delete info) -> toLeafInfos parentInfo "delete" info
+summarizable :: ComonadCofree (Syntax t) w => w a -> SummarizableTerm (w a)
+summarizable term = go (unwrap term) term
+  where go = \case
+          S.Method{} -> SummarizableTerm
+          S.Function{} -> SummarizableTerm
+          _ -> NotSummarizableTerm
 
-toLeafInfos :: ParentInfo -> Text -> DiffInfo -> [JSONSummary]
-toLeafInfos _ _ ErrorInfo{..} = pure $ ErrorSummary termName infoSpan
-toLeafInfos parentInfo patchType BranchInfo{..} = branches >>= toLeafInfos parentInfo patchType
-toLeafInfos _ _ HideInfo = []
-toLeafInfos parentInfo patchType LeafInfo{..} = case leafCategory of
-  C.Function -> pure $ JSONSummary (ParentInfo leafCategory termName leafSourceSpan) patchType
-  C.Method -> pure $ JSONSummary (ParentInfo leafCategory termName leafSourceSpan) patchType
-  _ -> case parentInfo of
-    None -> []
-    info -> pure $ JSONSummary info patchType
-
+toJSONSummaries :: TOCSummary DiffInfo -> [JSONSummary]
+toJSONSummaries TOCSummary{..} = case (maybeFst $ unPatch patch, maybeSnd $ unPatch patch) of
+  (_, Just diffInfo) -> toJSONSummaries' diffInfo
+  (Just diffInfo, _) -> toJSONSummaries' diffInfo
+  (Nothing, Nothing) -> panic "No diff"
+  where
+    toJSONSummaries' = \case
+      ErrorInfo{..} -> pure $ ErrorSummary termName infoSpan
+      BranchInfo{..} -> branches >>= toJSONSummaries'
+      HideInfo -> []
+      LeafInfo{..} -> pure $ JSONSummary parentInfo
 
 termToDiffInfo :: (StringConv leaf Text, DefaultFields fields) => Source Char -> SyntaxTerm leaf fields -> DiffInfo
 termToDiffInfo blob term = case unwrap term of
@@ -226,21 +230,3 @@ identifiable term = isIdentifiable (unwrap term) term
           S.TypeDecl{} -> Identifiable
           S.Ty{} -> Identifiable
           _ -> Unidentifiable
-
-appendSummary :: DefaultFields fields => Source Char -> SyntaxTerm leaf fields -> TOCSummary DiffInfo -> TOCSummary DiffInfo
-appendSummary source term summary =
-  case (parentInfo summary, parent term) of
-    (None, ParentSummarizable _) ->
-      summary { parentInfo = ParentInfo (category (extract term)) (toTermName source term) (Info.sourceSpan (extract term)) }
-    (_, ParentSummarizable _) -> summary
-    (_, NotSummarizable _) -> summary
-    -- TODO: Add IsExpression?
-
-data IsParent a = ParentSummarizable a | NotSummarizable a
-
-parent :: ComonadCofree (Syntax t) w => w a -> IsParent (w a)
-parent term = isParent (unwrap term) term
-  where isParent = \case
-          S.Method{} -> ParentSummarizable
-          S.Function{} -> ParentSummarizable
-          _ -> NotSummarizable
