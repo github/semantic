638163f2037bfb6b921323ab3e1ce442604137e3
Timothy Clem <timothy.clem@gmail.com>
2017-01-31 11:16:06 -0800

    Just compile against ansi c++

--- languages/ruby/Setup.hs
+++ languages/ruby/Setup.hs
@@ -18,8 +18,7 @@ makeScannerLib :: Args -> ConfigFlags -> IO HookedBuildInfo
 makeScannerLib _ flags = do
   let verbosity = fromFlag $ configVerbosity flags
   rawSystemExit verbosity "env" ["mkdir", "-p", "lib"]
-  let flag =  if buildOS == OSX then "-std=c++11" else "-std=c++0x"
-  rawSystemExit verbosity "env" ["gcc", flag, "-Ivendor/tree-sitter-ruby/src/", "-fPIC", "vendor/tree-sitter-ruby/src/scanner.cc", "-c", "-o", "lib/scanner.o"]
+  rawSystemExit verbosity "env" ["gcc", "-Ivendor/tree-sitter-ruby/src/", "-fPIC", "vendor/tree-sitter-ruby/src/scanner.cc", "-c", "-o", "lib/scanner.o"]
   rawSystemExit verbosity "env" ["ar", "rcvs", "lib/libscanner.a", "lib/scanner.o"]
   pure emptyHookedBuildInfo
 
