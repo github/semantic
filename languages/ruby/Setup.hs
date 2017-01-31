e0d363ebd03e3804e0e474540386787c87278065
joshvera <josh@joshvera.com>
2017-01-31 12:45:09 -0500

    Add custom build to Ruby for scanner library

--- languages/ruby/Setup.hs
+++ languages/ruby/Setup.hs
@@ -1,2 +1,13 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils
+main = defaultMainWithHooks simpleUserHooks { preConf = makeScannerLib }
+
+makeScannerLib :: Args -> ConfigFlags -> IO HookedBuildInfo
+makeScannerLib _ flags = do
+    let verbosity = fromFlag $ configVerbosity flags
+    rawSystemExit verbosity "env" ["mkdir", "-p", "lib"]
+    rawSystemExit verbosity "env" ["gcc", "-std=c++11", "-Ivendor/tree-sitter-ruby/src/", "-fPIC", "vendor/tree-sitter-ruby/src/scanner.cc", "-c", "-o", "lib/scanner.o"]
+    rawSystemExit verbosity "env" ["ar", "rcvs", "lib/libscanner.a", "lib/scanner.o"]
+    pure emptyHookedBuildInfo
