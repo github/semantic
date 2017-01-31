425933bfa72645c4584ddff6b619265459e64bf3
joshvera <josh@joshvera.com>
2017-01-31 15:52:38 -0500

    update apis

--- languages/c/src/Text/Parser/TreeSitter/C.hs
+++ languages/c/src/Text/Parser/TreeSitter/C.hs
@@ -3,4 +3,4 @@ module Text.Parser.TreeSitter.C where
 import Text.Parser.TreeSitter
 import Foreign.Ptr
 
-foreign import ccall "vendor/tree-sitter-c/src/parser.c ts_language_c" ts_language_c :: Ptr Language
+foreign import ccall "vendor/tree-sitter-c/src/parser.c tree_sitter_c" tree_sitter_c :: Ptr Language
