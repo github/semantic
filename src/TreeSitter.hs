f723b576e1c081ca2f4105e017cba196c8e0ec69
Rob Rix <rob.rix@me.com>
2017-02-10 12:59:22 -0500

    Pass the getter in.

--- src/TreeSitter.hs
+++ src/TreeSitter.hs
@@ -48,30 +48,25 @@ documentToTerm language document SourceBlob{..} = alloca $ \ root -> do
           name <- ts_node_p_name node document
           name <- peekCString name
           count <- ts_node_p_named_child_count node
-          children <- filter isNonEmpty <$> traverse (alloca . getChild (start range) node) (take (fromIntegral count) [0..])
+          children <- filter isNonEmpty <$> traverse (alloca . getChild ts_node_p_named_child (start range) node) (take (fromIntegral count) [0..])
 
           let startPos = SourcePos (1 + (fromIntegral $! ts_node_p_start_point_row node)) (1 + (fromIntegral $! ts_node_p_start_point_column node))
           let endPos = SourcePos (1 + (fromIntegral $! ts_node_p_end_point_row node)) (1 + (fromIntegral $! ts_node_p_end_point_column node))
           let sourceSpan = SourceSpan { spanStart = startPos , spanEnd = endPos }
 
           allChildrenCount <- ts_node_p_child_count node
-          let allChildren = filter isNonEmpty <$> traverse (alloca . getUnnamedChild (start range) node) (take (fromIntegral allChildrenCount) [0..])
+          let allChildren = filter isNonEmpty <$> traverse (alloca . getChild ts_node_p_child (start range) node) (take (fromIntegral allChildrenCount) [0..])
 
           -- Note: The strict application here is semantically important.
           -- Without it, we may not evaluate the value until after we’ve exited
           -- the scope that `node` was allocated within, meaning `alloca` will
           -- free it & other stack data may overwrite it.
           range `seq` sourceSpan `seq` assignTerm language source (range :. categoryForLanguageProductionName language (toS name) :. sourceSpan :. Nil) children allChildren
-        getChild start node n out = do
-          _ <- ts_node_p_named_child node n out
+        getChild getter start node n out = do
+          _ <- getter node n out
           let childRange = nodeRange node
           toTerm out childRange (slice (offsetRange childRange (negate start)) source)
         {-# INLINE getChild #-}
-        getUnnamedChild start node n out = do
-          _ <- ts_node_p_child node n out
-          let childRange = nodeRange node
-          toTerm out childRange (slice (offsetRange childRange (negate start)) source)
-        {-# INLINE getUnnamedChild #-}
         isNonEmpty child = category (extract child) /= Empty
 
         nodeRange node = Range { start = fromIntegral $ ts_node_p_start_char node, end = fromIntegral $ ts_node_p_end_char node }
