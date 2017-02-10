f3bad55fee8aa991f6a3fed78a3dfb29635717bf
Rob Rix <rob.rix@me.com>
2017-02-10 16:44:07 -0500

    Revert "Close over the correct source."
    This reverts commit 6770d9970d9e4aee6029e9b739583c1dc8cfc372.

--- src/TreeSitter.hs
+++ src/TreeSitter.hs
@@ -62,11 +62,11 @@ documentToTerm language document SourceBlob{..} = alloca $ \ root -> do
           -- the scope that `node` was allocated within, meaning `alloca` will
           -- free it & other stack data may overwrite it.
           range `seq` sourceSpan `seq` assignTerm language source (range :. categoryForLanguageProductionName language (toS name) :. sourceSpan :. Nil) children allChildren
-          where getChild getter node n out = do
-                  _ <- getter node n out
-                  let childRange = nodeRange node
-                  toTerm out childRange (slice childRange source)
-                {-# INLINE getChild #-}
+        getChild getter node n out = do
+          _ <- getter node n out
+          let childRange = nodeRange node
+          toTerm out childRange (slice childRange source)
+        {-# INLINE getChild #-}
         isNonEmpty child = category (extract child) /= Empty
 
         nodeRange node = Range { start = fromIntegral $ ts_node_p_start_char node, end = fromIntegral $ ts_node_p_end_char node }
