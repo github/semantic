5688c2b44770e702d7c91d12808fcb84269334c8
Rob Rix <rob.rix@me.com>
2017-02-10 12:26:02 -0500

    Pre-slice the source passed to toTerm.

--- src/TreeSitter.hs
+++ src/TreeSitter.hs
@@ -43,30 +43,29 @@ treeSitterParser language grammar blob = do
 documentToTerm :: Language -> Ptr Document -> Parser (Syntax.Syntax Text) (Record '[Range, Category, SourceSpan])
 documentToTerm language document SourceBlob{..} = alloca $ \ root -> do
   ts_document_root_node_p document root
-  toTerm root
-  where toTerm node = do
+  toTerm root source
+  where toTerm node source = do
           name <- ts_node_p_name node document
           name <- peekCString name
           count <- ts_node_p_named_child_count node
-          children <- filter isNonEmpty <$> traverse (alloca . getChild node) (take (fromIntegral count) [0..])
-
           let range = nodeRange node
+          children <- filter isNonEmpty <$> traverse (alloca . getChild (start range) node) (take (fromIntegral count) [0..])
 
           let startPos = SourcePos (1 + (fromIntegral $! ts_node_p_start_point_row node)) (1 + (fromIntegral $! ts_node_p_start_point_column node))
           let endPos = SourcePos (1 + (fromIntegral $! ts_node_p_end_point_row node)) (1 + (fromIntegral $! ts_node_p_end_point_column node))
           let sourceSpan = SourceSpan { spanStart = startPos , spanEnd = endPos }
 
           allChildrenCount <- ts_node_p_child_count node
-          let allChildren = filter isNonEmpty <$> traverse (alloca . getUnnamedChild node) (take (fromIntegral allChildrenCount) [0..])
+          let allChildren = filter isNonEmpty <$> traverse (alloca . getUnnamedChild (start range) node) (take (fromIntegral allChildrenCount) [0..])
 
           -- Note: The strict application here is semantically important.
           -- Without it, we may not evaluate the value until after we’ve exited
           -- the scope that `node` was allocated within, meaning `alloca` will
           -- free it & other stack data may overwrite it.
-          range `seq` sourceSpan `seq` assignTerm language (slice range source) (range :. categoryForLanguageProductionName language (toS name) :. sourceSpan :. Nil) children allChildren
-        getChild node n out = ts_node_p_named_child node n out >> toTerm out
+          range `seq` sourceSpan `seq` assignTerm language source (range :. categoryForLanguageProductionName language (toS name) :. sourceSpan :. Nil) children allChildren
+        getChild start node n out = ts_node_p_named_child node n out >> toTerm out (slice (offsetRange (nodeRange node) (negate start)) source)
         {-# INLINE getChild #-}
-        getUnnamedChild node n out = ts_node_p_child node n out >> toTerm out
+        getUnnamedChild start node n out = ts_node_p_child node n out >> toTerm out (slice (offsetRange (nodeRange node) (negate start)) source)
         {-# INLINE getUnnamedChild #-}
         isNonEmpty child = category (extract child) /= Empty
 
