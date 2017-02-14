ab8e407df3b7a86650756c43b8df7470e1ddd0ef
Rob Rix <rob.rix@me.com>
2017-02-14 15:42:14 -0500

    Get the tests compiling again.

--- test/Source/Spec.hs
+++ test/Source/Spec.hs
@@ -8,37 +8,38 @@ import Source
 import SourceSpan
 import Test.Hspec
 import Test.Hspec.LeanCheck
+import Test.LeanCheck
 
 spec :: Spec
 spec = parallel $ do
   describe "actualLineRanges" $ do
     prop "produces 1 more range than there are newlines" $
-      \ source -> Prologue.length (actualLineRanges (totalRange source) source) `shouldBe` succ (Text.count "\n" (sourceText source))
+      \ source -> Prologue.length (actualLineRanges (totalRange source) source) `shouldBe` succ (Text.count "\n" (toText source))
 
     prop "produces exhaustive ranges" $
       \ source -> foldMap (`slice` source) (actualLineRanges (totalRange source) source) `shouldBe` source
 
   describe "sourceSpanToRange" $ do
-    prop "computes single-line ranges" $
-      \ s -> let source = fromList s
+    prop "computes single-line ranges" . forAll (unListableByteString `mapT` tiers) $
+      \ s -> let source = Source s
                  spans = zipWith (\ i Range {..} -> SourceSpan (SourcePos i 0) (SourcePos i (end - start))) [0..] ranges
                  ranges = actualLineRanges (totalRange source) source in
         sourceSpanToRange source <$> spans `shouldBe` ranges
 
-    prop "computes multi-line ranges" $
-      \ s -> let source = fromList s in
+    prop "computes multi-line ranges" . forAll (unListableByteString `mapT` tiers) $
+      \ s -> let source = Source s in
         sourceSpanToRange source (totalSpan source) `shouldBe` totalRange source
 
-    prop "computes sub-line ranges" $
-      \ s -> let source = fromList ('*' : s <> "*") in
+    prop "computes sub-line ranges" . forAll (unListableByteString `mapT` tiers) $
+      \ s -> let source = Source ("*" <> s <> "*") in
         sourceSpanToRange source (insetSpan (totalSpan source)) `shouldBe` insetRange (totalRange source)
 
   describe "totalSpan" $ do
     prop "covers single lines" $
-      \ n -> totalSpan (fromList (replicate n '*')) `shouldBe` SourceSpan (SourcePos 0 0) (SourcePos 0 (max 0 n))
+      \ n -> totalSpan (fromText (Text.replicate n "*")) `shouldBe` SourceSpan (SourcePos 0 0) (SourcePos 0 (max 0 n))
 
     prop "covers multiple lines" $
-      \ n -> totalSpan (fromList (intersperse '\n' (replicate n '*'))) `shouldBe` SourceSpan (SourcePos 0 0) (SourcePos (max 0 (pred n)) (if n > 0 then 1 else 0))
+      \ n -> totalSpan (fromText (Text.intersperse '\n' (Text.replicate n "*"))) `shouldBe` SourceSpan (SourcePos 0 0) (SourcePos (max 0 (pred n)) (if n > 0 then 1 else 0))
 
 totalSpan :: Source -> SourceSpan
 totalSpan source = SourceSpan (SourcePos 0 0) (SourcePos (pred (Prologue.length ranges)) (end lastRange - start lastRange))
