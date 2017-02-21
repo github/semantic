687c7c5ea17267ff86020c9ef2534d1e5f76260c
Rob Rix <rob.rix@me.com>
2017-02-21 10:46:19 -0500

    Rename the Algorithm constructors to reflect the algorithms performed.

--- src/Algorithm.hs
+++ src/Algorithm.hs
@@ -7,11 +7,11 @@ import Prologue hiding (Pure)
 -- | A single step in a diffing algorithm, parameterized by the types of terms, diffs, and the result of the applicable algorithm.
 data AlgorithmF term diff result where
   -- | Diff two terms recursively in O(n) time, resulting in a single diff node.
-  Recursive :: term -> term -> AlgorithmF term diff diff
+  Linear :: term -> term -> AlgorithmF term diff diff
   -- | Diff two lists of terms by each element’s position in O(n³) time, resulting in a list of diffs.
-  ByIndex :: [term] -> [term] -> AlgorithmF term diff [diff]
+  SES :: [term] -> [term] -> AlgorithmF term diff [diff]
   -- | Diff two lists of terms by each element’s similarity in O(n³ log n), resulting in a list of diffs.
-  BySimilarity :: [term] -> [term] -> AlgorithmF term diff [diff]
+  RWS :: [term] -> [term] -> AlgorithmF term diff [diff]
 
 -- | The free applicative for 'AlgorithmF'. This enables us to construct diff values using <$> and <*> notation.
 type Algorithm term diff = Ap (AlgorithmF term diff)
@@ -31,13 +31,13 @@ iterAp' algebra = go
 -- DSL
 
 -- | Constructs a 'Recursive' diff of two terms.
-recursively :: term -> term -> Algorithm term diff diff
-recursively a b = liftAp (Recursive a b)
+linearly :: term -> term -> Algorithm term diff diff
+linearly a b = liftAp (Linear a b)
 
 -- | Constructs a 'ByIndex' diff of two lists of terms.
-byIndex :: [term] -> [term] -> Algorithm term diff [diff]
-byIndex a b = liftAp (ByIndex a b)
+bySES :: [term] -> [term] -> Algorithm term diff [diff]
+bySES a b = liftAp (SES a b)
 
 -- | Constructs a 'BySimilarity' diff of two lists of terms.
-bySimilarity :: [term] -> [term] -> Algorithm term diff [diff]
-bySimilarity a b = liftAp (BySimilarity a b)
+byRWS :: [term] -> [term] -> Algorithm term diff [diff]
+byRWS a b = liftAp (RWS a b)
