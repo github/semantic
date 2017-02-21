1cd3216f47ef176dfc4799e658d7147d2b7909a6
Rob Rix <rob.rix@me.com>
2017-02-21 16:03:35 -0500

    Stub in a GEq1 typeclass.

--- src/Data/Functor/Classes/Eq/Generic.hs
+++ src/Data/Functor/Classes/Eq/Generic.hs
@@ -1,1 +1,9 @@
 module Data.Functor.Classes.Eq.Generic where
+
+import Prologue
+
+class GEq1 f where
+  -- | Lift an equality test through the type constructor.
+  --
+  --   The function will usually be applied to an equality function, but the more general type ensures that the implementation uses it to compare elements of the first container with elements of the second.
+  gliftEq :: (a -> b -> Bool) -> f a -> f b -> Bool
