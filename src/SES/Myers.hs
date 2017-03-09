acf68d4a4336465a64d6b9d53748b65f57b296a7
Rob Rix <rob.rix@me.com>
2017-03-09 10:17:43 -0500

    Stub in a constructor for the furthest-reaching D-path in a given diagonal.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -6,7 +6,9 @@ import Prologue
 
 data MyersF a where
   MiddleSnake :: Vector.Vector a -> Vector.Vector a -> MyersF (Snake, EditDistance)
+  FindDPath :: EditDistance -> Diagonal -> MyersF Int
 
 data Snake = Snake { x :: Int, y :: Int, u :: Int, v :: Int }
 
 newtype EditDistance = EditDistance { unEditDistance :: Int }
+newtype Diagonal = Diagonal { unDiagonal :: Int }
