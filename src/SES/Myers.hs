ac8b2865cf632411a9ef4d057519fac95b66a306
Rob Rix <rob.rix@me.com>
2017-03-09 10:12:41 -0500

    Define a MyersF datatype with a single constructor.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE GADTs #-}
 module SES.Myers where
 
+import qualified Data.Vector as Vector
 import Prologue
+
+data MyersF a where
+  MiddleSnake :: Vector.Vector a -> Vector.Vector a -> MyersF Snake
 
 data Snake = Snake { x :: Int, y :: Int, u :: Int, v :: Int }
