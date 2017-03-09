1aebf61954090a6db8b678a3d583727a38a767d0
Rob Rix <rob.rix@me.com>
2017-03-09 10:13:47 -0500

    Report the edit distance alongside the middle snake.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -5,6 +5,8 @@ import qualified Data.Vector as Vector
 import Prologue
 
 data MyersF a where
-  MiddleSnake :: Vector.Vector a -> Vector.Vector a -> MyersF Snake
+  MiddleSnake :: Vector.Vector a -> Vector.Vector a -> MyersF (Snake, EditDistance)
 
 data Snake = Snake { x :: Int, y :: Int, u :: Int, v :: Int }
+
+newtype EditDistance = EditDistance { unEditDistance :: Int }
