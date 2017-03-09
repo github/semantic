aed09c20db1b3406620e1fc9cb8fdb0be3f2281a
Rob Rix <rob.rix@me.com>
2017-03-09 10:20:10 -0500

    Add a type synonym for Myers’ algorithm.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GADTs #-}
 module SES.Myers where
 
+import Control.Monad.Free.Freer
 import Data.These
 import qualified Data.Vector as Vector
 import Prologue
@@ -9,6 +10,8 @@ data MyersF a where
   SES :: [a] -> [a] -> MyersF [These a a]
   MiddleSnake :: Vector.Vector a -> Vector.Vector a -> MyersF (Snake, EditDistance)
   FindDPath :: EditDistance -> Diagonal -> MyersF Int
+
+type Myers = Freer MyersF
 
 data Snake = Snake { x :: Int, y :: Int, u :: Int, v :: Int }
 
