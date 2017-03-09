2eb5c5183d146f5a1c30a8015b726c1f5570a5b2
Rob Rix <rob.rix@me.com>
2017-03-09 10:18:50 -0500

    Stub in a constructor for the top-level SES operation.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE GADTs #-}
 module SES.Myers where
 
+import Data.These
 import qualified Data.Vector as Vector
 import Prologue
 
 data MyersF a where
+  SES :: [a] -> [a] -> MyersF [These a a]
   MiddleSnake :: Vector.Vector a -> Vector.Vector a -> MyersF (Snake, EditDistance)
   FindDPath :: EditDistance -> Diagonal -> MyersF Int
 
