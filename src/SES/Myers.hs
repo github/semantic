abb71497da2d1726247fdeb90b89bb09bfd14192
Rob Rix <rob.rix@me.com>
2017-03-09 12:32:37 -0500

    Add a helper to test whether an integer is odd.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -78,6 +78,9 @@ at v (Diagonal k) = do
 overlaps :: Endpoint -> Endpoint -> Bool
 overlaps (Endpoint x y) (Endpoint u v) = x - y == u - v && x <= u
 
+isOdd :: Integral a => a -> Bool
+isOdd = (== 1) . (`mod` 2)
+
 
 -- Instances
 
