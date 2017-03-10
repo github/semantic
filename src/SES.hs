58c49f4a9bb63958d570a2ecf901643a91c59be3
Timothy Clem <timothy.clem@gmail.com>
2017-03-10 09:54:17 -0800

    Use STArray instead of IntMap

--- src/SES.hs
+++ src/SES.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE Strict #-}
 module SES where
 
-import qualified Data.IntMap as Map
+import Data.Array.MArray
+import Data.Array.ST
 import Data.These
 import Prologue
 
@@ -14,38 +15,42 @@ type Cost term = These term term -> Int
 
 -- | Find the shortest edit script (diff) between two terms given a function to compute the cost.
 ses :: Comparable term -> Cost term -> [term] -> [term] -> [These term term]
-ses canCompare cost as bs = fst <$> evalState diffState Map.empty where
-  diffState = diffAt canCompare cost (0, 0) as bs
+ses canCompare cost as bs = runST $ do
+  array <- newArray (0, indexKey (length as) (length bs) (length bs)) Nothing
+  editScript <- diffAt array (length bs) canCompare cost (0, 0) as bs
+  pure $ fst <$> editScript
 
 -- | Find the shortest edit script between two terms at a given vertex in the edit graph.
-diffAt :: Comparable term -> Cost term -> (Int, Int) -> [term] -> [term] -> State (Map.IntMap [(These term term, Int)]) [(These term term, Int)]
-diffAt canCompare cost (i, j) as bs
+diffAt :: STArray s Int (Maybe [(These term term, Int)]) -> Int -> Comparable term -> Cost term -> (Int, Int) -> [term] -> [term] -> ST s [(These term term, Int)]
+diffAt array lenJ canCompare cost (i, j) as bs
   | (a : as) <- as, (b : bs) <- bs = do
-  cachedDiffs <- get
-  case Map.lookup cantorKey cachedDiffs of
-    Just diffs -> pure diffs
-    Nothing -> do
-      down <- recur (i, succ j) as (b : bs)
-      right <- recur (succ i, j) (a : as) bs
-      nomination <- best <$> if canCompare a b
-        then do
-          diagonal <- recur (succ i, succ j) as bs
-          pure [ delete a down, insert b right, consWithCost cost (These a b) diagonal ]
-        else pure [ delete a down, insert b right ]
-      cachedDiffs' <- get
-      put $ Map.insert cantorKey nomination cachedDiffs'
-      pure nomination
+    el <- readArray array index
+    case el of
+      Just diffs -> pure diffs
+      Nothing -> do
+        down <- recur (i, succ j) as (b : bs)
+        right <- recur (succ i, j) (a : as) bs
+        nomination <- best <$> if canCompare a b
+          then do
+            diagonal <- recur (succ i, succ j) as bs
+            pure [ delete a down, insert b right, consWithCost cost (These a b) diagonal ]
+          else pure [ delete a down, insert b right ]
+        writeArray array index (Just nomination)
+        pure nomination
   | null as = pure $ foldr insert [] bs
   | null bs = pure $ foldr delete [] as
   | otherwise = pure []
   where
-    cantorKey = ((i + j) * (i + j + 1)) `div` 2 + j
+    index = indexKey i j lenJ
     delete = consWithCost cost . This
     insert = consWithCost cost . That
     costOf [] = 0
     costOf ((_, c) : _) = c
     best = minimumBy (comparing costOf)
-    recur = diffAt canCompare cost
+    recur = diffAt array lenJ canCompare cost
+
+indexKey :: Int -> Int -> Int -> Int
+indexKey i j lenJ = j + i * lenJ
 
 -- | Prepend an edit script and the cumulative cost onto the edit script.
 consWithCost :: Cost term -> These term term -> [(These term term, Int)] -> [(These term term, Int)]
