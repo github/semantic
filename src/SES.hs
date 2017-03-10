f11e705e3dac465d3cfadce13e11e10701d9258f
Timothy Clem <timothy.clem@gmail.com>
2017-03-10 13:29:05 -0800

    Use tuples as array index key, fix test failures due to walk ordering

--- src/SES.hs
+++ src/SES.hs
@@ -16,41 +16,37 @@ type Cost term = These term term -> Int
 -- | Find the shortest edit script (diff) between two terms given a function to compute the cost.
 ses :: Comparable term -> Cost term -> [term] -> [term] -> [These term term]
 ses canCompare cost as bs = runST $ do
-  array <- newArray (0, indexKey (length as) (length bs) (length bs)) Nothing
-  editScript <- diffAt array (length bs) canCompare cost (0, 0) as bs
+  array <- newArray ((0, 0), (length bs, length as)) Nothing
+  editScript <- diffAt array canCompare cost (0, 0) as bs
   pure $ fst <$> editScript
 
 -- | Find the shortest edit script between two terms at a given vertex in the edit graph.
-diffAt :: STArray s Int (Maybe [(These term term, Int)]) -> Int -> Comparable term -> Cost term -> (Int, Int) -> [term] -> [term] -> ST s [(These term term, Int)]
-diffAt array lenJ canCompare cost (i, j) as bs
-  | (a : as) <- as, (b : bs) <- bs = do
-  el <- readArray array index
-  case el of
+diffAt :: STArray s (Int, Int) (Maybe [(These term term, Int)]) -> Comparable term -> Cost term -> (Int, Int) -> [term] -> [term] -> ST s [(These term term, Int)]
+diffAt array canCompare cost (i, j) as bs
+  | (a : as') <- as, (b : bs') <- bs = do
+  maybeDiff <- readArray array (i, j)
+  case maybeDiff of
     Just diffs -> pure diffs
     Nothing -> do
-      down <- recur (i, succ j) as (b : bs)
-      right <- recur (succ i, j) (a : as) bs
+      down <- recur (i, succ j) as' bs
+      right <- recur (succ i, j) as bs'
       nomination <- best <$> if canCompare a b
         then do
-          diagonal <- recur (succ i, succ j) as bs
+          diagonal <- recur (succ i, succ j) as' bs'
           pure [ delete a down, insert b right, consWithCost cost (These a b) diagonal ]
         else pure [ delete a down, insert b right ]
-      writeArray array index (Just nomination)
+      writeArray array (i, j) (Just nomination)
       pure nomination
   | null as = pure $ foldr insert [] bs
   | null bs = pure $ foldr delete [] as
   | otherwise = pure []
   where
-    index = indexKey i j lenJ
     delete = consWithCost cost . This
     insert = consWithCost cost . That
     costOf [] = 0
     costOf ((_, c) : _) = c
     best = minimumBy (comparing costOf)
-    recur = diffAt array lenJ canCompare cost
-
-indexKey :: Int -> Int -> Int -> Int
-indexKey i j lenJ = j + i * lenJ
+    recur = diffAt array canCompare cost
 
 -- | Prepend an edit script and the cumulative cost onto the edit script.
 consWithCost :: Cost term -> These term term -> [(These term term, Int)] -> [(These term term, Int)]
