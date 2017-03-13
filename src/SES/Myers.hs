beefe1ac20bff0a804ca7214a84fa96c4d4ef479
Rob Rix <rob.rix@me.com>
2017-03-13 11:33:46 -0400

    Extract the definition of the empty state.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -43,7 +43,7 @@ data Direction = Forward | Reverse
 -- Evaluation
 
 runMyers :: HasCallStack => (a -> a -> Bool) -> Myers a b -> b
-runMyers eq = runAll $ MyersState (Vector.replicate 100 0) (Vector.replicate 100 0)
+runMyers eq = runAll emptyState
   where runAll state step = case runMyersStep eq state step of
           Left a -> a
           Right next -> uncurry runAll next
@@ -182,6 +182,9 @@ getEq = GetEq `Then` return
 -- Implementation details
 
 data MyersState = MyersState { forward :: !(Vector.Vector Int), backward :: !(Vector.Vector Int) }
+
+emptyState :: MyersState
+emptyState = MyersState (Vector.replicate 100 0) (Vector.replicate 100 0)
 
 setForward :: Vector.Vector Int -> Myers a ()
 setForward v = modify (\ s -> s { forward = v })
