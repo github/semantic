9cf176a37d634aff53a7a5293ed964147a5a2228
Rick Winfrey <rewinfrey@github.com>
2017-03-21 14:48:47 -0700

    GitmonClient spec should not run in parallel

--- test/Spec.hs
+++ test/Spec.hs
@@ -19,19 +19,21 @@ import qualified IntegrationSpec
 import Test.Hspec
 
 main :: IO ()
-main = hspec . parallel $ do
-  describe "Alignment" AlignmentSpec.spec
-  describe "Data.Mergeable" Data.Mergeable.Spec.spec
-  describe "Data.RandomWalkSimilarity" Data.RandomWalkSimilarity.Spec.spec
-  describe "Diff" DiffSpec.spec
-  describe "Summary" SummarySpec.spec
-  describe "Interpreter" InterpreterSpec.spec
-  describe "PatchOutput" PatchOutputSpec.spec
-  describe "Range" RangeSpec.spec
-  describe "Source" SourceSpec.spec
-  describe "Term" TermSpec.spec
-  describe "TOC" TOCSpec.spec
-  describe "DiffCommand" DiffCommandSpec.spec
-  describe "ParseCommand" ParseCommandSpec.spec
-  describe "Integration" IntegrationSpec.spec
-  describe "GitmonClient" GitmonClientSpec.spec
+main = do
+  hspec . parallel $ do
+    describe "Alignment" AlignmentSpec.spec
+    describe "Data.Mergeable" Data.Mergeable.Spec.spec
+    describe "Data.RandomWalkSimilarity" Data.RandomWalkSimilarity.Spec.spec
+    describe "Diff" DiffSpec.spec
+    describe "Summary" SummarySpec.spec
+    describe "Interpreter" InterpreterSpec.spec
+    describe "PatchOutput" PatchOutputSpec.spec
+    describe "Range" RangeSpec.spec
+    describe "Source" SourceSpec.spec
+    describe "Term" TermSpec.spec
+    describe "TOC" TOCSpec.spec
+    describe "DiffCommand" DiffCommandSpec.spec
+    describe "ParseCommand" ParseCommandSpec.spec
+    describe "Integration" IntegrationSpec.spec
+
+  hspec $ describe "GitmonClient" GitmonClientSpec.spec
