ce5af997c98a2028f485a77a5e74cda0133b8799
Rob Rix <rob.rix@me.com>
2017-03-22 17:26:05 -0400

    Test that our implementation of Myers’ algorithm is lossless.

--- test/SES/Myers/Spec.hs
+++ test/SES/Myers/Spec.hs
@@ -20,3 +20,6 @@ spec = do
 
     prop "returns all elements individually for disjoint inputs" $
       \ as bs -> length (runMyers (==) (ses (makeEditGraph ((,) 0 <$> as :: [(Int, Char)]) ((,) 1 <$> bs :: [(Int, Char)])))) `shouldBe` length as + length bs
+
+    prop "is lossless w.r.t. both input elements & ordering" $
+      \ as bs -> foldr (\ each (as, bs) -> these (flip (,) bs. (:as)) ((,) as . (:bs)) (\ a b -> (a:as, b:bs)) each) ([], []) (runMyers (==) (ses (makeEditGraph as bs :: EditGraph Char Char))) `shouldBe` (as, bs)
