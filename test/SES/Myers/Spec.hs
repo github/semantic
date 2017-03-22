ec2c0c703c6469494892f4d41833fbd1bcd438d0
Rob Rix <rob.rix@me.com>
2017-03-22 17:20:37 -0400

    Renae the disjoint input test.

--- test/SES/Myers/Spec.hs
+++ test/SES/Myers/Spec.hs
@@ -18,5 +18,5 @@ spec = do
     prop "returns insertions in That" $
       \ bs -> runMyers (==) (ses (makeEditGraph [] bs :: EditGraph Char Char)) `shouldBe` fmap That bs
 
-    prop "returns all elements of disjoint inputs" $
+    prop "returns all elements individually for disjoint inputs" $
       \ as bs -> length (runMyers (==) (ses (makeEditGraph ((,) 0 <$> as :: [(Int, Char)]) ((,) 1 <$> bs :: [(Int, Char)])))) `shouldBe` length as + length bs
