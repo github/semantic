a13c7f009fce7b40a7e3acc2a4b18531c9609154
Rob Rix <rob.rix@me.com>
2017-03-23 14:14:03 -0400

    Correct the type annotations in the tests.

--- test/SES/Myers/Spec.hs
+++ test/SES/Myers/Spec.hs
@@ -10,16 +10,16 @@ spec :: Spec
 spec = do
   describe "ses" $ do
     prop "returns equal lists in These" $
-      \ as -> (ses (==) as as :: EditGraph Char Char) `shouldBe` zipWith These as as
+      \ as -> (ses (==) as as :: EditScript Char Char) `shouldBe` zipWith These as as
 
     prop "returns deletions in This" $
-      \ as -> (ses (==) as [] :: EditGraph Char Char) ses `shouldBe` fmap This as
+      \ as -> (ses (==) as [] :: EditScript Char Char) `shouldBe` fmap This as
 
     prop "returns insertions in That" $
-      \ bs -> (ses (==) [] bs :: EditGraph Char Char) ses `shouldBe` fmap That bs
+      \ bs -> (ses (==) [] bs :: EditScript Char Char) `shouldBe` fmap That bs
 
     prop "returns all elements individually for disjoint inputs" $
       \ as bs -> length (ses (==) ((,) 0 <$> as :: [(Int, Char)]) ((,) 1 <$> bs :: [(Int, Char)])) `shouldBe` length as + length bs
 
     prop "is lossless w.r.t. both input elements & ordering" $
-      \ as bs -> foldr (\ each (as, bs) -> these (flip (,) bs. (:as)) ((,) as . (:bs)) (\ a b -> (a:as, b:bs)) each) ([], []) (ses (==) as bs :: EditGraph Char Char) `shouldBe` (as, bs)
+      \ as bs -> foldr (\ each (as, bs) -> these (flip (,) bs. (:as)) ((,) as . (:bs)) (\ a b -> (a:as, b:bs)) each) ([], []) (ses (==) as bs :: EditScript Char Char) `shouldBe` (as, bs)
