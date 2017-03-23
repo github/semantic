2b1bf43a1e0b735ce0d79e25d32fb3e7670f7be4
Rob Rix <rob.rix@me.com>
2017-03-23 10:05:27 -0400

    Pop a couple of call stacks.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -174,11 +174,11 @@ runLCS (EditGraph as bs)
     return (catMaybes (these (const Nothing) (const Nothing) ((Just .) . (,)) <$> result))
 
 runEditDistance :: HasCallStack => EditGraph a b -> Myers a b Int
-runEditDistance graph = length . filter (these (const True) (const True) (const (const False))) <$> ses graph
+runEditDistance graph = let ?callStack = popCallStack callStack in length . filter (these (const True) (const True) (const (const False))) <$> ses graph
 
 
 runSearchUpToD :: HasCallStack => EditGraph a b -> Distance -> Myers a b (Maybe (EditScript a b, Distance))
-runSearchUpToD graph (Distance d) = for [negate d, negate d + 2 .. d] (searchAlongK graph (Distance d) . Diagonal)
+runSearchUpToD graph (Distance d) = let ?callStack = popCallStack callStack in for [negate d, negate d + 2 .. d] (searchAlongK graph (Distance d) . Diagonal)
 
 
 -- Smart constructors
