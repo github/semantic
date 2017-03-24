4515e581bfe84a2448d0e380ca8c21e00ed6d6e5
joshvera <josh@joshvera.com>
2017-03-24 17:10:03 -0400

    add typescript

---
+++ test/fixtures/typescript/public-field-definition.B.ts
@@ -0,0 +1,13 @@
+class Foo {
+	readonly foo;
+	static readonly foo1;
+	public readonly foo2;
+	public static readonly foo3: Number = 1;
+	private static foo4?: Number = 1;
+	foo5: Number = 1;
+	foo6?: Number = 1;
+	public foo7: Number = 1;
+	protected static readonly foo8: String = "hello";
+	static foo11: Number = 1;
+	foo10 = 2;
+}
