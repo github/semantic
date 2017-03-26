e178120950677465279cf8aa07f0c49465c65e3d
Rob Rix <rob.rix@me.com>
2017-03-26 17:44:27 -0400

    Define If statements.

--- src/Data/Syntax/Statement.hs
+++ src/Data/Syntax/Statement.hs
@@ -1,1 +1,7 @@
 module Data.Syntax.Statement where
+
+import Prologue
+
+-- | Conditional. This must have an else block, which can be filled with some default value when omitted in the source, e.g. 'pure ()' for C-style if-without-else or 'pure Nothing' for Ruby-style, in both cases assuming some appropriate Applicative context into which the If will be lifted.
+data If a = If { condition :: !a, thenBlock :: !a, elseBlock :: !a }
+  deriving (Eq, Show)
