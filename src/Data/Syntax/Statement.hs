574b4eba280a79d7b27447f294aaec0b661d96a6
Rob Rix <rob.rix@me.com>
2017-03-26 18:39:35 -0400

    Define Return and Yield syntaxes.

--- src/Data/Syntax/Statement.hs
+++ src/Data/Syntax/Statement.hs
@@ -5,3 +5,12 @@ import Prologue
 -- | Conditional. This must have an else block, which can be filled with some default value when omitted in the source, e.g. 'pure ()' for C-style if-without-else or 'pure Nothing' for Ruby-style, in both cases assuming some appropriate Applicative context into which the If will be lifted.
 data If a = If { condition :: !a, thenBlock :: !a, elseBlock :: !a }
   deriving (Eq, Show)
+
+
+-- Returns
+
+newtype Return a = Return a
+  deriving (Eq, Show)
+
+newtype Yield a = Yield a
+  deriving (Eq, Show)
