9486b80efc4a3bb6e55d892f4c8692c3c6e4b73b
Rob Rix <rob.rix@me.com>
2017-03-26 12:06:57 -0400

    Define a typeclass to embed/project elements into/out of a Union.

--- src/Data/Functor/Union.hs
+++ src/Data/Functor/Union.hs
@@ -1,7 +1,16 @@
-{-# LANGUAGE DataKinds, GADTs, KindSignatures, TypeOperators #-}
+{-# LANGUAGE DataKinds, GADTs, KindSignatures, MultiParamTypeClasses, TypeOperators #-}
 module Data.Functor.Union where
+
+import Prologue
 
 -- | N-ary union of type constructors.
 data Union (ts :: [* -> *]) (a :: *) where
   Here :: f a -> Union (f ': ts) a
   There :: Union ts a -> Union (f ': ts) a
+
+
+-- Classes
+
+class InUnion (fs :: [* -> *]) (f :: * -> *) where
+  emb :: f a -> Union fs a
+  proj :: Union fs a -> Maybe (f a)
