df3c26f17b6f0ee4259486269d25cc05818fb11f
Rob Rix <rob.rix@me.com>
2017-03-26 12:06:05 -0400

    Define an n-ary union of type-constructors.

--- src/Data/Functor/Union.hs
+++ src/Data/Functor/Union.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE DataKinds, GADTs, KindSignatures, TypeOperators #-}
 module Data.Functor.Union where
+
+-- | N-ary union of type constructors.
+data Union (ts :: [* -> *]) (a :: *) where
+  Here :: f a -> Union (f ': ts) a
+  There :: Union ts a -> Union (f ': ts) a
