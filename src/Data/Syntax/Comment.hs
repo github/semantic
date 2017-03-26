2add56eca4fda774960187a684ce73d3eef03bd0
Rob Rix <rob.rix@me.com>
2017-03-26 15:59:00 -0400

    Note some remaining ontological work.

--- src/Data/Syntax/Comment.hs
+++ src/Data/Syntax/Comment.hs
@@ -5,3 +5,7 @@ import Prologue
 -- | An unnested comment (line or block).
 newtype Comment a = Comment { commentContent :: ByteString }
   deriving (Eq, Show)
+
+-- TODO: nested comment types
+-- TODO: documentation comment types
+-- TODO: literate programming comment types? alternatively, consider those as markup
