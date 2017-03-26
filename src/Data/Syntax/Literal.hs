21e9d7093b5c10fc695275da8db0d6b050343a16
Rob Rix <rob.rix@me.com>
2017-03-26 15:32:48 -0400

    Define (possibly interpolated) string literals.

--- src/Data/Syntax/Literal.hs
+++ src/Data/Syntax/Literal.hs
@@ -1,1 +1,19 @@
+{-# LANGUAGE DataKinds #-}
 module Data.Syntax.Literal where
+
+import Data.Functor.Union
+import Prologue
+
+
+-- Strings, symbols
+
+newtype StringLiteral a = StringLiteral { stringElements :: [Union '[InterpolationElement, TextElement] a] } -- may also wish to include escapes
+  deriving (Eq, Show)
+
+-- | An interpolation element within a string literal.
+newtype InterpolationElement a = InterpolationElement { interpolationBody :: a }
+  deriving (Eq, Show)
+
+-- | A sequence of textual contents within a string literal.
+newtype TextElement a = TextElement { textElementContent :: ByteString }
+  deriving (Eq, Show)
