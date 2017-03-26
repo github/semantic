779256e9341e0b5b7089243084cc09b18c1ef07d
Rob Rix <rob.rix@me.com>
2017-03-26 12:10:36 -0400

    Define undifferentiated leaf/branch types.

--- src/Data/Syntax.hs
+++ src/Data/Syntax.hs
@@ -1,1 +1,12 @@
 module Data.Syntax where
+
+import Prologue
+
+
+-- Undifferentiated
+
+newtype Leaf a = Leaf { leafContent :: ByteString }
+  deriving (Eq, Show)
+
+newtype Branch a = Branch { branchElements :: [a] }
+  deriving (Eq, Show)
