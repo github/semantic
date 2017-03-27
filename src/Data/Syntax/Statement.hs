8e9cc1607dbc21bcae903ad1cbc4f97ceb7c746e
Rob Rix <rob.rix@me.com>
2017-03-26 21:17:02 -0400

    Rename the If syntax selectors.

--- src/Data/Syntax/Statement.hs
+++ src/Data/Syntax/Statement.hs
@@ -4,7 +4,7 @@ module Data.Syntax.Statement where
 import Prologue
 
 -- | Conditional. This must have an else block, which can be filled with some default value when omitted in the source, e.g. 'pure ()' for C-style if-without-else or 'pure Nothing' for Ruby-style, in both cases assuming some appropriate Applicative context into which the If will be lifted.
-data If a = If { condition :: !a, thenBlock :: !a, elseBlock :: !a }
+data If a = If { ifCondition :: !a, ifThenBody :: !a, ifElseBody :: !a }
   deriving (Eq, Show)
 
 
