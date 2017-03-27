4e91c973cf4884567c017c46168318c99e5c26d4
Rob Rix <rob.rix@me.com>
2017-03-27 10:33:03 -0400

    Add a todo about function types.

--- src/Data/Syntax/Declaration.hs
+++ src/Data/Syntax/Declaration.hs
@@ -4,3 +4,5 @@ import Prologue
 
 data Function a = Function { functionName :: !a, functionParameters :: ![a], functionBody :: !a }
   deriving (Eq, Show)
+
+-- TODO: How should we represent function types, where applicable?
