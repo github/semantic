65cd622b932b3b5cec866a69fb982b58ce832ae5
Rob Rix <rob.rix@me.com>
2017-03-27 10:33:15 -0400

    Define method declaration syntax.

--- src/Data/Syntax/Declaration.hs
+++ src/Data/Syntax/Declaration.hs
@@ -6,3 +6,9 @@ data Function a = Function { functionName :: !a, functionParameters :: ![a], fun
   deriving (Eq, Show)
 
 -- TODO: How should we represent function types, where applicable?
+
+data Method a = Method { methodName :: !a, methodParameters :: ![a], methodBody :: !a }
+  deriving (Eq, Show)
+
+-- TODO: Should we replace this with Function and differentiate by context?
+-- TODO: How should we distinguish class/instance methods?
