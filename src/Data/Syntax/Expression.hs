3632215e5697abbcf4759c696a778f36839de1d3
Rob Rix <rob.rix@me.com>
2017-03-29 15:51:47 -0400

    Define syntax for function calls.

--- src/Data/Syntax/Expression.hs
+++ src/Data/Syntax/Expression.hs
@@ -1,1 +1,7 @@
 module Data.Syntax.Expression where
+
+import Prologue
+
+-- | Typical prefix function application, like `f(x)` in many languages, or `f x` in Haskell.
+data Call a = Call { callFunction :: a, callParams :: [a] }
+  deriving (Eq, Show)
