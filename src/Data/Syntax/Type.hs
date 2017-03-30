b7aa2650f6b0b2ad2367e28b4c81856e8d3a4a2e
Rob Rix <rob.rix@me.com>
2017-03-30 14:06:47 -0400

    Define product type syntax.

--- src/Data/Syntax/Type.hs
+++ src/Data/Syntax/Type.hs
@@ -4,3 +4,6 @@ import Prologue
 
 data Annotation a = Annotation { annotationSubject :: !a, annotationType :: !a }
   deriving (Eq, Show)
+
+newtype Product a = Product { productElements :: [a] }
+  deriving (Eq, Show)
