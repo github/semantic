8647329cc75b12c006c090db7855a1c2cda0b265
Rob Rix <rob.rix@me.com>
2017-03-31 12:58:27 -0400

    Define an interpreter for Commands.

--- src/Command.hs
+++ src/Command.hs
@@ -13,3 +13,9 @@ data CommandF f where
   ReadFile :: FilePath -> CommandF SourceBlob
 
 type Command = Freer CommandF
+
+runCommand :: Command a -> IO a
+runCommand = iterFreerA $ \ command yield -> case command of
+  ReadFile path -> do
+    source <- readAndTranscodeFile path
+    yield (sourceBlob source path)
