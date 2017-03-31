f2a699edfd126d45cc546e6e1d084451fafe6f70
Rob Rix <rob.rix@me.com>
2017-03-31 14:28:36 -0400

    Add a ReadFilesAtSHAs instruction.

--- src/Command.hs
+++ src/Command.hs
@@ -6,11 +6,33 @@ module Command
 import Command.Diff as C
 import Command.Parse as C
 import Control.Monad.Free.Freer
+import Data.String
+import Debug.Trace (traceEventIO)
+import qualified Git
+import Git.Blob
+import Git.Libgit2
+import Git.Repository
+import Git.Types
+import GitmonClient
 import Prologue
 import Source
 
 data CommandF f where
   ReadFile :: FilePath -> CommandF SourceBlob
+  ReadFilesAtSHAs :: FilePath -> [FilePath] -> String -> String -> CommandF [(SourceBlob, SourceBlob)]
+
+
+  -- read a list of files from git
+  -- read the list of files changed between a pair of SHAs
+  -- parse source in some language
+  -- diff a pair of terms
+  -- render a term
+  -- render a diff
+
+  -- parallelize diffs of a list of paths + git shas
+  -- explicit gitmon effects/events
+  -- tracing events (traceEventIO & friends) for event logs
+  -- alternateObjectDirs??
 
 type Command = Freer CommandF
 
@@ -19,3 +41,44 @@ runCommand = iterFreerA $ \ command yield -> case command of
   ReadFile path -> do
     source <- readAndTranscodeFile path
     yield (sourceBlob source path)
+  ReadFilesAtSHAs gitDir paths sha1 sha2 -> do
+    blobs <- withRepository lgFactory gitDir $ do
+      -- repo <- getRepository
+      -- for_ alternateObjectDirs (liftIO . odbBackendAddPath repo . toS)
+
+      liftIO $ traceEventIO ("START readFilesAtSHAs: " <> show paths)
+
+      tree1 <- treeForSha sha1
+      tree2 <- treeForSha sha2
+
+      blobs <- for paths $ \ path -> (,) <$> blobForPathInTree path tree1 <*> blobForPathInTree path tree2
+
+      -- a <- blobsForSha sha1
+      -- b <- blobsForSha sha2
+
+      -- let _ = (a \\ b) <> (b \\ a)
+
+      liftIO $! traceEventIO ("END readFilesAtSHAs: " <> show paths)
+      pure blobs
+    yield blobs
+
+  where treeForSha sha = do
+          obj <- parseObjOid (toS sha)
+          commit <- reportGitmon "cat-file" $ lookupCommit obj
+          reportGitmon "cat-file" $ lookupTree (commitTree commit)
+        blobForPathInTree path tree = do
+          entry <- reportGitmon "ls-tree" $ treeEntry tree (toS path)
+          case entry of
+            Just (BlobEntry entryOid entryKind) -> do
+              blob <- reportGitmon "cat-file" $ lookupBlob entryOid
+              contents <- blobToByteString blob
+              transcoded <- liftIO $ transcode contents
+              let oid = renderObjOid $ blobOid blob
+              pure $! SourceBlob transcoded (toS oid) path (Just (toSourceKind entryKind))
+            _ -> pure $! emptySourceBlob path
+        blobsForSha sha = do
+          tree <- treeForSha sha
+          reportGitmon "ls-tree" $ treeBlobEntries tree
+        toSourceKind (Git.PlainBlob mode) = Source.PlainBlob mode
+        toSourceKind (Git.ExecutableBlob mode) = Source.ExecutableBlob mode
+        toSourceKind (Git.SymlinkBlob mode) = Source.SymlinkBlob mode
