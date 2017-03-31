cd8e082e6c857db3ad9bb0a00d0bd10c789614ed
Rob Rix <rob.rix@me.com>
2017-03-31 12:58:17 -0400

    Define a Command type.

--- src/Command.hs
+++ src/Command.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE GADTs #-}
 module Command
 ( module C
 ) where
 
 import Command.Diff as C
 import Command.Parse as C
+import Control.Monad.Free.Freer
+import Prologue
+import Source
+
+data CommandF f where
+  ReadFile :: FilePath -> CommandF SourceBlob
+
+type Command = Freer CommandF
