91f85d3dfd03c9e52fe068d7f95e79652f9c5dec
Rob Rix <rob.rix@me.com>
2017-04-05 14:26:55 -0400

    Stub in assignment onto Ruby programs.

--- src/Language/Ruby/Syntax.hs
+++ src/Language/Ruby/Syntax.hs
@@ -1,12 +1,15 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, GADTs #-}
 module Language.Ruby.Syntax where
 
+import Control.Monad.Free.Freer
 import Data.Functor.Union
 import qualified Data.Syntax.Comment as Comment
 import qualified Data.Syntax.Declaration as Declaration
 import qualified Data.Syntax.Literal as Literal
 import qualified Data.Syntax.Statement as Statement
+import Prologue
 
+-- | The type of Ruby syntax.
 type Ruby = Union
   '[Comment.Comment
   , Declaration.Class
@@ -16,3 +19,28 @@ type Ruby = Union
   , Statement.Return
   , Statement.Yield
   ]
+
+-- | Parse a node with the specified symbol. Produces a list of subnodes.
+rule :: symbol -> Assignment symbol [a]
+rule sym = Rule sym `Then` return
+
+
+data AssignmentF symbol a where
+  Rule :: symbol -> AssignmentF symbol [a]
+
+-- | Assignment from an AST with some set of 'symbol's onto some other value.
+--
+--   This is essentially a parser.
+type Assignment symbol = Freer (AssignmentF symbol)
+
+
+-- | A program in some syntax functor, over which we can perform analyses.
+type Program = Freer
+
+
+-- | Statically-known rules corresponding to symbols in the grammar.
+data Rule = ProgramRule | UninterpretedRule
+
+-- | Assignment onto a program in Ruby’s syntax.
+assignment :: Assignment Rule (Program Ruby (Maybe ()))
+assignment = foldr (>>) (return Nothing) <$> rule ProgramRule
