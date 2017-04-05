7e09170877b63f84ef90e42e46b57eac6e56ccc1
joshvera <josh@joshvera.com>
2017-04-05 11:38:51 -0400

    Add some state and run functions

--- src/Data/RWS.hs
+++ src/Data/RWS.hs
@@ -2,6 +2,7 @@
 module Data.RWS (rws) where
 
 import Prologue
+import Control.Monad.Free.Freer
 import Data.Record
 import Data.Align.Generic
 import Data.These
@@ -18,8 +19,27 @@ rws :: (GAlign f, Traversable f, Eq1 f, HasField fields Category, HasField field
     -> [These (Term f (Record fields)) (Term f (Record fields))] -- ^ The resulting list of similarity-matched diffs.
 rws editDistance canCompare as bs = undefined
 
+
+
 data RwsF a b result where
-    RWS :: RwsF a b (EditScript a b)
+  RWS :: RwsF a b (EditScript a b)
+  SES :: RwsF a b (EditScript a b)
+
+data EditGraph a b = EditGraph { as :: !(Array Int a), bs :: !(Array Int b) }
+  deriving (Eq, Show)
+
+data Step a b result where
+  M :: HasCallStack => RwsF a b c -> Step a b c
+  S :: State (RwsState a b) c -> Step a b c
+
+newtype RwsState a b = RwsState { unRwsState :: (Int, a, b) }
+
+type Rws a b = Freer (Step a b)
+
+runRWS :: HasCallStack => EditGraph a b -> Rws a b (EditScript a b)
+runRWS (EditGraph as bs)
+  | null as = return $ That . eraseFeatureVector <$> toList bs
+  | null bs = return $ This . eraseFeatureVector <$> toList as
 
 type FeatureVector = Array Int Double
 
