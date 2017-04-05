9ec60d5ff42029ed4bf179198c9e56e812452586
Rob Rix <rob.rix@me.com>
2017-04-05 11:23:13 -0400

    Test that readFilesAtSHAs will fetch all paths if none are specified.

--- test/Command/Spec.hs
+++ test/Command/Spec.hs
@@ -1,6 +1,8 @@
 module Command.Spec where
 
 import Command
+import Data.Bifunctor
+import Data.Functor.Both
 import Prologue hiding (readFile)
 import Source
 import Test.Hspec
@@ -15,3 +17,9 @@ spec = parallel $ do
     it "returns Nothing for absent files" $ do
       blob <- runCommand (readFile "this file should not exist")
       blob `shouldBe` Nothing
+
+  describe "readFilesAtSHAs" $ do
+    let repoPath = "test/fixtures/git/examples/all-languages.git"
+    it "returns blobs for all paths if none are specified" $ do
+      blobs <- runCommand (readFilesAtSHAs repoPath [] [] (both "dfac8fd681b0749af137aebf3203e77a06fbafc2" "2e4144eb8c44f007463ec34cb66353f0041161fe"))
+      blobs `shouldBe` [("methods.rb", both Nothing (Just (SourceBlob (Source "def foo\nend\n") "ff7bbbe9495f61d9e1e58c597502d152bab1761e" "methods.rb" (Just defaultPlainBlob))))]
