c56ac0d0f98c077c6fc0e20e6e0f8aeb088a800f
Rob Rix <rob.rix@me.com>
2017-04-05 14:01:36 -0400

    Extend the Ruby syntax definition a little.

--- src/Language/Ruby/Syntax.hs
+++ src/Language/Ruby/Syntax.hs
@@ -2,5 +2,17 @@
 module Language.Ruby.Syntax where
 
 import Data.Functor.Union
+import qualified Data.Syntax.Comment as Comment
+import qualified Data.Syntax.Declaration as Declaration
+import qualified Data.Syntax.Literal as Literal
+import qualified Data.Syntax.Statement as Statement
 
-type Ruby = Union '[]
+type Ruby = Union
+  '[Comment.Comment
+  , Declaration.Class
+  , Declaration.Method
+  , Literal.Boolean
+  , Statement.If
+  , Statement.Return
+  , Statement.Yield
+  ]
