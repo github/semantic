7d6c92dd3432fe742baf2635acbe05c7143b081f
Rob Rix <rob.rix@me.com>
2017-04-05 11:33:21 -0400

    Extract the expected state into a fixture type.

--- test/Command/Spec.hs
+++ test/Command/Spec.hs
@@ -3,8 +3,10 @@ module Command.Spec where
 import Command
 import Data.Bifunctor
 import Data.Functor.Both
+import Data.String
 import Prologue hiding (readFile)
 import Source
+import System.FilePath
 import Test.Hspec
 
 spec :: Spec
@@ -20,6 +22,12 @@ spec = parallel $ do
 
   describe "readFilesAtSHAs" $ do
     let repoPath = "test/fixtures/git/examples/all-languages.git"
+    let methodsFixture = Fixture
+          (both "dfac8fd681b0749af137aebf3203e77a06fbafc2" "2e4144eb8c44f007463ec34cb66353f0041161fe")
+          [ ("methods.rb", both Nothing (Just (SourceBlob (Source "def foo\nend\n") "ff7bbbe9495f61d9e1e58c597502d152bab1761e" "methods.rb" (Just defaultPlainBlob)))) ]
+
     it "returns blobs for all paths if none are specified" $ do
-      blobs <- runCommand (readFilesAtSHAs repoPath [] [] (both "dfac8fd681b0749af137aebf3203e77a06fbafc2" "2e4144eb8c44f007463ec34cb66353f0041161fe"))
-      blobs `shouldBe` [("methods.rb", both Nothing (Just (SourceBlob (Source "def foo\nend\n") "ff7bbbe9495f61d9e1e58c597502d152bab1761e" "methods.rb" (Just defaultPlainBlob))))]
+      blobs <- runCommand (readFilesAtSHAs repoPath [] [] (shas methodsFixture))
+      blobs `shouldBe` expectedBlobs methodsFixture
+
+data Fixture = Fixture { shas :: Both String, expectedBlobs :: [(FilePath, Both (Maybe SourceBlob))] }
