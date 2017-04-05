00637369248e3734217ddd71b301f52a6f3a5996
Rob Rix <rob.rix@me.com>
2017-04-05 11:37:09 -0400

    Move the fixtures into the where clause.

--- test/Command/Spec.hs
+++ test/Command/Spec.hs
@@ -21,11 +21,6 @@ spec = parallel $ do
       blob `shouldBe` Nothing
 
   describe "readFilesAtSHAs" $ do
-    let repoPath = "test/fixtures/git/examples/all-languages.git"
-    let methodsFixture = Fixture
-          (both "dfac8fd681b0749af137aebf3203e77a06fbafc2" "2e4144eb8c44f007463ec34cb66353f0041161fe")
-          [ ("methods.rb", both Nothing (Just (SourceBlob (Source "def foo\nend\n") "ff7bbbe9495f61d9e1e58c597502d152bab1761e" "methods.rb" (Just defaultPlainBlob)))) ]
-
     it "returns blobs for the specified paths" $ do
       blobs <- runCommand (readFilesAtSHAs repoPath [] ["methods.rb"] (shas methodsFixture))
       blobs `shouldBe` expectedBlobs methodsFixture
@@ -37,5 +32,11 @@ spec = parallel $ do
     it "returns entries for missing paths" $ do
       blobs <- runCommand (readFilesAtSHAs repoPath [] ["this file should not exist"] (shas methodsFixture))
       blobs `shouldBe` [("this file should not exist", pure Nothing)]
+
+  where repoPath = "test/fixtures/git/examples/all-languages.git"
+        methodsFixture = Fixture
+          (both "dfac8fd681b0749af137aebf3203e77a06fbafc2" "2e4144eb8c44f007463ec34cb66353f0041161fe")
+          [ ("methods.rb", both Nothing (Just methodsBlob)) ]
+        methodsBlob = SourceBlob (Source "def foo\nend\n") "ff7bbbe9495f61d9e1e58c597502d152bab1761e" "methods.rb" (Just defaultPlainBlob)
 
 data Fixture = Fixture { shas :: Both String, expectedBlobs :: [(FilePath, Both (Maybe SourceBlob))] }
