ecdb6a1d59716dd593a9eb9cfd3a85f827fd5716
Rob Rix <rob.rix@me.com>
2017-04-06 13:36:54 -0400

    Test that we can match a comment.

--- test/Language/Ruby/Syntax/Spec.hs
+++ test/Language/Ruby/Syntax/Spec.hs
@@ -1,7 +1,13 @@
 module Language.Ruby.Syntax.Spec where
 
+import Data.Functor.Union
+import qualified Data.Syntax.Comment as Comment
+import Language.Ruby.Syntax
 import Prologue
 import Test.Hspec
 
 spec :: Spec
-spec = pure ()
+spec = do
+  describe "stepAssignment" $ do
+    it "should match a comment" $ do
+      stepAssignment comment [Rose (Node Comment "hello") []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
