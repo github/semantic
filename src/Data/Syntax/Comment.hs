5166900702d192406b830a2725a6f53de81fbabd
Rob Rix <rob.rix@me.com>
2017-04-06 10:39:22 -0400

    Define an Eq1 instance for Comment.

--- src/Data/Syntax/Comment.hs
+++ src/Data/Syntax/Comment.hs
@@ -1,10 +1,14 @@
 module Data.Syntax.Comment where
 
+import Data.Functor.Classes.Eq.Generic
+import GHC.Generics
 import Prologue
 
 -- | An unnested comment (line or block).
 newtype Comment a = Comment { commentContent :: ByteString }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 Comment where liftEq = genericLiftEq
 
 -- TODO: nested comment types
 -- TODO: documentation comment types
