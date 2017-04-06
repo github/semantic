30ffbe64210c8830e11fff4344102e58df75fa90
Rob Rix <rob.rix@me.com>
2017-04-06 11:21:35 -0400

    Define Generic1 & Eq1 instances for literals.

--- src/Data/Syntax/Literal.hs
+++ src/Data/Syntax/Literal.hs
@@ -1,20 +1,27 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, DeriveAnyClass, DeriveGeneric #-}
 module Data.Syntax.Literal where
 
+import Data.Functor.Classes.Eq.Generic
 import Data.Syntax.Comment
 import Data.Functor.Union
+import GHC.Generics
 import Prologue
 
 -- Boolean
 
 newtype Boolean a = Boolean Bool
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 Boolean where liftEq = genericLiftEq
 
 
 -- Numeric
 
 -- | A literal integer of unspecified width. No particular base is implied.
 newtype Integer a = Integer { integerContent :: ByteString }
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 Data.Syntax.Literal.Integer where liftEq = genericLiftEq
 
 -- TODO: Should IntegerLiteral hold an Integer instead of a ByteString?
 -- TODO: Do we care about differentiating between hex/octal/decimal/binary integer literals?
@@ -24,21 +31,30 @@ newtype Integer a = Integer { integerContent :: ByteString }
 -- Strings, symbols
 
 newtype String a = String { stringElements :: [Union '[InterpolationElement, TextElement] a] }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 String where liftEq = genericLiftEq
 
 -- TODO: Should string literal bodies include escapes too?
 
 -- | An interpolation element within a string literal.
 newtype InterpolationElement a = InterpolationElement { interpolationBody :: a }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 InterpolationElement where liftEq = genericLiftEq
+
 
 -- | A sequence of textual contents within a string literal.
 newtype TextElement a = TextElement { textElementContent :: ByteString }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 TextElement where liftEq = genericLiftEq
 
 
 newtype Symbol a = SymbolLiteral { symbolContent :: ByteString }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 Symbol where liftEq = genericLiftEq
 
 -- TODO: Character literals.
 
@@ -46,14 +62,21 @@ newtype Symbol a = SymbolLiteral { symbolContent :: ByteString }
 -- Collections
 
 newtype ArrayLiteral a = ArrayLiteral { arrayElements :: [Union '[Identity, Comment] a] }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 ArrayLiteral where liftEq = genericLiftEq
+
 
 newtype HashLiteral a = HashLiteral { hashElements :: [Union '[KeyValue, Comment] a] }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 HashLiteral where liftEq = genericLiftEq
+
 
 data KeyValue a = KeyValue { key :: !a, value :: !a }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
 
+instance Eq1 KeyValue where liftEq = genericLiftEq
 
 -- TODO: Object literals as distinct from hash literals? Or coalesce object/hash literals into “key-value literals”?
 -- TODO: Function literals (lambdas, procs, anonymous functions, what have you).
