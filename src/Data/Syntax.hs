828c7da0015b79ac4a837ac24bc8c3fcb433dc57
Rob Rix <rob.rix@me.com>
2017-04-06 10:40:49 -0400

    Define Eq1 instances for Leaf, Branch, and Identifier.

--- src/Data/Syntax.hs
+++ src/Data/Syntax.hs
@@ -1,18 +1,26 @@
 module Data.Syntax where
 
+import Data.Functor.Classes.Eq.Generic
+import GHC.Generics
 import Prologue
 
 -- Undifferentiated
 
 newtype Leaf a = Leaf { leafContent :: ByteString }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 Leaf where liftEq = genericLiftEq
 
 newtype Branch a = Branch { branchElements :: [a] }
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 Branch where liftEq = genericLiftEq
 
 
 -- Common
 
 -- | An identifier of some other construct, whether a containing declaration (e.g. a class name) or a reference (e.g. a variable).
 newtype Identifier a = Identifier ByteString
-  deriving (Eq, Show)
+  deriving (Eq, Generic1, Show)
+
+instance Eq1 Identifier where liftEq = genericLiftEq
