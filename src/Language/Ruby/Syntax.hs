b2d5e7eac19e4ae24ce0366ea72ef8d80d3867a9
Rob Rix <rob.rix@me.com>
2017-04-06 09:43:12 -0400

    Take a stab at assigning method declarations.

--- src/Language/Ruby/Syntax.hs
+++ src/Language/Ruby/Syntax.hs
@@ -52,15 +52,18 @@ type Program = Freer
 
 
 -- | Statically-known rules corresponding to symbols in the grammar.
-data Grammar = Program | Uninterpreted | BeginBlock | EndBlock | Undef | Alias | Comment | True' | False' | Return | Yield | Break | Next | Redo | Retry | IfModifier | UnlessModifier | WhileModifier | UntilModifier | RescueModifier | While | Until | For | Do | Case | When | Pattern | If | Unless | Elsif | Else | Begin | Ensure | Rescue | Exceptions | ExceptionVariable | ElementReference | ScopeResolution | Call | MethodCall | ArgumentList | ArgumentListWithParens | SplatArgument | HashSplatArgument | BlockArgument | Class | Constant
+data Grammar = Program | Uninterpreted | BeginBlock | EndBlock | Undef | Alias | Comment | True' | False' | Return | Yield | Break | Next | Redo | Retry | IfModifier | UnlessModifier | WhileModifier | UntilModifier | RescueModifier | While | Until | For | Do | Case | When | Pattern | If | Unless | Elsif | Else | Begin | Ensure | Rescue | Exceptions | ExceptionVariable | ElementReference | ScopeResolution | Call | MethodCall | ArgumentList | ArgumentListWithParens | SplatArgument | HashSplatArgument | BlockArgument | Class | Constant | Method | Identifier
   deriving (Enum, Eq, Ord, Show)
 
 -- | Assignment from AST in Ruby’s grammar onto a program in Ruby’s syntax.
 assignment :: Assignment Grammar (Program Syntax (Maybe a))
 assignment = foldr (>>) (pure Nothing) <$> rule Program (children declaration)
-  where declaration = comment <|> class'
+  where declaration = comment <|> class' <|> method
         class' = rule Class (wrapU <$> (Declaration.Class <$> constant <*> pure [] <*> declaration))
         constant = rule Constant (wrapU <$> (Syntax.Identifier <$> content))
+        identifier = rule Identifier (wrapU <$> (Syntax.Identifier <$> content))
+        method = rule Method (wrapU <$> (Declaration.Method <$> identifier <*> pure [] <*> statement))
+        statement = expr
 
 comment :: Assignment Grammar (Program Syntax a)
 comment = wrapU . Comment.Comment <$> (rule Comment content)
