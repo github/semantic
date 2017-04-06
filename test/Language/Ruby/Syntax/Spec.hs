fe55642e6ba79bf8e1c5f36a483754a758e8c24a
Rob Rix <rob.rix@me.com>
2017-04-06 14:25:21 -0400

    Test that multiple alternatives are matched against.

--- test/Language/Ruby/Syntax/Spec.hs
+++ test/Language/Ruby/Syntax/Spec.hs
@@ -11,3 +11,6 @@ spec = do
   describe "stepAssignment" $ do
     it "matches nodes" $ do
       stepAssignment comment [Rose (Node Comment "hello") []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
+
+    it "attempts multiple alternatives" $ do
+      stepAssignment (if' <|> comment) [Rose (Node Comment "hello") []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
