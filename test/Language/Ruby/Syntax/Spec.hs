c44fb8a5c3605b5e1faefba2118ff291995c0a40
Rob Rix <rob.rix@me.com>
2017-04-07 09:16:57 -0400

    Generalize the fixture builder.

--- test/Language/Ruby/Syntax/Spec.hs
+++ test/Language/Ruby/Syntax/Spec.hs
@@ -10,13 +10,13 @@ spec :: Spec
 spec = do
   describe "stepAssignment" $ do
     it "matches nodes" $ do
-      stepAssignment comment [makeCommentAST "hello"] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
+      stepAssignment comment [ast Comment "hello" []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
 
     it "attempts multiple alternatives" $ do
-      stepAssignment (if' <|> comment) [makeCommentAST "hello"] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
+      stepAssignment (if' <|> comment) [ast Comment "hello" []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
 
     it "matches in sequence" $ do
-      stepAssignment ((,) <$> comment <*> comment) [makeCommentAST "hello", makeCommentAST "world"] `shouldBe` Just ([], (wrapU (Comment.Comment "hello"), wrapU (Comment.Comment "world")) :: (Program Syntax (), Program Syntax ()))
+      stepAssignment ((,) <$> comment <*> comment) [ast Comment "hello" [], ast Comment "world" []] `shouldBe` Just ([], (wrapU (Comment.Comment "hello"), wrapU (Comment.Comment "world")) :: (Program Syntax (), Program Syntax ()))
 
-makeCommentAST :: ByteString -> AST Grammar
-makeCommentAST s = Rose (Node Comment s) []
+ast :: Grammar -> ByteString -> [AST Grammar] -> AST Grammar
+ast g s c = Rose (Node g s) c
