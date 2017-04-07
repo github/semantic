ad0fa60a3e658553737ee1efc840ea47c1c815fa
Rob Rix <rob.rix@me.com>
2017-04-07 09:45:23 -0400

    Placate hlint.

--- test/Language/Ruby/Syntax/Spec.hs
+++ test/Language/Ruby/Syntax/Spec.hs
@@ -9,16 +9,16 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "stepAssignment" $ do
-    it "matches nodes" $ do
+    it "matches nodes" $
       stepAssignment comment [ast Comment "hello" []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
 
-    it "attempts multiple alternatives" $ do
+    it "attempts multiple alternatives" $
       stepAssignment (if' <|> comment) [ast Comment "hello" []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
 
-    it "matches in sequence" $ do
+    it "matches in sequence" $
       stepAssignment ((,) <$> comment <*> comment) [ast Comment "hello" [], ast Comment "world" []] `shouldBe` Just ([], (wrapU (Comment.Comment "hello"), wrapU (Comment.Comment "world")) :: (Program Syntax (), Program Syntax ()))
 
-    it "matches repetitions" $ do
+    it "matches repetitions" $
       stepAssignment (many comment) [ast Comment "colourless" [], ast Comment "green" [], ast Comment "ideas" [], ast Comment "sleep" [], ast Comment "furiously" []] `shouldBe` Just ([], [wrapU (Comment.Comment "colourless"), wrapU (Comment.Comment "green"), wrapU (Comment.Comment "ideas"), wrapU (Comment.Comment "sleep"), wrapU (Comment.Comment "furiously")] :: [Program Syntax ()])
 
 ast :: Grammar -> ByteString -> [AST Grammar] -> AST Grammar
