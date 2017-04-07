830c8f8c94176ba2a23d417b796e10f821c1e4a8
Rob Rix <rob.rix@me.com>
2017-04-07 09:14:03 -0400

    Use a helper to construct the expected AST values.

--- test/Language/Ruby/Syntax/Spec.hs
+++ test/Language/Ruby/Syntax/Spec.hs
@@ -10,10 +10,13 @@ spec :: Spec
 spec = do
   describe "stepAssignment" $ do
     it "matches nodes" $ do
-      stepAssignment comment [Rose (Node Comment "hello") []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
+      stepAssignment comment [makeCommentAST "hello"] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
 
     it "attempts multiple alternatives" $ do
-      stepAssignment (if' <|> comment) [Rose (Node Comment "hello") []] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
+      stepAssignment (if' <|> comment) [makeCommentAST "hello"] `shouldBe` Just ([], wrapU (Comment.Comment "hello") :: Program Syntax ())
 
     it "matches in sequence" $ do
-      stepAssignment ((,) <$> comment <*> comment) [Rose (Node Comment "hello") [], Rose (Node Comment "world") []] `shouldBe` Just ([], (wrapU (Comment.Comment "hello"), wrapU (Comment.Comment "world")) :: (Program Syntax (), Program Syntax ()))
+      stepAssignment ((,) <$> comment <*> comment) [makeCommentAST "hello", makeCommentAST "world"] `shouldBe` Just ([], (wrapU (Comment.Comment "hello"), wrapU (Comment.Comment "world")) :: (Program Syntax (), Program Syntax ()))
+
+makeCommentAST :: ByteString -> AST Grammar
+makeCommentAST s = Rose (Node Comment s) []
