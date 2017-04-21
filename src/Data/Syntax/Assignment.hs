27db847b061881a251f9f9ca38c4b329bda3da15
Rob Rix <rob.rix@me.com>
2017-04-21 13:27:44 -0400

    Rename node to subtree.

--- src/Data/Syntax/Assignment.hs
+++ src/Data/Syntax/Assignment.hs
@@ -102,13 +102,13 @@ runAssignment :: (Symbol grammar, Eq grammar, Show grammar) => Assignment (Node 
 runAssignment = iterFreer (\ assignment yield offset source nodes -> case (assignment, dropAnonymous nodes) of
   -- Nullability: some rules, e.g. 'pure a' and 'many a', should match at the end of input. Either side of an alternation may be nullable, ergo Alt can match at the end of input.
   (Alt a b, nodes) -> yield a offset source nodes <|> yield b offset source nodes -- FIXME: Symbol `Alt` Symbol `Alt` Symbol is inefficient, should build and match against an IntMap instead.
-  (assignment, node@(Rose (nodeSymbol :. range :. sourceSpan :. Nil) children) : rest) -> case assignment of
+  (assignment, subtree@(Rose (nodeSymbol :. range :. sourceSpan :. Nil) children) : rest) -> case assignment of
     Get -> yield (nodeSymbol :. range :. sourceSpan :. Nil) offset source nodes
     Source -> yield "" offset source rest
     Children childAssignment -> do
       c <- assignAllFrom childAssignment offset source children
       yield c offset source rest
-    _ -> Error ["No rule to match " <> show node]
+    _ -> Error ["No rule to match " <> show subtree]
   (Get, []) -> Error [ "Expected node but got end of input." ]
   (Source, []) -> Error [ "Expected leaf node but got end of input." ]
   (Children _, []) -> Error [ "Expected branch node but got end of input." ]
