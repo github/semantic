0991f0c8c1c3083f5b9b2d1b20868b4c9272c5d5
Rob Rix <rob.rix@me.com>
2017-04-21 12:17:45 -0400

    Revert "Give a default definition for <&> in terms of liftC2."
    This reverts commit 0a6bfa25ea2801aa7b7c2c19f1fcabf63bddaccf.

--- src/Control/Conjunctive.hs
+++ src/Control/Conjunctive.hs
@@ -6,7 +6,6 @@ infixl 4 <&>
 
 class Alternative f => Conjunctive f where
   (<&>) :: f (a -> b) -> f a -> f b
-  f <&> a = liftC2 ($) f a
 
   liftC2 :: (a -> b -> c) -> f a -> f b -> f c
   liftC2 f a b = f <$> a <*> b
