dd2ee0a669ae2e7945fdc78c41b9b79a09eb3cf4
Rob Rix <rob.rix@me.com>
2017-05-02 11:33:51 -0400

    Use the `leaf` helper to assign symbols.

--- src/Language/Ruby/Syntax.hs
+++ src/Language/Ruby/Syntax.hs
@@ -111,7 +111,7 @@ literal :: Assignment (Node Grammar) (Term Syntax Location)
 literal  =  leaf Language.Ruby.Syntax.True (const Literal.true)
         <|> leaf Language.Ruby.Syntax.False (const Literal.false)
         <|> leaf Language.Ruby.Syntax.Integer Literal.Integer
-        <|> symbol Symbol *> term <*> (Literal.Symbol <$> source)
+        <|> leaf Symbol Literal.Symbol
         <|> symbol Range *> term <*> children (Literal.Range <$> statement <*> statement) -- FIXME: represent the difference between .. and ...
 
 -- | Assignment of the current node’s annotation.
