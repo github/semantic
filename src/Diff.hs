4fe2a4cec154b9ec631a3f9cf88ac26cb34d27d7
Rob Rix <rob.rix@me.com>
2017-05-08 11:53:23 -0400

    Simplify mapAnnotations with the Bifunctor instance.

--- src/Diff.hs
+++ src/Diff.hs
@@ -44,7 +44,7 @@ mapAnnotations :: (Functor f, Functor g)
                => (annotation -> annotation')
                -> Free (TermF f (g annotation))  (Patch (Term f annotation))
                -> Free (TermF f (g annotation')) (Patch (Term f annotation'))
-mapAnnotations f = iter (\ (h :< functor) -> wrap (fmap f h :< functor)) . fmap (pure . fmap (fmap f))
+mapAnnotations f = iter (wrap . first (fmap f)) . fmap (pure . fmap (fmap f))
 
 -- | Map a function over the annotations of a single diff node, if it is in Free.
 modifyAnnotations :: (Functor f, Functor g) => (annotation -> annotation) -> Free (TermF f (g annotation)) a -> Free (TermF f (g annotation)) a
