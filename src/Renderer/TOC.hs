ca14a5ef90dfab924d0b59226e3c2673656ef37c
Rob Rix <rob.rix@me.com>
2017-05-10 11:56:06 -0400

    Reformat the exports list.

--- src/Renderer/TOC.hs
+++ src/Renderer/TOC.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE RankNTypes #-}
-module Renderer.TOC (toc, diffTOC, JSONSummary(..), Summarizable(..), isErrorSummary, tableOfContentsBy) where
+module Renderer.TOC
+( toc
+, diffTOC
+, JSONSummary(..)
+, Summarizable(..)
+, isErrorSummary
+, tableOfContentsBy
+) where
 
 import Category as C
 import Data.Aeson
