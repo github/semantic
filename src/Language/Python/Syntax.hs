873ddd3d51e13068f0a815e2ff5f5614970f19e7
Rick Winfrey <rewinfrey@github.com>
2017-05-12 09:44:53 -0700

    Add Python syntax boiler plate

--- src/Language/Python/Syntax.hs
+++ src/Language/Python/Syntax.hs
@@ -1,1 +1,9 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Language.Python.Syntax where
+
+import Language.Haskell.TH
+import Text.Parser.TreeSitter.Python
+import Text.Parser.TreeSitter.Language
+
+-- | Statically-known rules corresponding to symbols in the grammar.
+tmkSymbolDatatypet (mkName "Grammar") tree_sitter_python
