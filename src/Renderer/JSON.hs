23e04cdf60c3f2bb9d9cec87752aea82852e0207
Rob Rix <rob.rix@me.com>
2017-05-16 13:27:46 -0400

    Sort the extensions.

--- src/Renderer/JSON.hs
+++ src/Renderer/JSON.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, DataKinds, GADTs, ScopedTypeVariables, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DataKinds, GADTs, MultiParamTypeClasses, ScopedTypeVariables, TypeFamilies, TypeOperators, UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Renderer.JSON
 ( json
