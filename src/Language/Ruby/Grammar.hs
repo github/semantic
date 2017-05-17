e5aa98a7c1fc35d9ebb574faa9692f72913f809d
Rob Rix <rob.rix@me.com>
2017-05-17 14:15:27 -0400

    Move the Ruby Grammar datatype into its own module.

--- src/Language/Ruby/Grammar.hs
+++ src/Language/Ruby/Grammar.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Language.Ruby.Grammar where
+
+import Language.Haskell.TH
+import Prologue
+import Text.Parser.TreeSitter.Language
+import Text.Parser.TreeSitter.Ruby
+
+-- | Statically-known rules corresponding to symbols in the grammar.
+mkSymbolDatatype (mkName "Grammar") tree_sitter_ruby
