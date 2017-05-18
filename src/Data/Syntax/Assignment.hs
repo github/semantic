9519e557be21c02bdca59de74e43531f48ab972b
Rick Winfrey <rewinfrey@github.com>
2017-05-18 09:47:14 -0700

    Reorder error message to add callstack after the error syntax node

--- src/Data/Syntax/Assignment.hs
+++ src/Data/Syntax/Assignment.hs
@@ -167,9 +167,9 @@ data ErrorCause symbol
 showError :: Show symbol => Source.Source -> Error symbol -> ShowS
 showError source Error{..}
   = showSourcePos errorPos . showString ": error: " . showExpectation . showChar '\n'
-  . showString (prettyCallStack callStack) . showChar '\n'
   . showString context -- actualLines results include line endings, so no newline here
   . showString (replicate (succ (Info.column errorPos + lineNumberDigits)) ' ') . showChar '^' . showChar '\n'
+  . showString (prettyCallStack callStack) . showChar '\n'
   where showExpectation = case errorCause of
           UnexpectedEndOfInput [] -> showString "no rule to match at end of input nodes"
           UnexpectedEndOfInput symbols -> showString "expected " . showSymbols symbols . showString " at end of input nodes"
