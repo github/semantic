c89ee864ca5d18c0e0c1e16cf0279eb880b57ad2
Rob Rix <rob.rix@me.com>
2017-05-19 11:17:21 -0400

    :fire: rangeAt.

--- src/Source.hs
+++ src/Source.hs
@@ -120,7 +120,7 @@ sourceSpanToRange source SourceSpan{..} = Range start end
 rangeToSourceSpan :: Source -> Range -> SourceSpan
 rangeToSourceSpan source range = SourceSpan startPos endPos
   where startPos = maybe (SourcePos 1 1) (toStartPos 1) (head lineRanges)
-        endPos = toEndPos (Prologue.length lineRanges) (fromMaybe (rangeAt 0) (snd <$> unsnoc lineRanges))
+        endPos = toEndPos (Prologue.length lineRanges) (fromMaybe (Range 0 0) (snd <$> unsnoc lineRanges))
         lineRanges = actualLineRanges (slice range source)
         toStartPos line range = SourcePos line (succ (start range))
         toEndPos line range = SourcePos line (succ (end range))
