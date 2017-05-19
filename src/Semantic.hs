4958387ee41228088a3ea365908d9b2061a96ddc
Rob Rix <rob.rix@me.com>
2017-05-19 09:56:28 -0400

    Use Parsers for parsing.

--- src/Semantic.hs
+++ src/Semantic.hs
@@ -9,25 +9,19 @@ module Semantic
 
 import Control.Parallel.Strategies
 import qualified Control.Concurrent.Async as Async
-import qualified Data.Text as T
 import Data.Functor.Both
 import Data.Record
 import Diff
 import Info
 import Interpreter
 import Language
-import Language.Markdown
 import Patch
+import Parser
 import Prologue
 import Renderer
 import Source
 import Syntax
 import Term
-import Text.Parser.TreeSitter.C
-import Text.Parser.TreeSitter.Go
-import Text.Parser.TreeSitter.Ruby
-import Text.Parser.TreeSitter.TypeScript
-import TreeSitter
 
 -- This is the primary interface to the Semantic library which provides two
 -- major classes of functionality: semantic parsing and diffing of source code
@@ -73,17 +67,17 @@ parseBlobs renderer blobs = do
 
 -- | Parse a SourceBlob.
 parseBlob :: SourceBlob -> IO (Term (Syntax Text) (Record DefaultFields))
-parseBlob blob@SourceBlob{..} = parserForLanguage blobLanguage blob
+parseBlob blob@SourceBlob{..} = runParser (parserForLanguage blobLanguage) blob
 
 -- | Return a parser for a given langauge or the lineByLineParser parser.
-parserForLanguage :: Maybe Language -> SourceBlob -> IO (Cofree (Syntax Text) (Record DefaultFields))
-parserForLanguage Nothing = lineByLineParser
+parserForLanguage :: Maybe Language -> Parser (SyntaxTerm Text DefaultFields)
+parserForLanguage Nothing = LineByLineParser
 parserForLanguage (Just language) = case language of
-  C -> treeSitterParser C tree_sitter_c
-  TypeScript -> treeSitterParser TypeScript tree_sitter_typescript
-  Markdown -> cmarkParser
-  Ruby -> treeSitterParser Ruby tree_sitter_ruby
-  Language.Go -> treeSitterParser Language.Go tree_sitter_go
+  C -> CParser
+  TypeScript -> TypeScriptParser
+  Markdown -> MarkdownParser
+  Ruby -> RubyParser
+  Language.Go -> GoParser
 
 
 -- Internal
@@ -92,15 +86,3 @@ renderConcurrently :: (Monoid output, StringConv output ByteString) => (a -> b -
 renderConcurrently f diffs = do
   outputs <- Async.mapConcurrently (pure . uncurry f) diffs
   pure $ mconcat (outputs `using` parTraversable rseq)
-
--- | A fallback parser that treats a file simply as rows of strings.
-lineByLineParser :: SourceBlob -> IO (Cofree (Syntax Text) (Record DefaultFields))
-lineByLineParser SourceBlob{..} = pure . cofree . root $ case foldl' annotateLeaves ([], 0) lines of
-  (leaves, _) -> cofree <$> leaves
-  where
-    lines = actualLines source
-    root children = (sourceRange :. Program :. rangeToSourceSpan source sourceRange :. Nil) :< Indexed children
-    sourceRange = Source.totalRange source
-    leaf charIndex line = (Range charIndex (charIndex + T.length line) :. Program :. rangeToSourceSpan source (Range charIndex (charIndex + T.length line)) :. Nil) :< Leaf line
-    annotateLeaves (accum, charIndex) line =
-      (accum <> [ leaf charIndex (Source.toText line) ] , charIndex + Source.length line)
