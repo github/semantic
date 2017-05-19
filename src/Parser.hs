bfb85e69ea66dd7dcb63e69dd1299201c8b56562
Rob Rix <rob.rix@me.com>
2017-05-19 09:51:12 -0400

    Define an eliminator for parsers.

--- src/Parser.hs
+++ src/Parser.hs
@@ -6,9 +6,21 @@ import Language
 import Prologue
 import Source
 import Term
+import Text.Parser.TreeSitter.C
+import Text.Parser.TreeSitter.Go
+import Text.Parser.TreeSitter.Ruby
+import Text.Parser.TreeSitter.TypeScript
+import TreeSitter
 
 data Parser term where
   CParser :: Parser (SyntaxTerm Text DefaultFields)
   GoParser :: Parser (SyntaxTerm Text DefaultFields)
   RubyParser :: Parser (SyntaxTerm Text DefaultFields)
   TypeScriptParser :: Parser (SyntaxTerm Text DefaultFields)
+
+runParser :: Parser term -> SourceBlob -> IO term
+runParser parser = case parser of
+  CParser -> treeSitterParser C tree_sitter_c
+  GoParser -> treeSitterParser Go tree_sitter_go
+  RubyParser -> treeSitterParser Ruby tree_sitter_ruby
+  TypeScriptParser -> treeSitterParser TypeScript tree_sitter_typescript
