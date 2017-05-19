1d838e00cdd998e72842b4c89e7463ebe77c3239
Rob Rix <rob.rix@me.com>
2017-05-19 09:53:56 -0400

    Define a line-by-line Parser.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE GADTs, ScopedTypeVariables #-}
 module Parser where
 
+import Data.Record
+import qualified Data.Text as T
 import Info hiding (Go)
 import Language
 import Prologue
 import Source
+import Syntax hiding (Go)
 import Term
 import Text.Parser.TreeSitter.C
 import Text.Parser.TreeSitter.Go
@@ -17,6 +20,7 @@ data Parser term where
   GoParser :: Parser (SyntaxTerm Text DefaultFields)
   RubyParser :: Parser (SyntaxTerm Text DefaultFields)
   TypeScriptParser :: Parser (SyntaxTerm Text DefaultFields)
+  LineByLineParser :: Parser (SyntaxTerm Text DefaultFields)
 
 runParser :: Parser term -> SourceBlob -> IO term
 runParser parser = case parser of
@@ -24,3 +28,16 @@ runParser parser = case parser of
   GoParser -> treeSitterParser Go tree_sitter_go
   RubyParser -> treeSitterParser Ruby tree_sitter_ruby
   TypeScriptParser -> treeSitterParser TypeScript tree_sitter_typescript
+  LineByLineParser -> lineByLineParser
+
+-- | A fallback parser that treats a file simply as rows of strings.
+lineByLineParser :: SourceBlob -> IO (SyntaxTerm Text DefaultFields)
+lineByLineParser SourceBlob{..} = pure . cofree . root $ case foldl' annotateLeaves ([], 0) lines of
+  (leaves, _) -> cofree <$> leaves
+  where
+    lines = actualLines source
+    root children = (sourceRange :. Program :. rangeToSourceSpan source sourceRange :. Nil) :< Indexed children
+    sourceRange = Source.totalRange source
+    leaf charIndex line = (Range charIndex (charIndex + T.length line) :. Program :. rangeToSourceSpan source (Range charIndex (charIndex + T.length line)) :. Nil) :< Leaf line
+    annotateLeaves (accum, charIndex) line =
+      (accum <> [ leaf charIndex (Source.toText line) ] , charIndex + Source.length line)
