61e29e8fdbace35f38b7f3c3dad1257e2e3fac8c
Rob Rix <rob.rix@me.com>
2017-05-19 11:20:52 -0400

    Compute the line ranges within the range.

--- src/Source.hs
+++ src/Source.hs
@@ -121,7 +121,7 @@ rangeToSourceSpan :: Source -> Range -> SourceSpan
 rangeToSourceSpan source range = SourceSpan startPos endPos
   where startPos = maybe (SourcePos 1 1) (toStartPos 1) (head lineRanges)
         endPos = toEndPos (Prologue.length lineRanges) (fromMaybe (Range 0 0) (snd <$> unsnoc lineRanges))
-        lineRanges = actualLineRanges (slice range source)
+        lineRanges = actualLineRangesWithin range source
         toStartPos line range = SourcePos line (succ (start range))
         toEndPos line range = SourcePos line (succ (end range))
 
