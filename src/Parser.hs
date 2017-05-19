a5ab3637fb3c281fa64f0838bac61b6640ccb438
Rob Rix <rob.rix@me.com>
2017-05-19 09:51:01 -0400

    Define a Parser GADT.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,5 +1,14 @@
+{-# LANGUAGE GADTs, ScopedTypeVariables #-}
 module Parser where
 
+import Info hiding (Go)
+import Language
 import Prologue
 import Source
+import Term
 
+data Parser term where
+  CParser :: Parser (SyntaxTerm Text DefaultFields)
+  GoParser :: Parser (SyntaxTerm Text DefaultFields)
+  RubyParser :: Parser (SyntaxTerm Text DefaultFields)
+  TypeScriptParser :: Parser (SyntaxTerm Text DefaultFields)
