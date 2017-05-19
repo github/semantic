44b5e2ab47c7f3327913d9880b0ce7a61f652d89
Rob Rix <rob.rix@me.com>
2017-05-19 13:32:28 -0400

    Define ALaCarteParser in terms of ASTParser.

--- src/Parser.hs
+++ src/Parser.hs
@@ -24,7 +24,7 @@ import TreeSitter
 
 data Parser term where
   ASTParser :: (Bounded grammar, Enum grammar) => Ptr TS.Language -> Parser (AST grammar)
-  ALaCarteParser :: (InUnion fs (Syntax.Error [Error grammar]), Bounded grammar, Enum grammar, Eq grammar, Symbol grammar) => Ptr TS.Language -> Assignment (Node grammar) (Term (Union fs) Location) -> Parser (Term (Union fs) Location)
+  ALaCarteParser :: (InUnion fs (Syntax.Error [Error grammar]), Bounded grammar, Enum grammar, Eq grammar, Symbol grammar) => Parser (AST grammar) -> Assignment (Node grammar) (Term (Union fs) Location) -> Parser (Term (Union fs) Location)
   CParser :: Parser (SyntaxTerm Text DefaultFields)
   GoParser :: Parser (SyntaxTerm Text DefaultFields)
   MarkdownParser :: Parser (SyntaxTerm Text DefaultFields)
@@ -45,8 +45,8 @@ parserForLanguage (Just language) = case language of
 runParser :: Parser term -> Source -> IO term
 runParser parser = case parser of
   ASTParser language -> parseToAST language
-  ALaCarteParser language assignment -> \ source -> do
-    ast <- parseToAST language source
+  ALaCarteParser parser assignment -> \ source -> do
+    ast <- runParser parser source
     let Result errors term = assign assignment source ast
     pure (fromMaybe (cofree ((totalRange source :. totalSpan source :. Nil) :< inj (Syntax.Error errors))) term)
   CParser -> treeSitterParser C tree_sitter_c
