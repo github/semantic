b9e947b605e5e9756de24eb02afbcd147a8aceac
Rob Rix <rob.rix@me.com>
2017-05-19 09:43:31 -0400

    :fire: the Parser type synonym.

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,7 +3,3 @@ module Parser where
 import Prologue
 import Source
 
--- | A function that takes a source blob and returns an annotated AST.
--- | The return is in the IO monad because some of the parsers are written in C
--- | and aren't pure.
-type Parser f a = SourceBlob -> IO (Cofree f a)
