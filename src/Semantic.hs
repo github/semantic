aaf3bb0632f38f7ccede288a818bc4dc91fc45c7
Rob Rix <rob.rix@me.com>
2017-05-26 14:39:24 -0400

    Parse and render in the helper function.

--- src/Semantic.hs
+++ src/Semantic.hs
@@ -56,11 +56,11 @@ diffBlobPair decorator blobs = do
 -- | Parse a list of SourceBlobs and use the specified renderer to produce ByteString output.
 parseBlobs :: (Monoid output, StringConv output ByteString, NFData (Record fields)) => (Source -> Term (Syntax Text) (Record DefaultFields) -> Term (Syntax Text) (Record fields)) -> Renderer (Identity SourceBlob, Term (Syntax Text) (Record fields)) output -> [SourceBlob] -> IO ByteString
 parseBlobs decorator renderer blobs =
-  renderConcurrently (fmap (runRenderer renderer) . parse) (filter (not . nonExistentBlob) blobs)
+  renderConcurrently parseAndRender (filter (not . nonExistentBlob) blobs)
   where
-    parse blob = do
+    parseAndRender blob = do
       term <- decorator (source blob) <$> runParser (parserForLanguage (blobLanguage blob)) (source blob)
-      pure (Identity blob, term)
+      pure $! runRenderer renderer (Identity blob, term)
 
 -- | Parse a SourceBlob.
 parseBlob :: NFData (Record fields) => (Source -> Term (Syntax Text) (Record DefaultFields) -> Term (Syntax Text) (Record fields)) -> SourceBlob -> IO (Term (Syntax Text) (Record fields))
