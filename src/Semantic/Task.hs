087f84a22ac1b9b805427992f003a68516a24c09
Rob Rix <rob.rix@me.com>
2017-05-29 13:32:23 -0400

    Define a Task for diffing terms.

--- src/Semantic/Task.hs
+++ src/Semantic/Task.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE GADTs #-}
 module Semantic.Task
 ( Task
+, Decorator
+, Differ
 , parse
 , decorate
+, diff
 , render
 , distribute
 , parseAndRenderBlob
@@ -12,22 +15,27 @@ module Semantic.Task
 import qualified Control.Concurrent.Async as Async
 import Control.Monad.Free.Freer
 import Data.Aeson (Value)
+import Data.Functor.Both
+import Diff
 import qualified Language
 import Parser
 import Prologue
 import Renderer
 import Source
-
-type Decorator input output = Source -> input -> output
-
+import Term
 
 data TaskF output where
   Parse :: Parser term -> Source -> TaskF term
   Decorate :: Decorator term term' -> Source -> term -> TaskF term'
+  Diff :: Differ f a -> Both (Term f a) -> TaskF (Diff f a)
   Render :: Renderer input output -> input -> TaskF output
   Distribute :: Traversable t => t (Task output) -> TaskF (t output)
 
 type Task = Freer TaskF
+
+type Decorator input output = Source -> input -> output
+
+type Differ f a = Both (Term f a) -> Diff f a
 
 
 data NamedDecorator = IdentifierDecorator | IdentityDecorator
@@ -40,6 +48,9 @@ parse parser source = Parse parser source `Then` return
 
 decorate :: Decorator term term' -> Source -> term -> Task term'
 decorate decorator source term = Decorate decorator source term `Then` return
+
+diff :: Differ f a -> Both (Term f a) -> Task (Diff f a)
+diff differ terms = Diff differ terms `Then` return
 
 render :: Renderer input output -> input -> Task output
 render renderer input = Render renderer input `Then` return
@@ -73,5 +84,6 @@ runTask :: Task a -> IO a
 runTask = iterFreerA $ \ task yield -> case task of
   Parse parser source -> runParser parser source >>= yield
   Decorate decorator source term -> yield (decorator source term)
+  Diff differ terms -> yield (differ terms)
   Render renderer input -> yield (runRenderer renderer input)
   Distribute tasks -> Async.mapConcurrently runTask tasks >>= yield
