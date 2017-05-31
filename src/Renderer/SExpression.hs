a9a6c34a6f0e6084144099986e2dfc4f5f5b3994
Rob Rix <rob.rix@me.com>
2017-05-31 15:22:55 -0400

    Generalize the s-expression renderers to show all fields.

--- src/Renderer/SExpression.hs
+++ src/Renderer/SExpression.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE GADTs, RankNTypes, ScopedTypeVariables #-}
 module Renderer.SExpression
 ( renderSExpressionDiff
 , renderSExpressionTerm
@@ -10,18 +10,17 @@ import Data.Record
 import Prologue hiding (replicate, encodeUtf8)
 import Diff
 import Patch
-import Info
 import Term
 
 -- | Returns a ByteString SExpression formatted diff.
-renderSExpressionDiff :: (HasField fields Category, Foldable f) => Diff f (Record fields) -> ByteString
+renderSExpressionDiff :: (ConstrainAll Show fields, Foldable f) => Diff f (Record fields) -> ByteString
 renderSExpressionDiff diff = printDiff diff 0 <> "\n"
 
 -- | Returns a ByteString SExpression formatted term.
-renderSExpressionTerm :: (HasField fields Category, Foldable f) => Term f (Record fields) -> ByteString
+renderSExpressionTerm :: (ConstrainAll Show fields, Foldable f) => Term f (Record fields) -> ByteString
 renderSExpressionTerm term = printTerm term 0 <> "\n"
 
-printDiff :: (HasField fields Category, Foldable f) => Diff f (Record fields) -> Int -> ByteString
+printDiff :: (ConstrainAll Show fields, Foldable f) => Diff f (Record fields) -> Int -> ByteString
 printDiff diff level = case runFree diff of
   Pure patch -> case patch of
     Insert term -> pad (level - 1) <> "{+" <> printTerm term level <> "+}"
@@ -36,18 +35,19 @@ printDiff diff level = case runFree diff of
           | n < 1 = "\n"
           | otherwise = "\n" <> replicate (2 * n) space
 
-printTerm :: (HasField fields Category, Foldable f) => Term f (Record fields) -> Int -> ByteString
+printTerm :: (ConstrainAll Show fields, Foldable f) => Term f (Record fields) -> Int -> ByteString
 printTerm term level = go term level 0
   where
     pad :: Int -> Int -> ByteString
     pad p n | n < 1 = ""
             | otherwise = "\n" <> replicate (2 * (p + n)) space
-    go :: (HasField fields Category, Foldable f) => Term f (Record fields) -> Int -> Int -> ByteString
+    go :: (ConstrainAll Show fields, Foldable f) => Term f (Record fields) -> Int -> Int -> ByteString
     go term parentLevel level = case runCofree term of
       (annotation :< syntax) -> pad parentLevel level <> "(" <> showAnnotation annotation <> foldr (\t acc -> go t parentLevel (level + 1) <> acc) "" syntax <> ")"
 
-showAnnotation :: HasField fields Category => Record fields -> ByteString
-showAnnotation = toS . category
+showAnnotation :: ConstrainAll Show fields => Record fields -> ByteString
+showAnnotation Nil = ""
+showAnnotation (first :. rest) = show first <> " " <> showAnnotation rest
 
 space :: Word8
 space = fromIntegral $ ord ' '
