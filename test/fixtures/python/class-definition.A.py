7e6c065a429d834e9c430a1543459fbaf239f623
Rick Winfrey <rewinfrey@github.com>
2017-06-06 17:05:22 -0700

    Class definition test fixture

---
+++ test/fixtures/python/class-definition.A.py
@@ -0,0 +1,11 @@
+class A:
+  def b(self):
+    return c
+
+class C():
+  pass
+
+class B(method1):
+  def method1(self):
+    return
+
---
+++ test/fixtures/python/class-definition.B.py
@@ -0,0 +1,8 @@
+class D(method1):
+  def method1(self):
+    return
+
+class B:
+  def b(self):
+    return c
+
