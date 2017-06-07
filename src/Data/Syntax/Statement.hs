a92c4d012e9e634accd18404f312508f57ea9efb
Rick Winfrey <rewinfrey@github.com>
2017-06-07 13:11:40 -0700

    Add Statement.NoOp

--- src/Data/Syntax/Statement.hs
+++ src/Data/Syntax/Statement.hs
@@ -67,6 +67,12 @@ newtype Continue a = Continue a
 instance Eq1 Continue where liftEq = genericLiftEq
 instance Show1 Continue where liftShowsPrec = genericLiftShowsPrec
 
+newtype NoOp a = NoOp a
+  deriving (Eq, Foldable, Functor, GAlign, Generic1, Show, Traversable)
+
+instance Eq1 NoOp where liftEq = genericLiftEq
+instance Show1 NoOp where liftShowsPrec = genericLiftShowsPrec
+
 
 -- Loops
 
