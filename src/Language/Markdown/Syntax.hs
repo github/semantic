7591e154313807c819c7e0b5263bacadeb95bc75
Rob Rix <rob.rix@me.com>
2017-06-07 19:30:39 -0400

    Unqualify the grammar type.

--- src/Language/Markdown/Syntax.hs
+++ src/Language/Markdown/Syntax.hs
@@ -2,7 +2,7 @@
 module Language.Markdown.Syntax
 ( assignment
 , Syntax
-, Grammar.Grammar
+, Grammar
 , Error
 , Term
 ) where
@@ -15,7 +15,7 @@ import qualified Data.Syntax.Assignment as Assignment
 import qualified Data.Syntax.Markup as Markup
 import qualified Data.Syntax as Syntax
 import GHC.Stack
-import qualified Language.Markdown as Grammar (Grammar(..))
+import Language.Markdown as Grammar (Grammar(..))
 import Prologue hiding (Location, link, list)
 import qualified Term
 
@@ -35,13 +35,13 @@ type Syntax =
    , Syntax.Error Error
    ]
 
-type Error = Assignment.Error Grammar.Grammar
+type Error = Assignment.Error Grammar
 type Term = Term.Term (Union Syntax) (Record Location)
-type Assignment = HasCallStack => Assignment.Assignment (Cofree [] (Record (CMark.NodeType ': Location))) Grammar.Grammar Term
+type Assignment = HasCallStack => Assignment.Assignment (Cofree [] (Record (CMark.NodeType ': Location))) Grammar Term
 
 
 assignment :: Assignment
-assignment = makeTerm <$> symbol Grammar.Document <*> children (Markup.Document <$> many blockElement)
+assignment = makeTerm <$> symbol Document <*> children (Markup.Document <$> many blockElement)
 
 
 -- Block elements
@@ -50,18 +50,18 @@ blockElement :: Assignment
 blockElement = paragraph <|> list <|> heading
 
 paragraph :: Assignment
-paragraph = makeTerm <$> symbol Grammar.Paragraph <*> children (Markup.Paragraph <$> many inlineElement)
+paragraph = makeTerm <$> symbol Paragraph <*> children (Markup.Paragraph <$> many inlineElement)
 
 list :: Assignment
-list = (cofree .) . (:<) <$> symbol Grammar.List <*> (project (\ (((CMark.LIST CMark.ListAttributes{..}) :. _) :< _) -> case listType of
+list = (cofree .) . (:<) <$> symbol List <*> (project (\ (((CMark.LIST CMark.ListAttributes{..}) :. _) :< _) -> case listType of
   CMark.BULLET_LIST -> inj . Markup.UnorderedList
   CMark.ORDERED_LIST -> inj . Markup.OrderedList) <*> children (many item))
 
 item :: Assignment
-item = symbol Grammar.Item *> children blockElement
+item = symbol Item *> children blockElement
 
 heading :: Assignment
-heading = makeTerm <$> symbol Grammar.Heading <*> (Markup.Heading <$> project (\ ((CMark.HEADING level :. _) :< _) -> level) <*> children (many inlineElement))
+heading = makeTerm <$> symbol Heading <*> (Markup.Heading <$> project (\ ((CMark.HEADING level :. _) :< _) -> level) <*> children (many inlineElement))
 
 
 -- Inline elements
@@ -70,16 +70,16 @@ inlineElement :: Assignment
 inlineElement = strong <|> emphasis <|> text <|> link
 
 strong :: Assignment
-strong = makeTerm <$> symbol Grammar.Strong <*> children (Markup.Strong <$> many inlineElement)
+strong = makeTerm <$> symbol Strong <*> children (Markup.Strong <$> many inlineElement)
 
 emphasis :: Assignment
-emphasis = makeTerm <$> symbol Grammar.Emphasis <*> children (Markup.Emphasis <$> many inlineElement)
+emphasis = makeTerm <$> symbol Emphasis <*> children (Markup.Emphasis <$> many inlineElement)
 
 text :: Assignment
-text = makeTerm <$> symbol Grammar.Text <*> (Markup.Text <$> source)
+text = makeTerm <$> symbol Text <*> (Markup.Text <$> source)
 
 link :: Assignment
-link = makeTerm <$> symbol Grammar.Link <*> (uncurry Markup.Link <$> project (\ (((CMark.LINK url title) :. _) :< _) -> (toS url, toS title))) <* source
+link = makeTerm <$> symbol Link <*> (uncurry Markup.Link <$> project (\ (((CMark.LINK url title) :. _) :< _) -> (toS url, toS title))) <* source
 
 
 -- Implementation details
