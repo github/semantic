8131dfaf756c38f66d386ebaa3e9062fdaeb5e74
Rob Rix <rob.rix@me.com>
2017-06-21 15:33:06 -0400

    Move moveDownFrom/moveRightFrom/slideFrom out a scope.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -49,15 +49,15 @@ ses eq as' bs'
                     else
                       moveRightFrom left
 
-                -- | Move downward from a given vertex, inserting the element for the corresponding row.
-                moveDownFrom  (Endpoint x y script) = Endpoint       x (succ y) (if y >= 0 && y < m then That (bs ! y) : script else script)
+        -- | Move downward from a given vertex, inserting the element for the corresponding row.
+        moveDownFrom  (Endpoint x y script) = Endpoint       x (succ y) (if y >= 0 && y < m then That (bs ! y) : script else script)
 
-                -- | Move rightward from a given vertex, deleting the element for the corresponding column.
-                moveRightFrom (Endpoint x y script) = Endpoint (succ x)      y  (if x < n then This (as ! x) : script else script)
+        -- | Move rightward from a given vertex, deleting the element for the corresponding column.
+        moveRightFrom (Endpoint x y script) = Endpoint (succ x)      y  (if x < n then This (as ! x) : script else script)
 
-                -- | Slide down any diagonal edges from a given vertex.
-                slideFrom (Endpoint x y script)
-                  | inRange (Array.bounds as) x, a <- as ! x
-                  , inRange (Array.bounds bs) y, b <- bs ! y
-                  , a `eq` b  = slideFrom (Endpoint (succ x) (succ y) (These a b : script))
-                  | otherwise =            Endpoint       x        y               script
+        -- | Slide down any diagonal edges from a given vertex.
+        slideFrom (Endpoint x y script)
+          | inRange (Array.bounds as) x, a <- as ! x
+          , inRange (Array.bounds bs) y, b <- bs ! y
+          , a `eq` b  = slideFrom (Endpoint (succ x) (succ y) (These a b : script))
+          | otherwise =            Endpoint       x        y               script
