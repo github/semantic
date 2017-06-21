9308e7f64acdff8ee79e4ed8107c2b03c04bb5ed
Rob Rix <rob.rix@me.com>
2017-06-21 10:07:59 -0400

    Search edit envelopes tail-recursively.

--- src/SES/Myers.hs
+++ src/SES/Myers.hs
@@ -1,9 +1,8 @@
 {-# LANGUAGE GADTs, ImplicitParams, MultiParamTypeClasses, ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module SES.Myers
 ( EditScript
-, Myers
 , EditGraph(..)
-, Distance(..)
 , Diagonal(..)
 , Endpoint(..)
 , ses
@@ -22,8 +21,6 @@ import Prologue hiding (error)
 -- | An edit script, i.e. a sequence of changes/copies of elements.
 type EditScript a b = [These a b]
 
-type Myers a b = State (MyersState a b)
-
 -- | Notionally the cartesian product of two sequences, represented as a simple wrapper around those arrays holding those sequences’ elements for O(1) lookups.
 data EditGraph a b = EditGraph { as :: !(Array.Array Int a), bs :: !(Array.Array Int b) }
   deriving (Eq, Show)
@@ -31,10 +28,6 @@ data EditGraph a b = EditGraph { as :: !(Array.Array Int a), bs :: !(Array.Array
 -- | Construct an edit graph from Foldable sequences.
 makeEditGraph :: (Foldable t, Foldable u) => t a -> u b -> EditGraph a b
 makeEditGraph as bs = EditGraph (Array.listArray (0, pred (length as)) (toList as)) (Array.listArray (0, pred (length bs)) (toList bs))
-
--- | An edit distance, i.e. a cardinal number of changes.
-newtype Distance = Distance { unDistance :: Int }
-  deriving (Eq, Show)
 
 -- | A diagonal in the edit graph of lists of lengths n and m, numbered from -m to n.
 newtype Diagonal = Diagonal { unDiagonal :: Int }
@@ -48,27 +41,25 @@ data Endpoint a b = Endpoint { x :: {-# UNPACK #-} !Int, y :: {-# UNPACK #-} !In
 
 -- | Compute the shortest edit script using Myers’ algorithm.
 ses :: (Foldable t, Foldable u) => (a -> b -> Bool) -> t a -> u b -> EditScript a b
-ses eq as bs = let graph = makeEditGraph as bs in evalState (runSES eq graph) (emptyStateForGraph graph)
+ses eq as bs = let graph = makeEditGraph as bs in runSES eq graph
 
 
 -- Evaluation
 
 -- | Compute the shortest edit script (diff) of an edit graph.
-runSES :: (a -> b -> Bool) -> EditGraph a b -> Myers a b (EditScript a b)
+runSES :: (a -> b -> Bool) -> EditGraph a b -> EditScript a b
 runSES eq (EditGraph as bs)
-  | null bs = return (This <$> toList as)
-  | null as = return (That <$> toList bs)
-  | otherwise = do
-    Just script <- asum <$> for [0..(n + m)] (searchUpToD . Distance)
-    return (reverse script)
+  | null bs = This <$> toList as
+  | null as = That <$> toList bs
+  | otherwise = reverse (searchUpToD [0..(n + m)] (Map.singleton 0 (0, [])))
   where (n, m) = (length as, length bs)
 
         -- Search an edit graph for the shortest edit script up to a given proposed edit distance, building on the results of previous searches.
-        searchUpToD (Distance d) = do
-          v <- get
-          let extents = searchAlongK v . Diagonal <$> [ k | k <- [negate d, negate d + 2 .. d], inRange (negate m, n) k ]
-          put (Map.fromList ((k' &&& (x' &&& script')) <$> extents))
-          pure . fmap script' $! find isComplete extents
+        searchUpToD (d:ds) v =
+          let extents = searchAlongK v . Diagonal <$> [ k | k <- [negate d, negate d + 2 .. d], inRange (negate m, n) k ] in
+          case find isComplete extents of
+            Just (Extent _ _ script) -> script
+            _ -> searchUpToD ds (Map.fromList ((k' &&& (x' &&& script')) <$> extents))
           where isComplete (Extent k x _) = x >= n && (x - k) >= m
 
                 -- Search an edit graph for the shortest edit script along a specific diagonal, moving onto a given diagonal from one of its in-bounds adjacent diagonals (if any), and sliding down any diagonal edges eagerly.
@@ -116,11 +107,6 @@ data Extent a b = Extent { k' :: {-# UNPACK #-} !Int, x' :: {-# UNPACK #-} !Int,
 
 -- | The state stored by Myers’ algorithm; an array of m + n + 1 values indicating the maximum x-index reached and path taken along each diagonal.
 type MyersState a b = Map.IntMap (Int, EditScript a b)
-
--- | Compute the empty state of length m + n + 1 for a given edit graph.
-emptyStateForGraph :: EditGraph a b -> MyersState a b
-emptyStateForGraph _ =
-  Map.singleton 0 (0, [])
 
 
 -- | Lifted showing of arrays.
