2c113280bc19d4dd8c6c729a329f71afd3b7319a
Rob Rix <rob.rix@me.com>
2017-06-24 10:38:31 -0400

    Explicitly unpack Range’s fields.

--- src/Data/Range.hs
+++ src/Data/Range.hs
@@ -10,7 +10,7 @@ import Prologue
 import Test.LeanCheck
 
 -- | A half-open interval of integers, defined by start & end indices.
-data Range = Range { start :: !Int, end :: !Int }
+data Range = Range { start :: {-# UNPACK #-} !Int, end :: {-# UNPACK #-} !Int }
   deriving (Eq, Show, Generic, NFData)
 
 -- | Return the length of the range.
