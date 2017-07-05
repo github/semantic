01be90fbf71c04278b02a4fd7304c334df9fccae
Timothy Clem <timothy.clem@gmail.com>
2017-07-05 14:53:44 -0700

    Guess we use these fixtures for other stuff, create a new methods

--- test/fixtures/ruby/method-declaration.A.rb
+++ test/fixtures/ruby/method-declaration.A.rb
@@ -1,17 +1,2 @@
 def foo
 end
-
-def bar=
-end
-
-def -@(a)
-end
-
-def foo(a, b: false, c: nil, **)
-end
-
-def foo(*args)
-end
-
-def foo(&block)
-end
---
+++ test/fixtures/ruby/methods.A.rb
@@ -0,0 +1,17 @@
+def foo
+end
+
+def bar=
+end
+
+def -@(a)
+end
+
+def foo(a, b: false, c: nil, **)
+end
+
+def foo(*args)
+end
+
+def foo(&block)
+end
