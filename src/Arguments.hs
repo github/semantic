b630a11b6e49f36cd59be91dce8c4c710219433e
Rob Rix <rob.rix@me.com>
2017-07-19 14:08:38 -0400

    :fire: Arguments.

--- src/Arguments.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE GADTs, DuplicateRecordFields, RankNTypes, StandaloneDeriving, UndecidableInstances #-}
-{-# OPTIONS_GHC -funbox-strict-fields #-}
-module Arguments where
-
-import Data.Maybe
-import Language
-import Prologue
-import Renderer
