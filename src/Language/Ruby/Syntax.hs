9618db2cdb36e55dd31f3aeaa8b4a93f52834aad
Timothy Clem <timothy.clem@gmail.com>
2017-07-20 13:52:10 -0700

    Produce program syntaxes (for ruby at least)

--- src/Language/Ruby/Syntax.hs
+++ src/Language/Ruby/Syntax.hs
@@ -67,6 +67,7 @@ type Syntax = '[
   , Syntax.Empty
   , Syntax.Error
   , Syntax.Identifier
+  , Syntax.Program
   , []
   ]
 
@@ -76,7 +77,9 @@ type Assignment = HasCallStack => Assignment.Assignment (AST Grammar) Grammar Te
 
 -- | Assignment from AST in Ruby’s grammar onto a program in Ruby’s syntax.
 assignment :: Assignment
-assignment = makeTerm <$> symbol Program <*> children (many statement) <|> parseError
+assignment =
+      makeTerm <$> symbol Program <*> children (Syntax.Program <$> many statement)
+  <|> parseError
 
 statement :: Assignment
 statement =
