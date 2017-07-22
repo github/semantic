0ffb818898ba96d9c5c17508b4bcc7ec1b8e3a5b
Rob Rix <rob.rix@me.com>
2017-07-22 17:27:08 -0400

    Don’t bind the first assignment.

--- src/Data/Syntax/Assignment.hs
+++ src/Data/Syntax/Assignment.hs
@@ -233,7 +233,7 @@ runAssignment :: forall grammar a ast. (Symbol grammar, Enum grammar, Eq grammar
               -> Assignment ast grammar a                                -- ^ The 'Assignment' to run.
               -> State ast grammar                             -- ^ The current state.
               -> Either (Error grammar) (a, State ast grammar) -- ^ 'Either' an 'Error' or the pair of the assigned value & updated state.
-runAssignment toNode source assignment = requireExhaustive <=< go assignment
+runAssignment toNode source = (requireExhaustive <=<) . go
   where go :: Assignment ast grammar result -> State ast grammar -> Either (Error grammar) (result, State ast grammar)
         go = iterFreer run . fmap ((pure .) . (,))
         {-# INLINE go #-}
