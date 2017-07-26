2d264d4d44128943c2ec9db73ffc70a61dda56d3
Rob Rix <rob.rix@me.com>
2017-07-26 11:02:29 -0400

    Explicitly use the union operator.

--- src/Data/Syntax/Assignment.hs
+++ src/Data/Syntax/Assignment.hs
@@ -346,7 +346,7 @@ instance Enum grammar => Alternative (Assignment ast grammar) where
   empty = Choose mempty `Then` return
   (<|>) :: HasCallStack => Assignment ast grammar a -> Assignment ast grammar a -> Assignment ast grammar a
   Return a <|> _ = Return a
-  a        <|> b | Just c <- (liftA2 (<>) `on` choices) a b = Choose c `Then` identity
+  a        <|> b | Just c <- (liftA2 IntMap.union `on` choices) a b = Choose c `Then` identity
                  | otherwise = wrap $ Alt a b
     where choices :: Assignment ast grammar a -> Maybe (IntMap (Assignment ast grammar a))
           choices (Choose choices `Then` continue) = Just (continue <$> choices)
