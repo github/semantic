ee18cfff39b23b14a03ae9f2d87bfa5b28dc6d8a
Timothy Clem <timothy.clem@gmail.com>
2017-07-31 09:05:55 -0700

    Case id as ID

--- src/Semantic/Log.hs
+++ src/Semantic/Log.hs
@@ -35,9 +35,9 @@ logfmtFormatter Options{..} (Message level message pairs time) =
       ( kv "time" (showTime time)
       : kv "msg" (shows message)
       : kv "level" (shows level)
-      : kv "pid" (shows optionsProcessId)
+      : kv "pid" (shows optionsProcessID)
       : (uncurry kv . second shows <$> pairs)
-      <> [ kv "request_id" (shows x) | x <- toList optionsRequestId ] )
+      <> [ kv "request_id" (shows x) | x <- toList optionsRequestID ] )
   . showChar '\n' $ ""
   where
     kv k v = showString k . showChar '=' . v
@@ -70,10 +70,10 @@ data Options = Options
   { optionsEnableColour :: Bool -- ^ Whether to enable colour formatting for logging (Only works when logging to a terminal that supports ANSI colors).
   , optionsLevel :: Maybe Level -- ^ What level of messages to log. 'Nothing' disabled logging.
   , optionsPrintSource :: Bool -- ^ Whether to print the source reference when logging errors.
-  , optionsRequestId :: Maybe String -- ^ Optional request id for tracing across systems.
+  , optionsRequestID :: Maybe String -- ^ Optional request id for tracing across systems.
   , optionsIsTerminal :: Bool -- ^ Whether a terminal is attached (set automaticaly at runtime).
   , optionsFormatter :: Options -> Message -> String -- ^ Log formatter to use (set automaticaly at runtime).
-  , optionsProcessId :: CPid -- ^ ProcessID (set automaticaly at runtime).
+  , optionsProcessID :: CPid -- ^ ProcessID (set automaticaly at runtime).
   }
 
 defaultOptions :: Options
@@ -81,10 +81,10 @@ defaultOptions = Options
   { optionsEnableColour = True
   , optionsLevel = Just Warning
   , optionsPrintSource = False
-  , optionsRequestId = Nothing
+  , optionsRequestID = Nothing
   , optionsIsTerminal = False
   , optionsFormatter = logfmtFormatter
-  , optionsProcessId = 0
+  , optionsProcessID = 0
   }
 
 configureOptionsForHandle :: Handle -> Options -> IO Options
@@ -94,7 +94,7 @@ configureOptionsForHandle handle options = do
   pure $ options
     { optionsIsTerminal = isTerminal
     , optionsFormatter = if isTerminal then terminalFormatter else logfmtFormatter
-    , optionsProcessId = pid
+    , optionsProcessID = pid
     }
 
 withSGRCode :: Bool -> [SGR] -> ShowS -> ShowS
