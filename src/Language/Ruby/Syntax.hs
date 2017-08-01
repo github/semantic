6d1b07631634f7534a1f0ebc692866b2480ef394
Timothy Clem <timothy.clem@gmail.com>
2017-08-01 11:40:56 -0700

    Rework for statement for new in clause

--- src/Language/Ruby/Syntax.hs
+++ src/Language/Ruby/Syntax.hs
@@ -19,6 +19,7 @@ import Data.Union
 import GHC.Stack
 import Language.Ruby.Grammar as Grammar
 import Prologue hiding (for, get, Location, state, unless)
+import Prelude (reverse)
 import qualified Term
 
 -- | The type of Ruby syntax.
@@ -266,9 +267,16 @@ until' =
       makeTerm <$> symbol Until         <*> children      (Statement.While <$> invert expression <*> expressions)
   <|> makeTerm <$> symbol UntilModifier <*> children (flip Statement.While <$> expression <*> invert expression)
 
+-- (for
+--   (in
+--     (one or more binding expressions)
+--     (one and only one subject expression))
+--   (body - zero or more expressions))
 for :: Assignment
-for = makeTerm <$> symbol For <*> children (Statement.ForEach <$> vars <*> expression <*> expressions)
-  where vars = makeTerm <$> location <*> some expression
+for = makeTerm <$> symbol For <*> children (forStatement <*> expressions)
+  where
+    forStatement = (reverse <$> (symbol In *> children (many expression))) >>= \ (x:rest) ->
+      Statement.ForEach <$> (flip makeTerm (reverse rest) <$> location) <*> (pure x)
 
 case' :: Assignment
 case' = makeTerm <$> symbol Case <*> children (Statement.Match <$> expression <*> whens)
--- test/fixtures/ruby/for.A.rb
+++ test/fixtures/ruby/for.A.rb
@@ -5,3 +5,5 @@ end
 for x, y in z do
   f
 end
+
+for n in 1..3 do true end
