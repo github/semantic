696219509ad9a4fbacd7619d6daeefd32b23f8a4
Rob Rix <rob.rix@me.com>
2017-08-02 16:15:34 -0400

    Define parseError in Data.Syntax.

--- src/Data/Syntax.hs
+++ src/Data/Syntax.hs
@@ -22,6 +22,9 @@ makeTerm a f = cofree (a :< inj f)
 emptyTerm :: (HasCallStack, Empty :< fs) => Assignment.Assignment ast grammar (Term (Union fs) (Record Assignment.Location))
 emptyTerm = makeTerm <$> Assignment.location <*> pure Empty
 
+parseError :: (HasCallStack, Error :< fs, Bounded grammar, Enum grammar, Eq grammar) => Assignment.Assignment ast grammar (Term (Union fs) (Record Assignment.Location))
+parseError = makeTerm <$> Assignment.symbol maxBound <*> (Error Nothing [] <$ Assignment.source)
+
 handleError :: (HasCallStack, Error :< fs, Show grammar) => Assignment.Assignment ast grammar (Term (Union fs) (Record Assignment.Location)) -> Assignment.Assignment ast grammar (Term (Union fs) (Record Assignment.Location))
 handleError = flip catchError (\ err -> makeTerm <$> Assignment.location <*> pure (Error (Just (fmap show err)) []) <* Assignment.source)
 
