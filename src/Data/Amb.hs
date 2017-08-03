aeb4a65c613c0ffa822b881bcd7774bc022f7e84
Rob Rix <rob.rix@me.com>
2017-08-03 17:02:28 -0400

    Define an Applicative instance for Amb.

--- src/Data/Amb.hs
+++ src/Data/Amb.hs
@@ -12,3 +12,9 @@ instance Semigroup (Amb l r) where
   l <> None _ = l
   None _ <> r = r
   Some as <> Some bs = Some (as <> bs)
+
+instance Applicative (Amb l) where
+  pure a = Some (a :| [])
+  None l <*> _ = None l
+  _ <*> None r = None r
+  Some fs <*> Some as = Some (fs <*> as)
