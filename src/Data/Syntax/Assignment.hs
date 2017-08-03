0629b136d6593dd592d0751215331a0c58a5b7f4
Rob Rix <rob.rix@me.com>
2017-08-03 15:37:56 -0400

    Revert "Define a setter for the next set field."
    This reverts commit 1fe1dbc4669fb2460a58925ac00c226172a614f2.

--- src/Data/Syntax/Assignment.hs
+++ src/Data/Syntax/Assignment.hs
@@ -345,9 +345,6 @@ data State ast grammar = State
 makeState :: [ast] -> State ast grammar
 makeState = State 0 (Info.Pos 1 1) Nothing Nothing 0
 
-setNextSet :: State ast grammar -> Maybe IntSet.IntSet -> State ast grammar
-setNextSet state nextSet = state { stateNextSet = nextSet }
-
 
 -- Instances
 
