7dd880dd18ba18094e017da0457986503fb8deb2
Rob Rix <rob.rix@me.com>
2017-08-03 17:02:15 -0400

    Define a Semigroup instance over Amb.

--- src/Data/Amb.hs
+++ src/Data/Amb.hs
@@ -1,8 +1,14 @@
 module Data.Amb where
 
 import Data.List.NonEmpty
+import Data.Semigroup
 
 data Amb l r
   = None l
   | Some (NonEmpty r)
   deriving (Eq, Foldable, Functor, Show, Traversable)
+
+instance Semigroup (Amb l r) where
+  l <> None _ = l
+  None _ <> r = r
+  Some as <> Some bs = Some (as <> bs)
