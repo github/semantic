fa87a249ad9781368a834c202ccfeec77007fb3e
Rob Rix <rob.rix@me.com>
2017-08-03 15:38:11 -0400

    Revert "Use the state nodes to indicate that we’ve advanced past nodes.."
    This reverts commit b2485da5199703d48d11ccf6b2374712b5067ded.

--- test/Data/Syntax/Assignment/Spec.hs
+++ test/Data/Syntax/Assignment/Spec.hs
@@ -215,15 +215,15 @@ spec = do
       assignBy headF "hi" source (node Red 0 2 []) `shouldBe` Right "hi"
 
     it "advances past the current node" $
-      stateNodes . snd <$> runAssignment headF "hi" source (makeState [ node Red 0 2 [] ])
+      snd <$> runAssignment headF "hi" source (makeState [ node Red 0 2 [] ])
       `shouldBe`
-        Right []
+        Right (State 2 (Info.Pos 1 3) Nothing 1 [])
 
   describe "children" $ do
     it "advances past the current node" $
-      stateNodes . snd <$> runAssignment headF "a" (children (pure (Out ""))) (makeState [node Red 0 1 []])
+      snd <$> runAssignment headF "a" (children (pure (Out ""))) (makeState [node Red 0 1 []])
       `shouldBe`
-        Right []
+        Right (State 1 (Info.Pos 1 2) Nothing 1 [])
 
     it "matches if its subrule matches" $
       () <$ runAssignment headF "a" (children red) (makeState [node Blue 0 1 [node Red 0 1 []]])
