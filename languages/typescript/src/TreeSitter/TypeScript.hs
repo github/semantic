637a468a2697088340acaaa0412c75f96bbe966e
Rob Rix <rob.rix@me.com>
2017-08-11 08:01:56 -0400

    Rename the TypeScript module.

--- languages/typescript/src/TreeSitter/TypeScript.hs
+++ languages/typescript/src/TreeSitter/TypeScript.hs
@@ -1,6 +1,6 @@
-module Text.Parser.TreeSitter.TypeScript where
+module TreeSitter.TypeScript where
 
 import Foreign.Ptr
-import Text.Parser.TreeSitter
+import TreeSitter
 
 foreign import ccall unsafe "vendor/tree-sitter-typescript/src/parser.c tree_sitter_typescript" tree_sitter_typescript :: Ptr Language
