56d3c94b45210df7fa4fdd808e8eb9dae61d9ad8
Rob Rix <rob.rix@me.com>
2017-08-11 08:01:38 -0400

    Rename the JSON module.

--- languages/json/src/TreeSitter/JSON.hs
+++ languages/json/src/TreeSitter/JSON.hs
@@ -1,6 +1,6 @@
-module Text.Parser.TreeSitter.JSON where
+module TreeSitter.JSON where
 
 import Foreign.Ptr
-import Text.Parser.TreeSitter
+import TreeSitter
 
 foreign import ccall unsafe "vendor/tree-sitter-json/src/parser.c tree_sitter_json" tree_sitter_json :: Ptr Language
