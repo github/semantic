edab2a8436e4f197d52d10c4234191738ece4938
Rob Rix <rob.rix@me.com>
2017-08-11 08:01:32 -0400

    Rename the Go module.

--- languages/go/src/TreeSitter/Go.hs
+++ languages/go/src/TreeSitter/Go.hs
@@ -1,6 +1,6 @@
-module Text.Parser.TreeSitter.Go where
+module TreeSitter.Go where
 
-import Text.Parser.TreeSitter
+import TreeSitter
 import Foreign.Ptr
 
 foreign import ccall unsafe "vendor/tree-sitter-go/src/parser.c tree_sitter_go" tree_sitter_go :: Ptr Language
