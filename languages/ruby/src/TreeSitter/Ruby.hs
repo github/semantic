b9e75cb59f000e430c7c45bbca22c4f88da7c556
Rob Rix <rob.rix@me.com>
2017-08-11 08:35:29 -0400

    Get the tree-sitter-ruby package from the submodule.

--- languages/ruby/src/TreeSitter/Ruby.hs
+++
@@ -1,8 +0,0 @@
-module TreeSitter.Ruby
-( tree_sitter_ruby
-) where
-
-import Foreign.Ptr
-import TreeSitter.Language
-
-foreign import ccall unsafe "vendor/tree-sitter-ruby/src/parser.c tree_sitter_ruby" tree_sitter_ruby :: Ptr Language
