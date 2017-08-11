8eec429ac872408d33ffd92db76c6cf802a6b783
Rob Rix <rob.rix@me.com>
2017-08-11 08:01:44 -0400

    Rename the Python module.

--- languages/python/src/TreeSitter/Python.hs
+++ languages/python/src/TreeSitter/Python.hs
@@ -1,8 +1,8 @@
-module Text.Parser.TreeSitter.Python
+module TreeSitter.Python
 ( tree_sitter_python
 ) where
 
 import Foreign.Ptr
-import Text.Parser.TreeSitter
+import TreeSitter
 
 foreign import ccall unsafe "vendor/tree-sitter-python/src/parser.c tree_sitter_python" tree_sitter_python :: Ptr Language
