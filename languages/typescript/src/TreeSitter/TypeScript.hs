e74da6846dab30b9adf5d64623c285c932959279
Rob Rix <rob.rix@me.com>
2017-08-11 08:35:37 -0400

    Get the tree-sitter-typescript package from the submodule.

--- languages/typescript/src/TreeSitter/TypeScript.hs
+++
@@ -1,8 +0,0 @@
-module TreeSitter.TypeScript
-( tree_sitter_typescript
-) where
-
-import Foreign.Ptr
-import TreeSitter.Language
-
-foreign import ccall unsafe "vendor/tree-sitter-typescript/src/parser.c tree_sitter_typescript" tree_sitter_typescript :: Ptr Language
