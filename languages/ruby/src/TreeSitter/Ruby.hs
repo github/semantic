ee842366f30cd6305b9ec896b650693d92a7e35b
Rob Rix <rob.rix@me.com>
2017-08-11 08:01:49 -0400

    Rename the Ruby module.

--- languages/ruby/src/TreeSitter/Ruby.hs
+++ languages/ruby/src/TreeSitter/Ruby.hs
@@ -1,8 +1,8 @@
-module Text.Parser.TreeSitter.Ruby
+module TreeSitter.Ruby
 ( tree_sitter_ruby
 ) where
 
 import Foreign.Ptr
-import Text.Parser.TreeSitter
+import TreeSitter
 
 foreign import ccall unsafe "vendor/tree-sitter-ruby/src/parser.c tree_sitter_ruby" tree_sitter_ruby :: Ptr Language
