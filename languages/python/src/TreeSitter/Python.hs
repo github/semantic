b6075642d7188b04a9e4ce98cc4ff4c12282c8bf
Rob Rix <rob.rix@me.com>
2017-08-11 08:35:21 -0400

    Get the tree-sitter-python package from the submodule.

--- languages/python/src/TreeSitter/Python.hs
+++
@@ -1,8 +0,0 @@
-module TreeSitter.Python
-( tree_sitter_python
-) where
-
-import Foreign.Ptr
-import TreeSitter.Language
-
-foreign import ccall unsafe "vendor/tree-sitter-python/src/parser.c tree_sitter_python" tree_sitter_python :: Ptr Language
