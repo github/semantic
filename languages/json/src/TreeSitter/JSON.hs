e08f86300fc2f3044a1ff80ab38d34ca0ee3ce44
Rob Rix <rob.rix@me.com>
2017-08-11 08:35:14 -0400

    Get the tree-sitter-json package from the submodule.

--- languages/json/src/TreeSitter/JSON.hs
+++
@@ -1,8 +0,0 @@
-module TreeSitter.JSON
-( tree_sitter_json
-) where
-
-import Foreign.Ptr
-import TreeSitter.Language
-
-foreign import ccall unsafe "vendor/tree-sitter-json/src/parser.c tree_sitter_json" tree_sitter_json :: Ptr Language
