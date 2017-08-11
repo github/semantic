ace55d00ba32066d3e7d4a58a00c72f1c0764861
Rob Rix <rob.rix@me.com>
2017-08-11 08:35:07 -0400

    Get the tree-sitter-go package from the submodule.

--- languages/go/src/TreeSitter/Go.hs
+++
@@ -1,8 +0,0 @@
-module TreeSitter.Go
-( tree_sitter_go
-) where
-
-import Foreign.Ptr
-import TreeSitter.Language
-
-foreign import ccall unsafe "vendor/tree-sitter-go/src/parser.c tree_sitter_go" tree_sitter_go :: Ptr Language
