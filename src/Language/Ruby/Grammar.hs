a6db57793e192807270f26f8955102b92a200423
Timothy Clem <timothy.clem@gmail.com>
2017-08-14 14:36:08 -0700

    ++haskell-tree-sitter to enable dependent files for regenerating TH code

--- src/Language/Ruby/Grammar.hs
+++ src/Language/Ruby/Grammar.hs
@@ -5,7 +5,7 @@ import Language.Haskell.TH
 import Text.Parser.TreeSitter.Language
 import Text.Parser.TreeSitter.Ruby
 
--- Bump to get file to change to force template haskell to regenerate.
--- tree-sitter-ruby@6681b4c8d255ca0aec09c29777271736186b6c7d
+addDependentFileRelative "../../../languages/ruby/vendor/tree-sitter-ruby/src/parser.c"
+
 -- | Statically-known rules corresponding to symbols in the grammar.
 mkSymbolDatatype (mkName "Grammar") tree_sitter_ruby
