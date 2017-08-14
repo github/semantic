b6f307b92e3b4fa7c0083e23b95dbec6b2c25b37
Timothy Clem <timothy.clem@gmail.com>
2017-08-14 15:35:51 -0700

    Bump, bump

--- src/Language/Ruby/Grammar.hs
+++ src/Language/Ruby/Grammar.hs
@@ -5,7 +5,7 @@ import Language.Haskell.TH
 import Text.Parser.TreeSitter.Language
 import Text.Parser.TreeSitter.Ruby
 
--- Re-generate template haskell code when these files change:
+-- Re-generate template haskell code when these files change.
 addDependentFileRelative "../../../languages/ruby/vendor/tree-sitter-ruby/src/parser.c"
 
 -- | Statically-known rules corresponding to symbols in the grammar.
