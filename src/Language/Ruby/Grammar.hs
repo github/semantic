e5e221aa69f0b333a3946e88789b127f85ee658d
Timothy Clem <timothy.clem@gmail.com>
2017-08-14 13:19:01 -0700

    Try to get TH to run on all build nodes

--- src/Language/Ruby/Grammar.hs
+++ src/Language/Ruby/Grammar.hs
@@ -5,6 +5,7 @@ import Language.Haskell.TH
 import Text.Parser.TreeSitter.Language
 import Text.Parser.TreeSitter.Ruby
 
--- v4 - Bump to get file to change to force template haskell to regenerate.
+-- Bump to get file to change to force template haskell to regenerate.
+-- tree-sitter-ruby@6681b4c8d255ca0aec09c29777271736186b6c7d
 -- | Statically-known rules corresponding to symbols in the grammar.
 mkSymbolDatatype (mkName "Grammar") tree_sitter_ruby
