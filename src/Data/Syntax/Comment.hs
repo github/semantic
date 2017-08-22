507b2e02ef0bba69c19b5659cdba87cedc7780a5
Rob Rix <rob.rix@me.com>
2017-08-22 14:20:47 -0400

    Define Pretty1 for comments without orphans.

--- src/Data/Syntax/Comment.hs
+++ src/Data/Syntax/Comment.hs
@@ -4,17 +4,21 @@ module Data.Syntax.Comment where
 import Algorithm
 import Data.Align.Generic
 import Data.ByteString (ByteString)
+import Data.Text.Encoding
 import Data.Functor.Classes.Eq.Generic
-import Data.Functor.Classes.Pretty.Orphans
+import Data.Functor.Classes.Pretty
 import Data.Functor.Classes.Show.Generic
 import GHC.Generics
 
 -- | An unnested comment (line or block).
 newtype Comment a = Comment { commentContent :: ByteString }
-  deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Pretty1, Show, Traversable)
+  deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Show, Traversable)
 
 instance Eq1 Comment where liftEq = genericLiftEq
 instance Show1 Comment where liftShowsPrec = genericLiftShowsPrec
+
+instance Pretty1 Comment where
+  liftPretty _ _ (Comment c) = pretty ("Comment" :: String) <+> pretty (decodeUtf8With (\ _ -> ('\xfffd' <$)) c)
 
 -- TODO: nested comment types
 -- TODO: documentation comment types
