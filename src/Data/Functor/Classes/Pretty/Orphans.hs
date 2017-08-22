cf1619b58fc1b4e8cfa3809a8f491b015b0189f5
Rob Rix <rob.rix@me.com>
2017-08-22 13:45:09 -0400

    Define a Pretty instance for ByteString.

--- src/Data/Functor/Classes/Pretty/Orphans.hs
+++ src/Data/Functor/Classes/Pretty/Orphans.hs
@@ -3,4 +3,9 @@ module Data.Functor.Classes.Pretty.Orphans
 ( module Pretty
 ) where
 
+import Data.ByteString
 import Data.Functor.Classes.Pretty as Pretty
+import Data.Text.Encoding
+
+instance Pretty ByteString where
+  pretty = pretty . decodeUtf8With (\ _ -> ('\xfffd' <$))
