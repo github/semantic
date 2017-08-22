f9974f7ec1c84f3005d7933fc6b7c6965d1ff8ef
Rob Rix <rob.rix@me.com>
2017-08-22 13:55:12 -0400

    Define an orphan Pretty instance for SrcLoc.

--- src/Data/Functor/Classes/Pretty/Orphans.hs
+++ src/Data/Functor/Classes/Pretty/Orphans.hs
@@ -6,6 +6,17 @@ module Data.Functor.Classes.Pretty.Orphans
 import Data.ByteString
 import Data.Functor.Classes.Pretty as Pretty
 import Data.Text.Encoding
+import GHC.Stack
 
 instance Pretty ByteString where
   pretty = pretty . decodeUtf8With (\ _ -> ('\xfffd' <$))
+
+instance Pretty SrcLoc where
+  pretty SrcLoc{..}
+    | srcLocStartLine == srcLocEndLine
+    , srcLocStartCol == srcLocEndCol
+    = pretty srcLocFile <> colon <> pretty srcLocStartLine <> colon <> pretty srcLocStartCol
+    | srcLocStartLine == srcLocEndLine
+    = pretty srcLocFile <> colon <> pretty srcLocStartLine <> colon <> pretty srcLocStartCol <> pretty '-' <> pretty srcLocEndCol
+    | otherwise
+    = pretty srcLocFile <> colon <> pretty srcLocStartLine <> colon <> pretty srcLocStartCol <> pretty '-' <> pretty srcLocEndLine <> colon <> pretty srcLocEndCol
