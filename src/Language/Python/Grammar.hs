0afabb1183fed1478dbe9ee295abba7f82ed7df6
Rick Winfrey <rewinfrey@github.com>
2017-08-24 16:34:31 -0700

    Bump Python grammar

--- src/Language/Python/Grammar.hs
+++ src/Language/Python/Grammar.hs
@@ -6,5 +6,5 @@ import TreeSitter.Language
 import TreeSitter.Python
 
 -- | Statically-known rules corresponding to symbols in the grammar.
--- v3 - bump this to regenerate
+-- v4 - bump this to regenerate
 mkSymbolDatatype (mkName "Grammar") tree_sitter_python
