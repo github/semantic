84aaba64d986239c00629a75ad1706d095dd96ee
Rob Rix <rob.rix@me.com>
2017-08-25 12:25:18 -0400

    Add a convenience to read a file and guess its language.

--- src/Semantic/Util.hs
+++ src/Semantic/Util.hs
@@ -1,7 +1,13 @@
 module Semantic.Util where
 
+import Data.Blob
 import Data.Text.Prettyprint.Doc
 import Data.Text.Prettyprint.Doc.Util
+import Files
+import Language
 
 pp :: Pretty a => a -> IO ()
 pp = putDocW 100 . (<> line) . pretty
+
+file :: FilePath -> IO Blob
+file path = Files.readFile path (languageForType path)
