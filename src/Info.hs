7f44d01f4591a8d54e766370fd0c1d7b79e63d88
Rob Rix <rob.rix@me.com>
2017-08-30 14:37:35 -0400

    :fire: category/setCategory.

--- src/Info.hs
+++ src/Info.hs
@@ -3,16 +3,12 @@ module Info
 ( Range(..)
 , byteRange
 , setByteRange
-, Category(..)
-, category
-, setCategory
 , Span(..)
 , Pos(..)
 , sourceSpan
 , setSpan
 ) where
 
-import Category
 import Data.Range
 import Data.Record
 import Data.Span
@@ -22,12 +18,6 @@ byteRange = getField
 
 setByteRange :: HasField fields Range => Record fields -> Range -> Record fields
 setByteRange = setField
-
-category :: HasField fields Category => Record fields -> Category
-category = getField
-
-setCategory :: HasField fields Category => Record fields -> Category -> Record fields
-setCategory = setField
 
 sourceSpan :: HasField fields Span => Record fields -> Span
 sourceSpan = getField
