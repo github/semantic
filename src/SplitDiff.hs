30ab5f5ae3286792f5a7be648891987c06fa087c
Rob Rix <rob.rix@me.com>
2017-08-30 14:42:40 -0400

    Use getField directly in SplitDiff.

--- src/SplitDiff.hs
+++ src/SplitDiff.hs
@@ -3,8 +3,8 @@ module SplitDiff where
 import Control.Comonad
 import Control.Comonad.Trans.Cofree
 import Control.Monad.Free
+import Data.Range
 import Data.Record
-import Info
 import Term (Term, TermF)
 
 -- | A patch to only one side of a diff.
@@ -15,8 +15,8 @@ data SplitPatch a
   deriving (Show, Eq, Functor)
 
 -- | Get the range of a SplitDiff.
-getRange :: Functor f => HasField fields Range => SplitDiff f (Record fields) -> Range
-getRange diff = byteRange $ case diff of
+getRange :: (Functor f, HasField fields Range) => SplitDiff f (Record fields) -> Range
+getRange diff = getField $ case diff of
   Free annotated -> headF annotated
   Pure patch -> extract (splitTerm patch)
 
