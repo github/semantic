8c1925332d26a6848aea0f74ffde0f20211a55cf
Rob Rix <rob.rix@me.com>
2017-08-30 14:33:18 -0400

    :fire: identifierAlgebra.

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -12,26 +12,20 @@ module Renderer
 , renderToCTerm
 , declarationAlgebra
 , markupSectionAlgebra
-, identifierAlgebra
 , Summaries(..)
 , File(..)
 ) where
 
-import Control.Comonad.Cofree (Cofree, unwrap)
-import Control.Comonad.Trans.Cofree (CofreeF(..))
 import Control.DeepSeq
 import Data.Aeson (Value, (.=))
 import Data.ByteString (ByteString)
-import Data.Foldable (asum)
 import qualified Data.Map as Map
 import Data.Output
-import Data.Syntax.Algebra (RAlgebra)
 import Data.Text (Text)
 import Renderer.JSON as R
 import Renderer.Patch as R
 import Renderer.SExpression as R
 import Renderer.TOC as R
-import Syntax as S
 
 -- | Specification of renderers for diffs, producing output in the parameter type.
 data DiffRenderer output where
@@ -68,23 +62,6 @@ data SomeRenderer f where
 
 deriving instance Show (SomeRenderer f)
 
-identifierAlgebra :: RAlgebra (CofreeF Syntax a) (Cofree Syntax a) (Maybe Identifier)
-identifierAlgebra (_ :< syntax) = case syntax of
-  S.Assignment f _ -> identifier f
-  S.Class f _ _ -> identifier f
-  S.Export f _ -> f >>= identifier
-  S.Function f _ _ -> identifier f
-  S.FunctionCall f _ _ -> identifier f
-  S.Import f _ -> identifier f
-  S.Method _ f _ _ _ -> identifier f
-  S.MethodCall _ f _ _ -> identifier f
-  S.Module f _ -> identifier f
-  S.OperatorAssignment f _ -> identifier f
-  S.SubscriptAccess f _  -> identifier f
-  S.TypeDecl f _ -> identifier f
-  S.VarAssignment f _ -> asum $ identifier <$> f
-  _ -> Nothing
-  where identifier = fmap Identifier . extractLeafValue . unwrap . fst
 
 newtype Identifier = Identifier Text
   deriving (Eq, NFData, Show)
