4c852dd1079ef2fe91132d282bf57ced09903639
Rob Rix <rob.rix@me.com>
2017-09-01 10:12:07 -0400

    Array-backed jump tables hold a set of indices.

--- src/Data/Syntax/Assignment/Table/Array.hs
+++ src/Data/Syntax/Assignment/Table/Array.hs
@@ -1,5 +1,6 @@
 module Data.Syntax.Assignment.Table.Array
-( Table(tableAddresses)
+( Table
+, tableAddresses
 , singleton
 , fromListWith
 , toList
@@ -9,26 +10,31 @@ module Data.Syntax.Assignment.Table.Array
 import Control.Arrow ((&&&))
 import Data.Array
 import Data.Functor.Classes
+import qualified Data.Set as Set
 import Data.List.NonEmpty (NonEmpty(..))
 import Data.Semigroup (Max(..), Min(..), sconcat)
 import GHC.Stack
 import Prelude hiding (lookup)
 
-data Table i a = Table { tableAddresses :: [i], tableBranches :: Array i (Maybe a) }
+data Table i a = Table { tableAddressSet :: Set.Set i, tableBranches :: Array i (Maybe a) }
   deriving (Eq, Foldable, Functor, Show, Traversable)
 
+tableAddresses :: Ix i => Table i a -> [i]
+tableAddresses = Set.toList . tableAddressSet
+
+
 singleton :: Ix i => i -> a -> Table i a
-singleton i a = Table [i] (listArray (i, i) [Just a])
+singleton i a = Table (Set.singleton i) (listArray (i, i) [Just a])
 
 fromListWith :: (HasCallStack, Ix i) => (a -> a -> a) -> [(i, a)] -> Table i a
 fromListWith _ [] = error "fromList: empty list of associations"
-fromListWith with assocs@(a:as) = Table (fst <$> assocs) (accumArray merge Nothing (getMin mn, getMax mx) assocs)
+fromListWith with assocs@(a:as) = Table (Set.fromList (fst <$> assocs)) (accumArray merge Nothing (getMin mn, getMax mx) assocs)
   where (mn, mx) = sconcat ((Min &&& Max) . fst <$> a:|as)
         merge Nothing b = Just b
         merge (Just a) b = Just (with a b)
 
 toList :: Ix i => Table i a -> [(i, a)]
-toList Table{..} = tableAddresses >>= \ addr -> maybe [] (pure . (,) addr) (tableBranches ! addr)
+toList Table{..} = Set.toList tableAddressSet >>= \ addr -> maybe [] (pure . (,) addr) (tableBranches ! addr)
 
 
 lookup :: Ix i => i -> Table i a -> Maybe a
