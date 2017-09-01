baf20927f4f5582068c887d86107ba1770460e02
Rob Rix <rob.rix@me.com>
2017-09-01 09:59:03 -0400

    Define a Semigroup instance for Assignment.

--- src/Data/Syntax/Assignment.hs
+++ src/Data/Syntax/Assignment.hs
@@ -333,6 +333,9 @@ makeState = State 0 (Info.Pos 1 1) []
 
 -- Instances
 
+instance (Enum grammar, Eq (ast (AST ast grammar)), Ix grammar) => Semigroup (Assignment ast grammar a) where
+  (<>) = (<|>)
+
 instance (Enum grammar, Eq (ast (AST ast grammar)), Ix grammar) => Monoid (Assignment ast grammar a) where
   mempty = empty
   mappend = (<|>)
