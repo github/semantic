ea01aa9e2bb57b6f811a4d4a8ad9db8fd4e1d436
Rob Rix <rob.rix@me.com>
2017-08-31 22:29:42 -0400

    Define a Show1 instance for Table.

--- src/Data/Syntax/Assignment/Table/Array.hs
+++ src/Data/Syntax/Assignment/Table/Array.hs
@@ -3,5 +3,11 @@ module Data.Syntax.Assignment.Table.Array
 ) where
 
 import Data.Array
+import Data.Foldable (toList)
+import Data.Functor.Classes
 
 data Table i a = Table { tableAddresses :: [i], tableBranches :: Array i (Maybe a) }
+
+
+instance (Ix i, Show i) => Show1 (Table i) where
+  liftShowsPrec spA slA d Table{..} = showsBinaryWith showsPrec (const (liftShowList spA slA)) "Table" d tableAddresses (assocs tableBranches >>= \ (sym, a) -> (,) sym <$> toList a)
