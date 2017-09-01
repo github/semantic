93a990ce43d12285915c3d0da1b34cb31f1d8204
Rob Rix <rob.rix@me.com>
2017-08-31 23:20:48 -0400

    Stub in a datatype for an IntMap-based jump table.

--- src/Data/Syntax/Assignment/Table/IntMap.hs
+++ src/Data/Syntax/Assignment/Table/IntMap.hs
@@ -1,1 +1,5 @@
 module Data.Syntax.Assignment.Table.IntMap where
+import qualified Data.IntMap as IntMap
+
+data Table i a = Table { tableAddresses :: [i], tableBranches :: IntMap.IntMap a }
+  deriving (Eq, Foldable, Functor, Show, Traversable)
