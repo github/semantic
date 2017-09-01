5de3acd374824f5afc1b25e1f9ad3eaa3a3e35aa
Rob Rix <rob.rix@me.com>
2017-08-31 23:21:01 -0400

    Define singleton table construction.

--- src/Data/Syntax/Assignment/Table/IntMap.hs
+++ src/Data/Syntax/Assignment/Table/IntMap.hs
@@ -3,3 +3,6 @@ import qualified Data.IntMap as IntMap
 
 data Table i a = Table { tableAddresses :: [i], tableBranches :: IntMap.IntMap a }
   deriving (Eq, Foldable, Functor, Show, Traversable)
+
+singleton :: Enum i => i -> a -> Table i a
+singleton i a = Table [i] (IntMap.singleton (fromEnum i) a)
