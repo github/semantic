b2b19e06823f7f6caf0b6ed2e10753da965a055b
joshvera <josh@joshvera.com>
2017-09-01 17:43:14 -0400

    remove imports

--- src/Language/TypeScript.hs
+++ src/Language/TypeScript.hs
@@ -1,13 +1,2 @@
 {-# LANGUAGE DataKinds #-}
 module Language.TypeScript where
-
-import Control.Comonad (extract)
-import Control.Comonad.Cofree (unwrap)
-import Data.Foldable (toList)
-import Data.Source
-import Data.Text (Text)
-import Info
-import Language
-import qualified Syntax as S
-import Term
-
