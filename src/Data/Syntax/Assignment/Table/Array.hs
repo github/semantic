d6c8947350113510e8132b7ce2c76bc343008e91
Rob Rix <rob.rix@me.com>
2017-09-01 10:16:15 -0400

    Store the addresses in an IntSet.

--- src/Data/Syntax/Assignment/Table/Array.hs
+++ src/Data/Syntax/Assignment/Table/Array.hs
@@ -10,31 +10,31 @@ module Data.Syntax.Assignment.Table.Array
 import Control.Arrow ((&&&))
 import Data.Array
 import Data.Functor.Classes
-import qualified Data.Set as Set
+import qualified Data.IntSet as IntSet
 import Data.List.NonEmpty (NonEmpty(..))
 import Data.Semigroup (Max(..), Min(..), sconcat)
 import GHC.Stack
 import Prelude hiding (lookup)
 
-data Table i a = Table { tableAddressSet :: Set.Set i, tableBranches :: Array i (Maybe a) }
+data Table i a = Table { tableAddressSet :: IntSet.IntSet, tableBranches :: Array i (Maybe a) }
   deriving (Eq, Foldable, Functor, Show, Traversable)
 
-tableAddresses :: Ix i => Table i a -> [i]
-tableAddresses = Set.toList . tableAddressSet
+tableAddresses :: Enum i => Table i a -> [i]
+tableAddresses = fmap toEnum . IntSet.toList . tableAddressSet
 
 
-singleton :: Ix i => i -> a -> Table i a
-singleton i a = Table (Set.singleton i) (listArray (i, i) [Just a])
+singleton :: (Enum i, Ix i) => i -> a -> Table i a
+singleton i a = Table (IntSet.singleton (fromEnum i)) (listArray (i, i) [Just a])
 
-fromListWith :: (HasCallStack, Ix i) => (a -> a -> a) -> [(i, a)] -> Table i a
+fromListWith :: (HasCallStack, Enum i, Ix i) => (a -> a -> a) -> [(i, a)] -> Table i a
 fromListWith _ [] = error "fromList: empty list of associations"
-fromListWith with assocs@(a:as) = Table (Set.fromList (fst <$> assocs)) (accumArray merge Nothing (getMin mn, getMax mx) assocs)
+fromListWith with assocs@(a:as) = Table (IntSet.fromList (fromEnum . fst <$> assocs)) (accumArray merge Nothing (getMin mn, getMax mx) assocs)
   where (mn, mx) = sconcat ((Min &&& Max) . fst <$> a:|as)
         merge Nothing b = Just b
         merge (Just a) b = Just (with a b)
 
-toList :: Ix i => Table i a -> [(i, a)]
-toList Table{..} = Set.toList tableAddressSet >>= \ addr -> maybe [] (pure . (,) addr) (tableBranches ! addr)
+toList :: (Enum i, Ix i) => Table i a -> [(i, a)]
+toList Table{..} = toEnum <$> IntSet.toList tableAddressSet >>= \ addr -> maybe [] (pure . (,) addr) (tableBranches ! addr)
 
 
 lookup :: Ix i => i -> Table i a -> Maybe a
@@ -43,5 +43,5 @@ lookup i Table{..}
   | otherwise                        = Nothing
 
 
-instance (Ix i, Show i) => Show1 (Table i) where
+instance (Enum i, Ix i, Show i) => Show1 (Table i) where
   liftShowsPrec spA slA d t = showsBinaryWith showsPrec (const (liftShowList spA slA)) "Table" d (tableAddresses t) (toList t)
