949beb6a9be4961d6bae164a6b0d40d8424f5264
Rick Winfrey <rewinfrey@github.com>
2017-09-05 17:01:07 -0700

    Fix warnings

--- src/Language/Go/Syntax.hs
+++ src/Language/Go/Syntax.hs
@@ -6,26 +6,18 @@ module Language.Go.Syntax
 , Term
 ) where
 
-import Algorithm
-import Data.Align.Generic
 import Data.Functor (void)
-import Data.Functor.Classes.Eq.Generic
-import Data.Functor.Classes.Show.Generic
-import Data.List.NonEmpty (NonEmpty(..))
-import Data.Maybe (fromMaybe)
 import Data.Record
-import Data.Syntax (contextualize, emptyTerm, handleError, infixContext, makeTerm, makeTerm', makeTerm1, postContextualize)
+import Data.Syntax (emptyTerm, handleError, makeTerm)
 import qualified Data.Syntax as Syntax
 import Data.Syntax.Assignment hiding (Assignment, Error)
 import qualified Data.Syntax.Assignment as Assignment
 import qualified Data.Syntax.Comment as Comment
 import qualified Data.Syntax.Declaration as Declaration
-import qualified Data.Syntax.Expression as Expression
 import qualified Data.Syntax.Literal as Literal
 import qualified Data.Syntax.Statement as Statement
 import qualified Data.Syntax.Type as Type
 import Data.Union
-import GHC.Generics
 import GHC.Stack
 import Language.Go.Grammar as Grammar
 import qualified Term
