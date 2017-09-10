b330eb45eba408ed5c1f98317227ca1f65cc1bb2
Rob Rix <rob.rix@me.com>
2017-09-10 15:38:09 +0100

    Produce only a single entry for patches.

--- src/Renderer/TOC.hs
+++ src/Renderer/TOC.hs
@@ -157,7 +157,7 @@ tableOfContentsBy selector = fromMaybe [] . evalDiff diffAlgebra
             (Just a, Just []) -> Just [Changed a]
             (_     , entries) -> entries
           Var v -> join (envLookup v env)
-          Patch patch -> Just [] <> foldMap (\ t -> (pure . patchEntry patch <$> selector t) <> fold t) patch
+          Patch patch -> Just [] <> foldMap (\ t -> maybe (fold t) (Just . pure . patchEntry patch) (selector t)) patch
 
         patchEntry = these (const Deleted) (const Inserted) (const (const Replaced)) . unPatch
 
