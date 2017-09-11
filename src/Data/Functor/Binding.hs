1f64d680234c42127fbfac1eb85df647c7f4e266
Rob Rix <rob.rix@me.com>
2017-09-11 10:20:54 -0400

    Let holds an Env.

--- src/Data/Functor/Binding.hs
+++ src/Data/Functor/Binding.hs
@@ -29,23 +29,23 @@ newtype Metavar = Metavar Int
 
 
 data BindingF f recur
-  = Let [(Metavar, recur)] (f recur)
+  = Let (Env recur) (f recur)
   | Var Metavar
   deriving (Foldable, Functor, Traversable)
 
-bindings :: BindingF f recur -> [(Metavar, recur)]
+bindings :: BindingF f recur -> Env recur
 bindings (Let vars _) = vars
-bindings _            = []
+bindings _            = mempty
 
 
 freeMetavariables :: (Foldable syntax, Functor syntax, Recursive t, Base t ~ BindingF syntax) => t -> Set.Set Metavar
 freeMetavariables = cata $ \ diff -> case diff of
-  Let bindings body -> foldMap snd bindings <> foldr Set.delete (fold body) (fst <$> bindings)
+  Let vars body -> fold vars <> foldr Set.delete (fold body) (fst <$> unEnv vars)
   Var v -> Set.singleton v
 
 maxBoundMetavariable :: (Foldable syntax, Functor syntax, Recursive t, Base t ~ BindingF syntax) => t -> Maybe Metavar
 maxBoundMetavariable = cata $ \ diff -> case diff of
-  Let bindings _ -> foldMaxMap (Just . fst) bindings
+  Let vars _ -> foldMaxMap (Just . fst) (unEnv vars)
   Var _ -> Nothing
 
 foldMaxMap :: (Foldable t, Ord b) => (a -> Maybe b) -> t a -> Maybe b
@@ -53,7 +53,7 @@ foldMaxMap f = foldr (max . f) Nothing
 
 
 letBind :: (Foldable syntax, Functor syntax, Corecursive t, Recursive t, Base t ~ BindingF syntax) => t -> (Metavar -> syntax t) -> t
-letBind diff f = embed (Let [(n, diff)] body)
+letBind diff f = embed (Let (Env [(n, diff)]) body)
   where body = f n
         n = maybe (Metavar 0) succ (foldMaxMap maxBoundMetavariable body)
 
@@ -74,7 +74,7 @@ envLookup var = lookup var . unEnv
 
 
 instance Eq1 f => Eq1 (BindingF f) where
-  liftEq eq (Let v1 b1) (Let v2 b2) = liftEq (liftEq eq) v1 v2 && liftEq eq b1 b2
+  liftEq eq (Let v1 b1) (Let v2 b2) = liftEq eq v1 v2 && liftEq eq b1 b2
   liftEq _  (Var v1)    (Var v2)    = v1 == v2
   liftEq _  _           _           = False
 
@@ -86,7 +86,7 @@ instance Eq1 Env where
 
 
 instance Show1 f => Show1 (BindingF f) where
-  liftShowsPrec sp sl d (Let vars body) = showsBinaryWith (const (liftShowList sp sl)) (liftShowsPrec sp sl) "Let" d vars body
+  liftShowsPrec sp sl d (Let vars body) = showsBinaryWith (liftShowsPrec sp sl) (liftShowsPrec sp sl) "Let" d vars body
   liftShowsPrec _  _  d (Var var)       = showsUnaryWith showsPrec "Var" d var
 
 instance (Show1 f, Show a) => Show (BindingF f a) where
@@ -100,7 +100,7 @@ instance Pretty Metavar where
   pretty (Metavar v) = pretty v
 
 instance Pretty1 f => Pretty1 (BindingF f) where
-  liftPretty p pl (Let vars body) = pretty ("let" :: String) <+> align (vsep (prettyKV <$> vars)) <> line
+  liftPretty p pl (Let vars body) = pretty ("let" :: String) <+> align (vsep (prettyKV <$> unEnv vars)) <> line
                                  <> pretty ("in" :: String)  <+> liftPretty p pl body
     where prettyKV (var, val) = pretty var <+> pretty '=' <+> p val
   liftPretty _ _  (Var metavar)   = pretty metavar
@@ -110,7 +110,7 @@ instance (Pretty1 f, Pretty a) => Pretty (BindingF f a) where
 
 
 instance ToJSONFields1 f => ToJSONFields1 (BindingF f) where
-  toJSONFields1 (Let vars body) = [ "vars" .= vars ] <> toJSONFields1 body
+  toJSONFields1 (Let vars body) = [ "vars" .= unEnv vars ] <> toJSONFields1 body
   toJSONFields1 (Var v)         = [ "metavar" .= v ]
 
 instance (ToJSONFields1 f, ToJSON a) => ToJSONFields (BindingF f a) where
