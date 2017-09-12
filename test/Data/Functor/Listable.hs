a140d2f4c4bd590fa60cf3e0ef44cdd49a7532a6
Rob Rix <rob.rix@me.com>
2017-09-12 10:47:50 -0400

    Correct the Listable1 instance for Diff.

--- test/Data/Functor/Listable.hs
+++ test/Data/Functor/Listable.hs
@@ -189,7 +189,7 @@ instance (Listable1 f, Listable a, Listable b) => Listable (DiffF f a b) where
   tiers = tiers1
 
 instance Listable1 f => Listable1 (Diff f) where
-  liftTiers annTiers = go where go = liftCons1 (liftTiers2 annTiers go) Diff
+  liftTiers annTiers = go where go = liftCons1 (liftCons1 (liftTiers2 annTiers go) (Let mempty)) Diff
 
 instance (Listable1 f, Listable a) => Listable (Diff f a) where
   tiers = tiers1
