9d2469a2bff10c1513059dd417dd86322167ae87
Rick Winfrey <rewinfrey@github.com>
2017-09-12 15:40:44 -0700

    Add pretty-show / colourised `pp` function

--- src/Semantic/Util.hs
+++ src/Semantic/Util.hs
@@ -1,8 +1,14 @@
 module Semantic.Util where
 
 import Data.Blob
+import Language.Haskell.HsColour (hscolour, Output(TTY))
+import Language.Haskell.HsColour.Colourise (defaultColourPrefs)
+import Text.Show.Pretty (ppShow)
 import Files
 
+-- Produces colorized pretty-printed output for the terminal / GHCi.
+pp :: Show a => a -> IO ()
+pp = putStrLn . hscolour TTY defaultColourPrefs False False "" False . ppShow
 
 file :: FilePath -> IO Blob
 file path = Files.readFile path (languageForFilePath path)
