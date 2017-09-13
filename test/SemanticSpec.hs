e36034455ca8b5df04acf7e5652352b4ec837279
Rob Rix <rob.rix@me.com>
2017-09-13 09:45:10 -0400

    Correct the tests.

--- test/SemanticSpec.hs
+++ test/SemanticSpec.hs
@@ -7,6 +7,7 @@ import Data.Functor.Both as Both
 import Data.Functor.Sum
 import Diff
 import Language
+import Patch
 import Renderer
 import Semantic
 import Semantic.Task
@@ -33,11 +34,11 @@ spec = parallel $ do
   describe "diffTermPair" $ do
     it "produces an Insert when the first blob is missing" $ do
       result <- runTask (diffTermPair (both (emptyBlob "/foo") (sourceBlob "/foo" Nothing "")) (runBothWith replacing) (pure (termIn () [])))
-      result `shouldBe` Diff (Let mempty (Either (In () (InR []))))
+      result `shouldBe` Diff (Let mempty (Patch (Insert (In () []))))
 
     it "produces a Delete when the second blob is missing" $ do
       result <- runTask (diffTermPair (both (sourceBlob "/foo" Nothing "") (emptyBlob "/foo")) (runBothWith replacing) (pure (termIn () [])))
-      result `shouldBe` Diff (Let mempty (Either (In () (InL []))))
+      result `shouldBe` Diff (Let mempty (Patch (Delete (In () []))))
 
   where
     methodsBlob = Blob "def foo\nend\n" "ff7bbbe9495f61d9e1e58c597502d152bab1761e" "methods.rb" (Just defaultPlainBlob) (Just Ruby)
