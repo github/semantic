a3a16f735cc27deaa543f51e927bddfad4b4a24e
Rob Rix <rob.rix@me.com>
2017-09-14 16:04:20 -0400

    Define a Symmetrical instance for Patch.

--- src/Patch.hs
+++ src/Patch.hs
@@ -11,6 +11,7 @@ import Data.Aeson
 import Data.Align
 import Data.Bifoldable
 import Data.Bifunctor
+import Data.Bifunctor.Symmetrical
 import Data.Bitraversable
 import Data.Functor.Classes
 import Data.JSON.Fields
@@ -46,6 +47,9 @@ instance Bifunctor Patch where
   bimap f _ (Delete a) = Delete (f a)
   bimap _ g (Insert b) = Insert (g b)
   bimap f g (Replace a b) = Replace (f a) (g b)
+
+instance Symmetrical Patch where
+  mirror = patch Insert Delete (flip Replace)
 
 instance Bifoldable Patch where
   bifoldMap f _ (Delete a) = f a
