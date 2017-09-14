8b5d7c60ecd9a016f9b84178f0300ac31637d4c6
Rob Rix <rob.rix@me.com>
2017-09-14 16:02:52 -0400

    Define a Symmetrical instance for (,).

--- src/Data/Bifunctor/Symmetrical.hs
+++ src/Data/Bifunctor/Symmetrical.hs
@@ -1,6 +1,11 @@
 module Data.Bifunctor.Symmetrical where
 
 import Data.Bifunctor
+import Data.Tuple (swap)
 
 class Bifunctor s => Symmetrical s where
   mirror :: s a b -> s b a
+
+
+instance Symmetrical (,) where
+  mirror = swap
