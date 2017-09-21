f76461382a684fcf046c35005fcac29e2f3242fd
joshvera <josh@joshvera.com>
2017-09-21 12:04:15 -0400

    Fix indentation

--- src/Semantic/Util.hs
+++ src/Semantic/Util.hs
@@ -33,19 +33,19 @@ file :: FilePath -> IO Blob
 file path = Files.readFile path (languageForFilePath path)
 
 diffWithParser :: (HasField fields Data.Span.Span,
-                                     HasField fields Range,
-                                     Error :< fs,
-                                     Declaration.Method :< fs,
-                                     Declaration.Function :< fs,
-                                     Empty :< fs,
-                                     Apply Eq1 fs, Apply Show1 fs,
-                                     Apply Traversable fs, Apply Functor fs,
-                                     Apply Foldable fs, Apply Diffable fs,
-                                     Apply GAlign fs
-                                     ) =>
-                                    Parser (Term (Data.Union.Union fs) (Record fields))
-                                    -> Both Blob
-                                    -> Task (Diff (Union fs) (Record (Maybe Declaration ': fields)) (Record (Maybe Declaration ': fields)))
+                   HasField fields Range,
+                   Error :< fs,
+                   Declaration.Method :< fs,
+                   Declaration.Function :< fs,
+                   Empty :< fs,
+                   Apply Eq1 fs, Apply Show1 fs,
+                   Apply Traversable fs, Apply Functor fs,
+                   Apply Foldable fs, Apply Diffable fs,
+                   Apply GAlign fs)
+                  =>
+                  Parser (Term (Data.Union.Union fs) (Record fields))
+                  -> Both Blob
+                  -> Task (Diff (Union fs) (Record (Maybe Declaration ': fields)) (Record (Maybe Declaration ': fields)))
 diffWithParser parser = run (\ blob -> parse parser blob >>= decorate (declarationAlgebra blob))
   where
     run parse sourceBlobs = distributeFor sourceBlobs parse >>= runBothWith (diffTermPair sourceBlobs diffRecursively)
