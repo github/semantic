bd4dee20d555c4a4be1226bdf464dd506a623415
Rob Rix <rob.rix@me.com>
2017-09-25 19:52:08 -0400

    Migrate DiffSpec to à la carte syntax.

--- test/DiffSpec.hs
+++ test/DiffSpec.hs
@@ -4,15 +4,30 @@ module DiffSpec where
 import Category
 import Data.Functor.Both
 import Data.Functor.Listable ()
+import qualified Data.Syntax as Syntax
+import qualified Data.Syntax.Comment as Comment
+import qualified Data.Syntax.Declaration as Declaration
+import qualified Data.Syntax.Statement as Statement
 import Data.Record
-import RWS
+import Data.Union
 import Diff
 import Info
 import Interpreter
-import Syntax
+import RWS
 import Term
 import Test.Hspec
 import Test.Hspec.LeanCheck
+
+type Syntax = Union
+  '[ Comment.Comment
+   , Declaration.Function
+   , Declaration.Method
+   , Statement.If
+   , Syntax.Context
+   , Syntax.Empty
+   , Syntax.Identifier
+   , []
+   ]
 
 spec :: Spec
 spec = parallel $ do
@@ -22,14 +37,14 @@ spec = parallel $ do
 
   prop "equal terms produce identity diffs" $
     \ a -> let term = decorate (a :: Term Syntax (Record '[Category])) in
-      diffCost (diffSyntaxTerms term term) `shouldBe` 0
+      diffCost (diffTerms term term) `shouldBe` 0
 
   describe "beforeTerm" $ do
     prop "recovers the before term" $
-      \ a b -> let diff = diffSyntaxTerms a b :: Diff Syntax (Record '[Category]) (Record '[Category]) in
+      \ a b -> let diff = diffTerms a b :: Diff Syntax (Record '[Category]) (Record '[Category]) in
         beforeTerm diff `shouldBe` Just a
 
   describe "afterTerm" $ do
     prop "recovers the after term" $
-      \ a b -> let diff = diffSyntaxTerms a b :: Diff Syntax (Record '[Category]) (Record '[Category]) in
+      \ a b -> let diff = diffTerms a b :: Diff Syntax (Record '[Category]) (Record '[Category]) in
         afterTerm diff `shouldBe` Just b
