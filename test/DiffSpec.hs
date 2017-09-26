69aae9986d5a3e32bc78b22b70d642693d91107e
Rob Rix <rob.rix@me.com>
2017-09-26 09:59:31 -0400

    Ensure that c is distinct from both a and b.

--- test/DiffSpec.hs
+++ test/DiffSpec.hs
@@ -37,12 +37,12 @@ spec = parallel $ do
 
   prop "forward permutations are changes" $
     \ a b -> let wrap = termIn (Program :. Nil) . Indexed
-                 c = wrap [a] in
+                 c = wrap [a, b] in
       diffTerms (wrap [a, b, c]) (wrap [c, a, b :: Term Syntax (Record '[Category])]) `shouldBe` merge (Program :. Nil, Program :. Nil) (Indexed [ inserting c, mergeTerm a, mergeTerm b, deleting c ])
 
   prop "backward permutations are changes" $
     \ a b -> let wrap = termIn (Program :. Nil) . Indexed
-                 c = wrap [a] in
+                 c = wrap [a, b] in
       diffTerms (wrap [a, b, c]) (wrap [b, c, a :: Term Syntax (Record '[Category])]) `shouldBe` merge (Program :. Nil, Program :. Nil) (Indexed [ deleting a, mergeTerm b, mergeTerm c, inserting a ])
 
 mergeTerm :: Functor syntax => Term syntax ann -> Diff syntax ann ann
