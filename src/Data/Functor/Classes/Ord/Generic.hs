2ba30e3fee3d8bf8c3deb56a8436d7e5122d2b01
Rob Rix <rob.rix@me.com>
2017-09-29 11:41:43 -0400

    Give a generic derivation suitable for liftCompare.

--- src/Data/Functor/Classes/Ord/Generic.hs
+++ src/Data/Functor/Classes/Ord/Generic.hs
@@ -1,4 +1,5 @@
 module Data.Functor.Classes.Ord.Generic where
+import GHC.Generics
 
 -- | Generically-derivable lifting of the 'Ord' class to unary type constructors.
 class GOrd1 f where
@@ -6,3 +7,7 @@ class GOrd1 f where
   --
   --   The function will usually be applied to a comparison function, but the more general type ensures that the implementation uses it to compare elements of the first container with elements of the second.
   gliftCompare :: (a -> b -> Ordering) -> f a -> f b -> Ordering
+
+-- | A suitable implementation of Ord1’s liftCompare for Generic1 types.
+genericLiftCompare :: (Generic1 f, GOrd1 (Rep1 f)) => (a -> b -> Ordering) -> f a -> f b -> Ordering
+genericLiftCompare f a b = gliftCompare f (from1 a) (from1 b)
