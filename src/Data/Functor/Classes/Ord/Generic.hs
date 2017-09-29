fe53309b05f44733b4222d10eab496354d5a546d
Rob Rix <rob.rix@me.com>
2017-09-29 11:41:24 -0400

    Define a generically-derivable lifting of Ord to * -> *.

--- src/Data/Functor/Classes/Ord/Generic.hs
+++ src/Data/Functor/Classes/Ord/Generic.hs
@@ -1,1 +1,8 @@
 module Data.Functor.Classes.Ord.Generic where
+
+-- | Generically-derivable lifting of the 'Ord' class to unary type constructors.
+class GOrd1 f where
+  -- | Lift a comparison function through the type constructor.
+  --
+  --   The function will usually be applied to a comparison function, but the more general type ensures that the implementation uses it to compare elements of the first container with elements of the second.
+  gliftCompare :: (a -> b -> Ordering) -> f a -> f b -> Ordering
