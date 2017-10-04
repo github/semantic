f0edcd48f2bf8046360334f5e1773951ef1df7b9
Timothy Clem <timothy.clem@gmail.com>
2017-10-04 11:40:20 -0700

    Docs

--- src/Semantic/Queue.hs
+++ src/Semantic/Queue.hs
@@ -4,19 +4,22 @@ import Control.Concurrent.Async as Async
 import Control.Concurrent.STM.TMQueue
 import GHC.Conc
 
+-- Represents a TMQueue that's drained from a separate thread.
 data AsyncQ a b
   = AsyncQ
-  { queue :: TMQueue a
-  , sink :: Async ()
-  , extra :: b
+  { queue :: TMQueue a -- The queue.
+  , sink :: Async ()   -- A sink that will drain the queue.
+  , extra :: b         -- Any extra data the queue needs to use.
   }
 
+-- Create a new AsyncQ.
 newQueue :: (b -> TMQueue a -> IO ()) -> b-> IO (AsyncQ a b)
 newQueue f b = do
   q <- newTMQueueIO
   sink <- Async.async (f b q)
   pure (AsyncQ q sink b)
 
+-- Close the queue.
 closeQueue :: AsyncQ a b -> IO ()
 closeQueue AsyncQ{..} = do
   atomically (closeTMQueue queue)
