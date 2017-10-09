ea6a97c9c052399f98f29de703f0a308b4bf7b0a
Rick Winfrey <rewinfrey@github.com>
2017-10-09 11:54:29 -0700

    Assign ! unaryExpressions

--- src/Language/Go/Syntax.hs
+++ src/Language/Go/Syntax.hs
@@ -115,6 +115,7 @@ expressionChoices =
   , structType
   , typeDeclaration
   , typeIdentifier
+  , unaryExpression
   ]
 
 identifiers :: Assignment
@@ -255,6 +256,10 @@ typeDeclaration = handleError $ makeTerm <$> symbol TypeDeclaration <*> children
 
 parenthesizedExpression :: Assignment
 parenthesizedExpression = symbol ParenthesizedExpression *> children expressions
+
+unaryExpression :: Assignment
+unaryExpression = symbol UnaryExpression >>= \ location ->
+      makeTerm location . Expression.Not <$> children (symbol AnonBang *> expression)
 binaryExpression :: Assignment
 binaryExpression = makeTerm' <$> symbol BinaryExpression <*> children (infixTerm expression expression
   [ (inj .) . Expression.Plus             <$ symbol AnonPlus
