d274004231f36c0ca662b03519108b282aa415da
Rick Winfrey <rewinfrey@github.com>
2017-10-10 12:15:09 -0700

    Update instances to include Ord1

--- src/Data/Syntax/Comment.hs
+++ src/Data/Syntax/Comment.hs
@@ -5,6 +5,7 @@ import Algorithm
 import Data.Align.Generic
 import Data.ByteString (ByteString)
 import Data.Functor.Classes.Eq.Generic
+import Data.Functor.Classes.Ord.Generic
 import Data.Functor.Classes.Show.Generic
 import Data.Mergeable
 import GHC.Generics
@@ -14,6 +15,7 @@ newtype Comment a = Comment { commentContent :: ByteString }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Show, Traversable)
 
 instance Eq1 Comment where liftEq = genericLiftEq
+instance Ord1 Comment where liftCompare = genericLiftCompare
 instance Show1 Comment where liftShowsPrec = genericLiftShowsPrec
 
 -- TODO: nested comment types
