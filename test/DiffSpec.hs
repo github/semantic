3462892131b08b9a16b3d2ec8c18825edbbd1ff2
Rob Rix <rob.rix@me.com>
2017-10-23 11:59:19 -0400

    :fire: redundant imports.

--- test/DiffSpec.hs
+++ test/DiffSpec.hs
@@ -2,14 +2,8 @@
 module DiffSpec where
 
 import Data.Diff
-import Data.Functor.Both
-import Data.Functor.Foldable (cata)
 import Data.Functor.Listable (ListableSyntax)
 import Data.Record
-import Data.Term
-import Data.Union
-import Interpreter
-import RWS
 import Test.Hspec
 import Test.Hspec.LeanCheck
 
