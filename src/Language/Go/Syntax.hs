02adafb52623b77a5b4e635b7754c47272df0e25
Rick Winfrey <rewinfrey@github.com>
2017-10-24 17:38:24 -0700

    Add Go.Syntax for Variadic function arguments / parameters

---
+++ src/Language/Go/Syntax.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE DeriveAnyClass #-}
+module Language.Go.Syntax where
+
+import Algorithm
+import Data.Align.Generic
+import Data.Functor.Classes.Eq.Generic
+import Data.Functor.Classes.Ord.Generic
+import Data.Functor.Classes.Show.Generic
+import Data.Mergeable
+import GHC.Generics
+
+-- | Variadic (`...Type` used in function declarations, and `Type...` used in function calls).
+newtype Variadic a = Variadic a
+  deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+instance Eq1 Variadic where liftEq = genericLiftEq
+instance Ord1 Variadic where liftCompare = genericLiftCompare
+instance Show1 Variadic where liftShowsPrec = genericLiftShowsPrec
