840d6868919a9863021857639a82615aede9673b
Rick Winfrey <rewinfrey@github.com>
2017-10-25 16:39:21 -0700

    Update Variadic to contain a context
    - This is useful for capturing the type information for a variadic argument or parameter

--- src/Language/Go/Syntax.hs
+++ src/Language/Go/Syntax.hs
@@ -10,7 +10,7 @@ import Data.Mergeable
 import GHC.Generics
 
 -- | Variadic (`...Type` used in function declarations, and `Type...` used in function calls).
-newtype Variadic a = Variadic a
+data Variadic a = Variadic { variadicContext :: [a], variadicIdentifier :: a }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
 instance Eq1 Variadic where liftEq = genericLiftEq
