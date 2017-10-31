8b9858f312bf537b2a2be6978240aeafba3973f8
Rick Winfrey <rewinfrey@github.com>
2017-10-30 17:49:18 -0700

    Add DefaultPattern constructor for Go Syntax

--- src/Language/Go/Syntax.hs
+++ src/Language/Go/Syntax.hs
@@ -16,3 +16,11 @@ data Variadic a = Variadic { variadicContext :: [a], variadicIdentifier :: a }
 instance Eq1 Variadic where liftEq = genericLiftEq
 instance Ord1 Variadic where liftCompare = genericLiftCompare
 instance Show1 Variadic where liftShowsPrec = genericLiftShowsPrec
+
+-- | A pattern in a pattern-matching or computed jump control-flow statement, like 'case' in C or JavaScript, 'when' in Ruby, or the left-hand side of '->' in the body of Haskell 'case' expressions.
+data DefaultPattern a = DefaultPattern { patternBody :: !a }
+  deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+instance Eq1 DefaultPattern where liftEq = genericLiftEq
+instance Ord1 DefaultPattern where liftCompare = genericLiftCompare
+instance Show1 DefaultPattern where liftShowsPrec = genericLiftShowsPrec
