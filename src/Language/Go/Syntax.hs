cca7590d37b4389dd90c404400b76877eda26a4e
Rick Winfrey <rewinfrey@github.com>
2017-10-31 16:45:45 -0700

    Add RuneLiteral constructor

--- src/Language/Go/Syntax.hs
+++ src/Language/Go/Syntax.hs
@@ -3,6 +3,7 @@ module Language.Go.Syntax where
 
 import Algorithm
 import Data.Align.Generic
+import Data.ByteString (ByteString)
 import Data.Functor.Classes.Eq.Generic
 import Data.Functor.Classes.Ord.Generic
 import Data.Functor.Classes.Show.Generic
@@ -24,3 +25,11 @@ newtype DefaultPattern a = DefaultPattern { defaultPatternBody :: a }
 instance Eq1 DefaultPattern where liftEq = genericLiftEq
 instance Ord1 DefaultPattern where liftCompare = genericLiftCompare
 instance Show1 DefaultPattern where liftShowsPrec = genericLiftShowsPrec
+
+-- | A rune literal in Go (e.g. '⌘')
+newtype RuneLiteral a = RuneLiteral { runeLiteralContent :: ByteString }
+  deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+instance Eq1 RuneLiteral where liftEq = genericLiftEq
+instance Ord1 RuneLiteral where liftCompare = genericLiftCompare
+instance Show1 RuneLiteral where liftShowsPrec = genericLiftShowsPrec
