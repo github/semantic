f1cc87228541234af1a46e3cdcee37523526eee7
Rick Winfrey <rewinfrey@github.com>
2017-10-31 23:07:57 -0700

    Update :memo:

--- src/Language/Go/Syntax.hs
+++ src/Language/Go/Syntax.hs
@@ -10,7 +10,7 @@ import Data.Functor.Classes.Show.Generic
 import Data.Mergeable
 import GHC.Generics
 
--- | Variadic (`...Type` used in function declarations, and `Type...` used in function calls).
+-- | Variadic arguments and parameters in Go (e.g. parameter: `param ...Type`, argument: `Type...`).
 data Variadic a = Variadic { variadicContext :: [a], variadicIdentifier :: a }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
@@ -18,7 +18,7 @@ instance Eq1 Variadic where liftEq = genericLiftEq
 instance Ord1 Variadic where liftCompare = genericLiftCompare
 instance Show1 Variadic where liftShowsPrec = genericLiftShowsPrec
 
--- | A pattern in a pattern-matching or computed jump control-flow statement, like 'case' in C or JavaScript, 'when' in Ruby, or the left-hand side of '->' in the body of Haskell 'case' expressions.
+-- | A default pattern in a Go case statement (e.g. `switch { default: s() }`).
 newtype DefaultPattern a = DefaultPattern { defaultPatternBody :: a }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
@@ -34,7 +34,7 @@ instance Eq1 RuneLiteral where liftEq = genericLiftEq
 instance Ord1 RuneLiteral where liftCompare = genericLiftCompare
 instance Show1 RuneLiteral where liftShowsPrec = genericLiftShowsPrec
 
--- | A labeled statement in Go (e.g. a:continue)
+-- | A labeled statement in Go (e.g. label:continue)
 data Label a = Label { labelName :: a, labelStatement :: a }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
@@ -42,7 +42,7 @@ instance Eq1 Label where liftEq = genericLiftEq
 instance Ord1 Label where liftCompare = genericLiftCompare
 instance Show1 Label where liftShowsPrec = genericLiftShowsPrec
 
--- | A send statement in Go (e.g. chan <- value)
+-- | A send statement in Go (e.g. channel <- value).
 data Send a = Send { sendReceiver :: a, sendValue :: a }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
