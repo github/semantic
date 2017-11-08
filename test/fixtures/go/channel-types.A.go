c883c9926e2c0ef3dff8966c07f7d7db8549343e
Rick Winfrey <rewinfrey@github.com>
2017-11-07 16:57:03 -0800

    Add additional channel type tests

--- test/fixtures/go/channel-types.A.go
+++ test/fixtures/go/channel-types.A.go
@@ -5,5 +5,7 @@ type (
 c1 chan<- chan int
 c2 chan<- chan<- struct{}
 c3 chan<- <-chan int
+c4 <-chan <-chan int
+c5 chan (<-chan int)
 )
 }
--- test/fixtures/go/channel-types.B.go
+++ test/fixtures/go/channel-types.B.go
@@ -5,5 +5,7 @@ type (
 c2 chan<- chan string
 c3 chan<- chan<- struct{}
 c4 chan<- <-chan string
+c4 <-chan <-chan string
+c5 chan (<-chan string)
 )
 }
