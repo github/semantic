99677709ed5c5ebd29020705945cd3578adbfc44
Rick Winfrey <rewinfrey@github.com>
2017-11-07 16:28:17 -0800

    Add additional method declaration test cases

--- test/fixtures/go/method-declarations.B.go
+++ test/fixtures/go/method-declarations.B.go
@@ -3,3 +3,12 @@ package main
 func main() {}
 
 func (self Num) Equals(other Num) bool {}
+
+func (p *Point) OtherLength() float64 {
+	return math.Sqrt(math.Pow(p.x, 2) + p.x + math.Pow(p.y, 2) + p.y)
+}
+
+func (q *Point) Scale(factor int) {
+	p.x *= factor
+	p.y *= factor
+}
