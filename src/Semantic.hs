2262de15c9aad76b879b76b7576daad1722b329f
Rob Rix <rob.rix@me.com>
2017-11-21 12:45:08 -0500

    syntaxParser is no more.

--- src/Semantic.hs
+++ src/Semantic.hs
@@ -58,10 +58,8 @@ parseBlob renderer blob@Blob{..} = case (renderer, blobLanguage) of
       parse parser blob >>= decorate constructorLabel . (Nil <$) >>= render renderSExpressionTerm
 
   (TagsTermRenderer, lang)
-    | Just (SomeParser parser) <- lang >>= someParser (Proxy :: Proxy '[HasDeclaration, Foldable, Functor]) ->
+    | Just (SomeParser parser) <- someParser (Proxy :: Proxy '[HasDeclaration, Foldable, Functor]) <$> lang ->
       parse parser blob >>= decorate (declarationAlgebra blob) >>= render (renderToTags blob)
-    | Just syntaxParser <- lang >>= syntaxParserForLanguage ->
-      parse syntaxParser blob >>= decorate (syntaxDeclarationAlgebra blob) >>= render (renderToTags blob)
 
   _ -> throwError (SomeException (NoParserForLanguage blobPath blobLanguage))
 
