61cf444f3834a83df53a860f6fe4f7d7608f84b9
Rick Winfrey <rewinfrey@github.com>
2017-11-20 20:23:10 -0800

    Update label statements tests

--- test/fixtures/go/label-statements.A.go
+++ test/fixtures/go/label-statements.A.go
@@ -1,7 +1,11 @@
 package main
 
 func main() {
-{
-  insert_label:
+L: ;  // ';' terminates empty statement => L does not apply to for loop
+L1: { // L1 labels block => L1 does not apply to for loop
+	for i := 0; i < 10; i++ {
+		println(i);
+		break L1;  // comment
+	}
 }
 }
