596c4535abf57f3df76f2a496ab8563db5fc5cf7
Rick Winfrey <rewinfrey@github.com>
2017-11-21 13:32:33 -0800

    :memo: channel types in Go

--- src/Language/Go/Type.hs
+++ src/Language/Go/Type.hs
@@ -9,6 +9,7 @@ import Data.Functor.Classes.Show.Generic
 import Data.Mergeable
 import GHC.Generics
 
+-- | A Bidirectional channel in Go (e.g. `chan`).
 newtype BiDirectionalChannel a = BiDirectionalChannel { biDirectionalChannelElementType :: a }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
@@ -16,6 +17,7 @@ instance Eq1 BiDirectionalChannel where liftEq = genericLiftEq
 instance Ord1 BiDirectionalChannel where liftCompare = genericLiftCompare
 instance Show1 BiDirectionalChannel where liftShowsPrec = genericLiftShowsPrec
 
+-- | A Receive channel in Go (e.g. `<-chan`).
 newtype ReceiveChannel a = ReceiveChannel { receiveChannelElementType :: a }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
@@ -23,6 +25,7 @@ instance Eq1 ReceiveChannel where liftEq = genericLiftEq
 instance Ord1 ReceiveChannel where liftCompare = genericLiftCompare
 instance Show1 ReceiveChannel where liftShowsPrec = genericLiftShowsPrec
 
+-- | A Send channel in Go (e.g. `chan<-`).
 newtype SendChannel a = SendChannel { sendChannelElementType :: a }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
