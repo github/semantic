ae473f7ad239dceba98081a211517006434b376c
Rob Rix <rob.rix@me.com>
2017-12-01 13:57:56 -0500

    Define a Functor instance for Live.

--- src/Data/Abstract/Live.hs
+++ src/Data/Abstract/Live.hs
@@ -4,3 +4,6 @@ import Data.Abstract.Address
 import Data.Set as Set
 
 newtype Live l v = Live { unLive :: Set (Address l v) }
+
+instance Ord l => Functor (Live l) where
+  fmap f (Live as) = Live (Set.map (fmap f) as)
