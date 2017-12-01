51b8461be36d01d60d0c4a1e259ea07d8a2a4b50
Rob Rix <rob.rix@me.com>
2017-11-30 19:49:26 -0500

    Abstract the tracing evaluators over the term type.

--- src/Analysis/Abstract/Tracing.hs
+++ src/Analysis/Abstract/Tracing.hs
@@ -13,13 +13,11 @@ import Control.Monad.Effect.Writer
 import Data.Abstract.Address
 import Data.Abstract.Configuration
 import Data.Abstract.Eval
-import Data.Abstract.FreeVariables
 import Data.Abstract.Value
 import Data.Function (fix)
-import Data.Functor.Classes (Ord1)
+import Data.Functor.Foldable (Base, Recursive(..))
 import Data.Semigroup
 import Data.Set hiding (fromList)
-import Data.Term
 import GHC.Exts (IsList(Item, fromList))
 
 type TracingInterpreter t v g = Reader (Set (Address (LocationFor v) v)) ': Writer (g (Configuration (LocationFor v) t v)) ': Interpreter v
@@ -34,29 +32,29 @@ type ReachableStateInterpreter t v = TracingInterpreter t v Set
 --    evalTrace @(Value Syntax Precise) <term>
 --    evalReach @(Value Syntax Precise) <term>
 
-evalTrace :: forall v syntax ann
-          . ( Ord v, Ord ann, Ord1 syntax, Ord (Cell (LocationFor v) v)
-            , FreeVariables1 syntax
-            , Functor syntax
-            , MonadAddress (LocationFor v) (Eff (TraceInterpreter (Term syntax ann) v))
-            , MonadGC v (Eff (TraceInterpreter (Term syntax ann) v))
+evalTrace :: forall v term
+          . ( Ord v, Ord term, Ord (Cell (LocationFor v) v)
+            , Functor (Base term)
+            , Recursive term
+            , MonadAddress (LocationFor v) (Eff (TraceInterpreter term v))
+            , MonadGC v (Eff (TraceInterpreter term v))
             , Semigroup (Cell (LocationFor v) v)
-            , Eval (Term syntax ann) v (Eff (TraceInterpreter (Term syntax ann) v)) syntax
+            , Eval term v (Eff (TraceInterpreter term v)) (Base term)
             )
-          => Term syntax ann -> Final (TracingInterpreter (Term syntax ann) v []) v
-evalTrace = run @(TraceInterpreter (Term syntax ann) v) . fix (evTell @[] ev) pure
+          => term -> Final (TracingInterpreter term v []) v
+evalTrace = run @(TraceInterpreter term v) . fix (evTell @[] ev) pure
 
-evalReach :: forall v syntax ann
-          . ( Ord v, Ord ann, Ord (LocationFor v), Ord (Cell (LocationFor v) v), Ord1 syntax
-            , FreeVariables1 syntax
-            , Functor syntax
-            , MonadAddress (LocationFor v) (Eff (ReachableStateInterpreter (Term syntax ann) v))
-            , MonadGC v (Eff (ReachableStateInterpreter (Term syntax ann) v))
+evalReach :: forall v term
+          . ( Ord v, Ord term, Ord (LocationFor v), Ord (Cell (LocationFor v) v)
+            , Functor (Base term)
+            , Recursive term
+            , MonadAddress (LocationFor v) (Eff (ReachableStateInterpreter term v))
+            , MonadGC v (Eff (ReachableStateInterpreter term v))
             , Semigroup (Cell (LocationFor v) v)
-            , Eval (Term syntax ann) v (Eff (ReachableStateInterpreter (Term syntax ann) v)) syntax
+            , Eval term v (Eff (ReachableStateInterpreter term v)) (Base term)
             )
-          => Term syntax ann -> Final (TracingInterpreter (Term syntax ann) v Set) v
-evalReach = run @(ReachableStateInterpreter (Term syntax ann) v) . fix (evTell @Set ev) pure
+          => term -> Final (TracingInterpreter term v Set) v
+evalReach = run @(ReachableStateInterpreter term v) . fix (evTell @Set ev) pure
 
 
 evTell :: forall g t m v
