4ef26933dc6929949e84f235192ee7be1ca06e49
Rob Rix <rob.rix@me.com>
2017-12-06 08:58:06 -0700

    Stub in functions to render to DOT.

--- src/Rendering/DOT.hs
+++ src/Rendering/DOT.hs
@@ -1,1 +1,11 @@
 module Rendering.DOT where
+
+import qualified Data.ByteString as B
+import Data.Diff
+import Data.Term
+
+renderDOTDiff :: Diff syntax ann1 ann2 -> B.ByteString
+renderDOTDiff _ = ""
+
+renderDOTTerm :: Term syntax ann -> B.ByteString
+renderDOTTerm _ = ""
