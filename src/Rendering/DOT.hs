55308e6fc609d8892f35369704a8b1466060e402
Rob Rix <rob.rix@me.com>
2017-12-06 09:29:04 -0700

    Define Graph, Node, & Edge datatypes.

--- src/Rendering/DOT.hs
+++ src/Rendering/DOT.hs
@@ -11,3 +11,12 @@ renderDOTDiff _ _ = ""
 
 renderDOTTerm :: Blob -> Term syntax ann -> B.ByteString
 renderDOTTerm _ _ = ""
+
+data Graph = Graph { graphName :: Maybe B.ByteString, graphNodes :: [Node], graphEdges :: [Edge] }
+  deriving (Eq, Ord, Show)
+
+data Node = Node { nodeID :: Int, nodeLabel :: B.ByteString }
+  deriving (Eq, Ord, Show)
+
+data Edge = Edge { edgeFrom :: Int, edgeTo :: Int }
+  deriving (Eq, Ord, Show)
