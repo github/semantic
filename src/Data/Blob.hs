30f5b9bd4b4d25daa2e96e5fdaa6c31c54d27aeb
Timothy Clem <timothy.clem@gmail.com>
2017-12-11 11:09:37 -0800

    Just moving things around

--- src/Data/Blob.hs
+++ src/Data/Blob.hs
@@ -1,9 +1,9 @@
 module Data.Blob
 ( Blob(..)
-, These(..)
 , nullBlob
 , sourceBlob
 , BlobPair
+, These(..)
 , blobPairDiffing
 , blobPairInserting
 , blobPairDeleting
@@ -16,6 +16,21 @@ import Data.Bifunctor.Join
 import Data.Language
 import Data.These
 import Data.Source as Source
+
+
+-- | The source, oid, path, kind and language of a blob.
+data Blob = Blob
+  { blobSource :: Source -- ^ The UTF-8 encoded source text of the blob.
+  , blobPath :: FilePath -- ^ The file path to the blob.
+  , blobLanguage :: Maybe Language -- ^ The language of this blob. Nothing denotes a langauge we don't support yet.
+  }
+  deriving (Show, Eq)
+
+nullBlob :: Blob -> Bool
+nullBlob Blob{..} = nullSource blobSource
+
+sourceBlob :: FilePath -> Maybe Language -> Source -> Blob
+sourceBlob filepath language source = Blob source filepath language
 
 
 -- | Represents a blobs suitable for diffing which can be either a blob to
@@ -45,18 +60,3 @@ pathForBlobPair (Join (These _ Blob{..})) = blobPath
 languageTagForBlobPair :: BlobPair -> [(String, String)]
 languageTagForBlobPair pair = maybe [] showLanguage (languageForBlobPair pair)
   where showLanguage = pure . (,) "language" . show
-
-
--- | The source, oid, path, kind and language of a blob.
-data Blob = Blob
-  { blobSource :: Source -- ^ The UTF-8 encoded source text of the blob.
-  , blobPath :: FilePath -- ^ The file path to the blob.
-  , blobLanguage :: Maybe Language -- ^ The language of this blob. Nothing denotes a langauge we don't support yet.
-  }
-  deriving (Show, Eq)
-
-nullBlob :: Blob -> Bool
-nullBlob Blob{..} = nullSource blobSource
-
-sourceBlob :: FilePath -> Maybe Language -> Source -> Blob
-sourceBlob filepath language source = Blob source filepath language
