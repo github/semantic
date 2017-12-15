dca96401696955882e6595acbf220bd9fc738b8a
joshvera <josh@joshvera.com>
2017-12-15 11:22:30 -0800

    Stub comment and string in php assignment

--- src/Language/PHP/Assignment.hs
+++ src/Language/PHP/Assignment.hs
@@ -1,4 +1,53 @@
 {-# LANGUAGE DataKinds, RankNTypes, TypeOperators #-}
 module Language.PHP.Assignment
-(
+( assignment
+, Syntax
+, Grammar
+, Term
 ) where
+
+import Data.Union
+import Data.Record
+import qualified Data.Term as Term
+import Data.Syntax (emptyTerm, handleError, parseError, infixContext, makeTerm, makeTerm', makeTerm1, contextualize, postContextualize)
+import qualified Data.Syntax as Syntax
+import qualified Data.Syntax.Comment as Comment
+import qualified Data.Syntax.Declaration as Declaration
+import qualified Data.Syntax.Expression as Expression
+import qualified Data.Syntax.Literal as Literal
+import qualified Data.Syntax.Statement as Statement
+import qualified Data.Syntax.Type as Type
+import Assigning.Assignment hiding (Assignment, Error)
+import qualified Assigning.Assignment as Assignment
+import Language.PHP.Grammar as Grammar
+import Data.List.NonEmpty (some1)
+
+type Syntax = '[
+    Literal.TextElement
+  , Comment.Comment
+  , Syntax.Empty
+  , Syntax.Error
+  , Syntax.Context
+  , Syntax.Program
+  , [] ]
+
+type Term = Term.Term (Data.Union.Union Syntax) (Record Location)
+type Assignment = Assignment.Assignment [] Grammar Term
+
+-- | Assignment from AST in TypeScript’s grammar onto a program in TypeScript’s syntax.
+assignment :: Assignment
+assignment = handleError $ makeTerm <$> symbol Program <*> children (Syntax.Program <$> manyTerm statement) <|> parseError
+
+-- | Match a term optionally preceded by comment(s), or a sequence of comments if the term is not present.
+manyTerm :: Assignment -> Assignment.Assignment [] Grammar [Term]
+manyTerm term = many (contextualize comment term <|> makeTerm1 <$> (Syntax.Context <$> some1 comment <*> emptyTerm))
+
+statement :: Assignment
+statement = handleError everything
+  where everything = choice [ string ]
+
+comment :: Assignment
+comment = makeTerm <$> symbol Comment <*> (Comment.Comment <$> source)
+
+string :: Assignment
+string = makeTerm <$> symbol Grammar.String <*> (Literal.TextElement <$> source)
