ff43062310cc9a4f50a74666e8bc10a5435834c4
Rob Rix <rob.rix@me.com>
2017-12-18 11:07:40 -0500

    Correct how the graph title is constructed.

--- src/Rendering/DOT.hs
+++ src/Rendering/DOT.hs
@@ -19,7 +19,8 @@ import Data.Term
 import Data.These (These, mergeThese)
 
 renderDOTDiff :: (ConstructorName syntax, Foldable syntax, Functor syntax) => Join These Blob -> Diff syntax ann1 ann2 -> B.ByteString
-renderDOTDiff blobs diff = renderGraph (snd (cata diffAlgebra diff 0)) { graphName = Just (mergeThese ((<>) . (" -> " <>)) (runJoin (fmap (B.pack . blobPath) blobs))) }
+renderDOTDiff blobs diff = renderGraph (snd (cata diffAlgebra diff 0)) { graphName = Just (B.pack (mergeThese combine (runJoin (blobPath <$> blobs)))) }
+  where combine p1 p2 = p1 <> " -> " <> p2
 
 renderDOTTerm :: (ConstructorName syntax, Foldable syntax, Functor syntax) => Blob -> Term syntax ann -> B.ByteString
 renderDOTTerm Blob{..} term = renderGraph (snd (cata termAlgebra term 0)) { graphName = Just (B.pack blobPath) }
