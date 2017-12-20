6cf473d72f977a3ddf9cecf586d17abbd86441c2
Rob Rix <rob.rix@me.com>
2017-12-20 17:07:17 -0500

    :memo: Configuration’s fields.

--- src/Data/Abstract/Configuration.hs
+++ src/Data/Abstract/Configuration.hs
@@ -11,10 +11,10 @@ import GHC.Generics
 -- | A single point in a program’s execution.
 data Configuration l t v
   = Configuration
-    { configurationTerm :: t
-    , configurationRoots :: Live l v
-    , configurationEnvironment :: Environment l v
-    , configurationStore :: Store l v
+    { configurationTerm :: t                      -- ^ The “instruction,” i.e. the current term to evaluate.
+    , configurationRoots :: Live l v              -- ^ The set of rooted addresses.
+    , configurationEnvironment :: Environment l v -- ^ The environment binding any free variables in 'configurationTerm'.
+    , configurationStore :: Store l v             -- ^ The store of values.
     }
     deriving (Generic1)
 
