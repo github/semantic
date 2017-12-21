ec5f85295187d0a62bb9dc706c71ab55aac5c8c5
Rob Rix <rob.rix@me.com>
2017-12-21 11:08:41 -0500

    :memo: collect.

--- src/Control/Monad/Effect/NonDet.hs
+++ src/Control/Monad/Effect/NonDet.hs
@@ -10,6 +10,7 @@ import Control.Monad.Effect.NonDetEff
 
 -- | 'Monad's offering local isolation of nondeterminism effects.
 class (Alternative m, Monad m) => MonadNonDet m where
+  -- | Run a computation, collecting any local nondeterminism into a 'Monoid'al value.
   collect :: Monoid b => (a -> b) -> m a -> m b
 
 instance (NonDetEff :< fs) => MonadNonDet (Eff fs) where
