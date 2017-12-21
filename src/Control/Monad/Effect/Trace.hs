c5ecc98758a76bf2e2938ac8398c69f15db2b9ab
Rob Rix <rob.rix@me.com>
2017-12-21 11:19:54 -0500

    :memo: the type parameters to MonadTrace.

--- src/Control/Monad/Effect/Trace.hs
+++ src/Control/Monad/Effect/Trace.hs
@@ -7,6 +7,8 @@ import Data.Abstract.Configuration
 import Data.Abstract.Value
 
 -- | 'Monad's offering a writable trace of configurations.
+--
+--   @t@ is the type of terms, @v@ the type of values, @g@ the type of the collection represented by the log (e.g. '[]' for regular traces, or @Set@ for the trace of reachable states).
 class Monad m => MonadTrace t v g m where
   -- | Log the given collection of configurations.
   trace :: g (Configuration (LocationFor v) t v) -> m ()
