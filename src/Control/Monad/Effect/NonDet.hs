3c842832c0bca62c0ba7d6db1f31191fd47144cb
Rob Rix <rob.rix@me.com>
2017-12-21 11:08:54 -0500

    Reformat the signature for collect.

--- src/Control/Monad/Effect/NonDet.hs
+++ src/Control/Monad/Effect/NonDet.hs
@@ -11,7 +11,10 @@ import Control.Monad.Effect.NonDetEff
 -- | 'Monad's offering local isolation of nondeterminism effects.
 class (Alternative m, Monad m) => MonadNonDet m where
   -- | Run a computation, collecting any local nondeterminism into a 'Monoid'al value.
-  collect :: Monoid b => (a -> b) -> m a -> m b
+  collect :: Monoid b
+          => (a -> b)
+          -> m a
+          -> m b
 
 instance (NonDetEff :< fs) => MonadNonDet (Eff fs) where
   collect f = interpose (pure . f) (\ m k -> case m of
