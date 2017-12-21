9cac249c847df63b9fc242bb539b9c841001200f
Rob Rix <rob.rix@me.com>
2017-12-21 10:56:04 -0500

    :memo: reset.

--- src/Control/Monad/Effect/Fresh.hs
+++ src/Control/Monad/Effect/Fresh.hs
@@ -15,6 +15,8 @@ data Fresh a where
 class Monad m => MonadFresh m where
   -- | Get a fresh variable name, guaranteed unused (since the last 'reset').
   fresh :: m TName
+
+  -- | Reset the sequence of variable names. Useful to avoid complicated alpha-equivalence comparisons when iteratively recomputing the results of an analysis til convergence.
   reset :: TName -> m ()
 
 instance (Fresh :< fs) => MonadFresh (Eff fs) where
