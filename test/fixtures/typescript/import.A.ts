cbb003499fb32b2eb5a0fb35a1afc442ebb8c9b1
joshvera <josh@joshvera.com>
2018-01-16 18:17:12 -0500

    Add a import require test

--- test/fixtures/typescript/import.A.ts
+++ test/fixtures/typescript/import.A.ts
@@ -6,3 +6,4 @@ import { member1 , member2 as alias2 } from "ant-eater";
 import defaultMember, { member1, member2 as alias2 } from "anaconda";
 import defaultMember, * as name from "alligator";
 import "arctic-tern";
+import zip = require("../zip");
