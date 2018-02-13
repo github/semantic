6d1dc86761c86fe11cc67466280034267653d1d0
Timothy Clem <timothy.clem@gmail.com>
2018-02-12 16:18:19 -0800

    s/value/v

--- src/Control/Monad/Effect/Linker.hs
+++ src/Control/Monad/Effect/Linker.hs
@@ -4,12 +4,12 @@ module Control.Monad.Effect.Linker where
 import Control.Monad.Effect
 import Control.Monad.Effect.Reader
 import Data.Abstract.Linker
-import Data.Abstract.Value
 
-class Monad m => MonadLinker value m where
-  askLinker :: m (Linker value)
-  localLinker :: ((Linker value) -> (Linker value)) -> m b -> m b
 
-instance (Reader (Linker value) :< fs) => MonadLinker value (Eff fs) where
+class Monad m => MonadLinker v m where
+  askLinker :: m (Linker v)
+  localLinker :: (Linker v -> Linker v) -> m b -> m b
+
+instance (Reader (Linker v) :< fs) => MonadLinker v (Eff fs) where
   askLinker = ask
   localLinker = local
