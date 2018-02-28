d9ddce8b5ea52060c67c08c6ab7d6e5dd81f51b4
Rob Rix <rob.rix@me.com>
2018-02-28 10:40:07 -0500

    Define an Evaluator datatype.

--- src/Analysis/Abstract/Evaluator.hs
+++ src/Analysis/Abstract/Evaluator.hs
@@ -1,1 +1,18 @@
+{-# LANGUAGE DataKinds, RankNTypes, UndecidableInstances #-}
 module Analysis.Abstract.Evaluator where
+
+import Control.Monad.Effect
+import Control.Monad.Effect.Fail
+import Control.Monad.Effect.Reader
+import Control.Monad.Effect.State
+import Data.Abstract.Value
+
+data Evaluator effects value a
+  = Evaluator
+    { runEvaluator :: Members '[ Fail
+                               , Reader (EnvironmentFor value)
+                               , State  (EnvironmentFor value)
+                               , State  (StoreFor value)
+                               ] effects
+                   => Eff effects a
+    }
