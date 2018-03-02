60be5d2f1a5756acba7eeeb19f706aaf1822852c
Rob Rix <rob.rix@me.com>
2018-03-02 12:13:01 -0500

    :memo: evaluateTerm.

--- src/Control/Abstract/Analysis.hs
+++ src/Control/Abstract/Analysis.hs
@@ -5,4 +5,5 @@ module Control.Abstract.Analysis where
 --
 --   This typeclass is left intentionally unconstrained to avoid circular dependencies between it and other typeclasses.
 class Monad m => MonadAnalysis term value m | m -> term, m -> value where
+  -- | Evaluate a term to a value using the semantics of the current analysis. This should always be used instead of explicitly folding 'eval' over subterms, except in 'MonadAnalysis' instances themselves.
   evaluateTerm :: term -> m value
