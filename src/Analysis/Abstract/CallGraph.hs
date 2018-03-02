1773a39e3f669ba512b2a3becf6aac04d04533a3
Rob Rix <rob.rix@me.com>
2018-03-02 15:10:22 -0500

    Define a synonym for the call graph analysis’ effects.

--- src/Analysis/Abstract/CallGraph.hs
+++ src/Analysis/Abstract/CallGraph.hs
@@ -1,1 +1,19 @@
+{-# LANGUAGE DataKinds #-}
 module Analysis.Abstract.CallGraph where
+
+import Control.Monad.Effect.Fail
+import Control.Monad.Effect.NonDetEff
+import Control.Monad.Effect.Reader
+import Control.Monad.Effect.State
+import Data.Abstract.Linker
+import Data.Abstract.Value
+
+type CallGraphEffects term value
+  = '[ Fail
+     , NonDetEff
+     , State  (StoreFor value)
+     , State  (EnvironmentFor value)
+     , Reader (EnvironmentFor value)
+     , Reader (Linker term)
+     , State  (Linker value)
+     ]
