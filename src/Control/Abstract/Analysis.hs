59afa36269d0274442a7362a2e3efc6e2199d5ab
Rob Rix <rob.rix@me.com>
2018-03-02 10:21:10 -0500

    :memo: MonadAnalysis.

--- src/Control/Abstract/Analysis.hs
+++ src/Control/Abstract/Analysis.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE FunctionalDependencies #-}
 module Control.Abstract.Analysis where
 
+-- | A 'Monad' in which one can evaluate some specific term type to some specific value type.
+--
+--   This typeclass is left intentionally unconstrained to avoid circular dependencies between it and other typeclasses.
 class Monad m => MonadAnalysis term value m | m -> term, m -> value where
   evaluateTerm :: term
                -> m value
