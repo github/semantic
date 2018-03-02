b9c6795ca0f8ab95ddd3df93b517954058d718d8
Rob Rix <rob.rix@me.com>
2018-03-02 15:17:34 -0500

    Define a CallGraph synonym.

--- src/Analysis/Abstract/CallGraph.hs
+++ src/Analysis/Abstract/CallGraph.hs
@@ -1,25 +1,29 @@
 {-# LANGUAGE DataKinds, GeneralizedNewtypeDeriving, MultiParamTypeClasses, StandaloneDeriving #-}
 module Analysis.Abstract.CallGraph where
 
+import Algebra.Graph
 import Control.Abstract.Evaluator
 import Control.Monad.Effect.Fail
 import Control.Monad.Effect.NonDetEff
 import Control.Monad.Effect.Reader
 import Control.Monad.Effect.State
+import Data.Abstract.Evaluatable
 import Data.Abstract.Linker
 import Data.Abstract.Value
 
-type CallGraphEffects term value
+type CallGraphEffects term
   = '[ Fail
      , NonDetEff
-     , State  (StoreFor value)
-     , State  (EnvironmentFor value)
-     , Reader (EnvironmentFor value)
+     , State  (StoreFor CallGraph)
+     , State  (EnvironmentFor CallGraph)
+     , Reader (EnvironmentFor CallGraph)
      , Reader (Linker term)
-     , State  (Linker value)
+     , State  (Linker CallGraph)
      ]
 
-newtype CallGraphAnalysis term value a = CallGraphAnalysis { runCallGraphAnalysis :: Evaluator (CallGraphEffects term value) term value a }
+type CallGraph = Graph Name
+
+newtype CallGraphAnalysis term a = CallGraphAnalysis { runCallGraphAnalysis :: Evaluator (CallGraphEffects term) term CallGraph a }
   deriving (Applicative, Functor, Monad, MonadFail)
 
-deriving instance MonadEvaluator term value (CallGraphAnalysis term value)
+deriving instance MonadEvaluator term CallGraph (CallGraphAnalysis term)
