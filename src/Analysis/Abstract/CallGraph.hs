1f62a782a93482d0c379fdfb1a230a876f7fb682
Rob Rix <rob.rix@me.com>
2018-03-05 11:12:38 -0500

    Define a helper for building a graph from the bindings in the Store.
    Co-Authored-By: Patrick Thomson <patrickt@users.noreply.github.com>

--- src/Analysis/Abstract/CallGraph.hs
+++ src/Analysis/Abstract/CallGraph.hs
@@ -13,8 +13,11 @@ import Control.Monad.Effect.State
 import Data.Abstract.Address
 import Data.Abstract.Evaluatable
 import Data.Abstract.Linker
+import Data.Abstract.Store
 import Data.Abstract.Value
+import qualified Data.Map as Map
 import Data.Proxy
+import qualified Data.Set as Set
 import qualified Data.Syntax.Declaration as Declaration
 import Data.Term
 import Prologue hiding (empty)
@@ -34,6 +37,11 @@ newtype CallGraph = CallGraph { unCallGraph :: G.Graph Name }
 
 renderCallGraph :: CallGraph -> ByteString
 renderCallGraph = export (defaultStyle id) . unCallGraph
+
+
+buildStoreGraph :: StoreFor CallGraph -> CallGraph
+buildStoreGraph = foldMap (uncurry connectBinding) . Map.toList . unStore
+  where connectBinding (Monovariant name) body = foldMap (connect (vertex name)) (Set.toList body)
 
 
 newtype CallGraphAnalysis term a = CallGraphAnalysis { runCallGraphAnalysis :: Evaluator (CallGraphEffects term) term CallGraph a }
