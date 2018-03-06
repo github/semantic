c68728ea06af31a2cbe5d3a2038756a489be139d
Rob Rix <rob.rix@me.com>
2018-03-06 16:36:06 -0500

    Define an analyzeTerm method on MonadAnalysis.

--- src/Control/Abstract/Analysis.hs
+++ src/Control/Abstract/Analysis.hs
@@ -1,9 +1,15 @@
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE DefaultSignatures, FunctionalDependencies #-}
 module Control.Abstract.Analysis where
+
+import Prologue
 
 -- | A 'Monad' in which one can evaluate some specific term type to some specific value type.
 --
 --   This typeclass is left intentionally unconstrained to avoid circular dependencies between it and other typeclasses.
 class Monad m => MonadAnalysis term value m | m -> term, m -> value where
   -- | Evaluate a term to a value using the semantics of the current analysis. This should always be used instead of explicitly folding 'eval' over subterms, except in 'MonadAnalysis' instances themselves.
-  evaluateTerm :: term -> m value
+  analyzeTerm :: SubtermAlgebra (Base term) term (m value)
+
+  evaluateTerm :: MonadAnalysis term value m => term -> m value
+  default evaluateTerm :: (MonadAnalysis term value m, Recursive term) => term -> m value
+  evaluateTerm = foldSubterms analyzeTerm
