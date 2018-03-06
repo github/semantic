816218b9003fc0df655de2a55764856c202d5c0a
Rob Rix <rob.rix@me.com>
2018-03-06 14:22:05 -0500

    :memo: buildCallGraph.

--- src/Analysis/CallGraph.hs
+++ src/Analysis/CallGraph.hs
@@ -20,6 +20,7 @@ import Prologue hiding (empty)
 newtype CallGraph = CallGraph { unCallGraph :: G.Graph Name }
   deriving (Eq, Graph, Show)
 
+-- | Build the 'CallGraph' for a 'Term' recursively.
 buildCallGraph :: (CallGraphAlgebra syntax, Foldable syntax, FreeVariables1 syntax, Functor syntax) => Term syntax ann -> Set Name -> CallGraph
 buildCallGraph = foldSubterms callGraphAlgebra
 
