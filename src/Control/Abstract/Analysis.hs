e0e1dc2e67b3ecc91b21280fc6f0c713936f7f3f
Rob Rix <rob.rix@me.com>
2018-03-06 16:43:15 -0500

    Reformat/expand the :memo: for evaluateTerm.

--- src/Control/Abstract/Analysis.hs
+++ src/Control/Abstract/Analysis.hs
@@ -9,7 +9,9 @@ import Prologue
 class Monad m => MonadAnalysis term value m | m -> term, m -> value where
   analyzeTerm :: SubtermAlgebra (Base term) term (m value)
 
-  -- | Evaluate a term to a value using the semantics of the current analysis. This should always be used instead of explicitly folding 'eval' over subterms, except in 'MonadAnalysis' instances themselves.
+  -- | Evaluate a term to a value using the semantics of the current analysis.
+  --
+  --   This should always be called instead of explicitly folding either 'eval' or 'analyzeTerm' over subterms, except in 'MonadAnalysis' instances themselves.
   evaluateTerm :: MonadAnalysis term value m => term -> m value
   default evaluateTerm :: (MonadAnalysis term value m, Recursive term) => term -> m value
   evaluateTerm = foldSubterms analyzeTerm
