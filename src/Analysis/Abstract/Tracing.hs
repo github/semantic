c9a74da233c6b54cf59fc1d1745c9f4f5dc368da
Rob Rix <rob.rix@me.com>
2018-03-06 17:13:58 -0500

    Re-enable the tracing analysis.

--- src/Analysis/Abstract/Tracing.hs
+++ src/Analysis/Abstract/Tracing.hs
@@ -1,73 +1,101 @@
-{-# LANGUAGE AllowAmbiguousTypes, DataKinds, ScopedTypeVariables, TypeApplications, TypeFamilies #-}
+{-# LANGUAGE DataKinds, GeneralizedNewtypeDeriving, MultiParamTypeClasses, ScopedTypeVariables, StandaloneDeriving, TypeApplications, TypeFamilies, UndecidableInstances #-}
 module Analysis.Abstract.Tracing where
 
-import Prologue
+import Control.Abstract.Addressable
+import Control.Abstract.Analysis
+import Control.Abstract.Evaluator
+import Control.Abstract.Value
 import Control.Effect
-import Control.Monad.Effect hiding (run)
-import Control.Monad.Effect.Addressable
-import Control.Monad.Effect.Env
 import Control.Monad.Effect.Fail
 import Control.Monad.Effect.Reader
 import Control.Monad.Effect.State
-import Control.Monad.Effect.Trace
 import Control.Monad.Effect.Writer
 import Data.Abstract.Address
 import Data.Abstract.Configuration
-import Data.Abstract.Environment
-import Data.Abstract.Eval
-import Data.Abstract.Store
+import Data.Abstract.Evaluatable
+import Data.Abstract.Linker
 import Data.Abstract.Value
+import Prologue
 
 -- | The effects necessary for tracing analyses.
-type Tracing g t v
-  = '[ Writer (g (Configuration (LocationFor v) t v)) -- For 'MonadTrace'.
+type TracingEffects trace term value
+  = '[ Writer (trace (Configuration (LocationFor value) term value)) -- For 'MonadTrace'.
      , Fail                                           -- For 'MonadFail'.
-     , State (Store (LocationFor v) v)                -- For 'MonadStore'.
-     , Reader (Environment (LocationFor v) v)         -- For 'MonadEnv'.
+     , State (StoreFor value)                -- For 'MonadStore'.
+     , Reader (EnvironmentFor value)         -- For 'MonadEnv'.
+     , State (EnvironmentFor value)         -- For 'MonadEnv'.
+     , Reader (Linker term)
+     , State (Linker value)
      ]
 
 -- | Linear trace analysis.
-evalTrace :: forall v term
-          . ( Ord v, Ord term, Ord (Cell (LocationFor v) v)
-            , Functor (Base term)
-            , Recursive term
-            , Addressable (LocationFor v) (Eff (Tracing [] term v))
-            , MonadGC v (Eff (Tracing [] term v))
-            , Semigroup (Cell (LocationFor v) v)
-            , Eval term v (Eff (Tracing [] term v)) (Base term)
-            )
-          => term -> Final (Tracing [] term v) v
-evalTrace = run @(Tracing [] term v) . fix (evTell @[] (\ recur yield -> eval recur yield . project)) pure
+evaluateTrace :: forall v term
+              . ( Ord v, Ord term, Ord (Cell (LocationFor v) v)
+                , Corecursive term
+                , Evaluatable (Base term)
+                , FreeVariables term
+                , Recursive term
+                , MonadAddressable (LocationFor v) v (TracingAnalysis [] term v)
+                , MonadValue term v (TracingAnalysis [] term v)
+                , Semigroup (Cell (LocationFor v) v)
+                )
+              => term
+              -> Final (TracingEffects [] term v) v
+evaluateTrace = run @(TracingEffects [] term v) . runEvaluator . runTracingAnalysis . evaluateTerm
 
 -- | Reachable configuration analysis.
-evalReach :: forall v term
-          . ( Ord v, Ord term, Ord (LocationFor v), Ord (Cell (LocationFor v) v)
-            , Functor (Base term)
-            , Recursive term
-            , Addressable (LocationFor v) (Eff (Tracing Set term v))
-            , MonadGC v (Eff (Tracing Set term v))
-            , Semigroup (Cell (LocationFor v) v)
-            , Eval term v (Eff (Tracing Set term v)) (Base term)
-            )
-          => term -> Final (Tracing Set term v) v
-evalReach = run @(Tracing Set term v) . fix (evTell @Set (\ recur yield -> eval recur yield . project)) pure
+evaluateReach :: forall v term
+              . ( Ord v, Ord term, Ord (LocationFor v), Ord (Cell (LocationFor v) v)
+                , Corecursive term
+                , Evaluatable (Base term)
+                , FreeVariables term
+                , Recursive term
+                , MonadAddressable (LocationFor v) v (TracingAnalysis Set term v)
+                , MonadValue term v (TracingAnalysis Set term v)
+                , Semigroup (Cell (LocationFor v) v)
+                )
+              => term
+              -> Final (TracingEffects Set term v) v
+evaluateReach = run @(TracingEffects Set term v) . runEvaluator . runTracingAnalysis . evaluateTerm
 
 
 -- | Small-step evaluation which records every visited configuration.
-evTell :: forall g t m v
-       . ( Monoid (g (Configuration (LocationFor v) t v))
-         , Pointed g
-         , MonadTrace t v g m
-         , MonadEnv v m
-         , MonadStore v m
-         , MonadGC v m
+-- evTell :: forall g t m v
+--        . ( Monoid (g (Configuration (LocationFor v) t v))
+--          , Pointed g
+--          , MonadEvaluator t v m
+--          )
+--        => (((v -> m v) -> t -> m v) -> (v -> m v) -> t -> m v)
+--        -> ((v -> m v) -> t -> m v)
+--        -> (v -> m v) -> t -> m v
+-- evTell ev0 ev' yield e = do
+--   env <- askEnv
+--   store <- getStore
+--   roots <- askRoots
+--   tell (point (Configuration e roots env store) :: g (Configuration (LocationFor v) t v))
+--   ev0 ev' yield e
+
+newtype TracingAnalysis trace term value a = TracingAnalysis { runTracingAnalysis :: Evaluator (TracingEffects trace term value) term value a }
+  deriving (Applicative, Functor, Monad, MonadFail)
+
+deriving instance MonadEvaluator term value (TracingAnalysis trace term value)
+
+instance ( Corecursive term
+         , Evaluatable (Base term)
+         , FreeVariables term
+         , MonadAddressable (LocationFor value) value (TracingAnalysis trace term value)
+         , MonadValue term value (TracingAnalysis trace term value)
+         , Pointed trace
+         , Recursive term
+         , Semigroup (Cell (LocationFor value) value)
          )
-       => (((v -> m v) -> t -> m v) -> (v -> m v) -> t -> m v)
-       -> ((v -> m v) -> t -> m v)
-       -> (v -> m v) -> t -> m v
-evTell ev0 ev' yield e = do
-  env <- askEnv
-  store <- getStore
-  roots <- askRoots
-  trace (point (Configuration e roots env store) :: g (Configuration (LocationFor v) t v))
-  ev0 ev' yield e
+         => MonadAnalysis term value (TracingAnalysis trace term value) where
+  analyzeTerm term = do
+    env <- askLocalEnv
+    store <- getStore
+    roots <- pure mempty
+    trace (point (Configuration (embed (subterm <$> term)) roots env store))
+    eval term
+
+trace :: trace (Configuration (LocationFor value) term value) -> TracingAnalysis trace term value ()
+trace w = TracingAnalysis (Evaluator (tell w))
