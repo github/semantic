f0f7a6599a9daee8d4ffca5443f34b069e93a948
Rob Rix <rob.rix@me.com>
2018-03-07 12:23:31 -0500

    Define a Monoidal Map type.

--- src/Data/Map/Monoidal.hs
+++ src/Data/Map/Monoidal.hs
@@ -1,2 +1,6 @@
 -- | This module defines a 'Map' type whose 'Monoid' and 'Reducer' instances merge values using the 'Semigroup' instance for the underlying type.
 module Data.Map.Monoidal where
+
+import qualified Data.Map as Map
+
+newtype Map key value = Map { unMap :: Map.Map key value }
