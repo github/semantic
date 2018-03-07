352fc2ff0778a5b864fd6efc3b56282d838c3d64
Rob Rix <rob.rix@me.com>
2018-03-07 09:14:38 -0500

    :fire: redundant constraints on evaluateTerm.

--- src/Control/Abstract/Analysis.hs
+++ src/Control/Abstract/Analysis.hs
@@ -22,6 +22,6 @@ class Monad m => MonadAnalysis term value m | m -> term, m -> value where
   -- | Evaluate a term to a value using the semantics of the current analysis.
   --
   --   This should always be called instead of explicitly folding either 'eval' or 'analyzeTerm' over subterms, except in 'MonadAnalysis' instances themselves.
-  evaluateTerm :: MonadAnalysis term value m => term -> m value
-  default evaluateTerm :: (MonadAnalysis term value m, Recursive term) => term -> m value
+  evaluateTerm :: term -> m value
+  default evaluateTerm :: Recursive term => term -> m value
   evaluateTerm = foldSubterms analyzeTerm
