303e55b40343f779145c5f7e0981c133cd829e2e
Rob Rix <rob.rix@me.com>
2018-03-07 12:25:39 -0500

    Define a Semigroup instance for Map.

--- src/Data/Map/Monoidal.hs
+++ src/Data/Map/Monoidal.hs
@@ -3,6 +3,10 @@
 module Data.Map.Monoidal where
 
 import qualified Data.Map as Map
+import Prologue hiding (Map)
 
 newtype Map key value = Map { unMap :: Map.Map key value }
   deriving (Eq, Ord, Show)
+
+instance (Ord key, Semigroup value) => Semigroup (Map key value) where
+  Map a <> Map b = Map (Map.unionWith (<>) a b)
