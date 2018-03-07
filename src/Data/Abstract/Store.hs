fb19c9df32f92a456ba5939d60461da199769561
Rob Rix <rob.rix@me.com>
2018-03-07 13:28:29 -0500

    Define Store over a Monoidal Map.

--- src/Data/Abstract/Store.hs
+++ src/Data/Abstract/Store.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, StandaloneDeriving, UndecidableInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses, StandaloneDeriving, UndecidableInstances #-}
 module Data.Abstract.Store where
 
 import Data.Abstract.Address
 import Data.Abstract.Live
-import qualified Data.Map as Map
+import qualified Data.Map.Monoidal as Map
 import Data.Semigroup.Reducer
 import Prologue
 
 -- | A map of addresses onto cells holding their values.
 newtype Store l a = Store { unStore :: Map.Map l (Cell l a) }
-  deriving (Generic1, Monoid, Semigroup)
+  deriving (Generic1)
 
 deriving instance (Eq l, Eq (Cell l a)) => Eq (Store l a)
 deriving instance (Ord l, Ord (Cell l a)) => Ord (Store l a)
@@ -20,6 +20,9 @@ instance (Show l, Show1 (Cell l)) => Show1 (Store l) where liftShowsPrec = gener
 deriving instance Foldable (Cell l) => Foldable (Store l)
 deriving instance Functor (Cell l) => Functor (Store l)
 deriving instance Traversable (Cell l) => Traversable (Store l)
+deriving instance (Ord l, Semigroup (Cell l a)) => Semigroup (Store l a)
+deriving instance (Ord l, Semigroup (Cell l a)) => Monoid (Store l a)
+deriving instance (Ord l, Reducer a (Cell l a)) => Reducer (l, a) (Store l a)
 
 -- | Look up the cell of values for an 'Address' in a 'Store', if any.
 storeLookup :: Ord l => Address l a -> Store l a -> Maybe (Cell l a)
@@ -31,9 +34,7 @@ storeLookupAll address = fmap toList . storeLookup address
 
 -- | Append a value onto the cell for a given address, inserting a new cell if none existed.
 storeInsert :: (Ord l, Reducer a (Cell l a)) => Address l a -> a -> Store l a -> Store l a
-storeInsert (Address address) value
-  -- Per the docs, 'Map.insertWith' applies its function operand to the new value first, followed by the old value. For 'Latest' this will result in us always keeping the oldest value instead of the latest one, which is exactly opposite to the desired semantics.
-  = Store . Map.insertWith (flip (<>)) address (unit value) . unStore
+storeInsert (Address address) value = flip snoc (address, value)
 
 -- | The number of addresses extant in a 'Store'.
 storeSize :: Store l a -> Int
