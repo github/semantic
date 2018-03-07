9c7c6ed6523dad45bc839163403b4e6e2f6eab38
Rob Rix <rob.rix@me.com>
2018-03-07 12:30:34 -0500

    Define a Reducer instance for Map.

--- src/Data/Map/Monoidal.hs
+++ src/Data/Map/Monoidal.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
 -- | This module defines a 'Map' type whose 'Monoid' and 'Reducer' instances merge values using the 'Semigroup' instance for the underlying type.
 module Data.Map.Monoidal where
 
 import qualified Data.Map as Map
+import Data.Semigroup.Reducer
 import Prologue hiding (Map)
 
 newtype Map key value = Map { unMap :: Map.Map key value }
@@ -14,3 +15,8 @@ instance (Ord key, Semigroup value) => Semigroup (Map key value) where
 instance (Ord key, Semigroup value) => Monoid (Map key value) where
   mempty = Map Map.empty
   mappend = (<>)
+
+instance (Ord key, Reducer a value) => Reducer (key, a) (Map key value) where
+  unit (key, a) = Map (Map.singleton key (unit a))
+  cons (key, a) (Map m) = Map (Map.insertWith (<>) key (unit a) m)
+  snoc (Map m) (key, a) = Map (Map.insertWith (flip (<>)) key (unit a) m)
