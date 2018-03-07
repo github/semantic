158c9251fb436b7ce3f1f5b2d92b8aec3e4f691f
Rob Rix <rob.rix@me.com>
2018-03-07 10:36:18 -0500

    :fire: EvaluationEffects.

--- src/Analysis/Abstract/Evaluating.hs
+++ src/Analysis/Abstract/Evaluating.hs
@@ -6,23 +6,11 @@ import Control.Abstract.Evaluator
 import Data.Abstract.Address
 import Data.Abstract.Evaluatable
 import Data.Abstract.Linker
-import Data.Abstract.Store
 import Data.Abstract.Value
 import Data.Blob
 import Prelude hiding (fail)
 import qualified Data.Map as Map
 import System.FilePath.Posix
-
--- | The effects necessary for concrete interpretation.
-type EvaluationEffects t v
-  = '[ Fail                            -- Failure with an error message
-     , State (Store (LocationFor v) v) -- The heap
-     , State (EnvironmentFor v)        -- Global (imperative) environment
-     , Reader (EnvironmentFor v)       -- Local environment (e.g. binding over a closure)
-     , Reader (Linker t)               -- Cache of unevaluated modules
-     , State (Linker v)                -- Cache of evaluated modules
-     ]
-
 
 -- | Evaluate a term to a value.
 evaluate :: forall v term
@@ -35,8 +23,8 @@ evaluate :: forall v term
             , Semigroup (Cell (LocationFor v) v)
             )
          => term
-         -> Final (EvaluationEffects term v) v
-evaluate = run @(EvaluationEffects term v) . runEvaluator . runEvaluation . evaluateTerm
+         -> Final (EvaluatorEffects term v) v
+evaluate = run @(EvaluatorEffects term v) . runEvaluator . runEvaluation . evaluateTerm
 
 -- | Evaluate terms and an entry point to a value.
 evaluates :: forall v term
@@ -50,8 +38,8 @@ evaluates :: forall v term
              )
           => [(Blob, term)] -- List of (blob, term) pairs that make up the program to be evaluated
           -> (Blob, term)   -- Entrypoint
-          -> Final (EvaluationEffects term v) v
-evaluates pairs (_, t) = run @(EvaluationEffects term v) (runEvaluator (runEvaluation (withModules pairs (evaluateTerm t))))
+          -> Final (EvaluatorEffects term v) v
+evaluates pairs (_, t) = run @(EvaluatorEffects term v) (runEvaluator (runEvaluation (withModules pairs (evaluateTerm t))))
 
 -- | Run an action with the passed ('Blob', @term@) pairs available for imports.
 withModules :: (MonadAnalysis term value m, MonadEvaluator term value m) => [(Blob, term)] -> m a -> m a
@@ -59,7 +47,7 @@ withModules pairs = localModuleTable (const moduleTable)
   where moduleTable = Linker (Map.fromList (map (first (dropExtensions . blobPath)) pairs))
 
 -- | An analysis performing concrete evaluation of @term@s to @value@s.
-newtype Evaluation term value a = Evaluation { runEvaluation :: Evaluator (EvaluationEffects term value) term value a }
+newtype Evaluation term value a = Evaluation { runEvaluation :: Evaluator (EvaluatorEffects term value) term value a }
   deriving (Applicative, Functor, Monad, MonadFail)
 
 deriving instance MonadEvaluator term value (Evaluation term value)
