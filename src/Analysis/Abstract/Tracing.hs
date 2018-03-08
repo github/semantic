1478187f0f4f290540adfcc50281f40da2ec6cf4
Rob Rix <rob.rix@me.com>
2018-03-08 14:37:37 -0500

    Parameterize the typeclasses by the list of effects.

--- src/Analysis/Abstract/Tracing.hs
+++ src/Analysis/Abstract/Tracing.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, GeneralizedNewtypeDeriving, KindSignatures, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DataKinds, GeneralizedNewtypeDeriving, KindSignatures, MultiParamTypeClasses, StandaloneDeriving, TypeFamilies, TypeOperators, UndecidableInstances #-}
 module Analysis.Abstract.Tracing where
 
 import Control.Abstract.Analysis
@@ -18,26 +18,28 @@ type TracerFor trace m = Writer (TraceFor trace m)
 --   Instantiating @trace@ to @[]@ yields a linear trace analysis, while @Set@ yields a reachable state analysis.
 newtype TracingAnalysis (trace :: * -> *) m (effects :: [* -> *]) a
   = TracingAnalysis { runTracingAnalysis :: m effects a }
-  deriving (Applicative, Functor, Effectful, Monad, MonadEvaluator, MonadFail)
+  deriving (Applicative, Functor, Effectful, Monad, MonadFail)
 
-instance ( Corecursive (TermFor (m effects))
+deriving instance MonadEvaluator effects m => MonadEvaluator effects (TracingAnalysis trace m)
+
+instance ( Corecursive (TermFor m)
          , Effectful m
-         , Member (TracerFor trace (m effects)) effects
-         , MonadAnalysis (m effects)
-         , MonadEvaluator (m effects)
-         , Ord (LocationFor (ValueFor (m effects)))
-         , Recursive (TermFor (m effects))
-         , Reducer (ConfigurationFor (TermFor (m effects)) (ValueFor (m effects))) (TraceFor trace (m effects))
+         , Member (TracerFor trace m) effects
+         , MonadAnalysis effects m
+         , MonadEvaluator effects m
+         , Ord (LocationFor (ValueFor m))
+         , Recursive (TermFor m)
+         , Reducer (ConfigurationFor (TermFor m) (ValueFor m)) (TraceFor trace m)
          )
-         => MonadAnalysis (TracingAnalysis trace m effects) where
+         => MonadAnalysis effects (TracingAnalysis trace m) where
   analyzeTerm term = do
     config <- getConfiguration (embedSubterm term)
     trace (Reducer.unit config)
     liftAnalyze analyzeTerm term
 
 trace :: ( Effectful m
-         , Member (TracerFor trace (m effects)) effects
+         , Member (TracerFor trace m) effects
          )
-      => TraceFor trace (m effects)
+      => TraceFor trace m
       -> TracingAnalysis trace m effects ()
 trace = lift . tell
