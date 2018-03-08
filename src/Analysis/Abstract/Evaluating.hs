dbe31824b93dcb98501ab0e571884a1179867375
Rob Rix <rob.rix@me.com>
2018-03-08 11:35:37 -0500

    :fire: Evaluator.

--- src/Analysis/Abstract/Evaluating.hs
+++ src/Analysis/Abstract/Evaluating.hs
@@ -29,7 +29,7 @@ evaluate :: forall value term
             )
          => term
          -> Final (EvaluatingEffects term value) value
-evaluate = run @(EvaluatingEffects term value) . runEvaluator . runEvaluating . evaluateModule
+evaluate = run @(EvaluatingEffects term value) . runEvaluating . evaluateModule
 
 -- | Evaluate terms and an entry point to a value.
 evaluates :: forall value term
@@ -44,22 +44,49 @@ evaluates :: forall value term
           => [(Blob, term)] -- List of (blob, term) pairs that make up the program to be evaluated
           -> (Blob, term)   -- Entrypoint
           -> Final (EvaluatingEffects term value) value
-evaluates pairs (_, t) = run @(EvaluatingEffects term value) (runEvaluator (runEvaluating (withModules pairs (evaluateModule t))))
+evaluates pairs (_, t) = run @(EvaluatingEffects term value) (runEvaluating (withModules pairs (evaluateModule t)))
 
 -- | Run an action with the passed ('Blob', @term@) pairs available for imports.
 withModules :: (MonadAnalysis m, MonadEvaluator m) => [(Blob, TermFor m)] -> m a -> m a
 withModules pairs = localModuleTable (const moduleTable)
   where moduleTable = Linker (Map.fromList (map (first (dropExtensions . blobPath)) pairs))
 
--- | An analysis performing concrete evaluation of @term@s to @value@s.
-newtype Evaluating term value effects a = Evaluating { runEvaluating :: Evaluator term value effects a }
+-- | An analysis evaluating @term@s to @value@s with a list of @effects@ using 'Evaluatable', and producing incremental results of type @a@.
+newtype Evaluating term value effects a = Evaluating { runEvaluating :: Eff effects a }
   deriving (Applicative, Functor, Effectful, Monad)
 
 deriving instance Member Fail effects => MonadFail (Evaluating term value effects)
 deriving instance Member Fresh effects => MonadFresh (Evaluating term value effects)
 deriving instance Member NonDetEff effects => Alternative (Evaluating term value effects)
 deriving instance Member NonDetEff effects => MonadNonDet (Evaluating term value effects)
-deriving instance (Member Fail effects, MonadEvaluator (Evaluator term value effects), Ord (LocationFor value)) => MonadEvaluator (Evaluating term value effects)
+
+type EvaluatingEffects term value
+  = '[ Fail                          -- Failure with an error message
+     , Reader (EnvironmentFor value) -- Local environment (e.g. binding over a closure)
+     , State  (EnvironmentFor value) -- Global (imperative) environment
+     , State  (StoreFor value)       -- The heap
+     , Reader (Linker term)          -- Cache of unevaluated modules
+     , State  (Linker value)         -- Cache of evaluated modules
+     ]
+
+instance (Ord (LocationFor value), Members (EvaluatingEffects term value) effects) => MonadEvaluator (Evaluating term value effects) where
+  type TermFor (Evaluating term value effects) = term
+  type ValueFor (Evaluating term value effects) = value
+
+  getGlobalEnv = lift get
+  modifyGlobalEnv f = lift (modify f)
+
+  askLocalEnv = lift ask
+  localEnv f a = lift (local f (lower a))
+
+  getStore = lift get
+  modifyStore f = lift (modify f)
+
+  getModuleTable = lift get
+  modifyModuleTable f = lift (modify f)
+
+  askModuleTable = lift ask
+  localModuleTable f a = lift (local f (lower a))
 
 instance ( Evaluatable (Base term)
          , FreeVariables term
@@ -71,40 +98,3 @@ instance ( Evaluatable (Base term)
          )
          => MonadAnalysis (Evaluating term value effects) where
   analyzeTerm = eval
-
-type EvaluatingEffects term value
-  = '[ Fail                          -- Failure with an error message
-     , Reader (EnvironmentFor value) -- Local environment (e.g. binding over a closure)
-     , State  (EnvironmentFor value) -- Global (imperative) environment
-     , State  (StoreFor value)       -- The heap
-     , Reader (Linker term)          -- Cache of unevaluated modules
-     , State  (Linker value)         -- Cache of evaluated modules
-     ]
-
-instance (Ord (LocationFor value), Members (EvaluatingEffects term value) effects) => MonadEvaluator (Evaluator term value effects) where
-  type TermFor (Evaluator term value effects) = term
-  type ValueFor (Evaluator term value effects) = value
-
-  getGlobalEnv = Evaluator get
-  modifyGlobalEnv f = Evaluator (modify f)
-
-  askLocalEnv = Evaluator ask
-  localEnv f a = Evaluator (local f (runEvaluator a))
-
-  getStore = Evaluator get
-  modifyStore f = Evaluator (modify f)
-
-  getModuleTable = Evaluator get
-  modifyModuleTable f = Evaluator (modify f)
-
-  askModuleTable = Evaluator ask
-  localModuleTable f a = Evaluator (local f (runEvaluator a))
-
--- | An evaluator of @term@s to @value@s, producing incremental results of type @a@ using a list of @effects@.
-newtype Evaluator term value effects a = Evaluator { runEvaluator :: Eff effects a }
-  deriving (Applicative, Functor, Effectful, Monad)
-
-deriving instance Member Fail effects => MonadFail (Evaluator term value effects)
-deriving instance Member NonDetEff effects => Alternative (Evaluator term value effects)
-deriving instance Member NonDetEff effects => MonadNonDet (Evaluator term value effects)
-deriving instance Member Fresh effects => MonadFresh (Evaluator term value effects)
