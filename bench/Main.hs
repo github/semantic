6d5561f9b9d7805192f7b55d3befdff924dc11e7
Patrick Thomson <patrickt@github.com>
2018-03-12 10:51:07 -0400

    fix misalignment

--- bench/Main.hs
+++ bench/Main.hs
@@ -19,9 +19,9 @@ rbEval = whnfIO . fmap show . evaluateRubyFile . ("bench/bench-fixtures/ruby/" <
 main :: IO ()
 main = defaultMain
   [ bgroup "python" [ bench "assignment" $ pyEval "simple-assignment.py"
-                  , bench "function def" $ pyEval "function-definition.py"
-                  , bench "if + function calls" $ pyEval "if-statement-functions.py"
-                  ]
+                    , bench "function def" $ pyEval "function-definition.py"
+                    , bench "if + function calls" $ pyEval "if-statement-functions.py"
+                    ]
   , bgroup "ruby" [ bench "assignment" $ rbEval "simple-assignment.rb"
                   , bench "function def" $ rbEval "function-definition.rb"
                   , bench "if + function calls" $ rbEval "if-statement-functions.rb"
