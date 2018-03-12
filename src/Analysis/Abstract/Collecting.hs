d39c5758b932f92bf79e95242a5038f13a6f28c4
Rob Rix <rob.rix@me.com>
2018-03-12 14:35:57 -0400

    Define MonadGC.

--- src/Analysis/Abstract/Collecting.hs
+++ src/Analysis/Abstract/Collecting.hs
@@ -4,6 +4,8 @@ module Analysis.Abstract.Collecting
 ) where
 
 import Control.Abstract.Analysis
+import Data.Abstract.Live
+import Data.Abstract.Value
 import Prologue
 
 newtype Collecting m term value (effects :: [* -> *]) a = Collecting (m term value effects a)
@@ -18,3 +20,12 @@ instance ( MonadAnalysis term value (m term value effects)
   type RequiredEffects term value (Collecting m term value effects) = RequiredEffects term value (m term value effects)
 
   analyzeTerm term = liftAnalyze analyzeTerm term
+
+
+-- | 'Monad's offering a local set of 'Live' (rooted/reachable) addresses.
+class Monad m => MonadGC value m where
+  -- | Retrieve the local 'Live' set.
+  askRoots :: m (Live (LocationFor value) value)
+
+  -- | Run a computation with the given 'Live' set added to the local root set.
+  extraRoots :: Live (LocationFor value) value -> m a -> m a
