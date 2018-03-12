82c578ed0dba97d37127745ae741e886f57aed1a
Rob Rix <rob.rix@me.com>
2018-03-12 11:39:51 -0400

    :memo: raise & lower.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -68,9 +68,11 @@ instance Ord a => RunEffect NonDetEff a where
 
 -- | Types wrapping 'Eff' actions.
 --
---   Most instances of 'Effectful' will be derived using @-XGeneralizedNewtypeDeriving@, with these ultimately bottoming out on the instance for 'Eff' (for which 'raise' and 'lower' are simply the identity).
+--   Most instances of 'Effectful' will be derived using @-XGeneralizedNewtypeDeriving@, with these ultimately bottoming out on the instance for 'Eff' (for which 'raise' and 'lower' are simply the identity). Because of this, types can be nested arbitrarily deeply and still call 'raise'/'lower' just once to get at the (ultimately) underlying 'Eff'.
 class Effectful (m :: [* -> *] -> * -> *) where
+  -- | Raise an action in 'Eff' into an action in @m@.
   raise :: Eff effects a -> m effects a
+  -- | Lower an action in @m@ into an action in 'Eff'.
   lower :: m effects a -> Eff effects a
 
 instance Effectful Eff where
