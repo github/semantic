ed26b92bad5c64a2d1675cb12ffa8fbc27241769
Rob Rix <rob.rix@me.com>
2018-03-14 15:41:57 -0400

    Revert "Rename a bunch of type parameters."
    This reverts commit 646e3fd7cdfb1426ea6d47df828e42671abd8a39.

--- src/Data/Abstract/Address.hs
+++ src/Data/Abstract/Address.hs
@@ -6,12 +6,12 @@ import Data.Semigroup.Reducer
 import Prologue
 
 -- | An abstract address with a location of @l@ pointing to a variable of type @a@.
-newtype Address location value = Address { unAddress :: location }
+newtype Address l a = Address { unAddress :: l }
   deriving (Eq, Foldable, Functor, Generic1, Ord, Show, Traversable)
 
-instance Eq location => Eq1 (Address location) where liftEq = genericLiftEq
-instance Ord location => Ord1 (Address location) where liftCompare = genericLiftCompare
-instance Show location => Show1 (Address location) where liftShowsPrec = genericLiftShowsPrec
+instance Eq l => Eq1 (Address l) where liftEq = genericLiftEq
+instance Ord l => Ord1 (Address l) where liftCompare = genericLiftCompare
+instance Show l => Show1 (Address l) where liftShowsPrec = genericLiftShowsPrec
 
 
 -- | 'Precise' models precise store semantics where only the 'Latest' value is taken. Everything gets it's own address (always makes a new allocation) which makes for a larger store.
@@ -24,19 +24,19 @@ newtype Monovariant = Monovariant { unMonovariant :: Name }
 
 
 -- | The type into which stored values will be written for a given location type.
-type family Cell location = res | res -> location where
+type family Cell l = res | res -> l where
   Cell Precise = Latest
   Cell Monovariant = Set
 
 
 -- | A cell holding a single value. Writes will replace any prior value.
-newtype Latest value = Latest { unLatest :: value }
+newtype Latest a = Latest { unLatest :: a }
   deriving (Eq, Foldable, Functor, Generic1, Ord, Show, Traversable)
 
-instance Semigroup (Latest value) where
+instance Semigroup (Latest a) where
   _ <> a = a
 
-instance Reducer value (Latest value) where
+instance Reducer a (Latest a) where
   unit = Latest
   cons _ = id
   snoc _ = unit
