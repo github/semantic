e8df177ed45812b11f9456563beb23ba388b3d20
Rob Rix <rob.rix@me.com>
2018-03-14 11:11:39 -0400

    Rename a bunch of type parameters.

--- src/Data/Abstract/Address.hs
+++ src/Data/Abstract/Address.hs
@@ -6,12 +6,12 @@ import Data.Semigroup.Reducer
 import Prologue
 
 -- | An abstract address with a location of @l@ pointing to a variable of type @a@.
-newtype Address l a = Address { unAddress :: l }
+newtype Address location value = Address { unAddress :: location }
   deriving (Eq, Foldable, Functor, Generic1, Ord, Show, Traversable)
 
-instance Eq l => Eq1 (Address l) where liftEq = genericLiftEq
-instance Ord l => Ord1 (Address l) where liftCompare = genericLiftCompare
-instance Show l => Show1 (Address l) where liftShowsPrec = genericLiftShowsPrec
+instance Eq location => Eq1 (Address location) where liftEq = genericLiftEq
+instance Ord location => Ord1 (Address location) where liftCompare = genericLiftCompare
+instance Show location => Show1 (Address location) where liftShowsPrec = genericLiftShowsPrec
 
 
 -- | 'Precise' models precise store semantics where only the 'Latest' value is taken. Everything gets it's own address (always makes a new allocation) which makes for a larger store.
@@ -24,19 +24,19 @@ newtype Monovariant = Monovariant { unMonovariant :: Name }
 
 
 -- | The type into which stored values will be written for a given location type.
-type family Cell l = res | res -> l where
+type family Cell location = res | res -> location where
   Cell Precise = Latest
   Cell Monovariant = Set
 
 
 -- | A cell holding a single value. Writes will replace any prior value.
-newtype Latest a = Latest { unLatest :: a }
+newtype Latest value = Latest { unLatest :: value }
   deriving (Eq, Foldable, Functor, Generic1, Ord, Show, Traversable)
 
-instance Semigroup (Latest a) where
+instance Semigroup (Latest value) where
   _ <> a = a
 
-instance Reducer a (Latest a) where
+instance Reducer value (Latest value) where
   unit = Latest
   cons _ = id
   snoc _ = unit
