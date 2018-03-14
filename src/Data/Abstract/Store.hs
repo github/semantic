601c3b97d863801cba3c3456a6d3b0c4e2fc8ec0
Rob Rix <rob.rix@me.com>
2018-03-14 11:20:37 -0400

    Rename some more type parameters.

--- src/Data/Abstract/Store.hs
+++ src/Data/Abstract/Store.hs
@@ -8,38 +8,38 @@ import Data.Semigroup.Reducer
 import Prologue
 
 -- | A map of addresses onto cells holding their values.
-newtype Store l a = Store { unStore :: Monoidal.Map l (Cell l a) }
+newtype Store location value = Store { unStore :: Monoidal.Map location (Cell location value) }
   deriving (Generic1)
 
-deriving instance (Eq l, Eq (Cell l a)) => Eq (Store l a)
-deriving instance (Ord l, Ord (Cell l a)) => Ord (Store l a)
-deriving instance (Show l, Show (Cell l a)) => Show (Store l a)
-instance (Eq l, Eq1 (Cell l)) => Eq1 (Store l) where liftEq = genericLiftEq
-instance (Ord l, Ord1 (Cell l)) => Ord1 (Store l) where liftCompare = genericLiftCompare
-instance (Show l, Show1 (Cell l)) => Show1 (Store l) where liftShowsPrec = genericLiftShowsPrec
-deriving instance Foldable (Cell l) => Foldable (Store l)
-deriving instance Functor (Cell l) => Functor (Store l)
-deriving instance Traversable (Cell l) => Traversable (Store l)
-deriving instance (Ord l, Semigroup (Cell l a)) => Semigroup (Store l a)
-deriving instance (Ord l, Semigroup (Cell l a)) => Monoid (Store l a)
-deriving instance (Ord l, Reducer a (Cell l a)) => Reducer (l, a) (Store l a)
+deriving instance (Eq location, Eq (Cell location value)) => Eq (Store location value)
+deriving instance (Ord location, Ord (Cell location value)) => Ord (Store location value)
+deriving instance (Show location, Show (Cell location value)) => Show (Store location value)
+instance (Eq location, Eq1 (Cell location)) => Eq1 (Store location) where liftEq = genericLiftEq
+instance (Ord location, Ord1 (Cell location)) => Ord1 (Store location) where liftCompare = genericLiftCompare
+instance (Show location, Show1 (Cell location)) => Show1 (Store location) where liftShowsPrec = genericLiftShowsPrec
+deriving instance Foldable (Cell location) => Foldable (Store location)
+deriving instance Functor (Cell location) => Functor (Store location)
+deriving instance Traversable (Cell location) => Traversable (Store location)
+deriving instance (Ord location, Semigroup (Cell location value)) => Semigroup (Store location value)
+deriving instance (Ord location, Semigroup (Cell location value)) => Monoid (Store location value)
+deriving instance (Ord location, Reducer value (Cell location value)) => Reducer (location, value) (Store location value)
 
 -- | Look up the cell of values for an 'Address' in a 'Store', if any.
-storeLookup :: Ord l => Address l a -> Store l a -> Maybe (Cell l a)
+storeLookup :: Ord location => Address location value -> Store location value -> Maybe (Cell location value)
 storeLookup (Address address) = Monoidal.lookup address . unStore
 
 -- | Look up the list of values stored for a given address, if any.
-storeLookupAll :: (Ord l, Foldable (Cell l)) => Address l a -> Store l a -> Maybe [a]
+storeLookupAll :: (Ord location, Foldable (Cell location)) => Address location value -> Store location value -> Maybe [value]
 storeLookupAll address = fmap toList . storeLookup address
 
 -- | Append a value onto the cell for a given address, inserting a new cell if none existed.
-storeInsert :: (Ord l, Reducer a (Cell l a)) => Address l a -> a -> Store l a -> Store l a
+storeInsert :: (Ord location, Reducer value (Cell location value)) => Address location value -> value -> Store location value -> Store location value
 storeInsert (Address address) value = flip snoc (address, value)
 
 -- | The number of addresses extant in a 'Store'.
-storeSize :: Store l a -> Int
+storeSize :: Store location value -> Int
 storeSize = Monoidal.size . unStore
 
 -- | Restrict a 'Store' to only those 'Address'es in the given 'Live' set (in essence garbage collecting the rest).
-storeRestrict :: Ord l => Store l a -> Live l a -> Store l a
+storeRestrict :: Ord location => Store location value -> Live location value -> Store location value
 storeRestrict (Store m) roots = Store (Monoidal.filterWithKey (\ address _ -> Address address `liveMember` roots) m)
