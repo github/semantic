762a8e34a3835b2547b37f54e010a1340d38fd24
Rob Rix <rob.rix@me.com>
2018-03-14 12:33:17 -0400

    Don’t recompute the free variables in closure bodies.

--- src/Data/Abstract/Value.hs
+++ src/Data/Abstract/Value.hs
@@ -7,7 +7,6 @@ import Data.Abstract.Store
 import Data.Abstract.FreeVariables
 import Data.Abstract.Live
 import qualified Data.Abstract.Type as Type
-import qualified Data.Set as Set
 import Data.Scientific (Scientific)
 import Prologue
 import Prelude hiding (Float, Integer, String)
@@ -133,9 +132,9 @@ class ValueRoots value where
   -- | Compute the set of addresses rooted by a given value.
   valueRoots :: value -> LiveFor value
 
-instance (FreeVariables term, Ord location) => ValueRoots (Value location term) where
+instance Ord location => ValueRoots (Value location term) where
   valueRoots v
-    | Just (Closure names body env) <- prjValue v = envRoots env (foldr Set.delete (freeVariables (body :: term)) names)
+    | Just (Closure names body env) <- prjValue v = envAll (foldr envDelete env names) `const` (body :: term)
     | otherwise                                   = mempty
 
 instance ValueRoots (Type.Type m) where
