67c25c004adb6b36ac736d572b45550ca576956d
Rob Rix <rob.rix@me.com>
2018-03-14 16:42:54 -0400

    Derive the evaluator instances.

--- src/Analysis/Abstract/Elaborating.hs
+++ src/Analysis/Abstract/Elaborating.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, GeneralizedNewtypeDeriving, TypeFamilies #-}
+{-# LANGUAGE DataKinds, GeneralizedNewtypeDeriving, MultiParamTypeClasses, StandaloneDeriving, TypeFamilies #-}
 module Analysis.Abstract.Elaborating
 ( type Elaborating
 ) where
@@ -8,3 +8,8 @@ import Prologue
 
 newtype Elaborating m term value (effects :: [* -> *]) a = Elaborating (m term value effects a)
   deriving (Alternative, Applicative, Functor, Effectful, Monad, MonadFail, MonadFresh, MonadNonDet)
+
+deriving instance MonadEnvironment value (m term value effects) => MonadEnvironment value (Elaborating m term value effects)
+deriving instance MonadStore value (m term value effects) => MonadStore value (Elaborating m term value effects)
+deriving instance MonadModuleTable term value (m term value effects) => MonadModuleTable term value (Elaborating m term value effects)
+deriving instance MonadEvaluator term value (m term value effects) => MonadEvaluator term value (Elaborating m term value effects)
