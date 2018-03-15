e5e72b36e90950fa6d9d183dac534804d8bdfa59
Timothy Clem <timothy.clem@gmail.com>
2018-03-15 15:09:07 -0700

    Some ruby load specs and fixtures

--- test/Analysis/Ruby/Spec.hs
+++ test/Analysis/Ruby/Spec.hs
@@ -15,10 +15,21 @@ spec = parallel $ do
       let expectedEnv = Environment $ fromList [ (qualifiedName ["foo"], addr 0) ]
       env `shouldBe` expectedEnv
 
+    it "load" $ do
+      env <- evaluate "load.rb"
+      let expectedEnv = Environment $ fromList [ (qualifiedName ["foo"], addr 0) ]
+      env `shouldBe` expectedEnv
+
+    it "load wrap" $ do
+      res <- evaluate' "load-wrap.rb"
+      fst res `shouldBe` Left "free variable: \"foo\""
+      snd res `shouldBe` Environment (fromList [ ])
+
   where
     addr = Address . Precise
     fixtures = "test/fixtures/ruby/analysis/"
-    evaluate entry = snd . fst . fst . fst <$>
+    evaluate entry = snd <$> evaluate' entry
+    evaluate' entry = fst . fst . fst <$>
       evaluateFiles @RubyValue rubyParser
       [ fixtures <> entry
       , fixtures <> "foo.rb"
