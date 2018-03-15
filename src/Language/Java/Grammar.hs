8ccfecade0190867f805c8dbf78160b9e44ab270
Ayman Nadeem <aymannadeem@github.com>
2018-03-15 15:53:55 -0700

    tree-sitter-java updates

--- src/Language/Java/Grammar.hs
+++ src/Language/Java/Grammar.hs
@@ -1,1 +1,12 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Language.Java.Grammar where
+
+import Language.Haskell.TH
+import TreeSitter.Language
+import TreeSitter.Java
+
+-- Regenerate template haskell code when these files change:
+addDependentFileRelative "../../../vendor/haskell-tree-sitter/languages/java/vendor/tree-sitter-java/src/parser.c"
+
+-- | Statically-known rules corresponding to symbols in the grammar.
+mkSymbolDatatype (mkName "Grammar") tree_sitter_java
