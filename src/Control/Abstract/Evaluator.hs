6764d3110597e5272ea69cfb44ded3035aa76c7e
Rob Rix <rob.rix@me.com>
2018-03-15 10:09:02 -0400

    Note a TODO for Label.

--- src/Control/Abstract/Evaluator.hs
+++ src/Control/Abstract/Evaluator.hs
@@ -108,6 +108,7 @@ modifyModuleTable f = do
 
 
 -- | The type of labels.
+--   TODO: This should be rolled into 'Name' and tracked in the environment, both so that we can abstract over labels like any other location, and so that we can garbage collect unreachable labels.
 type Label = Int
 
 class Monad m => MonadControl term m where
