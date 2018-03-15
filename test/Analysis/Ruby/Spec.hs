572f05703708fe7a0d309cd20e2189794a4cc58c
Timothy Clem <timothy.clem@gmail.com>
2018-03-15 15:08:03 -0700

    Different spec name

--- test/Analysis/Ruby/Spec.hs
+++ test/Analysis/Ruby/Spec.hs
@@ -10,7 +10,7 @@ import SpecHelpers
 spec :: Spec
 spec = parallel $ do
   describe "evalutes Ruby" $ do
-    it "require_relative method" $ do
+    it "require_relative" $ do
       env <- evaluate "main.rb"
       let expectedEnv = Environment $ fromList [ (qualifiedName ["foo"], addr 0) ]
       env `shouldBe` expectedEnv
