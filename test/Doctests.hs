863d6aa4280bc37424a63dcf615ca2cf49cc6458
Rob Rix <rob.rix@me.com>
2018-03-16 14:06:38 -0400

    Add a doctest component.

---
+++ test/Doctests.hs
@@ -0,0 +1,30 @@
+module Main
+( main
+) where
+
+import System.Environment
+import Test.DocTest
+
+main :: IO ()
+main = getArgs >>= run
+
+run :: [String] -> IO ()
+run args = doctest (map ("-X" ++) extensions ++ args ++ ["-isrc"] ++ sources)
+
+extensions :: [String]
+extensions =
+  [ "DeriveFoldable"
+  , "DeriveFunctor"
+  , "DeriveGeneric"
+  , "DeriveTraversable"
+  , "FlexibleContexts"
+  , "FlexibleInstances"
+  , "OverloadedStrings"
+  , "RecordWildCards"
+  , "StrictData"
+  ]
+
+sources :: [String]
+sources =
+  [ "src/Data/Semiring.hs"
+  ]
