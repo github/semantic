7fdc5e2fda24861d6fed7f597b35fcc47aacfc7f
Timothy Clem <timothy.clem@gmail.com>
2018-03-19 14:42:00 -0700

    Write a small spec for evaluating PHP includes

---
+++ test/Analysis/PHP/Spec.hs
@@ -0,0 +1,26 @@
+module Analysis.PHP.Spec (spec) where
+
+import Data.Abstract.Value
+import Data.Map
+
+import SpecHelpers
+
+
+spec :: Spec
+spec = parallel $ do
+  describe "evalutes PHP" $ do
+    it "include" $ do
+      env <- evaluate "main.php"
+      let expectedEnv = Environment $ fromList
+            [ (qualifiedName ["foo"], addr 0)
+            ]
+      env `shouldBe` expectedEnv
+
+  where
+    addr = Address . Precise
+    fixtures = "test/fixtures/php/analysis/"
+    evaluate entry = snd . fst . fst . fst . fst <$>
+      evaluateFiles phpParser
+      [ fixtures <> entry
+      , fixtures <> "foo.php"
+      ]
