794a03a49c124887ff3b3a172386d76688fda05b
Rob Rix <rob.rix@me.com>
2018-03-21 16:41:41 -0400

    :memo: AppMerge.

--- src/Data/Semigroup/App.hs
+++ src/Data/Semigroup/App.hs
@@ -4,6 +4,7 @@ module Data.Semigroup.App where
 import Control.Applicative
 import Data.Semigroup
 
+-- | 'Semigroup' under '<*>' and '<>'.
 newtype AppMerge f a = AppMerge { runAppMerge :: f a }
   deriving (Alternative, Applicative, Bounded, Enum, Eq, Foldable, Functor, Monad, Ord, Show, Traversable)
 
