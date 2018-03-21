6be056bd9ad6ac5fdce2b38754932d77a2b43255
Rob Rix <rob.rix@me.com>
2018-03-21 17:16:31 -0400

    Swap the order of the types.

--- src/Data/Semigroup/App.hs
+++ src/Data/Semigroup/App.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Data.Semigroup.App
-( AppMerge(..)
-, App(..)
+( App(..)
+, AppMerge(..)
 ) where
 
 import Control.Applicative
@@ -11,20 +11,6 @@ import Data.Semigroup
 -- >>> import Test.QuickCheck
 -- >>> instance Arbitrary (f a) => Arbitrary (App f a) where arbitrary = App <$> arbitrary
 -- >>> instance Arbitrary (f a) => Arbitrary (AppMerge f a) where arbitrary = AppMerge <$> arbitrary
-
--- | 'Semigroup' under '<*>' and '<>'.
-newtype AppMerge f a = AppMerge { runAppMerge :: f a }
-  deriving (Alternative, Applicative, Bounded, Enum, Eq, Foldable, Functor, Monad, Ord, Show, Traversable)
-
--- $ Associativity:
---   prop> \ a b c -> a <> (b <> c) == (a <> b) <> (c :: AppMerge Maybe String)
-instance (Applicative f, Semigroup a) => Semigroup (AppMerge f a) where
-  AppMerge a <> AppMerge b = AppMerge ((<>) <$> a <*> b)
-
-instance (Applicative f, Monoid a, Semigroup a) => Monoid (AppMerge f a) where
-  mempty = AppMerge (pure mempty)
-  mappend = (<>)
-
 
 -- | 'Semigroup' under '*>'.
 newtype App f a = App { runApp :: f a }
@@ -37,4 +23,18 @@ instance Applicative f => Semigroup (App f a) where
 
 instance (Applicative f, Monoid a) => Monoid (App f a) where
   mempty = App (pure mempty)
+  mappend = (<>)
+
+
+-- | 'Semigroup' under '<*>' and '<>'.
+newtype AppMerge f a = AppMerge { runAppMerge :: f a }
+  deriving (Alternative, Applicative, Bounded, Enum, Eq, Foldable, Functor, Monad, Ord, Show, Traversable)
+
+-- $ Associativity:
+--   prop> \ a b c -> a <> (b <> c) == (a <> b) <> (c :: AppMerge Maybe String)
+instance (Applicative f, Semigroup a) => Semigroup (AppMerge f a) where
+  AppMerge a <> AppMerge b = AppMerge ((<>) <$> a <*> b)
+
+instance (Applicative f, Monoid a, Semigroup a) => Monoid (AppMerge f a) where
+  mempty = AppMerge (pure mempty)
   mappend = (<>)
