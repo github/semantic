f032aec337df423a163d343356c873316bf6c1cd
Rob Rix <rob.rix@me.com>
2018-03-21 19:09:06 -0400

    Define a Module type.

--- src/Data/Abstract/Module.hs
+++ src/Data/Abstract/Module.hs
@@ -1,1 +1,10 @@
-module Data.Abstract.Module where
+module Data.Abstract.Module
+( Module(..)
+, ModuleName
+) where
+
+import Data.Abstract.FreeVariables
+
+type ModuleName = Name
+
+data Module term = Module { moduleName :: ModuleName, modulePath :: FilePath, moduleTerm :: term }
