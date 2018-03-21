310a2d9e6d37fae72cf72aea0a03d89d8435221c
joshvera <josh@joshvera.com>
2018-03-21 15:23:44 -0400

    Missing import

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -7,6 +7,7 @@ import Control.Monad.Effect.NonDetEff
 import Control.Monad.Effect.Reader
 import Control.Monad.Effect.State
 import Control.Monad.Effect.Writer
+import Control.Monad.Effect.Resumable
 import Data.Semigroup.Reducer
 import Prologue
 
