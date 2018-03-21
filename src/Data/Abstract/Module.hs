5af7154996c0b9bcbc756a1d2229acca408b92cf
Rob Rix <rob.rix@me.com>
2018-03-21 19:09:37 -0400

    Derive a bunch of instances for Module.

--- src/Data/Abstract/Module.hs
+++ src/Data/Abstract/Module.hs
@@ -8,3 +8,4 @@ import Data.Abstract.FreeVariables
 type ModuleName = Name
 
 data Module term = Module { moduleName :: ModuleName, modulePath :: FilePath, moduleTerm :: term }
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
