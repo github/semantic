541e23043148cc626c15de951c6eb16c80b60eb7
Rob Rix <rob.rix@me.com>
2018-03-21 16:41:32 -0400

    Define a Semigroup instance for AppMerge.

--- src/Data/Semigroup/App.hs
+++ src/Data/Semigroup/App.hs
@@ -2,6 +2,10 @@
 module Data.Semigroup.App where
 
 import Control.Applicative
+import Data.Semigroup
 
 newtype AppMerge f a = AppMerge { runAppMerge :: f a }
   deriving (Alternative, Applicative, Bounded, Enum, Eq, Foldable, Functor, Monad, Ord, Show, Traversable)
+
+instance (Applicative f, Semigroup a) => Semigroup (AppMerge f a) where
+  AppMerge a <> AppMerge b = AppMerge ((<>) <$> a <*> b)
