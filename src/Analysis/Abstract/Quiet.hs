8b5ab170c30dd71ddb517e59511c8e9477794399
Rob Rix <rob.rix@me.com>
2018-03-26 10:05:41 -0400

    Derive some standard instances.

--- src/Analysis/Abstract/Quiet.hs
+++ src/Analysis/Abstract/Quiet.hs
@@ -6,3 +6,9 @@ import Prologue
 
 newtype Quiet m term value (effects :: [* -> *]) a = Quiet (m term value effects a)
   deriving (Alternative, Applicative, Functor, Effectful, Monad, MonadFail, MonadFresh, MonadNonDet)
+
+deriving instance MonadControl term (m term value effects) => MonadControl term (Quiet m term value effects)
+deriving instance MonadEnvironment value (m term value effects) => MonadEnvironment value (Quiet m term value effects)
+deriving instance MonadHeap value (m term value effects) => MonadHeap value (Quiet m term value effects)
+deriving instance MonadModuleTable term value (m term value effects) => MonadModuleTable term value (Quiet m term value effects)
+deriving instance MonadEvaluator term value (m term value effects) => MonadEvaluator term value (Quiet m term value effects)
