a4aae6a70c0cbc9bf261b0dad0e9b370581aa196
Rob Rix <rob.rix@me.com>
2018-03-26 10:05:59 -0400

    Rename Quiet to Quietly.

--- src/Analysis/Abstract/Quiet.hs
+++ src/Analysis/Abstract/Quiet.hs
@@ -4,11 +4,11 @@ module Analysis.Abstract.Quiet where
 import Control.Abstract.Analysis
 import Prologue
 
-newtype Quiet m term value (effects :: [* -> *]) a = Quiet (m term value effects a)
+newtype Quietly m term value (effects :: [* -> *]) a = Quietly (m term value effects a)
   deriving (Alternative, Applicative, Functor, Effectful, Monad, MonadFail, MonadFresh, MonadNonDet)
 
-deriving instance MonadControl term (m term value effects) => MonadControl term (Quiet m term value effects)
-deriving instance MonadEnvironment value (m term value effects) => MonadEnvironment value (Quiet m term value effects)
-deriving instance MonadHeap value (m term value effects) => MonadHeap value (Quiet m term value effects)
-deriving instance MonadModuleTable term value (m term value effects) => MonadModuleTable term value (Quiet m term value effects)
-deriving instance MonadEvaluator term value (m term value effects) => MonadEvaluator term value (Quiet m term value effects)
+deriving instance MonadControl term (m term value effects) => MonadControl term (Quietly m term value effects)
+deriving instance MonadEnvironment value (m term value effects) => MonadEnvironment value (Quietly m term value effects)
+deriving instance MonadHeap value (m term value effects) => MonadHeap value (Quietly m term value effects)
+deriving instance MonadModuleTable term value (m term value effects) => MonadModuleTable term value (Quietly m term value effects)
+deriving instance MonadEvaluator term value (m term value effects) => MonadEvaluator term value (Quietly m term value effects)
