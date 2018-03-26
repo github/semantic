35e0063de9f30767f64fedb56fdc8dea933df00b
Rob Rix <rob.rix@me.com>
2018-03-26 10:05:05 -0400

    Add a newtype for a quiet analysis.

--- src/Analysis/Abstract/Quiet.hs
+++ src/Analysis/Abstract/Quiet.hs
@@ -1,1 +1,8 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, KindSignatures #-}
 module Analysis.Abstract.Quiet where
+
+import Control.Abstract.Analysis
+import Prologue
+
+newtype Quiet m term value (effects :: [* -> *]) a = Quiet (m term value effects a)
+  deriving (Alternative, Applicative, Functor, Effectful, Monad, MonadFail, MonadFresh, MonadNonDet)
