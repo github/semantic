1c03f9c4d9352688a930ec5967732834ab9db0ad
Rob Rix <rob.rix@me.com>
2018-03-29 09:31:45 -0400

    Define a Location instance for Located.

--- src/Data/Abstract/Located.hs
+++ src/Data/Abstract/Located.hs
@@ -2,6 +2,7 @@
 module Data.Abstract.Located where
 
 import Control.Abstract.Evaluator
+import Data.Abstract.Address
 import Data.Range
 import Data.Record
 import Data.Span
@@ -9,8 +10,13 @@ import Data.Span
 -- TODO: Dependencies
 type Provenance = Record '[Range, Span]
 
+
 data Located location = Located { provenance :: !Provenance, location :: location }
   deriving (Eq, Ord, Show)
+
+instance Location location => Location (Located location) where
+  type Cell (Located location) = Cell location
+
 
 newtype LocatedValue value = LocatedValue { getLocatedValue :: value }
   deriving (Eq, Ord, Show)
