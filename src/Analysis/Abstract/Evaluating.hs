7f019d44e0b19f16110a4831bb74c6e8adcbab22
Rob Rix <rob.rix@me.com>
2018-03-29 17:59:25 -0400

    :fire: some indentation.

--- src/Analysis/Abstract/Evaluating.hs
+++ src/Analysis/Abstract/Evaluating.hs
@@ -20,10 +20,10 @@ import           Prologue
 newtype Evaluating term value effects a = Evaluating (Eff effects a)
   deriving (Applicative, Functor, Effectful, Monad)
 
-deriving instance Member Fail      effects => MonadFail   (Evaluating term value effects)
-deriving instance Member Fresh     effects => MonadFresh  (Evaluating term value effects)
-deriving instance Member NonDet    effects => Alternative (Evaluating term value effects)
-deriving instance Member NonDet    effects => MonadNonDet (Evaluating term value effects)
+deriving instance Member Fail   effects => MonadFail   (Evaluating term value effects)
+deriving instance Member Fresh  effects => MonadFresh  (Evaluating term value effects)
+deriving instance Member NonDet effects => Alternative (Evaluating term value effects)
+deriving instance Member NonDet effects => MonadNonDet (Evaluating term value effects)
 
 -- | Effects necessary for evaluating (whether concrete or abstract).
 type EvaluatingEffects term value
