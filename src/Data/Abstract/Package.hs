63d252b2add29eeb9ab53d8347788edbf6a0cc4a
Rob Rix <rob.rix@me.com>
2018-03-30 11:39:45 -0400

    Packages have modules.

--- src/Data/Abstract/Package.hs
+++ src/Data/Abstract/Package.hs
@@ -1,5 +1,9 @@
 module Data.Abstract.Package where
 
+import Data.Abstract.Module
+import qualified Data.Map as Map
+
 data Package term = Package
   { packageName :: String
+  , packageModules :: Map.Map ModuleName [Module term]
   }
