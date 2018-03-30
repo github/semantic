f04e0aee747d35860a579571dd5499ef024348a2
Rob Rix <rob.rix@me.com>
2018-03-29 22:59:50 -0400

    Define a MonadAddressable instance for Located using a MonadProvenance interface.

--- src/Data/Abstract/Located.hs
+++ src/Data/Abstract/Located.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TypeFamilies #-}
 module Data.Abstract.Located where
 
+import Control.Abstract.Addressable
 import Data.Abstract.Address
 import Data.Range
 import Data.Record
@@ -10,8 +11,17 @@ import Data.Span
 type Provenance = Record '[Range, Span]
 
 
+class Monad m => MonadProvenance m where
+  askProvenance :: m Provenance
+
+
 data Located location = Located { location :: location, provenance :: !Provenance }
   deriving (Eq, Ord, Show)
 
 instance Location location => Location (Located location) where
   type Cell (Located location) = Cell location
+
+instance (MonadAddressable location m, MonadProvenance m) => MonadAddressable (Located location) m where
+  derefCell (Address (Located loc _)) = derefCell (Address loc)
+
+  allocLoc name = Located <$> allocLoc name <*> askProvenance
