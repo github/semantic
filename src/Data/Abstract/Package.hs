1c6c860f78a16056cc24eb282eb55474233ce520
Rob Rix <rob.rix@me.com>
2018-03-30 11:40:34 -0400

    Define a PackageName synonym.

--- src/Data/Abstract/Package.hs
+++ src/Data/Abstract/Package.hs
@@ -1,9 +1,12 @@
 module Data.Abstract.Package where
 
+import Data.Abstract.FreeVariables
 import Data.Abstract.Module
 import qualified Data.Map as Map
 
+type PackageName = Name
+
 data Package term = Package
-  { packageName :: String
+  { packageName    :: PackageName
   , packageModules :: Map.Map ModuleName [Module term]
   }
