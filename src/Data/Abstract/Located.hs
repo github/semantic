c8829b39f3ca55c855e85394bd39ba8676ceaa1c
Rob Rix <rob.rix@me.com>
2018-03-29 23:18:41 -0400

    Located is defined in terms of Origin.

--- src/Data/Abstract/Located.hs
+++ src/Data/Abstract/Located.hs
@@ -3,25 +3,19 @@ module Data.Abstract.Located where
 
 import Control.Abstract.Addressable
 import Data.Abstract.Address
-import Data.Range
-import Data.Record
-import Data.Span
+import Data.Abstract.Origin
 
--- TODO: Dependencies
-type Provenance = Record '[Range, Span]
+class Monad m => MonadOrigin m where
+  askOrigin :: m Origin
 
 
-class Monad m => MonadProvenance m where
-  askProvenance :: m Provenance
-
-
-data Located location = Located { location :: location, provenance :: !Provenance }
+data Located location = Located { location :: location, provenance :: !Origin }
   deriving (Eq, Ord, Show)
 
 instance Location location => Location (Located location) where
   type Cell (Located location) = Cell location
 
-instance (MonadAddressable location m, MonadProvenance m) => MonadAddressable (Located location) m where
+instance (MonadAddressable location m, MonadOrigin m) => MonadAddressable (Located location) m where
   derefCell (Address (Located loc _)) = derefCell (Address loc)
 
-  allocLoc name = Located <$> allocLoc name <*> askProvenance
+  allocLoc name = Located <$> allocLoc name <*> askOrigin
