af4f2dd8b97373a68db424342e06a35fb4ec7a1b
Rob Rix <rob.rix@me.com>
2018-03-29 23:13:28 -0400

    Origin is a Semigroup.

--- src/Data/Abstract/Origin.hs
+++ src/Data/Abstract/Origin.hs
@@ -3,7 +3,12 @@ module Data.Abstract.Origin where
 import Data.Abstract.Module
 import Data.Range
 import Data.Span
+import Prologue
 
 data Origin
   = Unknown
   | Local ModuleName FilePath Range Span
+
+instance Semigroup Origin where
+  a       <> Unknown = a
+  _       <> b       = b
