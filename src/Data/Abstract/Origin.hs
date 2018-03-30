393636dc736550ca3ffe89bf11b5fc67500a5563
Rob Rix <rob.rix@me.com>
2018-03-29 23:17:08 -0400

    Local’s fields are strict.

--- src/Data/Abstract/Origin.hs
+++ src/Data/Abstract/Origin.hs
@@ -7,7 +7,7 @@ import Prologue
 
 data Origin
   = Unknown
-  | Local ModuleName FilePath Range Span
+  | Local !ModuleName !FilePath !Range !Span
   deriving (Eq, Ord, Show)
 
 instance Semigroup Origin where
