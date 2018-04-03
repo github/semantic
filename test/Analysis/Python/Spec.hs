7dd2c2b0c41b5fb0f258031d1fc0f8c4760fa8fc
Timothy Clem <timothy.clem@gmail.com>
2018-04-03 15:50:18 -0700

    With print in prelude, fix up these specs

--- test/Analysis/Python/Spec.hs
+++ test/Analysis/Python/Spec.hs
@@ -12,24 +12,27 @@ spec = parallel $ do
   describe "evaluates Python" $ do
     it "imports" $ do
       res <- snd <$> evaluate "main.py"
-      environment res `shouldBe` [ ("a", addr 0)
-                                 , ("b", addr 2)
+      environment res `shouldBe` [ ("print", addr 0)
+                                 , ("a", addr 1)
+                                 , ("b", addr 3)
                                  ]
 
-      heapLookup (Address (Precise 0)) (heap res) `shouldBe` ns "a" [ ("foo", addr 1) ]
-      heapLookup (Address (Precise 2)) (heap res) `shouldBe` ns "b" [ ("c", addr 3) ]
-      heapLookup (Address (Precise 3)) (heap res) `shouldBe` ns "c" [ ("baz", addr 4) ]
+      heapLookup (Address (Precise 1)) (heap res) `shouldBe` ns "a" [ ("foo", addr 2) ]
+      heapLookup (Address (Precise 3)) (heap res) `shouldBe` ns "b" [ ("c", addr 4) ]
+      heapLookup (Address (Precise 4)) (heap res) `shouldBe` ns "c" [ ("baz", addr 5) ]
 
     it "imports with aliases" $ do
       env <- environment . snd <$> evaluate "main1.py"
-      env `shouldBe` [ ("b", addr 0)
-                     , ("e", addr 2)
+      env `shouldBe` [ ("print", addr 0)
+                     , ("b", addr 1)
+                     , ("e", addr 3)
                      ]
 
     it "imports using 'from' syntax" $ do
       env <- environment . snd <$> evaluate "main2.py"
-      env `shouldBe` [ ("foo", addr 0)
-                     , ("bar", addr 1)
+      env `shouldBe` [ ("print", addr 0)
+                     , ("foo", addr 1)
+                     , ("bar", addr 2)
                      ]
 
     it "subclasses" $ do
