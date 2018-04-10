372e1aef8cabf5db8afc4dab15fd39f57d1e849e
Ayman Nadeem <aymannadeem@github.com>
2018-04-10 14:05:24 -0700

    WIP variable declarations

--- src/Language/Java/Assignment.hs
+++ src/Language/Java/Assignment.hs
@@ -16,7 +16,7 @@ import Data.Syntax (contextualize, emptyTerm, handleError, infixContext, makeTer
 import Data.Union
 import GHC.Stack
 import Language.Java.Grammar as Grammar
-import Language.Java.Syntax as Syntax
+import Language.Java.Syntax as Java.Syntax
 import qualified Assigning.Assignment as Assignment
 import qualified Data.Syntax as Syntax
 import qualified Data.Syntax.Comment as Comment
@@ -31,13 +31,18 @@ type Syntax =
   '[ Comment.Comment
    , Declaration.Class
    , Declaration.Method
+   , Declaration.VariableDeclaration
+   , Java.Syntax.ArrayType
+   , Literal.Array
    , Literal.Integer
    , Literal.String
    , Literal.TextElement
+   , Statement.Assignment
    , Syntax.Context
    , Syntax.Empty
    , Syntax.Error
    , Syntax.Identifier
+   , Syntax.AccessibilityModifier
    , Syntax.Program
    , Type.Int
    , Type.Void
@@ -70,16 +75,49 @@ expression = handleError (choice expressionChoices)
 expressionChoices :: [Assignment.Assignment [] Grammar Term]
 expressionChoices =
   [
-    class'
+    array_initializer
+  , char
+  , class'
+  , constantDeclaration
   , identifier
   , integer
   , method
   , return'
   , string
+  , local_variable_declaration_statement
   ]
+
+modifier :: Assignment
+modifier = makeTerm <$> symbol Modifier <*> (Syntax.AccessibilityModifier <$> source)
+
+array_initializer :: Assignment
+array_initializer = makeTerm <$> symbol ArrayInitializer <*> (Literal.Array <$> many expression)
 
 comment :: Assignment
 comment = makeTerm <$> symbol Comment <*> (Comment.Comment <$> source)
+
+-- constantDeclaration :: Assignment
+-- constantDeclaration = makeTerm <$> symbol ConstantDeclaration <*>
+
+-- TODO: Rename these to be conventionally namned functions (camelCase )
+local_variable_declaration :: Assignment
+local_variable_declaration = makeDecl <$> symbol LocalVariableDeclaration <*> many type' <*> children (Declaration.VariableDeclaration <$> vDeclList)
+  where
+    makeSingleDecl loc types (target, value) = makeTerm loc (Statement.Assignment types target value)
+    makeDecl loc types decls = fmap (makeSingleDecl loc types) decls
+    vDeclList = symbol VariableDeclaratorList *> children (many variableDeclarator)
+    variableDeclarator = makeTerm <$> symbol VariableDeclarator <*> ((,) <*> variable_declarator_id <*> expression)
+
+local_variable_declaration_statement :: Assignment
+local_variable_declaration_statement = makeTerm <$> symbol LocalVariableDeclarationStatement <*> children (many local_variable_declaration)
+
+unannotated_type :: Assignment
+unannotated_type = makeTerm <$> symbol Grammar.ArrayType <*> (Java.Syntax.ArrayType <$> source)
+
+variable_declarator_id :: Assignment
+variable_declarator_id = makeTerm <$> symbol VariableDeclaratorId <*> (Syntax.Identifier <$> (name <$> source))
+
+
 
 integer :: Assignment
 integer = makeTerm <$> symbol IntegerLiteral <*> children (symbol DecimalIntegerLiteral >> Literal.Integer <$> source)
@@ -87,23 +125,31 @@ integer = makeTerm <$> symbol IntegerLiteral <*> children (symbol DecimalInteger
 string :: Assignment
 string = makeTerm <$> symbol StringLiteral <*> (Literal.TextElement <$> source)
 
+char :: Assignment
+char = makeTerm <$> symbol CharacterLiteral <*> (Literal.TextElement <$> source)
+
 class' :: Assignment
-class' = makeTerm <$> symbol ClassDeclaration <*> children (Declaration.Class [] <$> term identifier <*> pure [] <*> classBody)
+class' = makeTerm <$> symbol ClassDeclaration <*> children (Declaration.Class <$> many modifier <*> term identifier <*> pure [] <*> classBody)
   where classBody = makeTerm <$> symbol ClassBody <*> children (manyTerm expression)
 
 identifier :: Assignment
 identifier = makeTerm <$> symbol Identifier <*> (Syntax.Identifier <$> (name <$> source))
 
 method :: Assignment
-method = makeTerm <$> symbol MethodDeclaration <*> children ((method <$ symbol MethodHeader <*> emptyTerm <*> children ((,) <$> type' <* symbol MethodDeclarator <*> children ( (,) <$> identifier <*> manyTerm parameter)) ) <* symbol MethodBody <*> children (makeTerm <$> symbol Block <*> children (manyTerm expression)))
-  where method receiver (returnType, (name, params)) body = Declaration.Method [returnType] receiver name params body
+method = makeTerm <$> symbol MethodDeclaration <*> children (
+             (makeMethod <$> many modifier <* symbol MethodHeader <*> emptyTerm <*> children ((,) <$> type' <* symbol MethodDeclarator <*> children ( (,) <$> identifier <*> manyTerm parameter)) )
+          <* symbol MethodBody <*> children (makeTerm <$> symbol Block <*> children (manyTerm expression))
+          )
+  where makeMethod modifiers receiver (returnType, (name, params)) body = Declaration.Method (returnType : modifiers) receiver name params body
         parameter = makeTerm <$> symbol FormalParameter <*> children (flip Type.Annotation <$> type' <* symbol VariableDeclaratorId <*> children identifier)
+
 -- TODO: re-introduce makeTerm later; matching types as part of the type rule for now
 
 return' :: Assignment
 return' = makeTerm <$> symbol ReturnStatement <*> children (Statement.Return <$> expression)
 
 type' :: Assignment
-type' = makeTerm <$> token VoidType <*> pure Type.Void
-  <|> makeTerm <$> token IntegralType <*> pure Type.Int
-  <|> makeTerm <$> symbol FloatingPointType <*> children (token AnonFloat $> Type.Float <|> token AnonDouble $> Type.Double)
+type' =   makeTerm <$> token VoidType <*> pure Type.Void
+     <|>  makeTerm <$> token IntegralType <*> pure Type.Int
+     <|>  makeTerm <$> token FloatingPointType <*> pure Type.Float
+     -- <|> makeTerm <$> symbol FloatingPointType <*> children (token AnonFloat $> Type.Float <|> token AnonDouble $> Type.Double)
--- src/Language/Java/Syntax.hs
+++ src/Language/Java/Syntax.hs
@@ -1,1 +1,16 @@
+{-# LANGUAGE DeriveAnyClass #-}
 module Language.Java.Syntax where
+
+import Data.Abstract.Evaluatable hiding (Label)
+import Diffing.Algorithm
+import Prologue
+
+newtype ArrayType a = ArrayType ByteString
+  deriving (Diffable, Eq, FreeVariables1, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+instance Eq1 ArrayType where liftEq = genericLiftEq
+instance Ord1 ArrayType where liftCompare = genericLiftCompare
+instance Show1 ArrayType where liftShowsPrec = genericLiftShowsPrec
+
+-- TODO: Implement Eval instance for ArrayType
+instance Evaluatable ArrayType
