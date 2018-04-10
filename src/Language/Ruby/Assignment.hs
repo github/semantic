2ad0e6e968245d953b4d7b91b9c7cb146c6f2d3c
Timothy Clem <timothy.clem@gmail.com>
2018-04-09 17:44:56 -0700

    Temporary fix for foo.() style calls

--- src/Language/Ruby/Assignment.hs
+++ src/Language/Ruby/Assignment.hs
@@ -298,7 +298,7 @@ methodCall =  makeTerm' <$> symbol MethodCall <*> children (require <|> load <|>
     send = inj <$> (regularCall <|> funcCall <|> scopeCall)
     funcCall = Ruby.Syntax.Send Nothing <$> methodSelector <*> args <*> optional block
     regularCall = symbol Call *> children (Ruby.Syntax.Send <$> (Just <$> postContextualize heredoc expression) <*> methodSelector) <*> args <*> optional block
-    scopeCall = symbol ScopeResolution *> children (Ruby.Syntax.Send . Just <$> expression <*> methodSelector) <*> args <*> optional block
+    scopeCall = symbol ScopeResolution *> children (Ruby.Syntax.Send . Just <$> postContextualize heredoc expression <*> methodSelector) <*> args <*> optional block
 
     require = inj <$> ((symbol Identifier <|> symbol Identifier') *> do
       s <- source
@@ -318,7 +318,9 @@ methodSelector = mk Identifier <|> mk Identifier' <|> mk Constant
     mk s = makeTerm <$> symbol s <*> (Syntax.Identifier <$> (name <$> source))
 
 call :: Assignment
-call = makeTerm <$> symbol Call <*> children (Ruby.Syntax.Send <$> (Just <$> term expression) <*> methodSelector <*> many expression <*> optional block)
+call = makeTerm <$> symbol Call <*> children (Ruby.Syntax.Send <$> (Just <$> postContextualize heredoc expression) <*> (methodSelector <|> emptyTerm) <*> args <*> optional block)
+  where
+    args = (symbol ArgumentList <|> symbol ArgumentListWithParens) *> children (many expression) <|> many expression
 
 rescue :: Assignment
 rescue =  rescue'
@@ -414,8 +416,7 @@ invert term = makeTerm <$> location <*> fmap Expression.Not term
 
 -- | Match a term optionally preceded by comment(s), or a sequence of comments if the term is not present.
 term :: Assignment -> Assignment
-term term = contextualize comment term <|> makeTerm1 <$> (Syntax.Context <$> some1 (comment <|> heredocEnd) <*> emptyTerm)
-  where heredocEnd = makeTerm <$> symbol HeredocEnd <*> (Literal.TextElement <$> source)
+term term = contextualize comment term <|> makeTerm1 <$> (Syntax.Context <$> some1 comment <*> emptyTerm)
 
 -- | Match a series of terms or comments until a delimiter is matched.
 manyTermsTill :: Assignment.Assignment [] Grammar Term -> Assignment.Assignment [] Grammar b -> Assignment.Assignment [] Grammar [Term]
