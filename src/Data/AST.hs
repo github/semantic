653bbce7af84fcb8f96456adf22c1b9057c743a8
Ayman Nadeem <aymannadeem@github.com>
2018-04-20 11:03:23 -0700

    add termToTree to output a prettier tree

--- src/Data/AST.hs
+++ src/Data/AST.hs
@@ -5,6 +5,7 @@ import Data.Range
 import Data.Record
 import Data.Span
 import Data.Term
+import Prologue
 
 -- | An AST node labelled with symbols and source location.
 type AST syntax grammar = Term syntax (Node grammar)
@@ -21,3 +22,11 @@ type Location = '[Range, Span]
 
 nodeLocation :: Node grammar -> Record Location
 nodeLocation Node{..} = nodeByteRange :. nodeSpan :. Nil
+
+newtype Tree (syntax) = Tree (syntax (Tree syntax))
+
+instance (Show1 syntax) => Show (Tree syntax) where
+  showsPrec precedence (Tree syntax) = showsPrec1 precedence syntax
+
+termToTree :: Functor syntax => Term syntax annotation -> Tree syntax
+termToTree = cata (\ (In _ syntax) -> Tree syntax)
