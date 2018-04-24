ef4ed428cfb2872836103f27064f745527c163da
Rob Rix <rob.rix@me.com>
2018-04-24 18:39:21 -0400

    Define an Interpreter instance for BadModuleResolutions.

--- src/Analysis/Abstract/BadModuleResolutions.hs
+++ src/Analysis/Abstract/BadModuleResolutions.hs
@@ -2,6 +2,7 @@
 module Analysis.Abstract.BadModuleResolutions where
 
 import Control.Abstract.Analysis
+import Control.Monad.Effect.Internal hiding (interpret)
 import Data.Abstract.Evaluatable
 import Prologue
 
@@ -26,3 +27,9 @@ instance ( Effectful m
             TypeScriptError nameToResolve -> yield nameToResolve)
 
   analyzeModule = liftAnalyze analyzeModule
+
+instance Interpreter                                       effects  result rest                       m
+      => Interpreter (Resumable (ResolutionError value) ': effects) result rest (BadModuleResolutions m) where
+  interpret = interpret . raise @m . relay pure (\ (Resumable err) yield -> case err of
+    RubyError nameToResolve -> yield nameToResolve
+    TypeScriptError nameToResolve -> yield nameToResolve) . lower
