c7462b2de3271f4edb9189d11815d25f28d07688
Rob Rix <rob.rix@me.com>
2018-05-04 18:38:59 -0400

    Rephrase Evaluating as providingCurrentTerm, providingCurrentModule, and evaluating handlers.

--- src/Analysis/Abstract/Evaluating.hs
+++ src/Analysis/Abstract/Evaluating.hs
@@ -1,23 +1,20 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, ScopedTypeVariables, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables, TypeFamilies, TypeOperators #-}
 module Analysis.Abstract.Evaluating
-( Evaluating
-, EvaluatingState(..)
+( EvaluatingState(..)
+, providingCurrentTerm
+, providingCurrentModule
+, evaluating
 ) where
 
-import Control.Abstract.Analysis
+import Control.Abstract.Evaluator
+import Control.Abstract.Value
 import qualified Control.Monad.Effect.Internal as Eff
 import Data.Abstract.Address
 import Data.Abstract.Module
-import Data.Abstract.Origin
 import Data.Semilattice.Lower
 import Prologue
 
 -- | An analysis evaluating @term@s to @value@s with a list of @effects@ using 'Evaluatable', and producing incremental results of type @a@.
-newtype Evaluating location term value effects a = Evaluating { runEvaluating :: Eff effects a }
-  deriving (Applicative, Functor, Effectful, Evaluator location term value, Monad)
-
-deriving instance Member NonDet effects => Alternative (Evaluating location term value effects)
-
 data EvaluatingState location term value = EvaluatingState
   { environment :: Environment location value
   , heap        :: Heap location value
@@ -47,39 +44,37 @@ type EvaluatingEffects location term value
      , State (JumpTable term)
      ]
 
-instance (termInfo ~ Base term (), Recursive term) => AnalyzeTerm location term value (Reader termInfo ': effects) effects (Evaluating location term value) where
-  analyzeTerm eval = handleReader . (() <$) <*> eval . fmap (second (raiseHandler weakenEff))
+providingCurrentTerm :: (Effectful m, Functor (Base term))
+                     => SubtermAlgebra (Base term) term (m (Reader (Base term ()) ': effects) a)
+                     -> SubtermAlgebra (Base term) term (m effects a)
+providingCurrentTerm recur term = liftHandlerOverSubtermAlgebra (handleReader (() <$ term)) recur term
 
-instance AnalyzeModule location term value (Reader ModuleInfo ': effects) effects (Evaluating location term value) where
-  analyzeModule eval = handleReader . moduleInfo <*> eval . fmap (second (raiseHandler weakenEff))
+providingCurrentModule :: Effectful m
+                       => SubtermAlgebra Module term (m (Reader ModuleInfo ': effects) a)
+                       -> SubtermAlgebra Module term (m effects a)
+providingCurrentModule recur m = liftHandlerOverSubtermAlgebra (handleReader (moduleInfo m)) recur m
 
-weakenEff :: Eff effects a -> Eff (effect ': effects) a
-weakenEff (Eff.Val a) = pure a
-weakenEff (Eff.E u q) = Eff.E (weaken u) (Eff.tsingleton (q Eff.>>> weakenEff))
 
-instance (AbstractHole value, Show value) => Interpreter (Evaluating location term value) (EvaluatingEffects location term value) where
-  type Result (Evaluating location term value) (EvaluatingEffects location term value) result
-    = ( Either String result
-      , EvaluatingState location term value)
-  interpret
-    = (\ (((((result, env), heap), modules), exports), jumps) -> (result, EvaluatingState env heap modules exports jumps))
-    . interpret
-    . runEvaluating
-    . handleState lowerBound -- State (JumpTable term)
-    . handleState lowerBound -- State (Exports location value)
-    . handleState lowerBound -- State (ModuleTable (Environment location value, value))
-    . handleState lowerBound -- State (Heap location value)
-    . handleState lowerBound -- State (Environment location value)
-    . handleReader lowerBound -- Reader LoadStack
-    . handleReader lowerBound -- Reader (Environment location value)
-    . handleReader lowerBound -- Reader (SomeOrigin term)
-    . raiseHandler
-      ( flip runFresh' 0
-      . runFail
-      -- NB: We should never have a 'Return', 'Break', or 'Continue' at this point in execution; the scope being returned from/broken from/continued should have intercepted the effect. This handler will therefore only be invoked if we issue a 'Return', 'Break', or 'Continue' outside of such a scope, and unfortunately if this happens it will handle it by resuming the scope being returned from. While it would be _slightly_ more correct to instead exit with the value being returned, we aren’t able to do that here since 'Interpreter'’s type is parametric in the value being returned—we don’t know that we’re returning a @value@ (because we very well may not be). On the balance, I felt the strange behaviour in error cases is worth the improved behaviour in the common case—we get to lose a layer of 'Either' in the result for each.
-      -- In general, it’s expected that none of the following effects will remain by the time 'interpret' is called—they should have been handled by local 'interpose's—but if they do, we’ll at least trace.
-      . Eff.interpret (\ control -> case control of
-        Break value -> traceM ("Evaluating.interpret: resuming uncaught break with " <> show value) $> value
-        Continue    -> traceM "Evaluating.interpret: resuming uncaught continue with hole" $> hole)
-      . Eff.interpret (\ (Return value) -> traceM ("Evaluating.interpret: resuming uncaught return with " <> show value) $> value))
-      -- TODO: Replace 'traceM's with e.g. 'Telemetry'.
+evaluating :: (AbstractHole value, Effectful m, Show value) => m (EvaluatingEffects location term value) result -> (Either String result, EvaluatingState location term value)
+evaluating
+  = (\ (((((result, env), heap), modules), exports), jumps) -> (result, EvaluatingState env heap modules exports jumps))
+  . Eff.run
+  . lower
+  . handleState lowerBound -- State (JumpTable term)
+  . handleState lowerBound -- State (Exports location value)
+  . handleState lowerBound -- State (ModuleTable (Environment location value, value))
+  . handleState lowerBound -- State (Heap location value)
+  . handleState lowerBound -- State (Environment location value)
+  . handleReader lowerBound -- Reader LoadStack
+  . handleReader lowerBound -- Reader (Environment location value)
+  . handleReader lowerBound -- Reader (SomeOrigin term)
+  . raiseHandler
+    ( flip runFresh' 0
+    . runFail
+    -- NB: We should never have a 'Return', 'Break', or 'Continue' at this point in execution; the scope being returned from/broken from/continued should have intercepted the effect. This handler will therefore only be invoked if we issue a 'Return', 'Break', or 'Continue' outside of such a scope, and unfortunately if this happens it will handle it by resuming the scope being returned from. While it would be _slightly_ more correct to instead exit with the value being returned, we aren’t able to do that here since 'Interpreter'’s type is parametric in the value being returned—we don’t know that we’re returning a @value@ (because we very well may not be). On the balance, I felt the strange behaviour in error cases is worth the improved behaviour in the common case—we get to lose a layer of 'Either' in the result for each.
+    -- In general, it’s expected that none of the following effects will remain by the time 'interpret' is called—they should have been handled by local 'interpose's—but if they do, we’ll at least trace.
+    . Eff.interpret (\ control -> case control of
+      Break value -> traceM ("Evaluating.interpret: resuming uncaught break with " <> show value) $> value
+      Continue    -> traceM "Evaluating.interpret: resuming uncaught continue with hole" $> hole)
+    . Eff.interpret (\ (Return value) -> traceM ("Evaluating.interpret: resuming uncaught return with " <> show value) $> value))
+    -- TODO: Replace 'traceM's with e.g. 'Telemetry'.
