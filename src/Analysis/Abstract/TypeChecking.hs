9af39cad2fe04e2b5153e227eaf703da4b93410c
Rob Rix <rob.rix@me.com>
2018-05-04 18:43:45 -0400

    Rephrase TypeChecking as a simple handler.

--- src/Analysis/Abstract/TypeChecking.hs
+++ src/Analysis/Abstract/TypeChecking.hs
@@ -1,28 +1,10 @@
-{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
 module Analysis.Abstract.TypeChecking
-( TypeChecking
+( typeChecking
 ) where
 
-import Control.Abstract.Analysis
+import Control.Abstract.Evaluator
 import Data.Abstract.Type
-import Prologue hiding (TypeError)
 
-newtype TypeChecking m (effects :: [* -> *]) a = TypeChecking { runTypeChecking :: m effects a }
-  deriving (Alternative, Applicative, Functor, Effectful, Monad)
-
-deriving instance Evaluator location term value m => Evaluator location term value (TypeChecking m)
-deriving instance AnalyzeModule location term value inner outer m => AnalyzeModule location term value inner outer (TypeChecking m)
-deriving instance AnalyzeTerm location term value inner outer m => AnalyzeTerm location term value inner outer (TypeChecking m)
-
-instance Interpreter m effects
-      => Interpreter (TypeChecking m) (Resumable TypeError ': effects) where
-  type Result (TypeChecking m) (Resumable TypeError ': effects) result = Result m effects (Either (SomeExc TypeError) result)
-  interpret
-    = interpret
-    . runTypeChecking
-    -- TODO: We should handle TypeError by yielding both sides of the exception,
-    -- but something is mysteriously busted in the innards of typechecking,
-    -- so doing that just yields an empty list in the result type, which isn't
-    -- extraordinarily helpful. Better for now to just die with an error and
-    -- tackle this issue in a separate PR.
-    . raiseHandler runError
+typeChecking :: Effectful m => m (Resumable TypeError ': effects) a -> m effects (Either (SomeExc TypeError) a)
+typeChecking = raiseHandler runError
