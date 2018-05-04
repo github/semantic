d31dce7490b6d5385405facbfd42d3cdb9886300
Rob Rix <rob.rix@me.com>
2018-05-04 19:40:53 -0400

    Provide empty cells with a Lower constraint.

--- src/Analysis/Abstract/BadAddresses.hs
+++ src/Analysis/Abstract/BadAddresses.hs
@@ -7,9 +7,10 @@ import Control.Abstract.Addressable
 import Control.Abstract.Evaluator
 import Control.Abstract.Value
 import Data.Abstract.Address
+import Data.Semilattice.Lower
 import Prologue
 
-resumingBadAddresses :: (AbstractHole value, Monoid (Cell location value), Show location) => Evaluator location term value (Resumable (AddressError location value) ': effects) a -> Evaluator location term value effects a
+resumingBadAddresses :: (AbstractHole value, Lower (Cell location value), Show location) => Evaluator location term value (Resumable (AddressError location value) ': effects) a -> Evaluator location term value effects a
 resumingBadAddresses = raiseHandler (relay pure (\ (Resumable err) yield -> traceM ("AddressError:" <> show err) *> case err of
-  UnallocatedAddress _   -> yield mempty
+  UnallocatedAddress _   -> yield lowerBound
   UninitializedAddress _ -> yield hole))
