b5c2cd6282f0f4c0d2e7a5ab2a8a7d0fa7223a87
Rob Rix <rob.rix@me.com>
2018-05-04 18:39:54 -0400

    Rephrase BadValues as a resumingBadValues handler.

--- src/Analysis/Abstract/BadValues.hs
+++ src/Analysis/Abstract/BadValues.hs
@@ -1,42 +1,26 @@
-{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving, ScopedTypeVariables, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE GADTs, TypeOperators #-}
 module Analysis.Abstract.BadValues
-( BadValues
+( resumingBadValues
 ) where
 
-import Control.Abstract.Analysis
+import Control.Abstract.Evaluator
+import Control.Abstract.Value
 import Data.Abstract.Value (ValueError(..))
 import Data.ByteString.Char8 (pack)
 import Prologue
 
-newtype BadValues m (effects :: [* -> *]) a = BadValues { runBadValues :: m effects a }
-  deriving (Alternative, Applicative, Functor, Effectful, Monad)
-
-deriving instance Evaluator location term value m => Evaluator location term value (BadValues m)
-deriving instance AnalyzeModule location term value inner outer m => AnalyzeModule location term value inner outer (BadValues m)
-deriving instance AnalyzeTerm location term value inner outer m => AnalyzeTerm location term value inner outer (BadValues m)
-
-instance ( AbstractHole value
-         , Evaluator location term value m
-         , Interpreter m effects
-         , Member (State (Environment location value)) effects
-         , Show value
-         )
-      => Interpreter (BadValues m) (Resumable (ValueError location value) ': effects) where
-  type Result (BadValues m) (Resumable (ValueError location value) ': effects) result = Result m effects result
-  interpret
-    = interpret
-    . runBadValues
-    . raiseHandler (relay pure (\ (Resumable err) yield -> traceM ("ValueError" <> show err) *> case err of
-      CallError val     -> yield val
-      StringError val   -> yield (pack (show val))
-      BoolError{}       -> yield True
-      BoundsError{}     -> yield hole
-      IndexError{}      -> yield hole
-      NumericError{}    -> yield hole
-      Numeric2Error{}   -> yield hole
-      ComparisonError{} -> yield hole
-      NamespaceError{}  -> lower @m getEnv >>= yield
-      BitwiseError{}    -> yield hole
-      Bitwise2Error{}   -> yield hole
-      KeyValueError{}   -> yield (hole, hole)
-      ArithmeticError{} -> yield hole))
+resumingBadValues :: (AbstractHole value, Member (State (Environment location value)) effects, Show value) => Evaluator location term value (Resumable (ValueError location value) ': effects) a -> Evaluator location term value effects a
+resumingBadValues = raiseHandler (relay pure (\ (Resumable err) yield -> traceM ("ValueError" <> show err) *> case err of
+  CallError val     -> yield val
+  StringError val   -> yield (pack (show val))
+  BoolError{}       -> yield True
+  BoundsError{}     -> yield hole
+  IndexError{}      -> yield hole
+  NumericError{}    -> yield hole
+  Numeric2Error{}   -> yield hole
+  ComparisonError{} -> yield hole
+  NamespaceError{}  -> lower getEnv >>= yield
+  BitwiseError{}    -> yield hole
+  Bitwise2Error{}   -> yield hole
+  KeyValueError{}   -> yield (hole, hole)
+  ArithmeticError{} -> yield hole))
