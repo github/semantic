8a8bfc05a3299d51de02e8b0cc335869619b3942
Rob Rix <rob.rix@me.com>
2018-05-04 18:40:07 -0400

    Rephrase BadVariables as a resumingBadVariables handler.

--- src/Analysis/Abstract/BadVariables.hs
+++ src/Analysis/Abstract/BadVariables.hs
@@ -1,39 +1,21 @@
-{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving, TypeFamilies, TypeOperators, UndecidableInstances #-}
-{-# OPTIONS_GHC -Wno-redundant-constraints #-} -- For the Interpreter instance’s Evaluator constraint
+{-# LANGUAGE GADTs, TypeOperators #-}
 module Analysis.Abstract.BadVariables
-( BadVariables
+( resumingBadVariables
 ) where
 
-import Control.Abstract.Analysis
+import Control.Abstract.Evaluator
 import Data.Abstract.Evaluatable
 import Prologue
 
--- An analysis that resumes from evaluation errors and records the list of unresolved free variables.
-newtype BadVariables m (effects :: [* -> *]) a = BadVariables { runBadVariables :: m effects a }
-  deriving (Alternative, Applicative, Functor, Effectful, Monad)
-
-deriving instance Evaluator location term value m => Evaluator location term value (BadVariables m)
-deriving instance AnalyzeModule location term value inner outer m => AnalyzeModule location term value inner outer (BadVariables m)
-deriving instance AnalyzeTerm location term value inner outer m => AnalyzeTerm location term value inner outer (BadVariables m)
-
-instance ( AbstractHole value
-         , Evaluator location term value m
-         , Interpreter m effects
-         , Show value
-         )
-      => Interpreter (BadVariables m) (Resumable (EvalError value) ': State [Name] ': effects) where
-  type Result (BadVariables m) (Resumable (EvalError value) ': State [Name] ': effects) result = Result m effects (result, [Name])
-  interpret
-    = interpret
-    . runBadVariables
-    . raiseHandler
-      ( flip runState []
-      . relay pure (\ (Resumable err) yield -> traceM ("EvalError" <> show err) *> case err of
-        EnvironmentLookupError{} -> yield hole
-        DefaultExportError{}     -> yield ()
-        ExportError{}            -> yield ()
-        IntegerFormatError{}     -> yield 0
-        FloatFormatError{}       -> yield 0
-        RationalFormatError{}    -> yield 0
-        FreeVariableError name   -> modify' (name :) *> yield hole
-        FreeVariablesError names -> modify' (names <>) *> yield (fromMaybeLast "unknown" names)))
+resumingBadVariables :: (AbstractHole value, Effectful m, Show value) => m (Resumable (EvalError value) ': State [Name] ': effects) a -> m effects (a, [Name])
+resumingBadVariables
+  = handleState []
+  . raiseHandler (relay pure (\ (Resumable err) yield -> traceM ("EvalError" <> show err) *> case err of
+    EnvironmentLookupError{} -> yield hole
+    DefaultExportError{}     -> yield ()
+    ExportError{}            -> yield ()
+    IntegerFormatError{}     -> yield 0
+    FloatFormatError{}       -> yield 0
+    RationalFormatError{}    -> yield 0
+    FreeVariableError name   -> modify' (name :) *> yield hole
+    FreeVariablesError names -> modify' (names <>) *> yield (fromMaybeLast "unknown" names)))
