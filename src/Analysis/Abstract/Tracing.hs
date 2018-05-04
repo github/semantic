cdb8f26de42e46aacda5c258401eb61ccfb15641
Rob Rix <rob.rix@me.com>
2018-05-04 19:12:51 -0400

    Rephrase Tracing as tracingTerms & tracing handlers.

--- src/Analysis/Abstract/Tracing.hs
+++ src/Analysis/Abstract/Tracing.hs
@@ -1,45 +1,36 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, ScopedTypeVariables, TypeFamilies, TypeOperators, UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-redundant-constraints #-} -- For the Interpreter instance’s Evaluator constraint
 module Analysis.Abstract.Tracing
-( Tracing
+( tracingTerms
+, tracing
 ) where
 
-import Control.Abstract.Analysis
+import Control.Abstract.Evaluator
 import Control.Monad.Effect.Writer
 import Data.Abstract.Configuration
 import Data.Abstract.Live
 import Data.Semigroup.Reducer as Reducer
-import Prologue
+import Prologue hiding (trace)
 
 -- | Trace analysis.
 --
 --   Instantiating @trace@ to @[]@ yields a linear trace analysis, while @Set@ yields a reachable state analysis.
-newtype Tracing (trace :: * -> *) m (effects :: [* -> *]) a = Tracing { runTracing :: m effects a }
-  deriving (Alternative, Applicative, Functor, Effectful, Monad)
+tracingTerms :: ( Corecursive term
+                , Members '[ Reader (Live location value)
+                           , State (Environment location value)
+                           , State (Heap location value)
+                           , Writer (trace (Configuration location term value))
+                           ] effects
+                , Ord location
+                , Reducer (Configuration location term value) (trace (Configuration location term value))
+                )
+             => trace (Configuration location term value)
+             -> SubtermAlgebra (Base term) term (Evaluator location term value effects a)
+             -> SubtermAlgebra (Base term) term (Evaluator location term value effects a)
+tracingTerms proxy recur term = getConfiguration (embedSubterm term) >>= trace . (`asTypeOf` proxy) . Reducer.unit >> recur term
 
-deriving instance Evaluator location term value m => Evaluator location term value (Tracing trace m)
-deriving instance AnalyzeModule location term value inner outer m => AnalyzeModule location term value inner outer (Tracing trace m)
+trace :: Member (Writer (trace (Configuration location term value))) effects => trace (Configuration location term value) -> Evaluator location term value effects ()
+trace = raise . tell
 
-instance ( Corecursive term
-         , Effectful m
-         , Member (Reader (Live location value)) effectsOut
-         , Member (Writer (trace (Configuration location term value))) effectsOut
-         , AnalyzeTerm location term value effectsIn effectsOut m
-         , Evaluator location term value m
-         , MonadEvaluator location term value effectsOut m
-         , Ord location
-         , Reducer (Configuration location term value) (trace (Configuration location term value))
-         )
-      => AnalyzeTerm location term value effectsIn effectsOut (Tracing trace m) where
-  analyzeTerm recur term = do
-    config <- getConfiguration (embedSubterm term)
-    raise (tell @(trace (Configuration location term value)) (Reducer.unit config))
-    Tracing (analyzeTerm (runTracing . recur) term)
-
-instance ( Evaluator location term value m
-         , Interpreter m effects
-         , Monoid (trace (Configuration location term value))
-         )
-      => Interpreter (Tracing trace m) (Writer (trace (Configuration location term value)) ': effects) where
-  type Result (Tracing trace m) (Writer (trace (Configuration location term value)) ': effects) result = Result m effects (result, trace (Configuration location term value))
-  interpret = interpret . runTracing . raiseHandler runWriter
+tracing :: Monoid (trace (Configuration location term value)) => Evaluator location term value (Writer (trace (Configuration location term value)) ': effects) a -> Evaluator location term value effects (a, trace (Configuration location term value))
+tracing = raiseHandler runWriter
