3a2ba0b6319671b9f7ba6bb5a4b38e090b0d1a6c
Rob Rix <rob.rix@me.com>
2018-05-06 12:04:29 -0400

    Generalize resumingBadModuleResolutions.

--- src/Analysis/Abstract/BadModuleResolutions.hs
+++ src/Analysis/Abstract/BadModuleResolutions.hs
@@ -7,7 +7,7 @@ import Control.Abstract.Evaluator
 import Data.Abstract.Evaluatable
 import Prologue
 
-resumingBadModuleResolutions :: Evaluator location term value (Resumable ResolutionError ': effects) a -> Evaluator location term value effects a
+resumingBadModuleResolutions :: Effectful m => m (Resumable ResolutionError ': effects) a -> m effects a
 resumingBadModuleResolutions = raiseHandler (relay pure (\ (Resumable err) yield -> traceM ("ResolutionError:" <> show err) *> case err of
   NotFoundError nameToResolve _ _ -> yield  nameToResolve
   GoImportError pathToResolve     -> yield [pathToResolve]))
