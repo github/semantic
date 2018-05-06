a23bc350da86db96df59ce2ede05d3543c2364e7
Rob Rix <rob.rix@me.com>
2018-05-06 17:36:33 -0400

    Constrain the specs to Value Precise.

--- test/SpecHelpers.hs
+++ test/SpecHelpers.hs
@@ -75,6 +75,10 @@ testEvaluating
   . runResolutionError
   . runEvalError
   . runAddressError
+  . constrainedToValuePrecise
+
+constrainedToValuePrecise :: Evaluator Precise term (Value Precise) effects a -> Evaluator Precise term (Value Precise) effects a
+constrainedToValuePrecise = id
 
 ns n = Just . Latest . Just . injValue . Namespace n
 addr = Address . Precise
