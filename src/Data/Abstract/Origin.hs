5038d11b8ef1a926e4dda82035ddbd6399b70956
Rob Rix <rob.rix@me.com>
2018-05-06 13:16:30 -0400

    Reformat Origin.

--- src/Data/Abstract/Origin.hs
+++ src/Data/Abstract/Origin.hs
@@ -3,5 +3,9 @@ module Data.Abstract.Origin where
 import Data.Abstract.Module
 import Data.Abstract.Package
 
-data Origin term = Origin { originPackage :: PackageInfo, originModule :: ModuleInfo, originTerm :: term }
+data Origin term = Origin
+  { originPackage :: PackageInfo
+  , originModule :: ModuleInfo
+  , originTerm :: term
+  }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
