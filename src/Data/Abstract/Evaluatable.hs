3b497bd2784b8f87fabcc5b46c4422a5ae8f453e
Rob Rix <rob.rix@me.com>
2018-05-07 10:24:35 -0400

    :fire: an unnecessary language extension.

--- src/Data/Abstract/Evaluatable.hs
+++ src/Data/Abstract/Evaluatable.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ConstraintKinds, DefaultSignatures, GADTs, RankNTypes, ScopedTypeVariables, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE ConstraintKinds, DefaultSignatures, GADTs, RankNTypes, TypeOperators, UndecidableInstances #-}
 module Data.Abstract.Evaluatable
 ( module X
 , Evaluatable(..)
