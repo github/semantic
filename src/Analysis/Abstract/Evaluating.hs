21717cd0798f13334dca2ce7fff3f278cc67cd48
Rob Rix <rob.rix@me.com>
2018-05-08 16:42:27 -0400

    Use the full type for the modules field of EvaluatingState.

--- src/Analysis/Abstract/Evaluating.hs
+++ src/Analysis/Abstract/Evaluating.hs
@@ -12,7 +12,7 @@ import Data.Semilattice.Lower
 data EvaluatingState location value = EvaluatingState
   { environment :: Environment location value
   , heap        :: Heap location value
-  , modules     :: EvaluatedModules location value
+  , modules     :: ModuleTable (Maybe (Environment location value, value))
   , exports     :: Exports location value
   }
 
@@ -27,14 +27,14 @@ evaluating :: Evaluator location term value
                 ': Reader (Environment location value)
                 ': State (Environment location value)
                 ': State (Heap location value)
-                ': State (EvaluatedModules location value)
+                ': State (ModuleTable (Maybe (Environment location value, value)))
                 ': State (Exports location value)
                 ': effects) result
            -> Evaluator location term value effects (Either String result, EvaluatingState location value)
 evaluating
   = fmap (\ ((((result, env), heap), modules), exports) -> (result, EvaluatingState env heap modules exports))
   . runState lowerBound -- State (Exports location value)
-  . runState lowerBound -- State (EvaluatedModules locaiton value)
+  . runState lowerBound -- State (ModuleTable (Maybe (Environment location value, value)))
   . runState lowerBound -- State (Heap location value)
   . runState lowerBound -- State (Environment location value)
   . runReader lowerBound -- Reader (Environment location value)
