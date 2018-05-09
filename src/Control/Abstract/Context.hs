30c4864f833af15975f8d02b3a456264425d4492
Rob Rix <rob.rix@me.com>
2018-05-09 16:16:33 -0400

    Define a helper to run an action with context from the Haskell call stack.

--- src/Control/Abstract/Context.hs
+++ src/Control/Abstract/Context.hs
@@ -8,6 +8,7 @@ module Control.Abstract.Context
 , Span
 , currentSpan
 , withCurrentSpan
+, withCurrentCallStack
 ) where
 
 import Control.Effect
@@ -15,6 +16,7 @@ import Control.Monad.Effect.Reader
 import Data.Abstract.Module
 import Data.Abstract.Package
 import Data.Span
+import GHC.Stack
 import Prologue
 
 -- | Get the currently evaluating 'ModuleInfo'.
@@ -40,3 +42,14 @@ currentSpan = raise ask
 -- | Run an action with a locally-replaced 'Span'.
 withCurrentSpan :: (Effectful m, Member (Reader Span) effects) => Span -> m effects a -> m effects a
 withCurrentSpan = raiseHandler . local . const
+
+
+-- | Run an action with locally-replaced 'ModuleInfo' & 'Span' derived from the passed 'SrcLoc'.
+withCurrentSrcLoc :: (Effectful m, Members '[Reader ModuleInfo, Reader Span] effects) => SrcLoc -> m effects a -> m effects a
+withCurrentSrcLoc loc = withCurrentModule (moduleInfoFromSrcLoc loc) . withCurrentSpan (spanFromSrcLoc loc)
+
+-- | Run ana ction with locally replaced 'ModuleInfo' & 'Span' derived from the Haskell call stack.
+--
+--   This is suitable for contextualizing builtins & other functionality intended for use from client code but defined in Haskell source.
+withCurrentCallStack :: (Effectful m, HasCallStack, Members '[Reader ModuleInfo, Reader Span] effects) => m effects a -> m effects a
+withCurrentCallStack = maybe id (withCurrentSrcLoc . snd) (listToMaybe (getCallStack callStack))
