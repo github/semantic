b359cc91a36a43f77161e97fc0b447fb4f6826d9
Rob Rix <rob.rix@me.com>
2018-05-09 13:38:19 -0400

    Reformat Configuration.

--- src/Data/Abstract/Configuration.hs
+++ src/Data/Abstract/Configuration.hs
@@ -8,14 +8,13 @@ import Data.Abstract.Live
 import Prologue
 
 -- | A single point in a program’s execution.
-data Configuration location term value
-  = Configuration
-    { configurationTerm        :: term                       -- ^ The “instruction,” i.e. the current term to evaluate.
-    , configurationRoots       :: Live location value        -- ^ The set of rooted addresses.
-    , configurationEnvironment :: Environment location value -- ^ The environment binding any free variables in 'configurationTerm'.
-    , configurationHeap        :: Heap location value        -- ^ The heap of values.
-    }
-    deriving (Generic1)
+data Configuration location term value = Configuration
+  { configurationTerm        :: term                       -- ^ The “instruction,” i.e. the current term to evaluate.
+  , configurationRoots       :: Live location value        -- ^ The set of rooted addresses.
+  , configurationEnvironment :: Environment location value -- ^ The environment binding any free variables in 'configurationTerm'.
+  , configurationHeap        :: Heap location value        -- ^ The heap of values.
+  }
+  deriving (Generic1)
 
 deriving instance (Eq   location, Eq   term, Eq   value, Eq   (Cell location value)) => Eq   (Configuration location term value)
 deriving instance (Ord  location, Ord  term, Ord  value, Ord  (Cell location value)) => Ord  (Configuration location term value)
