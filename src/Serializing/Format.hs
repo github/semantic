883eb6642292d0c43d65cab535885572d4194050
Rob Rix <rob.rix@me.com>
2018-05-11 14:12:13 -0400

    Define a serialization format type.

--- src/Serializing/Format.hs
+++ src/Serializing/Format.hs
@@ -1,1 +1,11 @@
-module Serializing.Format where
+{-# LANGUAGE GADTs #-}
+module Serializing.Format
+( Format(..)
+, Builder
+) where
+
+import Algebra.Graph.Class
+import Data.ByteString.Builder
+
+data Format input where
+  DOT :: (Ord vertex, ToGraph graph, ToVertex graph ~ vertex) => Style vertex Builder -> Format graph
