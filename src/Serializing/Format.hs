456171a9314abbcff9aace26969ca3ce5376c36e
Rob Rix <rob.rix@me.com>
2018-05-11 14:12:24 -0400

    Serialize DOT to a Builder.

--- src/Serializing/Format.hs
+++ src/Serializing/Format.hs
@@ -2,10 +2,15 @@
 module Serializing.Format
 ( Format(..)
 , Builder
+, runSerialize
 ) where
 
 import Algebra.Graph.Class
 import Data.ByteString.Builder
+import Serializing.DOT
 
 data Format input where
   DOT :: (Ord vertex, ToGraph graph, ToVertex graph ~ vertex) => Style vertex Builder -> Format graph
+
+runSerialize :: Format input -> input -> Builder
+runSerialize (DOT style) graph = serializeDOT style graph
