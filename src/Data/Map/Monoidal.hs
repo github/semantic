4c4ae865146aecb4751018df5f2070692ecbabb3
Rob Rix <rob.rix@me.com>
2018-05-11 09:10:56 -0400

    Define unMap as a function rather than a field selector.

--- src/Data/Map/Monoidal.hs
+++ src/Data/Map/Monoidal.hs
@@ -15,8 +15,11 @@ import Data.Semilattice.Lower
 import Prelude hiding (lookup)
 import Prologue hiding (Map)
 
-newtype Map key value = Map { unMap :: Map.Map key value }
+newtype Map key value = Map (Map.Map key value)
   deriving (Eq, Eq1, Eq2, Foldable, Functor, Ord, Ord1, Ord2, Show, Show1, Show2, Traversable)
+
+unMap :: Map key value -> Map.Map key value
+unMap (Map map) = map
 
 
 lookup :: Ord key => key -> Map key value -> Maybe value
