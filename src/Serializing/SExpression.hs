4944fdfe216c0748486df904370f1c1c597153bc
Rob Rix <rob.rix@me.com>
2018-05-11 14:43:57 -0400

    Define serialization to s-expressions.

--- src/Serializing/SExpression.hs
+++ src/Serializing/SExpression.hs
@@ -1,1 +1,41 @@
-module Serializing.SExpression where
+{-# LANGUAGE GADTs, RankNTypes, ScopedTypeVariables #-}
+module Serializing.SExpression
+( serializeSExpression
+, ToSExpression(..)
+) where
+
+import Analysis.ConstructorName
+import Data.ByteString.Builder
+import Data.Diff
+import Data.Patch
+import Data.Term
+import Prelude
+import Prologue
+
+serializeSExpression :: (Recursive t, ToSExpression (Base t)) => t -> Builder
+serializeSExpression t = cata toSExpression t 0 <> "\n"
+
+printTermF :: (ConstructorName syntax, Foldable syntax) => syntax (Int -> Builder) -> Int -> Builder
+printTermF syntax n = "(" <> stringUtf8 (constructorName syntax) <> foldMap ($ (n + 1)) syntax <> ")"
+
+nl :: Int -> Builder
+nl n | n <= 0    = ""
+     | otherwise = "\n"
+
+pad :: Int -> Builder
+pad n = stringUtf8 (replicate (2 * n) ' ')
+
+
+class ToSExpression base where
+  toSExpression :: base (Int -> Builder) -> (Int -> Builder)
+
+instance (ConstructorName syntax, Foldable syntax) => ToSExpression (TermF syntax ann) where
+  toSExpression (In _ syntax) n = nl n <> pad n <> printTermF syntax n
+
+instance (ConstructorName syntax, Foldable syntax) => ToSExpression (DiffF syntax ann1 ann2) where
+  toSExpression diff n = case diff of
+    Patch (Delete term) -> nl n <> pad (n - 1) <> "{-" <> printTermF (termFOut term) n <> "-}"
+    Patch (Insert term) -> nl n <> pad (n - 1) <> "{+" <> printTermF (termFOut term) n <> "+}"
+    Patch (Replace term1 term2) -> nl n       <> pad (n - 1) <> "{ " <> printTermF (termFOut term1) n
+                                <> nl (n + 1) <> pad (n - 1) <> "->" <> printTermF (termFOut term2) n <> " }"
+    Merge term -> nl n <> pad n <> "(" <> stringUtf8 (constructorName (termFOut term)) <> foldMap ($ (n + 1)) (termFOut term) <> ")"
