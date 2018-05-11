c5dd415ac851b858ed0ac4063e516184934aa914
Rob Rix <rob.rix@me.com>
2018-05-11 14:38:11 -0400

    Add support for serializing by Showing.

--- src/Serializing/Format.hs
+++ src/Serializing/Format.hs
@@ -10,7 +10,9 @@ import Data.ByteString.Builder
 import Serializing.DOT
 
 data Format input where
-  DOT :: (Ord vertex, ToGraph graph, ToVertex graph ~ vertex) => Style vertex Builder -> Format graph
+  DOT  :: (Ord vertex, ToGraph graph, ToVertex graph ~ vertex) => Style vertex Builder -> Format graph
+  Show :: Show input                                                                   => Format input
 
 runSerialize :: Format input -> input -> Builder
 runSerialize (DOT style) graph = serializeDOT style graph
+runSerialize Show        input = stringUtf8 (show input)
