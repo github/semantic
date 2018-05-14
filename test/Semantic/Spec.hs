5bae88724b9c2bb1b7cac2202e3b4bb93e4be36e
Rob Rix <rob.rix@me.com>
2018-05-14 16:09:39 -0400

    Move the diffTermPair tests into the interpreter spec.

--- test/Semantic/Spec.hs
+++ test/Semantic/Spec.hs
@@ -18,15 +18,5 @@ spec = parallel $ do
     it "renders with the specified renderer" $ do
       output <- fmap toOutput . runTask $ parseBlob SExpressionTermRenderer methodsBlob
       output `shouldBe` "(Program\n  (Method\n    (Empty)\n    (Identifier)\n    ([])))\n"
-
-  describe "diffTermPair" $ do
-    it "produces an Insert when the first term is missing" $ do
-      result <- runTask (diffTermPair replacing (That (termIn () [])))
-      result `shouldBe` (Diff (Patch (Insert (In () []))) :: Diff [] () ())
-
-    it "produces a Delete when the second term is missing" $ do
-      result <- runTask (diffTermPair replacing (This (termIn () [])))
-      result `shouldBe` (Diff (Patch (Delete (In () []))) :: Diff [] () ())
-
   where
     methodsBlob = Blob "def foo\nend\n" "methods.rb" (Just Ruby)
