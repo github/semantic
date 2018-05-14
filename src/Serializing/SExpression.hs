7908bd8e49dd9d8a9c797bb5820b61be8bd29cb8
Rob Rix <rob.rix@me.com>
2018-05-14 11:13:56 -0400

    Pass the name to printTermF.

--- src/Serializing/SExpression.hs
+++ src/Serializing/SExpression.hs
@@ -15,8 +15,8 @@ import Prologue
 serializeSExpression :: (Recursive t, ToSExpression (Base t)) => t -> Builder
 serializeSExpression t = cata toSExpression t 0 <> "\n"
 
-printTermF :: (ConstructorName syntax, Foldable syntax) => syntax (Int -> Builder) -> Int -> Builder
-printTermF syntax n = "(" <> stringUtf8 (constructorName syntax) <> foldMap ($ (n + 1)) syntax <> ")"
+printTermF :: Foldable syntax => String -> syntax (Int -> Builder) -> Int -> Builder
+printTermF name syntax n = "(" <> stringUtf8 name <> foldMap ($ (n + 1)) syntax <> ")"
 
 nl :: Int -> Builder
 nl n | n <= 0    = ""
@@ -30,12 +30,12 @@ class ToSExpression base where
   toSExpression :: base (Int -> Builder) -> (Int -> Builder)
 
 instance (ConstructorName syntax, Foldable syntax) => ToSExpression (TermF syntax ann) where
-  toSExpression (In _ syntax) n = nl n <> pad n <> printTermF syntax n
+  toSExpression (In _ syntax) n = nl n <> pad n <> printTermF (constructorName syntax) syntax n
 
 instance (ConstructorName syntax, Foldable syntax) => ToSExpression (DiffF syntax ann1 ann2) where
   toSExpression diff n = case diff of
-    Patch (Delete term) -> nl n <> pad (n - 1) <> "{-" <> printTermF (termFOut term) n <> "-}"
-    Patch (Insert term) -> nl n <> pad (n - 1) <> "{+" <> printTermF (termFOut term) n <> "+}"
-    Patch (Replace term1 term2) -> nl n       <> pad (n - 1) <> "{ " <> printTermF (termFOut term1) n
-                                <> nl (n + 1) <> pad (n - 1) <> "->" <> printTermF (termFOut term2) n <> " }"
+    Patch (Delete term) -> nl n <> pad (n - 1) <> "{-" <> printTermF (constructorName (termFOut term)) (termFOut term) n <> "-}"
+    Patch (Insert term) -> nl n <> pad (n - 1) <> "{+" <> printTermF (constructorName (termFOut term)) (termFOut term) n <> "+}"
+    Patch (Replace term1 term2) -> nl n       <> pad (n - 1) <> "{ " <> printTermF (constructorName (termFOut term1)) (termFOut term1) n
+                                <> nl (n + 1) <> pad (n - 1) <> "->" <> printTermF (constructorName (termFOut term2)) (termFOut term2) n <> " }"
     Merge term -> nl n <> pad n <> "(" <> stringUtf8 (constructorName (termFOut term)) <> foldMap ($ (n + 1)) (termFOut term) <> ")"
