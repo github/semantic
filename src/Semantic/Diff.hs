c22b9f6beb6ce284f2210696b9060f875ec53acf
Rob Rix <rob.rix@me.com>
2018-05-14 13:55:16 -0400

    Factor the parser out of run.

--- src/Semantic/Diff.hs
+++ src/Semantic/Diff.hs
@@ -27,18 +27,21 @@ diffBlobPair :: Members '[Distribute WrappedTask, Task, Telemetry, Exc SomeExcep
 diffBlobPair renderer blobs
   | Just (SomeParser parser) <- someParser @'[ConstructorName, Diffable, Eq1, GAlign, HasDeclaration, IdentifierName, Show1, ToJSONFields1, Traversable] <$> effectiveLanguage
   = case renderer of
-    ToCDiffRenderer         -> run (\ blob -> parse parser blob >>= decorate (declarationAlgebra blob))                     >>= render (renderToCDiff blobs)
-    JSONDiffRenderer        -> run (          parse parser      >=> decorate constructorLabel >=> decorate identifierLabel) >>= render (renderJSONDiff blobs)
-    SExpressionDiffRenderer -> run (          parse parser)                                                                                                   >>= serialize (SExpression ByConstructorName)
-    DOTDiffRenderer         -> run (          parse parser)                                                                 >>= render renderTreeGraph        >>= serialize (DOT (diffStyle (pathKeyForBlobPair blobs)))
+    ToCDiffRenderer         -> run parser (decorate . declarationAlgebra)                                  >>= render (renderToCDiff blobs)
+    JSONDiffRenderer        -> run parser (const (decorate constructorLabel >=> decorate identifierLabel)) >>= render (renderJSONDiff blobs)
+    SExpressionDiffRenderer -> run parser (const pure)                                                                                       >>= serialize (SExpression ByConstructorName)
+    DOTDiffRenderer         -> run parser (const pure)                                                     >>= render renderTreeGraph        >>= serialize (DOT (diffStyle (pathKeyForBlobPair blobs)))
   | otherwise = noLanguageForBlob effectivePath
   where effectivePath = pathForBlobPair blobs
         effectiveLanguage = languageForBlobPair blobs
         languageTag = languageTagForBlobPair blobs
 
-        run :: (Diffable syntax, Eq1 syntax, GAlign syntax, Show1 syntax, Traversable syntax) => Members [Distribute WrappedTask, Task, Telemetry, IO] effs => (Blob -> TaskEff (Term syntax (Record fields))) -> Eff effs (Diff syntax (Record fields) (Record fields))
-        run parse = do
-          terms <- distributeFor blobs (WrapTask . parse)
+        run :: (Diffable syntax, Eq1 syntax, GAlign syntax, Show1 syntax, Traversable syntax, Members [Distribute WrappedTask, Task, Telemetry, IO] effs)
+            => Parser (Term syntax (Record fields'))
+            -> (Blob -> Term syntax (Record fields') -> TaskEff (Term syntax (Record fields)))
+            -> Eff effs (Diff syntax (Record fields) (Record fields))
+        run parser decorate = do
+          terms <- distributeFor blobs (\ blob -> WrapTask (decorate blob =<< parse parser blob))
           time "diff" languageTag $ do
             diff <- diff (runJoin terms)
             diff <$ writeStat (Stat.count "diff.nodes" (bilength diff) languageTag)
