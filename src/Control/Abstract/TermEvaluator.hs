7e190024f8041387d88d9b2a3f741ed3b781e274
Rob Rix <rob.rix@me.com>
2018-05-15 12:59:47 -0400

    :memo: TermEvaluator.

--- src/Control/Abstract/TermEvaluator.hs
+++ src/Control/Abstract/TermEvaluator.hs
@@ -16,6 +16,9 @@ import Control.Monad.Effect.State     as X
 import Control.Monad.Effect.Trace     as X
 import Prologue
 
+-- | Evaluators specialized to some specific term type.
+--
+--   This is used to constrain the term type so that inference for analyses can resolve it correctly, but should not be used for any of the term-agonstic machinery like builtins, Evaluatable instances, the mechanics of the heap & environment, etc.
 newtype TermEvaluator term location value effects a = TermEvaluator { runTermEvaluator :: Evaluator location value effects a }
   deriving (Applicative, Effectful, Functor, Monad)
 
