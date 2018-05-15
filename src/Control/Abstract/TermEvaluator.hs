adbc029341a54b7e6460499aae8265b2bcb4bd57
Rob Rix <rob.rix@me.com>
2018-05-15 12:01:13 -0400

    Derive an Alternative instance for TermEvaluator.

--- src/Control/Abstract/TermEvaluator.hs
+++ src/Control/Abstract/TermEvaluator.hs
@@ -13,6 +13,9 @@ import Control.Monad.Effect.Reader    as X
 import Control.Monad.Effect.Resumable as X
 import Control.Monad.Effect.State     as X
 import Control.Monad.Effect.Trace     as X
+import Prologue
 
 newtype TermEvaluator term location value effects a = TermEvaluator { runTermEvaluator :: Evaluator location value effects a }
   deriving (Applicative, Effectful, Functor, Monad)
+
+deriving instance Member NonDet effects => Alternative (TermEvaluator term location value effects)
