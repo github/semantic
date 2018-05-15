eaed43360156ae65595409e6c76014bd19488904
Rob Rix <rob.rix@me.com>
2018-05-15 17:41:24 -0400

    :fire: the Generic instance for Range.

--- src/Data/Range.hs
+++ src/Data/Range.hs
@@ -14,7 +14,7 @@ import Prologue
 
 -- | A half-open interval of integers, defined by start & end indices.
 data Range = Range { start :: {-# UNPACK #-} !Int, end :: {-# UNPACK #-} !Int }
-  deriving (Eq, Show, Generic)
+  deriving (Eq, Show)
 
 emptyRange :: Range
 emptyRange = Range 0 0
