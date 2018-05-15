8c82e1f4a2525e416d12a4839667e2b83ae47ac8
Rob Rix <rob.rix@me.com>
2018-05-15 11:40:28 -0400

    Stub in a TermEvaluator newtype wrapping Evaluator.

--- src/Control/Abstract/TermEvaluator.hs
+++ src/Control/Abstract/TermEvaluator.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Abstract.TermEvaluator where
+
+import Control.Abstract.Evaluator
+
+newtype TermEvaluator term location value effects a = TermEvaluator { runTermEvaluator :: Evaluator location value effects a }
+  deriving (Applicative, Effectful, Functor, Monad)
