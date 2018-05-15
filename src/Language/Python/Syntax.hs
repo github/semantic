1d2b15680d635c230f715dc21ba1e476a50f207a
Timothy Clem <timothy.clem@gmail.com>
2018-05-15 11:21:00 -0700

    Handle python `import . from moduleY` syntax

--- src/Language/Python/Syntax.hs
+++ src/Language/Python/Syntax.hs
@@ -10,6 +10,7 @@ import qualified Data.ByteString.Char8 as BC
 import           Data.Functor.Classes.Generic
 import qualified Data.Language as Language
 import qualified Data.List.NonEmpty as NonEmpty
+import qualified Data.Semigroup.Reducer as Reducer
 import           Data.Mergeable
 import           Diffing.Algorithm
 import           GHC.Generics
@@ -95,9 +96,17 @@ instance Ord1 Import where liftCompare = genericLiftCompare
 instance Show1 Import where liftShowsPrec = genericLiftShowsPrec
 
 -- from a import b
--- from a import b as c
--- from a import *
 instance Evaluatable Import where
+  -- from . import moduleY
+  -- This is a bit of a special case in the syntax as this actually behaves like a qualified relative import.
+  eval (Import (RelativeQualifiedName n Nothing) [(name, _)]) = do
+    path <- NonEmpty.last <$> resolvePythonModules (RelativeQualifiedName n (Just (qualifiedName (unName name :| []))))
+    evalQualifiedImport name path
+
+  -- from a import b
+  -- from a import b as c
+  -- from a import *
+  -- from .moduleY import b
   eval (Import name xs) = do
     modulePaths <- resolvePythonModules name
 
@@ -115,6 +124,24 @@ instance Evaluatable Import where
         | otherwise = Env.overwrite xs importedEnv
 
 
+-- Evaluate a qualified import
+evalQualifiedImport :: ( AbstractValue location a effects
+                       , Addressable location effects
+                       , Reducer.Reducer a (Cell location a)
+                       , Members '[ (State (Exports location a))
+                                  , (State (Environment location a))
+                                  , (State (Heap location (Cell location) a))
+                                  , (Reader (Environment location a))
+                                  , (Modules location a)
+                                  ] effects
+                       )
+                    => Name -> ModulePath -> Evaluator location a effects a
+evalQualifiedImport name path = letrec' name $ \addr -> do
+  importedEnv <- maybe emptyEnv fst <$> isolate (require path)
+  modifyEnv (mergeEnvs importedEnv)
+  void $ makeNamespace name addr Nothing
+  unit
+
 newtype QualifiedImport a = QualifiedImport { qualifiedImportFrom :: QualifiedName }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable, FreeVariables1, Declarations1)
 
@@ -130,12 +157,8 @@ instance Evaluatable QualifiedImport where
     go (NonEmpty.zip (FV.name . BC.pack <$> qualifiedName) modulePaths)
     where
       -- Evaluate and import the last module, updating the environment
-      go ((name, path) :| []) = letrec' name $ \addr -> do
-        importedEnv <- maybe emptyEnv fst <$> isolate (require path)
-        modifyEnv (mergeEnvs importedEnv)
-        void $ makeNamespace name addr Nothing
-        unit
-      -- Evaluate each parent module, creating a just namespace
+      go ((name, path) :| []) = evalQualifiedImport name path
+      -- Evaluate each parent module, just creating a namespace
       go ((name, path) :| xs) = letrec' name $ \addr -> do
         void $ isolate (require path)
         void $ go (NonEmpty.fromList xs)
