e7a7ea202223cdcae83d086e68dd415b90c00bfd
Rob Rix <rob.rix@me.com>
2018-05-15 18:24:41 -0400

    Revert ":fire: the Generic instance for Range."
    This reverts commit d121bdaf3fee616d7dffa08ff2854e4426372141.

--- src/Data/Range.hs
+++ src/Data/Range.hs
@@ -14,7 +14,7 @@ import Prologue
 
 -- | A half-open interval of integers, defined by start & end indices.
 data Range = Range { start :: {-# UNPACK #-} !Int, end :: {-# UNPACK #-} !Int }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic)
 
 emptyRange :: Range
 emptyRange = Range 0 0
