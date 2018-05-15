d5bba69165b654866d2669999b3c0cf331006854
Rob Rix <rob.rix@me.com>
2018-05-15 11:59:42 -0400

    Re-export a bunch of effects stuff.

--- src/Control/Abstract/TermEvaluator.hs
+++ src/Control/Abstract/TermEvaluator.hs
@@ -1,7 +1,18 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Control.Abstract.TermEvaluator where
+module Control.Abstract.TermEvaluator
+( TermEvaluator(..)
+, module X
+) where
 
 import Control.Abstract.Evaluator
+import Control.Monad.Effect           as X
+import Control.Monad.Effect.Fail      as X
+import Control.Monad.Effect.Fresh     as X
+import Control.Monad.Effect.NonDet    as X
+import Control.Monad.Effect.Reader    as X
+import Control.Monad.Effect.Resumable as X
+import Control.Monad.Effect.State     as X
+import Control.Monad.Effect.Trace     as X
 
 newtype TermEvaluator term location value effects a = TermEvaluator { runTermEvaluator :: Evaluator location value effects a }
   deriving (Applicative, Effectful, Functor, Monad)
