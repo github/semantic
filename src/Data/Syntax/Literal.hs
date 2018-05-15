15c20c3f77660f6794c8f2fc456c1ca3a5f7ddca
Patrick Thomson <patrickt@github.com>
2018-05-15 12:45:14 -0400

    Include semantic information in JSON fields.
    Previously, instances of ToJSONFields1 for syntax nodes were provided
    by an instance for Sum that assumed a Foldable syntax node. This is
    not ideal because it omits almost all useful information associated
    with a node, aside from its children: boolean nodes had no information
    about their values, integers had no representation of the numbers they
    stored, &c. This patch adds explicit ToJSONFields1 instances for all
    our syntax nodes, and provides useful implementations where possible.
    
    To save keystrokes, there is a default implementation for
    ToJSONFields1 given a foldable instance. This preserves the old
    behavior and maintains backwards compatibility.
    
    I considered using advanced overlap for this, but it ultimately wasn't
    worth the trouble.
    
    Happily enough, there is no speed hit associated with making these
    instances explicit.

--- src/Data/Syntax/Literal.hs
+++ src/Data/Syntax/Literal.hs
@@ -2,6 +2,7 @@
 module Data.Syntax.Literal where
 
 import Control.Arrow ((>>>))
+import Data.JSON.Fields
 import Data.Abstract.Evaluatable
 import Data.ByteString.Char8 (readInteger, unpack)
 import qualified Data.ByteString.Char8 as B
@@ -29,6 +30,8 @@ instance Show1 Boolean where liftShowsPrec = genericLiftShowsPrec
 instance Evaluatable Boolean where
   eval (Boolean x) = boolean x
 
+instance ToJSONFields1 Boolean where
+  toJSONFields1 (Boolean b) = noChildren [ "value" .= b ]
 
 -- Numeric
 
@@ -44,6 +47,9 @@ instance Evaluatable Data.Syntax.Literal.Integer where
   -- TODO: This instance probably shouldn't have readInteger?
   eval (Data.Syntax.Literal.Integer x) =
     integer =<< maybeM (throwEvalError (IntegerFormatError x)) (fst <$> readInteger x)
+
+instance ToJSONFields1 Data.Syntax.Literal.Integer where
+  toJSONFields1 (Integer i) = noChildren ["as_string" .= unpack i]
 
 
 -- TODO: Should IntegerLiteral hold an Integer instead of a ByteString?
@@ -62,6 +68,9 @@ instance Evaluatable Data.Syntax.Literal.Float where
   eval (Float s) =
     float =<< either (const (throwEvalError (FloatFormatError s))) pure (parseScientific s)
 
+instance ToJSONFields1 Float where
+  toJSONFields1 (Float f) = noChildren ["as_string" .= unpack f]
+
 -- Rational literals e.g. `2/3r`
 newtype Rational a = Rational ByteString
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable, FreeVariables1, Declarations1)
@@ -77,6 +86,8 @@ instance Evaluatable Data.Syntax.Literal.Rational where
       parsed = readMaybe @Prelude.Integer (unpack trimmed)
     in rational =<< maybe (throwEvalError (RationalFormatError r)) (pure . toRational) parsed
 
+instance ToJSONFields1 Data.Syntax.Literal.Rational where
+  toJSONFields1 (Rational r) = noChildren ["as_string" .= unpack r]
 
 -- Complex literals e.g. `3 + 2i`
 newtype Complex a = Complex ByteString
@@ -88,6 +99,9 @@ instance Show1 Data.Syntax.Literal.Complex where liftShowsPrec = genericLiftShow
 
 -- TODO: Implement Eval instance for Complex
 instance Evaluatable Complex
+
+instance ToJSONFields1 Complex where
+  toJSONFields1 (Complex c) = noChildren ["as_string" .= unpack c]
 
 -- Strings, symbols
 
@@ -103,6 +117,7 @@ instance Show1 Data.Syntax.Literal.String where liftShowsPrec = genericLiftShows
 -- TODO: Implement Eval instance for String
 instance Evaluatable Data.Syntax.Literal.String
 
+instance ToJSONFields1 Data.Syntax.Literal.String
 
 -- | An interpolation element within a string literal.
 newtype InterpolationElement a = InterpolationElement { interpolationBody :: a }
@@ -115,6 +130,7 @@ instance Show1 InterpolationElement where liftShowsPrec = genericLiftShowsPrec
 -- TODO: Implement Eval instance for InterpolationElement
 instance Evaluatable InterpolationElement
 
+instance ToJSONFields1 InterpolationElement
 
 -- | A sequence of textual contents within a string literal.
 newtype TextElement a = TextElement { textElementContent :: ByteString }
@@ -123,6 +139,9 @@ newtype TextElement a = TextElement { textElementContent :: ByteString }
 instance Eq1 TextElement where liftEq = genericLiftEq
 instance Ord1 TextElement where liftCompare = genericLiftCompare
 instance Show1 TextElement where liftShowsPrec = genericLiftShowsPrec
+
+instance ToJSONFields1 TextElement where
+  toJSONFields1 (TextElement c) = noChildren ["as_string" .= unpack c]
 
 instance Evaluatable TextElement where
   eval (TextElement x) = string x
@@ -136,12 +155,16 @@ instance Show1 Null where liftShowsPrec = genericLiftShowsPrec
 
 instance Evaluatable Null where eval = const null
 
+instance ToJSONFields1 Null
+
 newtype Symbol a = Symbol { symbolContent :: ByteString }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable, FreeVariables1, Declarations1)
 
 instance Eq1 Symbol where liftEq = genericLiftEq
 instance Ord1 Symbol where liftCompare = genericLiftCompare
 instance Show1 Symbol where liftShowsPrec = genericLiftShowsPrec
+
+instance ToJSONFields1 Symbol
 
 instance Evaluatable Symbol where
   eval (Symbol s) = symbol s
@@ -156,6 +179,10 @@ instance Show1 Regex where liftShowsPrec = genericLiftShowsPrec
 -- TODO: Heredoc-style string literals?
 -- TODO: Character literals.
 
+instance ToJSONFields1 Regex where
+  toJSONFields1 (Regex r) = noChildren ["as_string" .= unpack r]
+
+
 -- TODO: Implement Eval instance for Regex
 instance Evaluatable Regex
 
@@ -169,6 +196,8 @@ instance Eq1 Array where liftEq = genericLiftEq
 instance Ord1 Array where liftCompare = genericLiftCompare
 instance Show1 Array where liftShowsPrec = genericLiftShowsPrec
 
+instance ToJSONFields1 Array
+
 instance Evaluatable Array where
   eval (Array a) = array =<< traverse subtermValue a
 
@@ -178,6 +207,8 @@ newtype Hash a = Hash { hashElements :: [a] }
 instance Eq1 Hash where liftEq = genericLiftEq
 instance Ord1 Hash where liftCompare = genericLiftCompare
 instance Show1 Hash where liftShowsPrec = genericLiftShowsPrec
+
+instance ToJSONFields1 Hash
 
 instance Evaluatable Hash where
   eval = hashElements >>> traverse (subtermValue >=> asPair) >=> hash
@@ -189,9 +220,13 @@ instance Eq1 KeyValue where liftEq = genericLiftEq
 instance Ord1 KeyValue where liftCompare = genericLiftCompare
 instance Show1 KeyValue where liftShowsPrec = genericLiftShowsPrec
 
+instance ToJSONFields1 KeyValue
+
 instance Evaluatable KeyValue where
   eval (fmap subtermValue -> KeyValue{..}) =
     join (kvPair <$> key <*> value)
+
+instance ToJSONFields1 Tuple
 
 newtype Tuple a = Tuple { tupleContents :: [a] }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable, FreeVariables1, Declarations1)
@@ -210,6 +245,8 @@ instance Eq1 Set where liftEq = genericLiftEq
 instance Ord1 Set where liftCompare = genericLiftCompare
 instance Show1 Set where liftShowsPrec = genericLiftShowsPrec
 
+instance ToJSONFields1 Set
+
 -- TODO: Implement Eval instance for Set
 instance Evaluatable Set
 
@@ -224,6 +261,8 @@ instance Eq1 Pointer where liftEq = genericLiftEq
 instance Ord1 Pointer where liftCompare = genericLiftCompare
 instance Show1 Pointer where liftShowsPrec = genericLiftShowsPrec
 
+instance ToJSONFields1 Pointer
+
 -- TODO: Implement Eval instance for Pointer
 instance Evaluatable Pointer
 
@@ -235,6 +274,8 @@ newtype Reference a = Reference a
 instance Eq1 Reference where liftEq = genericLiftEq
 instance Ord1 Reference where liftCompare = genericLiftCompare
 instance Show1 Reference where liftShowsPrec = genericLiftShowsPrec
+
+instance ToJSONFields1 Reference
 
 -- TODO: Implement Eval instance for Reference
 instance Evaluatable Reference
--- src/Language/Markdown/Syntax.hs
+++ src/Language/Markdown/Syntax.hs
@@ -2,10 +2,14 @@
 module Language.Markdown.Syntax where
 
 import Prologue hiding (Text)
+import Data.ByteString.Char8 (unpack)
+import Data.JSON.Fields
 import Diffing.Algorithm
 
 newtype Document a = Document [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+instance ToJSONFields1 Document
 
 instance Eq1 Document where liftEq = genericLiftEq
 instance Ord1 Document where liftCompare = genericLiftCompare
@@ -17,12 +21,16 @@ instance Show1 Document where liftShowsPrec = genericLiftShowsPrec
 newtype Paragraph a = Paragraph [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
+instance ToJSONFields1 Paragraph
+
 instance Eq1 Paragraph where liftEq = genericLiftEq
 instance Ord1 Paragraph where liftCompare = genericLiftCompare
 instance Show1 Paragraph where liftShowsPrec = genericLiftShowsPrec
 
 data Heading a = Heading { headingLevel :: Int, headingContent :: [a], sectionContent :: [a] }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+instance ToJSONFields1 Heading
 
 instance Eq1 Heading where liftEq = genericLiftEq
 instance Ord1 Heading where liftCompare = genericLiftCompare
@@ -31,9 +39,13 @@ instance Show1 Heading where liftShowsPrec = genericLiftShowsPrec
 newtype UnorderedList a = UnorderedList [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
+instance ToJSONFields1 UnorderedList
+
 instance Eq1 UnorderedList where liftEq = genericLiftEq
 instance Ord1 UnorderedList where liftCompare = genericLiftCompare
 instance Show1 UnorderedList where liftShowsPrec = genericLiftShowsPrec
+
+instance ToJSONFields1 OrderedList
 
 newtype OrderedList a = OrderedList [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
@@ -42,6 +54,8 @@ instance Eq1 OrderedList where liftEq = genericLiftEq
 instance Ord1 OrderedList where liftCompare = genericLiftCompare
 instance Show1 OrderedList where liftShowsPrec = genericLiftShowsPrec
 
+instance ToJSONFields1 BlockQuote
+
 newtype BlockQuote a = BlockQuote [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
@@ -49,12 +63,17 @@ instance Eq1 BlockQuote where liftEq = genericLiftEq
 instance Ord1 BlockQuote where liftCompare = genericLiftCompare
 instance Show1 BlockQuote where liftShowsPrec = genericLiftShowsPrec
 
+instance ToJSONFields1 ThematicBreak
+
 data ThematicBreak a = ThematicBreak
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
 instance Eq1 ThematicBreak where liftEq = genericLiftEq
 instance Ord1 ThematicBreak where liftCompare = genericLiftCompare
 instance Show1 ThematicBreak where liftShowsPrec = genericLiftShowsPrec
+
+instance ToJSONFields1 HTMLBlock where
+  toJSONFields1 (HTMLBlock b) = noChildren [ "as_string" .= unpack b ]
 
 newtype HTMLBlock a = HTMLBlock ByteString
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
@@ -66,6 +85,8 @@ instance Show1 HTMLBlock where liftShowsPrec = genericLiftShowsPrec
 newtype Table a = Table [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
+instance ToJSONFields1 Table
+
 instance Eq1 Table where liftEq = genericLiftEq
 instance Ord1 Table where liftCompare = genericLiftCompare
 instance Show1 Table where liftShowsPrec = genericLiftShowsPrec
@@ -73,12 +94,16 @@ instance Show1 Table where liftShowsPrec = genericLiftShowsPrec
 newtype TableRow a = TableRow [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
+instance ToJSONFields1 TableRow
+
 instance Eq1 TableRow where liftEq = genericLiftEq
 instance Ord1 TableRow where liftCompare = genericLiftCompare
 instance Show1 TableRow where liftShowsPrec = genericLiftShowsPrec
 
 newtype TableCell a = TableCell [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+instance ToJSONFields1 TableCell
 
 instance Eq1 TableCell where liftEq = genericLiftEq
 instance Ord1 TableCell where liftCompare = genericLiftCompare
@@ -90,12 +115,16 @@ instance Show1 TableCell where liftShowsPrec = genericLiftShowsPrec
 newtype Strong a = Strong [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
+instance ToJSONFields1 Strong
+
 instance Eq1 Strong where liftEq = genericLiftEq
 instance Ord1 Strong where liftCompare = genericLiftCompare
 instance Show1 Strong where liftShowsPrec = genericLiftShowsPrec
 
 newtype Emphasis a = Emphasis [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+instance ToJSONFields1 Emphasis
 
 instance Eq1 Emphasis where liftEq = genericLiftEq
 instance Ord1 Emphasis where liftCompare = genericLiftCompare
@@ -104,12 +133,18 @@ instance Show1 Emphasis where liftShowsPrec = genericLiftShowsPrec
 newtype Text a = Text ByteString
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
+instance ToJSONFields1 Text where
+  toJSONFields1 (Text s) = noChildren ["as_string" .= unpack s ]
+
 instance Eq1 Text where liftEq = genericLiftEq
 instance Ord1 Text where liftCompare = genericLiftCompare
 instance Show1 Text where liftShowsPrec = genericLiftShowsPrec
 
 data Link a = Link { linkURL :: ByteString, linkTitle :: Maybe ByteString }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+-- TODO: Better ToJSONFields1 instance
+instance ToJSONFields1 Link
 
 instance Eq1 Link where liftEq = genericLiftEq
 instance Ord1 Link where liftCompare = genericLiftCompare
@@ -118,12 +153,18 @@ instance Show1 Link where liftShowsPrec = genericLiftShowsPrec
 data Image a = Image { imageURL :: ByteString, imageTitle :: Maybe ByteString }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
+-- TODO: Better ToJSONFields1 instance
+instance ToJSONFields1 Image
+
 instance Eq1 Image where liftEq = genericLiftEq
 instance Ord1 Image where liftCompare = genericLiftCompare
 instance Show1 Image where liftShowsPrec = genericLiftShowsPrec
 
 data Code a = Code { codeLanguage :: Maybe ByteString, codeContent :: ByteString }
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
+
+-- TODO: Better ToJSONFields1 instance
+instance ToJSONFields1 Code
 
 instance Eq1 Code where liftEq = genericLiftEq
 instance Ord1 Code where liftCompare = genericLiftCompare
@@ -132,9 +173,13 @@ instance Show1 Code where liftShowsPrec = genericLiftShowsPrec
 data LineBreak a = LineBreak
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
 
+instance ToJSONFields1 LineBreak
+
 instance Eq1 LineBreak where liftEq = genericLiftEq
 instance Ord1 LineBreak where liftCompare = genericLiftCompare
 instance Show1 LineBreak where liftShowsPrec = genericLiftShowsPrec
+
+instance ToJSONFields1 Strikethrough
 
 newtype Strikethrough a = Strikethrough [a]
   deriving (Diffable, Eq, Foldable, Functor, GAlign, Generic1, Mergeable, Ord, Show, Traversable)
