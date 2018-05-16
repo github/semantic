453f60ee3617a2857c4c8a24c503a3ba83851a00
Timothy Clem <timothy.clem@gmail.com>
2018-05-16 10:21:24 -0700

    Some example js code for various module require/resolution options

---
+++ test/fixtures/javascript/analysis/main2.js
@@ -0,0 +1,9 @@
+// const bar = require('bar')
+// const tos = require('tos')
+const wap = require('wap')
+
+// console.log(bar.qux())
+// console.log(tos.qux())
+// console.log(wap.qux())
+
+wap.qux()
