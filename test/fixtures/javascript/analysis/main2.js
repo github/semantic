7137c057b47e7e7c6c531ef67aec980d7f3d824f
Timothy Clem <timothy.clem@gmail.com>
2018-05-16 10:43:38 -0700

    Fix up example a bit

--- test/fixtures/javascript/analysis/main2.js
+++ test/fixtures/javascript/analysis/main2.js
@@ -1,9 +1,7 @@
-// const bar = require('bar')
-// const tos = require('tos')
+const bar = require('bar')
+const tos = require('tos')
 const wap = require('wap')
 
-// console.log(bar.qux())
-// console.log(tos.qux())
-// console.log(wap.qux())
-
-wap.qux()
+console.log(bar())
+console.log(tos())
+console.log(wap())
