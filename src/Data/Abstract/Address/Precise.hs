1aabbf33493fa9a3df84204d78cad11df7668f21
Rob Rix <rob.rix@me.com>
2018-05-16 13:16:39 -0400

    Revert "Define a Heap type specialized to Precise locations."
    This reverts commit 2d2eb7a62a927118af3f20b729538cacefaf64b2.

--- src/Data/Abstract/Address/Precise.hs
+++ src/Data/Abstract/Address/Precise.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Data.Abstract.Address.Precise where
 
-import Data.Abstract.Address
-import Data.Abstract.Live
-import qualified Data.IntMap.Monoidal as Monoidal
 import Data.Monoid (Last(..))
 import Data.Semigroup.Reducer
 import Data.Semilattice.Lower
@@ -28,40 +25,3 @@ instance Reducer value (Latest value) where
 
 instance Show value => Show (Latest value) where
   showsPrec d = showsPrec d . getLast . unLatest
-
-
-newtype Heap value = Heap { unHeap :: Monoidal.IntMap (Latest value) }
-  deriving (Eq, Foldable, Functor, Lower, Monoid, Ord, Semigroup, Traversable)
-
--- | Look up the cell of values for an 'Address' in a 'Heap', if any.
-heapLookup :: Address Precise value -> Heap value -> Maybe (Latest value)
-heapLookup (Address (Precise address)) = Monoidal.lookup address . unHeap
-
--- | Look up the list of values stored for a given address, if any.
-heapLookupAll :: Address Precise value -> Heap value -> Maybe [value]
-heapLookupAll address = fmap toList . heapLookup address
-
--- | Append a value onto the cell for a given address, inserting a new cell if none existed.
-heapInsert :: Address Precise value -> value -> Heap value -> Heap value
-heapInsert (Address address) value = flip snoc (address, value)
-
--- | Manually insert a cell into the heap at a given address.
-heapInit :: Address Precise value -> Latest value -> Heap value -> Heap value
-heapInit (Address (Precise address)) cell (Heap h) = Heap (Monoidal.insert address cell h)
-
--- | The number of addresses extant in a 'Heap'.
-heapSize :: Heap value -> Int
-heapSize = Monoidal.size . unHeap
-
--- | Restrict a 'Heap' to only those 'Address'es in the given 'Live' set (in essence garbage collecting the rest).
-heapRestrict :: Heap value -> Live Precise value -> Heap value
-heapRestrict (Heap m) roots = Heap (Monoidal.filterWithKey (\ address _ -> Address (Precise address) `liveMember` roots) m)
-
-
-instance Reducer (Precise, value) (Heap value) where
-  unit = Heap . unit . first unPrecise
-  cons (Precise key, a) (Heap heap) = Heap (cons (key, a) heap)
-  snoc (Heap heap) (Precise key, a) = Heap (snoc heap (key, a))
-
-instance Show value => Show (Heap value) where
-  showsPrec d = showsUnaryWith showsPrec "Heap" d . Monoidal.pairs . unHeap
