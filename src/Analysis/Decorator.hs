dfd422f03bd57ffefcede73f509ed1fa5887daa1
Rob Rix <rob.rix@me.com>
2018-05-16 17:35:40 -0400

    :fire: the unused Identifier newtype.

--- src/Analysis/Decorator.hs
+++ src/Analysis/Decorator.hs
@@ -3,13 +3,9 @@ module Analysis.Decorator
 ( decoratorWithAlgebra
 ) where
 
-import Prologue
-import Data.Aeson
-import Data.ByteString.Char8 (ByteString)
-import Data.JSON.Fields
 import Data.Record
 import Data.Term
-import Data.Text.Encoding (decodeUtf8)
+import Prologue
 
 -- | Lift an algebra into a decorator for terms annotated with records.
 decoratorWithAlgebra :: Functor syntax
@@ -17,10 +13,3 @@ decoratorWithAlgebra :: Functor syntax
                      -> Term syntax (Record fs)                                         -- ^ A term to decorate with values produced by the R-algebra.
                      -> Term syntax (Record (a ': fs))                                  -- ^ A term decorated with values produced by the R-algebra.
 decoratorWithAlgebra alg = para $ \ c@(In a f) -> termIn (alg (fmap (second (rhead . termAnnotation)) c) :. a) (fmap snd f)
-
-
-newtype Identifier = Identifier ByteString
-  deriving (Eq, Show)
-
-instance ToJSONFields Identifier where
-  toJSONFields (Identifier i) = [ "identifier" .= decodeUtf8 i ]
