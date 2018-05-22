2e0a9fb55339c78b26f32e4d690f6fa479d58235
Rob Rix <rob.rix@me.com>
2018-05-22 17:29:37 -0400

    Exports holds locations.

--- src/Data/Abstract/Exports.hs
+++ src/Data/Abstract/Exports.hs
@@ -16,17 +16,17 @@ import qualified Data.Map as Map
 import Data.Semilattice.Lower
 
 -- | A map of export names to an alias & address tuple.
-newtype Exports location value = Exports { unExports :: Map.Map Name (Name, Maybe (Address location value)) }
+newtype Exports location value = Exports { unExports :: Map.Map Name (Name, Maybe location) }
   deriving (Eq, Lower, Monoid, Ord, Semigroup)
 
 null :: Exports location value -> Bool
 null = Map.null . unExports
 
 toEnvironment :: Exports location value -> Environment location value
-toEnvironment exports = unpairs (mapMaybe sequenceA (toList (unExports exports)))
+toEnvironment exports = unpairs (mapMaybe sequenceA (map (second (fmap Address)) (toList (unExports exports))))
 
 insert :: Name -> Name -> Maybe (Address location value) -> Exports location value -> Exports location value
-insert name alias address = Exports . Map.insert name (alias, address) . unExports
+insert name alias address = Exports . Map.insert name (alias, unAddress <$> address) . unExports
 
 -- TODO: Should we filter for duplicates here?
 aliases :: Exports location value -> [(Name, Name)]
