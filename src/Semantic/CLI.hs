5718ebadffe77df2a6faf10e8ac143df45b48035
Rob Rix <rob.rix@me.com>
2018-05-23 09:41:50 -0400

    Add a flag to diff with Show.

--- src/Semantic/CLI.hs
+++ src/Semantic/CLI.hs
@@ -61,6 +61,7 @@ arguments = info (version <*> helper <*> ((,) <$> optionsParser <*> argumentsPar
               <|> flag'                                        (Diff.runDiff JSONDiffRenderer)        (long "json"        <> help "Output JSON diff trees")
               <|> flag'                                        (Diff.runDiff ToCDiffRenderer)         (long "toc"         <> help "Output JSON table of contents diff summary")
               <|> flag'                                        (Diff.runDiff DOTDiffRenderer)         (long "dot"         <> help "Output the diff as a DOT graph")
+              <|> flag'                                        (Diff.runDiff ShowDiffRenderer)        (long "show"        <> help "Output using the Show instance (debug only, format subject to change without notice)")
       filesOrStdin <- Right <$> some (both <$> argument filePathReader (metavar "FILE_A") <*> argument filePathReader (metavar "FILE_B")) <|> pure (Left stdin)
       pure $ Task.readBlobPairs filesOrStdin >>= renderer
 
