433aa96cd7ef9cb37733ae7d207fb6c7fe93c889
Rob Rix <rob.rix@me.com>
2018-05-23 12:48:31 -0400

    Define a Variable effect.
    Co-Authored-By: Patrick Thomson <patrick.william.thomson@gmail.com>

--- src/Control/Abstract/Value.hs
+++ src/Control/Abstract/Value.hs
@@ -58,6 +58,10 @@ data Function m value return where
   Lambda :: [Name] -> Set Name -> m value -> Function m value value
   Call   :: value -> [m value]            -> Function m value value
 
+data Variable value return where
+  Variable :: Name -> Variable value value
+
+
 unit' :: (Effectful m, Member (Unit value) effects) => m effects value
 unit' = send Unit
 
