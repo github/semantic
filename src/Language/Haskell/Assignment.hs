e74bfca4bdb69676e6ea1b10c093b645dc28aad2
Rick Winfrey <rewinfrey@github.com>
2018-05-24 19:12:22 -0700

    Assign basic type synonym declarations

--- src/Language/Haskell/Assignment.hs
+++ src/Language/Haskell/Assignment.hs
@@ -17,6 +17,7 @@ import qualified Data.Syntax as Syntax
 import qualified Data.Syntax.Comment as Comment
 import qualified Data.Syntax.Declaration as Declaration
 import qualified Data.Syntax.Literal as Literal
+import qualified Data.Syntax.Type as Type
 import qualified Data.Term as Term
 import qualified Language.Haskell.Syntax as Syntax
 import Prologue
@@ -32,8 +33,15 @@ type Syntax = '[
   , Syntax.Context
   , Syntax.Empty
   , Syntax.Error
+  , Syntax.FunctionConstructor
   , Syntax.Identifier
+  , Syntax.ListConstructor
   , Syntax.Module
+  , Syntax.TupleConstructor
+  , Syntax.Type
+  , Syntax.TypeSynonym
+  , Syntax.UnitConstructor
+  , Type.TypeParameters
   , []
   ]
 
@@ -62,11 +70,20 @@ expressionChoices = [
                     , comment
                     , constructorIdentifier
                     , float
+                    , functionConstructor
                     , functionDeclaration
                     , integer
+                    , listConstructor
                     , listExpression
+                    , listType
                     , moduleIdentifier
                     , string
+                    , type'
+                    , typeConstructorIdentifier
+                    , typeSynonymDeclaration
+                    , typeVariableIdentifier
+                    , tuplingConstructor
+                    , unitConstructor
                     , variableIdentifier
                     , where'
                     ]
@@ -86,11 +103,20 @@ constructorIdentifier = makeTerm <$> symbol ConstructorIdentifier <*> (Syntax.Id
 moduleIdentifier :: Assignment
 moduleIdentifier = makeTerm <$> symbol ModuleIdentifier <*> (Syntax.Identifier . Name.name <$> source)
 
+typeConstructorIdentifier :: Assignment
+typeConstructorIdentifier = makeTerm <$> symbol TypeConstructorIdentifier <*> (Syntax.Identifier . Name.name <$> source)
+
+typeVariableIdentifier :: Assignment
+typeVariableIdentifier = makeTerm <$> symbol TypeVariableIdentifier <*> (Syntax.Identifier . Name.name <$> source)
+
 where' :: Assignment
 where' = makeTerm <$> (symbol Where <|> symbol Where') <*> children (many expression)
 
 functionBody :: Assignment
 functionBody = makeTerm <$> symbol FunctionBody <*> children (many expression)
+
+functionConstructor :: Assignment
+functionConstructor = makeTerm <$> token FunctionConstructor  <*> (Syntax.FunctionConstructor <$> emptyTerm)
 
 functionDeclaration :: Assignment
 functionDeclaration = makeTerm
@@ -104,9 +130,28 @@ functionDeclaration = makeTerm
 integer :: Assignment
 integer = makeTerm <$> symbol Integer <*> (Literal.Integer <$> source)
 
+listConstructor :: Assignment
+listConstructor = makeTerm <$> token ListConstructor <*> (Syntax.ListConstructor <$> emptyTerm)
+
+unitConstructor :: Assignment
+unitConstructor = makeTerm <$> token UnitConstructor <*> (Syntax.UnitConstructor <$> emptyTerm)
+
 listExpression :: Assignment
 listExpression = makeTerm <$> symbol ListExpression <*> children (Literal.Array <$> many listElement)
   where listElement = symbol Expression *> children expression
+
+listType :: Assignment
+listType = makeTerm <$> symbol ListType <*> children (Literal.Array <$> many type')
+
+tuplingConstructor :: Assignment
+tuplingConstructor = makeTerm <$> symbol TuplingConstructor <*> children (Syntax.TupleConstructor <$> emptyTerm)
+
+type' :: Assignment
+type' = (makeTerm <$> symbol Type <*> children (Syntax.Type <$> typeConstructor <*> typeParameters))
+     <|> (makeTerm <$> symbol TypePattern <*> children (Syntax.Type <$> typeConstructor <*> typeParameters))
+
+typeParameters :: Assignment
+typeParameters = makeTerm <$> location <*> (Type.TypeParameters <$> many expression)
 
 float :: Assignment
 float = makeTerm <$> symbol Float <*> (Literal.Float <$> source)
@@ -116,6 +161,23 @@ character = makeTerm <$> symbol Char <*> (Literal.Character <$> source)
 
 string :: Assignment
 string = makeTerm <$> symbol String <*> (Literal.TextElement <$> source)
+
+typeConstructor :: Assignment
+typeConstructor = typeConstructorIdentifier
+               <|> functionConstructor
+               <|> listConstructor
+               <|> listType
+               <|> tuplingConstructor
+               <|> unitConstructor
+
+typeSynonymDeclaration :: Assignment
+typeSynonymDeclaration = makeTerm
+                      <$> symbol TypeSynonymDeclaration
+                      <*> children (Syntax.TypeSynonym <$> typeLeft <*> typeRight)
+  where
+    typeLeft = makeTerm <$> location <*> (Syntax.Type <$> typeConstructor <*> typeParametersLeft)
+    typeParametersLeft = makeTerm <$> location <*> (Type.TypeParameters <$> manyTill expression (symbol TypeSynonymBody))
+    typeRight = symbol TypeSynonymBody *> children type'
 
 -- | Match a series of terms or comments until a delimiter is matched.
 manyTermsTill :: Assignment.Assignment [] Grammar Term -> Assignment.Assignment [] Grammar b -> Assignment.Assignment [] Grammar [Term]
