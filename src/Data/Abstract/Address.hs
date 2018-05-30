01b30e5ae137ea85bd162248cf5c2c2a45031926
Rob Rix <rob.rix@me.com>
2018-05-30 09:10:36 -0400

    :fire: the Eq1, Ord1, & Show1 instances for Address.

--- src/Data/Abstract/Address.hs
+++ src/Data/Abstract/Address.hs
@@ -14,10 +14,6 @@ import Prologue
 newtype Address location value = Address { unAddress :: location }
   deriving (Eq, Ord)
 
-instance Eq   location => Eq1   (Address location) where liftEq          _ a b = unAddress a    ==     unAddress b
-instance Ord  location => Ord1  (Address location) where liftCompare     _ a b = unAddress a `compare` unAddress b
-instance Show location => Show1 (Address location) where liftShowsPrec _ _     = showsPrec
-
 instance Show location => Show (Address location value) where
   showsPrec d = showsPrec d . unAddress
 
