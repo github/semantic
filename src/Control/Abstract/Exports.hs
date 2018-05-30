eff98cfb03720a19eb5f1c2f4ab85f69ce69dbed
Rob Rix <rob.rix@me.com>
2018-05-30 14:21:13 -0400

    :fire: modifyExports.

--- src/Control/Abstract/Exports.hs
+++ src/Control/Abstract/Exports.hs
@@ -12,10 +12,6 @@ import Data.Abstract.Name
 getExports :: Member (State (Exports address)) effects => Evaluator address value effects (Exports address)
 getExports = get
 
--- | Update the global export state.
-modifyExports :: Member (State (Exports address)) effects => (Exports address -> Exports address) -> Evaluator address value effects ()
-modifyExports = modify'
-
 -- | Add an export to the global export state.
 addExport :: Member (State (Exports address)) effects => Name -> Name -> Maybe address -> Evaluator address value effects ()
-addExport name alias = modifyExports . insert name alias
+addExport name alias = modify' . insert name alias
