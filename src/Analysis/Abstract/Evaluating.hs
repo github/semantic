a243f2d8379ee23c59bb044fcd4df8d1fee67a6b
Rob Rix <rob.rix@me.com>
2018-05-30 12:52:30 -0400

    Evaluating runs the env state.

--- src/Analysis/Abstract/Evaluating.hs
+++ src/Analysis/Abstract/Evaluating.hs
@@ -23,7 +23,7 @@ deriving instance (Show (Cell address value), Show address, Show value) => Show 
 evaluating :: Evaluator address value
                 (  Fail
                 ': Fresh
-                ': Env address
+                ': State (Environment address)
                 ': State (Heap address (Cell address) value)
                 ': State (ModuleTable (Maybe (Environment address, value)))
                 ': State (Exports address)
@@ -34,6 +34,6 @@ evaluating
   . runState lowerBound -- State (Exports address)
   . runState lowerBound -- State (ModuleTable (Maybe (Environment address, value)))
   . runState lowerBound -- State (Heap address (Cell address) value)
-  . runState lowerBound -- Env address
+  . runState lowerBound -- State (Environment address)
   . runFresh 0
   . runFail
