a878f85bdb6cdabc05a2f01e8e9aa6e36e69cc90
Rob Rix <rob.rix@me.com>
2018-05-29 20:52:57 -0400

    Hide an export.

--- src/Control/Abstract.hs
+++ src/Control/Abstract.hs
@@ -5,7 +5,7 @@ module Control.Abstract
 import Control.Abstract.Addressable as X
 import Control.Abstract.Configuration as X
 import Control.Abstract.Context as X
-import Control.Abstract.Environment as X
+import Control.Abstract.Environment as X hiding (Lookup)
 import Control.Abstract.Evaluator as X
 import Control.Abstract.Exports as X
 import Control.Abstract.Heap as X
