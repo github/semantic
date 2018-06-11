5dec496220852770b9761e1722cf66bef335af8c
Timothy Clem <timothy.clem@gmail.com>
2018-06-11 12:46:16 -0700

    Take SomeException, log errors

--- src/Semantic/Haystack.hs
+++ src/Semantic/Haystack.hs
@@ -1,20 +1,24 @@
 module Semantic.Haystack where
 
+import           Control.Exception
 import           Control.Monad.IO.Class
 import           Crypto.Hash
-import           Data.Aeson
+import           Data.Aeson hiding (Error)
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import           Network.HTTP.Client
 import           Network.HTTP.Types.Status (statusCode)
 import           Prologue hiding (hash)
+import           Semantic.Log
 import           Semantic.Queue
 import           System.IO.Error
 
 data ErrorReport
   = ErrorReport
-  { errorReportMessage :: Text
-  , errorReportContext :: [(Text, Text)]
-  }
+  { errorReportException :: SomeException
+  , errorReportContext   :: [(String, String)]
+  } deriving (Show)
 
 data HaystackClient
   = HaystackClient
@@ -26,7 +30,7 @@ data HaystackClient
   | NullHaystackClient -- ^ Doesn't report needles, good for testing or when the 'HAYSTACK_URL' env var isn't set.
 
 -- Queue an error to be reported
-queueErrorReport :: MonadIO io => AsyncQueue ErrorReport HaystackClient -> Text -> [(Text, Text)] -> io ()
+queueErrorReport :: MonadIO io => AsyncQueue ErrorReport HaystackClient -> SomeException -> [(String, String)] -> io ()
 queueErrorReport q@AsyncQueue{..} message = liftIO . queue q . ErrorReport message
 
 -- Create a Haystack HTTP client.
@@ -46,29 +50,34 @@ haystackClient maybeURL managerSettings hostName appName
 --
 -- Returns Left error if reporting failed (or if using the NullHaystackClient)
 -- or Right status code received from sending the report.
-reportError :: MonadIO io => HaystackClient -> ErrorReport -> io (Either String Int)
-reportError NullHaystackClient _               = pure (Left "Error not reported. NullHaystackClient configured.")
-reportError HaystackClient{..} ErrorReport{..} = do
+reportError :: MonadIO io => LogQueue -> HaystackClient -> ErrorReport -> io ()
+reportError logger NullHaystackClient e                 = logError logger e
+reportError logger HaystackClient{..} e@ErrorReport{..} = do
+  logError logger e
+
+  let errMsg = displayException errorReportException
   let payload = object $
         [ "app"     .= haystackClientAppName
         , "host"    .= haystackClientHostName
-        , "message" .= errorReportMessage
-        , "rollup"  .= rollup errorReportMessage
-        ] <> foldr (\(k, v) acc -> k .= v : acc) [] errorReportContext
-  liftIO $ print payload
+        , "message" .= errMsg
+        , "rollup"  .= rollup errMsg
+        ] <> foldr (\(k, v) acc -> Text.pack k .= v : acc) [] errorReportContext
   let request = haystackClientRequest { requestBody = RequestBodyLBS (encode payload) }
 
   response <- liftIO . tryIOError $ httpLbs request haystackClientManager
   case response of
-    Left e -> pure $ Left (displayException e)
+    Left e -> queueLogMessage logger Error ("Failed to report error to haystack: " <> displayException e) []
     Right response -> do
       let status = statusCode (responseStatus response)
-      liftIO $ print status
-      liftIO $ print (responseBody response)
-      pure $ Right status
+      if status /= 201
+        then queueLogMessage logger Error ("Failed to report error to haystack, status = " <> show status <> ".") []
+        else pure ()
   where
-    rollup :: Text -> Text
-    rollup = Text.decodeUtf8 . digestToHexByteString . md5 . Text.encodeUtf8
+    rollup :: String -> Text
+    rollup = Text.decodeUtf8 . digestToHexByteString . md5 . BC.pack
 
     md5 :: ByteString -> Digest MD5
     md5 = hash
+
+logError :: MonadIO io => LogQueue -> ErrorReport -> io ()
+logError logger ErrorReport{..} = queueLogMessage logger Error (displayException errorReportException) errorReportContext
