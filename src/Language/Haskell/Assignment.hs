932f2d2267176b1ca9ca0809b77d5f4ab16e6a5c
Rick Winfrey <rewinfrey@github.com>
2018-06-11 10:22:06 -0700

    Use single type constructor for identifiers

--- src/Language/Haskell/Assignment.hs
+++ src/Language/Haskell/Assignment.hs
@@ -38,7 +38,6 @@ type Syntax = '[
   , Syntax.AllConstructors
   , Syntax.AnnotatedTypeVariable
   , Syntax.Class
-  , Syntax.ConstructorIdentifier
   , Syntax.ConstructorOperator
   , Syntax.ConstructorSymbol
   , Syntax.Context
@@ -46,6 +45,7 @@ type Syntax = '[
   , Syntax.DefaultDeclaration
   , Syntax.Deriving
   , Syntax.Empty
+  , Syntax.EntityIdentifier
   , Syntax.Error
   , Syntax.EqualityConstraint
   , Syntax.Export
@@ -63,10 +63,8 @@ type Syntax = '[
   , Syntax.ListConstructor
   , Syntax.Module
   , Syntax.ModuleExport
-  , Syntax.ModuleIdentifier
   , Syntax.NewType
   , Syntax.Pragma
-  , Syntax.PrimitiveConstructorIdentifier
   , Syntax.QualifiedModuleIdentifier
   , Syntax.QualifiedTypeConstructorIdentifier
   , Syntax.QuotedName
@@ -77,16 +75,12 @@ type Syntax = '[
   , Syntax.StrictTypeVariable
   , Syntax.TupleConstructor
   , Syntax.Type
-  , Syntax.TypeClassIdentifier
   , Syntax.TypeConstructorExport
-  , Syntax.TypeConstructorIdentifier
   , Syntax.TypeOperator
   , Syntax.TypePattern
   , Syntax.TypeSignature
   , Syntax.TypeSynonym
-  , Syntax.TypeVariableIdentifier
   , Syntax.UnitConstructor
-  , Syntax.VariableIdentifier
   , Syntax.VariableOperator
   , Syntax.VariableSymbol
   , Type.TypeParameters
