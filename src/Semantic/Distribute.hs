baa37c02f1904382cd5a077f22b5a0225a3ea853
Rob Rix <rob.rix@me.com>
2018-06-13 15:57:56 -0400

    Revert "Distribute is a (properly) higher-order effect."
    This reverts commit 5b9bd9fe72cc8310a0fb8ba9cdd9a4abd4f350f8.

--- src/Semantic/Distribute.hs
+++ src/Semantic/Distribute.hs
@@ -9,6 +9,7 @@ module Semantic.Distribute
 
 import qualified Control.Concurrent.Async as Async
 import           Control.Monad.Effect
+import           Control.Monad.Effect.Exception
 import           Control.Monad.IO.Class
 import           Control.Parallel.Strategies
 import           Prologue hiding (MonadError (..))
@@ -16,28 +17,28 @@ import           Prologue hiding (MonadError (..))
 -- | Distribute a 'Traversable' container of tasks over the available cores (i.e. execute them concurrently), collecting their results.
 --
 --   This is a concurrent analogue of 'sequenceA'.
-distribute :: (Member Distribute effs, Traversable t) => t (Eff effs output) -> Eff effs (t output)
+distribute :: (Member (Distribute task) effs, Traversable t) => t (task output) -> Eff effs (t output)
 distribute = send . Distribute
 
 -- | Distribute the application of a function to each element of a 'Traversable' container of inputs over the available cores (i.e. perform the function concurrently for each element), collecting the results.
 --
 --   This is a concurrent analogue of 'for' or 'traverse' (with the arguments flipped).
-distributeFor :: (Member Distribute effs, Traversable t) => t a -> (a -> Eff effs output) -> Eff effs (t output)
+distributeFor :: (Member (Distribute task) effs, Traversable t) => t a -> (a -> task output) -> Eff effs (t output)
 distributeFor inputs toTask = distribute (fmap toTask inputs)
 
 -- | Distribute the application of a function to each element of a 'Traversable' container of inputs over the available cores (i.e. perform the function concurrently for each element), combining the results 'Monoid'ally into a final value.
 --
 --   This is a concurrent analogue of 'foldMap'.
-distributeFoldMap :: (Member Distribute effs, Monoid output, Traversable t) => (a -> Eff effs output) -> t a -> Eff effs output
+distributeFoldMap :: (Member (Distribute task) effs, Monoid output, Traversable t) => (a -> task output) -> t a -> Eff effs output
 distributeFoldMap toTask inputs = fmap fold (distribute (fmap toTask inputs))
 
 
 -- | Distribute effects run tasks concurrently.
-data Distribute m output where
-  Distribute :: Traversable t => t (m output) -> Distribute m (t output)
+data Distribute task (m :: * -> *) output where
+  Distribute :: Traversable t => t (task output) -> Distribute task m (t output)
 
 
 -- | Evaluate a 'Distribute' effect concurrently.
-runDistribute :: Eff '[Distribute, Lift IO] a -> Eff '[Lift IO] a
-runDistribute = interpret $ \ (Distribute tasks) ->
-  liftIO (Async.mapConcurrently (runM . runDistribute) tasks) >>= pure . withStrategy (parTraversable rseq)
+runDistribute :: (Member (Exc SomeException) effs, Member (Lift IO) effs, Effects effs) => (forall output . task output -> IO (Either SomeException output)) -> Eff (Distribute task ': effs) a -> Eff effs a
+runDistribute action = interpret (\ (Distribute tasks) ->
+  liftIO (Async.mapConcurrently action tasks) >>= either throwError pure . sequenceA . withStrategy (parTraversable (parTraversable rseq)))
