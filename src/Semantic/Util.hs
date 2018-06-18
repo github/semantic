d39171e9d79dd88fb602f1c7ea873cb36f9ff550
Rob Rix <rob.rix@me.com>
2018-06-18 11:09:01 -0400

    Determine Preludes from the language.

--- src/Semantic/Util.hs
+++ src/Semantic/Util.hs
@@ -17,10 +17,8 @@ import           Data.Functor.Foldable
 import qualified Data.Language as Language
 import           Data.Sum (weaken)
 import           Data.Term
-import qualified GHC.TypeLits as TypeLevel
 import           Language.Haskell.HsColour
 import           Language.Haskell.HsColour.Colourise
-import           Language.Preluded
 import           Parsing.Parser
 import           Prologue hiding (weaken)
 import           Semantic.Graph
@@ -28,10 +26,6 @@ import           Semantic.IO as IO
 import           Semantic.Task
 import           Text.Show (showListWith)
 import           Text.Show.Pretty (ppShow)
-
-import qualified Language.Python.Assignment as Python
-import qualified Language.Ruby.Assignment as Ruby
-import qualified Language.TypeScript.Assignment as TypeScript
 
 justEvaluating
   = runM
@@ -87,22 +81,18 @@ checking
   . runAddressError
   . runTypeError
 
-evalGoProject path = justEvaluating =<< evaluateProject goParser Language.Go Nothing path
-evalRubyProject path = justEvaluating =<< evaluateProject rubyParser Language.Ruby rubyPrelude path
-evalPHPProject path = justEvaluating =<< evaluateProject phpParser Language.PHP Nothing path
-evalPythonProject path = justEvaluating =<< evaluateProject pythonParser Language.Python pythonPrelude path
-evalJavaScriptProject path = justEvaluating =<< evaluateProject typescriptParser Language.JavaScript javaScriptPrelude path
-evalTypeScriptProject path = justEvaluating =<< evaluateProject typescriptParser Language.TypeScript Nothing path
+evalGoProject path         = justEvaluating =<< evaluateProject (Proxy :: Proxy 'Language.Go)         goParser         Language.Go         path
+evalRubyProject path       = justEvaluating =<< evaluateProject (Proxy :: Proxy 'Language.Ruby)       rubyParser       Language.Ruby       path
+evalPHPProject path        = justEvaluating =<< evaluateProject (Proxy :: Proxy 'Language.PHP)        phpParser        Language.PHP        path
+evalPythonProject path     = justEvaluating =<< evaluateProject (Proxy :: Proxy 'Language.Python)     pythonParser     Language.Python     path
+evalJavaScriptProject path = justEvaluating =<< evaluateProject (Proxy :: Proxy 'Language.TypeScript) typescriptParser Language.JavaScript path
+evalTypeScriptProject path = justEvaluating =<< evaluateProject (Proxy :: Proxy 'Language.TypeScript) typescriptParser Language.TypeScript path
 
-typecheckGoFile path = checking =<< evaluateProjectWithCaching goParser Language.Go Nothing path
-
-rubyPrelude = Just $ File (TypeLevel.symbolVal (Proxy :: Proxy (PreludePath Ruby.Term))) Language.Ruby
-pythonPrelude = Just $ File (TypeLevel.symbolVal (Proxy :: Proxy (PreludePath Python.Term))) Language.Python
-javaScriptPrelude = Just $ File (TypeLevel.symbolVal (Proxy :: Proxy (PreludePath TypeScript.Term))) Language.JavaScript
+typecheckGoFile path = checking =<< evaluateProjectWithCaching (Proxy :: Proxy 'Language.Go) goParser Language.Go path
 
 -- Evaluate a project, starting at a single entrypoint.
-evaluateProject parser lang prelude path = evaluatePackageWith id withTermSpans . fmap quieterm <$> runTask (readProject Nothing path lang [] >>= parsePackage parser prelude)
-evaluateProjectWithCaching parser lang prelude path = evaluatePackageWith convergingModules (withTermSpans . cachingTerms) . fmap quieterm <$> runTask (readProject Nothing path lang [] >>= parsePackage parser prelude)
+evaluateProject proxy parser lang path = evaluatePackageWith proxy id withTermSpans . fmap quieterm <$> runTask (readProject Nothing path lang [] >>= parsePackage parser)
+evaluateProjectWithCaching proxy parser lang path = evaluatePackageWith proxy convergingModules (withTermSpans . cachingTerms) . fmap quieterm <$> runTask (readProject Nothing path lang [] >>= parsePackage parser)
 
 
 parseFile :: Parser term -> FilePath -> IO term
