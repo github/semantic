5a9ea5e61babab3a40807d8464bea28eb7da969b
Rob Rix <rob.rix@me.com>
2018-06-19 12:47:11 -0400

    Add a convenience to compute the keys in a Monoidal Map.

--- src/Data/Map/Monoidal.hs
+++ src/Data/Map/Monoidal.hs
@@ -8,6 +8,7 @@ module Data.Map.Monoidal
 , insert
 , filterWithKey
 , pairs
+, keys
 , module Reducer
 ) where
 
@@ -40,6 +41,9 @@ filterWithKey f = Map . Map.filterWithKey f . unMap
 
 pairs :: Map key value -> [(key, value)]
 pairs = Map.toList . unMap
+
+keys :: Map key value -> [key]
+keys = Map.keys . unMap
 
 
 instance (Ord key, Semigroup value) => Semigroup (Map key value) where
