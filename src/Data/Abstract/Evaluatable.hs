fdc243258d6d39f56e0778faf798b8a021c36830
Rob Rix <rob.rix@me.com>
2018-06-21 10:13:12 -0400

    Align the things.

--- src/Data/Abstract/Evaluatable.hs
+++ src/Data/Abstract/Evaluatable.hs
@@ -93,7 +93,7 @@ evaluatePackageWith :: forall proxy lang address term value inner inner' inner''
                        , inner'' ~ (Modules address value ': Reader Span ': Reader PackageInfo ': outer)
                        )
                     => proxy lang
-                    -> (SubtermAlgebra Module      term (TermEvaluator term address value inner address)                  -> SubtermAlgebra Module      term (TermEvaluator term address value inner address))
+                    -> (SubtermAlgebra Module      term (TermEvaluator term address value inner address)            -> SubtermAlgebra Module      term (TermEvaluator term address value inner address))
                     -> (SubtermAlgebra (Base term) term (TermEvaluator term address value inner (ValueRef address)) -> SubtermAlgebra (Base term) term (TermEvaluator term address value inner (ValueRef address)))
                     -> Package term
                     -> TermEvaluator term address value outer [(Environment address, address)]
