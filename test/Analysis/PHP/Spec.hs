f885d432d7d36a6d701fe299c77d3f5be377f5fe
Rob Rix <rob.rix@me.com>
2018-06-22 15:45:42 -0400

    Update the tests to pass a list of paths.

--- test/Analysis/PHP/Spec.hs
+++ test/Analysis/PHP/Spec.hs
@@ -12,17 +12,17 @@ spec :: Spec
 spec = parallel $ do
   describe "PHP" $ do
     it "evaluates include and require" $ do
-      ((Right [(res, env)], heap), _) <- evaluate "main.php"
+      ((Right [(res, env)], heap), _) <- evaluate ["main.php"]
       res `shouldBe` unit
       Env.names env `shouldBe` [ "bar", "foo" ]
 
     it "evaluates include_once and require_once" $ do
-      ((Right [(res, env)], heap), _) <- evaluate "main_once.php"
+      ((Right [(res, env)], heap), _) <- evaluate ["main_once.php"]
       res `shouldBe` unit
       Env.names env `shouldBe` [ "bar", "foo" ]
 
     it "evaluates namespaces" $ do
-      ((Right [(_, env)], heap), _) <- evaluate "namespaces.php"
+      ((Right [(_, env)], heap), _) <- evaluate ["namespaces.php"]
       Env.names env `shouldBe` [ "Foo", "NS1" ]
 
       (derefQName heap ("NS1" :| [])               env >>= deNamespace) `shouldBe` Just ("NS1",  ["Sub1", "b", "c"])
@@ -31,5 +31,5 @@ spec = parallel $ do
 
   where
     fixtures = "test/fixtures/php/analysis/"
-    evaluate entry = evalPHPProject (fixtures <> entry)
-    evalPHPProject path = testEvaluating =<< evaluateProject (Proxy :: Proxy 'Language.PHP) phpParser Language.PHP path
+    evaluate = evalPHPProject . map (fixtures <>)
+    evalPHPProject = testEvaluating <=< evaluateProject (Proxy :: Proxy 'Language.PHP) phpParser Language.PHP
--- test/Analysis/Ruby/Spec.hs
+++ test/Analysis/Ruby/Spec.hs
@@ -18,61 +18,61 @@ spec :: Spec
 spec = parallel $ do
   describe "Ruby" $ do
     it "evaluates require_relative" $ do
-      ((Right [(res, env)], heap), _) <- evaluate "main.rb"
+      ((Right [(res, env)], heap), _) <- evaluate ["main.rb"]
       res `shouldBe` Value.Integer (Number.Integer 1)
       Env.names env `shouldContain` ["foo"]
 
     it "evaluates load" $ do
-      ((Right [(_, env)], _), _) <- evaluate "load.rb"
+      ((Right [(_, env)], _), _) <- evaluate ["load.rb"]
       Env.names env `shouldContain` ["foo"]
 
     it "evaluates load with wrapper" $ do
-      ((res, heap), _) <- evaluate "load-wrap.rb"
+      ((res, heap), _) <- evaluate ["load-wrap.rb"]
       res `shouldBe` Left (SomeExc (inject @(EnvironmentError Precise) (FreeVariable "foo")))
 
     it "evaluates subclass" $ do
-      ((Right [(res, env)], heap), _) <- evaluate "subclass.rb"
+      ((Right [(res, env)], heap), _) <- evaluate ["subclass.rb"]
       res `shouldBe` String "\"<bar>\""
       Env.names env `shouldContain` [ "Bar", "Foo" ]
 
       (derefQName heap ("Bar" :| []) env >>= deNamespace) `shouldBe` Just ("Bar",  ["baz", "foo", "inspect"])
 
     it "evaluates modules" $ do
-      ((Right [(res, env)], heap), _) <- evaluate "modules.rb"
+      ((Right [(res, env)], heap), _) <- evaluate ["modules.rb"]
       res `shouldBe` String "\"<hello>\""
       Env.names env `shouldContain` [ "Bar" ]
 
     it "handles break correctly" $ do
-      ((res, _), _) <- evaluate "break.rb"
+      ((res, _), _) <- evaluate ["break.rb"]
       fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 3)]
 
     it "handles break correctly" $ do
-      ((res, _), _) <- evaluate "next.rb"
+      ((res, _), _) <- evaluate ["next.rb"]
       fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 8)]
 
     it "calls functions with arguments" $ do
-      ((res, _), _) <- evaluate "call.rb"
+      ((res, _), _) <- evaluate ["call.rb"]
       fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 579)]
 
     it "evaluates early return statements" $ do
-      ((res, _), _) <- evaluate "early-return.rb"
+      ((res, _), _) <- evaluate ["early-return.rb"]
       fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 123)]
 
     it "has prelude" $ do
-      ((res, _), _) <- evaluate "preluded.rb"
+      ((res, _), _) <- evaluate ["preluded.rb"]
       fmap fst <$> res `shouldBe` Right [String "\"<foo>\""]
 
     it "evaluates __LINE__" $ do
-      ((res, _), _) <- evaluate "line.rb"
+      ((res, _), _) <- evaluate ["line.rb"]
       fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 4)]
 
     it "resolves builtins used in the prelude" $ do
-      ((res, _), traces) <- evaluate "puts.rb"
+      ((res, _), traces) <- evaluate ["puts.rb"]
       fmap fst <$> res `shouldBe` Right [Unit]
       traces `shouldContain` [ "\"hello\"" ]
 
   where
     ns n = Just . Latest . Last . Just . Namespace n
     fixtures = "test/fixtures/ruby/analysis/"
-    evaluate entry = evalRubyProject (fixtures <> entry)
-    evalRubyProject path = testEvaluating =<< evaluateProject (Proxy :: Proxy 'Language.Ruby) rubyParser Language.Ruby path
+    evaluate = evalRubyProject . map (fixtures <>)
+    evalRubyProject = testEvaluating <=< evaluateProject (Proxy :: Proxy 'Language.Ruby) rubyParser Language.Ruby
