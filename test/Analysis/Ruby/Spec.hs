dad58e7c20f38e0d4bba0457e2492869c2895fdc
Rob Rix <rob.rix@me.com>
2018-06-25 17:28:35 -0400

    Fix the Ruby specs.

--- test/Analysis/Ruby/Spec.hs
+++ test/Analysis/Ruby/Spec.hs
@@ -3,6 +3,7 @@ module Analysis.Ruby.Spec (spec) where
 import Data.Abstract.Environment as Env
 import Data.Abstract.Evaluatable
 import Data.Abstract.Value as Value
+import qualified Data.Abstract.ModuleTable as ModuleTable
 import Data.Abstract.Number as Number
 import Data.AST
 import Control.Monad.Effect (SomeExc(..))
@@ -17,61 +18,87 @@ import SpecHelpers
 spec :: Spec
 spec = parallel $ do
   describe "Ruby" $ do
-    pure ()
-    -- it "evaluates require_relative" $ do
-    --   ((res@(~(Right [(_, env)])), _), _) <- evaluate ["main.rb", "foo.rb"]
-    --   map fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 1)]
-    --   Env.names env `shouldContain` ["foo"]
-    --
-    -- it "evaluates load" $ do
-    --   ((res@(~(Right [(_, env)])), _), _) <- evaluate ["load.rb", "foo.rb"]
-    --   map fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 1)]
-    --   Env.names env `shouldContain` ["foo"]
-    --
-    -- it "evaluates load with wrapper" $ do
-    --   ((res, _), _) <- evaluate ["load-wrap.rb", "foo.rb"]
-    --   res `shouldBe` Left (SomeExc (inject @(EnvironmentError Precise) (FreeVariable "foo")))
-    --
-    -- it "evaluates subclass" $ do
-    --   ((res@(~(Right [(_, env)])), heap), _) <- evaluate ["subclass.rb"]
-    --   map fst <$> res `shouldBe` Right [String "\"<bar>\""]
-    --   Env.names env `shouldContain` [ "Bar", "Foo" ]
-    --
-    --   (derefQName heap ("Bar" :| []) env >>= deNamespace) `shouldBe` Just ("Bar",  ["baz", "foo", "inspect"])
-    --
-    -- it "evaluates modules" $ do
-    --   ((res@(~(Right [(_, env)])), _), _) <- evaluate ["modules.rb"]
-    --   map fst <$> res `shouldBe` Right [String "\"<hello>\""]
-    --   Env.names env `shouldContain` [ "Bar" ]
-    --
-    -- it "handles break correctly" $ do
-    --   ((res, _), _) <- evaluate ["break.rb"]
-    --   fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 3)]
-    --
-    -- it "handles break correctly" $ do
-    --   ((res, _), _) <- evaluate ["next.rb"]
-    --   fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 8)]
-    --
-    -- it "calls functions with arguments" $ do
-    --   ((res, _), _) <- evaluate ["call.rb"]
-    --   fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 579)]
-    --
-    -- it "evaluates early return statements" $ do
-    --   ((res, _), _) <- evaluate ["early-return.rb"]
-    --   fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 123)]
-    --
-    -- it "has prelude" $ do
-    --   ((res, _), _) <- evaluate ["preluded.rb"]
-    --   fmap fst <$> res `shouldBe` Right [String "\"<foo>\""]
-    --
-    -- it "evaluates __LINE__" $ do
-    --   ((res, _), _) <- evaluate ["line.rb"]
-    --   fmap fst <$> res `shouldBe` Right [Value.Integer (Number.Integer 4)]
-    --
-    -- it "resolves builtins used in the prelude" $ do
-    --   ((res, _), traces) <- evaluate ["puts.rb"]
-    --   fmap fst <$> res `shouldBe` Right [Unit]
-    --   traces `shouldContain` [ "\"hello\"" ]
+    it "evaluates require_relative" $ do
+      ((res, heap), _) <- evaluate ["main.rb", "foo.rb"]
+      case ModuleTable.lookup "main.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> do
+          heapLookupAll addr heap `shouldBe` Just [Value.Integer (Number.Integer 1)]
+          Env.names env `shouldContain` [ "foo" ]
+        other -> expectationFailure (show other)
+
+    it "evaluates load" $ do
+      ((res, heap), _) <- evaluate ["load.rb", "foo.rb"]
+      case ModuleTable.lookup "load.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> do
+          heapLookupAll addr heap `shouldBe` Just [Value.Integer (Number.Integer 1)]
+          Env.names env `shouldContain` [ "foo" ]
+        other -> expectationFailure (show other)
+
+    it "evaluates load with wrapper" $ do
+      ((res, _), _) <- evaluate ["load-wrap.rb", "foo.rb"]
+      res `shouldBe` Left (SomeExc (inject @(EnvironmentError Precise) (FreeVariable "foo")))
+
+    it "evaluates subclass" $ do
+      ((res, heap), _) <- evaluate ["subclass.rb"]
+      case ModuleTable.lookup "subclass.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> do
+          heapLookupAll addr heap `shouldBe` Just [String "\"<bar>\""]
+          Env.names env `shouldContain` [ "Bar", "Foo" ]
+
+          (derefQName heap ("Bar" :| []) env >>= deNamespace) `shouldBe` Just ("Bar",  ["baz", "foo", "inspect"])
+        other -> expectationFailure (show other)
+
+    it "evaluates modules" $ do
+      ((res, heap), _) <- evaluate ["modules.rb"]
+      case ModuleTable.lookup "modules.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> do
+          heapLookupAll addr heap `shouldBe` Just [String "\"<hello>\""]
+          Env.names env `shouldContain` [ "Bar" ]
+        other -> expectationFailure (show other)
+
+    it "handles break correctly" $ do
+      ((res, heap), _) <- evaluate ["break.rb"]
+      case ModuleTable.lookup "break.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> heapLookupAll addr heap `shouldBe` Just [Value.Integer (Number.Integer 3)]
+        other -> expectationFailure (show other)
+
+    it "handles next correctly" $ do
+      ((res, heap), _) <- evaluate ["next.rb"]
+      case ModuleTable.lookup "next.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> heapLookupAll addr heap `shouldBe` Just [Value.Integer (Number.Integer 8)]
+        other -> expectationFailure (show other)
+
+    it "calls functions with arguments" $ do
+      ((res, heap), _) <- evaluate ["call.rb"]
+      case ModuleTable.lookup "call.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> heapLookupAll addr heap `shouldBe` Just [Value.Integer (Number.Integer 579)]
+        other -> expectationFailure (show other)
+
+    it "evaluates early return statements" $ do
+      ((res, heap), _) <- evaluate ["early-return.rb"]
+      case ModuleTable.lookup "early-return.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> heapLookupAll addr heap `shouldBe` Just [Value.Integer (Number.Integer 123)]
+        other -> expectationFailure (show other)
+
+    it "has prelude" $ do
+      ((res, heap), _) <- evaluate ["preluded.rb"]
+      case ModuleTable.lookup "preluded.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> heapLookupAll addr heap `shouldBe` Just [String "\"<foo>\""]
+        other -> expectationFailure (show other)
+
+    it "evaluates __LINE__" $ do
+      ((res, heap), _) <- evaluate ["line.rb"]
+      case ModuleTable.lookup "line.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> heapLookupAll addr heap `shouldBe` Just [Value.Integer (Number.Integer 4)]
+        other -> expectationFailure (show other)
+
+    it "resolves builtins used in the prelude" $ do
+      ((res, heap), traces) <- evaluate ["puts.rb"]
+      case ModuleTable.lookup "puts.rb" <$> res of
+        Right (Just (Module _ (addr, env) :| [])) -> do
+          heapLookupAll addr heap `shouldBe` Just [Unit]
+          traces `shouldContain` [ "\"hello\"" ]
+        other -> expectationFailure (show other)
 
   where
     ns n = Just . Latest . Last . Just . Namespace n
