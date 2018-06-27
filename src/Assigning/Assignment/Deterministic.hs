98d463d394b97369a2a152783d7df1ecdbb4ff47
Rob Rix <rob.rix@me.com>
2018-06-27 19:01:18 -0400

    Carry an Offset along with the input.

--- src/Assigning/Assignment/Deterministic.hs
+++ src/Assigning/Assignment/Deterministic.hs
@@ -11,7 +11,11 @@ class (Alternative (f s), Ord s, Show s) => Assigning f s where
 combine :: Ord s => Bool -> Set s -> Set s -> Set s
 combine e s1 s2 = if e then s1 <> s2 else lowerBound
 
-type State s = [s]
+data State s = State
+  { stateOffset :: {-# UNPACK #-} !Offset
+  , stateInput  :: ![s]
+  }
+  deriving (Eq, Ord, Show)
 
 data Offset = Offset
   { offsetBytes :: {-# UNPACK #-} !Int
@@ -41,24 +45,24 @@ instance Ord s => Applicative (DetPar s) where
             res `seq` pure (inp2, res)
 
 instance Ord s => Alternative (DetPar s) where
-  empty = DetPar False lowerBound (\ s _ -> Left (Error lowerBound [] (listToMaybe s)))
+  empty = DetPar False lowerBound (\ s _ -> Left (Error lowerBound [] (listToMaybe (stateInput s))))
   DetPar n1 f1 p1 <|> DetPar n2 f2 p2 = DetPar (n1 || n2) (f1 <> f2) (p1 `palt` p2)
     where p1 `palt` p2 = p
-            where p [] follow =
-                    if      n1 then p1 [] follow
-                    else if n2 then p2 [] follow
+            where p state@(State _ []) follow =
+                    if      n1 then p1 state follow
+                    else if n2 then p2 state follow
                     else Left (Error lowerBound (toList (f1 <> f2)) Nothing)
-                  p inp@(s:_) follow =
-                    if      s `Set.member` f1 then p1 inp follow
-                    else if s `Set.member` f2 then p2 inp follow
-                    else if n1 && s `Set.member` follow then p1 inp follow
-                    else if n2 && s `Set.member` follow then p2 inp follow
+                  p state@(State _ inp@(s:_)) follow =
+                    if      s `Set.member` f1 then p1 state { stateInput = inp } follow
+                    else if s `Set.member` f2 then p2 state { stateInput = inp } follow
+                    else if n1 && s `Set.member` follow then p1 state { stateInput = inp } follow
+                    else if n2 && s `Set.member` follow then p2 state { stateInput = inp } follow
                     else Left (Error lowerBound (toList (combine n1 f1 follow <> combine n2 f2 follow)) (Just s))
 
 instance (Ord s, Show s) => Assigning DetPar s where
-  sym s = DetPar False (Set.singleton s) (\ ss _ -> case ss of
+  sym s = DetPar False (Set.singleton s) (\ ss _ -> case stateInput ss of
     []    -> Left (Error lowerBound [s] Nothing)
-    _:inp -> Right (inp, s))
+    _:inp -> Right (ss { stateInput = inp }, s))
 
 invokeDet :: DetPar s a -> State s -> Either (Error s) a
 invokeDet (DetPar _ _ p) inp = snd <$> p inp lowerBound
