548c878d12efd337e1ed20fa89b1326b7b90211e
Ayman Nadeem <aymannadeem@github.com>
2018-06-27 17:18:36 -0400

    methodReference test

---
+++ test/fixtures/java/corpus/methodReference.java
@@ -0,0 +1,11 @@
+class TermsSetQueryBuilder {
+    void Terms() {
+        min = read(Script::new);
+    }
+}
+
+class TermsSetQueryBuilder {
+    void Terms() {
+        min = read(Script::yodawg);
+    }
+}
