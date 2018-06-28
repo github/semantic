37e1ebcb3f94c2da26b406d052c6a145b89f2e7e
Charlie Somerville <charlie@charlie.bz>
2018-06-28 13:59:03 +1000

    change Env.{pairs, unpairs} to take Bindings instead, add flatPairs

--- src/Data/Abstract/Exports.hs
+++ src/Data/Abstract/Exports.hs
@@ -7,7 +7,7 @@ module Data.Abstract.Exports
     , toEnvironment
     ) where
 
-import Data.Abstract.Environment (Environment, unpairs)
+import Data.Abstract.Environment (Environment, unpairs, newEnv)
 import Data.Abstract.Name
 import qualified Data.Map as Map
 import Prelude hiding (null)
@@ -21,7 +21,7 @@ null :: Exports address -> Bool
 null = Map.null . unExports
 
 toEnvironment :: Exports address -> Environment address
-toEnvironment exports = unpairs (mapMaybe sequenceA (toList (unExports exports)))
+toEnvironment exports = newEnv (unpairs (mapMaybe sequenceA (toList (unExports exports))))
 
 insert :: Name -> Name -> Maybe address -> Exports address -> Exports address
 insert name alias address = Exports . Map.insert name (alias, address) . unExports
