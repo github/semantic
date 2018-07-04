da3d73b02e970006f0f3a8219d4f945c673c8f1c
Rob Rix <rob.rix@me.com>
2018-07-04 09:23:25 -0400

    Define a ValueRoots instance for Abstract.

--- src/Data/Abstract/Value/Abstract.hs
+++ src/Data/Abstract/Value/Abstract.hs
@@ -1,4 +1,10 @@
 module Data.Abstract.Value.Abstract where
 
+import Control.Abstract
+
 data Abstract = Abstract
   deriving (Eq, Ord, Show)
+
+
+instance Ord address => ValueRoots address Abstract where
+  valueRoots = mempty
