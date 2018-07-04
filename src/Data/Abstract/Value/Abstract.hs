2362310765be286a43ad8199babb75b1275d4586
Rob Rix <rob.rix@me.com>
2018-07-04 09:30:48 -0400

    Define an AbstractFunction instance for Abstract.

--- src/Data/Abstract/Value/Abstract.hs
+++ src/Data/Abstract/Value/Abstract.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE GADTs, UndecidableInstances #-}
 module Data.Abstract.Value.Abstract where
 
 import Control.Abstract
+import Data.Abstract.Environment as Env
+import Prologue
 
 data Abstract = Abstract
   deriving (Eq, Ord, Show)
@@ -23,3 +26,20 @@ instance AbstractIntro Abstract where
   hash _     = Abstract
   kvPair _ _ = Abstract
   null       = Abstract
+
+instance ( Member (Allocator address Abstract) effects
+         , Member (Env address) effects
+         , Member (Return address) effects
+         )
+      => AbstractFunction address Abstract effects where
+  closure names _ body = do
+    env <- foldr (\ name rest -> do
+      addr <- alloc name
+      assign addr Abstract
+      Env.insert name addr <$> rest) (pure lowerBound) names
+    addr <- locally (bindAll env *> body `catchReturn` \ (Return ptr) -> pure ptr)
+    deref addr
+
+  call Abstract params = do
+    traverse_ (>>= deref) params
+    box Abstract
