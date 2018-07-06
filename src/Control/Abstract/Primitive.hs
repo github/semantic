970431306aa7e5682adfb5915655269523150819
Rick Winfrey <rewinfrey@github.com>
2018-07-06 10:34:24 -0700

    Add defineNamespace helper

--- src/Control/Abstract/Primitive.hs
+++ src/Control/Abstract/Primitive.hs
@@ -41,6 +41,22 @@ defineClass name superclasses scope = define name $ do
     Env.newEnv . Env.head <$> getEnv
   klass name (map (string . formatName) superclasses) env
 
+defineNamespace :: ( AbstractValue address value effects
+                   , HasCallStack
+                   , Member (Allocator address value) effects
+                   , Member (Env address) effects
+                   , Member (Reader ModuleInfo) effects
+                   , Member (Reader Span) effects
+                   )
+                => Name
+                -> Evaluator address value effects a
+                -> Evaluator address value effects ()
+defineNamespace name scope = define name $ do
+  env <- locally $ do
+    void scope
+    Env.newEnv . Env.head <$> getEnv
+  namespace name env
+
 lambda :: (AbstractFunction address value effects, Member Fresh effects)
        => (Name -> Evaluator address value effects address)
        -> Evaluator address value effects value
