d6542b9e706bdf26805bb28ae9d0e530bc6ca40d
joshvera <josh@joshvera.com>
2018-07-11 14:44:14 -0400

    Fix up handleState implementations

--- src/Semantic/Telemetry.hs
+++ src/Semantic/Telemetry.hs
@@ -134,8 +134,8 @@ data Telemetry (m :: * -> *) output where
   WriteLog  :: Level -> String -> [(String, String)] -> Telemetry m ()
 
 instance Effect Telemetry where
-  handleState c dist (Request (WriteStat stat) k) = Request (WriteStat stat) (dist . (<$ c) . k)
-  handleState c dist (Request (WriteLog level message pairs) k) = Request (WriteLog level message pairs) (dist . (<$ c) . k)
+  handleState c dist (Request (WriteStat stat) k) = Request (WriteStat stat) (\result -> dist (pure result <$ c) k)
+  handleState c dist (Request (WriteLog level message pairs) k) = Request (WriteLog level message pairs) (\result -> dist (pure result <$ c) k)
 
 -- | Run a 'Telemetry' effect by expecting a 'Reader' of 'Queue's to write stats and logs to.
 runTelemetry :: (Member (Lift IO) effects, Effects effects) => LogQueue -> StatQueue -> Eff (Telemetry ': effects) a -> Eff effects a
