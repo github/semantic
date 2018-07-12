57949b03e09748c927aa16ebf85a45805ff9e9cc
Rob Rix <rob.rix@me.com>
2018-07-12 13:53:29 -0400

    Fix a redundant import warning.

--- src/Semantic/Graph.hs
+++ src/Semantic/Graph.hs
@@ -29,7 +29,6 @@ import           Data.Abstract.Evaluatable
 import           Data.Abstract.Module
 import qualified Data.Abstract.ModuleTable as ModuleTable
 import           Data.Abstract.Package as Package
-import           Data.Abstract.Value.Type
 import           Data.Abstract.Value.Concrete (Value, ValueError (..), runValueErrorWith)
 import           Data.Graph
 import           Data.Project
