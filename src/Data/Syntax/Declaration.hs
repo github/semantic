ace5ad3086150395e339e3377be137ad61e75f99
Rick Winfrey <rewinfrey@github.com>
2018-07-13 09:49:43 -0700

    Adjust indentation

--- src/Data/Syntax/Declaration.hs
+++ src/Data/Syntax/Declaration.hs
@@ -34,9 +34,10 @@ instance Declarations a => Declarations (Function a) where
   declaredName Function{..} = declaredName functionName
 
 instance Declarations1 Function where
-  liftDeclaredName declaredName Function{..} = case declaredName functionName of
-                                                 [] -> Nothing
-                                                 (x:_) -> Just x
+  liftDeclaredName declaredName Function{..} =
+    case declaredName functionName of
+      [] -> Nothing
+      (x:_) -> Just x
 
 data Method a = Method { methodContext :: ![a], methodReceiver :: !a, methodName :: !a, methodParameters :: ![a], methodBody :: !a }
   deriving (Eq, Ord, Show, Foldable, Traversable, Functor, Generic1, Hashable1, Mergeable, FreeVariables1, Declarations1, ToJSONFields1, Named1, Message1)
