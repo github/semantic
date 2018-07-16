5df6613cf045d02b0658ba610d0b5ad86db75288
Rob Rix <rob.rix@me.com>
2018-07-16 10:26:12 -0400

    Use Declarations for declared names in Go.

--- src/Language/Go/Syntax.hs
+++ src/Language/Go/Syntax.hs
@@ -84,7 +84,7 @@ instance Show1 QualifiedImport where liftShowsPrec = genericLiftShowsPrec
 instance Evaluatable QualifiedImport where
   eval (QualifiedImport importPath aliasTerm) = do
     paths <- resolveGoImport importPath
-    alias <- either (throwEvalError . FreeVariablesError) pure (freeVariable $ subterm aliasTerm)
+    alias <- maybeM (throwEvalError (FreeVariablesError [])) (declaredName (subterm aliasTerm))
     void . letrec' alias $ \addr -> do
       for_ paths $ \p -> do
         traceResolve (unPath importPath) p
