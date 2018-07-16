43b3aee44d1de4def5bec0301cf1a1dcb8b53db6
Rob Rix <rob.rix@me.com>
2018-07-16 10:16:02 -0400

    Parameterizze liftDeclaredName by the declaredName function for its children.

--- src/Data/Abstract/Declarations.hs
+++ src/Data/Abstract/Declarations.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, UndecidableInstances #-}
 module Data.Abstract.Declarations  where
 
-import Data.Abstract.FreeVariables
 import Data.Abstract.Name
 import Data.Sum
 import Data.Term
@@ -13,18 +12,18 @@ class Declarations syntax where
 
 class Declarations1 syntax where
   -- | Lift a function mapping each element to its set of free variables through a containing structure, collecting the results into a single set.
-  liftDeclaredName :: (a -> [Name]) -> syntax a -> Maybe Name
+  liftDeclaredName :: (a -> Maybe Name) -> syntax a -> Maybe Name
   liftDeclaredName _ _ = Nothing
 
 instance Declarations t => Declarations (Subterm t a) where
   declaredName = declaredName . subterm
 
-deriving instance (Declarations1 syntax, FreeVariables1 syntax) => Declarations (Term syntax ann)
+deriving instance Declarations1 syntax => Declarations (Term syntax ann)
 
-instance (FreeVariables recur, Declarations1 syntax) => Declarations (TermF syntax ann recur) where
-  declaredName = liftDeclaredName freeVariables . termFOut
+instance (Declarations recur, Declarations1 syntax) => Declarations (TermF syntax ann recur) where
+  declaredName = liftDeclaredName declaredName . termFOut
 
-instance (Apply Declarations1 fs) => Declarations1 (Sum fs) where
+instance Apply Declarations1 fs => Declarations1 (Sum fs) where
   liftDeclaredName f = apply @Declarations1 (liftDeclaredName f)
 
 instance Declarations1 []
