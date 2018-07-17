91e4bc4b84d78c2a6ec81ce3cf3e47675a5552a9
Rob Rix <rob.rix@me.com>
2018-07-17 14:00:22 -0400

    Align the fields.

--- src/Semantic/Graph.hs
+++ src/Semantic/Graph.hs
@@ -231,7 +231,7 @@ resumingUnspecialized = runUnspecializedWith (\ err@(Unspecialized _) -> trace (
 
 resumingAddressError :: (AbstractHole value, Lower (Cell address value), Member Trace effects, Show address, Effects effects) => Evaluator address value (Resumable (AddressError address value) ': effects) a -> Evaluator address value effects a
 resumingAddressError = runAddressErrorWith (\ err -> trace ("AddressError: " <> prettyShow err) *> case err of
-  Unallocated _   -> pure lowerBound
+  Unallocated   _ -> pure lowerBound
   Uninitialized _ -> pure hole)
 
 resumingValueError :: (Member Trace effects, Show address, Effects effects) => Evaluator address (Value address body) (Resumable (ValueError address body) ': effects) a -> Evaluator address (Value address body) effects a
