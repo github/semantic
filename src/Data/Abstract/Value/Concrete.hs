1decd8ade1292587831a816a5a03ba457499da54
Rob Rix <rob.rix@me.com>
2018-07-23 13:20:26 -0400

    Define a runFunction handler for Concrete semantics.

--- src/Data/Abstract/Value/Concrete.hs
+++ src/Data/Abstract/Value/Concrete.hs
@@ -3,6 +3,7 @@ module Data.Abstract.Value.Concrete
   ( Value (..)
   , ValueError (..)
   , ClosureBody (..)
+  , runFunction
   , runUnit
   , runBoolean
   , runPair
@@ -75,6 +76,34 @@ runPair = interpret $ \case
   Abstract.Pair a b -> pure (KVPair a b)
   Abstract.AsPair (KVPair a b) -> pure (a, b)
   Abstract.AsPair other        -> throwValueError (KeyValueError other)
+
+runFunction :: ( Coercible body (Eff effects)
+               , Member (Allocator address (Value address body)) effects
+               , Member (Env address) effects
+               , Member (Exc (Return address)) effects
+               , Member Fresh effects
+               , Member (Reader ModuleInfo) effects
+               , Member (Reader PackageInfo) effects
+               , Member (Resumable (ValueError address body)) effects
+               , PureEffects effects
+               )
+            => Evaluator address (Value address body) (Abstract.Function address (Value address body) ': effects) a
+            -> Evaluator address (Value address body) effects a
+runFunction = interpret $ \case
+  Abstract.Function params fvs body -> do
+    packageInfo <- currentPackage
+    moduleInfo <- currentModule
+    i <- fresh
+    Closure packageInfo moduleInfo params (ClosureBody i (coerce (runFunction (Evaluator body)))) <$> close (foldr Set.delete fvs params)
+  Abstract.Call op params -> do
+    case op of
+      Closure packageInfo moduleInfo names (ClosureBody _ body) env -> do
+        -- Evaluate the bindings and body with the closure’s package/module info in scope in order to
+        -- charge them to the closure's origin.
+        withCurrentPackage packageInfo . withCurrentModule moduleInfo $ do
+          bindings <- foldr (\ (name, addr) rest -> Env.insert name addr <$> rest) (pure env) (zip names params)
+          locally (catchReturn (bindAll bindings *> Evaluator (coerce body)))
+      _ -> throwValueError (CallError op) >>= box
 
 
 instance AbstractHole (Value address body) where
