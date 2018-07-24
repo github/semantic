cc2cf50b98fbc5f51fe650ba3bf2ea1f506c1493
Ayman Nadeem <aymannadeem@github.com>
2018-07-24 18:17:42 -0400

    test throws, trycatches and type argument

---
+++ test/fixtures/java/corpus/Throws.B.java
@@ -0,0 +1,6 @@
+class Beyonce {
+  BufferedReader newReader() throws FileNotFoundException {
+      new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+    }
+}
+// this doesn't work
--- test/fixtures/java/corpus/tryWithResourcesStatement.java
+++
@@ -1,6 +0,0 @@
-class Dino {
-  void hi() {
-      try (CustomAnalyzer analyzer = new CustomAnalyzer()){
-    }
-  }
-}
