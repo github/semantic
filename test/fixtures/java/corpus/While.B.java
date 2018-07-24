6475c60beacb46ed6a9c6788686cbc150b1c34cb
Ayman Nadeem <aymannadeem@github.com>
2018-07-24 18:23:08 -0400

    test while

---
+++ test/fixtures/java/corpus/While.B.java
@@ -0,0 +1,8 @@
+class WhileDemo {
+    void main(String[] args){
+        String theString = "cheese";
+        while (theString != "poop") {
+            System.out.println("Count is: " + theString);
+        }
+    }
+}
