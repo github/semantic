98320e4e15a096f3c940b7c96bcfc7562ee37e50
Rob Rix <rob.rix@me.com>
2018-07-25 09:58:09 -0400

    :fire: a redundant raiseEff.

--- src/Data/Abstract/Value/Concrete.hs
+++ src/Data/Abstract/Value/Concrete.hs
@@ -104,7 +104,7 @@ runFunction = interpret $ \case
         withCurrentPackage packageInfo . withCurrentModule moduleInfo $ do
           bindings <- foldr (\ (name, addr) rest -> Env.insert name addr <$> rest) (pure lowerBound) (zip names params)
           let fnEnv = Env.push env
-          withEnv fnEnv (catchReturn (bindAll bindings *> raiseEff (coerce body)))
+          withEnv fnEnv (catchReturn (bindAll bindings *> coerce body))
       _ -> throwValueError (CallError op) >>= box
 
 
