219b29ea117a8f43f27f71505015d57e914b2816
Rick Winfrey <rewinfrey@github.com>
2018-07-31 12:45:08 -0700

    Add ErrorContext and BaseError

--- src/Data/Abstract/ErrorContext.hs
+++ src/Data/Abstract/ErrorContext.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE KindSignatures #-}
+
 module Data.Abstract.ErrorContext where
 
 import Data.Abstract.Module (ModuleInfo)
 import Data.Span (Span)
 
-data ErrorContext = ErrorContext { moduleInfo :: ModuleInfo , span :: Span }
+data ErrorContext = ErrorContext { moduleInfo :: ModuleInfo , span :: Span } deriving (Show)
+
+data BaseError (exc :: * -> *) resume = BaseError { errorContext :: ErrorContext, err :: exc resume } deriving (Show)
