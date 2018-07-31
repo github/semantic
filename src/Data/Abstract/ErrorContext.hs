0a1b63e595399f1fc470dc00fe68bb17c453b9c5
Rick Winfrey <rewinfrey@github.com>
2018-07-31 16:56:52 -0700

    Add custom Show ErrorContext instance

--- src/Data/Abstract/ErrorContext.hs
+++ src/Data/Abstract/ErrorContext.hs
@@ -6,8 +6,26 @@ module Data.Abstract.ErrorContext (
 )
 where
 
-data ErrorContext = ErrorContext { moduleInfo :: ModuleInfo , span :: Span } deriving (Show)
 import qualified Data.Abstract.Module as M
 import qualified Data.Span as S
 
 data BaseError (exc :: * -> *) resume = BaseError { errorContext :: ErrorContext, err :: exc resume } deriving (Show)
+
+data ErrorContext = ErrorContext { moduleInfo :: M.ModuleInfo , span :: S.Span }
+
+instance Show ErrorContext where
+  showsPrec _ ErrorContext{..} = shows errorString
+    where errorString = path <> position
+          path = "path: " <> show (M.modulePath moduleInfo)
+          position | startLine <- S.posLine (S.spanStart span)
+                   , endLine <- S.posLine (S.spanEnd span)
+                   , startLine == endLine
+                   = let startLine = show (S.posLine (S.spanStart span))
+                         startCol  = show (S.posColumn (S.spanStart span))
+                         endCol    = show (S.posColumn (S.spanEnd span))
+                         in startLine <> ":" <> startCol <> "-" <> endCol
+                   | otherwise = let startLine = show (S.posLine (S.spanStart span))
+                                     endLine   = show (S.posLine (S.spanEnd span))
+                                     startCol  = show (S.posColumn (S.spanStart span))
+                                     endCol    = show (S.posColumn (S.spanEnd span))
+                                     in startLine <> ":" <> startCol <> "-" <> endLine <> ":" <> endCol
