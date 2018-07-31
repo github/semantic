1ec2f1ab5a2fec95f14cc6678181e0af3e2c3365
Rick Winfrey <rewinfrey@github.com>
2018-07-31 16:56:31 -0700

    Imports

--- src/Data/Abstract/ErrorContext.hs
+++ src/Data/Abstract/ErrorContext.hs
@@ -2,9 +2,8 @@
 
 module Data.Abstract.ErrorContext where
 
-import Data.Abstract.Module (ModuleInfo)
-import Data.Span (Span)
-
 data ErrorContext = ErrorContext { moduleInfo :: ModuleInfo , span :: Span } deriving (Show)
+import qualified Data.Abstract.Module as M
+import qualified Data.Span as S
 
 data BaseError (exc :: * -> *) resume = BaseError { errorContext :: ErrorContext, err :: exc resume } deriving (Show)
