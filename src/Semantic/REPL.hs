7f60152896b78182b6038af5ce51c53c1c4de034
Rob Rix <rob.rix@me.com>
2018-08-01 12:04:52 -0400

    Add boilerplate for running effectful actions.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- src/Semantic/REPL.hs
+++ src/Semantic/REPL.hs
@@ -3,6 +3,24 @@ module Semantic.REPL () where
 
 import Control.Abstract
 import Control.Monad.IO.Class
+import Data.Abstract.Address
+import Data.Abstract.Evaluatable
+import Data.Abstract.Module
+import Data.Abstract.ModuleTable as ModuleTable
+import Data.Abstract.Package
+import Data.Abstract.Value.Concrete as Concrete
+import Data.Coerce
+import Data.Graph (topologicalSort)
+import Data.Language as Language
+import Data.List (uncons)
+import Data.Project
+import Parsing.Parser
+import Prologue
+import Semantic.Graph
+import Semantic.IO as IO
+import Semantic.Task
+import Semantic.Util
+import System.FilePath
 
 data REPL (m :: * -> *) result where
   Prompt :: REPL m String
@@ -27,3 +45,61 @@ runREPL = interpret $ \case
     putStr "repl: "
     getLine
   Output s -> liftIO (putStrLn s)
+
+rubyREPL = evaluatingREPL <=< repl (Proxy :: Proxy 'Language.Ruby) rubyParser Language.Ruby
+
+evaluatingREPL
+  = runM
+  . runState lowerBound
+  . runFresh 0
+  . runPrintingTrace
+  . fmap reassociate
+  . runLoadError
+  . runUnspecialized
+  . runEnvironmentError
+  . runEvalError
+  . runResolutionError
+  . runAddressError
+  . runValueError
+
+repl proxy parser lang paths = runTaskWithOptions debugOptions $ do
+  blobs <- catMaybes <$> traverse IO.readFile (flip File lang <$> paths)
+  package <- fmap quieterm <$> parsePackage parser (Project (takeDirectory (maybe "/" fst (uncons paths))) blobs lang [])
+  modules <- topologicalSort <$> runImportGraphToModules proxy package
+  pure (runTermEvaluator @_ @_ @(Value Precise (REPLEff Precise))
+       (runREPL
+       (runReader (packageInfo package)
+       (runReader (lowerBound @Span)
+       (runReader (lowerBound @(ModuleTable (NonEmpty (Module (ModuleResult Precise)))))
+       (raiseHandler (runModules (ModuleTable.modulePaths (packageModules package)))
+       (evaluate proxy id withTermSpans (Concrete.runFunction coerce coerce) modules)))))))
+
+
+newtype REPLEff address a = REPLEff
+  { runREPLEff :: Eff '[ Function address (Value address (REPLEff address))
+                       , Exc (LoopControl address)
+                       , Exc (Return address)
+                       , Env address
+                       , Deref address (Value address (REPLEff address))
+                       , Allocator address (Value address (REPLEff address))
+                       , Reader ModuleInfo
+
+                       , Modules address
+                       , Reader (ModuleTable (NonEmpty (Module (ModuleResult address))))
+                       , Reader Span
+                       , Reader PackageInfo
+                       , REPL
+
+                       , Resumable (ValueError address (REPLEff address))
+                       , Resumable (AddressError address (Value address (REPLEff address)))
+                       , Resumable ResolutionError
+                       , Resumable EvalError
+                       , Resumable (EnvironmentError address)
+                       , Resumable (Unspecialized (Value address (REPLEff address)))
+                       , Resumable (LoadError address)
+                       , Trace
+                       , Fresh
+                       , State (Heap address Latest (Value address (REPLEff address)))
+                       , Lift IO
+                       ] a
+  }
