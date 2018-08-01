90a53ff9e3540390237fa6e1fd00f41c1152047a
Rob Rix <rob.rix@me.com>
2018-08-01 12:02:47 -0400

    Define smart constructors for REPL.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- src/Semantic/REPL.hs
+++ src/Semantic/REPL.hs
@@ -4,3 +4,9 @@ module Semantic.REPL () where
 data REPL (m :: * -> *) result where
   Prompt :: REPL m String
   Output :: String -> REPL m ()
+
+prompt :: (Effectful m, Member REPL effects) => m effects String
+prompt = send Prompt
+
+output :: (Effectful m, Member REPL effects) => String -> m effects ()
+output s = send (Output s)
