e34076f0c4a23524da54a03676c33c14482bbc1e
Rob Rix <rob.rix@me.com>
2018-08-01 12:03:19 -0400

    Define a handler for REPL effects.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- src/Semantic/REPL.hs
+++ src/Semantic/REPL.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE GADTs, KindSignatures, LambdaCase, TypeOperators #-}
 module Semantic.REPL () where
 
+import Control.Abstract
+import Control.Monad.IO.Class
+
 data REPL (m :: * -> *) result where
   Prompt :: REPL m String
   Output :: String -> REPL m ()
@@ -10,3 +13,11 @@ prompt = send Prompt
 
 output :: (Effectful m, Member REPL effects) => String -> m effects ()
 output s = send (Output s)
+
+
+runREPL :: (Effectful m, MonadIO (m effects), PureEffects effects) => m (REPL ': effects) a -> m effects a
+runREPL = interpret $ \case
+  Prompt -> liftIO $ do
+    putStr "repl: "
+    getLine
+  Output s -> liftIO (putStrLn s)
