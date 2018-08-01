5fc1dc845bd3bf12388babf1c3346a7db4efde52
Rob Rix <rob.rix@me.com>
2018-08-01 12:02:36 -0400

    Define a REPL effect datatype.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- src/Semantic/REPL.hs
+++ src/Semantic/REPL.hs
@@ -1,1 +1,6 @@
+{-# LANGUAGE GADTs, KindSignatures, LambdaCase, TypeOperators #-}
 module Semantic.REPL () where
+
+data REPL (m :: * -> *) result where
+  Prompt :: REPL m String
+  Output :: String -> REPL m ()
