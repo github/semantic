8da81469bc583432a14f9335edf7e1f50504bb42
Rob Rix <rob.rix@me.com>
2018-08-01 12:03:28 -0400

    Define PureEffect & Effect instances for REPL.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- src/Semantic/REPL.hs
+++ src/Semantic/REPL.hs
@@ -15,6 +15,12 @@ output :: (Effectful m, Member REPL effects) => String -> m effects ()
 output s = send (Output s)
 
 
+instance PureEffect REPL
+instance Effect REPL where
+  handleState state handler (Request Prompt k) = Request Prompt (handler . (<$ state) . k)
+  handleState state handler (Request (Output s) k) = Request (Output s) (handler . (<$ state) . k)
+
+
 runREPL :: (Effectful m, MonadIO (m effects), PureEffects effects) => m (REPL ': effects) a -> m effects a
 runREPL = interpret $ \case
   Prompt -> liftIO $ do
