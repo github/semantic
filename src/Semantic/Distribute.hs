e1708d6dfba5464206b67db5ab5055d5b7041398
Rob Rix <rob.rix@me.com>
2018-08-03 13:08:41 -0400

    Revert "Add a Distribute handler taking a handler for monad transformers wrapping IO."
    This reverts commit f9f69104e2df65e014cec182e57747436356460a.

--- src/Semantic/Distribute.hs
+++ src/Semantic/Distribute.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables, TypeOperators #-}
+{-# LANGUAGE TypeOperators #-}
 module Semantic.Distribute
 ( distribute
 , distributeFor
 , distributeFoldMap
 , Distribute
 , runDistribute
-, runDistributeWithHandler
 ) where
 
 import qualified Control.Concurrent.Async as Async
@@ -44,12 +43,3 @@ instance Effect Distribute where
 -- | Evaluate a 'Distribute' effect concurrently.
 runDistribute :: Eff '[Distribute, Lift IO] a -> Eff '[Lift IO] a
 runDistribute = interpret (\ (Distribute task) -> liftIO (Async.runConcurrently (Async.Concurrently (runM (runDistribute task)))))
-
-
--- | Evaluate a 'Distribute' effect concurrently, using a handler to lower the final monad into 'IO'.
-runDistributeWithHandler :: forall m a . MonadIO m => (forall x . m x -> IO x) -> Eff '[Distribute, Lift m] a -> Eff '[Lift m] a
-runDistributeWithHandler handler = interpret $ \ (Distribute task) ->
-  sendIO (liftIO @m (Async.runConcurrently (Async.Concurrently (handler (runM (runDistributeWithHandler handler task))))))
-
-sendIO :: (Member (Lift m) effects) => m a -> Eff effects a
-sendIO = send . Lift
