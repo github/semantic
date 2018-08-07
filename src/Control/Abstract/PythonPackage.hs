ed0ef5df0d2a95eab9f143d748f5e0d4157b4db3
joshvera <josh@joshvera.com>
2018-08-07 14:47:06 -0400

    Fix packageState logic

--- src/Control/Abstract/PythonPackage.hs
+++ src/Control/Abstract/PythonPackage.hs
@@ -1,58 +1,76 @@
 {-# LANGUAGE GADTs, GeneralizedNewtypeDeriving, ScopedTypeVariables, TypeFamilies, TypeOperators,
-             UndecidableInstances, LambdaCase #-}
+             UndecidableInstances, LambdaCase, MultiWayIf #-}
 module Control.Abstract.PythonPackage
 ( runPythonPackaging, Strategy(..) ) where
 
-import           Control.Abstract.Heap (AddressError)
+import           Control.Abstract.Heap (AddressError, deref, Allocator, Deref)
 import           Control.Abstract.Modules (LoadError (..))
 import           Control.Abstract.Value
 import           Control.Monad.Effect.Internal (raiseHandler, lowerHandler)
 import           Control.Monad.Effect (Effectful (..), Effects, interpose)
 import           qualified Control.Monad.Effect as Eff
-import           Data.Abstract.Evaluatable (EvalError, Evaluator, Resumable, variable, State, runState, put)
+import           Data.Abstract.Evaluatable (EvalError, Evaluator, Resumable, variable, State, runState, put, get, Env, PackageInfo, Fresh, Trace, trace)
+import Control.Abstract.Evaluator (LoopControl, Return)
 import           Data.Abstract.Module hiding (Module)
 import           Data.Abstract.Name (Name (..), name)
 import           Data.Abstract.Path (stripQuotes)
-import           Data.Abstract.Value.Concrete (Value)
+import           Data.Abstract.Value.Concrete (Value(..), ValueError(..))
+import Data.Abstract.Address
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.Text as Text
 import qualified Data.Map as Map
 import qualified Data.List as List
 import           Data.Term
 import           Prologue
+import Data.Coerce
 
 data Strategy = Unknown | Packages [Text] | FindPackages [Text]
   deriving (Show)
 
-runPythonPackaging :: forall address value effects a. (
+runPythonPackaging :: forall effects address body a. (
                       Eff.PureEffects effects
-                      , Member (Resumable (AddressError address value)) effects
-                      , Member (Resumable EvalError) effects
+                      , Show address
+                      , Member (Resumable (ValueError address body)) effects
+                      , Member Fresh effects
+                      , Coercible body (Eff.Eff effects)
                       , Member (State Strategy) effects
-                      , Member (Function address value) effects)
-                   => Evaluator address value effects a
-                   -> Evaluator address value effects a
-runPythonPackaging = (Eff.interpose @(Function address value) $ \case
+                      , Member Trace effects
+                      , Member (Allocator address (Value address body)) effects
+                      , Member (Deref address (Value address body)) effects
+                      , Member (Env address) effects
+                      , Member (Eff.Exc (LoopControl address)) effects
+                      , Member (Eff.Exc (Return address)) effects
+                      , Member (Eff.Reader ModuleInfo) effects
+                      , Member (Eff.Reader PackageInfo) effects
+                      , Member (Function address (Value address body)) effects)
+                   => Evaluator address (Value address body) effects a
+                   -> Evaluator address (Value address body) effects a
+runPythonPackaging evaluator = (Eff.interpose @(Function address (Value address body)) $ \case
   (Call callName params) -> do
+    case callName of
+      Closure _ _ name' paramNames _ _ -> do
+        let bindings = foldr (\ (name, addr) rest -> Map.insert name addr rest) lowerBound (zip paramNames params)
 
-    name' <- asString callName
-    params' <- traverse asPair params
-
-    case name' of
-      "find_packages" -> do
-        case List.lookup (name "exclude") params' of
-          Just value -> do
-            as <- array value
-            as' <- traverse asString as
-            (put (FindPackages (stripQuotes <$> as')))
-          Nothing -> pure ()
-      "setup" -> do
-        case List.lookup (name "packages") params' of
-          Just value -> do
-            as <- asArray value
-            as' <- traverse asString as
-            raise (put (Packages (stripQuotes <$> as')))
-          Nothing -> pure ()
-      _ -> raise (put Unknown)
+        case name' of
+          Just n | name "find_packages" == n -> do
+            case Map.lookup (name "exclude") bindings of
+              Just address -> do
+                as <- (asArray <=< deref) address
+                as' <- traverse (asString <=< deref) as
+                put (FindPackages (stripQuotes <$> as'))
+              _ -> trace "In PythonPackaging" >> put (FindPackages [])
+          Just n | name "setup" == n -> do
+            packageState <- get
+            case packageState of
+              Unknown -> case Map.lookup (name "packages") bindings of
+                Just address -> do
+                  as <- (asArray <=< deref) address
+                  as' <- traverse (asString <=< deref) as
+                  put (Packages (stripQuotes <$> as'))
+                _ -> pure ()
+              _ -> pure ()
+          _ -> pure ()
+      _ -> pure ()
     call callName params
-  (Function params vars body) -> function params vars (raiseEff body)
-  )
+  (Function name params vars body) ->  function name params vars (raiseEff body)
+  ) evaluator
