b58500ab1d0147709e5823a9666bbed7b14a437a
Rick Winfrey <rewinfrey@github.com>
2018-08-07 16:43:09 -0700

    :fire: ErrorContext && enhance BaseError

--- src/Data/Abstract/ErrorContext.hs
+++ src/Data/Abstract/ErrorContext.hs
@@ -10,26 +10,8 @@ import qualified Data.Abstract.Module as M
 import qualified Data.Span as S
 import Prologue
 
-data BaseError (exc :: * -> *) resume = BaseError { errorContext :: ErrorContext, err :: exc resume } deriving (Show)
+data BaseError (exc :: * -> *) resume = BaseError { baseErrorModuleInfo :: ModuleInfo, baseErrorSpan :: Span, baseErrorException :: exc resume }
 
-data ErrorContext = ErrorContext { moduleInfo :: M.ModuleInfo , span :: S.Span }
-
-instance Show ErrorContext where
-  showsPrec _ ErrorContext{..} = shows errorString
-    where errorString = path <> position
-          path = show (M.modulePath moduleInfo)
-          position | startLine <- S.posLine (S.spanStart span)
-                   , endLine <- S.posLine (S.spanEnd span)
-                   , startLine == endLine
-                   = let startLine = show (S.posLine (S.spanStart span))
-                         startCol  = show (S.posColumn (S.spanStart span))
-                         endCol    = show (S.posColumn (S.spanEnd span))
-                         in startLine <> ":" <> startCol <> "-" <> endCol
-                   | otherwise = let startLine = show (S.posLine (S.spanStart span))
-                                     endLine   = show (S.posLine (S.spanEnd span))
-                                     startCol  = show (S.posColumn (S.spanStart span))
-                                     endCol    = show (S.posColumn (S.spanEnd span))
-                                     in startLine <> ":" <> startCol <> "-" <> endLine <> ":" <> endCol
 
 instance (Eq1 exc) => Eq1 (BaseError exc) where
   liftEq f (BaseError _ exc1) (BaseError _ exc2) = liftEq f exc1 exc2
