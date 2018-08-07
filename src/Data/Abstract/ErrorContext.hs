fdd6ea9344f2d513df19f6126b99b81225fc9fcf
Rick Winfrey <rewinfrey@github.com>
2018-08-07 16:44:12 -0700

    Define new Show instance for BaseError

--- src/Data/Abstract/ErrorContext.hs
+++ src/Data/Abstract/ErrorContext.hs
@@ -14,6 +14,14 @@ import Prologue
 
 data BaseError (exc :: * -> *) resume = BaseError { baseErrorModuleInfo :: ModuleInfo, baseErrorSpan :: Span, baseErrorException :: exc resume }
 
+instance (Show (exc resume)) => Show (BaseError exc resume) where
+  showsPrec _ BaseError{..} = shows baseErrorException <> showString " " <> showString errorLocation
+    where errorLocation | startErrorLine == endErrorLine = M.modulePath baseErrorModuleInfo <> " " <> startErrorLine <> ":" <> startErrorCol <> "-" <> endErrorCol
+                        | otherwise = M.modulePath baseErrorModuleInfo <> " " <> startErrorLine <> ":" <> startErrorCol <> "-" <> endErrorLine <> ":" <> endErrorCol
+          startErrorLine = show $ S.posLine (S.spanStart baseErrorSpan)
+          endErrorLine   = show $ S.posLine (S.spanEnd baseErrorSpan)
+          startErrorCol  = show $ S.posColumn (S.spanStart baseErrorSpan)
+          endErrorCol    = show $ S.posColumn (S.spanEnd baseErrorSpan)
 
 instance (Eq1 exc) => Eq1 (BaseError exc) where
   liftEq f (BaseError _ _ exc1) (BaseError _ _ exc2) = liftEq f exc1 exc2
