993bb633e8a86b5d2f079d7e19e27c75084717c0
Rob Rix <rob.rix@me.com>
2018-08-09 15:33:27 -0400

    :fire: the cell parameter to Configuration.

--- src/Data/Abstract/Configuration.hs
+++ src/Data/Abstract/Configuration.hs
@@ -3,12 +3,13 @@ module Data.Abstract.Configuration ( Configuration (..) ) where
 import Data.Abstract.Environment
 import Data.Abstract.Heap
 import Data.Abstract.Live
+import Prologue
 
 -- | A single point in a program’s execution.
-data Configuration term address cell value = Configuration
-  { configurationTerm    :: term                    -- ^ The “instruction,” i.e. the current term to evaluate.
-  , configurationRoots   :: Live address            -- ^ The set of rooted addresses.
-  , configurationContext :: EvalContext address     -- ^ The evaluation context in 'configurationTerm'.
-  , configurationHeap    :: Heap address cell value -- ^ The heap of values.
+data Configuration term address value = Configuration
+  { configurationTerm    :: term                   -- ^ The “instruction,” i.e. the current term to evaluate.
+  , configurationRoots   :: Live address           -- ^ The set of rooted addresses.
+  , configurationContext :: EvalContext address    -- ^ The evaluation context in 'configurationTerm'.
+  , configurationHeap    :: Heap address Set value -- ^ The heap of values.
   }
   deriving (Eq, Ord, Show)
