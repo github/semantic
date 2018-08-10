b93c15370e2bc26e771a00ea458211a5b632d898
Rob Rix <rob.rix@me.com>
2018-08-10 13:44:04 -0400

    Move the address types into their own modules.

--- src/Data/Abstract/Address/Monovariant.hs
+++ src/Data/Abstract/Address/Monovariant.hs
@@ -1,1 +1,13 @@
-module Data.Abstract.Address.Monovariant where
+module Data.Abstract.Address.Monovariant
+( Monovariant(..)
+) where
+
+import Data.Abstract.Name
+import Prologue
+
+-- | 'Monovariant' models using one address for a particular name. It trackes the set of values that a particular address takes and uses it's name to lookup in the store and only allocation if new.
+newtype Monovariant = Monovariant { unMonovariant :: Name }
+  deriving (Eq, Ord)
+
+instance Show Monovariant where
+  showsPrec d = showsUnaryWith showsPrec "Monovariant" d . unMonovariant
