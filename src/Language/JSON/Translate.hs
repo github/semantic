b5ca0082839215504aa82a0dd627e6bae04fd9d3
Timothy Clem <timothy.clem@gmail.com>
2018-08-16 16:57:06 -0700

    Clean up need for splice

--- src/Language/JSON/Translate.hs
+++ src/Language/JSON/Translate.hs
@@ -8,16 +8,19 @@ import           Reprinting.Translate
 
 instance Translation 'JSON where
   translation _ content context = case (content, context) of
-    (Fragment f, _) -> emit $ splice f
-    (Truth t, _)    -> emit . splice $ if t then "true" else "false"
-    (Nullity, _)    -> emit . splice $ "null"
+    (Fragment f, _) -> emit f
 
-    (Open, List:_)         -> emit $ splice "["
-    (Open, Associative:_)  -> emit $ splice "{"
+    (Truth t, _) -> emit $ if t then "true" else "false"
+    (Nullity, _) -> emit "null"
 
-    (Close, List:_)        -> emit $ splice "]"
-    (Close, Associative:_) -> emit $ splice "}"
+    (Open, List:_)        -> emit "["
+    (Open, Associative:_) -> emit "{"
 
-    (Separator, List:_)       -> emit $ splice ","
-    (Separator, Associative:_) -> emit $ splice ":"
+    (Close, List:_)        -> emit "]"
+    (Close, Associative:_) -> emit "}"
+
+    (Separator, List:_)        -> emit ","
+    (Separator, Associative:_) -> emit ","
+    (Separator, Pair:_)        -> emit ":"
+
     _ -> Exc.throwError (Unexpected "invalid context")
