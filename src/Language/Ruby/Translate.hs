5ac0b5a9009624e176647c0348e842be864f4c31
Timothy Clem <timothy.clem@gmail.com>
2018-08-21 11:16:39 -0700

    Continue to refine tokenize api with Ruby as example

--- src/Language/Ruby/Translate.hs
+++ src/Language/Ruby/Translate.hs
@@ -3,7 +3,7 @@ module Language.Ruby.Translate
   ) where
 
 import Data.Machine
-import Data.Semigroup (stimesMonoid)
+import Data.Semigroup (stimes)
 import Data.Reprinting.Splice
 import Data.Reprinting.Token
 import Data.Sequence (Seq)
@@ -13,25 +13,33 @@ translatingRuby = flattened <~ auto step where
 
 step :: Splice -> Seq Splice
 step s@(Unhandled el cs) = case (el, cs) of
-  (TOpen,  TMethod:_) -> emit "def" <> layout Space
-  (TClose, TMethod:_) -> emit "end"
+  (TOpen,  TMethod:_)  -> emit "def" <> layout Space
+  (TClose, TMethod:xs) -> endContext (depth xs) <> emit "end"
 
   (TOpen,  TParams:TMethod:_) -> emit "("
   (TSep,   TParams:TMethod:_) -> emit "," <> layout Space
   (TClose, TParams:TMethod:_) -> emit ")"
 
-  (TOpen,  Imperative:xs) -> indented (n xs)
-  (TSep,   Imperative:xs) -> indented (n xs)
-  (TClose, Imperative:_)  -> layout HardWrap
-
-  -- (TOpen, TComment:_) -> layout HardWrap
-  -- (TSep, TComment:_) -> layout HardWrap
+  (TOpen,  Imperative:[]) -> mempty
+  (TOpen,  Imperative:xs) -> layout HardWrap <> indent (depth xs)
+  (TSep,   Imperative:xs) -> layout HardWrap <> indent (depth xs)
+  (TClose, Imperative:[]) -> layout HardWrap
+  (TClose, Imperative:xs) -> indent (pred (depth xs))
 
   _ -> pure s
 
   where
-    indented times = stimesMonoid times (layouts [HardWrap, Indent])
+    endContext times = layout HardWrap <> indent (pred times)
     emit = splice el cs
-    n = length . filter (== Imperative)
 
 step x = pure x
+
+-- | Depth of imperative scope.
+depth :: [Context] -> Int
+depth = length . filter (== Imperative)
+
+-- | Indent n times.
+indent :: Integral b => b -> Seq Splice
+indent times
+  | times > 0 = stimes times (layout Indent)
+  | otherwise = mempty
