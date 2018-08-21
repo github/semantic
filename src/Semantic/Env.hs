9598505b94211e849554860313d3eabc001136eb
Rick Winfrey <rewinfrey@github.com>
2018-08-21 14:55:54 -0700

    :fire: unnecessary `Num` constraint

--- src/Semantic/Env.hs
+++ src/Semantic/Env.hs
@@ -11,7 +11,7 @@ import Text.Read (readMaybe)
 envLookupString :: MonadIO io => String -> String -> io String
 envLookupString defaultVal k = liftIO $ fromMaybe defaultVal <$> lookupEnv k
 
-envLookupNum :: (MonadIO io, Num a, Read a) => a -> String -> io a
+envLookupNum :: (MonadIO io, Read a) => a -> String -> io a
 envLookupNum defaultVal k = liftIO $ parse <$> lookupEnv k
   where parse x | Just s <- x, Just p <- readMaybe s = p
                 | otherwise = defaultVal
