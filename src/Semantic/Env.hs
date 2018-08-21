64bea60ea931ad9625182267fb8eb4546c788a83
Rick Winfrey <rewinfrey@github.com>
2018-08-21 14:49:56 -0700

    :abc:

--- src/Semantic/Env.hs
+++ src/Semantic/Env.hs
@@ -11,7 +11,7 @@ import Text.Read (readMaybe)
 envLookupString :: MonadIO io => String -> String -> io String
 envLookupString defaultVal k = liftIO $ fromMaybe defaultVal <$> lookupEnv k
 
-envLookupNum :: (Read a, MonadIO io, Num a) => a -> String -> io a
+envLookupNum :: (MonadIO io, Num a, Read a) => a -> String -> io a
 envLookupNum defaultVal k = liftIO $ parse <$> lookupEnv k
   where parse x | Just s <- x, Just p <- readMaybe s = p
                 | otherwise = defaultVal
