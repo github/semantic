d230df876a8c720d77ddba1283f2fd5f08111736
Rick Winfrey <rewinfrey@github.com>
2018-08-21 16:43:13 -0700

    Fix typo

--- src/Semantic/Env.hs
+++ src/Semantic/Env.hs
@@ -12,7 +12,7 @@ import Text.Read (readMaybe)
 envLookupString :: MonadIO io => String -> String -> io String
 envLookupString defaultVal k = liftIO $ fromMaybe defaultVal <$> lookupEnv k
 
--- | Although the `Num a` constraint is redundant (hence -Wno-redundant-constraints), we use this constraint to communicate this function is mean to read Num values.
+-- | Although the `Num a` constraint is redundant (hence -Wno-redundant-constraints), we use this constraint to communicate this function is meant to read Num values.
 envLookupNum :: (MonadIO io, Num a, Read a) => a -> String -> io a
 envLookupNum defaultVal k = liftIO $ parse <$> lookupEnv k
   where parse x | Just s <- x, Just p <- readMaybe s = p
