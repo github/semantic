5ced60c857377cd12b5becb0de030e1a8fb84224
Timothy Clem <timothy.clem@gmail.com>
2018-08-22 13:47:30 -0700

    Very basic test for ruby printing

--- test/Reprinting/Spec.hs
+++ test/Reprinting/Spec.hs
@@ -15,42 +15,63 @@ import Data.Foldable
 import Semantic.IO
 import Data.Blob
 import Language.JSON.PrettyPrint
+import Language.Ruby.PrettyPrint
 
 spec :: Spec
 spec = describe "reprinting" $ do
-  let path = "test/fixtures/javascript/reprinting/map.json"
+  context "JSON" $ do
+    let path = "test/fixtures/javascript/reprinting/map.json"
+    (src, tree) <- runIO $ do
+      src  <- blobSource <$> readBlobFromPath (File path Language.JSON)
+      tree <- parseFile jsonParser path
+      pure (src, tree)
 
-  (src, tree) <- runIO $ do
-    src  <- blobSource <$> readBlobFromPath (File path Language.JSON)
-    tree <- parseFile jsonParser "test/fixtures/javascript/reprinting/map.json"
-    pure (src, tree)
+    describe "tokenization" $ do
 
-  describe "tokenization" $ do
+      it "should pass over a pristine tree" $ do
+        let tagged = mark Unmodified tree
+        let toks = tokenizing src tagged
+        toks `shouldBe` [Chunk src]
 
-    it "should pass over a pristine tree" $ do
-      let tagged = mark Unmodified tree
-      let toks = tokenizing src tagged
-      toks `shouldBe` [Chunk src]
+      it "should emit control tokens but only 1 chunk for a wholly-modified tree" $ do
+        let toks = tokenizing src (mark Refactored tree)
+        for_ @[] [TList, THash] $ \t -> do
+          toks `shouldSatisfy` elem (TControl (Enter t))
+          toks `shouldSatisfy` elem (TControl (Exit t))
 
-    it "should emit control tokens but only 1 chunk for a wholly-modified tree" $ do
-      let toks = tokenizing src (mark Refactored tree)
-      for_ @[] [TList, THash] $ \t -> do
-        toks `shouldSatisfy` elem (TControl (Enter t))
-        toks `shouldSatisfy` elem (TControl (Exit t))
+    describe "pipeline" $ do
 
-  describe "pipeline" $ do
-    it "should roundtrip exactly over a pristine tree" $ do
-      let tagged = mark Unmodified tree
-      let printed = runReprinter src defaultJSONPipeline tagged
-      printed `shouldBe` Right src
+      it "should roundtrip exactly over a pristine tree" $ do
+        let tagged = mark Unmodified tree
+        let printed = runReprinter src defaultJSONPipeline tagged
+        printed `shouldBe` Right src
 
-    it "should roundtrip exactly over a wholly-modified tree" $ do
-      let tagged = mark Refactored tree
-      let printed = runReprinter src defaultJSONPipeline tagged
-      printed `shouldBe` Right src
+      it "should roundtrip exactly over a wholly-modified tree" $ do
+        let tagged = mark Refactored tree
+        let printed = runReprinter src defaultJSONPipeline tagged
+        printed `shouldBe` Right src
 
-    it "should be able to parse the output of a refactor" $ do
-      let tagged = increaseNumbers (mark Refactored tree)
-      let (Right printed) = runReprinter src defaultJSONPipeline tagged
-      tree' <- runTask (parse jsonParser (Blob printed path Language.JSON))
-      length tree' `shouldSatisfy` (/= 0)
+      it "should be able to parse the output of a refactor" $ do
+        let tagged = increaseNumbers (mark Refactored tree)
+        let (Right printed) = runReprinter src defaultJSONPipeline tagged
+        tree' <- runTask (parse jsonParser (Blob printed path Language.JSON))
+        length tree' `shouldSatisfy` (/= 0)
+
+  context "Ruby" $ do
+    let dir = "test/fixtures/ruby/reprinting"
+    let path = dir </> "function.rb"
+    let expectedPath = dir </> "function.out.rb"
+    (src, tree, expected) <- runIO $ do
+      expected  <- blobSource <$> readBlobFromPath (File expectedPath Language.Ruby)
+      src  <- blobSource <$> readBlobFromPath (File path Language.Ruby)
+      tree <- parseFile miniRubyParser path
+      pure (src, tree, expected)
+
+    describe "pipeline" $ do
+
+      it "should roundtrip over a wholly-modified tree" $ do
+        let tagged = mark Refactored tree
+        let (Right printed) = runReprinter src printingRuby tagged
+        printed `shouldBe` expected
+        tree' <- runTask (parse miniRubyParser (Blob printed expectedPath Language.Ruby))
+        length tree' `shouldSatisfy` (/= 0)
