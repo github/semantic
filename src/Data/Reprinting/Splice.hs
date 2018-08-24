38160fff053bcc5a660537461adb9a1050d71dcd
Timothy Clem <timothy.clem@gmail.com>
2018-08-24 10:36:03 -0700

    Re-order

--- src/Data/Reprinting/Splice.hs
+++ src/Data/Reprinting/Splice.hs
@@ -1,20 +1,43 @@
 module Data.Reprinting.Splice
-  ( Splice(..)
+  ( Fragment(..)
+  , copy
+  , insert
+  , defer
+  , Splice(..)
   , emit
   , layout
   , layouts
   , space
   , indent
-  , Fragment(..)
-  , copy
-  , insert
-  , defer
   , Whitespace(..)
   ) where
 
 import Data.Reprinting.Token
 import Data.Sequence (singleton, fromList)
 import Prologue hiding (Element)
+
+-- | An intermediate representation of concrete syntax in the reprinting pipeline.
+data Fragment
+  = Verbatim Text
+  -- ^ Verbatim copy of original 'Text' (un-refactored).
+  | New Element [Context] Text
+  -- ^ New 'Text' to be inserted, along with original 'Element' and `Context`
+  -- allowing later steps to re-write.
+  | Defer Element [Context]
+  -- ^ To be handled further down the pipeline.
+  deriving (Eq, Show)
+
+-- | Copy along some original, un-refactored 'Text'.
+copy :: Text -> Seq Fragment
+copy = singleton . Verbatim
+
+-- | Construct an 'New' datum.
+insert :: Element -> [Context] -> Text -> Seq Fragment
+insert el c = singleton . New el c
+
+-- | Construct an 'Defer' splice.
+defer :: Element -> [Context] -> Seq Fragment
+defer el = singleton . Defer el
 
 -- | The final representation of concrete syntax in the reprinting pipeline.
 data Splice
@@ -43,29 +66,6 @@ indent :: Integral b => b -> Seq Splice
 indent times
   | times > 0 = stimes times (layout Indent)
   | otherwise = mempty
-
--- | An intermediate representation of concrete syntax in the reprinting pipeline.
-data Fragment
-  = Verbatim Text
-  -- ^ Verbatim copy of original 'Text' (un-refactored).
-  | New Element [Context] Text
-  -- ^ New 'Text' to be inserted, along with original 'Element' and `Context`
-  -- allowing later steps to re-write.
-  | Defer Element [Context]
-  -- ^ To be handled further down the pipeline.
-  deriving (Eq, Show)
-
--- | Copy along some original, un-refactored 'Text'.
-copy :: Text -> Seq Fragment
-copy = singleton . Verbatim
-
--- | Construct an 'New' datum.
-insert :: Element -> [Context] -> Text -> Seq Fragment
-insert el c = singleton . New el c
-
--- | Construct an 'Defer' splice.
-defer :: Element -> [Context] -> Seq Fragment
-defer el = singleton . Defer el
 
 -- | Indentation, spacing, and other whitespace.
 data Whitespace
