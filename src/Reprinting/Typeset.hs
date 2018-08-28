a3c8f170fc55e0334b6acc9d43f75b1a6e44d5ac
Timothy Clem <timothy.clem@gmail.com>
2018-08-28 10:32:43 -0700

    A typesetting that shows whitespace for help debugging

--- src/Reprinting/Typeset.hs
+++ src/Reprinting/Typeset.hs
@@ -1,6 +1,7 @@
 module Reprinting.Typeset
   ( typeset
   , typesetting
+  , typesettingWithVisualWhitespace
   ) where
 
 import Prologue
@@ -24,3 +25,16 @@ step (Layout (Indent 0 Spaces)) = mempty
 step (Layout (Indent n Spaces)) = stimes n space
 step (Layout (Indent 0 Tabs))   = mempty
 step (Layout (Indent n Tabs))   = stimes n "\t"
+
+-- | Typeset, but show whitespace with printable characters for debugging purposes.
+typesettingWithVisualWhitespace :: Monad m => ProcessT m Splice (Doc a)
+typesettingWithVisualWhitespace = auto step where
+  step :: Splice -> Doc a
+  step (Emit t)                   = pretty t
+  step (Layout SoftWrap)          = softline
+  step (Layout HardWrap)          = "\\n" <> hardline
+  step (Layout Space)             = "."
+  step (Layout (Indent 0 Spaces)) = mempty
+  step (Layout (Indent n Spaces)) = stimes n "."
+  step (Layout (Indent 0 Tabs))   = mempty
+  step (Layout (Indent n Tabs))   = stimes n "\t"
