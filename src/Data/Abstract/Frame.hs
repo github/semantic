ef96820d4dceec6b43334695c0cd88eb9a720109
joshvera <josh@joshvera.com>
2018-09-07 17:53:09 -0400

    Implement frames

--- src/Data/Abstract/Frame.hs
+++ src/Data/Abstract/Frame.hs
@@ -1,16 +1,6 @@
 module Data.Abstract.Frame () where
 
 
-newtype Heap address = Heap { unScope :: Monoidal.Map address (Frame address) }
-  deriving (Eq, Foldable, Lower, Monoid, Ord, Semigroup)
-
-data HeapFrame = HeapFrame { scopeId :: scopeId, []}
-
-newtype Heap scopeId = Heap { unHeap :: [scopeId] }
-
-data Frame address where
-    Frame :: address -> HeapTy address -> Frame address
-
 -- setSlot :: t -> value t heap -> Frame s scopes -> Heap scopes -> Heap scopes
 -- setSlot d v f h = case lookup h f of
 --     Just (slots, links) ->
