ba3ae715fe33d810c41e363ad99924592b4ac407
joshvera <josh@joshvera.com>
2018-09-12 11:51:17 -0400

    Store Spans in the scope graph

--- src/Control/Abstract/ScopeGraph.hs
+++ src/Control/Abstract/ScopeGraph.hs
@@ -3,17 +3,18 @@ module Control.Abstract.ScopeGraph (runScopeEnv, ScopeEnv) where
 
 import Control.Abstract.Evaluator
 import Data.Abstract.Name
+import Data.Span
 import Data.Abstract.ScopeGraph as ScopeGraph
 import Prologue
 
-data ScopeEnv address ddata (m :: * -> *) a where
-    Lookup :: Reference -> ScopeEnv address ddata m (Maybe address)
-    Declare :: Declaration -> ddata -> ScopeEnv address ddata m ()
-    Reference :: Reference -> Declaration -> ScopeEnv address ddata m ()
-    Create :: Map EdgeLabel [Name] -> ScopeEnv Name ddata m ()
+data ScopeEnv address (m :: * -> *) a where
+    Lookup :: Reference -> ScopeEnv address m (Maybe address)
+    Declare :: Declaration -> Span -> ScopeEnv address m ()
+    Reference :: Reference -> Declaration -> ScopeEnv address m ()
+    Create :: Map EdgeLabel [Name] -> ScopeEnv Name m ()
 
-instance PureEffect (ScopeEnv address ddata)
-instance Effect (ScopeEnv address ddata) where
+instance PureEffect (ScopeEnv address)
+instance Effect (ScopeEnv address) where
   handleState c dist (Request (Lookup ref) k)         = Request (Lookup ref) (dist . (<$ c) . k)
   handleState c dist (Request (Declare decl ddata) k) = Request (Declare decl ddata) (dist . (<$ c) . k)
   handleState c dist (Request (Reference ref decl) k) = Request (Reference ref decl) (dist . (<$ c) . k)
@@ -21,27 +22,27 @@ instance Effect (ScopeEnv address ddata) where
 
 runScopeEnv :: (Ord scope, Effects effects, Member Fresh effects)
             => scope
-            -> Evaluator address value (ScopeEnv scope ddata ': effects) a
-            -> Evaluator address value effects (ScopeGraph scope ddata, a)
+            -> Evaluator address value (ScopeEnv scope ': effects) a
+            -> Evaluator address value effects (ScopeGraph scope, a)
 runScopeEnv scope = runState (ScopeGraph.emptyGraph scope) . reinterpret handleScopeEnv
 
-handleScopeEnv :: forall scope address ddata value effects a. (Ord scope, Member Fresh effects)
-          => ScopeEnv scope ddata (Eff (ScopeEnv scope ddata ': effects)) a
-          -> Evaluator address value (State (ScopeGraph scope ddata) ': effects) a
+handleScopeEnv :: forall scope address value effects a. (Ord scope, Member Fresh effects)
+          => ScopeEnv scope (Eff (ScopeEnv scope ': effects)) a
+          -> Evaluator address value (State (ScopeGraph scope) ': effects) a
 handleScopeEnv = \case
     Lookup ref -> do
-        graph <- get @(ScopeGraph scope ddata)
+        graph <- get @(ScopeGraph scope)
         pure (ScopeGraph.scopeOfRef ref graph)
     Declare decl ddata -> do
         graph <- get
-        put @(ScopeGraph scope ddata) (ScopeGraph.declare decl ddata graph)
+        put @(ScopeGraph scope) (ScopeGraph.declare decl ddata graph)
         pure ()
     Reference ref decl -> do
         graph <- get
-        put @(ScopeGraph scope ddata) (ScopeGraph.reference ref decl graph)
+        put @(ScopeGraph scope) (ScopeGraph.reference ref decl graph)
         pure ()
     Create edges -> do
-        graph <- get @(ScopeGraph scope ddata)
+        graph <- get @(ScopeGraph scope)
         scope <- gensym
         put  (ScopeGraph.create scope edges graph)
         pure ()
