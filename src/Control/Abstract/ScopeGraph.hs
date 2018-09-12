6f404d77a21f3f7b0edd3c4a07bfc474f65ccb69
joshvera <josh@joshvera.com>
2018-09-12 16:28:39 -0400

    add helper functions

--- src/Control/Abstract/ScopeGraph.hs
+++ src/Control/Abstract/ScopeGraph.hs
@@ -1,18 +1,32 @@
 {-# LANGUAGE GADTs, KindSignatures, LambdaCase, RankNTypes, ScopedTypeVariables, TypeOperators #-}
-module Control.Abstract.ScopeGraph (runScopeEnv, ScopeEnv) where
+module Control.Abstract.ScopeGraph (runScopeEnv, ScopeEnv, lookup, declare, reference, create) where
 
 import Control.Abstract.Evaluator
 import Control.Abstract.Heap
 import Data.Abstract.Name
 import Data.Span
-import Data.Abstract.ScopeGraph as ScopeGraph
+import Data.Abstract.ScopeGraph (Declaration, Reference, EdgeLabel, ScopeGraph)
+import qualified Data.Abstract.ScopeGraph as ScopeGraph
 import Prologue
+import Prelude hiding (lookup)
 
 data ScopeEnv address (m :: * -> *) a where
     Lookup :: Reference -> ScopeEnv address m (Maybe address)
     Declare :: Declaration -> Span -> ScopeEnv address m ()
     Reference :: Reference -> Declaration -> ScopeEnv address m ()
     Create :: Map EdgeLabel [address] -> ScopeEnv address m ()
+
+lookup :: forall address value effects. Member (ScopeEnv address) effects => Reference -> Evaluator address value effects (Maybe address)
+lookup = send . Lookup @address
+
+declare :: forall address value effects. Member (ScopeEnv address) effects => Declaration -> Span -> Evaluator address value effects ()
+declare = (send .) . Declare @address
+
+reference :: forall address value effects. Member (ScopeEnv address) effects => Reference -> Declaration -> Evaluator address value effects ()
+reference = (send .) . Reference @address
+
+create :: forall address value effects. Member (ScopeEnv address) effects => Map EdgeLabel [address] -> Evaluator address value effects ()
+create = send . Create @address
 
 instance PureEffect (ScopeEnv address)
 instance Effect (ScopeEnv address) where
