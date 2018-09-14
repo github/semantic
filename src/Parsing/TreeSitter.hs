aea6e81ad7d4281d5d78a130780c13b0068594f7
Timothy Clem <timothy.clem@gmail.com>
2018-09-14 14:01:15 -0700

    This can use our timeout effect now

--- src/Parsing/TreeSitter.hs
+++ src/Parsing/TreeSitter.hs
@@ -16,7 +16,6 @@ import           Data.ByteString.Unsafe (unsafeUseAsCStringLen)
 import           Foreign
 import           Foreign.C.Types (CBool (..))
 import           Foreign.Marshal.Array (allocaArray)
-import           System.Timeout
 
 import Data.AST (AST, Node (Node))
 import Data.Blob
@@ -25,6 +24,7 @@ import Data.Range
 import Data.Source
 import Data.Span
 import Data.Term
+import Semantic.Timeout
 
 import qualified TreeSitter.Language as TS
 import qualified TreeSitter.Node as TS
@@ -57,7 +57,7 @@ runParser parser blobSource  = unsafeUseAsCStringLen (sourceBytes blobSource) $ 
 
 -- | Parse 'Source' with the given 'TS.Language' and return its AST.
 -- Returns Nothing if the operation timed out.
-parseToAST :: (Bounded grammar, Enum grammar, Member (Lift IO) effects, Member Trace effects, PureEffects effects) => Duration -> Ptr TS.Language -> Blob -> Eff effects (Maybe (AST [] grammar))
+parseToAST :: (Bounded grammar, Enum grammar, Member (Lift IO) effects, Member Timeout effects, Member Trace effects, PureEffects effects) => Duration -> Ptr TS.Language -> Blob -> Eff effects (Maybe (AST [] grammar))
 parseToAST parseTimeout language Blob{..} = bracket TS.ts_parser_new TS.ts_parser_delete $ \ parser -> do
   liftIO $ do
     TS.ts_parser_halt_on_error parser (CBool 1)
@@ -68,7 +68,7 @@ parseToAST parseTimeout language Blob{..} = bracket TS.ts_parser_new TS.ts_parse
   parsing <- liftIO . async $ runParser parser blobSource
 
   -- Kick the parser off asynchronously and wait according to the provided timeout.
-  res <- liftIO . timeout (toMicroseconds parseTimeout) $ wait parsing
+  res <- timeout parseTimeout $ liftIO (wait parsing)
 
   case res of
     Just Failed          -> Nothing  <$ trace ("tree-sitter: parsing failed " <> blobPath)
