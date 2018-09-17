e2bd55950d7eba2cf1ed1770c2c74d3a5f9d69e2
Rob Rix <rob.rix@me.com>
2018-09-17 15:38:21 -0400

    Widen the heap.

--- src/Semantic/Graph.hs
+++ src/Semantic/Graph.hs
@@ -111,8 +111,8 @@ runCallGraph lang includePackages modules package = do
       runGraphAnalysis
         = runTermEvaluator @_ @(Hole (Maybe Name) (Located Monovariant)) @Abstract
         . graphing @_ @_ @(Maybe Name) @Monovariant
-        . caching
         . runState (lowerBound @(Heap (Hole (Maybe Name) (Located Monovariant)) Abstract))
+        . caching
         . runFresh 0
         . resumingLoadError
         . resumingUnspecialized
