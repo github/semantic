2d071e50905e988124322deda3388dac07bfcbe6
joshvera <josh@joshvera.com>
2018-09-17 13:05:24 -0400

    Remove Data.Abstract.Frame for now

--- src/Data/Abstract/Frame.hs
+++
@@ -1,9 +0,0 @@
-module Data.Abstract.Frame () where
-
-
--- setSlot :: t -> value t heap -> Frame s scopes -> Heap scopes -> Heap scopes
--- setSlot d v f h = case lookup h f of
---     Just (slots, links) ->
-
--- data Frame types scopes where
---     Frame :: Slots types scopes -> Links types scopes -> Frame types scopes
