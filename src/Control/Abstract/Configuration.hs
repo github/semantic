c3616e261f6a04d156ed45885ff32127bae0a49f
joshvera <josh@joshvera.com>
2018-09-18 18:34:31 -0400

    what if we don't need these

--- src/Control/Abstract/Configuration.hs
+++ src/Control/Abstract/Configuration.hs
@@ -8,8 +8,7 @@ import Control.Abstract.Roots
 import Control.Abstract.TermEvaluator
 
 -- | Get the current 'Configuration' with a passed-in term.
-getConfiguration :: (Member (Reader (Live address)) effects, Member (Env address) effects, Member (State (Heap address value)) effects)
+getConfiguration :: (Member (Reader (Live address)) effects, Member (Env address) effects, Member (State (Heap address address value)) effects)
                  => term
                  -> TermEvaluator term address value effects (Configuration term address value)
 getConfiguration term = Configuration term <$> TermEvaluator askRoots <*> TermEvaluator getEvalContext <*> TermEvaluator getHeap
-
