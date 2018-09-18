c2bfde5c958a2fcbcd29abf532aea3fb13927d74
Patrick Thomson <patrickt@github.com>
2018-09-18 11:38:10 -0400

    move Fragment into its own file for clarity's sake

--- src/Data/Reprinting/Splice.hs
+++ src/Data/Reprinting/Splice.hs
@@ -20,30 +20,8 @@ import Prologue hiding (Element)
 
 import Data.Machine
 
+import Data.Reprinting.Fragment
 import Data.Reprinting.Token
-
--- | An intermediate representation of concrete syntax in the reprinting pipeline.
-data Fragment
-  = Verbatim Text
-  -- ^ Verbatim copy of original 'Text' (un-refactored).
-  | New Element [Context] Text
-  -- ^ New 'Text' to be inserted, along with original 'Element' and `Context`
-  -- allowing later steps to re-write.
-  | Defer Element [Context]
-  -- ^ To be handled further down the pipeline.
-  deriving (Eq, Show)
-
--- | Copy along some original, un-refactored 'Text'.
-copy :: Text -> Plan k Fragment ()
-copy = yield . Verbatim
-
--- | Insert some new 'Text'.
-insert :: Element -> [Context] -> Text -> Plan k Fragment ()
-insert el c = yield . New el c
-
--- | Defer processing an element to a later stage.
-defer :: Element -> [Context] -> Plan k Fragment ()
-defer el = yield . Defer el
 
 -- | The final representation of concrete syntax in the reprinting pipeline.
 data Splice
