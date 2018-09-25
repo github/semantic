df7176e9a2a7027f2762207e124caa6e11bf07ba
Timothy Clem <timothy.clem@gmail.com>
2018-09-25 11:18:02 -0500

    Don't need to know about Location here

--- src/Analysis/Decorator.hs
+++ src/Analysis/Decorator.hs
@@ -3,13 +3,12 @@ module Analysis.Decorator
 ( decoratorWithAlgebra
 ) where
 
-import Data.Location
 import Data.Term
 import Prologue
 
 -- | Lift an algebra into a decorator for terms annotated with records.
 decoratorWithAlgebra :: Functor syntax
-                     => RAlgebra (TermF syntax Location) (Term syntax Location) a -- ^ An R-algebra on terms.
-                     -> Term syntax Location                                      -- ^ A term to decorate with values produced by the R-algebra.
-                     -> Term syntax (a, Location)                                 -- ^ A term decorated with values produced by the R-algebra.
+                     => RAlgebra (TermF syntax a) (Term syntax a) b -- ^ An R-algebra on terms.
+                     -> Term syntax a                               -- ^ A term to decorate with values produced by the R-algebra.
+                     -> Term syntax (b, a)                          -- ^ A term decorated with values produced by the R-algebra.
 decoratorWithAlgebra alg = para $ \ c@(In a f) -> termIn (alg (fmap (second (fst . termAnnotation)) c), a) (fmap snd f)
