c4be4036eafff1145f5351d0a5ed901b85c8bbee
Timothy Clem <timothy.clem@gmail.com>
2018-09-25 11:18:51 -0500

    Fix up the tests

--- test/Diffing/Interpreter/Spec.hs
+++ test/Diffing/Interpreter/Spec.hs
@@ -7,9 +7,9 @@ import Data.Foldable (asum)
 import Data.Functor.Foldable (cata)
 import Data.Functor.Listable
 import Data.Maybe
+import Data.Location
 import Data.Mergeable
 import Data.Patch (after, before)
-import Data.Record
 import Data.Sum
 import Data.Term
 import Data.These
@@ -25,48 +25,48 @@ spec :: Spec
 spec = parallel $ do
   describe "diffTerms" $ do
     it "returns a replacement when comparing two unicode equivalent terms" $
-      let termA = termIn Nil (inject (Syntax.Identifier "t\776"))
-          termB = termIn Nil (inject (Syntax.Identifier "\7831")) in
-          diffTerms termA termB `shouldBe` replacing termA (termB :: Term ListableSyntax (Record '[]))
+      let termA = termIn emptyAnnotation (inject (Syntax.Identifier "t\776"))
+          termB = termIn emptyAnnotation (inject (Syntax.Identifier "\7831")) in
+          diffTerms termA termB `shouldBe` replacing termA (termB :: Term ListableSyntax (DiffAnnotation ()))
 
     prop "produces correct diffs" $
-      \ a b -> let diff = diffTerms a b :: Diff ListableSyntax (Record '[]) (Record '[]) in
+      \ a b -> let diff = diffTerms a b :: Diff ListableSyntax (DiffAnnotation ()) (DiffAnnotation ()) in
                    (beforeTerm diff, afterTerm diff) `shouldBe` (Just a, Just b)
 
     prop "produces identity diffs for equal terms " $
-      \ a -> let diff = diffTerms a a :: Diff ListableSyntax (Record '[]) (Record '[]) in
+      \ a -> let diff = diffTerms a a :: Diff ListableSyntax (DiffAnnotation ()) (DiffAnnotation ()) in
                  length (diffPatches diff) `shouldBe` 0
 
     it "produces unbiased insertions within branches" $
-      let term s = termIn Nil (inject [ termIn Nil (inject (Syntax.Identifier s)) ]) :: Term ListableSyntax (Record '[])
-          wrap = termIn Nil . inject in
-      diffTerms (wrap [ term "b" ]) (wrap [ term "a", term "b" ]) `shouldBe` merge (Nil, Nil) (inject [ inserting (term "a"), merging (term "b") ])
+      let term s = termIn emptyAnnotation (inject [ termIn emptyAnnotation (inject (Syntax.Identifier s)) ]) :: Term ListableSyntax (DiffAnnotation ())
+          wrap = termIn emptyAnnotation . inject in
+      diffTerms (wrap [ term "b" ]) (wrap [ term "a", term "b" ]) `shouldBe` merge (emptyAnnotation, emptyAnnotation) (inject [ inserting (term "a"), merging (term "b") ])
 
     let noContext :: Term ListableSyntax a -> Bool
         noContext = isNothing . project @Syntax.Context . termOut
 
     prop "compares nodes against context" . forAll (filterT (noContext . fst) tiers) $
-      \ (a, b) -> diffTerms a (termIn Nil (inject (Syntax.Context (pure b) a))) `shouldBe` insertF (In Nil (inject (Syntax.Context (pure (inserting b)) (merging (a :: Term ListableSyntax (Record '[]))))))
+      \ (a, b) -> diffTerms a (termIn emptyAnnotation (inject (Syntax.Context (pure b) a))) `shouldBe` insertF (In emptyAnnotation (inject (Syntax.Context (pure (inserting b)) (merging (a :: Term ListableSyntax (DiffAnnotation ()))))))
 
     prop "diffs forward permutations as changes" $
-      \ a -> let wrap = termIn Nil . inject
+      \ a -> let wrap = termIn emptyAnnotation . inject
                  b = wrap [a]
                  c = wrap [a, b] in
-        diffTerms (wrap [a, b, c]) (wrap [c, a, b :: Term ListableSyntax (Record '[])]) `shouldBe` merge (Nil, Nil) (inject [ inserting c, merging a, merging b, deleting c ])
+        diffTerms (wrap [a, b, c]) (wrap [c, a, b :: Term ListableSyntax (DiffAnnotation ())]) `shouldBe` merge (emptyAnnotation, emptyAnnotation) (inject [ inserting c, merging a, merging b, deleting c ])
 
     prop "diffs backward permutations as changes" $
-      \ a -> let wrap = termIn Nil . inject
+      \ a -> let wrap = termIn emptyAnnotation . inject
                  b = wrap [a]
                  c = wrap [a, b] in
-        diffTerms (wrap [a, b, c]) (wrap [b, c, a :: Term ListableSyntax (Record '[])]) `shouldBe` merge (Nil, Nil) (inject [ deleting a, merging b, merging c, inserting a ])
+        diffTerms (wrap [a, b, c]) (wrap [b, c, a :: Term ListableSyntax (DiffAnnotation ())]) `shouldBe` merge (emptyAnnotation, emptyAnnotation) (inject [ deleting a, merging b, merging c, inserting a ])
 
   describe "diffTermPair" $ do
     prop "produces an Insert when the first term is missing" $ do
-      \ after -> let diff = diffTermPair (That after) :: Diff ListableSyntax (Record '[]) (Record '[]) in
+      \ after -> let diff = diffTermPair (That after) :: Diff ListableSyntax (DiffAnnotation ()) (DiffAnnotation ()) in
         diff `shouldBe` inserting after
 
     prop "produces a Delete when the second term is missing" $ do
-      \ before -> let diff = diffTermPair (This before) :: Diff ListableSyntax (Record '[]) (Record '[]) in
+      \ before -> let diff = diffTermPair (This before) :: Diff ListableSyntax (DiffAnnotation ()) (DiffAnnotation ()) in
         diff `shouldBe` deleting before
 
 
@@ -81,3 +81,6 @@ afterTerm :: (Foldable syntax, Mergeable syntax) => Diff syntax ann1 ann2 -> May
 afterTerm = cata $ \ diff -> case diff of
   Patch patch -> (after patch >>= \ (In     b  r) -> termIn b <$> sequenceAlt r) <|> (before patch >>= asum)
   Merge                             (In (_, b) r) -> termIn b <$> sequenceAlt r
+
+emptyAnnotation :: DiffAnnotation ()
+emptyAnnotation = ((), Location emptyRange emptySpan)
