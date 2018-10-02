eda36d53312cce2ab8842fa20231cad5fefabaa8
Charlie Somerville <charlie@charlie.bz>
2018-10-02 11:05:40 +1000

    remove collectingTerms

--- src/Analysis/Abstract/Collecting.hs
+++ src/Analysis/Abstract/Collecting.hs
@@ -1,25 +1,10 @@
 {-# LANGUAGE TypeOperators #-}
 module Analysis.Abstract.Collecting
-( collectingTerms
-, providingLiveSet
+( providingLiveSet
 ) where
 
 import Control.Abstract
 import Prologue
-
--- | An analysis performing GC after every instruction.
-collectingTerms :: ( Member (Reader (Live address)) effects
-                   , Member (State (Heap address value)) effects
-                   , Ord address
-                   , ValueRoots address value
-                   )
-                => SubtermAlgebra (Base term) term (Evaluator term address value effects value)
-                -> SubtermAlgebra (Base term) term (Evaluator term address value effects value)
-collectingTerms recur term = do
-  roots <- askRoots
-  v <- recur term
-  v <$ gc (roots <> valueRoots v)
-
 
 providingLiveSet :: PureEffects effects => Evaluator term address value (Reader (Live address) ': effects) a -> Evaluator term address value effects a
 providingLiveSet = runReader lowerBound
