82326ea23775128783f2e3f1546785f3cf691751
Rob Rix <rob.rix@me.com>
2018-10-22 12:57:51 -0400

    Simplify the collecting carriers.

--- src/Analysis/Abstract/Collecting.hs
+++ src/Analysis/Abstract/Collecting.hs
@@ -5,5 +5,5 @@ module Analysis.Abstract.Collecting
 import Control.Abstract
 import Prologue
 
-providingLiveSet :: Carrier sig m => Evaluator term address value (ReaderC (Live address) (Evaluator term address value m)) a -> Evaluator term address value m a
-providingLiveSet = runReader lowerBound . runEvaluator
+providingLiveSet :: Carrier sig m => Evaluator term address value (ReaderC (Live address) (Eff m)) a -> Evaluator term address value m a
+providingLiveSet = raiseHandler (runReader lowerBound)
