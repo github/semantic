92854ee26606d98066e62940e7a4409b750d42a6
Rob Rix <rob.rix@me.com>
2018-10-25 16:42:53 -0400

    Re-export Control.Abstract.ScopeGraph from Control.Abstract.

--- src/Control/Abstract.hs
+++ src/Control/Abstract.hs
@@ -10,4 +10,5 @@ import Control.Abstract.Hole as X
 import Control.Abstract.Modules as X
 import Control.Abstract.Primitive as X
 import Control.Abstract.Roots as X
+import Control.Abstract.ScopeGraph as X
 import Control.Abstract.Value as X
