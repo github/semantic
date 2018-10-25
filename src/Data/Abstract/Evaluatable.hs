097af57735133be914c377204c5a9508c3e11eed
Rob Rix <rob.rix@me.com>
2018-10-25 17:34:04 -0400

    Factor the interposes into a runInTerm helper.

--- src/Data/Abstract/Evaluatable.hs
+++ src/Data/Abstract/Evaluatable.hs
@@ -5,6 +5,9 @@ module Data.Abstract.Evaluatable
 , ModuleC
 , ValueC
 , evaluate
+, runInModule
+, runInTerm
+, evaluateModules
 , traceResolve
 -- * Preludes
 , HasPrelude(..)
@@ -84,29 +87,29 @@ class (Show1 constr, Foldable constr) => Evaluatable constr where
 
 
 type ModuleC address value m
-  = EavesdropC (Modules address)                                  (Eff
-  ( InterposeC (Resumable (BaseError (UnspecializedError value))) (Eff
-  ( ErrorC (LoopControl address)                                  (Eff
-  ( ErrorC (Return address)                                       (Eff
-  ( EnvC address                                                  (Eff
-  ( ScopeEnvC address                                             (Eff
-  ( DerefC address value                                          (Eff
-  ( AllocatorC address                                            (Eff
-  ( ReaderC ModuleInfo                                            (Eff
-    m)))))))))))))))))
+  = ErrorC (LoopControl address) (Eff
+  ( ErrorC (Return address)      (Eff
+  ( EnvC address                 (Eff
+  ( ScopeEnvC address            (Eff
+  ( DerefC address value         (Eff
+  ( AllocatorC address           (Eff
+  ( ReaderC ModuleInfo           (Eff
+    m)))))))))))))
 
 type ValueC term address value m
-  = FunctionC term address value (Eff
-  ( WhileC value                 (Eff
-  ( BooleanC value               (Eff
-    m)))))
+  = FunctionC term address value                                  (Eff
+  ( WhileC value                                                  (Eff
+  ( BooleanC value                                                (Eff
+  ( EavesdropC (Modules address)                                  (Eff
+  ( InterposeC (Resumable (BaseError (UnspecializedError value))) (Eff
+    m)))))))))
 
 evaluate :: ( AbstractValue term address value valueC
             , Carrier sig c
             , allocatorC ~ AllocatorC address (Eff (ReaderC ModuleInfo (Eff c)))
             , Carrier (Allocator address :+: Reader ModuleInfo :+: sig) allocatorC
             , Carrier (Deref value :+: Allocator address :+: Reader ModuleInfo :+: sig) (DerefC address value (Eff allocatorC))
-            , booleanC ~ BooleanC value (Eff moduleC)
+            , booleanC ~ BooleanC value (Eff (EavesdropC (Modules address) (Eff (InterposeC (Resumable (BaseError (UnspecializedError value))) (Eff moduleC)))))
             , Carrier (Boolean value :+: moduleSig) booleanC
             , whileC ~ WhileC value (Eff booleanC)
             , moduleSig ~ (Eavesdrop (Modules address) :+: Interpose (Resumable (BaseError (UnspecializedError value))) :+: Error (LoopControl address) :+: Error (Return address) :+: Env address :+: ScopeEnv address :+: Deref value :+: Allocator address :+: Reader ModuleInfo :+: sig)
@@ -141,15 +144,13 @@ evaluate :: ( AbstractValue term address value valueC
          -> [Module term]
          -> Evaluator term address value c (ModuleTable (NonEmpty (Module (ModuleResult address))))
 evaluate lang analyzeModule analyzeTerm modules = do
-  (_, (preludeBinds, _)) <- runInModule lowerBound moduleInfoFromCallStack . runValue $ do
+  (_, (preludeBinds, _)) <- runInModule lowerBound moduleInfoFromCallStack . runInTerm evalTerm $ do
     definePrelude lang
     box unit
   evaluateModules (run preludeBinds <$> modules)
-  where run preludeBinds m = (<$ m) <$> runInModule preludeBinds (moduleInfo m) (analyzeModule (runValue . evalTerm . moduleBody) m)
+  where run preludeBinds m = (<$ m) <$> runInModule preludeBinds (moduleInfo m) (analyzeModule (runInTerm evalTerm . evalTerm . moduleBody) m)
 
         evalTerm = fix (analyzeTerm (\ ev -> eval ev . project)) >=> address
-
-        runValue = runBoolean . runWhile . runFunction evalTerm
 
 runInModule :: ( Carrier sig m
                , allocatorC ~ (AllocatorC address (Eff (ReaderC ModuleInfo (Eff m))))
@@ -158,8 +159,6 @@ runInModule :: ( Carrier sig m
                , Carrier (Deref value :+: allocatorSig) (DerefC address value (Eff allocatorC))
                , Effect sig
                , Member Fresh sig
-               , Member (Modules address) sig
-               , Member (Resumable (BaseError (UnspecializedError value))) sig
                , Ord address
                )
             => Bindings address
@@ -174,8 +173,24 @@ runInModule prelude info
   . runEnv (EvalContext Nothing (X.push (newEnv prelude)))
   . runReturn
   . runLoopControl
-  . raiseHandler runInterpose
-  . raiseHandler runEavesdrop
+
+runInTerm :: ( Carrier sig m
+             , booleanC ~ BooleanC value (Eff (EavesdropC (Modules address) (Eff (InterposeC (Resumable (BaseError (UnspecializedError value))) (Eff m)))))
+             , booleanSig ~ (Boolean value :+: Eavesdrop (Modules address) :+: Interpose (Resumable (BaseError (UnspecializedError value))) :+: sig)
+             , Carrier booleanSig booleanC
+             , whileC ~ WhileC value (Eff booleanC)
+             , whileSig ~ (While value :+: booleanSig)
+             , Carrier whileSig whileC
+             , functionC ~ FunctionC term address value (Eff whileC)
+             , functionSig ~ (Function term address value :+: whileSig)
+             , Carrier functionSig functionC
+             , Member (Modules address) sig
+             , Member (Resumable (BaseError (UnspecializedError value))) sig
+             )
+          => (term -> Evaluator term address value (ValueC term address value m) address)
+          -> Evaluator term address value (ValueC term address value m) a
+          -> Evaluator term address value m a
+runInTerm evalTerm = raiseHandler runInterpose . raiseHandler runEavesdrop . runBoolean . runWhile . runFunction evalTerm
 
 evaluateModules :: ( Carrier sig m
                    , Member (Reader (ModuleTable (NonEmpty (Module (ModuleResult address))))) sig
