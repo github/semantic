4aae4db57d4a1b860fb8255ce4902a46ec95628b
Rob Rix <rob.rix@me.com>
2018-10-26 12:37:26 -0400

    Implement python packaging directly in the carrier.

--- src/Control/Abstract/PythonPackage.hs
+++ src/Control/Abstract/PythonPackage.hs
@@ -38,46 +38,61 @@ runPythonPackaging :: ( Carrier sig m
                       , Member (Reader Span) sig
                       , Member (Function term address (Value term address)) sig
                       )
-                   => Evaluator term address (Value term address) (PythonPackagingC (Function term address (Value term address)) (Eff m)) a
+                   => Evaluator term address (Value term address) (PythonPackagingC term address (Eff m)) a
                    -> Evaluator term address (Value term address) m a
-runPythonPackaging = raiseHandler $ interpose (runEvaluator . \case
-  Call callName super params k -> Evaluator . k =<< do
-    case callName of
-      Closure _ _ name' paramNames _ _ -> do
-        let bindings = foldr (uncurry Map.insert) lowerBound (zip paramNames params)
-        let asStrings = deref >=> asArray >=> traverse (deref >=> asString)
+runPythonPackaging = raiseHandler (runPythonPackagingC . interpret)
 
-        case name' of
-          Just n
-            | name "find_packages" == n -> do
-              as <- maybe (pure mempty) (fmap (fmap stripQuotes) . asStrings) (Map.lookup (name "exclude") bindings)
-              put (FindPackages as)
-            | name "setup" == n -> do
-              packageState <- get
-              if packageState == Unknown then do
-                as <- maybe (pure mempty) (fmap (fmap stripQuotes) . asStrings) (Map.lookup (name "packages") bindings)
-                put (Packages as)
-                else
-                  pure ()
-          _ -> pure ()
-      _ -> pure ()
-    call callName super params
-  Function name params body k -> function name params body >>= Evaluator . k
-  BuiltIn b k -> builtIn b >>= Evaluator . k)
+newtype PythonPackagingC term address m a = PythonPackagingC { runPythonPackagingC :: m a }
 
-interpose :: (Member eff sig, HFunctor eff, Carrier sig m)
-          => (forall v. eff m (m v) -> m v)
-          -> Eff (PythonPackagingC eff m) a
-          -> m a
-interpose handler = runPythonPackagingC handler . interpret
+wrap :: Evaluator term address (Value term address) m a -> PythonPackagingC term address (Eff m) a
+wrap = PythonPackagingC . runEvaluator
 
-newtype PythonPackagingC eff m a = PythonPackagingC ((forall x . eff m (m x) -> m x) -> m a)
-
-runPythonPackagingC :: (forall x . eff m (m x) -> m x) -> PythonPackagingC eff m a -> m a
-runPythonPackagingC f (PythonPackagingC m) = m f
-
-instance (Member eff sig, HFunctor eff, Carrier sig m) => Carrier sig (PythonPackagingC eff m) where
-  ret a = PythonPackagingC (const (ret a))
+instance ( Carrier sig m
+         , Member (Allocator address) sig
+         , Member (Boolean (Value term address)) sig
+         , Member (Deref (Value term address)) sig
+         , Member (Env address) sig
+         , Member (Error (LoopControl address)) sig
+         , Member (Error (Return address)) sig
+         , Member Fresh sig
+         , Member (Function term address (Value term address)) sig
+         , Member (Reader ModuleInfo) sig
+         , Member (Reader PackageInfo) sig
+         , Member (Reader Span) sig
+         , Member (Resumable (BaseError (AddressError address (Value term address)))) sig
+         , Member (Resumable (BaseError (ValueError term address))) sig
+         , Member (State (Heap address (Value term address))) sig
+         , Member (State Strategy) sig
+         , Member Trace sig
+         , Ord address
+         , Show address
+         , Show term
+         )
+      => Carrier sig (PythonPackagingC term address (Eff m)) where
+  ret = PythonPackagingC . ret
   eff op
-    | Just e <- prj op = PythonPackagingC (\ handler -> handler (handlePure (runPythonPackagingC handler) e))
-    | otherwise        = PythonPackagingC (\ handler -> eff (handlePure (runPythonPackagingC handler) op))
+    | Just e <- prj op = wrap $ case handleCoercible e of
+      Call callName super params k -> Evaluator . k =<< do
+        case callName of
+          Closure _ _ name' paramNames _ _ -> do
+            let bindings = foldr (uncurry Map.insert) lowerBound (zip paramNames params)
+            let asStrings = deref >=> asArray >=> traverse (deref >=> asString)
+
+            case name' of
+              Just n
+                | name "find_packages" == n -> do
+                  as <- maybe (pure mempty) (fmap (fmap stripQuotes) . asStrings) (Map.lookup (name "exclude") bindings)
+                  put (FindPackages as)
+                | name "setup" == n -> do
+                  packageState <- get
+                  if packageState == Unknown then do
+                    as <- maybe (pure mempty) (fmap (fmap stripQuotes) . asStrings) (Map.lookup (name "packages") bindings)
+                    put (Packages as)
+                    else
+                      pure ()
+              _ -> pure ()
+          _ -> pure ()
+        call callName super params
+      Function name params body k -> function name params body >>= Evaluator . k
+      BuiltIn b k -> builtIn b >>= Evaluator . k
+    | otherwise        = PythonPackagingC (eff (handleCoercible op))
