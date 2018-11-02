cd3030d2bf74bbab63ac0d57fd3055418bd290b4
Timothy Clem <timothy.clem@gmail.com>
2018-11-02 13:55:30 -0700

    Introduce some symbol/tags tests

---
+++ test/Tags/Spec.hs
@@ -0,0 +1,26 @@
+module Tags.Spec (spec) where
+
+import Tags.Tagging
+import SpecHelpers
+
+
+spec :: Spec
+spec = parallel $ do
+  describe "ruby" $ do
+    it "produces tags for methods" $ do
+      (blob, tree) <- parseTestFile rubyParser "test/fixtures/ruby/tags/simple_method.rb"
+      runTagging blob tree `shouldBe` Right
+        [ Tag "foo" "Method" (Span (Pos 1 1) (Pos 2 4)) ["Statements"] (Just "def foo") Nothing ]
+
+    it "produces tags for methods with docs" $ do
+      (blob, tree) <- parseTestFile rubyParser "test/fixtures/ruby/tags/simple_method_with_docs.rb"
+      runTagging blob tree `shouldBe` Right
+        [ Tag "foo" "Method" (Span (Pos 2 1) (Pos 3 4)) ["Statements"] (Just "def foo") (Just "# Public: foo") ]
+
+    it "produces tags for methods and classes" $ do
+      (blob, tree) <- parseTestFile rubyParser "test/fixtures/ruby/tags/class_module.rb"
+      runTagging blob tree `shouldBe` Right
+        [ Tag "Foo" "Module" (Span (Pos 2 1 ) (Pos 12 4)) ["Statements"] (Just "module Foo") (Just "# Public: Foo")
+        , Tag "Bar" "Class" (Span (Pos 5 3 ) (Pos 11 6)) ["Module", "Context", "Statements"] (Just "class Bar") (Just "# Public: Bar")
+        , Tag "baz" "Method" (Span (Pos 8 5 ) (Pos 10 8)) ["Class", "Context", "Module", "Context", "Statements"] (Just "def baz(a)") (Just "# Public: baz")
+        ]
---
+++ test/fixtures/ruby/tags/class_module.rb
@@ -0,0 +1,12 @@
+# Public: Foo
+module Foo
+
+  # Public: Bar
+  class Bar
+
+    # Public: baz
+    def baz(a)
+      a * 10
+    end
+  end
+end
