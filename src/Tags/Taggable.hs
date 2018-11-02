b462c7b064dc17c69a22115e3185ac45adbacffd
Timothy Clem <timothy.clem@gmail.com>
2018-11-01 17:16:34 -0700

    No need for State in this fold

--- src/Tags/Taggable.hs
+++ src/Tags/Taggable.hs
@@ -47,28 +47,26 @@ import qualified Language.TypeScript.Syntax as TypeScript
 data Token
   = Enter { tokenName :: Text, tokenSnippetRange :: Maybe Range }
   | Exit  { tokenName :: Text, tokenSnippetRange :: Maybe Range}
-  | Iden { identifierName :: Text, tokenSpan :: Span, docsLiteralRange :: Maybe Range }
+  | Iden  { identifierName :: Text, tokenSpan :: Span, docsLiteralRange :: Maybe Range }
   deriving (Eq, Show)
 
 data Tagger a where
   Pure :: a -> Tagger a
   Bind :: Tagger a -> (a -> Tagger b) -> Tagger b
   Tell :: Token -> Tagger ()
-  Get :: Tagger InternalState
-  Put :: InternalState -> Tagger ()
 
-compile :: InternalState -> Tagger a -> Machine.Plan k Token (InternalState, a)
-compile p = \case
-  Pure a   -> pure (p, a)
-  Bind a f -> compile p a >>= (\(new, v) -> compile new (f v))
-  Tell t   -> Machine.yield t $> (p, ())
-  Get      -> pure (p, p)
-  Put p'   -> pure (p', ())
+compile :: Tagger a -> Machine.Plan k Token a
+compile = \case
+  Pure a   -> pure a
+  Bind a f -> compile a >>= compile . f
+  Tell t   -> Machine.yield t $> ()
 
 instance Functor Tagger where fmap = liftA
+
 instance Applicative Tagger where
   pure  = Pure
   (<*>) = ap
+
 instance Monad Tagger where (>>=) = Bind
 
 enter, exit :: String -> Maybe Range -> Tagger ()
@@ -77,19 +75,6 @@ exit c = Tell . Exit (pack c)
 
 emitIden :: Span -> Maybe Range -> Name -> Tagger ()
 emitIden span docsLiteralRange name = Tell (Iden (formatName name) span docsLiteralRange)
-
-data InternalState
-  = InternalState
-  { _location  :: Location
-  , _language :: Language
-  }
-
- -- InternalState handling
-asks :: (InternalState -> a) -> Tagger a
-asks f = f <$> Get
-
--- modify :: (InternalState -> InternalState) -> Tagger ()
--- modify f = Get >>= \x -> Put . f $! x
 
 class (Show1 constr, Traversable constr) => Taggable constr where
   docsLiteral ::
@@ -102,12 +87,6 @@ class (Show1 constr, Traversable constr) => Taggable constr where
 
   snippet :: Location -> constr (Term syntax Location) -> Maybe Range
   snippet _ _ = Nothing
-
--- withLocation :: Annotated t Location => t -> Tagger a -> Tagger a
--- withLocation t act = do
---   old <- asks location
---   modify (\x -> x { location = annotation t })
---   act <* modify (\x -> x { location = old })
 
 type IsTaggable syntax =
   ( Functor syntax
@@ -125,21 +104,19 @@ tagging :: (IsTaggable syntax)
   -> Term syntax Location
   -> Machine.Source Token
 tagging Blob{..} term = pipe
-  where pipe  = Machine.construct . fmap snd $ compile state go
-        state = InternalState (termAnnotation term) blobLanguage
-        go    = foldSubterms (descend blobLanguage) term
+  where pipe = Machine.construct $ compile go
+        go   = foldSubterms (descend blobLanguage) term
 
-descend :: forall constr syntax.
-  ( Taggable constr
-  , ConstructorName constr
-  , Declarations (constr (Term syntax Location))
+descend ::
+  ( Taggable (TermF syntax Location)
+  , ConstructorName (TermF syntax Location)
+  , Declarations ((TermF syntax Location) (Term syntax Location))
   , Functor syntax
   , Foldable syntax
   , HasTextElement syntax
   )
-  => Language -> SubtermAlgebra constr (Term syntax Location) (Tagger ())
-descend lang t = do
-  (InternalState loc _) <- asks id
+  => Language -> SubtermAlgebra (TermF syntax Location) (Term syntax Location) (Tagger ())
+descend lang t@(In loc _) = do
   let term = fmap subterm t
   let snippetRange = snippet loc term
   let litRange = docsLiteral lang term
@@ -160,10 +137,10 @@ instance ( Apply Show1 fs, Apply Functor fs, Apply Foldable fs, Apply Traversabl
 
 instance (Taggable a) => Taggable (TermF a Location) where
   docsLiteral l t = docsLiteral l (termFOut t)
-  snippet _ t = snippet (termFAnnotation t) (termFOut t)
+  snippet ann t = snippet ann (termFOut t)
 
 instance Taggable Syntax.Context where
-  snippet ann t@(Syntax.Context _ (Term (In sub _))) = Just $ subtractLocation ann sub
+  snippet ann (Syntax.Context _ (Term (In sub _))) = Just $ subtractLocation ann sub
 
 instance Taggable Declaration.Function where
   docsLiteral Python (Declaration.Function _ _ _ (Term (In _ bodyF)))
