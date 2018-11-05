d0fc88efeec14b390dac4dd7a7b520c6bf665fd4
Timothy Clem <timothy.clem@gmail.com>
2018-11-05 12:33:23 -0800

    Some python specs for tags

--- test/Tags/Spec.hs
+++ test/Tags/Spec.hs
@@ -24,3 +24,24 @@ spec = parallel $ do
         , Tag "Bar" "Class"  (Span (Pos 5 3 ) (Pos 11 6)) ["Module", "Context", "Statements"] (Just "class Bar") (Just "# Public: Bar")
         , Tag "baz" "Method" (Span (Pos 8 5 ) (Pos 10 8)) ["Class", "Context", "Module", "Context", "Statements"] (Just "def baz(a)") (Just "# Public: baz")
         ]
+
+  describe "python" $ do
+    it "produces tags for functions" $ do
+      (blob, tree) <- parseTestFile pythonParser "test/fixtures/python/tags/simple_functions.py"
+      runTagging blob tree `shouldBe` Right
+        [ Tag "Foo" "Function" (Span (Pos 1 1) (Pos 5 17)) ["Statements"] (Just "def Foo(x):") Nothing
+        , Tag "Bar" "Function" (Span (Pos 7 1) (Pos 11 13)) ["Statements"] (Just "def Bar():") Nothing
+        , Tag "local" "Function" (Span (Pos 8 5) (Pos 9 17)) ["Statements", "Function", "Statements"] (Just "def local():") Nothing
+        ]
+
+    it "produces tags for functions with docs" $ do
+      (blob, tree) <- parseTestFile pythonParser "test/fixtures/python/tags/simple_function_with_docs.py"
+      runTagging blob tree `shouldBe` Right
+        [ Tag "Foo" "Function" (Span (Pos 1 1) (Pos 3 13)) ["Statements"] (Just "def Foo(x):") (Just "\"\"\"This is the foo function\"\"\"") ]
+
+    it "produces tags for classes" $ do
+      (blob, tree) <- parseTestFile pythonParser "test/fixtures/python/tags/class.py"
+      runTagging blob tree `shouldBe` Right
+        [ Tag "Foo" "Class" (Span (Pos 1 1) (Pos 5 17)) ["Statements"] (Just "class Foo:") (Just "\"\"\"The Foo class\"\"\"")
+        , Tag "f" "Function" (Span (Pos 3 5) (Pos 5 17)) ["Statements", "Class", "Statements"] (Just "def f(self):") (Just "\"\"\"The f method\"\"\"")
+        ]
