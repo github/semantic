6dee04f1dd63ca541a86f3e1a0b8d5c0c280bbd4
Timothy Clem <timothy.clem@gmail.com>
2018-11-05 07:57:21 -0800

    Space and docs

--- test/Tags/Spec.hs
+++ test/Tags/Spec.hs
@@ -17,10 +17,10 @@ spec = parallel $ do
       runTagging blob tree `shouldBe` Right
         [ Tag "foo" "Method" (Span (Pos 2 1) (Pos 3 4)) ["Statements"] (Just "def foo") (Just "# Public: foo") ]
 
-    it "produces tags for methods and classes" $ do
+    it "produces tags for methods and classes with docs" $ do
       (blob, tree) <- parseTestFile rubyParser "test/fixtures/ruby/tags/class_module.rb"
       runTagging blob tree `shouldBe` Right
         [ Tag "Foo" "Module" (Span (Pos 2 1 ) (Pos 12 4)) ["Statements"] (Just "module Foo") (Just "# Public: Foo")
-        , Tag "Bar" "Class" (Span (Pos 5 3 ) (Pos 11 6)) ["Module", "Context", "Statements"] (Just "class Bar") (Just "# Public: Bar")
+        , Tag "Bar" "Class"  (Span (Pos 5 3 ) (Pos 11 6)) ["Module", "Context", "Statements"] (Just "class Bar") (Just "# Public: Bar")
         , Tag "baz" "Method" (Span (Pos 8 5 ) (Pos 10 8)) ["Class", "Context", "Module", "Context", "Statements"] (Just "def baz(a)") (Just "# Public: baz")
         ]
