f39da9578a1ffd56befadc7decb0dcda6c98f4a3
joshvera <josh@joshvera.com>
2018-11-16 16:09:11 -0500

    Move ImportPath to Data.ImportPath

--- src/Language/TypeScript/Resolution.hs
+++ src/Language/TypeScript/Resolution.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DeriveAnyClass #-}
 module Language.TypeScript.Resolution
   ( ImportPath (..)
   , IsRelative (..)
@@ -12,14 +11,7 @@ module Language.TypeScript.Resolution
   , typescriptExtensions
   ) where
 
-import Prologue
-
-import           Data.Aeson
 import qualified Data.Map as Map
-import qualified Data.Text as T
-import           Proto3.Suite
-import qualified Proto3.Wire.Decode as Decode
-import qualified Proto3.Wire.Encode as Encode
 import           System.FilePath.Posix
 
 import           Data.Abstract.BaseError
@@ -28,38 +20,7 @@ import qualified Data.Abstract.Module as M
 import           Data.Abstract.Package
 import           Data.Abstract.Path
 import qualified Data.Language as Language
-
-data IsRelative = Unknown | Relative | NonRelative
-  deriving (Bounded, Enum, Finite, MessageField, Named, Eq, Generic, Hashable, Ord, Show, ToJSON, NFData)
-
-instance Primitive IsRelative where
-  encodePrimitive = Encode.enum
-  decodePrimitive = either (const def) id <$> Decode.enum
-  primType _ = Named (Single (nameOf (Proxy @IsRelative)))
-
-instance HasDefault IsRelative where
-  def = Unknown
-
-data ImportPath = ImportPath { unPath :: FilePath, pathIsRelative :: IsRelative }
-  deriving (Eq, Generic, Hashable, Message, Named, Ord, Show, ToJSON, NFData)
-
-instance MessageField ImportPath where
-  encodeMessageField num = Encode.embedded num . encodeMessage (fieldNumber 1)
-  decodeMessageField = fromMaybe def <$> Decode.embedded (decodeMessage (fieldNumber 1))
-  protoType _ = messageField (Prim $ Named (Single (nameOf (Proxy @ImportPath)))) Nothing
-
-instance HasDefault ImportPath where
-  def = ImportPath mempty Relative
-
--- TODO: fix the duplication present in this and Python
-importPath :: Text -> ImportPath
-importPath str = let path = stripQuotes str in ImportPath (T.unpack path) (pathType path)
-  where
-    pathType xs | not (T.null xs), T.head xs == '.' = Relative -- TODO: fix partiality
-                | otherwise = NonRelative
-
-toName :: ImportPath -> Name
-toName = name . T.pack . unPath
+import Data.ImportPath
 
 -- Node.js resolution algorithm: https://nodejs.org/api/modules.html#modules_all_together
 --
