f52babc63939752d4b5d31dab1bd676f046d3065
joshvera <josh@joshvera.com>
2018-11-29 12:31:42 -0500

    Fix type errors

--- test/Analysis/PHP/Spec.hs
+++ test/Analysis/PHP/Spec.hs
@@ -13,27 +13,27 @@ spec :: TaskConfig -> Spec
 spec config = parallel $ do
   describe "PHP" $ do
     it "evaluates include and require" $ do
-      ((scopeGraph, heap) res) <- evaluate ["main.php", "foo.php", "bar.php"]
+      (scopeGraph, (heap, res)) <- evaluate ["main.php", "foo.php", "bar.php"]
       case ModuleTable.lookup "main.php" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef))) :| [])) -> do
+        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
           valueRef `shouldBe` Rval unit
           const () <$> SpecHelpers.lookupDeclaration "bar" scopeAndFrame heap scopeGraph `shouldBe` Just ()
           const () <$> SpecHelpers.lookupDeclaration "foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
         other -> expectationFailure (show other)
 
     it "evaluates include_once and require_once" $ do
-      (_, res) <- evaluate ["main_once.php", "foo.php", "bar.php"]
+      (scopeGraph, (heap, res)) <- evaluate ["main_once.php", "foo.php", "bar.php"]
       case ModuleTable.lookup "main_once.php" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef))) :| [])) -> do
+        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
           valueRef `shouldBe` Rval unit
           const () <$> SpecHelpers.lookupDeclaration "bar" scopeAndFrame heap scopeGraph `shouldBe` Just ()
           const () <$> SpecHelpers.lookupDeclaration "foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
         other -> expectationFailure (show other)
 
     it "evaluates namespaces" $ do
-      (_, res) <- evaluate ["namespaces.php"]
+      (scopeGraph, (heap, res)) <- evaluate ["namespaces.php"]
       case ModuleTable.lookup "namespaces.php" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef))) :| [])) -> do
+        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
           const () <$> SpecHelpers.lookupDeclaration "Foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
           const () <$> SpecHelpers.lookupDeclaration "NS1" scopeAndFrame heap scopeGraph `shouldBe` Just ()
 
