4b0a88881b372e9bdcecc178d7a7b990e35f8234
joshvera <josh@joshvera.com>
2018-12-04 10:36:23 -0500

    Expect dang method on Baz
    And to choose the correct superclass method

--- test/Analysis/Python/Spec.hs
+++ test/Analysis/Python/Spec.hs
@@ -65,7 +65,8 @@ spec config = parallel $ do
       (scopeGraph, (heap, res)) <- evaluate ["multiple_inheritance.py"]
       case ModuleTable.lookup "multiple_inheritance.py" <$> res of
         Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
-          const () <$> SpecHelpers.lookupDeclaration undefined scopeAndFrame heap scopeGraph `shouldBe` Just ()
+          SpecHelpers.lookupObjectMembers "Baz" scopeAndFrame heap scopeGraph `shouldBe` Just [ "dang" ]
+          valueRef `shouldBe` Rval (String "\"bar!\"")
         other -> expectationFailure (show other)
 
   where
