4b2022fb9deae67ba14ebdc6c85361f4b5d8f6f5
Rob Rix <rob.rix@me.com>
2018-12-06 10:38:59 -0500

    :fire: commented-out functionality.

--- src/Data/Abstract/Heap.hs
+++ src/Data/Abstract/Heap.hs
@@ -144,41 +144,6 @@ isHeapEmpty h@Heap{..} = heapSize h == 1 &&
   where
     toEmptyFrame Frame{..} = Frame () (Map.mapKeysMonotonic (const ()) <$> links) slots
 
--- -- | A map of addresses onto cells holding their values.
--- newtype Heap address address value = Heap { unHeap :: Monoidal.Map address (Set value) }
---   deriving (Eq, Foldable, Lower, Monoid, Ord, Semigroup)
-
--- -- | Look up the cell of values for an 'Address' in a 'Heap', if any.
--- heapLookup :: Ord address => address -> Heap address address value -> Maybe (Set value)
--- heapLookup address = Monoidal.lookup address . unHeap
-
--- -- | Look up the list of values stored for a given address, if any.
--- heapLookupAll :: Ord address => address -> Heap address address value -> Maybe [value]
--- heapLookupAll address = fmap toList . heapLookup address
-
--- -- | Append a value onto the cell for a given address, inserting a new cell if none existed.
--- heapInsert :: (Ord address, Ord value) => address -> value -> Heap address address value -> Heap address address value
--- heapInsert address value = flip snoc (address, value)
-
--- -- | Manually insert a cell into the heap at a given address.
--- heapInit :: Ord address => address -> Set value -> Heap address address value -> Heap address address value
--- heapInit address cell (Heap h) = Heap (Monoidal.insert address cell h)
-
--- -- | The number of addresses extant in a 'Heap'.
--- heapSize :: Heap address address value -> Int
--- heapSize = Monoidal.size . unHeap
-
--- -- | Restrict a 'Heap' to only those addresses in the given 'Live' set (in essence garbage collecting the rest).
--- heapRestrict :: Ord address => Heap address address value -> Live address -> Heap address address value
--- heapRestrict (Heap m) roots = Heap (Monoidal.filterWithKey (\ address _ -> address `liveMember` roots) m)
-
--- heapDelete :: Ord address => address -> Heap address address value -> Heap address address value
--- heapDelete addr = Heap . Monoidal.delete addr . unHeap
-
--- instance (Ord address, Ord value) => Reducer (address, value) (Heap address address value) where
---   unit = Heap . unit
---   cons (addr, a) (Heap heap) = Heap (cons (addr, a) heap)
---   snoc (Heap heap) (addr, a) = Heap (snoc heap (addr, a))
 
 instance (Show address, Show value) => Show (Heap address address value) where
   showsPrec d = showsUnaryWith showsPrec "Heap" d . Map.toList . heap
