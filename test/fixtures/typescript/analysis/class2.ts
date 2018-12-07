c4cd4e00d0b8a342365296875aef011476534434
joshvera <josh@joshvera.com>
2018-12-07 16:55:38 -0500

    Add test files for new expressions

---
+++ test/fixtures/typescript/analysis/class2.ts
@@ -0,0 +1,11 @@
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+export { Greeter }
