d0f7433690f1621cc891d8a89c86417ef060ee3e
Rob Rix <rob.rix@me.com>
2018-12-07 15:23:03 -0500

    Module results hold values.

--- test/Analysis/PHP/Spec.hs
+++ test/Analysis/PHP/Spec.hs
@@ -14,8 +14,8 @@ spec config = parallel $ do
     xit "evaluates include and require" $ do
       (scopeGraph, (heap, res)) <- evaluate ["main.php", "foo.php", "bar.php"]
       case ModuleTable.lookup "main.php" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
-          valueRef `shouldBe` Rval unit
+        Right (Just (Module _ (scopeAndFrame, value) :| [])) -> do
+          value `shouldBe` unit
           const () <$> SpecHelpers.lookupDeclaration "bar" scopeAndFrame heap scopeGraph `shouldBe` Just ()
           const () <$> SpecHelpers.lookupDeclaration "foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
         other -> expectationFailure (show other)
@@ -23,8 +23,8 @@ spec config = parallel $ do
     xit "evaluates include_once and require_once" $ do
       (scopeGraph, (heap, res)) <- evaluate ["main_once.php", "foo.php", "bar.php"]
       case ModuleTable.lookup "main_once.php" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
-          valueRef `shouldBe` Rval unit
+        Right (Just (Module _ (scopeAndFrame, value) :| [])) -> do
+          value `shouldBe` unit
           const () <$> SpecHelpers.lookupDeclaration "bar" scopeAndFrame heap scopeGraph `shouldBe` Just ()
           const () <$> SpecHelpers.lookupDeclaration "foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
         other -> expectationFailure (show other)
@@ -32,7 +32,7 @@ spec config = parallel $ do
     xit "evaluates namespaces" $ do
       (scopeGraph, (heap, res)) <- evaluate ["namespaces.php"]
       case ModuleTable.lookup "namespaces.php" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
+        Right (Just (Module _ (scopeAndFrame, value) :| [])) -> do
           const () <$> SpecHelpers.lookupDeclaration "Foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
           const () <$> SpecHelpers.lookupDeclaration "NS1" scopeAndFrame heap scopeGraph `shouldBe` Just ()
 
--- test/Analysis/Ruby/Spec.hs
+++ test/Analysis/Ruby/Spec.hs
@@ -20,16 +20,16 @@ spec config = parallel $ do
     it "evaluates require_relative" $ do
       (scopeGraph, (heap, res)) <- evaluate ["main.rb", "foo.rb"]
       case ModuleTable.lookup "main.rb" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
-          valueRef `shouldBe` Rval (Value.Integer (Number.Integer 1))
+        Right (Just (Module _ (scopeAndFrame, value) :| [])) -> do
+          value `shouldBe` Value.Integer (Number.Integer 1)
           () <$ SpecHelpers.lookupDeclaration "foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
         other -> expectationFailure (show other)
 
     it "evaluates load" $ do
       (scopeGraph, (heap, res)) <- evaluate ["load.rb", "foo.rb"]
       case ModuleTable.lookup "load.rb" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
-          valueRef `shouldBe` Rval (Value.Integer (Number.Integer 1))
+        Right (Just (Module _ (scopeAndFrame, value) :| [])) -> do
+          value `shouldBe` Value.Integer (Number.Integer 1)
           () <$ SpecHelpers.lookupDeclaration "foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
         other -> expectationFailure (show other)
 
@@ -40,8 +40,8 @@ spec config = parallel $ do
     it "evaluates subclass" $ do
       (scopeGraph, (heap, res)) <- evaluate ["subclass.rb"]
       case ModuleTable.lookup "subclass.rb" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
-          valueRef `shouldBe` Rval (String "\"<bar>\"")
+        Right (Just (Module _ (scopeAndFrame, value) :| [])) -> do
+          value `shouldBe` String "\"<bar>\""
           () <$ SpecHelpers.lookupDeclaration "Bar" scopeAndFrame heap scopeGraph `shouldBe` Just ()
           () <$ SpecHelpers.lookupDeclaration "Foo" scopeAndFrame heap scopeGraph `shouldBe` Just ()
           SpecHelpers.lookupMembers "Bar" Superclass scopeAndFrame heap scopeGraph `shouldBe` Just ["baz", "foo", "inspect"]
@@ -50,62 +50,55 @@ spec config = parallel $ do
     it "evaluates modules" $ do
       (scopeGraph, (heap, res)) <- evaluate ["modules.rb"]
       case ModuleTable.lookup "modules.rb" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
+        Right (Just (Module _ (scopeAndFrame, value) :| [])) -> do
           const () <$> SpecHelpers.lookupDeclaration "Bar" scopeAndFrame heap scopeGraph `shouldBe` Just ()
         other -> expectationFailure (show other)
 
     it "handles break correctly" $ do
       (_, (_, res)) <- evaluate ["break.rb"]
       case ModuleTable.lookup "break.rb" <$> res of
-        Right (Just (Module _ (_, valueRef) :| [])) -> valueRef `shouldBe` Rval (Value.Integer (Number.Integer 3))
+        Right (Just (Module _ (_, value) :| [])) -> value `shouldBe` Value.Integer (Number.Integer 3)
         other                                       -> expectationFailure (show other)
 
     it "handles next correctly" $ do
       (_, (_, res)) <- evaluate ["next.rb"]
       case ModuleTable.lookup "next.rb" <$> res of
-        Right (Just (Module _ (_, valueRef) :| [])) -> valueRef `shouldBe` Rval (Value.Integer (Number.Integer 8))
+        Right (Just (Module _ (_, value) :| [])) -> value `shouldBe` Value.Integer (Number.Integer 8)
         other                                       -> expectationFailure (show other)
 
     it "calls functions with arguments" $ do
       (_, (_, res)) <- evaluate ["call.rb"]
       case ModuleTable.lookup "call.rb" <$> res of
-        Right (Just (Module _ (_, valueRef) :| [])) -> valueRef `shouldBe` Rval (Value.Integer (Number.Integer 579))
+        Right (Just (Module _ (_, value) :| [])) -> value `shouldBe` Value.Integer (Number.Integer 579)
         other                                       -> expectationFailure (show other)
 
     it "evaluates early return statements" $ do
       (_, (_, res)) <- evaluate ["early-return.rb"]
       case ModuleTable.lookup "early-return.rb" <$> res of
-        Right (Just (Module _ (_, valueRef) :| [])) -> valueRef `shouldBe` Rval (Value.Integer (Number.Integer 123))
+        Right (Just (Module _ (_, value) :| [])) -> value `shouldBe` Value.Integer (Number.Integer 123)
         other                                       -> expectationFailure (show other)
 
     it "has prelude" $ do
       (_, (_, res)) <- evaluate ["preluded.rb"]
       case ModuleTable.lookup "preluded.rb" <$> res of
-        Right (Just (Module _ (_, valueRef) :| [])) -> valueRef `shouldBe` Rval (String "\"<foo>\"")
+        Right (Just (Module _ (_, value) :| [])) -> value `shouldBe` String "\"<foo>\""
         other                                       -> expectationFailure (show other)
 
     it "evaluates __LINE__" $ do
       (_, (_, res)) <- evaluate ["line.rb"]
       case ModuleTable.lookup "line.rb" <$> res of
-        Right (Just (Module _ (_, valueRef) :| [])) -> valueRef `shouldBe` Rval (Value.Integer (Number.Integer 4))
+        Right (Just (Module _ (_, value) :| [])) -> value `shouldBe` Value.Integer (Number.Integer 4)
         other                                       -> expectationFailure (show other)
 
     it "resolves builtins used in the prelude" $ do
       (scopeGraph, (heap, res)) <- evaluate ["puts.rb"]
       case ModuleTable.lookup "puts.rb" <$> res of
-        Right (Just (Module _ (scopeAndFrame, valueRef) :| [])) -> do
-          valueRef `shouldBe` Rval Unit
+        Right (Just (Module _ (scopeAndFrame, value) :| [])) -> do
+          value `shouldBe` Unit
           const () <$> SpecHelpers.lookupDeclaration "puts" scopeAndFrame heap scopeGraph `shouldBe` Just ()
         other -> expectationFailure (show other)
 
   where
     fixtures = "test/fixtures/ruby/analysis/"
     evaluate = evalRubyProject . map (fixtures <>)
-    evalRubyProject files = testEvaluating =<< do
-      action <- evaluateProject' config (Proxy :: Proxy 'Language.Ruby) rubyParser files
-      pure $ do
-        moduleTable <- action
-        for moduleTable (traverse (\ (Module info (scopeAndFrame, valueRef)) -> do
-
-          valueRef' <- raiseHandler (runReader info . runReader emptySpan) (runDeref (value valueRef >>= rvalBox))
-          pure (Module info (scopeAndFrame, valueRef'))))
+    evalRubyProject = testEvaluating <=< evaluateProject' config (Proxy :: Proxy 'Language.Ruby) rubyParser
