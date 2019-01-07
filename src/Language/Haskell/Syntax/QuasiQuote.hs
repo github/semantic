d2639d096b56afe754f6b7f62d18be3ff35f4a0f
Patrick Thomson <patrickt@github.com>
2019-01-07 16:01:42 -0500

    DerivingVia over the Haskell syntax modules.

--- src/Language/Haskell/Syntax/QuasiQuote.hs
+++ src/Language/Haskell/Syntax/QuasiQuote.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveAnyClass, DuplicateRecordFields #-}
+{-# LANGUAGE DeriveAnyClass, DerivingVia, DuplicateRecordFields #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 
 module Language.Haskell.Syntax.QuasiQuote where
@@ -12,46 +12,31 @@ import Proto3.Suite.Class
 
 data QuasiQuotation a = QuasiQuotation { quasiQuotationHead :: a, quasiQuotationBody :: a }
   deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, Named1, Message1, NFData1)
-
-instance Eq1 QuasiQuotation where liftEq = genericLiftEq
-instance Ord1 QuasiQuotation where liftCompare = genericLiftCompare
-instance Show1 QuasiQuotation where liftShowsPrec = genericLiftShowsPrec
+  deriving (Eq1, Show1, Ord1) via Generically QuasiQuotation
 
 instance Evaluatable QuasiQuotation
 
 newtype QuasiQuotationExpressionBody a = QuasiQuotationExpressionBody { name :: Name }
   deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, Named1, Message1, NFData1)
-
-instance Eq1 QuasiQuotationExpressionBody where liftEq = genericLiftEq
-instance Ord1 QuasiQuotationExpressionBody where liftCompare = genericLiftCompare
-instance Show1 QuasiQuotationExpressionBody where liftShowsPrec = genericLiftShowsPrec
+  deriving (Eq1, Show1, Ord1) via Generically QuasiQuotationExpressionBody
 
 instance Evaluatable QuasiQuotationExpressionBody
 
 data QuasiQuotationPattern a = QuasiQuotationPattern
   deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, Named1, Message1, NFData1)
-
-instance Eq1 QuasiQuotationPattern where liftEq = genericLiftEq
-instance Ord1 QuasiQuotationPattern where liftCompare = genericLiftCompare
-instance Show1 QuasiQuotationPattern where liftShowsPrec = genericLiftShowsPrec
+  deriving (Eq1, Show1, Ord1) via Generically QuasiQuotationPattern
 
 instance Evaluatable QuasiQuotationPattern
 
 data QuasiQuotationType a = QuasiQuotationType
   deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, Named1, Message1, NFData1)
-
-instance Eq1 QuasiQuotationType where liftEq = genericLiftEq
-instance Ord1 QuasiQuotationType where liftCompare = genericLiftCompare
-instance Show1 QuasiQuotationType where liftShowsPrec = genericLiftShowsPrec
+  deriving (Eq1, Show1, Ord1) via Generically QuasiQuotationType
 
 instance Evaluatable QuasiQuotationType
 
 data QuasiQuotationDeclaration a = QuasiQuotationDeclaration
   deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, Named1, Message1, NFData1)
-
-instance Eq1 QuasiQuotationDeclaration where liftEq = genericLiftEq
-instance Ord1 QuasiQuotationDeclaration where liftCompare = genericLiftCompare
-instance Show1 QuasiQuotationDeclaration where liftShowsPrec = genericLiftShowsPrec
+  deriving (Eq1, Show1, Ord1) via Generically QuasiQuotationDeclaration
 
 instance Evaluatable QuasiQuotationDeclaration
 
@@ -66,18 +51,12 @@ instance Evaluatable QuasiQuotationQuoter
 
 data QuasiQuotationExpression a = QuasiQuotationExpression
   deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, Named1, Message1, NFData1)
-
-instance Eq1 QuasiQuotationExpression where liftEq = genericLiftEq
-instance Ord1 QuasiQuotationExpression where liftCompare = genericLiftCompare
-instance Show1 QuasiQuotationExpression where liftShowsPrec = genericLiftShowsPrec
+  deriving (Eq1, Show1, Ord1) via Generically QuasiQuotationExpression
 
 instance Evaluatable QuasiQuotationExpression
 
 newtype Splice a = Splice { value :: a }
   deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, Named1, Message1, NFData1)
-
-instance Eq1 Splice where liftEq = genericLiftEq
-instance Ord1 Splice where liftCompare = genericLiftCompare
-instance Show1 Splice where liftShowsPrec = genericLiftShowsPrec
+  deriving (Eq1, Show1, Ord1) via Generically Splice
 
 instance Evaluatable Splice
