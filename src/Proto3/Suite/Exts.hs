a91aa3030c910cad5b31405b65d382bea8909174
Patrick Thomson <patrickt@github.com>
2019-01-08 15:36:33 -0500

    Better documentation for the extensions.

--- src/Proto3/Suite/Exts.hs
+++ src/Proto3/Suite/Exts.hs
@@ -18,13 +18,20 @@ newtype PrimitiveEnum a = PrimitiveEnum a
   deriving stock (Eq, Ord)
   deriving newtype (Bounded, Named, Enum, HasDefault)
 
--- | Piggybacks on top of the 'Enumerated' instance, as the generated code would.
--- This instance will get easier when we have DerivingVia.
+-- | Provides a DerivingVia hook to opt into a sensible definition of 'Primitive'
+-- for a given 'Enum'. Should the decoding fail, the 'HasDefault' instance is used
+-- as a fallback.
 instance forall a. (Enum a, Bounded a, Named a, HasDefault a) => Primitive (PrimitiveEnum a) where
   primType _ = Named (Single (nameOf (Proxy @a)))
   encodePrimitive = Encode.enum
   decodePrimitive = either (const def) id <$> Decode.enum
 
-enumerateUpper :: (IsString string, Show a, Enum a) => string -> [a] -> Proxy a -> [(string, Int)]
+-- | A convenience function for declaring 'Finite' instances. Please note that this does
+-- not map CamelCase to LOUD_SNAKE_CASE, though it will work fine with single-word constructors.
+enumerateUpper :: (IsString string, Show a, Enum a)
+               => string   -- ^ The name of the first field, e.g. "UNKNOWN_MYTYPE. Has value 0.
+               -> [a]      -- ^ Subsequent entries to use; the names will be 'Show'n and capitalized.
+               -> Proxy a
+               -> [(string, Int)]
 enumerateUpper msg as _ = (msg, 0) : fmap (\a -> (go a, fromEnum a)) as
   where go = fromString . fmap toUpper . show
