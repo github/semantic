bb204429a70e9caa25442ee2e907814aa65b39a7
Patrick Thomson <patrickt@github.com>
2019-01-08 15:16:13 -0500

    Make sure RequestContext has the instances it needs.

--- src/Data/GitHub/Request/Context.hs
+++ src/Data/GitHub/Request/Context.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Data.GitHub.Request.Context
   ( RequestContext (..)
   ) where
@@ -11,28 +13,27 @@ import Data.GitHub.Request.Method
 import Data.GitHub.Auth
 import Data.GitHub.IPVersion
 
-
 data RequestContext = RequestContext
-  { requestID :: Text
-  , requestMethod :: RequestMethod
-  , requestURL :: Text
-  , requestIPAddress :: Text
-  , requestIPVersion :: IPVersion
-  , requestV4Int :: Word32
-  , requestV6Int :: ByteString
-  , requestUserAgent :: Text
-  , requestSessionID :: Word32
-  , requestController :: Text
+  { requestID               :: Text
+  , requestMethod           :: RequestMethod
+  , requestURL              :: Text
+  , requestIPAddress        :: Text
+  , requestIPVersion        :: IPVersion
+  , requestV4Int            :: Word32
+  , requestV6Int            :: ByteString
+  , requestUserAgent        :: Text
+  , requestSessionID        :: Word32
+  , requestController       :: Text
   , requestControllerAction :: Text
-  , requestAPIRoute :: Text
-  , requestCategory :: Text
-  , requestFrom :: Text
-  , requestAuth :: AuthTypes
-  , requestClientID :: Text
-  , requestReferrer :: Text
-  , requestCountryCode :: Nested Text
-  , requestCountryName :: Nested Text
-  , requestRegion :: Nested (Wrapped Text)
-  , requestRegionName :: Nested (Wrapped Text)
-  , requestCity :: Nested (Wrapped Text)
-  }
+  , requestAPIRoute         :: Text
+  , requestCategory         :: Text
+  , requestFrom             :: Text
+  , requestAuth             :: AuthTypes
+  , requestClientID         :: Text
+  , requestReferrer         :: Text
+  , requestCountryCode      :: Nested (Wrapped Text)
+  , requestCountryName      :: Nested (Wrapped Text)
+  , requestRegion           :: Nested (Wrapped Text)
+  , requestRegionName       :: Nested (Wrapped Text)
+  , requestCity             :: Nested (Wrapped Text)
+  } deriving (Eq, Show, Generic, Named, Message)
