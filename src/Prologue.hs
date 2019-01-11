716da369fe69c5d454d195f3ce22c6ec8f37feb5
Patrick Thomson <patrickt@github.com>
2019-01-11 12:49:56 -0500

    Add fromRight/fromLeft to Prologue and according hlint rules.
    These are new in base-4.10; I had no idea they existed until Rob
    pointed them out. This adds them to the Prologue and institutes hlint
    rules to suggest when we can use them.

--- src/Prologue.hs
+++ src/Prologue.hs
@@ -16,6 +16,7 @@ import Data.Bits as X
 import Data.ByteString as X (ByteString)
 import Data.Coerce as X
 import Data.Functor.Both as X (Both (Both), runBothWith)
+import Data.Either as X (fromLeft, fromRight)
 import Data.IntMap as X (IntMap)
 import Data.IntSet as X (IntSet)
 import Data.Ix as X (Ix (..))
