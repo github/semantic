ceae60fbf9f97715d30e3a8fac0b7cb5bd24d5f4
Rick Winfrey <rewinfrey@github.com>
2019-01-28 17:31:50 -0800

    Remove access controls Syntax constructors
    Co-Authored-By: Rob Rix <robrix@github.com>

--- src/Data/Syntax/Declaration.hs
+++ src/Data/Syntax/Declaration.hs
@@ -73,33 +73,6 @@ instance Declarations1 Function where
 instance FreeVariables1 Function where
   liftFreeVariables freeVariables f@Function{..} = foldMap freeVariables f `Set.difference` foldMap freeVariables functionParameters
 
-data Public a = Public
-  deriving (Declarations1, Diffable, Eq, Ord, Show, Foldable, Traversable, FreeVariables1, Functor, Generic1, Hashable1, ToJSONFields1, Named1, Message1, NFData1)
-  deriving (Eq1, Show1, Ord1) via Generically Public
-
-instance Evaluatable Public
-
-instance AccessControls1 Public where
-  liftTermToAccessControl _ _ = Just ScopeGraph.Public
-
-data Protected a = Protected
-  deriving (Declarations1, Diffable, Eq, Ord, Show, Foldable, Traversable, FreeVariables1, Functor, Generic1, Hashable1, ToJSONFields1, Named1, Message1, NFData1)
-  deriving (Eq1, Show1, Ord1) via Generically Protected
-
-instance Evaluatable Protected
-
-instance AccessControls1 Protected where
-  liftTermToAccessControl _ _ = Just ScopeGraph.Protected
-
-data Private a = Private
-  deriving (Declarations1, Diffable, Eq, Ord, Show, Foldable, Traversable, FreeVariables1, Functor, Generic1, Hashable1, ToJSONFields1, Named1, Message1, NFData1)
-  deriving (Eq1, Show1, Ord1) via Generically Private
-
-instance Evaluatable Private
-
-instance AccessControls1 Private where
-  liftTermToAccessControl _ _ = Just ScopeGraph.Private
-
 data Method a = Method { methodContext :: [a]
                        , methodAccessControl :: a
                        , methodReceiver :: a
