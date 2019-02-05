704cd60d768571e6827a5e7ae460b21bdcb86c91
Rick Winfrey <rewinfrey@github.com>
2019-02-05 13:56:55 -0800

    Don't need `_ <-` pattern

--- src/Parsing/TreeSitter.hs
+++ src/Parsing/TreeSitter.hs
@@ -85,7 +85,7 @@ parseToAST parseTimeout language Blob{..} = bracket (liftIO TS.ts_parser_new) (l
     Just (Succeeded ast) -> Just ast <$ trace ("tree-sitter: parsing succeeded " <> blobPath)
     Nothing -> do
       trace $ "tree-sitter: parsing timed out " <> blobPath
-      _ <- liftIO $ TS.ts_parser_set_enabled parser (CBool 0)
+      liftIO (TS.ts_parser_set_enabled parser (CBool 0))
       Nothing <$ liftIO (wait parsing)
 
 
