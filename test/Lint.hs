1c270b0f0c7804e187dab115f2b05efcea7b83e3
Patrick Thomson <patrickt@github.com>
2019-02-19 16:38:02 -0500

    Remove otiose Lint.hs

--- test/Lint.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import Language.Haskell.HLint (Severity(..), hlint, suggestionSeverity)
-import System.Exit (exitFailure, exitSuccess)
-
-main :: IO ()
-main = do
-    hints <- hlint [ "src", "services/indexer/src" ]
-    if null (filter ((>= Warning) . suggestionSeverity) hints) then exitSuccess else exitFailure
