256d22c6d280017b716a9893f32884585a62c790
Patrick Thomson <patrickt@github.com>
2019-03-06 10:52:58 -0500

    fix (?) PythonPackaging

--- src/Control/Abstract/PythonPackage.hs
+++ src/Control/Abstract/PythonPackage.hs
@@ -32,29 +32,28 @@ instance ( Carrier sig m
          , Member (Abstract.Array (Value term address)) sig
          )
       => Carrier sig (PythonPackagingC term address m) where
-  -- eff (R other) = PythonPackagingC . eff . handleCoercible $ other
-  -- eff (L op)
-  --   | Just e <- prj op = wrap $ case handleCoercible e of
-  --     Call callName params k -> Evaluator . k =<< do
-  --       case callName of
-  --         Closure _ _ name' _ paramNames _ _ _ -> do
-  --           let bindings = foldr (uncurry Map.insert) lowerBound (zip paramNames params)
-  --           let asStrings = asArray >=> traverse asString
+  eff op
+    | Just e <- prj op = wrap $ case handleCoercible e of
+      Call callName params k -> Evaluator . k =<< do
+        case callName of
+          Closure _ _ name' _ paramNames _ _ _ -> do
+            let bindings = foldr (uncurry Map.insert) lowerBound (zip paramNames params)
+            let asStrings = asArray >=> traverse asString
 
-  --           if Just (name "find_packages") == name' then do
-  --             as <- maybe (pure mempty) (fmap (fmap stripQuotes) . asStrings) (Map.lookup (name "exclude") bindings)
-  --             put (FindPackages as)
-  --           else if Just (name "setup") == name' then do
-  --             packageState <- get
-  --             if packageState == Control.Abstract.PythonPackage.Unknown then do
-  --               as <- maybe (pure mempty) (fmap (fmap stripQuotes) . asStrings) (Map.lookup (name "packages") bindings)
-  --               put (Packages as)
-  --               else
-  --                 pure ()
-  --           else pure ()
-  --         _ -> pure ()
-  --       call callName params
-  --     Function name params body scope k -> function name params body scope >>= Evaluator . k
-  --     BuiltIn n b k -> builtIn n b >>= Evaluator . k
-  --     Bind obj value k -> bindThis obj value >>= Evaluator . k
-  --   | otherwise        = PythonPackagingC (eff (handleCoercible op))
+            if Just (name "find_packages") == name' then do
+              as <- maybe (pure mempty) (fmap (fmap stripQuotes) . asStrings) (Map.lookup (name "exclude") bindings)
+              put (FindPackages as)
+            else if Just (name "setup") == name' then do
+              packageState <- get
+              if packageState == Control.Abstract.PythonPackage.Unknown then do
+                as <- maybe (pure mempty) (fmap (fmap stripQuotes) . asStrings) (Map.lookup (name "packages") bindings)
+                put (Packages as)
+                else
+                  pure ()
+            else pure ()
+          _ -> pure ()
+        call callName params
+      Function name params body scope k -> function name params body scope >>= Evaluator . k
+      BuiltIn n b k -> builtIn n b >>= Evaluator . k
+      Bind obj value k -> bindThis obj value >>= Evaluator . k
+    | otherwise = PythonPackagingC . eff $ handleCoercible op
