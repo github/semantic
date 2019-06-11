598000c3ab25b1dfe58053550e89b81817f43c3e
Rob Rix <robrix@github.com>
2019-06-11 11:59:29 -0400

    Stub in compilation to Core.

--- semantic-python/src/Language/Python/Core.hs
+++ semantic-python/src/Language/Python/Core.hs
@@ -2,7 +2,10 @@ module Language.Python.Core
 ( compileModule
 ) where
 
+import Control.Applicative
+import Data.Core as Core
 import TreeSitter.Python.AST as Py
 
-compileModule :: Py.Module -> ()
-compileModule _ = ()
+compileModule :: Alternative m => Py.Module -> m Core
+compileModule (Module Nothing) = pure Unit
+compileModule (Module (Just _)) = pure Unit
