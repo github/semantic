23e18ade2e4dcef0e3273796fa8a81f629aafccc
Rob Rix <robrix@github.com>
2019-06-11 12:05:06 -0400

    Note a FIXME.

--- semantic-python/src/Language/Python/Core.hs
+++ semantic-python/src/Language/Python/Core.hs
@@ -7,6 +7,7 @@ import Data.Core as Core
 import Prelude hiding (fail)
 import TreeSitter.Python.AST as Py
 
+-- FIXME: we should really try not to fail
 compile :: MonadFail m => Py.Module -> m Core
 compile (Module Nothing) = pure Unit
 compile (Module (Just _)) = pure Unit
