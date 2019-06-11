eb7244236f2cc7028e9436a8a2bdd9f07611e1f0
Rob Rix <robrix@github.com>
2019-06-11 13:09:14 -0400

    Split out instances for PrimaryExpression.

--- semantic-python/src/Language/Python/Core.hs
+++ semantic-python/src/Language/Python/Core.hs
@@ -21,28 +21,39 @@ defaultCompile t = fail $ "compilation unimplemented for " <> show t
 instance (Compile l, Compile r) => Compile (Either l r) where compile = compileSum
 
 instance Compile Py.AssertStatement
+instance Compile Py.Attribute
 instance Compile Py.Await
+instance Compile Py.BinaryOperator
 instance Compile Py.Block
 instance Compile Py.BooleanOperator
 instance Compile Py.BreakStatement
+instance Compile Py.Call
 instance Compile Py.ClassDefinition
 instance Compile Py.ComparisonOperator
 
 instance Compile Py.CompoundStatement where compile = compileSum
 
+instance Compile Py.ConcatenatedString
 instance Compile Py.ConditionalExpression
 instance Compile Py.ContinueStatement
 instance Compile Py.DecoratedDefinition
 instance Compile Py.DeleteStatement
+instance Compile Py.Dictionary
+instance Compile Py.DictionaryComprehension
+instance Compile Py.Ellipsis
 instance Compile Py.ExecStatement
 
 instance Compile Py.Expression where compile = compileSum
 
 instance Compile Py.ExpressionStatement
+instance Compile Py.False
+instance Compile Py.Float
 instance Compile Py.ForStatement
 instance Compile Py.FunctionDefinition
 instance Compile Py.FutureImportStatement
+instance Compile Py.GeneratorExpression
 instance Compile Py.GlobalStatement
+instance Compile Py.Identifier
 
 instance Compile Py.IfStatement where
   compile IfStatement{..} = If <$> compile condition <*> compile consequence <*> case alternative of
@@ -53,24 +64,39 @@ instance Compile Py.IfStatement where
 
 instance Compile Py.ImportFromStatement
 instance Compile Py.ImportStatement
+instance Compile Py.Integer
+instance Compile Py.KeywordIdentifier
 instance Compile Py.Lambda
+instance Compile Py.List
+instance Compile Py.ListComprehension
 
 instance Compile Py.Module where
   compile (Module Nothing) = pure Unit
   compile (Module (Just statements)) = block <$> traverse compile statements
 
 instance Compile Py.NamedExpression
+instance Compile Py.None
 instance Compile Py.NonlocalStatement
 instance Compile Py.NotOperator
+instance Compile Py.ParenthesizedExpression
 instance Compile Py.PassStatement
-instance Compile Py.PrimaryExpression
+
+instance Compile Py.PrimaryExpression where compile = compileSum
+
 instance Compile Py.PrintStatement
 instance Compile Py.ReturnStatement
 instance Compile Py.RaiseStatement
+instance Compile Py.Set
+instance Compile Py.SetComprehension
 
 instance Compile Py.SimpleStatement where compile = compileSum
 
+instance Compile Py.String
+instance Compile Py.Subscript
+instance Compile Py.True
 instance Compile Py.TryStatement
+instance Compile Py.Tuple
+instance Compile Py.UnaryOperator
 instance Compile Py.WhileStatement
 instance Compile Py.WithStatement
 
@@ -95,4 +121,5 @@ instance Compile t => GCompileSum (M1 C c (M1 S s (K1 R t))) where
 -- FIXME: depend on https://github.com/tree-sitter/haskell-tree-sitter/pull/90 so we can get rid of these orphan instances
 deriving instance Generic Py.CompoundStatement
 deriving instance Generic Py.Expression
+deriving instance Generic Py.PrimaryExpression
 deriving instance Generic Py.SimpleStatement
