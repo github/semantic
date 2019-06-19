0f54a5c506bab0e0760895fd823dfe47f6307b28
Rob Rix <robrix@github.com>
2019-06-19 13:36:44 -0400

    Stub in a serialization class for generics over precise ASTs.

--- src/Serializing/SExpression/Precise.hs
+++ src/Serializing/SExpression/Precise.hs
@@ -1,2 +1,13 @@
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, OverloadedStrings #-}
 module Serializing.SExpression.Precise
-() where
+( serializeSExpression
+) where
+
+import Data.ByteString.Builder
+import GHC.Generics
+
+serializeSExpression :: (Generic t, GToSExpression (Rep t)) => t -> Builder
+serializeSExpression t = gtoSExpression (from t) 0 <> "\n"
+
+class GToSExpression f where
+  gtoSExpression :: f (Int -> Builder) -> (Int -> Builder)
