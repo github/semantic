d15b031285e1f82cd639f411db6b32791198674a
Rob Rix <robrix@github.com>
2019-06-19 13:46:31 -0400

    Prepend constructor names.

--- src/Serializing/SExpression/Precise.hs
+++ src/Serializing/SExpression/Precise.hs
@@ -4,13 +4,21 @@ module Serializing.SExpression.Precise
 ) where
 
 import Data.ByteString.Builder
+import Data.Foldable (fold)
+import Data.List (intersperse)
 import GHC.Generics
 
 serializeSExpression :: (Generic t, GToSExpression (Rep t)) => t -> Builder
 serializeSExpression t = gtoSExpression (from t) 0 <> "\n"
 
+gtoSExpression :: GToSExpression f => f (Int -> Builder) -> (Int -> Builder)
+gtoSExpression f n = "(" <> fold (intersperse " " (gtoSExpression' f n)) <> ")"
+
 class GToSExpression f where
-  gtoSExpression :: f (Int -> Builder) -> (Int -> Builder)
+  gtoSExpression' :: f (Int -> Builder) -> (Int -> [Builder])
 
 instance GToSExpression f => GToSExpression (M1 D d f) where
-  gtoSExpression = gtoSExpression . unM1
+  gtoSExpression' = gtoSExpression' . unM1
+
+instance (Constructor c, GToSExpression f) => GToSExpression (M1 C c f) where
+  gtoSExpression' m n = stringUtf8 (conName m) : gtoSExpression' (unM1 m) (n + 1)
