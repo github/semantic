1240265cdecf7dbc603ac8b7da7220d8374b8a62
Rob Rix <robrix@github.com>
2019-06-25 18:01:56 -0400

    Make Core scope-safe.

--- semantic-core/src/Data/Core.hs
+++ semantic-core/src/Data/Core.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE DeriveTraversable, FlexibleContexts, LambdaCase, OverloadedStrings, RecordWildCards, TemplateHaskell,
-             TypeFamilies #-}
+{-# LANGUAGE DeriveTraversable, FlexibleContexts, LambdaCase, OverloadedStrings, RankNTypes, RecordWildCards,
+             ScopedTypeVariables, TemplateHaskell, TypeFamilies #-}
 module Data.Core
 ( Core(..)
 , CoreF(..)
@@ -15,6 +15,7 @@ module Data.Core
 ) where
 
 import Control.Applicative (Alternative (..))
+import Control.Monad (ap)
 import Data.Foldable (foldl')
 import Data.Functor.Foldable hiding (ListF (..))
 import Data.Functor.Foldable.TH
@@ -27,28 +28,28 @@ import GHC.Stack
 data Edge = Lexical | Import
   deriving (Eq, Ord, Show)
 
-data Core
-  = Var Name
+data Core a
+  = Var a
   | Let Name
   -- | Sequencing without binding; analogous to '>>' or '*>'.
-  | Core :>> Core
-  | Lam Name Core
+  | Core a :>> Core a
+  | Lam (Core (Incr a))
   -- | Function application; analogous to '$'.
-  | Core :$ Core
+  | Core a :$ Core a
   | Unit
   | Bool Bool
-  | If Core Core Core
+  | If (Core a) (Core a) (Core a)
   | String Text
   -- | Load the specified file (by path).
-  | Load Core
-  | Edge Edge Core
+  | Load (Core a)
+  | Edge Edge (Core a)
   -- | Allocation of a new frame.
   | Frame
-  | Core :. Core
+  | Core a :. Core a
   -- | Assignment of a value to the reference returned by the lhs.
-  | Core := Core
-  | Ann Loc Core
-  deriving (Eq, Ord, Show)
+  | Core a := Core a
+  | Ann Loc (Core a)
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 infixl 2 :$
 infixr 1 :>>
@@ -62,39 +63,94 @@ infixr 1 :>>$
 infix  3 :=$
 infixl 4 :.$
 
-instance Semigroup Core where
+instance Semigroup (Core a) where
   (<>) = (:>>)
 
-lams :: Foldable t => t Name -> Core -> Core
-lams names body = foldr Lam body names
+instance Applicative Core where
+  pure = Var
+  (<*>) = ap
+
+instance Monad Core where
+  a >>= f = gfoldT id Let (:>>) Lam (:$) Unit Bool If String Load Edge Frame (:.) (:=) Ann (incr (pure Z) (fmap S)) (f <$> a)
+
+
+lam :: Eq a => a -> Core a -> Core a
+lam n b = Lam (bind n b)
+
+lams :: (Eq a, Foldable t) => t a -> Core a -> Core a
+lams names body = foldr lam body names
 
 -- | Application of a function to a sequence of arguments.
-($$*) :: Foldable t => Core -> t Core -> Core
+($$*) :: Foldable t => Core a -> t (Core a) -> Core a
 ($$*) = foldl' (:$)
 
 infixl 9 $$*
 
-unapply :: Alternative m => Core -> m (Core, Core)
+unapply :: Alternative m => Core a -> m (Core a, Core a)
 unapply (f :$ a) = pure (f, a)
 unapply _        = empty
 
-unapplies :: Core -> (Core, Stack Core)
+unapplies :: Core a -> (Core a, Stack (Core a))
 unapplies core = case unapply core of
   Just (f, a) -> (:> a) <$> unapplies f
   Nothing     -> (core, Nil)
 
-block :: Foldable t => t Core -> Core
+block :: Foldable t => t (Core a) -> Core a
 block cs
   | null cs   = Unit
   | otherwise = foldr1 (:>>) cs
 
-ann :: HasCallStack => Core -> Core
+ann :: HasCallStack => Core a -> Core a
 ann = annWith callStack
 
-annWith :: CallStack -> Core -> Core
+annWith :: CallStack -> Core a -> Core a
 annWith callStack c = maybe c (flip Ann c) (stackLoc callStack)
 
-stripAnnotations :: Core -> Core
+stripAnnotations :: Core a -> Core a
 stripAnnotations = cata go where
   go (AnnF _ item) = item
   go item          = embed item
+
+
+gfoldT :: forall m n b
+       .  (forall a . m a -> n a)
+       -> (forall a . Name -> n a)
+       -> (forall a . n a -> n a -> n a)
+       -> (forall a . n (Incr a) -> n a)
+       -> (forall a . n a -> n a -> n a)
+       -> (forall a . n a)
+       -> (forall a . Bool -> n a)
+       -> (forall a . n a -> n a -> n a -> n a)
+       -> (forall a . Text -> n a)
+       -> (forall a . n a -> n a)
+       -> (forall a . Edge -> n a -> n a)
+       -> (forall a . n a)
+       -> (forall a . n a -> n a -> n a)
+       -> (forall a . n a -> n a -> n a)
+       -> (forall a . Loc -> n a -> n a)
+       -> (forall a . Incr (m a) -> m (Incr a))
+       -> Core (m b)
+       -> n b
+gfoldT var let' seq' lam app unit bool if' string load edge frame dot assign ann dist = go
+  where go :: Core (m x) -> n x
+        go = \case
+          Var a -> var a
+          Let a -> let' a
+          a :>> b -> go a `seq'` go b
+          Lam b -> lam (go (dist <$> b))
+          f :$ a -> go f `app` go a
+          Unit -> unit
+          Bool b -> bool b
+          If c t e -> if' (go c) (go t) (go e)
+          String s -> string s
+          Load t -> load (go t)
+          Edge e t -> edge e (go t)
+          Frame -> frame
+          a :. b -> go a `dot` go b
+          a := b -> go a `assign` go b
+          Ann loc t -> ann loc (go t)
+
+
+-- | Bind occurrences of a name in a 'Value' term, producing a 'Core' in which the name is bound.
+bind :: Eq a => a -> Core a -> Core (Incr a)
+bind name = fmap (match name)
