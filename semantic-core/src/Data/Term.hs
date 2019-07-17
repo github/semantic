37b00e5a478aef5b485c96a77ddfbcee2fb5e9a5
Rob Rix <robrix@github.com>
2019-07-17 11:09:04 -0400

    Derive Foldable, Functor, & Traversable instances for Term.

--- semantic-core/src/Data/Term.hs
+++ semantic-core/src/Data/Term.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE QuantifiedConstraints, StandaloneDeriving, UndecidableInstances #-}
+{-# LANGUAGE DeriveTraversable, QuantifiedConstraints, StandaloneDeriving, UndecidableInstances #-}
 module Data.Term
 ( Term(..)
 ) where
@@ -8,3 +8,9 @@ data Term sig a
   | Term (sig (Term sig) a)
 
 deriving instance (Show a, forall g x . (Show x, forall y . Show y => Show (g y)) => Show (sig g x)) => Show (Term sig a)
+
+deriving instance ( forall g . Foldable    g => Foldable    (sig g)) => Foldable    (Term sig)
+deriving instance ( forall g . Functor     g => Functor     (sig g)) => Functor     (Term sig)
+deriving instance ( forall g . Foldable    g => Foldable    (sig g)
+                  , forall g . Functor     g => Functor     (sig g)
+                  , forall g . Traversable g => Traversable (sig g)) => Traversable (Term sig)
