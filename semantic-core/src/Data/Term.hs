43061122b9c9b38c29b8c050bd5d9d7e4b04db72
Rob Rix <robrix@github.com>
2019-07-17 11:17:06 -0400

    Define an iterator for Term.

--- semantic-core/src/Data/Term.hs
+++ semantic-core/src/Data/Term.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE DeriveTraversable, FlexibleInstances, MultiParamTypeClasses, QuantifiedConstraints, RankNTypes, StandaloneDeriving, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DeriveTraversable, FlexibleInstances, LambdaCase, MultiParamTypeClasses, QuantifiedConstraints, RankNTypes, ScopedTypeVariables, StandaloneDeriving, TypeOperators, UndecidableInstances #-}
 module Data.Term
 ( Term(..)
 , Syntax(..)
+, iter
 ) where
 
 import Control.Effect.Carrier
@@ -58,3 +59,18 @@ instance Syntax (Scope ()) where
 instance (Syntax l, Syntax r) => Syntax (l :+: r) where
   foldSyntax go bound free (L l) = L (foldSyntax go bound free l)
   foldSyntax go bound free (R r) = R (foldSyntax go bound free r)
+
+
+iter :: forall m n sig a b
+     .  Syntax sig
+     => (forall a . m a -> n a)
+     -> (forall a . sig n a -> n a)
+     -> (forall a . Incr () (n a) -> m (Incr () (n a)))
+     -> (a -> m b)
+     -> Term sig a
+     -> n b
+iter var alg bound = go
+  where go :: forall x y . (x -> m y) -> Term sig x -> n y
+        go free = \case
+          Var a -> var (free a)
+          Term t -> alg (foldSyntax go bound free t)
