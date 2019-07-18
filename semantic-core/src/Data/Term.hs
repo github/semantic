49baa468f9413541544b6ea7c98c63f0fda724a8
Rob Rix <robrix@github.com>
2019-07-18 14:43:25 -0400

    :fire: iter.

--- semantic-core/src/Data/Term.hs
+++ semantic-core/src/Data/Term.hs
@@ -2,7 +2,6 @@
 module Data.Term
 ( Term(..)
 , Syntax(..)
-, iter
 ) where
 
 import Control.Effect.Carrier
@@ -59,18 +58,3 @@ instance Syntax (Scope ()) where
 instance (Syntax l, Syntax r) => Syntax (l :+: r) where
   foldSyntax go bound free (L l) = L (foldSyntax go bound free l)
   foldSyntax go bound free (R r) = R (foldSyntax go bound free r)
-
-
-iter :: forall m n sig a b
-     .  Syntax sig
-     => (forall a . m a -> n a)
-     -> (forall a . sig n a -> n a)
-     -> (forall a . Incr () (n a) -> m (Incr () (n a)))
-     -> (a -> m b)
-     -> Term sig a
-     -> n b
-iter var alg bound = go
-  where go :: forall x y . (x -> m y) -> Term sig x -> n y
-        go free = \case
-          Var a -> var (free a)
-          Term t -> alg (foldSyntax go bound free t)
