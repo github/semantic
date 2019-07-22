52da8b764cf125330b5a5c9ce30a269d9b21af89
Rob Rix <robrix@github.com>
2019-07-22 15:13:14 -0400

    Qualify the import of Data.Core.

--- semantic-core/test/Generators.hs
+++ semantic-core/test/Generators.hs
@@ -16,7 +16,7 @@ import Hedgehog hiding (Var)
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
-import Data.Core
+import qualified Data.Core as Core
 import Data.Name
 import Data.Term
 
@@ -27,31 +27,31 @@ name :: MonadGen m => m (Named User)
 name = Gen.prune ((Named . Ignored <*> id) <$> names) where
   names = Gen.text (Range.linear 1 10) Gen.lower
 
-boolean :: MonadGen m => m (Term Core User)
-boolean = bool <$> Gen.bool
+boolean :: MonadGen m => m (Term Core.Core User)
+boolean = Core.bool <$> Gen.bool
 
-variable :: MonadGen m => m (Term Core User)
+variable :: MonadGen m => m (Term Core.Core User)
 variable = pure . namedValue <$> name
 
-ifthenelse :: MonadGen m => m (Term Core User) -> m (Term Core User)
-ifthenelse bod = Gen.subterm3 boolean bod bod if'
+ifthenelse :: MonadGen m => m (Term Core.Core User) -> m (Term Core.Core User)
+ifthenelse bod = Gen.subterm3 boolean bod bod Core.if'
 
-apply :: MonadGen m => m (Term Core User) -> m (Term Core User)
+apply :: MonadGen m => m (Term Core.Core User) -> m (Term Core.Core User)
 apply gen = go where
   go = Gen.recursive
     Gen.choice
-    [ Gen.subterm2 gen gen ($$)]
-    [ Gen.subterm2 go go ($$)  -- balanced
-    , Gen.subtermM go (\x -> lam <$> name <*> pure x)
+    [ Gen.subterm2 gen gen (Core.$$)]
+    [ Gen.subterm2 go go (Core.$$)  -- balanced
+    , Gen.subtermM go (\x -> Core.lam <$> name <*> pure x)
     ]
 
-lambda :: MonadGen m => m (Term Core User) -> m (Term Core User)
+lambda :: MonadGen m => m (Term Core.Core User) -> m (Term Core.Core User)
 lambda bod = do
   arg <- name
-  Gen.subterm bod (lam arg)
+  Gen.subterm bod (Core.lam arg)
 
-atoms :: MonadGen m => [m (Term Core User)]
-atoms = [boolean, variable, pure unit]
+atoms :: MonadGen m => [m (Term Core.Core User)]
+atoms = [boolean, variable, pure Core.unit]
 
-literal :: MonadGen m => m (Term Core User)
+literal :: MonadGen m => m (Term Core.Core User)
 literal = Gen.recursive Gen.choice atoms [lambda literal]
