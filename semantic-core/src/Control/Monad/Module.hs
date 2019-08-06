c09e465efb15adf8ca31ac5ca8a79f0a6582b849
Rob Rix <robrix@github.com>
2019-08-06 11:40:15 -0400

    :memo: RightModule and its laws.

--- semantic-core/src/Control/Monad/Module.hs
+++ semantic-core/src/Control/Monad/Module.hs
@@ -8,6 +8,23 @@ module Control.Monad.Module
 
 import Control.Effect.Carrier
 
+-- | Modules over monads allow lifting of a monad’s product (i.e. 'Control.Monad.join') into another structure composed with the monad. A right-module @f m@ over a monad @m@ therefore allows one to extend @m@’s '>>=' operation to values of @f m@ using the '>>=*' operator.
+--
+--   Note that we are calling this a right-module following Maciej Piróg, Nicolas Wu, & Jeremy Gibbons in _Modules Over Monads and their Algebras_; confusingly, other sources refer to this as a left-module.
+--
+--   Laws:
+--
+--   Right-identity:
+--
+-- @
+-- m >>=* return = m
+-- @
+--
+--   Associativity:
+--
+-- @
+-- m >>=* (k >=> h) = (m >>=* k) >>=* h
+-- @
 class (forall g . Functor g => Functor (f g), HFunctor f) => RightModule f where
   (>>=*) :: Monad m => f m a -> (a -> m b) -> f m b
   infixl 1 >>=*
