1cd10683238cf3b5b406661e85bbe259cdc9cea3
Patrick Thomson <patrickt@github.com>
2019-09-11 11:43:11 -0400

    Add test cases for translating early returns.

---
+++ semantic-python/test/fixtures/2-01-return-statement.py
@@ -0,0 +1,3 @@
+# CHECK-JQ: .tree.contents[0][1].contents[1].contents.value == []
+def foo(a):
+    return a
---
+++ semantic-python/test/fixtures/2-02-return-doesnt-translate.py
@@ -0,0 +1,6 @@
+# CHECK-JQ: .tree.contents[0][1].contents[1].contents.value == []
+
+def foo(a):
+    return a
+    a
+    ()
