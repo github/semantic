586db55ab3c503012be4fafe235e6f28d82a9bf2
Rob Rix <robrix@github.com>
2019-09-18 12:57:21 -0400

    Correct the docs.

--- src/Tags/Taggable.hs
+++ src/Tags/Taggable.hs
@@ -3,10 +3,12 @@
 Taggable allows projecting syntax terms to a list of named symbols. In order to
 identify a new syntax as Taggable, you need to:
 
-1. Give that syntax a non-derived Taggable instance and implement at least the
-'symbolName' method.
+1. Give that syntax a non-derived @TaggableBy 'Custom@ instance and implement at least the
+'symbolName'' method.
 
-2. Make sure that 'symbolsToSummarize' in Tagging.hs includes the string
+2. Add an equation to 'TaggableInstance' for the type with the value ''Custom'.
+
+3. Make sure that 'symbolsToSummarize' in Tagging.hs includes the string
 constructor name of this syntax.
 
 -}
