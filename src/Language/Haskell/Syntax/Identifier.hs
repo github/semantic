1ba6f0acd8cf82f697598d5daf8e54de6be6e984
Rob Rix <robrix@github.com>
2019-09-18 12:12:26 -0400

    Move the Taggable instances for Haskell out.

--- src/Language/Haskell/Syntax/Identifier.hs
+++ src/Language/Haskell/Syntax/Identifier.hs
@@ -8,9 +8,10 @@ import Prologue
 import Data.Abstract.Evaluatable
 import Data.JSON.Fields
 import Diffing.Algorithm
+import Tags.Taggable (Taggable)
 
 newtype QualifiedTypeClassIdentifier a = QualifiedTypeClassIdentifier { values :: NonEmpty a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically QualifiedTypeClassIdentifier
 
 instance Hashable1 QualifiedTypeClassIdentifier where liftHashWithSalt = foldl
@@ -18,7 +19,7 @@ instance Hashable1 QualifiedTypeClassIdentifier where liftHashWithSalt = foldl
 instance Evaluatable QualifiedTypeClassIdentifier
 
 newtype QualifiedTypeConstructorIdentifier a = QualifiedTypeConstructorIdentifier { values :: NonEmpty a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically QualifiedTypeConstructorIdentifier
 
 instance Hashable1 QualifiedTypeConstructorIdentifier where liftHashWithSalt = foldl
@@ -26,7 +27,7 @@ instance Hashable1 QualifiedTypeConstructorIdentifier where liftHashWithSalt = f
 instance Evaluatable QualifiedTypeConstructorIdentifier
 
 newtype QualifiedConstructorIdentifier a = QualifiedConstructorIdentifier { values :: NonEmpty a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically QualifiedConstructorIdentifier
 
 instance Hashable1 QualifiedConstructorIdentifier where liftHashWithSalt = foldl
@@ -34,7 +35,7 @@ instance Hashable1 QualifiedConstructorIdentifier where liftHashWithSalt = foldl
 instance Evaluatable QualifiedConstructorIdentifier
 
 newtype QualifiedInfixVariableIdentifier a = QualifiedInfixVariableIdentifier { values :: NonEmpty a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically QualifiedInfixVariableIdentifier
 
 instance Hashable1 QualifiedInfixVariableIdentifier where liftHashWithSalt = foldl
@@ -42,7 +43,7 @@ instance Hashable1 QualifiedInfixVariableIdentifier where liftHashWithSalt = fol
 instance Evaluatable QualifiedInfixVariableIdentifier
 
 newtype QualifiedModuleIdentifier a = QualifiedModuleIdentifier { values :: NonEmpty a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically QualifiedModuleIdentifier
 
 instance Hashable1 QualifiedModuleIdentifier where liftHashWithSalt = foldl
@@ -50,7 +51,7 @@ instance Hashable1 QualifiedModuleIdentifier where liftHashWithSalt = foldl
 instance Evaluatable QualifiedModuleIdentifier
 
 newtype QualifiedVariableIdentifier a = QualifiedVariableIdentifier { values :: NonEmpty a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically QualifiedVariableIdentifier
 
 instance Hashable1 QualifiedVariableIdentifier where liftHashWithSalt = foldl
@@ -58,67 +59,67 @@ instance Hashable1 QualifiedVariableIdentifier where liftHashWithSalt = foldl
 instance Evaluatable QualifiedVariableIdentifier
 
 newtype TypeVariableIdentifier a = TypeVariableIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically TypeVariableIdentifier
 
 instance Evaluatable TypeVariableIdentifier
 
 newtype TypeConstructorIdentifier a = TypeConstructorIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically TypeConstructorIdentifier
 
 instance Evaluatable TypeConstructorIdentifier
 
 newtype ModuleIdentifier a = ModuleIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically ModuleIdentifier
 
 instance Evaluatable ModuleIdentifier
 
 newtype ConstructorIdentifier a = ConstructorIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically ConstructorIdentifier
 
 instance Evaluatable ConstructorIdentifier
 
 newtype ImplicitParameterIdentifier a = ImplicitParameterIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically ImplicitParameterIdentifier
 
 instance Evaluatable ImplicitParameterIdentifier
 
 newtype InfixConstructorIdentifier a = InfixConstructorIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically InfixConstructorIdentifier
 
 instance Evaluatable InfixConstructorIdentifier
 
 newtype InfixVariableIdentifier a = InfixVariableIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically InfixVariableIdentifier
 
 instance Evaluatable InfixVariableIdentifier
 
 newtype TypeClassIdentifier a = TypeClassIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically TypeClassIdentifier
 
 instance Evaluatable TypeClassIdentifier
 
 newtype VariableIdentifier a = VariableIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically VariableIdentifier
 
 instance Evaluatable VariableIdentifier
 
 newtype PrimitiveConstructorIdentifier a = PrimitiveConstructorIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically PrimitiveConstructorIdentifier
 
 instance Evaluatable PrimitiveConstructorIdentifier
 
 newtype PrimitiveVariableIdentifier a = PrimitiveVariableIdentifier { name :: Name }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically PrimitiveVariableIdentifier
 
 instance Evaluatable PrimitiveVariableIdentifier
--- src/Language/Haskell/Syntax/Pattern.hs
+++ src/Language/Haskell/Syntax/Pattern.hs
@@ -8,82 +8,83 @@ import Prologue
 import Data.Abstract.Evaluatable
 import Data.JSON.Fields
 import Diffing.Algorithm
+import Tags.Taggable (Taggable)
 
 newtype StrictPattern a = StrictPattern { value :: a}
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically StrictPattern
 
 instance Evaluatable StrictPattern
 
 newtype ListPattern a = ListPattern { value :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically ListPattern
 
 instance Evaluatable ListPattern
 
 -- e.g. The `n@num1` in `f n@num1 x@num2 = x`
 data AsPattern a = AsPattern { asPatternLeft :: a, asPatternRight :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically AsPattern
 
 instance Evaluatable AsPattern
 
 newtype TypePattern a = TypePattern { typePatternContent :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically TypePattern
 
 instance Evaluatable TypePattern
 
 -- e.g. The `a = 1` in `foo Bar{ a = 1 } = baz`.
 data FieldPattern a = FieldPattern { fieldPatternLeft :: a, fieldPatternRight :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically FieldPattern
 
 instance Evaluatable FieldPattern
 
 -- e.g. The `~a` in `f ~a = 1`
 newtype IrrefutablePattern a = IrrefutablePattern { value :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically IrrefutablePattern
 
 instance Evaluatable IrrefutablePattern
 
 -- For handling guards in case alternative expressions.
 newtype CaseGuardPattern a = CaseGuardPattern { values :: [a] }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically CaseGuardPattern
 
 instance Evaluatable CaseGuardPattern
 
 -- For handling guards in function declarations.
 newtype FunctionGuardPattern a = FunctionGuardPattern { values :: [a] }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically FunctionGuardPattern
 
 instance Evaluatable FunctionGuardPattern
 
 data ViewPattern a = ViewPattern { viewPatternLeft :: a, viewPatternRight :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically ViewPattern
 
 instance Evaluatable ViewPattern
 
 -- e.g. The `Bar{..}` in `foo Bar{..} = baz`.
 newtype LabeledPattern a = LabeledPattern { value :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically LabeledPattern
 
 instance Evaluatable LabeledPattern
 
 -- The `a <- b` in `f a | a <- b = c` of a function declaration.
 data PatternGuard a = PatternGuard { patternGuardPattern :: a, patternGuardExpression :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically PatternGuard
 
 instance Evaluatable PatternGuard
 
 newtype Guard a = Guard { value :: a }
-  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, ToJSONFields1, Traversable, NFData1)
+  deriving (Declarations1, Diffable, Eq, Foldable, FreeVariables1, Functor, Generic1, Hashable1, Ord, Show, Taggable, ToJSONFields1, Traversable, NFData1)
   deriving (Eq1, Show1, Ord1) via Generically Guard
 
 instance Evaluatable Guard
