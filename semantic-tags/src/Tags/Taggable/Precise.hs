56cd54f98fa12f455a96fce019dad57a4272740d
Rob Rix <robrix@github.com>
2019-09-18 18:42:56 -0400

    Stub in a tagging algorithm.

--- semantic-tags/src/Tags/Taggable/Precise.hs
+++ semantic-tags/src/Tags/Taggable/Precise.hs
@@ -1,12 +1,17 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE AllowAmbiguousTypes, DataKinds, DeriveGeneric, DisambiguateRecordFields, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, NamedFieldPuns, OverloadedStrings, ScopedTypeVariables, TypeApplications, TypeFamilies, UndecidableInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
 module Tags.Taggable.Precise
 ( Python(..)
+, runTagging
 ) where
 
+import           Control.Effect.Reader
 import           Data.Aeson as A
-import           Data.Span
+import           Data.Blob
+import           Data.Monoid (Endo(..))
+import           Data.Location
 import           Data.Text (Text)
-import           GHC.Generics (Generic)
+import           GHC.Generics
 import qualified TreeSitter.Python.AST as Python
 
 data Tag = Tag
@@ -23,3 +28,70 @@ instance ToJSON Tag
 
 
 newtype Python a = Python { getPython :: Python.Module a }
+  deriving (Eq, Generic, Ord, Show)
+
+type ContextToken = (Text, Maybe Range)
+
+runTagging :: Blob -> [Text] -> Python Location -> [Tag]
+runTagging blob symbolsToSummarize
+  = ($ [])
+  . appEndo
+  . run
+  . runReader @[ContextToken] []
+  . runReader blob
+  . runReader symbolsToSummarize
+  . tag
+  . getPython where
+
+class ToTag t where
+  tag
+    :: ( Carrier sig m
+       , Member (Reader Blob) sig
+       , Member (Reader [ContextToken]) sig
+       , Member (Reader [Text]) sig
+       )
+    => t Location
+    -> m (Endo [Tag])
+
+instance (ToTagBy strategy t, strategy ~ ToTagInstance t) => ToTag t where
+  tag = tag' @strategy
+
+
+class ToTagBy (strategy :: Strategy) t where
+  tag'
+    :: ( Carrier sig m
+       , Member (Reader Blob) sig
+       , Member (Reader [ContextToken]) sig
+       , Member (Reader [Text]) sig
+       )
+    => t Location
+    -> m (Endo [Tag])
+
+
+data Strategy = Generic | Custom
+
+type family ToTagInstance t :: Strategy where
+  ToTagInstance Python.FunctionDefinition = 'Custom
+  ToTagInstance _                         = 'Generic
+
+instance ToTagBy 'Custom Python.FunctionDefinition where
+  tag' Python.FunctionDefinition {} = pure mempty
+
+
+instance (Generic (t Location), GToTag (Rep (t Location))) => ToTagBy 'Generic t where
+  tag' = gtag . from
+
+
+class GToTag t where
+  gtag
+    :: ( Carrier sig m
+       , Member (Reader Blob) sig
+       , Member (Reader [ContextToken]) sig
+       , Member (Reader [Text]) sig
+       )
+    => t Location
+    -> m (Endo [Tag])
+
+
+instance GToTag (M1 i c f) where
+  gtag _ = pure mempty
