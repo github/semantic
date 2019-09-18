2aea021890c349c97b1a020372b7213c0d24b155
Patrick Thomson <patrickt@github.com>
2019-09-18 13:05:10 -0400

    Use some tree information in a unit test.

--- semantic-python/test/fixtures/1-04-toplevel-assignment.py
+++ semantic-python/test/fixtures/1-04-toplevel-assignment.py
@@ -1,3 +1,5 @@
 # CHECK-JQ: .scope | has("hello") and has("goodbye")
+# CHECK-JQ: .tree.location.span | [.start.line, .start.column] == [0, 0]
+# CHECK-JQ: .tree.location.span | [.end.line, .end.column] == [5, 0]
 hello = ()
 goodbye = ()
