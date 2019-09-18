efa83c30363c7d15cfcbd314e976f19d9039dafb
Rob Rix <robrix@github.com>
2019-09-18 12:21:39 -0400

    Revert "Move the Taggable instances for Data.Syntax.Comment out."
    This reverts commit 211e2754d0ef06280744170c476587dc02dfc33c.

--- src/Tags/Taggable.hs
+++ src/Tags/Taggable.hs
@@ -38,6 +38,7 @@ import Data.Text hiding (empty)
 import Streaming hiding (Sum)
 import Streaming.Prelude (yield)
 
+import qualified Data.Syntax.Comment as Comment
 import qualified Data.Syntax.Declaration as Declaration
 import qualified Data.Syntax.Directive as Directive
 import qualified Data.Syntax.Expression as Expression
@@ -182,6 +183,8 @@ instance Taggable Ruby.Send where
   symbolName Ruby.Send{..} = declaredName =<< sendSelector
 
 instance Taggable []
+instance Taggable Comment.Comment
+instance Taggable Comment.HashBang
 
 instance Taggable Expression.And
 instance Taggable Expression.Await
