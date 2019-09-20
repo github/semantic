2748529c13c4f3f173309ec0a6786e842e9eba13
Rob Rix <robrix@github.com>
2019-09-20 14:16:42 -0400

    Copy Location in as Loc.

---
+++ semantic-source/src/Source/Loc.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DeriveGeneric, DerivingVia #-}
+module Source.Loc
+( Loc(..)
+, Span(..)
+, Range(..)
+) where
+
+import Control.DeepSeq (NFData)
+import Data.Hashable (Hashable)
+import Data.Monoid.Generic
+import GHC.Generics (Generic)
+import Source.Range
+import Source.Span
+
+data Loc = Loc
+  { locByteRange :: {-# UNPACK #-} !Range
+  , locSpan      :: {-# UNPACK #-} !Span
+  }
+  deriving (Eq, Ord, Show, Generic)
+  deriving Semigroup via GenericSemigroup Loc
+
+instance Hashable Loc
+instance NFData   Loc
+
+instance HasSpan Loc where
+  span = lens locSpan (\l s -> l { locSpan = s }) where
+    lens get put afa s = fmap (put s) (afa (get s))
+  {-# INLINE span #-}
