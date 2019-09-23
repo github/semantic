dccc1942aa147996bdf141a8f02b08ae7c65683b
Patrick Thomson <patrickt@github.com>
2019-09-23 12:13:40 -0400

    Break toplevel assignment case because it's wrong.

--- semantic-python/test/fixtures/1-04-toplevel-assignment.py
+++ semantic-python/test/fixtures/1-04-toplevel-assignment.py
@@ -1,5 +1,4 @@
 # CHECK-JQ: .scope | has("hello") and has("goodbye")
-# CHECK-JQ: .tree.location.span | [.start.line, .start.column] == [0, 0]
-# CHECK-JQ: .tree.location.span | [.end.line, .end.column] == [5, 0]
+# CHECK-TREE: { hello <- #unit; goodbye <- #unit; #record { hello: hello, goodbye: goodbye }}
 hello = ()
 goodbye = ()
