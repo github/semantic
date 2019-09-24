6e7b373214b8bb9aca2cdb0808424c30a7a5e531
Rob Rix <robrix@github.com>
2019-09-24 14:10:27 -0400

    :fire: a redundant language extension.

--- semantic-tags/src/Tags/Taggable/Precise.hs
+++ semantic-tags/src/Tags/Taggable/Precise.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE AllowAmbiguousTypes, DataKinds, DisambiguateRecordFields, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, NamedFieldPuns, OverloadedStrings, ScopedTypeVariables, TypeApplications, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes, DataKinds, DisambiguateRecordFields, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, NamedFieldPuns, ScopedTypeVariables, TypeApplications, TypeFamilies, TypeOperators, UndecidableInstances #-}
 module Tags.Taggable.Precise
 ( runTagging
 , Element(..)
