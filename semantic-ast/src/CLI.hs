0b22d3fdab5194ebfd53fe9febb6d462fff7fe1f
Ayman Nadeem <aymannadeem@github.com>
2019-10-01 17:02:30 -0400

    delete unnecessary imports

--- semantic-ast/src/CLI.hs
+++ semantic-ast/src/CLI.hs
@@ -2,37 +2,7 @@
 {-# LANGUAGE TypeApplications #-}
 module CLI (main) where
 
-import           Control.Effect.Reader
-import           Control.Exception as Exc (displayException)
-import           Data.Blob
-import           Data.Blob.IO
-import           Data.Handle
-import qualified Data.Language as Language
-import           Data.List (intercalate)
-import           Data.Project
-import qualified Data.Text as T
-import qualified Data.Flag as Flag
-import           Options.Applicative hiding (style)
-import           Prologue
-import           Semantic.Api hiding (File)
-import qualified Semantic.AST as AST
-import           Semantic.Config
-import qualified Semantic.Graph as Graph
-import qualified Semantic.Task as Task
-import qualified Semantic.Git as Git
-import           Semantic.Task.Files
-import           Semantic.Telemetry
-import qualified Semantic.Telemetry.Log as Log
-import           Semantic.Version
-import           Serializing.Format hiding (Options)
-import           System.Exit (die)
-import           System.FilePath
 
-import Control.Concurrent (mkWeakThreadId, myThreadId)
-import Control.Exception (Exception(..), throwTo)
-import Data.Typeable (Typeable)
-import System.Posix.Signals
-import System.Mem.Weak (deRefWeak)
 
 newtype SignalException = SignalException Signal
   deriving (Show, Typeable)
