a05bdcb68fc70464009a0fcee5735b1eee73abea
Ayman Nadeem <aymannadeem@github.com>
2019-10-01 17:03:37 -0400

    unmarshal and friends

--- semantic-ast/src/CLI.hs
+++ semantic-ast/src/CLI.hs
@@ -3,6 +3,9 @@
 module CLI (main) where
 
 import System.Environment
+import TreeSitter.Unmarshal
+import TreeSitter.Python.AST
+import TreeSitter.Python
 import Source.Range
 import Source.Span
 import Data.ByteString (ByteString)
