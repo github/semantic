665429fa927b4f328d6df8e4c20399f639757f46
Patrick Thomson <patrickt@github.com>
2019-10-07 20:16:54 -0400

    We have these JSON instances already.

--- semantic-python/test/Instances.hs
+++ semantic-python/test/Instances.hs
@@ -9,11 +9,11 @@ module Instances () where
 
 import           Analysis.ScopeGraph
 import           Data.Aeson
+import           Data.File
 import           Data.Loc
 import qualified Data.Map as Map
-import           Data.File
+import           Data.Name (Name (..))
 import           Data.Text (Text)
-import           Data.Name
 
 deriving newtype instance ToJSON Name
 deriving newtype instance ToJSONKey Name
@@ -22,20 +22,6 @@ instance ToJSON a => ToJSON (File a) where
   toJSON File{fileLoc, fileBody} = object
     [ "location" .= fileLoc
     , "body" .= fileBody
-    ]
-
-instance ToJSON Span where
-  toJSON Span{spanStart, spanEnd} = object
-    [ "kind"  .= ("span" :: Text)
-    , "start" .= spanStart
-    , "end"   .= spanEnd
-    ]
-
-instance ToJSON Pos where
-  toJSON Pos{posLine, posCol} = object
-    [ "kind" .= ("pos" :: Text)
-    , "line" .= posLine
-    , "column"  .= posCol
     ]
 
 instance ToJSON Loc where
