c97c8c7e1208a229b8d1a0d8d92cc702b68d0d38
Rob Rix <robrix@github.com>
2019-10-10 13:12:32 -0400

    Inline Loc into File.

--- semantic-core/src/Data/File.hs
+++ semantic-core/src/Data/File.hs
@@ -1,18 +1,25 @@
 {-# LANGUAGE DeriveTraversable #-}
 module Data.File
 ( File(..)
+, fileLoc
 , fromBody
 ) where
 
 import Data.Loc
 import Data.Maybe (fromJust)
 import GHC.Stack
+import Source.Span
 
 data File a = File
-  { fileLoc  :: !Loc
+  { filePath :: !Path
+  , fileSpan :: {-# UNPACK #-} !Span
   , fileBody :: !a
   }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
+fileLoc :: File a -> Loc
+fileLoc (File p s _) = Loc p s
+
 fromBody :: HasCallStack => a -> File a
-fromBody body = File (fromJust (stackLoc callStack)) body
+fromBody body = File path span body where
+  Loc path span = fromJust (stackLoc callStack)
