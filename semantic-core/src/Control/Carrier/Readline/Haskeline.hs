81d1a1117ed61128dc8b96eac90589c157c4dd81
Rob Rix <robrix@github.com>
2019-10-10 15:54:29 -0400

    Internalize ControlIOC.

--- semantic-core/src/Control/Carrier/Readline/Haskeline.hs
+++ semantic-core/src/Control/Carrier/Readline/Haskeline.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes, ScopedTypeVariables, TypeApplications, TypeOperators, UndecidableInstances #-}
 module Control.Carrier.Readline.Haskeline
 ( -- * Readline effect
   module Control.Effect.Readline
@@ -6,7 +6,6 @@ module Control.Carrier.Readline.Haskeline
 , runReadline
 , runReadlineWithHistory
 , ReadlineC (..)
-, ControlIOC (..)
   -- * Re-exports
 , Carrier
 , run
@@ -19,16 +18,16 @@ import Control.Effect.Reader
 import Control.Effect.Readline hiding (Carrier)
 import Control.Monad.Fix
 import Control.Monad.IO.Unlift
-import Control.Monad.Trans.Class
+import Data.Coerce
 import Data.Text.Prettyprint.Doc.Render.Text
 import System.Console.Haskeline hiding (Handler, handle)
 import System.Directory
 import System.FilePath
 
-runReadline :: MonadException m => Prefs -> Settings m -> ReadlineC m a -> m a
-runReadline prefs settings = runInputTWithPrefs prefs settings . runM . runReader (Line 0) . runReadlineC
+runReadline :: MonadUnliftIO m => Prefs -> Settings m -> ReadlineC m a -> m a
+runReadline prefs settings = runControlIO . runInputTWithPrefs prefs (coerce settings) . runM . runReader (Line 0) . runReadlineC
 
-runReadlineWithHistory :: MonadException m => ReadlineC m a -> m a
+runReadlineWithHistory :: MonadUnliftIO m => ReadlineC m a -> m a
 runReadlineWithHistory block = do
   homeDir <- liftIO getHomeDirectory
   prefs <- liftIO $ readPrefs (homeDir </> ".haskeline")
@@ -42,12 +41,12 @@ runReadlineWithHistory block = do
 
   runReadline prefs settings block
 
-newtype ReadlineC m a = ReadlineC { runReadlineC :: ReaderC Line (LiftC (InputT m)) a }
+newtype ReadlineC m a = ReadlineC { runReadlineC :: ReaderC Line (LiftC (InputT (ControlIOC m))) a }
   deriving (Applicative, Functor, Monad, MonadFix, MonadIO)
 
-instance (MonadException m, MonadIO m) => Carrier (Readline :+: Lift (InputT m)) (ReadlineC m) where
+instance MonadUnliftIO m => Carrier (Readline :+: Lift (InputT (ControlIOC m))) (ReadlineC m) where
   eff (L (Prompt prompt k)) = ReadlineC $ do
-    str <- lift (lift (getInputLine (cyan <> prompt <> plain)))
+    str <- sendM (getInputLine @(ControlIOC m) (cyan <> prompt <> plain))
     Line line <- ask
     local increment (runReadlineC (k line str))
     where cyan = "\ESC[1;36m\STX"
