404457564873e9e9c6ef76bae0f027750d5fb29b
Rob Rix <robrix@github.com>
2019-10-10 15:58:23 -0400

    :fire: the IsString constraint.

--- semantic-core/src/Control/Effect/Readline.hs
+++ semantic-core/src/Control/Effect/Readline.hs
@@ -10,7 +10,6 @@ module Control.Effect.Readline
 ) where
 
 import Control.Effect.Carrier
-import Data.String
 import Data.Text.Prettyprint.Doc
 import Data.Text.Prettyprint.Doc.Render.Terminal
 import GHC.Generics (Generic1)
@@ -25,8 +24,8 @@ instance HFunctor Readline
 instance Effect   Readline
 
 
-prompt :: (IsString str, Member Readline sig, Carrier sig m) => String -> m (Int, Maybe str)
-prompt p = fmap (fmap fromString) <$> send (Prompt p (curry pure))
+prompt :: (Member Readline sig, Carrier sig m) => String -> m (Int, Maybe String)
+prompt p = send (Prompt p (curry pure))
 
 print :: (Carrier sig m, Member Readline sig) => Doc AnsiStyle -> m ()
 print s = send (Print s (pure ()))
