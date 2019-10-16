be95b43b71a4994964e7bef70f221d739dd5e802
Rob Rix <robrix@github.com>
2019-10-16 18:03:13 -0400

    Reformat formatIdentifier.

--- src/Rendering/TOC.hs
+++ src/Rendering/TOC.hs
@@ -128,9 +128,11 @@ recordSummary changeText record = case record of
   Declaration ErrorDeclaration text _ srcSpan language -> ErrorSummary text srcSpan language
   decl -> TOCSummary (toCategoryName (declarationKind decl)) (formatIdentifier decl) (declarationSpan decl) changeText
   where
-    formatIdentifier (Declaration (MethodDeclaration (Just receiver)) identifier _ _ Language.Go) = "(" <> receiver <> ") " <> identifier
-    formatIdentifier (Declaration (MethodDeclaration (Just receiver)) identifier _ _ _          ) = receiver <> "." <> identifier
-    formatIdentifier decl = declarationIdentifier decl
+    formatIdentifier (Declaration kind identifier _ _ lang) = case kind of
+      MethodDeclaration (Just receiver)
+        | Language.Go <- lang -> "(" <> receiver <> ") " <> identifier
+        | otherwise           -> receiver <> "." <> identifier
+      _                       -> identifier
 
 renderToCDiff :: (Foldable f, Functor f) => BlobPair -> Diff f (Maybe Declaration) (Maybe Declaration) -> Summaries
 renderToCDiff blobs = uncurry Summaries . bimap toMap toMap . List.partition isValidSummary . diffTOC
