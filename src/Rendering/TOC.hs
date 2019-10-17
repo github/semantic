1990de19c21877824af9aad0136e4c3a2788da9b
Rob Rix <robrix@github.com>
2019-10-17 02:44:57 -0400

    Fix an indentation bug.

--- src/Rendering/TOC.hs
+++ src/Rendering/TOC.hs
@@ -82,7 +82,7 @@ tableOfContentsBy selector = fromMaybe [] . cata (\ r -> case r of
   Merge (In (_, ann2) r) -> case (selector (In ann2 r), fold r) of
     (Just a, Just entries) -> Just ((Changed, a) : entries)
     (_     , entries)      -> entries)
-   where patchEntry = patch (Deleted,) (Inserted,) (const (Replaced,))
+  where patchEntry = patch (Deleted,) (Inserted,) (const (Replaced,))
 
 
 data DedupeKey = DedupeKey !DeclarationKind {-# UNPACK #-} !T.Text
