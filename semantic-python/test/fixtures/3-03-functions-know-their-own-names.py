ff23c301fb0890f38546651b6895a78c64b4e35f
Patrick Thomson <patrickt@github.com>
2019-10-18 10:22:19 -0400

    don't worry about this since it's a known bug

--- semantic-python/test/fixtures/3-03-functions-know-their-own-names.py
+++ semantic-python/test/fixtures/3-03-functions-know-their-own-names.py
@@ -1,4 +1,2 @@
 # CHECK-TREE: { recursive <- rec recursive = \a -> recursive; #record { recursive: recursive }}
 def recursive(a): return recursive
-
-# this should scope correctly in the graph but it doesn't seem to yet
