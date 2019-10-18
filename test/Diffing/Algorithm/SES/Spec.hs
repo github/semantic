15fdc198bb99550704e8500c95a70c15629bb3c9
Rob Rix <robrix@github.com>
2019-10-17 22:53:35 -0400

    Don’t convert to These.

--- test/Diffing/Algorithm/SES/Spec.hs
+++ test/Diffing/Algorithm/SES/Spec.hs
@@ -1,6 +1,6 @@
 module Diffing.Algorithm.SES.Spec (spec) where
 
-import Data.These
+import Data.Edit
 import Diffing.Algorithm.SES
 import Test.Hspec
 import Test.Hspec.LeanCheck
@@ -21,4 +21,4 @@ spec = do
       \ as bs -> length (ses (==) ((,) 0 <$> as :: [(Int, Char)]) ((,) 1 <$> bs :: [(Int, Char)])) `shouldBe` length as + length bs
 
     prop "is lossless w.r.t. both input elements & ordering" $
-      \ as bs -> foldr (\ each (as, bs) -> these (flip (,) bs. (:as)) ((,) as . (:bs)) (\ a b -> (a:as, b:bs)) (toThese each)) ([], []) (ses (==) as bs :: [Edit Char Char]) `shouldBe` (as, bs)
+      \ as bs -> foldr (\ each (as, bs) -> edit (flip (,) bs. (:as)) ((,) as . (:bs)) (\ a b -> (a:as, b:bs)) each) ([], []) (ses (==) as bs :: [Edit Char Char]) `shouldBe` (as, bs)
