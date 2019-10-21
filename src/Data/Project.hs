2d5a7e7fbbf320e3610a04fff19b5c46fb5a817a
Patrick Thomson <patrickt@github.com>
2019-10-21 17:48:27 -0400

    Comment and clarify.

--- src/Data/Project.hs
+++ src/Data/Project.hs
@@ -38,15 +38,18 @@ projectFiles = fmap blobFile . projectBlobs
 
 readProjectFromPaths :: MonadIO m
                      => Maybe Path.AbsRelDir -- ^ An optional root directory for the project
-                     -> Path.AbsRelFileDir   -- ^ A file or directory to parse. Passing a directory will load
+                     -> Path.AbsRelFileDir   -- ^ A file or directory to parse. Passing a file path loads all files in that file's parent directory.
                      -> Language
-                     -> [Path.AbsRelDir]
+                     -> [Path.AbsRelDir]     -- ^ Directories to exclude.
                      -> m Project
 readProjectFromPaths maybeRoot path lang excludeDirs = do
-  let (rootDir :: Path.AbsRelDir) = case maybeRoot of
-        Just root -> fromMaybe (Path.dirFromFileDir path) (Path.fromAbsRel root)
+  let (rootDir :: Path.AbsRelDir) = case maybeRoot >>= Path.fromAbsRel of
+        -- If we were provided a root directory, use that.
+        Just root -> root
         Nothing   -> case Path.fileFromFileDir path of
+          -- If we weren't and the path is a file, drop its file name.
           Just fp -> Path.takeDirectory fp
+          -- Otherwise, load from the path.
           Nothing -> Path.dirFromFileDir path
 
   paths <- liftIO $ findFilesInDir rootDir exts excludeDirs
