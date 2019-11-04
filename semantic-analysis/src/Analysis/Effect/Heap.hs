b1c2f261e9184fa48f145aa0680ba17b9976d67b
Rob Rix <robrix@github.com>
2019-11-04 12:16:00 -0500

    Define a smart constructor for Deref.

--- semantic-analysis/src/Analysis/Effect/Heap.hs
+++ semantic-analysis/src/Analysis/Effect/Heap.hs
@@ -1,10 +1,15 @@
-{-# LANGUAGE DeriveFunctor, DeriveGeneric #-}
+{-# LANGUAGE DeriveFunctor, DeriveGeneric, FlexibleContexts #-}
 module Analysis.Effect.Heap
-( Heap(..)
+( deref
+, Heap(..)
 ) where
 
 import Control.Effect.Carrier
 import GHC.Generics (Generic1)
+
+deref :: (Member (Heap addr value) sig, Carrier sig m) => addr -> m (Maybe value)
+deref addr = send (Deref addr pure)
+
 
 data Heap addr value m k
   = Deref addr (Maybe value -> m k)
