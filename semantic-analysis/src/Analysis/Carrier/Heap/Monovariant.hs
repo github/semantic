a17a6825d32faf7e927e44d1f1560adc5f0e2742
Rob Rix <robrix@github.com>
2019-11-04 12:52:03 -0500

    Define a monovariant Heap carrier.

--- semantic-analysis/src/Analysis/Carrier/Heap/Monovariant.hs
+++ semantic-analysis/src/Analysis/Carrier/Heap/Monovariant.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Analysis.Carrier.Heap.Monovariant
-( -- * Heap effect
-  module Analysis.Effect.Heap
+( -- * Heap carrier
+  runHeap
+, HeapC(..)
+  -- * Heap effect
+, module Analysis.Effect.Heap
 ) where
 
 import Analysis.Effect.Heap
+import Control.Effect.State.Strict
+import qualified Control.Monad.Fail as Fail
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+runHeap :: HeapC addr value m a -> m (Map.Map addr (Set.Set value), a)
+runHeap (HeapC m) = runState Map.empty m
+
+newtype HeapC addr value m a = HeapC (StateC (Map.Map addr (Set.Set value)) m a)
+  deriving (Applicative, Functor, Monad, Fail.MonadFail)
