24f6159acc39f098f3ea6f4cfc4a5a7ec2676ab3
Rob Rix <robrix@github.com>
2019-11-04 14:35:24 -0500

    Domain is scope-safe.

--- semantic-analysis/src/Analysis/Domain.hs
+++ semantic-analysis/src/Analysis/Domain.hs
@@ -1,13 +1,19 @@
+{-# LANGUAGE QuantifiedConstraints, StandaloneDeriving #-}
 module Analysis.Domain
 ( Domain(..)
 ) where
 
 import Data.Text (Text)
+import Syntax.Scope
 
-data Domain name a
+data Domain name f a
   = Unit
   | Bool Bool
   | String Text
-  | Record [(name, a)]
-  | Lam name a
-  deriving (Eq, Ord, Show)
+  | Record [(name, f a)]
+  | Lam name (Scope () f a)
+
+deriving instance (Eq   name, Eq   a, forall a . Eq   a => Eq   (f a), Monad f) => Eq   (Domain name f a)
+deriving instance (Ord  name, Ord  a, forall a . Eq   a => Eq   (f a)
+                                    , forall a . Ord  a => Ord  (f a), Monad f) => Ord  (Domain name f a)
+deriving instance (Show name, Show a, forall a . Show a => Show (f a))          => Show (Domain name f a)
