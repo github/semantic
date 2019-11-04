08e1061b90f0633d3bf13fbd082e718d606e1183
Rob Rix <robrix@github.com>
2019-11-04 12:47:20 -0500

    Define a Carrier instance for Heap.

--- semantic-analysis/src/Analysis/Carrier/Heap/Precise.hs
+++ semantic-analysis/src/Analysis/Carrier/Heap/Precise.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Analysis.Carrier.Heap.Precise
 ( -- * Heap carrier
   runHeap
@@ -8,12 +8,21 @@ module Analysis.Carrier.Heap.Precise
 ) where
 
 import Analysis.Effect.Heap
+import Control.Effect.Carrier
 import Control.Effect.State.Strict
 import qualified Control.Monad.Fail as Fail
 import qualified Data.IntMap as IntMap
+
+type Precise = Int
 
 runHeap :: HeapC value m a -> m (IntMap.IntMap value, a)
 runHeap (HeapC m) = runState mempty m
 
 newtype HeapC value m a = HeapC (StateC (IntMap.IntMap value) m a)
   deriving (Applicative, Functor, Monad, Fail.MonadFail)
+
+instance (Carrier sig m, Effect sig)
+      => Carrier (Heap Precise value :+: sig) (HeapC value m) where
+  eff (L (Deref addr k))        = HeapC (gets (IntMap.lookup addr)) >>= k
+  eff (L (Assign addr value k)) = HeapC (modify (IntMap.insert addr value)) >> k
+  eff (R other)                 = HeapC (eff (R (handleCoercible other)))
