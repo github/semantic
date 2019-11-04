5b8364cd51a20e2e860b8f39de360e75b8d3d7e9
Rob Rix <robrix@github.com>
2019-11-04 12:32:01 -0500

    Stub in a Heap carrier.

--- semantic-analysis/src/Analysis/Carrier/Heap/Precise.hs
+++ semantic-analysis/src/Analysis/Carrier/Heap/Precise.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Analysis.Carrier.Heap.Precise
-( -- * Heap effect
-  module Analysis.Effect.Heap
+( -- * Heap carrier
+  HeapC(..)
+  -- * Heap effect
+, module Analysis.Effect.Heap
 ) where
 
 import Analysis.Effect.Heap
+import qualified Control.Monad.Fail as Fail
+
+newtype HeapC addr value m a = HeapC { runHeap :: m a }
+  deriving (Applicative, Functor, Monad, Fail.MonadFail)
