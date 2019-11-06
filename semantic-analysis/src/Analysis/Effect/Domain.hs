e8a47496531b2bc5d15c31a2d0f95dd383ff3e8a
Rob Rix <robrix@github.com>
2019-11-06 12:37:58 -0500

    Stub in a Domain effect with abstraction & concretization operations.

--- semantic-analysis/src/Analysis/Effect/Domain.hs
+++ semantic-analysis/src/Analysis/Effect/Domain.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Analysis.Effect.Domain
-( -- * Re-exports
-  Carrier
+( -- * Domain effect
+  Domain(..)
+  -- * Re-exports
+, Carrier
 , run
 ) where
 
 import Control.Effect.Carrier
+
+data Domain term value m k
+  = Abstract term (value -> m k)
+  | Concretize value (term -> m k)
+  deriving (Functor)
