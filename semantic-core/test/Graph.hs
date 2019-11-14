a7bb2695ed225e234be4f990bc13c324be144e62
Patrick Thomson <patrickt@github.com>
2019-11-14 15:50:10 -0500

    Actually run the scope graph.

--- semantic-core/test/Graph.hs
+++ semantic-core/test/Graph.hs
@@ -5,12 +5,18 @@ module Graph
   , parseEither
   ) where
 
---import qualified Analysis.ScopeGraph as ScopeGraph
+import           Control.Effect.Carrier
+import qualified Analysis.ScopeGraph as ScopeGraph
 import           Core.Core
 import           Core.Name
 import           Core.Parser as Parse
 import           Syntax.Term
 import qualified Text.Trifecta as Trifecta
+import           Data.Semilattice.Lower
+import qualified Core.Eval as Eval
+import           Analysis.File
+import qualified System.Path as Path
+import qualified System.Path.IO as Path (readFile)
 
 import qualified Test.Tasty as Tasty
 import qualified Test.Tasty.HUnit as HUnit
@@ -20,11 +26,13 @@ parseEither p = Trifecta.foldResult (Left . show . Trifecta._errDoc) Right . Tri
 
 testSimpleScopeGraph :: Tasty.TestTree
 testSimpleScopeGraph = HUnit.testCase "simple.score" $ do
-  contents <- readFile "semantic-core/test/fixtures/simple.score"
-  (_parsed :: Term Core Name) <- case parseEither Parse.core contents of
-    Right x -> pure x
+  let p = Path.absRel "semantic-core/test/fixtures/simple.score"
+  contents <- Path.readFile p
+  case parseEither Parse.core contents of
     Left m  -> HUnit.assertFailure ("Couldn't parse simple.score: " <> m)
-  pure ()
+    Right (x :: Term Core Name) -> do
+      (_heap, [_res]) <- pure (ScopeGraph.scopeGraph Eval.eval [File p lowerBound (hoistTerm inj x)])
+      pure ()
 
 testTree :: Tasty.TestTree
 testTree = Tasty.testGroup "Core scope graphing"
