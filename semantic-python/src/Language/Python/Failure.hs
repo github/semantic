609ddb989c45827cf91c32482ca0a31e5e136d94
Patrick Thomson <patrickt@github.com>
2019-11-14 15:26:04 -0500

    Add eliminateFailures, even though it doesn't do what I want.

--- semantic-python/src/Language/Python/Failure.hs
+++ semantic-python/src/Language/Python/Failure.hs
@@ -1,18 +1,26 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveGeneric, DeriveTraversable, ExistentialQuantification, FlexibleContexts, KindSignatures, RankNTypes, StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveGeneric, DeriveTraversable, ExistentialQuantification, FlexibleContexts, KindSignatures, RankNTypes, StandaloneDeriving, TypeOperators, QuantifiedConstraints #-}
 
 module Language.Python.Failure
   ( Failure (..)
   , unimplemented
   , invariantViolated
+  , eliminateFailures
   ) where
+
+import Prelude hiding (fail)
 
 import Control.Effect.Carrier
 import Data.Coerce
 import Data.Kind
+import Syntax.Term
 
 data Failure (f :: Type -> Type) a
   = forall a . Show a => Unimplemented a
   | InvariantViolated String
+
+instance Show (Failure f a) where
+  show (Unimplemented a) = "unimplemented: " <> show a
+  show (InvariantViolated a) = "invariant violated: " <> a
 
 deriving instance Functor (Failure f)
 
@@ -23,3 +31,12 @@ unimplemented x = send . Unimplemented $ x
 
 invariantViolated :: (Member Failure sig, Carrier sig m) => String -> m a
 invariantViolated = send . InvariantViolated
+
+eliminateFailures :: forall a sig . (HFunctor sig, forall g . Functor g => Functor (sig g))
+                  => (forall x y . Failure x y -> Term sig y)
+                  -> Term (Failure :+: sig) a
+                  -> Term sig a
+eliminateFailures _ (Var v) = Var v
+eliminateFailures f (Alg (L x)) = f x
+eliminateFailures f (Alg (R other)) = Alg (hmap (eliminateFailures f) other)
+
