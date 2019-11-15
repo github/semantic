c78334f9a7a696355dde2f8ef3c28e004ba9f6a2
Patrick Thomson <patrickt@github.com>
2019-11-15 12:45:53 -0500

    Add another test case, but I'm not sure that these values are right.

--- semantic-core/test/Graph.hs
+++ semantic-core/test/Graph.hs
@@ -19,6 +19,7 @@ import           Source.Span
 import           Syntax.Term
 import qualified System.Path as Path
 import qualified System.Path.IO as Path (readFile)
+import           Text.Pretty.Simple
 import qualified Text.Trifecta as Trifecta
 
 import qualified Test.Tasty as Tasty
@@ -28,8 +29,8 @@ import qualified Test.Tasty.HUnit as HUnit
 parseEither :: Trifecta.Parser a -> String -> Either String a
 parseEither p = Trifecta.foldResult (Left . show . Trifecta._errDoc) Right . Trifecta.parseString (p <* Trifecta.eof) mempty
 
-testSimpleScopeGraph :: Tasty.TestTree
-testSimpleScopeGraph = HUnit.testCase "simple.score" $ do
+testSimple :: Tasty.TestTree
+testSimple = HUnit.testCase "simple.score" $ do
   let p = Path.absRel "semantic-core/test/fixtures/simple.score"
   contents <- Path.readFile p
   case parseEither Parse.core contents of
@@ -41,8 +42,20 @@ testSimpleScopeGraph = HUnit.testCase "simple.score" $ do
       ref^.span_.start_ @?= Pos 4 2
       ref^.span_.end_   @?= Pos 4 7
 
+testConditional :: Tasty.TestTree
+testConditional = HUnit.testCase "conditional.score" $ do
+  let p = Path.absRel "semantic-core/test/fixtures/conditional.score"
+  contents <- Path.readFile p
+  case parseEither Parse.core contents of
+    Left m  -> HUnit.assertFailure ("Couldn't parse conditional.score: " <> m)
+    Right (x :: Term (Ann Span :+: Core) Name) -> do
+      (heap, [File { fileBody = Right (ScopeGraph res) }]) <- pure (scopeGraph Eval.eval [File p lowerBound x])
+      [(merle, [refm]), (taako, [reft])] <- pure (Map.toList res)
+      refm^.span_.start_ @?= Pos 6 4
+      reft^.span_.start_ @?= Pos 7 9
 
 testTree :: Tasty.TestTree
 testTree = Tasty.testGroup "Core scope graphing"
-  [ testSimpleScopeGraph
+  [ testSimple
+  , testConditional
   ]
