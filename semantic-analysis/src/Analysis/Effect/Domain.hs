9a3e06b0704925223f91dd3977dd1b03890600d2
Rob Rix <robrix@github.com>
2019-12-13 11:36:49 -0500

    Define smart constructors for unit, bool, & string construction.

--- semantic-analysis/src/Analysis/Effect/Domain.hs
+++ semantic-analysis/src/Analysis/Effect/Domain.hs
@@ -3,6 +3,9 @@ module Analysis.Effect.Domain
 ( -- * Domain effect
   abstract
 , concretize
+, unit
+, bool
+, string
 , Domain(..)
   -- * Re-exports
 , Algebra
@@ -10,8 +13,9 @@ module Analysis.Effect.Domain
 , run
 ) where
 
-import Analysis.Intro
+import Analysis.Intro as A
 import Control.Algebra
+import Data.Text (Text)
 import GHC.Generics (Generic1)
 
 abstract :: Has (Domain abstract) sig m => Intro -> m abstract
@@ -19,6 +23,16 @@ abstract concrete = send (Abstract concrete pure)
 
 concretize :: Has (Domain abstract) sig m => abstract -> m Intro
 concretize abstract = send (Concretize abstract pure)
+
+
+unit :: Has (Domain abstract) sig m => m abstract
+unit = abstract A.Unit
+
+bool :: Has (Domain abstract) sig m => Bool -> m abstract
+bool = abstract . A.Bool
+
+string :: Has (Domain abstract) sig m => Text -> m abstract
+string = abstract . A.String
 
 
 data Domain abstract m k
