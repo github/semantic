470092b60db1b264d3473d814050c3ec5332c158
Ayman Nadeem <aymannadeem@github.com>
2019-12-18 17:26:34 -0500

    WIP comment describing why we need GValue and assoc method

--- semantic-ast/src/Marshal/JSON.hs
+++ semantic-ast/src/Marshal/JSON.hs
@@ -67,8 +67,8 @@ instance GFields fields => GMarshalJSON (C1 c fields) where
 instance (GFields f, GFields g) => GFields (f :*: g) where
   gfields acc (f :*: g) = gfields (gfields acc g) f
 
--- To actually get a value out of this datum, we'll need one more typeclass. Let's call its method 'gvalue'.
 -- Implement base case for products
+-- To actually get a value out of this datum, we'll need one more typeclass: GValue. Let's call its method 'gvalue'.
 instance (GValue p, Selector s) => GFields (S1 s p) where
   gfields acc x = (Text.pack (selName x), gvalue (unM1 x)) : acc
 -- knows what the type of x is, whereas M1 has parameters that can be instantiated to anything
