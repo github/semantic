1d292b03950ed6ab037949c211fe8fd57844c86f
Rob Rix <robrix@github.com>
2019-12-19 11:06:40 -0500

    Parameterize terms by addresses.

--- semantic-analysis/src/Analysis/Effect/Domain.hs
+++ semantic-analysis/src/Analysis/Effect/Domain.hs
@@ -17,47 +17,46 @@ module Analysis.Effect.Domain
 
 import Analysis.Intro (Intro)
 import qualified Analysis.Intro as A
-import Analysis.Name
 import Control.Algebra
 import Control.Monad ((>=>))
 import Control.Monad.Fail as Fail
 import Data.Text (Text)
 import GHC.Generics (Generic1)
 
-abstract :: Has (Domain term abstract) sig m => Intro term Name -> m abstract
+abstract :: Has (Domain term addr abstract) sig m => Intro term addr -> m abstract
 abstract concrete = send (Abstract concrete pure)
 
-concretize :: Has (Domain term abstract) sig m => abstract -> m (Intro term Name)
+concretize :: Has (Domain term addr abstract) sig m => abstract -> m (Intro term addr)
 concretize abstract = send (Concretize abstract pure)
 
 
-unit :: forall term abstract m sig . Has (Domain term abstract) sig m => m abstract
-unit = abstract @term A.Unit
+unit :: forall term addr abstract m sig . Has (Domain term addr abstract) sig m => m abstract
+unit = abstract @term @addr A.Unit
 
-bool :: forall term abstract m sig . Has (Domain term abstract) sig m => Bool -> m abstract
-bool = abstract @term . A.Bool
+bool :: forall term addr abstract m sig . Has (Domain term addr abstract) sig m => Bool -> m abstract
+bool = abstract @term @addr . A.Bool
 
-asBool :: forall term abstract m sig . (Has (Domain term abstract) sig m, MonadFail m, forall a . Show a => Show (term a)) => abstract -> m Bool
-asBool = concretize @term >=> \case
+asBool :: forall term addr abstract m sig . (Has (Domain term addr abstract) sig m, MonadFail m, forall a . Show a => Show (term a), Show addr) => abstract -> m Bool
+asBool = concretize @term @addr >=> \case
   A.Bool b -> pure b
   other    -> typeError "Bool" other
 
-string :: forall term abstract m sig . Has (Domain term abstract) sig m => Text -> m abstract
-string = abstract @term . A.String
+string :: forall term addr abstract m sig . Has (Domain term addr abstract) sig m => Text -> m abstract
+string = abstract @term @addr . A.String
 
-asString :: forall term abstract m sig . (Has (Domain term abstract) sig m, MonadFail m, forall a . Show a => Show (term a)) => abstract -> m Text
-asString = concretize @term >=> \case
+asString :: forall term addr abstract m sig . (Has (Domain term addr abstract) sig m, MonadFail m, forall a . Show a => Show (term a), Show addr) => abstract -> m Text
+asString = concretize @term @addr >=> \case
   A.String t -> pure t
   other      -> typeError "String" other
 
 
-data Domain term abstract m k
-  = Abstract   (Intro term Name) (abstract        -> m k)
-  | Concretize abstract          (Intro term Name -> m k)
+data Domain term addr abstract m k
+  = Abstract   (Intro term addr) (abstract        -> m k)
+  | Concretize abstract          (Intro term addr -> m k)
   deriving (Functor, Generic1)
 
-instance HFunctor (Domain term abstract)
-instance Effect   (Domain term abstract)
+instance HFunctor (Domain term addr abstract)
+instance Effect   (Domain term addr abstract)
 
 
 typeError :: (Show a, MonadFail m) => String -> a -> m b
