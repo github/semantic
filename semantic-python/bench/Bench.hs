ac0cc2f60fd65aa756fe22251db6913475208aaf
Patrick Thomson <patrickt@github.com>
2019-12-19 16:39:00 -0500

    Print out number of terms.

--- semantic-python/bench/Bench.hs
+++ semantic-python/bench/Bench.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE LambdaCase, TypeOperators #-}
 
 module Main (main) where
 
@@ -13,9 +13,32 @@ import qualified Core.Parser
 import qualified Data.ByteString as ByteString
 import           Gauge
 import           Source.Span
+import           Syntax.Scope
 import           Syntax.Term
+import           Syntax.Var
 import qualified System.Path as Path
 import qualified Text.Trifecta as Trifecta
+
+count :: Term (Ann Span :+: Core) Int -> Int
+count = \case
+  Var n -> n
+  Alg (L (Ann _ a)) -> count a
+  Alg (R core) -> case core of
+    a :$ b                     -> 1 + count a + count b
+    a :>> b                    -> 1 + count a + count b
+    (Named _ a) :>>= (Scope x) -> 1 + count a + count (fmap (unVar (const 0) count) x)
+    Lam (Named _ (Scope x))    -> 1 + count (fmap (unVar (const 0) count) x)
+    Rec (Named _ (Scope x))    -> 1 + count (fmap (unVar (const 0) count) x)
+    Unit                       -> 1
+    Bool _                     -> 1
+    If a b c                   -> 1 + count a + count b + count c
+    String _                   -> 1
+    Load _                     -> 1
+    Record xs                  -> sum (fmap (\(_, a) -> 1 + count a) xs)
+    a :. _                     -> 2 + count a
+    a :? _                     -> 2 + count a
+    a := b                     -> 1 + count a + count b
+
 
 parsePrelude :: IO (Term (Ann Span :+: Core) Name)
 parsePrelude = do
@@ -31,6 +54,7 @@ graphPrelude t = scopeGraph Eval.eval [File (Path.absRel "<interactive>") (Span 
 main :: IO ()
 main = do
   prel <- parsePrelude
+  putStrLn ("Count of terms in prelude: " <> show (count (1 <$ prel)))
   defaultMain
     [ bgroup "prelude"
       [ bench "parse" $ whnfIO parsePrelude
