f7c2c74ef832d4ecd5fd6a411863b49e8496824e
Rob Rix <robrix@github.com>
2019-12-19 12:53:39 -0500

    Take addressed terms in Concrete.

--- semantic-analysis/src/Analysis/Concrete.hs
+++ semantic-analysis/src/Analysis/Concrete.hs
@@ -52,11 +52,11 @@ type Heap term = IntMap.IntMap (Concrete term)
 concrete
   :: (forall sig m
      .  (Has (Reader Path.AbsRelFile) sig m, Has (Reader Span) sig m, MonadFail m)
-     => (term Name -> m (Concrete (term Name)))
-     -> (term Name -> m (Concrete (term Name)))
+     => (term Addr -> m (Concrete (term Addr)))
+     -> (term Addr -> m (Concrete (term Addr)))
      )
-  -> [File (term Name)]
-  -> (Heap (term Name), [File (Either (Path.AbsRelFile, Span, String) (Concrete (term Name)))])
+  -> [File (term Addr)]
+  -> (Heap (term Addr), [File (Either (Path.AbsRelFile, Span, String) (Concrete (term Addr)))])
 concrete eval
   = run
   . evalFresh 0
@@ -70,11 +70,11 @@ runFile
      )
   => (forall sig m
      .  (Has (Reader Path.AbsRelFile) sig m, Has (Reader Span) sig m, MonadFail m)
-     => (term Name -> m (Concrete (term Name)))
-     -> (term Name -> m (Concrete (term Name)))
+     => (term Addr -> m (Concrete (term Addr)))
+     -> (term Addr -> m (Concrete (term Addr)))
      )
-  -> File (term Name)
-  -> m (File (Either (Path.AbsRelFile, Span, String) (Concrete (term Name))))
+  -> File (term Addr)
+  -> m (File (Either (Path.AbsRelFile, Span, String) (Concrete (term Addr))))
 runFile eval file = traverse run file
   where run = runReader (filePath file)
             . runReader (fileSpan file)
@@ -86,10 +86,10 @@ runFile eval file = traverse run file
 -- concreteAnalysis
 --   :: forall term m sig
 --   .  ( Has (A.Env Addr) sig m
---      , Has (A.Heap Addr (Concrete (term Name))) sig m
---      , Has (State (Heap (term Name))) sig m
+--      , Has (A.Heap Addr (Concrete (term Addr))) sig m
+--      , Has (State (Heap (term Addr))) sig m
 --      )
---   => Analysis Addr (Concrete (term Name)) m
+--   => Analysis Addr (Concrete (term Addr)) m
 -- concreteAnalysis = Analysis{..}
 --   where -- abstract _ name body = do
 --         --   path <- ask
@@ -109,12 +109,12 @@ runFile eval file = traverse run file
 --             pure (name, addr)
 --           pure (Record (Map.fromList fields'))
 --         addr ... n = do
---           val <- A.deref @Addr @(Concrete (term Name)) addr
+--           val <- A.deref @Addr @(Concrete (term Addr)) addr
 --           heap <- get
 --           pure (val >>= lookupConcrete heap n)
 
 
--- lookupConcrete :: Heap (term Name) -> Name -> Concrete (term Name) -> Maybe Addr
+-- lookupConcrete :: Heap (term Addr) -> Name -> Concrete (term Addr) -> Maybe Addr
 -- lookupConcrete heap name = run . evalState IntSet.empty . runNonDetA . inConcrete
 --   where -- look up the name in a concrete value
 --         inConcrete = inFrame <=< maybeA . recordFrame
@@ -136,7 +136,7 @@ runFile eval file = traverse run file
 --   > λ let (heap, res) = concrete [ruby]
 --   > λ writeFile "/Users/rob/Desktop/heap.dot" (export (addressStyle heap) (heapAddressGraph heap))
 --   > λ :!dot -Tsvg < ~/Desktop/heap.dot > ~/Desktop/heap.svg
-heapGraph :: (Addr -> Concrete (term Name) -> a) -> (Either Edge Name -> Addr -> G.Graph a) -> Heap (term Name) -> G.Graph a
+heapGraph :: (Addr -> Concrete (term Addr) -> a) -> (Either Edge Name -> Addr -> G.Graph a) -> Heap (term Addr) -> G.Graph a
 heapGraph vertex edge h = foldr (uncurry graph) G.empty (IntMap.toList h)
   where graph k v rest = (G.vertex (vertex k v) `G.connect` outgoing v) `G.overlay` rest
         outgoing = \case
@@ -146,14 +146,14 @@ heapGraph vertex edge h = foldr (uncurry graph) G.empty (IntMap.toList h)
           Closure _ _ _ _ env -> foldr (G.overlay . edge (Left Lexical)) G.empty env
           Record frame -> Map.foldrWithKey (\ k -> G.overlay . edge (Right k)) G.empty frame
 
-heapValueGraph :: Heap (term Name) -> G.Graph (Concrete (term Name))
+heapValueGraph :: Heap (term Addr) -> G.Graph (Concrete (term Addr))
 heapValueGraph h = heapGraph (const id) (const fromAddr) h
   where fromAddr addr = maybe G.empty G.vertex (IntMap.lookup addr h)
 
-heapAddressGraph :: Heap (term Name) -> G.Graph (EdgeType (term Name), Addr)
+heapAddressGraph :: Heap (term Addr) -> G.Graph (EdgeType (term Addr), Addr)
 heapAddressGraph = heapGraph (\ addr v -> (Value v, addr)) (fmap G.vertex . (,) . either Edge Slot)
 
-addressStyle :: Heap (term Name) -> G.Style (EdgeType (term Name), Addr) Text
+addressStyle :: Heap (term Addr) -> G.Style (EdgeType (term Addr), Addr) Text
 addressStyle heap = (G.defaultStyle vertex) { G.edgeAttributes }
   where vertex (_, addr) = pack (show addr) <> " = " <> maybe "?" fromConcrete (IntMap.lookup addr heap)
         edgeAttributes _ (Slot name,    _) = ["label" G.:= unName name]
