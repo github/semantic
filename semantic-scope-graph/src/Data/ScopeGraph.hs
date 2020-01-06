eaeb5b621978d9a8e71840c532e2ddf45c5759ba
joshvera <josh@joshvera.com>
2020-01-06 18:12:42 -0500

    stub out a scope graph effect

--- semantic-scope-graph/src/Data/ScopeGraph.hs
+++ semantic-scope-graph/src/Data/ScopeGraph.hs
@@ -1,1 +1,12 @@
 module Data.ScopeGraph () where
+
+import Source.Loc (Loc(..))
+import Source.Source as Source
+
+data ScopeGraph = ScopeGraph
+
+class ToScopeGraph t where
+    scopeGraph :: Source -> t Loc -> ScopeGraph
+
+runScopeGraph :: Source -> ReaderC ScopeGraph Identity () -> ScopeGraph
+runScopeGraph = undefined
