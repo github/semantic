6ff8abbe41feef0c2246c6aac1884b8e55d7e897
joshvera <josh@joshvera.com>
2020-01-08 13:13:48 -0500

    spit out a python tree
    Co-Authored-By: Patrick Thomson <patrickt@users.noreply.github.com>

--- semantic-python/app/Main.hs
+++ semantic-python/app/Main.hs
@@ -1,4 +1,16 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, TypeApplications, TypeOperators #-}
 module Main (main) where
 
+import qualified Data.ByteString as ByteString
+import Language.Python.ScopeGraph
+import qualified TreeSitter.Python.AST as Py
+import qualified TreeSitter.Python as TSP
+import qualified TreeSitter.Unmarshal as TS
+import Source.Span
+import Source.Range
+
 main :: IO ()
-main = putStrLn "hell world"
+main = do
+  file <- ByteString.readFile "semantic-python/test/fixtures/1-01-empty-module.py"
+  tree <- TS.parseByteString @Py.Module @(Range, Span) TSP.tree_sitter_python file
+  print (show tree)
