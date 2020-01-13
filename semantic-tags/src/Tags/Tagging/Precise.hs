666c09b7e2525ace462bea54274c3d09a496e30d
Rob Rix <rob.rix@me.com>
2020-01-13 13:49:11 -0500

    :memo: Traversable1.

--- semantic-tags/src/Tags/Tagging/Precise.hs
+++ semantic-tags/src/Tags/Tagging/Precise.hs
@@ -97,6 +97,8 @@ instance GFoldable1 c U1 where
 
 -- FIXME: move Traversable1 into semantic-ast.
 -- FIXME: derive Traversable1 instances for TH-generated syntax types.
+
+-- | Simultaneous traversal of subterms of kind @*@ and @* -> *@ in an 'Applicative' context.
 class Traversable1 c t where
   -- | Map annotations and subterms of kind @* -> *@ into an 'Applicative' context.
   traverse1
