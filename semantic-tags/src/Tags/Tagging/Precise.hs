81aab02035867556f3cbcc055008320dd1883eec
Rob Rix <rob.rix@me.com>
2020-01-13 14:24:42 -0500

    :fire: GFoldable1.

--- semantic-tags/src/Tags/Tagging/Precise.hs
+++ semantic-tags/src/Tags/Tagging/Precise.hs
@@ -17,7 +17,6 @@ module Tags.Tagging.Precise
 , yield
 , runTagging
 , firstLine
-, GFoldable1(..)
 , Traversable1(..)
 , for1
 , foldMap1
@@ -59,41 +58,6 @@ runTagging source
 
 firstLine :: Source -> Text
 firstLine = Text.takeWhile (/= '\n') . toText . Source.take 180
-
-
--- FIXME: move GFoldable1 into semantic-ast.
-class GFoldable1 c t where
-  -- | Generically map functions over fields of kind @* -> *@, monoidally combining the results.
-  gfoldMap1
-    :: Monoid b
-    => (forall f . c f => f a -> b)
-    -> t a
-    -> b
-
-instance GFoldable1 c f => GFoldable1 c (M1 i c' f) where
-  gfoldMap1 alg = gfoldMap1 @c alg . unM1
-
-instance (GFoldable1 c f, GFoldable1 c g) => GFoldable1 c (f :*: g) where
-  gfoldMap1 alg (f :*: g) = gfoldMap1 @c alg f <> gfoldMap1 @c alg g
-
-instance (GFoldable1 c f, GFoldable1 c g) => GFoldable1 c (f :+: g) where
-  gfoldMap1 alg (L1 l) = gfoldMap1 @c alg l
-  gfoldMap1 alg (R1 r) = gfoldMap1 @c alg r
-
-instance GFoldable1 c (K1 R t) where
-  gfoldMap1 _ _ = mempty
-
-instance GFoldable1 c Par1 where
-  gfoldMap1 _ _ = mempty
-
-instance c t => GFoldable1 c (Rec1 t) where
-  gfoldMap1 alg (Rec1 t) = alg t
-
-instance (Foldable f, GFoldable1 c g) => GFoldable1 c (f :.: g) where
-  gfoldMap1 alg = foldMap (gfoldMap1 @c alg) . unComp1
-
-instance GFoldable1 c U1 where
-  gfoldMap1 _ _ = mempty
 
 
 -- FIXME: move Traversable1 into semantic-ast.
