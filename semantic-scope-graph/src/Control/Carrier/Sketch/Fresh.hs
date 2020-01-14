353311e2108d6948da5a7ad129fbdad6a24e30b4
Patrick Thomson <patrickt@github.com>
2020-01-14 11:43:28 -0500

    Remove old graph manipulation code

--- semantic-scope-graph/src/Control/Carrier/Sketch/Fresh.hs
+++ semantic-scope-graph/src/Control/Carrier/Sketch/Fresh.hs
@@ -41,22 +41,11 @@ newtype SketchC address m a = SketchC (StateC (Sketchbook address) (FreshC m) a)
 
 instance forall address sig m . (Effect sig, Algebra sig m) => Algebra (Sketch address :+: sig) (SketchC address m) where
   alg (L (Declare name _props k)) = do
-    ua <- SketchC fresh
-    ub <- SketchC fresh
-    parent <- SketchC (gets @(Sketchbook address) getParent)
-
-    let newScope = ScopeGraph.scope ua
-    let newDecl  = ScopeGraph.decl ub name
-    let newGraph = ScopeGraph.edge parent newScope
-                   <> ScopeGraph.edge newScope newDecl
-
-    SketchC (modify ())
-
     k ()
   alg (R other) = SketchC (alg (R (R (handleCoercible other))))
 
 runSketch ::
-  (ScopeGraph.Addressable address, Functor m)
+  (Ord address, Functor m)
   => Maybe Path.AbsRelFile
   -> SketchC address m a
   -> m (ScopeGraph address, a)
