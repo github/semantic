f94e6c5431dafa7abd0542d33e5ddc2237baba86
Patrick Thomson <patrickt@github.com>
2020-01-14 14:53:10 -0500

    get our first test cases up

--- semantic-python/test-graphing/GraphTest.hs
+++ semantic-python/test-graphing/GraphTest.hs
@@ -8,7 +8,6 @@ module Main (main) where
 
 import           Control.Algebra
 import           Control.Carrier.Sketch.Fresh
-import           Control.Monad
 import           Convert.ToScopeGraph
 import qualified Data.ByteString as ByteString
 import           Data.Name (Name)
@@ -18,6 +17,8 @@ import           Source.Loc
 import qualified Source.Source as Source
 import           System.Exit (die)
 import qualified System.Path as Path
+import qualified Test.Tasty as Tasty
+import qualified Test.Tasty.HUnit as HUnit
 import qualified TreeSitter.Python as TSP
 import qualified TreeSitter.Python.AST as Py
 import qualified TreeSitter.Unmarshal as TS
@@ -52,18 +53,15 @@ sampleGraphThing = do
   declare @Name "goodbye" DeclProperties
   pure Complete
 
-
-assertEqual :: (Show a, Eq a) => a -> a -> IO ()
-assertEqual a b = unless (a == b) (die (show a <> "\ndoes not equal\n" <> show b))
-
-main :: IO ()
-main = do
+assertSimpleAssignment :: HUnit.Assertion
+assertSimpleAssignment = do
   let path = "semantic-python/test/fixtures/1-04-toplevel-assignment.py"
   file <- ByteString.readFile path
   tree <- TS.parseByteString @Py.Module @Loc TSP.tree_sitter_python file
   pyModule <- either die pure tree
   let (expecto, Complete) = run $ runSketch Nothing sampleGraphThing
   let (result, Complete) = runScopeGraph (Path.absRel path) (Source.fromUTF8 file) pyModule
-  print result
-  assertEqual expecto result
+  HUnit.assertEqual "Should work for simple case" expecto result
 
+main :: IO ()
+main = Tasty.defaultMain (HUnit.testCase "toplevel assignment" assertSimpleAssignment)
