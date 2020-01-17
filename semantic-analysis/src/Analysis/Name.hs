a4a23e9a510c3c8a240528d02f175f095db69d17
Patrick Thomson <patrickt@github.com>
2020-01-17 10:31:09 -0500

    Move the Named functor to Analysis.Functor.Named.

--- semantic-analysis/src/Analysis/Name.hs
+++ semantic-analysis/src/Analysis/Name.hs
@@ -1,14 +1,9 @@
-{-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Analysis.Name
 ( Name(..)
-, Named(..)
-, named
-, named'
-, namedName
-, namedValue
 ) where
 
-import Data.Function (on)
 import Data.String (IsString)
 import Data.Text (Text)
 
@@ -17,24 +12,3 @@ newtype Name = Name { unName :: Text }
   deriving (Eq, IsString, Ord, Show)
 
 
--- | Annotates an @a@ with a 'Name'-provided name, which is ignored for '==' and 'compare'.
-data Named a = Named Name a
-  deriving (Foldable, Functor, Show, Traversable)
-
-named :: Name -> a -> Named a
-named = Named
-
-named' :: Name -> Named Name
-named' u = Named u u
-
-namedName :: Named a -> Name
-namedName (Named n _) = n
-
-namedValue :: Named a -> a
-namedValue (Named _ a) = a
-
-instance Eq a => Eq (Named a) where
-  (==) = (==) `on` namedValue
-
-instance Ord a => Ord (Named a) where
-  compare = compare `on` namedValue
