aefd0ee03c4c4af4315c9cadbd99ca0b4f730a3e
Timothy Clem <timothy.clem@gmail.com>
2020-01-24 13:53:14 -0800

    Revert to master for this file

--- semantic-tsx/src/Language/TSX.hs
+++ semantic-tsx/src/Language/TSX.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -freduction-depth=0 #-}
 -- | Semantic functionality for TSX programs.
 module Language.TSX
@@ -13,9 +12,6 @@ import qualified TreeSitter.TSX (tree_sitter_tsx)
 import qualified TreeSitter.TSX.AST as TSX
 import qualified TreeSitter.Unmarshal as TS
 
-import qualified Data.Term as Term
-import Diffing.Interpreter
-
 newtype Term a = Term { getTerm :: TSX.Program a }
 
 instance TS.SymbolMatching Term where
@@ -27,13 +23,3 @@ instance TS.Unmarshal Term where
 
 instance Tags.ToTags Term where
   tags src = Tags.runTagging src . TsxTags.tags . getTerm
-
-instance DiffTerms Term
-  -- diffTermPair :: Edit (term ann1) (term ann2) -> Diff.Diff (Syntax term) ann1 ann2
-
-instance Term.IsTerm Term where
-  type Syntax Term = Term
-  -- toTermF :: term ann -> TermF (Syntax term) ann (term ann)
-  -- toTermF = coerce
-  -- fromTermF :: TermF (Syntax term) ann (term ann) -> term ann
-  -- fromTermF = coerce
