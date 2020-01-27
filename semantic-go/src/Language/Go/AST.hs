6e36439f9c57a514823c763162103bf25b81d425
Ayman Nadeem <aymannadeem@github.com>
2020-01-27 15:39:23 -0500

    add contents to Language.Go.AST

--- semantic-go/src/Language/Go/AST.hs
+++ semantic-go/src/Language/Go/AST.hs
@@ -1,1 +1,21 @@
-module Language.Go.AST () where 
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.Go.AST
+( module Language.Go.AST
+) where
+
+import           Prelude hiding (False, Float, Integer, Rational, String, True)
+import           TreeSitter.GenerateSyntax
+import qualified TreeSitter.Go as Grammar
+
+astDeclarationsForLanguage Grammar.tree_sitter_go "../../vendor/tree-sitter-go/src/node-types.json"
