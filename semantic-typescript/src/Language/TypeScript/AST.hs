7496c67128e3d72322241c4b2fe6fedd54493b57
Ayman Nadeem <aymannadeem@github.com>
2020-01-27 16:06:35 -0500

    add contents to Language.TypeScript.AST

--- semantic-typescript/src/Language/TypeScript/AST.hs
+++ semantic-typescript/src/Language/TypeScript/AST.hs
@@ -1,1 +1,21 @@
-module Language.TypeScript.AST () where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.TypeScript.AST
+( module Language.TypeScript.AST
+) where
+
+import           Prelude hiding (False, Float, Integer, String, True)
+import           TreeSitter.GenerateSyntax
+import qualified TreeSitter.TypeScript as Grammar
+
+astDeclarationsForLanguage Grammar.tree_sitter_typescript "../../../vendor/tree-sitter-typescript/typescript/src/node-types.json"
