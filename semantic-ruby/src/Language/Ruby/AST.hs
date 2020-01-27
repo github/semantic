c448e0f0d992924952e7e08a7b8453878ed94578
Ayman Nadeem <aymannadeem@github.com>
2020-01-27 15:50:51 -0500

    add contents to Language.Ruby.AST

--- semantic-ruby/src/Language/Ruby/AST.hs
+++ semantic-ruby/src/Language/Ruby/AST.hs
@@ -1,1 +1,21 @@
-module Language.Ruby.AST () where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.Ruby.AST
+( module Language.Ruby.AST
+) where
+
+import           Prelude hiding (False, Float, Integer, Rational, String, True)
+import           TreeSitter.GenerateSyntax
+import qualified TreeSitter.Ruby as Grammar
+
+astDeclarationsForLanguage Grammar.tree_sitter_ruby "../../vendor/tree-sitter-ruby/src/node-types.json"
