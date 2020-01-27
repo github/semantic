5f218d909067f25024c6ab01478126d91d3c6485
Ayman Nadeem <aymannadeem@github.com>
2020-01-27 15:17:35 -0500

    add AST contents

--- semantic-java/src/Language/Java/AST.hs
+++ semantic-java/src/Language/Java/AST.hs
@@ -1,1 +1,21 @@
-module Language.Java.AST () where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.Java.AST
+( module Language.Java.AST
+) where
+
+import           TreeSitter.GenerateSyntax
+import qualified TreeSitter.Java as Grammar
+import           TreeSitter.Token
+
+astDeclarationsForLanguage Grammar.tree_sitter_java "../../vendor/tree-sitter-java/src/node-types.json"
