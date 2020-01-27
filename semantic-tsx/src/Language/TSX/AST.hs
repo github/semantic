177ed479ced86729bd7795067f5271f7e9a88f05
Ayman Nadeem <aymannadeem@github.com>
2020-01-27 16:14:20 -0500

    add contents to Language.TSX.AST

--- semantic-tsx/src/Language/TSX/AST.hs
+++ semantic-tsx/src/Language/TSX/AST.hs
@@ -1,1 +1,21 @@
-module Language.TSX.AST () where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.TSX.AST
+( module Language.TSX.AST
+) where
+
+import           Prelude hiding (False, Float, Integer, String, True)
+import           TreeSitter.GenerateSyntax
+import qualified TreeSitter.TSX as Grammar
+
+astDeclarationsForLanguage Grammar.tree_sitter_tsx "../../../vendor/tree-sitter-typescript/tsx/src/node-types.json"
