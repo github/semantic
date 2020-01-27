1d82bcde1ccffea2284ccdc739dae10ef062d91d
Ayman Nadeem <aymannadeem@github.com>
2020-01-27 14:33:52 -0500

    actually add contents to AST

--- semantic-python/src/Language/Python/AST.hs
+++ semantic-python/src/Language/Python/AST.hs
@@ -1,1 +1,21 @@
-module Language.Python.AST () where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.Python.AST
+( module Language.Python.AST
+) where
+
+import           Prelude hiding (False, Float, Integer, String, True)
+import           TreeSitter.GenerateSyntax
+import qualified TreeSitter.Python as Grammar
+
+astDeclarationsForLanguage Grammar.tree_sitter_python "../../../vendor/tree-sitter-python/src/node-types.json"
