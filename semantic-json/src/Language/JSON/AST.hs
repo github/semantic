0945a3a74f7e0f482f938df1ba57510fa1527df3
Rob Rix <rob.rix@me.com>
2020-01-27 13:57:41 -0500

    Copy in the AST generation from tree-sitter-json.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- semantic-json/src/Language/JSON/AST.hs
+++ semantic-json/src/Language/JSON/AST.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 module Language.JSON.AST
-() where
+( module Language.JSON.AST
+) where
+
+import           Prelude hiding (String)
+import           TreeSitter.GenerateSyntax
+import qualified TreeSitter.JSON as Grammar
+
+astDeclarationsForLanguage Grammar.tree_sitter_json "../../../vendor/tree-sitter-json/src/node-types.json"
