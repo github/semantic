ccd51b3be707e4597574a2f83b12f2249fe30d12
Ayman Nadeem <aymannadeem@github.com>
2020-02-03 16:58:24 -0500

    add Go test

---
+++ semantic-go/test/Test.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DisambiguateRecordFields, OverloadedStrings, TypeApplications #-}
+module Main (main) where
+
+import qualified System.Path as Path
+import           Test.Tasty
+import           Language.Go.Grammar
+import qualified Language.Go.AST as Go
+import           TreeSitter.Test.Helpers
+import           AST.Unmarshal
+
+main :: IO ()
+main
+  =   readCorpusFiles (Path.relDir "tree-sitter-go/vendor/tree-sitter-go/corpus")
+  >>= traverse (testCorpus parse)
+  >>= defaultMain . tests
+  where parse = parseByteString @Go.SourceFile @() tree_sitter_go
+
+tests :: [TestTree] -> TestTree
+tests = testGroup "tree-sitter-go corpus tests"
