7e08d781c94290e88045441a16a2c7c52506d2b3
Rob Rix <rob.rix@me.com>
2020-02-04 14:13:10 -0500

    Add a module header describing the organizational concerns.

--- semantic-ast/src/AST/Traversable1/Class.hs
+++ semantic-ast/src/AST/Traversable1/Class.hs
@@ -1,2 +1,3 @@
+-- | This module defines the 'Traversable1' class and its generic derivation using 'GTraversable1'. Note that any changes to this file will require recompilation of all of the AST modules, which is quite expensive; thus, most additions should be made in "AST.Traversable1" instead, and that that module should not be imported by the AST modules.
 module AST.Traversable1.Class
 () where
