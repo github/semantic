0980c942df5a230670ddd1fa104da37a25648cb9
Ayman Nadeem <aymannadeem@github.com>
2020-02-06 16:30:17 -0500

    Update PreciseTest.hs

--- semantic-python/test/PreciseTest.hs
+++ semantic-python/test/PreciseTest.hs
@@ -5,8 +5,8 @@ import qualified System.Path as Path
 import           Test.Tasty
 import           TreeSitter.Python
 import qualified TreeSitter.Python.AST as Py
-import           TreeSitter.Test.Helpers
-import           TreeSitter.Unmarshal
+import           AST.Test
+import           AST.Unmarshal
 
 main :: IO ()
 main
