96732a7cff809fd8efc5b4dfaa777cc6d45511c2
Ayman Nadeem <aymannadeem@github.com>
2020-02-06 16:30:39 -0500

    Update Test.hs

--- semantic-ruby/test/Test.hs
+++ semantic-ruby/test/Test.hs
@@ -5,8 +5,8 @@ import qualified System.Path as Path
 import           Test.Tasty
 import           TreeSitter.Ruby
 import qualified TreeSitter.Ruby.AST as Rb
-import           TreeSitter.Test.Helpers
-import           TreeSitter.Unmarshal
+import           AST.Test
+import           AST.Unmarshal
 
 main :: IO ()
 main
