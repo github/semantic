f4a6a0a9e50e5ca99db0aeb4317c6b3fb909978b
Ayman Nadeem <aymannadeem@github.com>
2020-02-06 16:29:51 -0500

    change Json test to import AST.Test instead of TreeSitter.Test.Helpers

--- semantic-json/test/Test.hs
+++ semantic-json/test/Test.hs
@@ -5,8 +5,8 @@ import qualified System.Path as Path
 import           Test.Tasty
 import           TreeSitter.JSON
 import qualified TreeSitter.JSON.AST as JSON
-import           TreeSitter.Test.Helpers
-import           TreeSitter.Unmarshal
+import           AST.Test
+import           AST.Unmarshal
 
 main :: IO ()
 main
