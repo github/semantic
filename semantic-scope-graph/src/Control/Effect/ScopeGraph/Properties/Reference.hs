c3e8a15b8163947a682780e03b60392b3fe6b86c
joshvera <josh@joshvera.com>
2020-02-07 18:10:33 -0500

    Update Reference
    should probably remove ReferenceInfo?

--- semantic-scope-graph/src/Control/Effect/ScopeGraph/Properties/Reference.hs
+++ semantic-scope-graph/src/Control/Effect/ScopeGraph/Properties/Reference.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeApplications #-}
+
 -- | The 'Declaration' record type is used by the 'Control.Effect.Sketch' module to keep
 -- track of the parameters that need to be passed when establishing a new reference.
 -- It is currently unused, but will possess more fields in the future as scope graph
@@ -6,4 +10,12 @@ module Control.Effect.ScopeGraph.Properties.Reference
   ( Reference (..)
   ) where
 
+import Data.ScopeGraph as ScopeGraph (Kind, Relation)
+import GHC.Generics (Generic)
+import Source.Span
+
 data Reference = Reference
+  { kind            :: ScopeGraph.Kind
+  , relation        :: ScopeGraph.Relation
+  , span            :: Span
+    } deriving Generic
