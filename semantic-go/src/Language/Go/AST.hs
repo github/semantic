7fe0562bda4c91ed6b3ae4c2bbbe1ea295b30673
Ayman Nadeem <aymannadeem@github.com>
2020-02-08 08:20:34 -0500

    Update AST.hs

--- semantic-go/src/Language/Go/AST.hs
+++ semantic-go/src/Language/Go/AST.hs
@@ -17,6 +17,6 @@ module Language.Go.AST
 import           Prelude hiding (False, Float, Integer, Rational, String, True)
 import           AST.GenerateSyntax
 import           Language.Haskell.TH.Syntax (runIO)
-import qualified TreeSitter.Go as Go
+import qualified TreeSitter.Go as Go (getNodeTypesPath, tree_sitter_go)
 
 runIO Go.getNodeTypesPath >>= astDeclarationsForLanguage Go.tree_sitter_go
