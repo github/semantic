1cf4191d1d0b52d1727593849b41ecdb1861d869
Ayman Nadeem <aymannadeem@github.com>
2020-02-08 08:18:13 -0500

    import getNodeTypesPath, tree_sitter_tsx for Ruby

--- semantic-ruby/src/Language/Ruby/AST.hs
+++ semantic-ruby/src/Language/Ruby/AST.hs
@@ -17,6 +17,6 @@ module Language.Ruby.AST
 import           Prelude hiding (False, Float, Integer, Rational, String, True)
 import           AST.GenerateSyntax
 import           Language.Haskell.TH.Syntax (runIO)
-import qualified TreeSitter.Ruby as Ruby
+import qualified TreeSitter.Ruby as Ruby (getNodeTypesPath, tree_sitter_ruby)
 
 runIO Ruby.getNodeTypesPath >>= astDeclarationsForLanguage Ruby.tree_sitter_ruby
