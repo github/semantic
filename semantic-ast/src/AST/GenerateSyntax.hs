acecb82e5fed343482bf2c51a03d43a6c44a6f78
Ayman Nadeem <aymannadeem@github.com>
2020-02-20 16:09:52 -0500

    add an extra arg to symbolMatchingInstance

--- semantic-ast/src/AST/GenerateSyntax.hs
+++ semantic-ast/src/AST/GenerateSyntax.hs
@@ -120,8 +120,8 @@ makeHasFieldInstance ty param elim =
       getField = TS.gann . $elim |]
 
 -- | Create TH-generated SymbolMatching instances for sums, products, leaves
-symbolMatchingInstance :: [(String, Named)] -> Name -> Named -> DatatypeName -> Q [Dec]
 symbolMatchingInstance allSymbols name named (DatatypeName str) = do
+symbolMatchingInstance :: [(String, Named)] -> Name -> Name -> Named -> DatatypeName -> Q [Dec]
   let tsSymbols = elemIndices (str, named) allSymbols
       names = intercalate ", " $ fmap (debugPrefix . (!!) allSymbols) tsSymbols
   [d|instance TS.SymbolMatching $(conT name) where
