065183b2022650ddc45f8d4789812f41b2ae57d0
Patrick Thomson <patrickt@github.com>
2020-02-21 10:40:40 -0500

    Don't use labelled graphs to underlie Stack.Graph.

--- semantic-scope-graph/src/Stack/Graph.hs
+++ semantic-scope-graph/src/Stack/Graph.hs
@@ -1,37 +1,40 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 
 {-# LANGUAGE OverloadedLists #-}
-module Stack.Graph (Direction(..), Graph(..), Node(..), Symbol(..), (>>-), (-<<), scope, newScope, singleton) where
+module Stack.Graph
+  ( Graph(..)
+  , Node(..)
+  , Symbol(..)
+  , (>>-)
+  , (-<<)
+  , singleton
+  -- * Reexports
+  , Class.empty
+  , Class.vertex
+  , Class.overlay
+  , Class.connect
+  -- * Smart constructors
+  , scope
+  , newScope
+  , declaration
+  , reference
+  , popSymbol
+  , pushSymbol
+  , root
+  , testGraph
+  ) where
 
-import           Algebra.Graph.Label (Dioid (..), Label, Semiring (..))
-import           Algebra.Graph.Labelled ((-<), (>-))
-import qualified Algebra.Graph.Labelled as Labelled
+import qualified Algebra.Graph as Algebraic
+import qualified Algebra.Graph.Class as Class
 import           Analysis.Name (Name)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Semilattice.Lower
 import           Data.String
-import           Data.Text (Text)
-import           Debug.Trace
-import           GHC.Exts
 import qualified Scope.Types as Scope
-
-data Direction = From | To | Bidi | Zero
-    deriving (Show, Eq, Ord)
-instance Semigroup Direction where
-  From <> From = From
-  To <> To = To
-  Zero <> a = a
-  a <> Zero = a
-  _ <> _ = Bidi
-instance Monoid Direction where
-  mempty = Zero
-instance Semiring Direction where
-  one = Zero
-  (<.>) = (<>)
-instance Dioid Direction
 
 newtype Symbol = Symbol Name
     deriving (IsString, Show, Eq, Ord)
@@ -51,27 +54,35 @@ data Node = Root
 instance Lower Node where
   lowerBound = Root
 
-newtype Graph a = Graph { unGraph :: Labelled.Graph Direction a }
+newtype Graph a = Graph { unGraph :: Algebraic.Graph a }
   deriving (Eq, Show)
 
+instance Class.Graph (Stack.Graph.Graph a) where
+  type Vertex (Stack.Graph.Graph a) = a
+  empty = Graph Class.empty
+  vertex = Graph . Class.vertex
+  overlay (Graph a) (Graph b) = Graph (Class.overlay a b)
+  connect (Graph a) (Graph b) = Graph (Class.overlay a b)
+
 instance Lower a => Lower (Graph a) where
-  lowerBound = Graph (Labelled.vertex lowerBound)
+  lowerBound = Graph (Algebraic.vertex lowerBound)
 
 scope, declaration, popSymbol, reference, pushSymbol :: Symbol -> Graph Node
-scope = Graph . Labelled.vertex . Scope
-declaration = Graph . Labelled.vertex . Declaration
-reference = Graph . Labelled.vertex . Reference
-popSymbol = Graph . Labelled.vertex . PopSymbol
-pushSymbol = Graph . Labelled.vertex . PushSymbol
+scope = Class.vertex . Scope
+declaration = Class.vertex . Declaration
+reference = Class.vertex . Reference
+popSymbol = Class.vertex . PopSymbol
+pushSymbol = Class.vertex . PushSymbol
+
 root :: Graph Node
-root = Graph (Labelled.vertex Root)
+root = Graph (Algebraic.vertex Root)
 
 (>>-), (-<<) :: Graph a -> Graph a -> Graph a
-Graph left >>- Graph right = Graph (Labelled.connect From left right)
+Graph left >>- Graph right = Graph (Algebraic.connect left right)
 (-<<) = flip (>>-)
 
 singleton :: Node -> Graph Node
-singleton node = Graph (Labelled.vertex node)
+singleton = Class.vertex
 
 newScope :: Name -> Map Scope.EdgeLabel [Name] -> Graph Node -> Graph Node
 newScope name edges graph =
