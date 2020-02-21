391d0151e6d3f58bc5e7a29f122ad0608561b050
Timothy Clem <timothy.clem@gmail.com>
2020-02-21 09:24:28 -0800

    Clean up import list

--- src/Semantic/Api/ScopeGraph.hs
+++ src/Semantic/Api/ScopeGraph.hs
@@ -5,27 +5,13 @@ import           Control.Effect.Parse
 import           Control.Effect.Reader
 import           Control.Exception
 import           Control.Lens
-import           Data.Abstract.Declarations
 import           Data.Blob
-import           Data.ByteString.Builder
-import           Data.Foldable
-import           Data.Functor.Foldable
 import           Data.Language
-import           Data.Map.Strict (Map)
 import           Data.ProtoLens (defMessage)
-import           Data.Term (IsTerm (..), TermF)
-import           Data.Text (Text)
-import           Data.Text (pack)
-import qualified Parsing.Parser as Parser
 import           Proto.Semantic as P hiding (Blob, BlobPair)
 import           Proto.Semantic_Fields as P
 import           Proto.Semantic_JSON ()
-import           Semantic.Api.Bridge
-import qualified Semantic.Api.LegacyTypes as Legacy
-import           Semantic.Config
 import           Semantic.Task
-import           Serializing.Format (Format)
-import           Source.Loc as Loc
 
 parseScopeGraph :: ( Has Distribute sig m
                    , Has (Error SomeException) sig m
