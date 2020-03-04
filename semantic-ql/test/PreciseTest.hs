df4ffa8691f196ea04be674b5513af8da0288549
Rick Winfrey <rewinfrey@github.com>
2020-03-04 09:04:02 -0800

    Update semantic-ql to use tree-sitter-ql package from hackage

---
+++ semantic-ql/test/PreciseTest.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DisambiguateRecordFields, OverloadedStrings, TypeApplications #-}
+module Main (main) where
+
+import qualified System.Path as Path
+import           Test.Tasty
+import qualified Language.QL.AST as QL
+import           Language.QL.Grammar
+import           AST.Test
+import           AST.Unmarshal
+
+main :: IO ()
+main
+  =   Path.absDir <$> QL.getTestCorpusDir
+  >>= readCorpusFiles'
+  >>= traverse (testCorpus parse)
+  >>= defaultMain . tests
+  where parse = parseByteString @QL.Module @() tree_sitter_ql
+
+tests :: [TestTree] -> TestTree
+tests = testGroup "tree-sitter-python corpus tests"
