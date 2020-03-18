36b63d401068c246d2376056f2dce09b6fcbc0e4
Patrick Thomson <patrickt@github.com>
2020-03-18 17:53:02 -0400

    Remove unused Edge functions.

--- semantic-stack-graph/src/Stack/Edge.hs
+++ semantic-stack-graph/src/Stack/Edge.hs
@@ -2,14 +2,10 @@
 
 module Stack.Edge
   ( Edge (..)
-  , parseEdges
-  , formatEdge
   ) where
 
-import           Control.Comonad
-import           Data.Text (Text)
-import qualified Data.Text as T
-import           Stack.Node (Node)
+import Data.Text (Text)
+import Stack.Node (Node)
 
 -- | This is suitable for conversion from (label, node, node) tuples.
 data Edge = Edge
@@ -17,12 +13,3 @@ data Edge = Edge
   , sinkNode   :: Node
   , label      :: Text
   } deriving (Eq, Show, Ord)
-
-parseEdges :: Text -> [Edge]
-parseEdges = const []
-
-formatEdge :: Edge -> Text
-formatEdge (Edge src sink lab) =
-  get src <> ":" <> get sink <> ":" <> lab
-    where get = T.pack . show . extract
-
