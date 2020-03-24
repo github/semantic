3962e510294a160befcae0fecc7b3f87711bd677
Patrick Thomson <patrickt@github.com>
2020-03-24 11:17:15 -0400

    Stray import.

--- semantic-analysis/src/Analysis/File.hs
+++ semantic-analysis/src/Analysis/File.hs
@@ -7,7 +7,6 @@ module Analysis.File
 ) where
 
 import           Data.Maybe (fromJust, listToMaybe)
-import           Data.Semilattice.Lower
 import           GHC.Stack
 import           Source.Language as Language
 import           Source.Span
