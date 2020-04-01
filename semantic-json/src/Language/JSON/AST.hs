e748ca79b6490a6657fb5c3007e70a9d9ca14e21
Patrick Thomson <patrickt@github.com>
2020-04-01 12:34:24 -0400

    We need -XKindSignatures here.

--- semantic-json/src/Language/JSON/AST.hs
+++ semantic-json/src/Language/JSON/AST.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
@@ -14,9 +15,9 @@ module Language.JSON.AST
 , JSON.getTestCorpusDir
 ) where
 
-import           Prelude hiding (String)
 import           AST.GenerateSyntax
 import           Language.Haskell.TH.Syntax (runIO)
+import           Prelude hiding (String)
 import qualified TreeSitter.JSON as JSON (getNodeTypesPath, getTestCorpusDir, tree_sitter_json)
 
 runIO JSON.getNodeTypesPath >>= astDeclarationsForLanguage JSON.tree_sitter_json
