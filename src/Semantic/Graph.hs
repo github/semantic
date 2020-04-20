c2542a1e93de614e440b667a72b4d363a5c1587c
Jinxuan Zhu <zhujinxuan@gmail.com>
2020-04-20 00:38:05 -0500

    Add traceError with empty dir

--- src/Semantic/Graph.hs
+++ src/Semantic/Graph.hs
@@ -68,13 +68,13 @@ import           Data.Abstract.Value.Concrete as Concrete (Value, ValueError (..
 import           Data.Abstract.Value.Type as Type
 import           Data.Blob
 import           Data.Functor.Foldable
+import           Data.Functor  (($>))
 import           Data.Graph.Algebraic
 import           Data.Graph.ControlFlowVertex (VertexDeclaration)
 import           Data.Language as Language
 import           Data.List (find, isPrefixOf)
 import           Data.Map (Map)
 import qualified Data.Map as Map
-import           Data.Maybe (fromMaybe)
 import           Data.Proxy
 import           Data.Text (pack, unpack)
 import           Language.Haskell.HsColour
@@ -384,7 +384,7 @@ resumingResolutionError :: ( Has Trace sig m
 resumingResolutionError = runResolutionErrorWith $ \ baseError -> do
   traceError "ResolutionError" baseError
   case baseErrorException baseError of
-    NotFoundError nameToResolve _ _ -> pure $ fromMaybe (Path.toAbsRel Path.emptyFile) $ Path.fileFromFileDir nameToResolve
+    NotFoundError nameToResolve _ _ -> maybe (traceError ("NotFileError: resolve path is " ++ show nameToResolve) baseError $> Path.toAbsRel Path.emptyFile) pure $ Path.fileFromFileDir nameToResolve
     GoImportError pathToResolve     -> pure [Path.absRel pathToResolve]
 
 resumingLoadError :: ( Has Trace sig m
