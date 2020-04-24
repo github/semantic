e3a46c4405f1f74e5f4878d66eb3397b456405e7
Ayman Nadeem <aymannadeem@github.com>
2020-04-24 13:32:18 -0400

    stub out functor instance

--- semantic-ast/src/AST/Parse.hs
+++ semantic-ast/src/AST/Parse.hs
@@ -15,3 +15,7 @@ import GHC.TypeLits (Symbol, Nat)
 -- @
 data Err fail succeed = parseL (fail :: String) | parseR (succeed :: Symbol)
   deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Show, Traversable)
+
+instance Functor (Err a) where
+    fmap f (parseL x) = parseL x
+    fmap f (parseR y) = parseR (f y)
