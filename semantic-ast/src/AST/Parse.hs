f3810df48163d733d026a90fbd2e2efdd03ea930
Ayman Nadeem <aymannadeem@github.com>
2020-04-28 10:26:03 -0400

    Err wraps a but has no args

--- semantic-ast/src/AST/Parse.hs
+++ semantic-ast/src/AST/Parse.hs
@@ -7,13 +7,12 @@ import GHC.Generics (Generic, Generic1)
 
 -- | An AST node representing an Error, showing a parse that's succeeded or failed.
 --
--- Error types are isomorphic to Either.
+-- Error types are isomorphic to Either String. 
 --
 -- For example, consider the following:
 -- @
 -- data If f a = If { ann :: a, condition :: f (Expr f a), consequence :: f (Stmt f a), alternative :: f (Stmt f a) }
 -- @
 -- When the parse fails, the f will be substituted with Err
-data Err fail succeed a = ParseFail { ann :: a } | ParseSucceed { ann :: a }
+data Err a = ParseFail { ann :: a } | ParseSucceed { ann :: a }
   deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Show, Traversable)
-
