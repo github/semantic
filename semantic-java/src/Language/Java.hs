188fe07b11b1c656c8b3e7df15cf0e6d443aa0b6
Ayman Nadeem <aymannadeem@github.com>
2020-04-29 13:55:36 -0400

    parameterize Java program with Err

--- semantic-java/src/Language/Java.hs
+++ semantic-java/src/Language/Java.hs
@@ -4,12 +4,13 @@ module Language.Java
 , Language.Java.Grammar.tree_sitter_java
 ) where
 
+import qualified AST.Parse as Parse
+import qualified AST.Unmarshal as TS
 import           Data.Proxy
 import qualified Language.Java.AST as Java
 import qualified Language.Java.Tags as JavaTags
 import qualified Tags.Tagging.Precise as Tags
 import qualified Language.Java.Grammar (tree_sitter_java)
-import qualified AST.Unmarshal as TS
 
 newtype Term a = Term { getTerm :: Java.Program Err a }
 
