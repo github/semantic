effeebaee18b0c8686597199a04894b81acbf3ee
Ayman Nadeem <aymannadeem@github.com>
2020-04-29 13:56:26 -0400

    parameterize TSX program with Err

--- semantic-tsx/src/Language/TSX.hs
+++ semantic-tsx/src/Language/TSX.hs
@@ -5,14 +5,15 @@ module Language.TSX
 , Language.TSX.Grammar.tree_sitter_tsx
 ) where
 
+import qualified AST.Parse as Parse
+import qualified AST.Unmarshal as TS
 import           Data.Proxy
 import qualified Language.TSX.AST as TSX
 import qualified Language.TSX.Tags as TsxTags
 import qualified Tags.Tagging.Precise as Tags
 import qualified Language.TSX.Grammar (tree_sitter_tsx)
-import qualified AST.Unmarshal as TS
 
-newtype Term a = Term { getTerm :: TSX.Program a }
+newtype Term a = Term { getTerm :: TSX.Program Err a }
 
 instance TS.SymbolMatching Term where
   matchedSymbols _ = TS.matchedSymbols (Proxy :: Proxy TSX.Program)
