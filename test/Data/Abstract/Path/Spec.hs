7f18e26e2f62dcdbfc7dd16bb3ac57a81e3459c4
Jinxuan Zhu <zhujinxuan@gmail.com>
2020-05-01 22:24:19 -0500

    Refactor the tests of joinPaths

--- test/Data/Abstract/Path/Spec.hs
+++ test/Data/Abstract/Path/Spec.hs
@@ -2,19 +2,22 @@ module Data.Abstract.Path.Spec(spec) where
 
 import Data.Abstract.Path
 import SpecHelpers
+import qualified System.Path as Path
 
 spec :: Spec
 spec = parallel $
   describe "joinPaths" $ do
     it "joins empty paths" $
-      joinUntypedPaths "" "" `shouldBe` "."
+      go (Path.currentDir) Path.currentDir $ Path.relDir "."
     it "joins relative paths" $
-      joinUntypedPaths "a/b" "./c" `shouldBe` "a/b/c"
+      go (Path.relDir "a/b") (Path.relFile "./c") $ Path.relFile "a/b/c"
     it "joins absolute paths" $
-      joinUntypedPaths "/a/b" "c" `shouldBe` "/a/b/c"
+      go (Path.absDir "/a/b") (Path.relDir "c" ) $ Path.absDir "/a/b/c"
     it "walks up directories for ../" $
-      joinUntypedPaths "a/b" "../c" `shouldBe` "a/c"
+      go (Path.relDir "a/b") (Path.relFile "../c") $ Path.relFile "a/c"
     it "walks up directories for multiple ../" $
-      joinUntypedPaths "a/b" "../../c" `shouldBe` "c"
+      go (Path.relDir "a/b") (Path.relFile "../../c") $ Path.relFile "c"
     it "stops walking at top directory" $
-      joinUntypedPaths "a/b" "../../../c" `shouldBe` "c"
+      go (Path.relDir "a/b") (Path.relFile "../../../c" ) $ Path.relFile "c"
+  where
+    go x y z = joinPaths (Path.toAbsRel x) y `shouldBe` Path.toAbsRel z
