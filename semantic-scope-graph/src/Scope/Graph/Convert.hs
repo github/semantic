3570e6c6d098918d5b77499a32ac7a120d20573d
joshvera <josh@joshvera.com>
2020-05-06 17:55:04 -0400

    Add ValueNode and ReturnNodes constructors
    To keep track of return nodes from early/multiple returns and Values returned from bodies.
    
    Co-Authored-By: Rebecca Valentine <bekavalentine@users.noreply.github.com>

--- semantic-scope-graph/src/Scope/Graph/Convert.hs
+++ semantic-scope-graph/src/Scope/Graph/Convert.hs
@@ -6,35 +6,40 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
+
 module Scope.Graph.Convert
-  ( ToScopeGraph (..)
-  , Result (..)
-  , todo
-  , complete
-  ) where
+  ( ToScopeGraph (..),
+    Result (..),
+    todo,
+    complete,
+  )
+where
 
 import Control.Effect.ScopeGraph
 import Data.List.NonEmpty
 import Data.Typeable
 import Source.Loc
+import qualified Stack.Graph as Stack
 
 class Typeable t => ToScopeGraph t where
   scopeGraph ::
     ( ScopeGraphEff sig m
-    )
-    => t Loc
-    -> m Result
+    ) =>
+    t Loc ->
+    m Result
 
 data Result
   = Complete
+  | ValueNode Stack.Node
+  | ReturnNodes [Stack.Node]
   | Todo (NonEmpty String)
-    deriving (Eq, Show, Ord)
+  deriving (Eq, Show, Ord)
 
 instance Semigroup Result where
   Complete <> Complete = Complete
-  Todo a <> Todo b     = Todo (a <> b)
-  a <> Complete        = a
-  Complete <> a        = a
+  Todo a <> Todo b = Todo (a <> b)
+  a <> Complete = a
+  Complete <> a = a
 
 instance Monoid Result where mempty = Complete
 
