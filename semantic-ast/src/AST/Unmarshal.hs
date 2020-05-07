e48a0fe0290d9d0d45a75ac7daa85b1b29c5c235
Ayman Nadeem <aymannadeem@github.com>
2020-05-07 13:24:21 -0400

    Remove unmarshal instance for Err, as we no longer need this

--- semantic-ast/src/AST/Unmarshal.hs
+++ semantic-ast/src/AST/Unmarshal.hs
@@ -220,11 +220,6 @@ class UnmarshalField t where
     -> [Node] -- ^ nodes
     -> MatchM (t (f ann))
 
-instance UnmarshalField Err where
-  unmarshalField _ _ [] = pure $ Fail "No items provided to unmarshalField."
-  unmarshalField _ _ [x] = Succeed <$> unmarshalNode x
-  unmarshalField d f _ = pure $ Fail ("type '" <> d <> "' expected zero or one nodes in field '" <> f <> "' but got multiple")
-
 instance UnmarshalField Maybe where
   unmarshalField _ _ []  = pure Nothing
   unmarshalField _ _ [x] = Just <$> unmarshalNode x
