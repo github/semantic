9157a5257a55f4363c9a4010b557c27f8b3f71a0
Ayman Nadeem <aymannadeem@github.com>
2020-05-07 15:25:00 -0400

    remove -ddump-splices used for local testing

--- semantic-json/src/Language/JSON/AST.hs
+++ semantic-json/src/Language/JSON/AST.hs
@@ -14,7 +14,6 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# OPTIONS_GHC -ddump-splices #-}
 
 module Language.JSON.AST
 ( module Language.JSON.AST
