c9f0deabdb47af71560ca8cda04381a8d9cd61a2
Ayman Nadeem <aymannadeem@github.com>
2020-05-07 13:07:01 -0400

    success is a noun (and a journey)

--- semantic-ast/src/AST/Parse.hs
+++ semantic-ast/src/AST/Parse.hs
@@ -18,14 +18,15 @@ import GHC.Generics (Generic, Generic1)
 -- data If f a = If { ann :: a, condition :: f (Expr f a), consequence :: f (Stmt f a), alternative :: f (Stmt f a) }
 -- @
 -- When the parse fails, the f will be substituted with Err
-data Err a = Fail String | Succeed a
+-- TODO: this could work with AST.Element Prj given the kindedness was adjusted from (*) to (* -> *).
+data Err a = Fail String | Success a
   deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Traversable)
 
 instance Applicative Err where
-    pure            = Succeed
+    pure            = Success
     Fail e <*> _    = Fail e
-    Succeed a <*> r = fmap a r
+    Success a <*> r = fmap a r
 
 instance Show a => Show (Err a) where
   show (Fail msg)  = msg 
-  show (Succeed a) = show a
+  show (Success a) = show a
