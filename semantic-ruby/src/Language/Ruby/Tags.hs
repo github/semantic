835d86b7ea467937dd4c0d8b1b3a3f1ee8e50268
Ayman Nadeem <aymannadeem@github.com>
2020-05-08 15:52:57 -0400

    add error to Rb.Module instance

--- semantic-ruby/src/Language/Ruby/Tags.hs
+++ semantic-ruby/src/Language/Ruby/Tags.hs
@@ -122,9 +122,9 @@ instance ToTags Rb.Module where
         name = expr,
         extraChildren
       } = enterScope True $ case expr of
-      Prj Rb.Constant {text, ann} -> yield text ann
-      Prj Rb.ScopeResolution {name = Prj Rb.Constant {text, ann}} -> yield text ann
-      Prj Rb.ScopeResolution {name = Prj Rb.Identifier {text, ann}} -> yield text ann
+      EPrj Rb.Constant {text, ann} -> yield text ann
+      EPrj Rb.ScopeResolution {name = EPrj Rb.Constant {text, ann}} -> yield text ann
+      EPrj Rb.ScopeResolution {name = EPrj Rb.Identifier {text, ann}} -> yield text ann
       _ -> gtags t
       where
         range' = case extraChildren of
