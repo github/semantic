a399bdde14fa91d632b8ec7ec34c5aca95e95714
Alexei Pastuchov <alexei.pastuchov@pyur.com>
2020-05-14 17:21:23 +0200

    exclude expressions and macros from corpus tests

--- semantic-rust/test/Test.hs
+++ semantic-rust/test/Test.hs
@@ -1,21 +1,25 @@
-{-# LANGUAGE OverloadedStrings, TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 module Main (main) where
 
 import           AST.Test
-import           AST.Unmarshal
+import           AST.Unmarshal (parseByteString)
 import qualified Language.Rust.AST as Rust
 import           Language.Rust.Grammar
 import qualified System.Path as Path
 import           Test.Tasty
+import Control.Monad (liftM)
 
 main :: IO ()
 main
   =   Path.absDir <$> Rust.getTestCorpusDir
-  >>= readCorpusFiles'
+  >>= excludeMacrosCorpus . readCorpusFiles'
   >>= traverse (testCorpus parse)
   >>= defaultMain . tests
   where
-    parse = parseByteString @Rust.Program @() tree_sitter_rust
+    parse = parseByteString @Rust.SourceFile @() tree_sitter_rust
+    excludeMacrosCorpus l = liftM (filter (f "expressions") ) $ liftM (filter (f "macros") ) l
+      where f p bn = p /= (Path.toString . Path.takeBaseName) bn
 
 tests :: [TestTree] -> TestTree
 tests = testGroup "tree-sitter-rust corpus tests"
