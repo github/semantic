f85e082e2fa5e83a4823326701aaa0e9dffdc002
Patrick Thomson <patrickt@github.com>
2020-05-19 13:23:40 -0400

    python works, with hack to deal with node-types

--- semantic-python/src/Language/Python/AST.hs
+++ semantic-python/src/Language/Python/AST.hs
@@ -20,4 +20,4 @@ import           AST.GenerateSyntax
 import           Language.Haskell.TH.Syntax (runIO)
 import qualified TreeSitter.Python as Python (getNodeTypesPath, getTestCorpusDir, tree_sitter_python)
 
-runIO Python.getNodeTypesPath >>= astDeclarationsForLanguage Python.tree_sitter_python
+astDeclarationsForLanguage Python.tree_sitter_python "/private/var/tmp/_bazel_patrickt/9ab2276bd7abbc23224fda87a0d88539/external/stackage/tree-sitter-python-0.9.0.2/vendor/tree-sitter-python/src/node-types.json"
