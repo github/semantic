d2dec69de4836aec6a2b643bc8a4a45e3de0cb1e
Ayman Nadeem <aymannadeem@github.com>
2020-05-21 14:35:12 -0400

    fix TSX test

--- semantic-tsx/test/PreciseTest.hs
+++ semantic-tsx/test/PreciseTest.hs
@@ -1,14 +1,11 @@
 {-# LANGUAGE DisambiguateRecordFields, OverloadedStrings, TypeApplications #-}
 module Main (main) where
 
-import qualified System.Path as Path
-import           Test.Tasty
+
 import           TreeSitter.TSX
-import qualified TreeSitter.TSX.AST as Ts
 import           AST.TestHelpers
 import           AST.Unmarshal
 import qualified Language.TSX.AST as Tsx
-import           Language.TSX.Grammar
 import qualified System.Path as Path
 import           Test.Tasty
 
