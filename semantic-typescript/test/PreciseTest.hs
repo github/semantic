4f493028aa14e646f0af49c4afbb4a5da023a0ca
Ayman Nadeem <aymannadeem@github.com>
2020-05-21 14:30:00 -0400

    fix TypeScript tests for precise ASTs

--- semantic-typescript/test/PreciseTest.hs
+++ semantic-typescript/test/PreciseTest.hs
@@ -1,14 +1,10 @@
 {-# LANGUAGE DisambiguateRecordFields, OverloadedStrings, TypeApplications #-}
 module Main (main) where
 
-import qualified System.Path as Path
-import           Test.Tasty
 import           TreeSitter.TypeScript
-import qualified TreeSitter.TypeScript.AST as Ts
 import           AST.TestHelpers
 import           AST.Unmarshal
 import qualified Language.TypeScript.AST as Ts
-import           Language.TypeScript.Grammar
 import qualified System.Path as Path
 import           Test.Tasty
 
