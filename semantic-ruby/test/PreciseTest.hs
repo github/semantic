893d012b0f63001e981469b06c78d549b91ee58e
Ayman Nadeem <aymannadeem@github.com>
2020-05-21 14:41:06 -0400

    fix precise tests for Ruby

--- semantic-ruby/test/PreciseTest.hs
+++ semantic-ruby/test/PreciseTest.hs
@@ -1,14 +1,10 @@
 {-# LANGUAGE DisambiguateRecordFields, OverloadedStrings, TypeApplications #-}
 module Main (main) where
 
-import qualified System.Path as Path
-import           Test.Tasty
 import           TreeSitter.Ruby
-import qualified TreeSitter.Ruby.AST as Rb
 import           AST.TestHelpers
 import           AST.Unmarshal
 import qualified Language.Ruby.AST as Rb
-import           Language.Ruby.Grammar
 import qualified System.Path as Path
 import           Test.Tasty
 
