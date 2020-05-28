c699af5ec4af91cef8b6bfaba6281575b6a09245
joshvera <josh@joshvera.com>
2020-05-27 20:09:48 -0400

    Add readBlobsFromJSON

--- src/Data/Handle.hs
+++ src/Data/Handle.hs
@@ -6,52 +6,59 @@
 {-# LANGUAGE TypeApplications #-}
 
 module Data.Handle
-  ( Handle(..)
-  , getHandle
-  , stdin
-  , stdout
-  , stderr
-  , readBlobsFromHandle
-  , readPathsFromHandle
-  , readBlobPairsFromHandle
-  , readFromHandle
-  , openFileForReading
-  , InvalidProtoException(..)
+  ( Handle (..),
+    getHandle,
+    stdin,
+    stdout,
+    stderr,
+    readBlobsFromHandle,
+    readBlobsFromJSON,
+    readPathsFromHandle,
+    readBlobPairsFromHandle,
+    readFromHandle,
+    openFileForReading,
+    InvalidProtoException (..),
+    InvalidJSONException (..),
   )
 where
 
-import           Control.Exception              ( Exception
-                                                , throw
-                                                )
-import           Control.Lens
-import           Control.Monad.IO.Class
-import qualified Data.ByteString               as BL
-import qualified Data.ByteString.Char8         as BLC
-import qualified Data.Either                   as Either
-import           Data.ProtoLens
-import qualified Data.Text                     as Text
-import qualified Proto.Semantic                as Proto
-import           Proto.Semantic_Fields          ( blobs
-                                                , content
-                                                , language
-                                                , path
-                                                )
-import qualified Source.Language               as Language
-import qualified Source.Source                 as Source
-import qualified System.IO                     as IO
-import qualified System.Path                   as Path
-
-import           Data.Blob               hiding ( blobs )
+import Control.Exception
+  ( Exception,
+    throw,
+  )
+import Control.Lens
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Blob hiding (blobs)
+import qualified Data.Blob as Blob
+import qualified Data.ByteString as BL
+import qualified Data.ByteString.Char8 as BLC
+import Data.ByteString.Lazy as BLZ
+import qualified Data.Either as Either
+import Data.ProtoLens
+import qualified Data.Text as Text
+import qualified Proto.Semantic as Proto
+import Proto.Semantic_Fields
+  ( blobs,
+    content,
+    language,
+    path,
+  )
+import qualified Source.Language as Language
+import qualified Source.Source as Source
+import qualified System.IO as IO
+import qualified System.Path as Path
 
 data Handle mode where
-  ReadHandle  ::IO.Handle -> Handle 'IO.ReadMode
-  WriteHandle ::IO.Handle -> Handle 'IO.WriteMode
+  ReadHandle :: IO.Handle -> Handle 'IO.ReadMode
+  WriteHandle :: IO.Handle -> Handle 'IO.WriteMode
 
-deriving instance Eq   (Handle mode)
+deriving instance Eq (Handle mode)
+
 deriving instance Show (Handle mode)
 
 getHandle :: Handle mode -> IO.Handle
-getHandle (ReadHandle  handle) = handle
+getHandle (ReadHandle handle) = handle
 getHandle (WriteHandle handle) = handle
 
 stdin :: Handle 'IO.ReadMode
@@ -67,10 +74,13 @@ openFileForReading :: FilePath -> IO (Handle 'IO.ReadMode)
 openFileForReading path = ReadHandle <$> IO.openFile path IO.ReadMode
 
 -- | Read JSON encoded blobs from a handle.
-readBlobsFromHandle :: forall m . MonadIO m => Handle 'IO.ReadMode -> m [Blob]
+readBlobsFromHandle :: forall m. MonadIO m => Handle 'IO.ReadMode -> m [Blob]
 readBlobsFromHandle handle = do
   request <- readFromHandle @Proto.StackGraphRequest handle
   pure (fromProto <$> request ^. blobs)
+
+readBlobsFromJSON :: MonadIO m => Handle 'IO.ReadMode -> m [Blob]
+readBlobsFromJSON = fmap Blob.blobs <$> readFromJSON
 
 -- | Read line delimited paths from a handle
 readPathsFromHandle :: MonadIO m => Handle 'IO.ReadMode -> m [FilePath]
@@ -80,6 +90,7 @@ readPathsFromHandle (ReadHandle h) =
 -- | Read JSON encoded blob pairs from a handle.
 readBlobPairsFromHandle :: Handle 'IO.ReadMode -> m [BlobPair]
 readBlobPairsFromHandle = undefined
+
 -- do
 -- request <- readFromHandle @Proto.DiffTreeRequest @m
 -- pure (fromProto <$> request^.blobs)
@@ -87,27 +98,37 @@ readBlobPairsFromHandle = undefined
 newtype InvalidProtoException = InvalidProtoException String
   deriving (Eq, Show, Exception)
 
+newtype InvalidJSONException = InvalidJSONException String
+  deriving (Eq, Show, Exception)
+
 -- | Read JSON-encoded data from a 'Handle'. Throws
 -- 'InvalidJSONException' on parse failure.
-readFromHandle
-  :: forall a m . (Message a, MonadIO m) => Handle 'IO.ReadMode -> m a
+readFromHandle ::
+  forall a m. (Message a, MonadIO m) => Handle 'IO.ReadMode -> m a
 readFromHandle (ReadHandle h) = do
   input <- liftIO $ BL.hGetContents h
   case decodeMessage input of
-    Left  e -> throw (InvalidProtoException e)
+    Left e -> throw (InvalidProtoException e)
     Right d -> pure d
 
+readFromJSON :: (FromJSON a, MonadIO m) => Handle 'IO.ReadMode -> m a
+readFromJSON (ReadHandle h) = do
+  input <- liftIO $ BL.hGetContents h
+  case eitherDecode (BLZ.fromStrict input) of
+    Left e -> throw (InvalidJSONException e)
+    Right d -> pure d
 
 fromProto :: Proto.Blob -> Blob
 fromProto blob =
-  let
-    lang  = Language.textToLanguage (blob ^. language)
-    -- TODO: The Left case is wrong here.
-    path' = Either.fromRight (Path.absRel "")
-                             (Path.parse (Text.unpack $ blob ^. path))
-    lang' =
-      if Language.knownLanguage lang then lang else Language.forPath path'
-  in
-    fromSource (path' :: Path.AbsRelFile)
-               lang'
-               (Source.fromText $ blob ^. content)
+  let lang = Language.textToLanguage (blob ^. language)
+      -- TODO: The Left case is wrong here.
+      path' =
+        Either.fromRight
+          (Path.absRel "")
+          (Path.parse (Text.unpack $ blob ^. path))
+      lang' =
+        if Language.knownLanguage lang then lang else Language.forPath path'
+   in fromSource
+        (path' :: Path.AbsRelFile)
+        lang'
+        (Source.fromText $ blob ^. content)
