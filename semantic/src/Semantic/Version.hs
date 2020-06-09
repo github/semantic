25abbb85e48cd59c2fbabf82c878afcb49358458
Patrick Thomson <patrickt@github.com>
2020-06-09 15:20:08 -0400

    semantic:lib now builds

--- semantic/src/Semantic/Version.hs
+++ semantic/src/Semantic/Version.hs
@@ -4,11 +4,12 @@ module Semantic.Version
   ) where
 
 import Data.Version (showVersion)
-import Paths_semantic (version)
+--import Paths_semantic (version)
 
 -- The version string of this build of semantic.
 buildVersion :: String
-buildVersion = showVersion version
+buildVersion = "0.11.0.0"
+--buildVersion = showVersion version
 
 -- The SHA1 hash of this build of semantic.
 -- If compiled as a development build, this will be @<development>@.
