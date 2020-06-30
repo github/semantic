b8f299587742975cb100c28545080ca8a738fbc3
Patrick Thomson <patrickt@github.com>
2020-06-30 17:27:25 -0400

    Revert "kind of get semantic-ruby working, but having trouble with stuff"
    This reverts commit b314ade302918f815d5f65a839bc68bb8eb211a9.

--- semantic-ruby/test/PreciseTest.hs
+++ semantic-ruby/test/PreciseTest.hs
@@ -1,38 +1,20 @@
-{-# LANGUAGE DisambiguateRecordFields #-}
-{-# LANGUAGE ImplicitParams #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DisambiguateRecordFields, OverloadedStrings, TypeApplications #-}
 module Main (main) where
 
+import           TreeSitter.Ruby
 import           AST.TestHelpers
 import           AST.Unmarshal
 import qualified Language.Ruby.AST as Rb
 import qualified System.Path as Path
-import qualified System.Path.Fixture as Fixture
-import qualified Bazel.Runfiles as Bazel
 import           Test.Tasty
-import System.IO
-import Control.Concurrent
-import           TreeSitter.Ruby
 
 main :: IO ()
-main = do
-  let ?project = Path.relDir "semantic-ruby"
-
-  rf <- Bazel.create
-  let ?runfiles = rf
-
-  let parse = parseByteString @Rb.Program @() tree_sitter_ruby
-
-  Rb.getTestCorpusDir >>= print
-  print (Path.toString (Fixture.bazelDir "/."))
-  hFlush stdout
-  threadDelay 1000000000
-
-  Fixture.bazelDir <$> Rb.getTestCorpusDir
-    >>= readCorpusFiles'
-    >>= traverse (testCorpus parse)
-    >>= defaultMain . tests
+main
+  =   Path.absDir <$> Rb.getTestCorpusDir
+  >>= readCorpusFiles'
+  >>= traverse (testCorpus parse)
+  >>= defaultMain . tests
+  where parse = parseByteString @Rb.Program @() tree_sitter_ruby
 
 tests :: [TestTree] -> TestTree
 tests = testGroup "tree-sitter-ruby corpus tests"
