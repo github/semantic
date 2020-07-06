c262709045dd85e0dcb81e4583db0954492992d3
Timothy Clem <timothy.clem@gmail.com>
2020-07-06 11:50:21 -0700

    Let these benchmarks compile again
    Co-Authored-By: Rebecca Valentine <bekavalentine@users.noreply.github.com>
    Co-Authored-By: Max Brunsfeld <maxbrunsfeld@gmail.com>

--- semantic-tags/bench/Main.hs
+++ semantic-tags/bench/Main.hs
@@ -15,18 +15,18 @@ main = do
   -- bytes <- fmap fromUTF8 $ B.readFile "/Users/tclem/Downloads/jquery-3.5.1.min.js"
   -- let lineIndexes = B.elemIndices (toEnum (ord '\n')) (Source.bytes bytes)
   defaultMain
-    [ baselineCalculateLine bytes
-    , calculateLinePRVersion bytes
-    , calculateLineWithMap bytes
+    -- [ baselineCalculateLine bytes
+    -- , calculateLinePRVersion bytes
+    [ calculateLineWithMap bytes
     ]
 
-baselineCalculateLine :: Source -> Benchmark
-baselineCalculateLine bytes = bench "baseline calculateLineAndSpans" $ do
-  nf (baselineCalculateLineAndSpans bytes) (Loc (Range 73754 73757) (Span (Pos 1 73669) (Pos 1 73672)))
+-- baselineCalculateLine :: Source -> Benchmark
+-- baselineCalculateLine bytes = bench "baseline calculateLineAndSpans" $ do
+--   nf (baselineCalculateLineAndSpans bytes) (Loc (Range 73754 73757) (Span (Pos 1 73669) (Pos 1 73672)))
 
-calculateLinePRVersion :: Source -> Benchmark
-calculateLinePRVersion bytes = bench "calculateLineAndSpansPRVersion " $ do
-  nf (calculateLineAndSpansPRVersion bytes) (Loc (Range 73754 73757) (Span (Pos 1 73669) (Pos 1 73672)))
+-- calculateLinePRVersion :: Source -> Benchmark
+-- calculateLinePRVersion bytes = bench "calculateLineAndSpansPRVersion " $ do
+--   nf (calculateLineAndSpansPRVersion bytes) (Loc (Range 73754 73757) (Span (Pos 1 73669) (Pos 1 73672)))
 
 calculateLineWithMap :: Source -> Benchmark
 calculateLineWithMap src = bench "calculateLineAndSpansWithIndexes" $ do
