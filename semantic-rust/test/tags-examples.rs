a1c35998b314c2b1d7facae872be830133cc9d6b
Ayman Nadeem <aymannadeem@github.com>
2020-07-21 14:03:42 -0400

    add function declarations

--- semantic-rust/test/tags-examples.rs
+++ semantic-rust/test/tags-examples.rs
@@ -11,3 +11,33 @@ mod english {
 
 pub mod english;
 
+// Function declarations
+
+fn main() {}
+
+fn add(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+
+fn takes_slice(slice: &str) {
+    println!("Got: {}", slice);
+}
+
+fn foo() -> [u32; 2] {
+    return [1, 2];
+}
+
+fn foo() -> (u32, u16) {
+    return (1, 2);
+}
+
+fn foo() {
+    return
+}
+
+fn foo(x: impl FnOnce() -> result::Result<T, E>) {}
+
+fn foo(#[attr] x: i32, #[attr] x: i64) {}
+
+fn accumulate(self) -> Machine<{State::Accumulate}> {}
+
