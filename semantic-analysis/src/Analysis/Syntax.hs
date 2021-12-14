3b89756033b059b7051f6f4b8c4422657f922717
Rob Rix <rob.rix@me.com>
2021-12-14 14:37:29 -0500

    Stub in an interface for syntax.

--- semantic-analysis/src/Analysis/Syntax.hs
+++ semantic-analysis/src/Analysis/Syntax.hs
@@ -1,2 +1,14 @@
 module Analysis.Syntax
-() where
+( Syntax(..)
+) where
+
+import Data.Text (Text)
+
+class Syntax rep where
+  iff :: rep -> rep -> rep -> rep
+  noop :: rep
+
+  bool :: Bool -> rep
+  string :: Text -> rep
+
+  throw :: rep -> rep
