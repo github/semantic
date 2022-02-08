7074057e6ff86bd0b2a33c3ec618f73784ce8928
Rob Rix <rob.rix@me.com>
2022-02-08 11:49:38 -0500

    :memo: ExcSet.

--- semantic-analysis/src/Analysis/Exception.hs
+++ semantic-analysis/src/Analysis/Exception.hs
@@ -9,4 +9,5 @@ import qualified Data.Set as Set
 newtype Exception = Exception { exceptionName :: String }
   deriving (Eq, Ord, Show)
 
+-- | Sets whose elements are each a variable or an exception.
 newtype ExcSet = ExcSet { values :: Set.Set (Either Name Exception) }
