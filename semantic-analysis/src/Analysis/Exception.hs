cc0659b61f738df062cfb06b98530d90fb43d5f7
Rob Rix <rob.rix@me.com>
2022-02-08 11:49:32 -0500

    Define sets of exceptions.

--- semantic-analysis/src/Analysis/Exception.hs
+++ semantic-analysis/src/Analysis/Exception.hs
@@ -1,6 +1,12 @@
 module Analysis.Exception
 ( Exception(..)
+, ExcSet(..)
 ) where
+
+import           Analysis.Name
+import qualified Data.Set as Set
 
 newtype Exception = Exception { exceptionName :: String }
   deriving (Eq, Ord, Show)
+
+newtype ExcSet = ExcSet { values :: Set.Set (Either Name Exception) }
