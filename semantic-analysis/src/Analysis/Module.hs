2b0308ea5a8a18cf1e1c90323bbcd5c4fdb35a73
Rob Rix <rob.rix@me.com>
2022-03-07 11:56:31 -0500

    Add imports to modules.

--- semantic-analysis/src/Analysis/Module.hs
+++ semantic-analysis/src/Analysis/Module.hs
@@ -4,7 +4,9 @@ module Analysis.Module
 
 import           Analysis.Name
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 data Module a = Module
-  { body :: Map.Map Name a -> a
+  { body    :: Map.Map Name a -> a
+  , imports :: Set.Set Name
   }
