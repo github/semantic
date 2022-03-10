294bc3dcd80370a51f7abd519e7dc066ccd9224a
Rob Rix <rob.rix@me.com>
2022-03-10 11:53:28 -0500

    Define a Statement effect.

--- semantic-analysis/src/Analysis/Effect/Statement.hs
+++ semantic-analysis/src/Analysis/Effect/Statement.hs
@@ -1,2 +1,11 @@
-module Analysis.Statement
-() where
+{-# LANGUAGE GADTs #-}
+module Analysis.Effect.Statement
+( -- * Statement effect
+  Statement(..)
+) where
+
+import Data.Kind as K
+
+-- Statement effect
+
+data Statement (m :: K.Type -> K.Type) k where
