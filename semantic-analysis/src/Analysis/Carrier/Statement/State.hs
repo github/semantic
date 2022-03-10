236a83f9430421ab13178dc4374ad2e145972904
Rob Rix <rob.rix@me.com>
2022-03-10 12:09:13 -0500

    Define a Statement carrier.

--- semantic-analysis/src/Analysis/Carrier/Statement/State.hs
+++ semantic-analysis/src/Analysis/Carrier/Statement/State.hs
@@ -1,2 +1,24 @@
 module Analysis.Carrier.Statement.State
-()
+( -- * Messages
+  Message(..)
+  -- * Statement carrier
+, runStatement
+, StatementC(..)
+) where
+
+import Control.Carrier.State.Church
+import Data.List.NonEmpty (NonEmpty)
+import Data.Text (Text)
+
+-- Messages
+
+data Message
+  = Import (NonEmpty Text)
+
+
+-- Statement carrier
+
+runStatement :: ([Message] -> a -> m r) -> StatementC m a -> m r
+runStatement k (StatementC m) = runState (k . reverse) [] m
+
+newtype StatementC m a = StatementC { runStatementC :: StateC [Message] m a }
