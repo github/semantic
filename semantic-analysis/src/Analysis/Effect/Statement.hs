b2d7b7f270f99d6fb5a926da443ee3889bc040fb
Rob Rix <rob.rix@me.com>
2022-03-10 11:56:48 -0500

    Add an import statement.

--- semantic-analysis/src/Analysis/Effect/Statement.hs
+++ semantic-analysis/src/Analysis/Effect/Statement.hs
@@ -1,11 +1,20 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 module Analysis.Effect.Statement
 ( -- * Statement effect
-  Statement(..)
+  simport
+, Statement(..)
 ) where
 
+import Control.Algebra
 import Data.Kind as K
+import Data.List.NonEmpty (NonEmpty)
+import Data.Text
 
 -- Statement effect
 
+simport :: Has Statement sig m => NonEmpty Text -> m ()
+simport ns = send (Import ns)
+
 data Statement (m :: K.Type -> K.Type) k where
+  Import :: NonEmpty Text -> Statement m ()
