d64ea5963efe62e681aa6bcbcd3d4760b4b35430
Rob Rix <rob.rix@me.com>
2022-05-18 11:13:41 -0400

    Ingest function calls.

--- semantic-analysis/src/Analysis/Syntax.hs
+++ semantic-analysis/src/Analysis/Syntax.hs
@@ -26,7 +26,7 @@ import           Analysis.Effect.Store
 import           Analysis.File
 import           Analysis.Name (Name, name, nameI)
 import           Analysis.Reference as Ref
-import           Control.Applicative (Alternative (..), liftA3)
+import           Control.Applicative (Alternative (..), liftA2, liftA3)
 import           Control.Effect.Labelled
 import           Control.Effect.Throw (Throw, throwError)
 import           Control.Monad (guard)
@@ -155,6 +155,7 @@ parseTerm attrs edges = \case
   "identifier" -> const . Var . name <$> attrs A..: fromString "text"
   "import"     -> const . Import . fromList . map snd . sortOn fst <$> traverse (resolveWith (const moduleNameComponent)) edges
   "function"   -> liftA3 Function . pure . name <$> attrs A..: fromString "name" <*> pure (pure []) <*> findEdgeNamed edges "body"
+  "call"       -> liftA2 Call . const . Var . name <$> attrs A..: fromString "function" <*> (sequenceA <$> traverse resolve edges)
   t            -> A.parseFail ("unrecognized type: " <> t <> " attrs: " <> show attrs <> " edges: " <> show edges)
 
 findEdgeNamed :: (Foldable t, A.FromJSON a, Eq a) => t A.Value -> a -> A.Parser (Graph -> Term)
