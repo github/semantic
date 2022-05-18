572bd7d329ff5e2a299c1941e48fb51a03c446be
Rob Rix <rob.rix@me.com>
2022-05-18 11:55:24 -0400

    Show modules with record syntax.

--- semantic-analysis/src/Analysis/Module.hs
+++ semantic-analysis/src/Analysis/Module.hs
@@ -17,8 +17,15 @@ data Module a = Module
   }
 
 instance Show a => Show (Module a) where
-  showsPrec p (Module b i e u) = showParen (p > 10) (showString "Module" . sp . showsPrec 11 (b mempty) . sp . showsPrec 11 i . sp . showsPrec 11 e . sp . showsPrec 11 u)
+  showsPrec p (Module b i e u) = showParen (p > 10) (showString "Module" . sp . braces
+    ( field "body"    (b mempty) . comma . sp
+    . field "imports" i          . comma . sp
+    . field "exports" e          . comma . sp
+    . field "unknown" u))
     where
+    braces b = showChar '{' . sp . b . sp . showChar '}'
+    comma = showChar ','
+    field n b = showString n . sp . showChar '=' . sp . showsPrec 0 b
     sp = showChar ' '
 
 
