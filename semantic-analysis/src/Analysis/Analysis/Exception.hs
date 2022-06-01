208c7bbc6ed69b04353fb155f8ebc81c85c7e38f
Rob Rix <rob.rix@me.com>
2022-06-01 14:34:17 -0400

    Map strings to exceptions.
    This is really hacky but will have to do until such time as we can make Exception properly recursive. Furthermore, it's no more hacky than what we were doing before.

--- semantic-analysis/src/Analysis/Analysis/Exception.hs
+++ semantic-analysis/src/Analysis/Analysis/Exception.hs
@@ -171,7 +171,7 @@ instance (Algebra sig m, Alternative m) => Algebra (Dom ExcSet :+: sig) (ExcC m)
       DIf c t e -> fmap (mappend c) <$> runExcC (hdl (t <$ ctx) <|> hdl (e <$ ctx))
       DString s -> pure (str (Text.dropAround (== '"') s) <$ ctx)
       t :>>> u  -> pure (t <> u <$ ctx)
-      DDie e    -> pure $ e{ freeVariables = mempty } <> fromExceptions [Exception n | n <- Set.toList (freeVariables e)] <$ ctx
+      DDie e    -> pure $ e{ strings = mempty } <> fromExceptions [Exception (name n) | n <- Set.toList (strings e)] <$ ctx
       where
       nil = (mempty :: ExcSet) <$ ctx
 
