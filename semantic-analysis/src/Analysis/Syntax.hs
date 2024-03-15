f8a8f06f604c67b8b8335f8696cf40d6f67f8959
Rob Rix <robrix@github.com>
2024-03-15 11:17:39 -0400

    Initial stab at ABT-style term representations.

--- semantic-analysis/src/Analysis/Syntax.hs
+++ semantic-analysis/src/Analysis/Syntax.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
 module Analysis.Syntax
-() where
+( -- * Syntax
+  Term(..)
+  -- * Vectors
+, Vec(..)
+) where
+
+import Data.Kind (Type)
+import GHC.TypeLits (Natural, type (+))
+
+-- Syntax
+
+-- | (Currently) untyped term representations.
+data Term (sig :: Natural -> Type) where
+  (:$:) :: sig n -> Vec n (Term sig) -> Term sig
+
+
+-- Vectors
+
+-- FIXME: move this into its own module, or use a dependency, or something.
+data Vec (n :: Natural) a where
+  Nil :: Vec 0 a
+  Cons :: a -> Vec n a -> Vec (1 + n) a
