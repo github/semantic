14cdce0d65720f3c70341b067cfb54a8477bcdfa
Rob Rix <robrix@github.com>
2024-03-15 11:04:11 -0400

    Move the Term type into its own module.

--- semantic-analysis/src/Analysis/Syntax.hs
+++ semantic-analysis/src/Analysis/Syntax.hs
@@ -25,8 +25,9 @@ module Analysis.Syntax
 import qualified Analysis.Carrier.Statement.State as S
 import           Analysis.Effect.Domain
 import           Analysis.File
-import           Analysis.Name (Name, name)
+import           Analysis.Name (name)
 import           Analysis.Reference as Ref
+import           Analysis.Syntax.Python
 import           Analysis.VM
 import           Control.Applicative (Alternative (..), liftA2, liftA3)
 import           Control.Carrier.Throw.Either (runThrow)
@@ -44,85 +45,15 @@ import           Data.Foldable (fold, foldl')
 import           Data.Function (fix)
 import qualified Data.IntMap as IntMap
 import           Data.List (sortOn)
-import           Data.List.NonEmpty (NonEmpty, fromList)
+import           Data.List.NonEmpty (fromList)
 import           Data.Maybe (listToMaybe)
 import           Data.Monoid (First (..))
-import qualified Data.Set as Set
 import           Data.String (IsString (..))
 import           Data.Text (Text)
 import qualified Data.Vector as V
 import qualified Source.Source as Source
 import           Source.Span
 import           System.FilePath
-
-data Term
-  = Var Name
-  | Noop
-  | Iff Term Term Term
-  | Bool Bool
-  | String Text
-  | Throw Term
-  | Let Name Term Term
-  | Term :>> Term
-  | Import (NonEmpty Text)
-  | Function Name [Name] Term
-  | Call Term [Term]
-  | Locate Span Term
-  deriving (Eq, Ord, Show)
-
-infixl 1 :>>
-
-subterms :: Term -> Set.Set Term
-subterms t = Set.singleton t <> case t of
-  Var _          -> mempty
-  Noop           -> mempty
-  Iff c t e      -> subterms c <> subterms t <> subterms e
-  Bool _         -> mempty
-  String _       -> mempty
-  Throw t        -> subterms t
-  Let _ v b      -> subterms v <> subterms b
-  a :>> b        -> subterms a <> subterms b
-  Import _       -> mempty
-  Function _ _ b -> subterms b
-  Call f as      -> subterms f <> foldMap subterms as
-  Locate _ b     -> subterms b
-
-
--- Abstract interpretation
-
-eval0 :: (Has (Env addr) sig m, HasLabelled Store (Store addr val) sig m, Has (Dom val) sig m, Has (Reader Reference) sig m, Has S.Statement sig m) => Term -> m val
-eval0 = fix eval
-
-eval
-  :: (Has (Env addr) sig m, HasLabelled Store (Store addr val) sig m, Has (Dom val) sig m, Has (Reader Reference) sig m, Has S.Statement sig m)
-  => (Term -> m val)
-  -> (Term -> m val)
-eval eval = \case
-  Var n     -> lookupEnv n >>= maybe (dvar n) fetch
-  Noop      -> dunit
-  Iff c t e -> do
-    c' <- eval c
-    dif c' (eval t) (eval e)
-  Bool b    -> dbool b
-  String s  -> dstring s
-  Throw e   -> eval e >>= ddie
-  Let n v b -> do
-    v' <- eval v
-    let' n v' (eval b)
-  t :>> u   -> do
-    t' <- eval t
-    u' <- eval u
-    t' >>> u'
-  Import ns -> S.simport ns >> dunit
-  Function n ps b -> letrec n (dabs ps (foldr (\ (p, a) m -> let' p a m) (eval b) . zip ps))
-  Call f as -> do
-    f' <- eval f
-    as' <- traverse eval as
-    dapp f' as'
-  Locate s t -> local (setSpan s) (eval t)
-  where
-  setSpan s r = r{ refSpan = s }
-
 
 -- Parsing
 
--- semantic-analysis/src/Analysis/Syntax/Python.hs
+++ semantic-analysis/src/Analysis/Syntax/Python.hs
@@ -1,3 +1,94 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 -- | This belongs in @semantic-python@ instead of @semantic-analysis@, but for the sake of expedience…
 module Analysis.Syntax.Python
-() where
+( -- * Syntax
+  Term(..)
+, subterms
+  -- * Abstract interpretation
+, eval0
+, eval
+) where
+
+import           Analysis.Effect.Domain
+import qualified Analysis.Effect.Statement as S
+import           Analysis.Name
+import           Analysis.Reference
+import           Analysis.VM
+import           Control.Effect.Labelled
+import           Control.Effect.Reader
+import           Data.Function (fix)
+import           Data.List.NonEmpty (NonEmpty)
+import qualified Data.Set as Set
+import           Data.Text (Text)
+import           Source.Span (Span)
+
+-- Syntax
+
+data Term
+  = Var Name
+  | Noop
+  | Iff Term Term Term
+  | Bool Bool
+  | String Text
+  | Throw Term
+  | Let Name Term Term
+  | Term :>> Term
+  | Import (NonEmpty Text)
+  | Function Name [Name] Term
+  | Call Term [Term]
+  | Locate Span Term
+  deriving (Eq, Ord, Show)
+
+infixl 1 :>>
+
+subterms :: Term -> Set.Set Term
+subterms t = Set.singleton t <> case t of
+  Var _          -> mempty
+  Noop           -> mempty
+  Iff c t e      -> subterms c <> subterms t <> subterms e
+  Bool _         -> mempty
+  String _       -> mempty
+  Throw t        -> subterms t
+  Let _ v b      -> subterms v <> subterms b
+  a :>> b        -> subterms a <> subterms b
+  Import _       -> mempty
+  Function _ _ b -> subterms b
+  Call f as      -> subterms f <> foldMap subterms as
+  Locate _ b     -> subterms b
+
+
+-- Abstract interpretation
+
+eval0 :: (Has (Env addr) sig m, HasLabelled Store (Store addr val) sig m, Has (Dom val) sig m, Has (Reader Reference) sig m, Has S.Statement sig m) => Term -> m val
+eval0 = fix eval
+
+eval
+  :: (Has (Env addr) sig m, HasLabelled Store (Store addr val) sig m, Has (Dom val) sig m, Has (Reader Reference) sig m, Has S.Statement sig m)
+  => (Term -> m val)
+  -> (Term -> m val)
+eval eval = \case
+  Var n     -> lookupEnv n >>= maybe (dvar n) fetch
+  Noop      -> dunit
+  Iff c t e -> do
+    c' <- eval c
+    dif c' (eval t) (eval e)
+  Bool b    -> dbool b
+  String s  -> dstring s
+  Throw e   -> eval e >>= ddie
+  Let n v b -> do
+    v' <- eval v
+    let' n v' (eval b)
+  t :>> u   -> do
+    t' <- eval t
+    u' <- eval u
+    t' >>> u'
+  Import ns -> S.simport ns >> dunit
+  Function n ps b -> letrec n (dabs ps (foldr (\ (p, a) m -> let' p a m) (eval b) . zip ps))
+  Call f as -> do
+    f' <- eval f
+    as' <- traverse eval as
+    dapp f' as'
+  Locate s t -> local (setSpan s) (eval t)
+  where
+  setSpan s r = r{ refSpan = s }
