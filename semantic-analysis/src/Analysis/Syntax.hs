b798092d5eda6b6093af045a4fa8c765a5cb55d4
Rob Rix <robrix@github.com>
2024-03-15 14:35:11 -0400

    Go quantify a kite.

--- semantic-analysis/src/Analysis/Syntax.hs
+++ semantic-analysis/src/Analysis/Syntax.hs
@@ -2,6 +2,10 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Analysis.Syntax
 ( -- * Syntax
   Term(..)
@@ -15,13 +19,21 @@ module Analysis.Syntax
 , toList
 ) where
 
+import Control.Monad (guard)
 import Data.Kind (Type)
+import Data.Typeable
+import Unsafe.Coerce (unsafeCoerce)
 
 -- Syntax
 
 -- | (Currently) untyped term representations.
 data Term (sig :: Nat -> Type) where
-  (:$:) :: sig n -> Vec n (Term sig) -> Term sig
+  (:$:) :: KnownNat n => sig n -> Vec n (Term sig) -> Term sig
+
+instance (forall n . Eq (sig n)) => Eq (Term sig) where
+  a :$: as == b :$: bs = case sameNat a b of
+    Just Refl -> a == b && as == bs
+    _         -> False
 
 
 -- Vectors
@@ -35,6 +47,23 @@ type N0 = 'Z
 type N1 = 'S N0
 type N2 = 'S N1
 type N3 = 'S N2
+
+
+-- | Reify 'Nat's back from type-level singletons.
+class KnownNat (n :: Nat) where
+  reifyNat :: proxy n -> Nat
+
+instance KnownNat 'Z where
+  reifyNat _ = Z
+
+instance KnownNat n => KnownNat ('S n) where
+  reifyNat _ = S (reifyNat (undefined :: proxy n))
+
+
+-- | Test the equality of type-level 'Nat's at runtime, generating a type-level equality if equal.
+sameNat :: forall a b proxy1 proxy2 . (KnownNat a, KnownNat b) => proxy1 a -> proxy2 b -> Maybe (a :~: b)
+sameNat a b = unsafeCoerce Refl <$ guard (reifyNat a == reifyNat b)
+
 
 -- FIXME: move this into its own module, or use a dependency, or something.
 data Vec (n :: Nat) a where
