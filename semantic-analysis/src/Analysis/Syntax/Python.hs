e7c5928f730b53bcd3430e91edb61bc9d965da2c
Rob Rix <robrix@github.com>
2024-03-18 16:04:51 -0400

    Rename the synonyms to elide the primes.

--- semantic-analysis/src/Analysis/Syntax/Python.hs
+++ semantic-analysis/src/Analysis/Syntax/Python.hs
@@ -10,17 +10,17 @@ module Analysis.Syntax.Python
 ( -- * Syntax
   T.Term(..)
 , Python(..)
-, pattern Noop''
-, pattern Iff''
-, pattern Bool''
-, pattern String''
-, pattern Throw''
-, pattern Let''
-, pattern (:>>>>)
-, pattern Import''
-, pattern Function''
-, pattern Call''
-, pattern Locate''
+, pattern Noop
+, pattern Iff
+, pattern Bool
+, pattern String
+, pattern Throw
+, pattern Let
+, pattern (:>>)
+, pattern Import
+, pattern Function
+, pattern Call
+, pattern Locate
   -- * Abstract interpretation
 , eval0
 , eval
@@ -58,58 +58,58 @@ data Python (arity :: T.Nat) where
 
 infixl 1 :>>>
 
-pattern Noop'' :: T.Term Python v
-pattern Noop'' = Noop' T.:$: T.Nil
+pattern Noop :: T.Term Python v
+pattern Noop = Noop' T.:$: T.Nil
 
-pattern Iff'' :: T.Term Python v -> T.Term Python v -> T.Term Python v -> T.Term Python v
-pattern Iff'' c t e = Iff' T.:$: T.Cons c (T.Cons t (T.Cons e T.Nil))
+pattern Iff :: T.Term Python v -> T.Term Python v -> T.Term Python v -> T.Term Python v
+pattern Iff c t e = Iff' T.:$: T.Cons c (T.Cons t (T.Cons e T.Nil))
 
-pattern Bool'' :: Bool -> T.Term Python v
-pattern Bool'' b = Bool' b T.:$: T.Nil
+pattern Bool :: Bool -> T.Term Python v
+pattern Bool b = Bool' b T.:$: T.Nil
 
-pattern String'' :: Text -> T.Term Python v
-pattern String'' t = String' t T.:$: T.Nil
+pattern String :: Text -> T.Term Python v
+pattern String t = String' t T.:$: T.Nil
 
-pattern Throw'' :: T.Term Python v -> T.Term Python v
-pattern Throw'' e = Throw' T.:$: T.Cons e T.Nil
+pattern Throw :: T.Term Python v -> T.Term Python v
+pattern Throw e = Throw' T.:$: T.Cons e T.Nil
 
-pattern Let'' :: Name -> T.Term Python v -> T.Term Python v -> T.Term Python v
-pattern Let'' n v b = Let' n T.:$: T.Cons v (T.Cons b T.Nil)
+pattern Let :: Name -> T.Term Python v -> T.Term Python v -> T.Term Python v
+pattern Let n v b = Let' n T.:$: T.Cons v (T.Cons b T.Nil)
 
-pattern (:>>>>) :: T.Term Python v -> T.Term Python v -> T.Term Python v
-pattern s :>>>> t = (:>>>) T.:$: T.Cons s (T.Cons t T.Nil)
+pattern (:>>) :: T.Term Python v -> T.Term Python v -> T.Term Python v
+pattern s :>> t = (:>>>) T.:$: T.Cons s (T.Cons t T.Nil)
 
-infixl 1 :>>>>
+infixl 1 :>>
 
-pattern Import'' :: NonEmpty Text -> T.Term Python v
-pattern Import'' i = Import' i T.:$: T.Nil
+pattern Import :: NonEmpty Text -> T.Term Python v
+pattern Import i = Import' i T.:$: T.Nil
 
-pattern Function'' :: Name -> [Name] -> T.Term Python v -> T.Term Python v
-pattern Function'' n as b = Function' n as T.:$: T.Cons b T.Nil
+pattern Function :: Name -> [Name] -> T.Term Python v -> T.Term Python v
+pattern Function n as b = Function' n as T.:$: T.Cons b T.Nil
 
-pattern Call''
+pattern Call
   :: T.Term Python v
   -> [T.Term Python v]
   -> T.Term Python v
-pattern Call'' f as <- Call' T.:$: T.Cons f (T.Cons (fromArgs -> as) T.Nil)
-  where Call'' f as = Call' T.:$: T.Cons f (T.Cons (foldr ACons'' ANil'' as) T.Nil)
+pattern Call f as <- Call' T.:$: T.Cons f (T.Cons (fromArgs -> as) T.Nil)
+  where Call f as = Call' T.:$: T.Cons f (T.Cons (foldr ACons ANil as) T.Nil)
 
 fromArgs :: T.Term Python v -> [T.Term Python v]
 fromArgs = \case
-  ANil'' -> []
-  ACons'' a as -> a:fromArgs as
+  ANil -> []
+  ACons a as -> a:fromArgs as
   _ -> fail "unexpected constructor in spine of argument list"
 
-pattern ANil'' :: T.Term Python v
-pattern ANil'' = ANil' T.:$: T.Nil
+pattern ANil :: T.Term Python v
+pattern ANil = ANil' T.:$: T.Nil
 
-pattern ACons'' :: T.Term Python v -> T.Term Python v -> T.Term Python v
-pattern ACons'' a as = ACons' T.:$: T.Cons a (T.Cons as T.Nil)
+pattern ACons :: T.Term Python v -> T.Term Python v -> T.Term Python v
+pattern ACons a as = ACons' T.:$: T.Cons a (T.Cons as T.Nil)
 
-pattern Locate'' :: Span -> T.Term Python v -> T.Term Python v
-pattern Locate'' s t = Locate' s T.:$: T.Cons t T.Nil
+pattern Locate :: Span -> T.Term Python v -> T.Term Python v
+pattern Locate s t = Locate' s T.:$: T.Cons t T.Nil
 
-{-# COMPLETE Noop'', Iff'', Bool'', String'', Throw'', Let'', (:>>>>), Import'', Function'', Call'', Locate'' #-}
+{-# COMPLETE Noop, Iff, Bool, String, Throw, Let, (:>>), Import, Function, Call, Locate #-}
 
 
 -- Abstract interpretation
@@ -123,26 +123,26 @@ eval
   -> (T.Term Python Name -> m val)
 eval eval = \case
   T.Var n     -> lookupEnv n >>= maybe (dvar n) fetch
-  Noop''      -> dunit
-  Iff'' c t e -> do
+  Noop      -> dunit
+  Iff c t e -> do
     c' <- eval c
     dif c' (eval t) (eval e)
-  Bool'' b    -> dbool b
-  String'' s  -> dstring s
-  Throw'' e   -> eval e >>= ddie
-  Let'' n v b -> do
+  Bool b    -> dbool b
+  String s  -> dstring s
+  Throw e   -> eval e >>= ddie
+  Let n v b -> do
     v' <- eval v
     let' n v' (eval b)
-  t :>>>> u   -> do
+  t :>> u   -> do
     t' <- eval t
     u' <- eval u
     t' >>> u'
-  Import'' ns -> S.simport ns >> dunit
-  Function'' n ps b -> letrec n (dabs ps (foldr (\ (p, a) m -> let' p a m) (eval b) . zip ps))
-  Call'' f as -> do
+  Import ns -> S.simport ns >> dunit
+  Function n ps b -> letrec n (dabs ps (foldr (\ (p, a) m -> let' p a m) (eval b) . zip ps))
+  Call f as -> do
     f' <- eval f
     as' <- traverse eval as
     dapp f' as'
-  Locate'' s t -> local (setSpan s) (eval t)
+  Locate s t -> local (setSpan s) (eval t)
   where
   setSpan s r = r{ refSpan = s }
