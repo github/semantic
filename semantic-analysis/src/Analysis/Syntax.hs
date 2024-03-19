809ab74d192ded23bdc50b176ad8fae49d8232f2
Rob Rix <robrix@github.com>
2024-03-19 14:55:18 -0400

    UndecidableInstances instead of QuantifiedConstraints.
    I'd rather do this than fuss with CI for old versions of the compiler.

--- semantic-analysis/src/Analysis/Syntax.hs
+++ semantic-analysis/src/Analysis/Syntax.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Analysis.Syntax
 ( -- * Syntax
   Term(..)
@@ -18,19 +18,19 @@ data Term sig v
   = Var v
   | Term (sig (Term sig v))
 
-instance (forall t . Eq t => Eq (sig t), Eq v) => Eq (Term sig v) where
+instance (Eq (sig (Term sig v)), Eq v) => Eq (Term sig v) where
   Var v1  == Var v2  = v1 == v2
   Term s1 == Term s2 = s1 == s2
   _       == _       = False
 
-instance (forall t . Eq t => Eq (sig t), forall t. Ord t => Ord (sig t), Ord v) => Ord (Term sig v) where
+instance (Ord (sig (Term sig v)), Ord v) => Ord (Term sig v) where
   compare (Var v1)  (Var v2)  = compare v1 v2
   compare (Var _)   _         = LT
   compare (Term s1) (Term s2) = compare s1 s2
   compare _          _        = GT
 
 
-subterms :: (forall t . Eq t => Eq (sig t), forall t . Ord t => Ord (sig t), Ord v, Foldable sig, Functor sig) => Term sig v -> Set.Set (Term sig v)
+subterms :: (Ord (sig (Term sig v)), Ord v, Foldable sig, Functor sig) => Term sig v -> Set.Set (Term sig v)
 subterms = paraTerm (Set.singleton . Var) (foldMap (uncurry Set.insert))
 
 foldTerm :: Functor sig => (v -> r) -> (sig r -> r) -> (Term sig v -> r)
